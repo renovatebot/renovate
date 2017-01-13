0b7d196bfac525c44c2dee3a003ca3f987c81b88
Rhys Arkins <rhys@keylocation.sg>
2017-01-13 16:06:30 +0100

    Add optionalDependencies to defaults

--- app/config/defaults.js
+++ app/config/defaults.js
@@ -1,6 +1,6 @@
 module.exports = {
   logLevel: 'info',
-  depTypes: ['dependencies', 'devDependencies'],
+  depTypes: ['dependencies', 'devDependencies', 'optionalDependencies'],
   templates: {
     branchName: params => `renovate/${params.depName}-${params.newVersionMajor}.x`,
     commitMessage: params => `Update dependency ${params.depName} to version ${params.newVersion}`,
