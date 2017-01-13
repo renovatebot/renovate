e4c44ba407a01aabeb971b43ad1d1fe1f366a98e
Rhys Arkins <rhys@keylocation.sg>
2017-01-13 21:57:33 +0100

    Use commander for CLI

--- app/config/parser.js
+++ app/config/parser.js
@@ -1,4 +1,6 @@
 const winston = require('winston');
+const program = require('commander');
+
 // Add the logger
 const logger = new (winston.Logger)({
   transports: [
@@ -15,36 +17,65 @@ module.exports = {
 
 let config = null;
 
+// This function reads in all configs and combines them
 function getGlobalConfig() {
-  // This function reads in all configs and merges them
-  /* eslint-disable global-require */
+  // eslint-disable-next-line global-require
   const defaultConfig = require('./defaults');
+  // save default templates for cascading later
+  defaultConfig.defaultTemplates = defaultConfig.templates;
+
+  // Custom config file options - optional
   let customConfig = {};
   try {
-    customConfig = require('../../config'); // eslint-disable-line import/no-unresolved
+    // eslint-disable-next-line import/no-unresolved,global-require
+    customConfig = require('../../config');
   } catch (err) {
     // Do nothing
     logger.verbose('No custom config found');
   }
-  /* eslint-enable global-require */
-  const cliConfig = {};
+
+  // Environment variables
+  const envConfig = {};
   if (process.env.LOG_LEVEL) {
-    cliConfig.logLevel = process.env.LOG_LEVEL;
+    envConfig.logLevel = process.env.LOG_LEVEL;
   }
   if (process.env.RENOVATE_TOKEN) {
-    cliConfig.token = process.env.RENOVATE_TOKEN;
+    envConfig.token = process.env.RENOVATE_TOKEN;
   }
-  // Check if repository name and package file are provided via CLI
-  if (process.argv[2]) {
+
+  // Parse any CLI commands
+  const cliConfig = {};
+  program
+  .arguments('[repository] [fileName]')
+  .option('--dep-types <types>', 'List of dependency types')
+  .option('--labels <labels>', 'List of labels to apply')
+  .option('--log-level <level>', 'Log Level')
+  .option('--token <token>', 'GitHub Auth Token')
+  .action((repository, fileName) => {
     cliConfig.repositories = [
       {
-        repository: process.argv[2],
-        packageFiles: [process.argv[3] || 'package.json'],
+        repository,
+        packageFiles: [fileName || 'package.json'],
       },
     ];
+  })
+  .parse(process.argv);
+
+  if (program.depTypes) {
+    cliConfig.depTypes = program.depTypes.split(',');
   }
+  if (program.labels) {
+    cliConfig.labels = program.labels.split(',');
+  }
+  if (program.logLevel) {
+    cliConfig.logLevel = program.logLevel;
+  }
+  if (program.token) {
+    cliConfig.token = program.token;
+  }
+
   // Set global config
-  config = Object.assign({}, defaultConfig, customConfig, cliConfig);
+  config = Object.assign({}, defaultConfig, customConfig, envConfig, cliConfig);
   // Set log level
   logger.level = config.logLevel;
 
@@ -84,9 +115,6 @@ function getGlobalConfig() {
 
   // Add logger to config
   config.logger = logger;
-
-  // Save default templates
-  config.defaultTemplates = defaultConfig.templates;
 
   return config;
 }
