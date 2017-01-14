215d59660b0790cc6cef1e9bcadda196377def97
Rhys Arkins <rhys@keylocation.sg>
2017-01-14 19:47:18 +0100

    cron

--- bin/heroku/clock.js
+++ bin/heroku/clock.js
@@ -1,7 +1,7 @@
 const CronJob = require('cron').CronJob;
 
 new CronJob({
-  cronTime: '0 * * * *', // Every hour
+  cronTime: '48 * * * *', // Every hour
   onTick: require('../../app/index.js'),
   start: true,
   timeZone: 'America/Los_Angeles',
