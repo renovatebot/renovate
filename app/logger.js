7bcfc4687974dbf2084aead1c23c19346dff7219
Rhys Arkins <rhys@keylocation.sg>
2017-01-14 14:38:28 +0100

    Fix logger.level

--- app/logger.js
+++ app/logger.js
@@ -1,10 +1,8 @@
 const logger = require('winston');
 
-// Set log level from environment. It may be overriden later by configuration
-logger.level = process.env.LOG_LEVEL || 'info';
-
 // Colorize console logs
 logger.configure({
+  level: process.env.LOG_LEVEL || 'info',
   transports: [
     new (logger.transports.Console)({ colorize: true }),
   ],
