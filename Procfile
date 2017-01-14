fcf2b70cc25ebbd85ad07f1347d276b60d1d9525
Rhys Arkins <rhys@keylocation.sg>
2017-01-14 19:55:06 +0100

    Drop worker and clock

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: node bin/heroku/web.js
-worker: node app/index.js
-clock: node bin/heroku/clock.js
+removate: node app/index.js
--- bin/heroku/clock.js
+++
@@ -1,8 +0,0 @@
-const CronJob = require('cron').CronJob;
-
-new CronJob({
-  cronTime: '48 * * * *', // Every hour
-  onTick: require('../../app/index.js'),
-  start: true,
-  timeZone: 'America/Los_Angeles',
-});
