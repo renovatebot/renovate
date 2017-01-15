0f13d75073d7a450e7f4cb79612cb5cd157461ea
Rhys Arkins <rhys@keylocation.sg>
2017-01-15 15:09:20 +0100

    Rename renovate file to worker

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-require('./renovate');
+require('./worker');
 require('./helpers/github');
 require('./helpers/npm');
 require('./helpers/package-json');
---
+++ test/worker.js
@@ -0,0 +1,1 @@
+const renovate = require('../lib/worker');
