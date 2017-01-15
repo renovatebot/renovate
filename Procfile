0a0def87d6a16d81ddd35376f8a1e5cc99d54989
Rhys Arkins <rhys@keylocation.sg>
2017-01-15 16:21:27 +0100

    Clean up

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: node bin/heroku/web.js
-worker: node app/renovate.js
-clock: node bin/heroku/clock.js
--- bin/heroku/clock.js
+++
@@ -1,9 +0,0 @@
-const CronJob = require('cron').CronJob;
-const renovate = require('../../app/renovate');
-
-new CronJob({
-  cronTime: '*/5 * * * *', // Every 5 mins
-  onTick: renovate.start(),
-  start: true,
-  timeZone: 'America/Los_Angeles',
-});
