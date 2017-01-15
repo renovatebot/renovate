3808a05d808039344b0aa3e2d25582ece3f50b38
Rhys Arkins <rhys@keylocation.sg>
2017-01-15 08:52:21 +0100

    Allow config file specification in env

--- app/config/file.js
+++ app/config/file.js
@@ -1,14 +1,23 @@
 const logger = require('winston');
 
+let configFile = process.env.RENOVATE_CONFIG_FILE || 'config';
+if (!isPathAbsolute(configFile)) {
+  configFile = `../../${configFile}`;
+}
+
 let config = {};
 try {
-  // eslint-disable-next-line import/no-unresolved,global-require
-  config = require('../../config');
+  // eslint-disable-next-line global-require,import/no-dynamic-require
+  config = require(configFile);
 } catch (err) {
   // Do nothing
-  logger.verbose('No custom config found');
+  logger.verbose('Could not locate config file');
 }
 
 logger.debug(`File config = ${JSON.stringify(config)}`);
 
 module.exports = config;
+
+function isPathAbsolute(path) {
+  return /^(?:\/|[a-z]+:\/\/)/.test(path);
+}
