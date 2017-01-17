49d3081ca4f8e4b597317c16bf9e2f39c2473849
Rhys Arkins <rhys@keylocation.sg>
2017-01-18 00:53:17 +0100

    Add helper

---
+++ test/helpers/handlebars.js
@@ -0,0 +1,15 @@
+const handlebars = require('../../lib/helpers/handlebars');
+
+const template = 'renovate/{{ depName }}-{{ newVersionMajor }}.x';
+const upgrade = {
+  depName: 'q',
+  newVersionMajor: 1,
+};
+
+describe('helpers/handlebars', () => {
+  describe('.transform(template, params)', () => {
+    it('should work', () => {
+      handlebars.transform(template, upgrade).should.eql('renovate/q-1.x');
+    });
+  });
+});
