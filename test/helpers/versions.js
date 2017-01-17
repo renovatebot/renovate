2e8721ebeea57f1a5d78bcf2801654e0bec654af
Rhys Arkins <rhys@keylocation.sg>
2017-01-17 12:46:32 +0100

    Don’t upgrade to future versions unnecessarily
    Closes #55
    
    commit 4914c5bbf401711b61d9a54330532175a51a167c
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Tue Jan 17 12:45:45 2017 +0100
    
        Update debug
    
    commit 55c8307381397399afda01038b6c50416cba27a3
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Tue Jan 17 12:44:08 2017 +0100
    
        Ignore versions newer than “latest”
    
    commit 8aa3e7daa937c0d86f89e1d704853ebee46a54a8
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Tue Jan 17 11:17:31 2017 +0100
    
        Ignore futures if current is not future

--- test/helpers/versions.js
+++ test/helpers/versions.js
@@ -3,61 +3,69 @@ const versionsHelper = require('../../lib/helpers/versions');
 
 chai.should();
 
+const qJson = require('../_fixtures/npm/01.json');
+
 describe('helpers/versions', () => {
-  describe('.determineUpgrades(depName, currentVersion, versions)', () => {
-    const testVersions = {
-      '0.1.0': {},
-      '1.0.0': {},
-      '1.0.1': {},
-      '1.1.0': {},
-      '2.0.0-alpha1': {},
-      '2.0.0': {},
-      '2.0.1': {},
-      '3.0.0': {},
-      '3.1.0': {},
-    };
+  describe('.determineUpgrades(dep, currentVersion)', () => {
     it('return empty if invalid current version', () => {
-      versionsHelper.determineUpgrades('foo', 'invalid', { '1.0.0': {} }).should.have.length(0);
+      versionsHelper.determineUpgrades(qJson, 'invalid').should.have.length(0);
     });
     it('return empty if null versions', () => {
-      versionsHelper.determineUpgrades('foo', '1.0.0', null).should.have.length(0);
+      const testDep = {
+        name: 'q',
+      };
+      versionsHelper.determineUpgrades(testDep, '1.0.0').should.have.length(0);
     });
     it('return empty if empty versions', () => {
-      versionsHelper.determineUpgrades('foo', '1.0.0', []).should.have.length(0);
+      const testDep = {
+        name: 'q',
+        versions: [],
+      };
+      versionsHelper.determineUpgrades(testDep, '1.0.0', []).should.have.length(0);
     });
-    it('supports minor and major upgrades, including for ranges', () => {
+    it('supports minor and major upgrades for pinned versions', () => {
       const upgradeVersions = [
         {
-          newVersion: '1.1.0',
-          newVersionMajor: 1,
+          newVersion: '0.9.7',
+          newVersionMajor: 0,
           upgradeType: 'minor',
-          workingVersion: '1.0.1',
+          workingVersion: '0.4.4',
         },
         {
-          newVersion: '2.0.1',
-          newVersionMajor: 2,
+          newVersion: '1.4.1',
+          newVersionMajor: 1,
           upgradeType: 'major',
-          workingVersion: '1.0.1',
-        },
-        {
-          newVersion: '3.1.0',
-          newVersionMajor: 3,
-          upgradeType: 'major',
-          workingVersion: '1.0.1',
+          workingVersion: '0.4.4',
         },
       ];
-      versionsHelper.determineUpgrades('foo', '1.0.1', testVersions).should.eql(upgradeVersions);
-      versionsHelper.determineUpgrades('foo', '~1.0.1', testVersions).should.eql(upgradeVersions);
+      versionsHelper.determineUpgrades(qJson, '^0.4.0').should.eql(upgradeVersions);
     });
-    it('supports pinning', () => {
+    it('supports minor and major upgrades for ranged versions', () => {
+      const pinVersions = [
+        {
+          newVersion: '0.9.7',
+          newVersionMajor: 0,
+          upgradeType: 'minor',
+          workingVersion: '0.4.4',
+        },
+        {
+          newVersion: '1.4.1',
+          newVersionMajor: 1,
+          upgradeType: 'major',
+          workingVersion: '0.4.4',
+        },
+      ];
+      versionsHelper.determineUpgrades(qJson, '~0.4.0').should.eql(pinVersions);
+    });
+    it('supports future versions', () => {
       const upgradeVersions = [
         {
-          newVersion: '3.1.0',
-          newVersionMajor: 3,
+          newVersion: '2.0.3',
+          newVersionMajor: 2,
           upgradeType: 'pin',
         },
       ];
-      versionsHelper.determineUpgrades('foo', '^3.0.0', testVersions).should.eql(upgradeVersions);
+      versionsHelper.determineUpgrades(qJson, '^2.0.0').should.eql(upgradeVersions);
     });
   });
   describe('.isRange(input)', () => {
@@ -90,6 +98,17 @@ describe('helpers/versions', () => {
       versionsHelper.isValidVersion('singapore/renovate').should.eql(false);
       versionsHelper.isValidVersion('singapore/renovate#master').should.eql(false);
       versionsHelper.isValidVersion('https://github.com/singapore/renovate.git').should.eql(false);
+    });
+  });
+  describe('.isPastLatest(dep, version)', () => {
+    it('should return false for less than', () => {
+      versionsHelper.isPastLatest(qJson, '1.0.0').should.eql(false);
+    });
+    it('should return false for equal', () => {
+      versionsHelper.isPastLatest(qJson, '1.4.1').should.eql(false);
+    });
+    it('should return true for greater than', () => {
+      versionsHelper.isPastLatest(qJson, '2.0.3').should.eql(true);
     });
   });
 });
