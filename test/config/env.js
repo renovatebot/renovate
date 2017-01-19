bff0bf63b6dbbd814420f3c5810f91b46171a4db
Rhys Arkins <rhys@keylocation.sg>
2017-01-19 18:34:21 +0100

    env

---
+++ test/config/env.js
@@ -0,0 +1,19 @@
+const env = require('../../lib/config/env.js');
+
+describe('config/env', () => {
+  describe('.getEnvName(definition)', () => {
+    it('returns existing env', () => {
+      const option = {
+        name: 'foo',
+        env: 'FOO',
+      };
+      env.getEnvName(option).should.eql('FOO');
+    });
+    it('generates RENOVATE_ env', () => {
+      const option = {
+        name: 'oneTwoThree',
+      };
+      env.getEnvName(option).should.eql('RENOVATE_ONE_TWO_THREE');
+    });
+  });
+});
