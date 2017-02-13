768c50899770ac47d8d352748b147e4d9c5750dc
Rhys Arkins <rhys@keylocation.sg>
2017-02-13 20:06:15 +0100

    Add tests

--- test/workers/branch.spec.js
+++ test/workers/branch.spec.js
@@ -1,5 +1,74 @@
-require('../../lib/workers/branch');
+const branchWorker = require('../../lib/workers/branch');
 
-it('placeholder', () => {
-  // TODO: write tests for this module - this is here so the file shows up in coverage
+describe('workers/branch', () => {
+  describe('useBaseBranch(branchName, config)', () => {
+    let config;
+    // const branchName = 'renovate/foo';
+    beforeEach(() => {
+      config = {
+        api: {
+          getBranchPr: jest.fn(),
+        },
+      };
+    });
+    it('returns false if no PR', async () => {
+      config.api.getBranchPr.mockReturnValueOnce(null);
+      expect(await branchWorker.useBaseBranch('', config)).toBe(false);
+    });
+    /*
+    it('returns false if unmergeable and cannot rebase', () => {
+      const pr = {
+        isUnmergeable: true,
+        canRebase: false,
+      };
+      branchWorker.useBaseBranch(pr, {}).should.eql(false);
+    });
+    it('returns true if unmergeable and can rebase', () => {
+      const pr = {
+        isUnmergeable: true,
+        canRebase: true,
+      };
+      branchWorker.useBaseBranch(pr, {}).should.eql(true);
+    });
+    it('returns false if stale but not configured to rebase', () => {
+      const pr = {
+        isStale: true,
+        canRebase: true,
+      };
+      config.rebaseStalePrs = false;
+      branchWorker.useBaseBranch(pr, config).should.eql(false);
+    });
+    it('returns false if stale but cannot rebase', () => {
+      const pr = {
+        isStale: true,
+        canRebase: false,
+      };
+      config.rebaseStalePrs = true;
+      branchWorker.useBaseBranch(pr, config).should.eql(false);
+    });
+    it('returns true if stale and rebase stale configured', () => {
+      const pr = {
+        isStale: true,
+        canRebase: true,
+      };
+      config.rebaseStalePrs = true;
+      branchWorker.useBaseBranch(pr, config).should.eql(true);
+    });
+    */
+  });
+  describe('getYarnLockFile(packageJson, config)', () => {
+    let config;
+    beforeEach(() => {
+      config = {
+        packageFile: 'package.json',
+        api: {
+          getFileContent: jest.fn(),
+        },
+      };
+    });
+    it('returns null if no existing yarn.lock', async () => {
+      config.api.getFileContent.mockReturnValueOnce(false);
+      expect(await branchWorker.getYarnLockFile('', config)).toBe(null);
+    });
+  });
 });
