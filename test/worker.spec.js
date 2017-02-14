ef30a73c3ad51a5b2ca64388f19bd4c16e83c6bf
Rhys Arkins <rhys@keylocation.sg>
2017-02-14 22:04:57 +0100

    Refactor for more worker tests (#104)

--- test/worker.spec.js
+++ test/worker.spec.js
@@ -1,5 +1,108 @@
-require('../lib/worker');
+const logger = require('winston');
+const worker = require('../lib/worker');
+const branchWorker = require('../lib/workers/branch');
+const prWorker = require('../lib/workers/pr');
+const defaultConfig = require('../lib/config/defaults').getConfig();
+const npmApi = require('../lib/api/npm');
+const versionsHelper = require('../lib/helpers/versions');
 
-it('placeholder', () => {
-  // TODO: write tests for this module - this is here so the file shows up in coverage
+logger.remove(logger.transports.Console);
+
+jest.mock('../lib/workers/branch');
+jest.mock('../lib/workers/pr');
+jest.mock('../lib/api/npm');
+jest.mock('../lib/helpers/versions');
+
+describe('worker', () => {
+  describe('updateDependency(upgrade)', () => {
+    let config;
+    beforeEach(() => {
+      config = Object.assign({}, defaultConfig);
+      config.api = {
+        checkForClosedPr: jest.fn(),
+      };
+      branchWorker.ensureBranch = jest.fn();
+      prWorker.ensurePr = jest.fn();
+    });
+    it('returns immediately if closed PR found', async () => {
+      config.api.checkForClosedPr.mockReturnValue(true);
+      await worker.updateDependency(config);
+      expect(branchWorker.ensureBranch.mock.calls.length).toBe(0);
+    });
+    it('does not return immediately if recreateClosed true', async () => {
+      config.api.checkForClosedPr.mockReturnValue(true);
+      config.recreateClosed = true;
+      await worker.updateDependency(config);
+      expect(branchWorker.ensureBranch.mock.calls.length).toBe(1);
+    });
+    it('pins', async () => {
+      config.upgradeType = 'pin';
+      await worker.updateDependency(config);
+      expect(branchWorker.ensureBranch.mock.calls.length).toBe(1);
+    });
+    it('majors', async () => {
+      config.upgradeType = 'major';
+      await worker.updateDependency(config);
+      expect(branchWorker.ensureBranch.mock.calls.length).toBe(1);
+    });
+    it('minors', async () => {
+      config.upgradeType = 'minor';
+      await worker.updateDependency(config);
+      expect(branchWorker.ensureBranch.mock.calls.length).toBe(1);
+    });
+    it('handles errors', async () => {
+      config.api.checkForClosedPr = jest.fn(() => {
+        throw new Error('oops');
+      });
+      await worker.updateDependency(config);
+      expect(branchWorker.ensureBranch.mock.calls.length).toBe(0);
+    });
+  });
+  describe('processUpgradesSequentially(baseConfig, upgrades)', () => {
+    let config;
+    beforeEach(() => {
+      config = {};
+      worker.updateDependency = jest.fn();
+    });
+    it('handles zero upgrades', async () => {
+      await worker.processUpgradesSequentially(config, []);
+      expect(worker.updateDependency.mock.calls.length).toBe(0);
+    });
+    it('handles non-zero upgrades', async () => {
+      await worker.processUpgradesSequentially(config, [{}, {}]);
+      expect(worker.updateDependency.mock.calls.length).toBe(2);
+    });
+  });
+  describe('findUpgrades(dependencies, config)', () => {
+    let config;
+    beforeEach(() => {
+      config = {};
+      worker.updateDependency = jest.fn();
+    });
+    it('handles null', async () => {
+      const allUpgrades = await worker.findUpgrades([], config);
+      expect(allUpgrades).toMatchObject([]);
+    });
+    it('handles one dep', async () => {
+      const dep = {
+        depName: 'foo',
+        currentVersion: '1.0.0',
+      };
+      const upgrade = { newVersion: '1.1.0' };
+      npmApi.getDependency = jest.fn(() => ({}));
+      versionsHelper.determineUpgrades = jest.fn(() => [upgrade]);
+      const allUpgrades = await worker.findUpgrades([dep], config);
+      expect(allUpgrades).toMatchObject([Object.assign({}, dep, upgrade)]);
+    });
+    it('handles no upgrades', async () => {
+      const dep = {
+        depName: 'foo',
+        currentVersion: '1.0.0',
+      };
+      npmApi.getDependency = jest.fn(() => ({}));
+      versionsHelper.determineUpgrades = jest.fn(() => []);
+      const allUpgrades = await worker.findUpgrades([dep], config);
+      expect(allUpgrades).toMatchObject([]);
+    });
+  });
 });
