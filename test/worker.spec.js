c9fe8463a405efa04249e517418b2ac23749cd3d
Rhys Arkins <rhys@keylocation.sg>
2017-04-15 11:04:24 +0200

    More package tests

--- test/worker.spec.js
+++ test/worker.spec.js
@@ -189,6 +189,23 @@ describe('worker', () => {
       const updatedDeps = worker.assignDepConfigs(config, deps);
       expect(updatedDeps).toMatchSnapshot();
     });
+    it('nested package config overrides depType and general config', () => {
+      config.foo = 'bar';
+      config.depTypes = [{
+        depType: 'dependencies',
+        foo: 'beta',
+        packages: [{
+          packageName: 'a',
+          foo: 'gamma',
+        }],
+      }];
+      deps.push({
+        depName: 'a',
+        depType: 'dependencies',
+      });
+      const updatedDeps = worker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
   });
   describe('getDepTypeConfig(depTypes, depTypeName)', () => {
     it('handles empty depTypes', () => {
