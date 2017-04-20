b7155e53d6bff91928f160c0b0f59d08ac624877
Rhys Arkins <rhys@keylocation.sg>
2017-04-20 10:00:46 +0200

    Refactor yarn

--- test/helpers/yarn.spec.js
+++ test/helpers/yarn.spec.js
@@ -1,5 +1,30 @@
-require('../../lib/helpers/yarn');
+const yarnHelper = require('../../lib/helpers/yarn');
 
-it('placeholder', () => {
-  // TODO: write tests for this module - this is here so the file shows up in coverage
+describe('generateLockFile(newPackageJson, npmrcContent, yarnrcContent)', () => {});
+
+describe('getLockFile(packageJson, config)', () => {
+  let api;
+  beforeEach(() => {
+    api = {
+      getFileContent: jest.fn(),
+    };
+  });
+  it('returns null if no existing yarn.lock', async () => {
+    api.getFileContent.mockReturnValueOnce(false);
+    expect(await yarnHelper.getLockFile('package.json', '', api)).toBe(null);
+  });
+  it('returns yarn.lock file', async () => {
+    api.getFileContent.mockReturnValueOnce('Existing yarn.lock');
+    api.getFileContent.mockReturnValueOnce(null); // npmrc
+    api.getFileContent.mockReturnValueOnce(null); // yarnrc
+    yarnHelper.generateLockFile = jest.fn();
+    yarnHelper.generateLockFile.mockReturnValueOnce('New yarn.lock');
+    const yarnLockFile = {
+      name: 'yarn.lock',
+      contents: 'New yarn.lock',
+    };
+    expect(await yarnHelper.getLockFile('package.json', '', api)).toMatchObject(yarnLockFile);
+  });
 });
+
+describe('maintainLockFile(inputConfig)', () => {});
