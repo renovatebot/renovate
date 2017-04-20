3312fd027d100e7017e855f70d1edad810a388c7
Rhys Arkins <rhys@keylocation.sg>
2017-04-20 14:32:26 +0200

    Drop tests

--- test/config/index.spec.js
+++ test/config/index.spec.js
@@ -1,10 +1,5 @@
 const argv = require('../_fixtures/config/argv');
 const should = require('chai').should();
-const githubApi = require('../../lib/api/github');
-const gitlabApi = require('../../lib/api/gitlab');
-
-githubApi.getRepos = jest.fn();
-gitlabApi.getRepos = jest.fn();
 
 describe('config/index', () => {
   describe('.parseConfigs(env, defaultArgv)', () => {
@@ -45,16 +40,6 @@ describe('config/index', () => {
         err = e;
       }
       expect(err.message).toBe('At least one repository must be configured');
-    });
-    it('autodiscovers github repos', async () => {
-      const env = { GITHUB_TOKEN: 'abc' };
-      defaultArgv = defaultArgv.concat(['--autodiscover']);
-      await configParser.parseConfigs(env, defaultArgv);
-    });
-    it('autodiscovers gitlab repos', async () => {
-      const env = { GITLAB_TOKEN: 'abc' };
-      defaultArgv = defaultArgv.concat(['--autodiscover', '--platform=gitlab']);
-      await configParser.parseConfigs(env, defaultArgv);
     });
     it('errors for unknown platform', async () => {
       const env = { GITHUB_TOKEN: 'abc' };
