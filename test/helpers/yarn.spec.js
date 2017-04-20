51526ee07d9e6d5c08f8ef5a71d6fcd147aa638d
Rhys Arkins <rhys@keylocation.sg>
2017-04-20 11:49:42 +0200

    Update tests

--- test/helpers/yarn.spec.js
+++ test/helpers/yarn.spec.js
@@ -1,7 +1,31 @@
 const yarnHelper = require('../../lib/helpers/yarn');
+const defaultConfig = require('../../lib/config/defaults').getConfig();
 
-describe('generateLockFile(newPackageJson, npmrcContent, yarnrcContent)', () => {});
+jest.mock('fs');
+jest.mock('child_process');
+jest.mock('tmp');
 
+const fs = require('fs');
+const cp = require('child_process');
+const tmp = require('tmp');
+
+describe('generateLockFile(newPackageJson, npmrcContent, yarnrcContent)', () => {
+  tmp.dirSync = jest.fn(() => ({ name: 'somedir' }));
+  fs.writeFileSync = jest.fn();
+  fs.readFileSync = jest.fn(() => 'yarn-lock-contents');
+  cp.spawnSync = jest.fn(() => ({
+    stdout: '',
+    stderror: '',
+  }));
+  it('generates lock files', async () => {
+    const yarnLock =
+      await yarnHelper.generateLockFile('package-json-contents', 'npmrc-contents', 'yarnrc-contents');
+    expect(tmp.dirSync.mock.calls.length).toEqual(1);
+    expect(fs.writeFileSync.mock.calls.length).toEqual(3);
+    expect(fs.readFileSync.mock.calls.length).toEqual(1);
+    expect(yarnLock).toEqual('yarn-lock-contents');
+  });
+});
 describe('getLockFile(packageJson, config)', () => {
   let api;
   beforeEach(() => {
@@ -27,4 +51,34 @@ describe('getLockFile(packageJson, config)', () => {
   });
 });
 
-describe('maintainLockFile(inputConfig)', () => {});
+describe('maintainLockFile(inputConfig)', () => {
+  let config;
+  beforeEach(() => {
+    config = Object.assign({}, defaultConfig);
+    config.packageFile = 'package.json';
+    config.api = {
+      getFileContent: jest.fn(),
+    };
+    config.api.getFileContent.mockReturnValueOnce('oldPackageContent');
+    yarnHelper.getLockFile = jest.fn();
+  });
+  it('returns null if no file to maintain', async () => {
+    const yarnLock = await yarnHelper.maintainLockFile(config);
+    expect(config.api.getFileContent.mock.calls.length).toBe(2);
+    expect(yarnLock).toEqual(null);
+  });
+  it('returns null if contents match', async () => {
+    config.api.getFileContent.mockReturnValueOnce('oldYarnLockContent');
+    yarnHelper.getLockFile.mockReturnValueOnce({ contents: 'oldYarnLockContent' });
+    const yarnLock = await yarnHelper.maintainLockFile(config);
+    expect(config.api.getFileContent.mock.calls.length).toBe(2);
+    expect(yarnLock).toEqual(null);
+  });
+  it('returns new yarn lock if contents differ', async () => {
+    config.api.getFileContent.mockReturnValueOnce('oldYarnLockContent');
+    yarnHelper.getLockFile.mockReturnValueOnce({ contents: 'newYarnLockContent' });
+    const yarnLock = await yarnHelper.maintainLockFile(config);
+    expect(config.api.getFileContent.mock.calls.length).toBe(2);
+    expect(yarnLock).toEqual({ contents: 'newYarnLockContent' });
+  });
+});
