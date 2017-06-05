88648ab4a5676063cf3bfba149932a4ce44d0619
Rhys Arkins <rhys@keylocation.sg>
2017-06-05 12:13:39 +0200

    Add npm caching (#266)
    * use snapshots
    
    * Use npm cache
    
    Closes #246
    
    * Add test for caching

--- test/api/npm.spec.js
+++ test/api/npm.spec.js
@@ -7,17 +7,36 @@ jest.mock('registry-url');
 jest.mock('registry-auth-token');
 jest.mock('got');
 
+const npmResponse = {
+  body: {
+    versions: {
+      '0.0.1': {
+        foo: 1,
+      },
+    },
+  },
+};
+
 describe('api/npm', () => {
   beforeEach(() => {
     jest.resetAllMocks();
+    npm.resetCache();
   });
   it('should fetch package info from npm', async () => {
     registryUrl.mockImplementation(() => 'https://npm.mycustomregistry.com/');
-    got.mockImplementation(() => Promise.resolve({ body: { some: 'data' } }));
+    got.mockImplementation(() => Promise.resolve(npmResponse));
     const res = await npm.getDependency('foobar');
-    expect(res).toMatchObject({ some: 'data' });
+    expect(res).toMatchSnapshot();
     const call = got.mock.calls[0];
     expect(call).toMatchSnapshot();
+  });
+  it('should cache package info from npm', async () => {
+    registryUrl.mockImplementation(() => 'https://npm.mycustomregistry.com/');
+    got.mockImplementation(() => Promise.resolve(npmResponse));
+    const res1 = await npm.getDependency('foobar');
+    const res2 = await npm.getDependency('foobar');
+    expect(res1).toEqual(res2);
+    expect(got.mock.calls.length).toEqual(1);
   });
   it('should return null if lookup fails', async () => {
     registryUrl.mockImplementation(() => 'https://npm.mycustomregistry.com/');
@@ -33,9 +52,9 @@ describe('api/npm', () => {
       type: 'Basic',
       token: '1234',
     }));
-    got.mockImplementation(() => Promise.resolve({ body: { some: 'data' } }));
+    got.mockImplementation(() => Promise.resolve(npmResponse));
     const res = await npm.getDependency('foobar');
-    expect(res).toMatchObject({ some: 'data' });
+    expect(res).toMatchSnapshot();
     const call = got.mock.calls[0];
     expect(call).toMatchSnapshot();
   });
