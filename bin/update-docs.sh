dce7f4c9a8d9a922dfb748327df72fa82b36835f
Rhys Arkins <rhys@keylocation.sg>
2017-06-07 14:38:45 +0200

    Drop usage from README and fix md lint (#276)

--- bin/update-docs.sh
+++ bin/update-docs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-perl -0777 -i -pe 's/\n  Usage:.*package-test\n/`node dist\/renovate --help`/se' readme.md
 perl -0777 -i -pe 's/\n  Usage:.*package-test\n/`node dist\/renovate --help`/se' docs/configuration.md
 perl -0777 -i -pe 's/## Configuration Options.*//se' docs/configuration.md
 node bin/update-configuration-table.js >> docs/configuration.md
