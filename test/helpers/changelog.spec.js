91856cec7a1d787e5d3367e04ec1ecaf902c0c1e
Rhys Arkins <rhys@keylocation.sg>
2017-06-13 11:08:37 +0200

    Refactor changelog logic to directly use JSON (#296)
    Closes #264 
    
    * Basic refactor
    
    * Refactor into module
    
    * Further refactor
    
    * Use json for group pr body
    
    * Add commit links
    
    * improve body
    
    * Refactor names
    
    * Add signature
    
    * Refactor config
    
    * Simplify non-group pr body template
    
    * Fix Pull/Merge terminology
    
    * Fix test
    
    * Fix pr test
    
    * Fix prettier

--- test/helpers/changelog.spec.js
+++ test/helpers/changelog.spec.js
@@ -1,31 +1,40 @@
 const changelog = require('changelog');
-const getChangeLog = require('../../lib/helpers/changelog');
+const changelogHelper = require('../../lib/helpers/changelog');
 
 jest.mock('changelog');
 
 describe('helpers/changelog', () => {
-  describe('getChangeLog(depName, fromVersion, newVersion)', () => {
+  describe('changelogHelper.getChangeLog(depName, fromVersion, newVersion)', () => {
     it('returns empty if no fromVersion', async () => {
-      expect(await getChangeLog('renovate', null, '1.0.0')).toBe('');
+      expect(
+        await changelogHelper.getChangeLog('renovate', null, '1.0.0')
+      ).toBe('No changelog available');
     });
     it('returns empty if fromVersion equals newVersion', async () => {
-      expect(await getChangeLog('renovate', '1.0.0', '1.0.0')).toBe('');
+      expect(
+        await changelogHelper.getChangeLog('renovate', '1.0.0', '1.0.0')
+      ).toBe('No changelog available');
     });
-    it('returns empty if generated markdown is null', async () => {
-      changelog.markdown.mockReturnValueOnce(null);
-      expect(await getChangeLog('renovate', '1.0.0', '2.0.0')).toBe('');
+    it('returns empty if generated json is null', async () => {
+      changelog.generate.mockReturnValueOnce(null);
+      expect(
+        await changelogHelper.getChangeLog('renovate', '1.0.0', '2.0.0')
+      ).toBe('No changelog available');
     });
     it('returns header if generated markdown is valid', async () => {
+      changelog.generate.mockReturnValueOnce({});
       changelog.markdown.mockReturnValueOnce('dummy');
-      expect(await getChangeLog('renovate', '1.0.0', '2.0.0')).toBe(
-        '### Changelog\n\ndummy'
-      );
+      expect(
+        await changelogHelper.getChangeLog('renovate', '1.0.0', '2.0.0')
+      ).toBe('### Changelog\n\ndummy');
     });
     it('returns empty if error thrown', async () => {
-      changelog.markdown = jest.fn(() => {
+      changelog.generate = jest.fn(() => {
         throw new Error('foo');
       });
-      expect(await getChangeLog('renovate', '1.0.0', '2.0.0')).toBe('');
+      expect(
+        await changelogHelper.getChangeLog('renovate', '1.0.0', '2.0.0')
+      ).toBe('No changelog available');
     });
   });
 });
