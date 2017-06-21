a141232191270974dd76ea0cd55b7115c822b38b
Rhys Arkins <rhys@keylocation.sg>
2017-06-21 19:45:07 +0200

    Refactor logger

---
+++ test/helpers/logger.spec.js
@@ -0,0 +1,9 @@
+const loggerHelper = require('../../lib/helpers/logger');
+
+describe('helpers/logger', () => {
+  describe('configSerializer(config)', () => {
+    it('something', async () => {
+      // nothing
+    });
+  });
+});
