6f3a4b19204adc4abc9abb5237291534701f6c75
Rhys Arkins <rhys@keylocation.sg>
2017-06-23 07:06:11 +0200

    Test mergeRenovateJson

---
+++ test/workers/repository/merge-renovate-json.spec.js
@@ -0,0 +1,31 @@
+const mergeRenovateJson = require('../../../lib/workers/repository/merge-renovate-json');
+const bunyan = require('bunyan');
+
+const logger = bunyan.createLogger({
+  name: 'test',
+  stream: process.stdout,
+  level: 'fatal',
+});
+
+describe('workers/repository/merge-renovate-json', () => {
+  describe('mergeRenovateJson(config)', () => {
+    let config;
+    beforeEach(() => {
+      config = {
+        api: {
+          getFileJson: jest.fn(),
+        },
+        logger,
+      };
+    });
+    it('returns same config if no renovate.json found', async () => {
+      expect(await mergeRenovateJson(config)).toEqual(config);
+    });
+    it('returns extended config if renovate.json found', async () => {
+      config.api.getFileJson.mockReturnValueOnce({ foo: 1 });
+      const returnConfig = await mergeRenovateJson(config);
+      expect(returnConfig.foo).toBe(1);
+      expect(returnConfig.renovateOnboarded).toBe(true);
+    });
+  });
+});
