481072e5a2b41055032eb45acfea6fd5f9368eb1
Rhys Arkins <rhys@keylocation.sg>
2017-06-24 11:34:11 +0200

    Recombine repository worker

--- test/workers/repository.spec.js
+++ test/workers/repository.spec.js
@@ -1,9 +1,27 @@
-const onboarding = require('../../../lib/workers/repository/onboarding');
-const logger = require('../../_fixtures/logger');
+const repositoryWorker = require('../../lib/workers/repository');
+const logger = require('../_fixtures/logger');
 
-jest.mock('../../../lib/api/github');
-
-describe('workers/repository/onboarding', () => {
+describe('workers/repository', () => {
+  describe('mergeRenovateJson(config)', () => {
+    let config;
+    beforeEach(() => {
+      config = {
+        api: {
+          getFileJson: jest.fn(),
+        },
+        logger,
+      };
+    });
+    it('returns same config if no renovate.json found', async () => {
+      expect(await repositoryWorker.mergeRenovateJson(config)).toEqual(config);
+    });
+    it('returns extended config if renovate.json found', async () => {
+      config.api.getFileJson.mockReturnValueOnce({ foo: 1 });
+      const returnConfig = await repositoryWorker.mergeRenovateJson(config);
+      expect(returnConfig.foo).toBe(1);
+      expect(returnConfig.renovateJsonPresent).toBe(true);
+    });
+  });
   describe('getOnboardingStatus(config)', () => {
     let config;
     beforeEach(() => {
@@ -18,30 +36,30 @@ describe('workers/repository/onboarding', () => {
     });
     it('returns true if onboarding is false', async () => {
       config.onboarding = false;
-      const res = await onboarding.getOnboardingStatus(config);
+      const res = await repositoryWorker.getOnboardingStatus(config);
       expect(res).toEqual(true);
       expect(config.api.findPr.mock.calls.length).toBe(0);
     });
     it('returns complete if renovate onboarded', async () => {
       config.renovateJsonPresent = true;
-      const res = await onboarding.getOnboardingStatus(config);
+      const res = await repositoryWorker.getOnboardingStatus(config);
       expect(res).toEqual(true);
       expect(config.api.findPr.mock.calls.length).toBe(0);
     });
     it('returns complete if pr and pr is closed', async () => {
       config.api.findPr.mockReturnValueOnce({ isClosed: true });
-      const res = await onboarding.getOnboardingStatus(config);
+      const res = await repositoryWorker.getOnboardingStatus(config);
       expect(res).toEqual(true);
       expect(config.api.findPr.mock.calls.length).toBe(1);
     });
     it('returns in progres if pr and pr is not closed', async () => {
       config.api.findPr.mockReturnValueOnce({});
-      const res = await onboarding.getOnboardingStatus(config);
+      const res = await repositoryWorker.getOnboardingStatus(config);
       expect(res).toEqual(false);
       expect(config.api.findPr.mock.calls.length).toBe(1);
     });
     it('returns none if no pr', async () => {
-      const res = await onboarding.getOnboardingStatus(config);
+      const res = await repositoryWorker.getOnboardingStatus(config);
       expect(res).toEqual(false);
       expect(config.api.findPr.mock.calls.length).toBe(1);
     });
@@ -59,7 +77,7 @@ describe('workers/repository/onboarding', () => {
     });
     it('should commit files and create PR', async () => {
       config.platform = 'github';
-      await onboarding.onboardRepository(config);
+      await repositoryWorker.onboardRepository(config);
       expect(config.api.commitFilesToBranch.mock.calls.length).toBe(1);
       expect(config.api.createPr.mock.calls.length).toBe(1);
       expect(
@@ -71,7 +89,7 @@ describe('workers/repository/onboarding', () => {
     });
     it('should adapt for gitlab phrasing', async () => {
       config.platform = 'gitlab';
-      await onboarding.onboardRepository(config);
+      await repositoryWorker.onboardRepository(config);
       expect(config.api.createPr.mock.calls[0][2].indexOf('Pull Request')).toBe(
         -1
       );
