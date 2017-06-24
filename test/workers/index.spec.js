17262039ed9b7e972e900527e870743a426bbf1e
Rhys Arkins <rhys@keylocation.sg>
2017-06-24 15:10:12 +0200

    Finish workers coverage

--- test/workers/index.spec.js
+++ test/workers/index.spec.js
@@ -1,5 +1,37 @@
-require('../../lib/workers/index');
+const indexWorker = require('../../lib/workers/index');
+const repositoryWorker = require('../../lib/workers/repository');
+const configParser = require('../../lib/config');
 
-it('placeholder', () => {
-  // TODO: write tests for this module - this is here so the file shows up in coverage
+describe('lib/workers/index', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    configParser.parseConfigs = jest.fn();
+    configParser.getRepoConfig = jest.fn();
+    repositoryWorker.processRepo = jest.fn();
+  });
+  it('handles zero repos', async () => {
+    configParser.parseConfigs.mockReturnValueOnce({
+      repositories: [],
+    });
+    await indexWorker.start();
+  });
+  it('processes repositories', async () => {
+    configParser.parseConfigs.mockReturnValueOnce({
+      foo: 1,
+      repositories: ['a', 'b'],
+    });
+    configParser.getRepoConfig.mockReturnValue({
+      repository: 'foo',
+    });
+    await indexWorker.start();
+    expect(configParser.parseConfigs.mock.calls.length).toBe(1);
+    expect(configParser.getRepoConfig.mock.calls).toMatchSnapshot();
+    expect(repositoryWorker.processRepo.mock.calls.length).toBe(2);
+  });
+  it('catches errors', async () => {
+    configParser.parseConfigs.mockImplementationOnce(() => {
+      throw new Error('a');
+    });
+    await indexWorker.start();
+  });
 });
