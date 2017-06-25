da9cdf65af4b986df7de8e4c2bad395c81ca7bc8
Rhys Arkins <rhys@keylocation.sg>
2017-06-25 07:06:48 +0200

    Unit test gitlab api
    Closes #228

---
+++ test/_fixtures/logger/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  fatal: jest.fn(),
+  error: jest.fn(),
+  warn: jest.fn(),
+  info: jest.fn(),
+  debug: jest.fn(),
+  trace: jest.fn(),
+  child: jest.fn(() => module.exports),
+};
