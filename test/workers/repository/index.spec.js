22c32c098a46a9023887fe77736120671cce4dc8
Rhys Arkins <rhys@keylocation.sg>
2017-06-26 14:00:52 +0200

    Move upgrades

--- test/workers/repository/index.spec.js
+++ test/workers/repository/index.spec.js
@@ -1,10 +1,13 @@
 const repositoryWorker = require('../../../lib/workers/repository/index');
+const branchWorker = require('../../../lib/workers/branch');
+
 const apis = require('../../../lib/workers/repository/apis');
 const onboarding = require('../../../lib/workers/repository/onboarding');
 
 const logger = require('../../_fixtures/logger');
 
 jest.mock('../../../lib/workers/repository/onboarding');
+jest.mock('../../../lib/workers/branch');
 
 apis.initApis = jest.fn(input => input);
 apis.mergeRenovateJson = jest.fn(input => input);
@@ -32,6 +35,24 @@ describe('workers/repository', () => {
         throw new Error('bad init');
       });
       await repositoryWorker.processRepo(config);
+    });
+  });
+  describe('updateBranchesSequentially(branchUpgrades, logger)', () => {
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+    it('handles empty case', async () => {
+      await repositoryWorker.updateBranchesSequentially({}, logger);
+      expect(branchWorker.updateBranch.mock.calls.length).toBe(0);
+    });
+    it('updates branches', async () => {
+      const branchUpgrades = {
+        foo: {},
+        bar: {},
+        baz: {},
+      };
+      await repositoryWorker.updateBranchesSequentially(branchUpgrades, logger);
+      expect(branchWorker.updateBranch.mock.calls.length).toBe(3);
     });
   });
 });
--- test/workers/repository/upgrades.spec.js
+++ test/workers/repository/upgrades.spec.js
@@ -1,12 +1,10 @@
-const repositoryWorker = require('../../../lib/workers/repository/index');
+const upgrades = require('../../../lib/workers/repository/upgrades');
 const packageFileWorker = require('../../../lib/workers/package-file');
-const branchWorker = require('../../../lib/workers/branch');
 const logger = require('../../_fixtures/logger');
 
-jest.mock('../../../lib/workers/branch');
 jest.mock('../../../lib/workers/package-file');
 
-describe('workers/repository', () => {
+describe('workers/repository/upgrades', () => {
   describe('determineRepoUpgrades(config)', () => {
     let config;
     beforeEach(() => {
@@ -16,8 +14,8 @@ describe('workers/repository', () => {
     });
     it('returns empty array if no packageFiles', async () => {
       config.packageFiles = [];
-      const upgrades = await repositoryWorker.determineRepoUpgrades(config);
-      expect(upgrades.length).toBe(0);
+      const res = await upgrades.determineRepoUpgrades(config);
+      expect(res.length).toBe(0);
     });
     it('returns empty array if none found', async () => {
       config.packageFiles = [
@@ -27,8 +25,8 @@ describe('workers/repository', () => {
         },
       ];
       packageFileWorker.processPackageFile.mockReturnValue([]);
-      const upgrades = await repositoryWorker.determineRepoUpgrades(config);
-      expect(upgrades.length).toBe(0);
+      const res = await upgrades.determineRepoUpgrades(config);
+      expect(res.length).toBe(0);
     });
     it('returns array if upgrades found', async () => {
       config.packageFiles = [
@@ -42,31 +40,28 @@ describe('workers/repository', () => {
       ];
       packageFileWorker.processPackageFile.mockReturnValueOnce(['a']);
       packageFileWorker.processPackageFile.mockReturnValueOnce(['b', 'c']);
-      const upgrades = await repositoryWorker.determineRepoUpgrades(config);
-      expect(upgrades.length).toBe(3);
+      const res = await upgrades.determineRepoUpgrades(config);
+      expect(res.length).toBe(3);
     });
   });
   describe('groupUpgradesByBranch(upgrades, logger)', () => {
     it('returns empty object if no input array', async () => {
-      const res = await repositoryWorker.groupUpgradesByBranch([], logger);
+      const res = await upgrades.groupUpgradesByBranch([], logger);
       expect(res).toEqual({});
     });
     it('returns one branch if one input', async () => {
-      const upgrades = [
+      const input = [
         {
           branchName: 'foo-{{version}}',
           version: '1.1.0',
         },
       ];
-      const res = await repositoryWorker.groupUpgradesByBranch(
-        upgrades,
-        logger
-      );
+      const res = await upgrades.groupUpgradesByBranch(input, logger);
       expect(Object.keys(res).length).toBe(1);
       expect(res).toMatchSnapshot();
     });
     it('does not group if different compiled branch names', async () => {
-      const upgrades = [
+      const input = [
         {
           branchName: 'foo-{{version}}',
           version: '1.1.0',
@@ -80,15 +75,12 @@ describe('workers/repository', () => {
           version: '1.1.0',
         },
       ];
-      const res = await repositoryWorker.groupUpgradesByBranch(
-        upgrades,
-        logger
-      );
+      const res = await upgrades.groupUpgradesByBranch(input, logger);
       expect(Object.keys(res).length).toBe(3);
       expect(res).toMatchSnapshot();
     });
     it('groups if same compiled branch names', async () => {
-      const upgrades = [
+      const input = [
         {
           branchName: 'foo',
           version: '1.1.0',
@@ -102,15 +94,12 @@ describe('workers/repository', () => {
           version: '1.1.0',
         },
       ];
-      const res = await repositoryWorker.groupUpgradesByBranch(
-        upgrades,
-        logger
-      );
+      const res = await upgrades.groupUpgradesByBranch(input, logger);
       expect(Object.keys(res).length).toBe(2);
       expect(res).toMatchSnapshot();
     });
     it('groups if same compiled group name', async () => {
-      const upgrades = [
+      const input = [
         {
           branchName: 'foo',
           version: '1.1.0',
@@ -128,33 +117,9 @@ describe('workers/repository', () => {
           groupBranchName: 'renovate/my-group',
         },
       ];
-      const res = await repositoryWorker.groupUpgradesByBranch(
-        upgrades,
-        logger
-      );
+      const res = await upgrades.groupUpgradesByBranch(input, logger);
       expect(Object.keys(res).length).toBe(2);
       expect(res).toMatchSnapshot();
     });
-  });
-  describe('updateBranchesSequentially(branchUpgrades, logger)', () => {
-    beforeEach(() => {
-      jest.resetAllMocks();
-    });
-    it('handles empty case', async () => {
-      await repositoryWorker.updateBranchesSequentially({}, logger);
-      expect(branchWorker.updateBranch.mock.calls.length).toBe(0);
-    });
-    it('updates branches', async () => {
-      const branchUpgrades = {
-        foo: {},
-        bar: {},
-        baz: {},
-      };
-      await repositoryWorker.updateBranchesSequentially(branchUpgrades, logger);
-      expect(branchWorker.updateBranch.mock.calls.length).toBe(3);
-    });
-  });
-  describe('processRepo(repoConfig)', () => {
-    // TODO
   });
 });
