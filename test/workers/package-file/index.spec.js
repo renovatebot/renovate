0a5b45b100c8679c051ec705c1993cc33f230139
Rhys Arkins <rhys@keylocation.sg>
2017-06-27 10:38:59 +0200

    Refactor package-file

---
+++ test/workers/package-file/index.spec.js
@@ -0,0 +1,372 @@
+const packageFileWorker = require('../../../lib/workers/package-file');
+const depTypeWorker = require('../../../lib/workers/dep-type');
+
+jest.mock('../../../lib/workers/dep-type');
+
+describe('packageFileWorker', () => {
+  describe('findUpgrades(config)', () => {
+    let config;
+    beforeEach(() => {
+      config = {
+        api: {
+          getFileJson: jest.fn(),
+        },
+        depTypes: ['dependencies', 'devDependencies'],
+      };
+      packageFileWorker.updateBranch = jest.fn();
+    });
+    it('handles null', async () => {
+      const allUpgrades = await packageFileWorker.findUpgrades(config);
+      expect(allUpgrades).toHaveLength(0);
+    });
+    it('handles no renovate config', async () => {
+      config.enabled = false;
+      config.api.getFileJson.mockReturnValueOnce({});
+      const res = await packageFileWorker.findUpgrades(config);
+      expect(res).toEqual([]);
+    });
+    it('returns empty array if config disabled', async () => {
+      config.api.getFileJson.mockReturnValueOnce({
+        renovate: {
+          enabled: false,
+        },
+      });
+      const res = await packageFileWorker.findUpgrades(config);
+      expect(res).toEqual([]);
+    });
+    it('calls depTypeWorker', async () => {
+      config.api.getFileJson.mockReturnValueOnce({});
+      depTypeWorker.findUpgrades.mockReturnValueOnce([{}]);
+      depTypeWorker.findUpgrades.mockReturnValueOnce([{}, {}]);
+      const res = await packageFileWorker.findUpgrades(config);
+      expect(res).toHaveLength(3);
+    });
+    it('maintains yarn.lock', async () => {
+      config.api.getFileJson.mockReturnValueOnce({});
+      config.maintainYarnLock = true;
+      depTypeWorker.findUpgrades.mockReturnValue([]);
+      const res = await packageFileWorker.findUpgrades(config);
+      expect(res).toHaveLength(1);
+    });
+    /*
+    it('handles one dep', async () => {
+      const dep = {
+        depName: 'foo',
+        currentVersion: '1.0.0',
+      };
+      Object.assign(config, dep);
+      const upgrade = { newVersion: '1.1.0' };
+      npmApi.getDependency = jest.fn(() => ({}));
+      versionsHelper.determineUpgrades = jest.fn(() => [upgrade]);
+      const allUpgrades = await packageFileWorker.findUpgrades(config);
+      expect(allUpgrades).toMatchObject([Object.assign({}, dep, upgrade)]);
+    });
+    it('handles no return', async () => {
+      const dep = {
+        depName: 'foo',
+        currentVersion: '1.0.0',
+      };
+      Object.assign(config, dep);
+      const upgrade = { newVersion: '1.1.0' };
+      npmApi.getDependency = jest.fn(() => ({}));
+      npmApi.getDependency.mockReturnValueOnce(null);
+      versionsHelper.determineUpgrades = jest.fn(() => [upgrade]);
+      const allUpgrades = await packageFileWorker.findUpgrades(config);
+      expect(allUpgrades).toMatchObject([]);
+    });
+    it('handles no upgrades', async () => {
+      const dep = {
+        depName: 'foo',
+        currentVersion: '1.0.0',
+      };
+      Object.assign(config, dep);
+      npmApi.getDependency = jest.fn(() => ({}));
+      versionsHelper.determineUpgrades = jest.fn(() => []);
+      const allUpgrades = await packageFileWorker.findUpgrades(config);
+      expect(allUpgrades).toMatchObject([]);
+    });
+  });
+  describe('assignDepConfigs(inputConfig, deps)', () => {
+    let config;
+    let deps;
+    beforeEach(() => {
+      config = {};
+      deps = [];
+    });
+    it('handles empty deps', () => {
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchObject([]);
+    });
+    it('handles string deps', () => {
+      config.foo = 'bar';
+      config.depTypes = ['dependencies', 'devDependencies'];
+      deps.push({
+        depName: 'a',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('handles multiple deps', () => {
+      config.foo = 'bar';
+      deps.push({
+        depName: 'a',
+      });
+      deps.push({
+        depName: 'b',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('handles depType config without override', () => {
+      config.foo = 'bar';
+      config.depTypes = [
+        {
+          depType: 'dependencies',
+          alpha: 'beta',
+        },
+      ];
+      deps.push({
+        depName: 'a',
+        depType: 'dependencies',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('handles depType config with override', () => {
+      config.foo = 'bar';
+      config.depTypes = [
+        {
+          depType: 'dependencies',
+          foo: 'beta',
+        },
+      ];
+      deps.push({
+        depName: 'a',
+        depType: 'dependencies',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('handles package config', () => {
+      config.foo = 'bar';
+      config.packages = [
+        {
+          packageName: 'a',
+          labels: ['renovate'],
+        },
+      ];
+      deps.push({
+        depName: 'a',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('package config overrides depType and general config', () => {
+      config.foo = 'bar';
+      config.depTypes = [
+        {
+          depType: 'dependencies',
+          foo: 'beta',
+        },
+      ];
+      config.packages = [
+        {
+          packageName: 'a',
+          foo: 'gamma',
+        },
+      ];
+      deps.push({
+        depName: 'a',
+        depType: 'dependencies',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('nested package config overrides depType and general config', () => {
+      config.foo = 'bar';
+      config.depTypes = [
+        {
+          depType: 'dependencies',
+          foo: 'beta',
+          packages: [
+            {
+              packageName: 'a',
+              foo: 'gamma',
+            },
+          ],
+        },
+      ];
+      deps.push({
+        depName: 'a',
+        depType: 'dependencies',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('handles regex package pattern', () => {
+      config.foo = 'bar';
+      config.packages = [
+        {
+          packagePattern: 'eslint',
+          labels: ['eslint'],
+        },
+      ];
+      deps.push({
+        depName: 'eslint',
+      });
+      deps.push({
+        depName: 'eslint-foo',
+      });
+      deps.push({
+        depName: 'a',
+      });
+      deps.push({
+        depName: 'also-eslint',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('handles regex wildcard package pattern', () => {
+      config.foo = 'bar';
+      config.packages = [
+        {
+          packagePattern: '^eslint',
+          labels: ['eslint'],
+        },
+      ];
+      deps.push({
+        depName: 'eslint',
+      });
+      deps.push({
+        depName: 'eslint-foo',
+      });
+      deps.push({
+        depName: 'a',
+      });
+      deps.push({
+        depName: 'also-eslint',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+    it('handles non-regex package name', () => {
+      config.foo = 'bar';
+      config.packages = [
+        {
+          packageName: 'eslint',
+          labels: ['eslint'],
+        },
+      ];
+      deps.push({
+        depName: 'eslint',
+      });
+      deps.push({
+        depName: 'eslint-foo',
+      });
+      deps.push({
+        depName: 'a',
+      });
+      deps.push({
+        depName: 'also-eslint',
+      });
+      const updatedDeps = packageFileWorker.assignDepConfigs(config, deps);
+      expect(updatedDeps).toMatchSnapshot();
+    });
+  });
+  describe('getDepTypeConfig(depTypes, depTypeName)', () => {
+    it('handles empty depTypes', () => {
+      const depTypeConfig = packageFileWorker.getDepTypeConfig(
+        [],
+        'dependencies'
+      );
+      expect(depTypeConfig).toMatchObject({});
+    });
+    it('handles all strings', () => {
+      const depTypes = ['dependencies', 'devDependencies'];
+      const depTypeConfig = packageFileWorker.getDepTypeConfig(
+        depTypes,
+        'dependencies'
+      );
+      expect(depTypeConfig).toMatchObject({});
+    });
+    it('handles missed object', () => {
+      const depTypes = [
+        'dependencies',
+        {
+          depType: 'devDependencies',
+          foo: 'bar',
+        },
+      ];
+      const depTypeConfig = packageFileWorker.getDepTypeConfig(
+        depTypes,
+        'dependencies'
+      );
+      expect(depTypeConfig).toMatchObject({});
+    });
+    it('handles hit object', () => {
+      const depTypes = [
+        {
+          depType: 'dependencies',
+          foo: 'bar',
+        },
+        'devDependencies',
+      ];
+      const depTypeConfig = packageFileWorker.getDepTypeConfig(
+        depTypes,
+        'dependencies'
+      );
+      const expectedResult = {
+        foo: 'bar',
+      };
+      expect(depTypeConfig).toMatchObject(expectedResult);
+    });
+  });
+  describe('getUpgrades(config)', () => {
+    let config;
+    beforeEach(() => {
+      packageFileWorker.assignDepConfigs = jest.fn(() => []);
+      packageFileWorker.findUpgrades = jest.fn(() => []);
+      packageJson.extractDependencies = jest.fn(() => []);
+      config = require('../../../lib/config/defaults').getConfig();
+      config.api = {
+        getFileJson: jest.fn(() => ({})),
+      };
+      config.logger = logger;
+    });
+    it('returns empty array if no package content', async () => {
+      config.api.getFileJson.mockReturnValueOnce(null);
+      const res = await packageFileWorker.findUpgrades(config);
+      expect(res).toEqual([]);
+    });
+    it('extracts dependencies for each depType', async () => {
+      config.depTypes = [
+        'dependencies',
+        {
+          depType: 'devDependencies',
+          foo: 'bar',
+        },
+      ];
+      const res = await packageFileWorker.findUpgrades(config);
+      expect(res).toEqual([]);
+      expect(packageJson.extractDependencies.mock.calls).toMatchSnapshot();
+    });
+    it('filters dependencies', async () => {
+      packageJson.extractDependencies.mockReturnValueOnce([
+        {
+          depName: 'a',
+        },
+      ]);
+      packageFileWorker.assignDepConfigs.mockReturnValueOnce(['a']);
+      packageFileWorker.findUpgrades.mockReturnValueOnce(['a']);
+      const res = await packageFileWorker.findUpgrades(config);
+      expect(res).toHaveLength(1);
+      expect(res).toMatchSnapshot();
+    });
+    it('maintains yarn.lock', async () => {
+      config.maintainYarnLock = true;
+      const res = await packageFileWorker.findUpgrades(config);
+      expect(res).toHaveLength(1);
+    });
+    */
+  });
+});
