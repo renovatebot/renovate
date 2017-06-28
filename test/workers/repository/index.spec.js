b9bb016c2aa7bfd3a79b103c103b0bd91e9b5581
Rhys Arkins <rhys@keylocation.sg>
2017-06-28 15:41:02 +0200

    Skip repositories with no package.json
    Closes #376

--- test/workers/repository/index.spec.js
+++ test/workers/repository/index.spec.js
@@ -9,7 +9,9 @@ const logger = require('../../_fixtures/logger');
 
 apis.initApis = jest.fn(input => input);
 apis.mergeRenovateJson = jest.fn(input => input);
-apis.detectPackageFiles = jest.fn(input => input);
+apis.detectPackageFiles = jest.fn(input =>
+  Object.assign(input, { packageFiles: ['package.json'] })
+);
 
 describe('workers/repository', () => {
   describe('renovateRepository', () => {
@@ -17,22 +19,32 @@ describe('workers/repository', () => {
     beforeEach(() => {
       onboarding.getOnboardingStatus = jest.fn();
       onboarding.ensurePr = jest.fn();
-      upgrades.groupUpgradesByBranch = jest.fn();
+      upgrades.determineRepoUpgrades = jest.fn(() => []);
+      upgrades.groupUpgradesByBranch = jest.fn(() => ({}));
       branchWorker.updateBranch = jest.fn();
       config = {
+        api: {},
         logger,
         packageFiles: [],
       };
     });
-    it('returns early if repo is not onboarded', async () => {
+    it('skips repository if no package.json', async () => {
+      config.api.getFileJson = jest.fn(() => ({}));
+      await repositoryWorker.renovateRepository(config);
+      expect(onboarding.getOnboardingStatus.mock.calls.length).toBe(0);
+      expect(config.logger.error.mock.calls.length).toBe(0);
+    });
+    it('does not skip repository if package.json', async () => {
+      config.api.getFileJson = jest.fn(() => ({ a: 1 }));
       onboarding.getOnboardingStatus.mockReturnValueOnce(false);
       await repositoryWorker.renovateRepository(config);
-    });
-    it('detects package files if none configured', async () => {
-      onboarding.getOnboardingStatus.mockReturnValueOnce(true);
-      await repositoryWorker.renovateRepository(config);
+      expect(onboarding.getOnboardingStatus.mock.calls.length).toBe(1);
+      expect(branchWorker.updateBranch.mock.calls.length).toBe(0);
+      expect(onboarding.ensurePr.mock.calls.length).toBe(1);
+      expect(config.logger.error.mock.calls.length).toBe(0);
     });
     it('calls branchWorker', async () => {
+      config.hasRenovateJson = true;
       onboarding.getOnboardingStatus.mockReturnValueOnce(true);
       upgrades.groupUpgradesByBranch.mockReturnValueOnce({
         foo: {},
@@ -41,23 +53,14 @@ describe('workers/repository', () => {
       });
       await repositoryWorker.renovateRepository(config);
       expect(branchWorker.updateBranch.mock.calls.length).toBe(3);
-    });
-    it('calls ensurePr', async () => {
-      onboarding.getOnboardingStatus.mockReturnValueOnce(false);
-      upgrades.groupUpgradesByBranch.mockReturnValueOnce({
-        foo: {},
-        bar: {},
-        baz: {},
-      });
-      await repositoryWorker.renovateRepository(config);
-      expect(branchWorker.updateBranch.mock.calls.length).toBe(0);
-      expect(onboarding.ensurePr.mock.calls.length).toBe(1);
+      expect(config.logger.error.mock.calls.length).toBe(0);
     });
     it('swallows errors', async () => {
       apis.initApis.mockImplementationOnce(() => {
         throw new Error('bad init');
       });
       await repositoryWorker.renovateRepository(config);
+      expect(config.logger.error.mock.calls.length).toBe(1);
     });
   });
 });
