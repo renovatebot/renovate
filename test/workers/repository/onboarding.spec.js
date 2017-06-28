c8f962af8877cb92de12f37d1a71295305e4cb2a
Rhys Arkins <rhys@keylocation.sg>
2017-06-28 11:05:53 +0200

    Update tests

--- test/workers/repository/onboarding.spec.js
+++ test/workers/repository/onboarding.spec.js
@@ -9,30 +9,82 @@ describe('lib/workers/repository/onboarding', () => {
         api: {
           createPr: jest.fn(() => ({ displayNumber: 1 })),
           getBranchPr: jest.fn(),
+          updatePr: jest.fn(),
         },
         logger,
       };
     });
-    it('should return simple PR description if no upgrades', async () => {
+    it('creates pr', async () => {
       await onboarding.ensurePr(config, {});
-      expect(config.api.getBranchPr.mock.calls.length).toBe(1);
       expect(config.api.createPr.mock.calls.length).toBe(1);
+      expect(config.api.updatePr.mock.calls.length).toBe(0);
       expect(config.api.createPr.mock.calls).toMatchSnapshot();
     });
-    /*
-    it('should commit files and create PR', async () => {
-      config.platform = 'github';
-      await onboarding.onboardRepository(config);
-      expect(config.api.commitFilesToBranch.mock.calls.length).toBe(1);
+    it('updates pr', async () => {
+      config.api.getBranchPr.mockReturnValueOnce({});
+      await onboarding.ensurePr(config, {});
+      expect(config.api.createPr.mock.calls.length).toBe(0);
+      expect(config.api.updatePr.mock.calls.length).toBe(1);
+    });
+    it('does not update pr', async () => {
+      const existingPrBody = `Welcome to [Renovate](https://keylocation.sg/our-tech/renovate)!
+
+This is an onboarding PR, to help you understand and configure Renovate before any real changes are made. Once you close this Pull Request, we will begin keeping your \`package.json\` dependencies up-to-date via automated Pull Requests.
+
+---
+
+It looks like your repository dependencies are already up-to-date and no initial Pull Requests will be necessary.
+
+Would you like to change this? Simply edit the \`renovate.json\` in this branch and Renovate will update this Pull Request description the next time it runs.
+
+The [Configuration](https://github.com/singapore/renovate/blob/master/docs/configuration.md) and [Configuration FAQ](https://github.com/singapore/renovate/blob/master/docs/faq.md) documents should be helpful if you wish to modify this behaviour.
+
+---
+
+#### Important!
+
+You do not need to *merge* this Pull Request - renovate will begin even if it's closed *unmerged*.
+In fact, you only need to add a \`renovate.json\` file to your repository if you wish to override any default settings. The file is included as part of this PR only in case you wish to change default settings before you start.
+
+Alternatively, you can add the same configuration settings into a "renovate" section of \`package.json\`, which might be more convenient if you have only one.
+
+If the default settings are all suitable for you, simply close this Pull Request unmerged and your first renovation will begin the next time the program is run.
+`;
+      config.api.getBranchPr.mockReturnValueOnce({
+        title: 'Configure Renovate',
+        body: existingPrBody,
+      });
+      await onboarding.ensurePr(config, {});
+      expect(config.api.createPr.mock.calls.length).toBe(0);
+      expect(config.api.updatePr.mock.calls.length).toBe(0);
+    });
+    it('creates complex pr', async () => {
+      const branchUpgrades = {
+        'branch-a': [
+          {
+            prTitle: 'Pin a',
+            isPin: true,
+            depName: 'a',
+            repositoryUrl: 'https://a',
+            currentVersion: '^1.0.0',
+            newVersion: '1.1.0',
+          },
+        ],
+        'branch-b': [
+          {
+            prTitle: 'Upgrade b',
+            depName: 'b',
+            repositoryUrl: 'https://b',
+            currentVersion: '1.0.0',
+            newVersion: '2.0.0',
+          },
+        ],
+      };
+      await onboarding.ensurePr(config, branchUpgrades);
       expect(config.api.createPr.mock.calls.length).toBe(1);
-      expect(
-        config.api.createPr.mock.calls[0][2].indexOf('Pull Request')
-      ).not.toBe(-1);
-      expect(
-        config.api.createPr.mock.calls[0][2].indexOf('Merge Request')
-      ).toBe(-1);
-      expect(config.api.commitFilesToBranch.mock.calls).toMatchSnapshot();
-    });*/
+      expect(config.api.updatePr.mock.calls.length).toBe(0);
+      expect(config.api.createPr.mock.calls).toMatchSnapshot();
+    });
   });
   describe('getOnboardingStatus(config)', () => {
     let config;
