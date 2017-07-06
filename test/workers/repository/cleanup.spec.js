85fe5d614ee465404f55c289f9245d05c6b165e5
Rhys Arkins <rhys@keylocation.sg>
2017-07-06 21:28:59 +0200

    Simplify repository branch cleanup

--- test/workers/repository/cleanup.spec.js
+++ test/workers/repository/cleanup.spec.js
@@ -22,10 +22,6 @@ describe('workers/repository/cleanup', () => {
       await cleanup.pruneStaleBranches(config, branchNames);
       expect(config.api.getAllRenovateBranches.mock.calls).toHaveLength(0);
     });
-    it('returns if no branch names', async () => {
-      await cleanup.pruneStaleBranches(config, branchNames);
-      expect(config.api.getAllRenovateBranches.mock.calls).toHaveLength(0);
-    });
     it('returns if no remaining branches', async () => {
       branchNames = ['renovate/a', 'renovate/b'];
       config.api.getAllRenovateBranches.mockReturnValueOnce(branchNames);
@@ -33,52 +29,13 @@ describe('workers/repository/cleanup', () => {
       expect(config.api.getAllRenovateBranches.mock.calls).toHaveLength(1);
       expect(config.api.getAllPrs.mock.calls).toHaveLength(0);
     });
-    it('returns if remaining branch has mergeable PR', async () => {
+    it('deletes remaining branch', async () => {
       branchNames = ['renovate/a', 'renovate/b'];
       config.api.getAllRenovateBranches.mockReturnValueOnce(
         branchNames.concat(['renovate/c'])
       );
-      config.api.getAllPrs.mockReturnValueOnce([
-        { number: 4, state: 'open', branchName: 'test-a' },
-        { number: 5, state: 'open', branchName: 'renovate/c' },
-      ]);
-      config.api.getPr.mockReturnValueOnce({ mergeable: true });
       await cleanup.pruneStaleBranches(config, branchNames);
       expect(config.api.getAllRenovateBranches.mock.calls).toHaveLength(1);
-      expect(config.api.getAllPrs.mock.calls).toHaveLength(1);
-      expect(config.api.getPr.mock.calls).toHaveLength(1);
-      expect(config.api.deleteBranch.mock.calls).toHaveLength(0);
-    });
-    it('deletes if remaining branch has non-mergeable PR', async () => {
-      branchNames = ['renovate/a', 'renovate/b'];
-      config.api.getAllRenovateBranches.mockReturnValueOnce(
-        branchNames.concat(['renovate/c'])
-      );
-      config.api.getAllPrs.mockReturnValueOnce([
-        { number: 4, state: 'open', branchName: 'test-a' },
-        { number: 5, state: 'open', branchName: 'renovate/c' },
-      ]);
-      config.api.getPr.mockReturnValueOnce({ mergeable: false });
-      await cleanup.pruneStaleBranches(config, branchNames);
-      expect(config.api.getAllRenovateBranches.mock.calls).toHaveLength(1);
-      expect(config.api.getAllPrs.mock.calls).toHaveLength(1);
-      expect(config.api.getPr.mock.calls).toHaveLength(1);
-      expect(config.api.deleteBranch.mock.calls).toHaveLength(1);
-    });
-    it('deletes if no matching PR', async () => {
-      branchNames = ['renovate/a', 'renovate/b'];
-      config.api.getAllRenovateBranches.mockReturnValueOnce(
-        branchNames.concat(['renovate/c'])
-      );
-      config.api.getAllPrs.mockReturnValueOnce([
-        { number: 4, state: 'open', branchName: 'test-a' },
-        { number: 5, state: 'open', branchName: 'renovate/a' },
-      ]);
-      config.api.getPr.mockReturnValueOnce({ mergeable: false });
-      await cleanup.pruneStaleBranches(config, branchNames);
-      expect(config.api.getAllRenovateBranches.mock.calls).toHaveLength(1);
-      expect(config.api.getAllPrs.mock.calls).toHaveLength(1);
-      expect(config.api.getPr.mock.calls).toHaveLength(0);
       expect(config.api.deleteBranch.mock.calls).toHaveLength(1);
     });
   });
