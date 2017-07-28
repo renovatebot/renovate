0e1afa9ee5183045786a3bf7ffdcfacd403eeaca
Rhys Arkins <rhys@keylocation.sg>
2017-07-28 20:47:33 +0200

    Validate type

--- test/config/validation.spec.js
+++ test/config/validation.spec.js
@@ -5,12 +5,25 @@ describe('config/validation', () => {
     it('returns nested errors', () => {
       const config = {
         foo: 1,
-        depTypes: {
+        prBody: 'some-body',
+        lockFileMaintenance: {
           bar: 2,
         },
       };
       const errors = configValidation.validateConfig(config);
       expect(errors).toHaveLength(2);
+      expect(errors).toMatchSnapshot();
+    });
+    it('errors for all types', () => {
+      const config = {
+        enabled: 1,
+        schedule: 'after 5pm',
+        semanticPrefix: 7,
+        githubAppId: 'none',
+        lockFileMaintenance: false,
+      };
+      const errors = configValidation.validateConfig(config);
+      expect(errors).toHaveLength(5);
       expect(errors).toMatchSnapshot();
     });
   });
