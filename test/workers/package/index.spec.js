a6a6b18e44965f517b8556d772b25fb33d2354df
Rhys Arkins <rhys@keylocation.sg>
2017-07-31 15:54:04 +0200

    fix: Check for valid version before performing npm lookup
    Closes #564

--- test/workers/package/index.spec.js
+++ test/workers/package/index.spec.js
@@ -5,7 +5,6 @@ const defaultConfig = require('../../../lib/config/defaults').getConfig();
 const configParser = require('../../../lib/config');
 
 jest.mock('../../../lib/workers/branch/schedule');
-jest.mock('../../../lib/workers/package/versions');
 jest.mock('../../../lib/api/npm');
 
 describe('lib/workers/package/index', () => {
@@ -14,23 +13,31 @@ describe('lib/workers/package/index', () => {
     beforeEach(() => {
       config = configParser.filterConfig(defaultConfig, 'package');
       config.depName = 'foo';
+      config.currentVersion = '1.0.0';
     });
     it('returns empty if package is disabled', async () => {
       config.enabled = false;
       const res = await pkgWorker.renovatePackage(config);
       expect(res).toMatchObject([]);
     });
+    it('returns warning if using invalid version', async () => {
+      config.currentVersion =
+        'git+ssh://git@github.com/joefraley/eslint-config-meridian.git';
+      const res = await pkgWorker.renovatePackage(config);
+      expect(res).toMatchSnapshot();
+    });
     it('returns error if no npm dep found', async () => {
       config.repoIsOnboarded = true;
       config.schedule = 'some schedule';
       const res = await pkgWorker.renovatePackage(config);
+      expect(res).toMatchSnapshot();
       expect(res).toHaveLength(1);
       expect(res[0].type).toEqual('error');
       expect(npmApi.getDependency.mock.calls.length).toBe(1);
     });
     it('returns warning if warning found', async () => {
       npmApi.getDependency.mockReturnValueOnce({});
-      versions.determineUpgrades.mockReturnValueOnce([
+      versions.determineUpgrades = jest.fn(() => [
         {
           type: 'warning',
           message: 'bad version',
@@ -41,7 +48,7 @@ describe('lib/workers/package/index', () => {
     });
     it('returns array if upgrades found', async () => {
       npmApi.getDependency.mockReturnValueOnce({});
-      versions.determineUpgrades.mockReturnValueOnce([{}]);
+      versions.determineUpgrades = jest.fn(() => [{}]);
       const res = await pkgWorker.renovatePackage(config);
       expect(res).toHaveLength(1);
       expect(Object.keys(res[0])).toMatchSnapshot();
