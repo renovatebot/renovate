188277bfdbc44a45f9fdccd3d9153cefe3eb3ac6
Rhys Arkins <rhys@keylocation.sg>
2017-09-15 18:47:34 +0200

    fix lint

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-    'env': {
-      'jest': true,
-    },
-    'rules': {
-      'import/no-extraneous-dependencies': 0,
-      'global-require': 0
-    },
+  env: {
+    jest: true
+  },
+  rules: {
+    "prefer-promise-reject-errors": 0,
+    "import/no-extraneous-dependencies": 0,
+    "global-require": 0
+  }
 };
