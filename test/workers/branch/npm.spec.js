9ff555c74b42e5f3029fe0f0c01c0f5ad5c0bb33
Rhys Arkins <rhys@keylocation.sg>
2017-09-20 22:56:57 +0200

    fix: promisify lockfiles (#826)
    `npm` and `yarn` lockfile generation use promisified child process `exec` now instead of `spawnSync`.

--- test/workers/branch/npm.spec.js
+++ test/workers/branch/npm.spec.js
@@ -3,35 +3,92 @@ const logger = require('../../_fixtures/logger');
 const getInstalledPath = require('get-installed-path');
 
 jest.mock('fs-extra');
-jest.mock('child_process');
+jest.mock('child-process-promise');
 jest.mock('get-installed-path');
 
 getInstalledPath.mockImplementation(() => null);
 
 const fs = require('fs-extra');
-const cp = require('child_process');
+const { exec } = require('child-process-promise');
 
 describe('generateLockFile', () => {
   it('generates lock files', async () => {
-    cp.spawnSync = jest.fn(() => ({
+    getInstalledPath.mockReturnValueOnce('node_modules/npm');
+    exec.mockReturnValueOnce({
       stdout: '',
       stderror: '',
-    }));
+    });
     fs.readFileSync = jest.fn(() => 'package-lock-contents');
     const lockFile = await npmHelper.generateLockFile('some-dir', logger);
     expect(fs.readFileSync.mock.calls.length).toEqual(1);
     expect(lockFile).toEqual('package-lock-contents');
   });
   it('catches errors', async () => {
-    cp.spawnSync = jest.fn(() => ({
+    getInstalledPath.mockReturnValueOnce('node_modules/npm');
+    exec.mockReturnValueOnce({
       stdout: '',
       stderror: 'some-error',
-    }));
+    });
     fs.readFileSync = jest.fn(() => {
       throw new Error('not found');
     });
     const lockFile = await npmHelper.generateLockFile('some-dir', logger);
     expect(fs.readFileSync.mock.calls.length).toEqual(1);
     expect(lockFile).toBe(null);
+  });
+  it('finds npm embedded in renovate', async () => {
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => '/node_modules/renovate');
+    getInstalledPath.mockImplementationOnce(
+      () => '/node_modules/renovate/node_modules/npm'
+    );
+    exec.mockReturnValueOnce({
+      stdout: '',
+      stderror: '',
+    });
+    fs.readFileSync = jest.fn(() => 'package-lock-contents');
+    const lockFile = await npmHelper.generateLockFile('some-dir', logger);
+    expect(fs.readFileSync.mock.calls.length).toEqual(1);
+    expect(lockFile).toEqual('package-lock-contents');
+  });
+  it('finds npm globally', async () => {
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => '/node_modules/renovate');
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => '/node_modules/npm');
+    exec.mockReturnValueOnce({
+      stdout: '',
+      stderror: '',
+    });
+    fs.readFileSync = jest.fn(() => 'package-lock-contents');
+    const lockFile = await npmHelper.generateLockFile('some-dir', logger);
+    expect(fs.readFileSync.mock.calls.length).toEqual(1);
+    expect(lockFile).toEqual('package-lock-contents');
+  });
+  it('uses fallback npm', async () => {
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => '/node_modules/renovate');
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    exec.mockReturnValueOnce({
+      stdout: '',
+      stderror: '',
+    });
+    fs.readFileSync = jest.fn(() => 'package-lock-contents');
+    const lockFile = await npmHelper.generateLockFile('some-dir', logger);
+    expect(fs.readFileSync.mock.calls.length).toEqual(1);
+    expect(lockFile).toEqual('package-lock-contents');
   });
 });
--- test/workers/branch/yarn.spec.js
+++ test/workers/branch/yarn.spec.js
@@ -3,35 +3,92 @@ const logger = require('../../_fixtures/logger');
 const getInstalledPath = require('get-installed-path');
 
 jest.mock('fs-extra');
-jest.mock('child_process');
+jest.mock('child-process-promise');
 jest.mock('get-installed-path');
 
 getInstalledPath.mockImplementation(() => null);
 
 const fs = require('fs-extra');
-const cp = require('child_process');
+const { exec } = require('child-process-promise');
 
 describe('generateLockFile', () => {
   it('generates lock files', async () => {
-    cp.spawnSync = jest.fn(() => ({
+    getInstalledPath.mockReturnValueOnce('node_modules/yarn');
+    exec.mockReturnValueOnce({
       stdout: '',
       stderror: '',
-    }));
-    fs.readFileSync = jest.fn(() => 'yarn-lock-contents');
-    const yarnLock = await yarnHelper.generateLockFile('some-dir', logger);
+    });
+    fs.readFileSync = jest.fn(() => 'package-lock-contents');
+    const lockFile = await yarnHelper.generateLockFile('some-dir', logger);
     expect(fs.readFileSync.mock.calls.length).toEqual(1);
-    expect(yarnLock).toEqual('yarn-lock-contents');
+    expect(lockFile).toEqual('package-lock-contents');
   });
-  it('catches and throws errors', async () => {
-    cp.spawnSync = jest.fn(() => ({
+  it('catches errors', async () => {
+    getInstalledPath.mockReturnValueOnce('node_modules/yarn');
+    exec.mockReturnValueOnce({
       stdout: '',
       stderror: 'some-error',
-    }));
+    });
     fs.readFileSync = jest.fn(() => {
       throw new Error('not found');
     });
     const lockFile = await yarnHelper.generateLockFile('some-dir', logger);
     expect(fs.readFileSync.mock.calls.length).toEqual(1);
     expect(lockFile).toBe(null);
+  });
+  it('finds yarn embedded in renovate', async () => {
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => '/node_modules/renovate');
+    getInstalledPath.mockImplementationOnce(
+      () => '/node_modules/renovate/node_modules/yarn'
+    );
+    exec.mockReturnValueOnce({
+      stdout: '',
+      stderror: '',
+    });
+    fs.readFileSync = jest.fn(() => 'package-lock-contents');
+    const lockFile = await yarnHelper.generateLockFile('some-dir', logger);
+    expect(fs.readFileSync.mock.calls.length).toEqual(1);
+    expect(lockFile).toEqual('package-lock-contents');
+  });
+  it('finds yarn globally', async () => {
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => '/node_modules/renovate');
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => '/node_modules/yarn');
+    exec.mockReturnValueOnce({
+      stdout: '',
+      stderror: '',
+    });
+    fs.readFileSync = jest.fn(() => 'package-lock-contents');
+    const lockFile = await yarnHelper.generateLockFile('some-dir', logger);
+    expect(fs.readFileSync.mock.calls.length).toEqual(1);
+    expect(lockFile).toEqual('package-lock-contents');
+  });
+  it('uses fallback yarn', async () => {
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => '/node_modules/renovate');
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    getInstalledPath.mockImplementationOnce(() => {
+      throw new Error('not found');
+    });
+    exec.mockReturnValueOnce({
+      stdout: '',
+      stderror: '',
+    });
+    fs.readFileSync = jest.fn(() => 'package-lock-contents');
+    const lockFile = await yarnHelper.generateLockFile('some-dir', logger);
+    expect(fs.readFileSync.mock.calls.length).toEqual(1);
+    expect(lockFile).toEqual('package-lock-contents');
   });
 });
