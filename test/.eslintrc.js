fe79c2b03109ba3a564c05f1d624ba9273c76e57
Rhys Arkins <rhys@keylocation.sg>
2017-10-01 06:50:08 +0200

    add prettier to package.json

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   env: {
-    jest: true
+    jest: true,
   },
   rules: {
-    "prefer-promise-reject-errors": 0,
-    "import/no-extraneous-dependencies": 0,
-    "global-require": 0
-  }
+    'prefer-promise-reject-errors': 0,
+    'import/no-extraneous-dependencies': 0,
+    'global-require': 0,
+  },
 };
