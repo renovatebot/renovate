0fb7b27003e0d10681bcec0df8897e22d142dfcc
Rhys Arkins <rhys@keylocation.sg>
2017-11-03 09:24:10 +0100

    update eslintrc
    commit 0a90c86c25582a3a9b5189560728b8bf5fd307bb
    Merge: 5157356 9c1b7d7
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Fri Nov 3 09:18:01 2017 +0100
    
        Merge branch 'master' into mega-refactor
    
        # Conflicts:
        #	lib/workers/repository/apis.js
        #	lib/workers/repository/index.js
        #	test/workers/repository/__snapshots__/apis.spec.js.snap
        #	test/workers/repository/apis.spec.js
    
    commit 515735690ea714814697612db364c2405ad01c18
    Merge: f9b8873 543d93c
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Fri Nov 3 08:26:47 2017 +0100
    
        Merge remote-tracking branch 'origin/master' into mega-refactor
    
        # Conflicts:
        #	lib/workers/repository/apis.js
        #	test/workers/repository/__snapshots__/apis.spec.js.snap
        #	test/workers/repository/apis.spec.js
    
    commit f9b88731929033710db60079c0669abc3aa3226b
    Merge: d7b6e75 11d7f26
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Fri Nov 3 07:52:44 2017 +0100
    
        Merge remote-tracking branch 'origin/master' into mega-refactor
    
        # Conflicts:
        #	lib/workers/repository/apis.js
        #	lib/workers/repository/index.js
        #	test/config/decrypt.spec.js
    
    commit d7b6e7544e0f704154690a405309fd4901c83eec
    Merge: b95c4ee 3665021
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Fri Nov 3 07:46:00 2017 +0100
    
        Merge branch 'master' into mega-refactor
    
    commit b95c4eed148066b43750efbed3df822b19e849d5
    Merge: ac30897 9dd3fd9
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Fri Nov 3 07:35:57 2017 +0100
    
        Merge remote-tracking branch 'origin/master' into mega-refactor
    
    commit ac30897ea655b093974e10e54561b102cedaeec5
    Merge: 06cf642 9c43d74
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Fri Nov 3 06:02:58 2017 +0100
    
        Merge branch 'master' into mega-refactor
    
    commit 06cf642d5c38464593d8a57feeb9bb44eb4a40eb
    Merge: 5d7120c 143044e
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Thu Nov 2 09:08:46 2017 +0100
    
        Merge branch 'master' into mega-refactor
    
        # Conflicts:
        #	lib/workers/repository/apis.js
    
    commit 5d7120c1968eb39f2b00bde64b27afec8d4d7186
    Merge: f6121b1 90c3b1c
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Wed Nov 1 14:25:01 2017 +0100
    
        Merge branch 'master' into mega-refactor
    
        # Conflicts:
        #	lib/workers/repository/index.js
        #	lib/workers/repository/onboarding.js
        #	test/manager/index.spec.js
        #	test/workers/repository/__snapshots__/onboarding.spec.js.snap
    
    commit f6121b17febce50239338ed92a202c92e22e381e
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Wed Nov 1 11:02:20 2017 +0100
    
        feat: forked repositories must use renovateFork=true
    
        Closes #940
    
        BREAKING CHANGE: Forked repositories won’t be renovated unless configured with a renovate.json containing renovateFork=true
    
    commit 62f38c34aed2d3dd490f9e5539970432f61869e3
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Wed Nov 1 10:55:29 2017 +0100
    
        fix: merge platform config into config
    
    commit 3a9e64b3e0732bdc9aa4cb26107b41331e853ad2
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Tue Oct 31 14:00:28 2017 +0100
    
        switch bin to lib for testing
    
    commit 28cc515a90740f4ddcf62fb68aa2f120ec6f1540
    Author: Rhys Arkins <rhys@keylocation.sg>
    Date:   Tue Oct 31 13:55:06 2017 +0100
    
        refactor: mega

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'prefer-destructuring': 0,
     'prefer-promise-reject-errors': 0,
     'import/no-extraneous-dependencies': 0,
     'global-require': 0,
