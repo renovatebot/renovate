11d7f26cc12cecfc32628757245f0a29cd84a0e7
Rhys Arkins <rhys@keylocation.sg>
2017-11-03 07:51:44 +0100

    refactor: perform decrypt as part of merge renovate.json (#1086)
    Also clarify docs that encrypted config must be contained in renovate.json (i.e. not package.json).

--- test/config/decrypt.spec.js
+++ test/config/decrypt.spec.js
@@ -1,37 +1,36 @@
 const { decryptConfig } = require('../../lib/config/decrypt.js');
-const defaultConfig = require('../../lib/config/defaults').getConfig();
 const logger = require('../_fixtures/logger');
 const fs = require('fs');
 
 const privateKey = fs.readFileSync('test/_fixtures/keys/private.pem');
 
-describe('config/massage', () => {
-  describe('massageConfig', () => {
+describe('config/decrypt', () => {
+  describe('decryptConfig()', () => {
     let config;
     beforeEach(() => {
-      config = { ...defaultConfig, logger };
+      config = {};
     });
     it('returns empty with no privateKey', () => {
       delete config.encrypted;
-      const res = decryptConfig(config);
+      const res = decryptConfig(config, logger);
       expect(res).toMatchObject(config);
     });
     it('warns if no privateKey found', () => {
       config.encrypted = { a: '1' };
-      const res = decryptConfig(config);
+      const res = decryptConfig(config, logger);
       expect(res.encrypted).not.toBeDefined();
       expect(res.a).not.toBeDefined();
     });
     it('handles invalid encrypted type', () => {
       config.encrypted = 1;
       config.privateKey = privateKey;
-      const res = decryptConfig(config);
+      const res = decryptConfig(config, logger, privateKey);
       expect(res.encrypted).not.toBeDefined();
     });
     it('handles invalid encrypted value', () => {
       config.encrypted = { a: 1 };
       config.privateKey = privateKey;
-      const res = decryptConfig(config);
+      const res = decryptConfig(config, logger, privateKey);
       expect(res.encrypted).not.toBeDefined();
       expect(res.a).not.toBeDefined();
     });
@@ -50,7 +49,7 @@ describe('config/massage', () => {
           },
         },
       ];
-      const res = decryptConfig(config);
+      const res = decryptConfig(config, logger, privateKey);
       expect(res.encrypted).not.toBeDefined();
       expect(res.packageFiles[0].devDependencies.encrypted).not.toBeDefined();
       expect(res.packageFiles[0].devDependencies.branchPrefix).toEqual(
