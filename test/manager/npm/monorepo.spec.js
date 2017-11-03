7f2ff2f0e1edaf09d6d0342a418fab14c97c9f46
Rhys Arkins <rhys@keylocation.sg>
2017-11-03 08:31:17 +0100

    refactor: move monorepos logic to npm manager

---
+++ test/manager/npm/monorepo.spec.js
@@ -0,0 +1,80 @@
+const { checkMonorepos } = require('../../../lib/manager/npm/monorepos');
+
+let config;
+beforeEach(() => {
+  jest.resetAllMocks();
+  config = { ...require('../../_fixtures/config') };
+  config.errors = [];
+  config.warnings = [];
+});
+
+describe('manager/npm/monorepo', () => {
+  describe('checkMonorepos', () => {
+    it('adds yarn workspaces', async () => {
+      config.packageFiles = [
+        {
+          packageFile: 'package.json',
+          content: { workspaces: ['packages/*'] },
+        },
+        {
+          packageFile: 'packages/something/package.json',
+          content: { name: '@a/b' },
+        },
+        {
+          packageFile: 'packages/something-else/package.json',
+          content: { name: '@a/c' },
+        },
+      ];
+      const res = await checkMonorepos(config);
+      expect(res.monorepoPackages).toMatchSnapshot();
+    });
+    it('adds nested yarn workspaces', async () => {
+      config.packageFiles = [
+        {
+          packageFile: 'frontend/package.json',
+          content: { workspaces: ['packages/*'] },
+        },
+        {
+          packageFile: 'frontend/packages/something/package.json',
+          content: { name: '@a/b' },
+        },
+        {
+          packageFile: 'frontend/packages/something-else/package.json',
+          content: { name: '@a/c' },
+        },
+      ];
+      const res = await checkMonorepos(config);
+      expect(res.monorepoPackages).toMatchSnapshot();
+    });
+    it('adds lerna packages', async () => {
+      config.packageFiles = [
+        {
+          packageFile: 'package.json',
+          content: {},
+        },
+        {
+          packageFile: 'packages/something/package.json',
+          content: { name: '@a/b' },
+        },
+        {
+          packageFile: 'packages/something-else/package.json',
+          content: { name: '@a/c' },
+        },
+      ];
+      config.api.getFileJson.mockReturnValue({ packages: ['packages/*'] });
+      const res = await checkMonorepos(config);
+      expect(res.monorepoPackages).toMatchSnapshot();
+    });
+    it('skips if no lerna packages', async () => {
+      config.packageFiles = [
+        {
+          packageFile: 'package.json',
+          content: {},
+        },
+      ];
+      config.api.getFileJson.mockReturnValue({});
+      const res = await checkMonorepos(config);
+      expect(res.monorepoPackages).toMatchSnapshot();
+    });
+  });
+});
