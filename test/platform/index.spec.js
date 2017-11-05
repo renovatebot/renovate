2d7369666d816f1bd803ec950ee3382ea2c31632
Rhys Arkins <rhys@keylocation.sg>
2017-11-05 08:28:02 +0100

    add vsts test

--- test/platform/index.spec.js
+++ test/platform/index.spec.js
@@ -1,12 +1,15 @@
 const github = require('../../lib/platform/github');
 const gitlab = require('../../lib/platform/gitlab');
+const vsts = require('../../lib/platform/vsts');
 
 describe('platform', () => {
   it('has same API for github and gitlab', () => {
     const githubMethods = Object.keys(github);
     const gitlabMethods = Object.keys(gitlab);
+    const vstsMethods = Object.keys(vsts);
     expect(githubMethods).toMatchSnapshot();
     expect(gitlabMethods).toMatchSnapshot();
     expect(githubMethods).toMatchObject(gitlabMethods);
+    expect(githubMethods).toMatchObject(vstsMethods);
   });
 });
