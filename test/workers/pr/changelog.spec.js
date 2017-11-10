3e34d528190be798b53b31ee72141dc05f28ee6a
Rhys Arkins <rhys@keylocation.sg>
2017-11-10 14:48:14 +0100

    feat: cache changelog responses (#1143)
    Returns cached value if from the to versions are an exact match. No expiry.
    
    Closes #262

--- test/workers/pr/changelog.spec.js
+++ test/workers/pr/changelog.spec.js
@@ -4,46 +4,41 @@ const changelogHelper = require('../../../lib/workers/pr/changelog');
 jest.mock('changelog');
 
 describe('workers/pr/changelog', () => {
-  describe('changelogHelper.getChangeLog(depName, fromVersion, newVersion)', () => {
-    it('returns empty if no fromVersion', async () => {
-      expect(
-        await changelogHelper.getChangeLog('renovate', null, '1.0.0')
-      ).toBe('No changelog available');
-    });
-    it('returns empty if fromVersion equals newVersion', async () => {
-      expect(
-        await changelogHelper.getChangeLog('renovate', '1.0.0', '1.0.0')
-      ).toBe('No changelog available');
-    });
-    it('returns empty if generated json is null', async () => {
-      changelog.generate.mockReturnValueOnce(null);
-      expect(
-        await changelogHelper.getChangeLog('renovate', '1.0.0', '2.0.0')
-      ).toBe('No changelog available');
-    });
-    it('returns header if generated markdown is valid', async () => {
-      changelog.generate.mockReturnValueOnce({});
-      changelog.markdown.mockReturnValueOnce('dummy');
-      expect(
-        await changelogHelper.getChangeLog('renovate', '1.0.0', '2.0.0')
-      ).toBe('### Changelog\n\ndummy');
-    });
-    it('returns empty if error thrown', async () => {
-      changelog.generate = jest.fn(() => {
-        throw new Error('foo');
-      });
-      expect(
-        await changelogHelper.getChangeLog('renovate', '1.0.0', '2.0.0')
-      ).toBe('No changelog available');
-    });
-  });
   describe('getChangeLogJSON', () => {
+    it('returns null if no fromVersion', async () => {
+      expect(
+        await changelogHelper.getChangeLogJSON('renovate', null, '1.0.0')
+      ).toBe(null);
+    });
+    it('returns null if fromVersion equals newVersion', async () => {
+      expect(
+        await changelogHelper.getChangeLogJSON('renovate', '1.0.0', '1.0.0')
+      ).toBe(null);
+    });
+    it('logs when no JSON', async () => {
+      changelog.generate = jest.fn(() => null);
+      expect(
+        await changelogHelper.getChangeLogJSON('renovate', '1.0.0', '1.5.0')
+      ).toBe(null);
+    });
+    it('returns JSON', async () => {
+      changelog.generate = jest.fn(() => ({ a: 1 }));
+      expect(
+        await changelogHelper.getChangeLogJSON('renovate', '1.0.0', '2.0.0')
+      ).toMatchObject({ a: 1 });
+    });
+    it('returns cached JSON', async () => {
+      changelog.generate = jest.fn(() => ({ a: 1 }));
+      expect(
+        await changelogHelper.getChangeLogJSON('renovate', '1.0.0', '2.0.0')
+      ).toMatchObject({ a: 1 });
+    });
     it('filters unnecessary warns', async () => {
       changelog.generate = jest.fn(() => {
         throw new Error('Unknown Github Repo');
       });
       expect(
-        await changelogHelper.getChangeLogJSON('renovate', '1.0.0', '2.0.0')
+        await changelogHelper.getChangeLogJSON('renovate', '1.0.0', '3.0.0')
       ).toBe(null);
     });
   });
