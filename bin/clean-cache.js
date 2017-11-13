ae12555c93aafb37f63b55e96cf71c2a363c49ad
Rhys Arkins <rhys@keylocation.sg>
2017-11-13 15:37:31 +0100

    feat: persistent disk cache for changelog data

--- bin/clean-cache.js
+++ bin/clean-cache.js
@@ -2,5 +2,6 @@ const fs = require('fs-extra');
 const os = require('os');
 
 (async () => {
+  await fs.remove(os.tmpdir() + '/renovate-changelog-cache');
   await fs.remove(os.tmpdir() + '/renovate-npm-cache');
 })();
--- test/workers/pr/changelog.spec.js
+++ test/workers/pr/changelog.spec.js
@@ -28,7 +28,7 @@ describe('workers/pr/changelog', () => {
       ).toMatchObject({ a: 1 });
     });
     it('returns cached JSON', async () => {
-      changelog.generate = jest.fn(() => ({ a: 1 }));
+      changelog.generate = jest.fn(() => ({ a: 2 }));
       expect(
         await changelogHelper.getChangeLogJSON('renovate', '1.0.0', '2.0.0')
       ).toMatchObject({ a: 1 });
