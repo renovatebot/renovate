4663967379be909602ee801aabce7d5df9a453a5
Rhys Arkins <rhys@arkins.net>
2018-02-06 15:49:21 +0100

    fix: better lerna version detection

--- test/workers/branch/lerna.spec.js
+++ test/workers/branch/lerna.spec.js
@@ -6,13 +6,25 @@ const { exec } = require('child-process-promise');
 
 describe('generateLockFiles()', () => {
   it('generates package-lock.json files', async () => {
+    platform.getFile.mockReturnValueOnce(
+      JSON.stringify({ dependencies: { lerna: '2.0.0' } })
+    );
     exec.mockReturnValueOnce({});
     const res = await lernaHelper.generateLockFiles('npm', 'some-dir', {});
     expect(res.error).toBe(false);
   });
   it('generates yarn.lock files', async () => {
+    platform.getFile.mockReturnValueOnce(
+      JSON.stringify({ devDependencies: { lerna: '2.0.0' } })
+    );
     exec.mockReturnValueOnce({});
     const res = await lernaHelper.generateLockFiles('yarn', 'some-dir', {});
+    expect(res.error).toBe(false);
+  });
+  it('defaults to latest', async () => {
+    platform.getFile.mockReturnValueOnce(undefined);
+    exec.mockReturnValueOnce({});
+    const res = await lernaHelper.generateLockFiles('npm', 'some-dir', {});
     expect(res.error).toBe(false);
   });
 });
