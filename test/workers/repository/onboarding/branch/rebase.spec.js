9263fd36b3eaac40ad67d7c909ca5815600fa90b
Rhys Arkins <rhys@arkins.net>
2018-02-22 19:46:57 +0100

    fix: don’t overwrite edited onboarding configs
    Before, we were attempting to rebase edited branches if renovate.json was the only modified file. This “lost” the committer information - which seemed ok - but it also meant that on the subsequent run it then assumed Renovate was the only committer. Now we don’t rebase edited onboarding branches anymore.
    
    Closes #1360

--- test/workers/repository/onboarding/branch/rebase.spec.js
+++ test/workers/repository/onboarding/branch/rebase.spec.js
@@ -13,35 +13,27 @@ describe('workers/repository/onboarding/branch/rebase', () => {
       };
     });
     it('does nothing if branch is up to date', async () => {
-      platform.getFile.mockReturnValueOnce('{}'); // package.json
-      platform.getFile.mockReturnValueOnce(
-        JSON.stringify(defaultConfig.onboardingConfig, null, 2) + '\n'
-      ); // renovate.json
-      platform.getBranchPr.mockReturnValueOnce({ number: 1 });
-      platform.getPr.mockReturnValueOnce({ isStale: false });
+      const contents =
+        JSON.stringify(defaultConfig.onboardingConfig, null, 2) + '\n';
+      platform.getFile.mockReturnValueOnce(contents); // package.json
+      platform.getFile.mockReturnValueOnce(contents); // renovate.json
+      platform.getBranchPr.mockReturnValueOnce({ isStale: false });
       await rebaseOnboardingBranch(config);
       expect(platform.commitFilesToBranch.mock.calls).toHaveLength(0);
     });
-    it('rebases unmodified branch', async () => {
-      platform.getBranchPr.mockReturnValueOnce({ number: 1 });
-      platform.getPr.mockReturnValueOnce({ isStale: true, canRebase: true });
-      await rebaseOnboardingBranch(config);
-      expect(platform.commitFilesToBranch.mock.calls).toHaveLength(1);
-    });
-    it('rebases modified branch', async () => {
-      platform.getBranchPr.mockReturnValueOnce({ number: 1 });
-      platform.getPr.mockReturnValueOnce({ isStale: true, canRebase: false });
-      platform.getPrFiles.mockReturnValueOnce(['renovate.json']);
+    it('rebases onboarding branch', async () => {
+      platform.getBranchPr.mockReturnValueOnce({
+        isStale: true,
+        canRebase: true,
+      });
       await rebaseOnboardingBranch(config);
       expect(platform.commitFilesToBranch.mock.calls).toHaveLength(1);
     });
     it('does not rebase modified branch with additional files', async () => {
-      platform.getBranchPr.mockReturnValueOnce({ number: 1 });
-      platform.getPr.mockReturnValueOnce({ isStale: true, canRebase: false });
-      platform.getPrFiles.mockReturnValueOnce([
-        'renovate.json',
-        'package.json',
-      ]);
+      platform.getBranchPr.mockReturnValueOnce({
+        isStale: true,
+        canRebase: false,
+      });
       await rebaseOnboardingBranch(config);
       expect(platform.commitFilesToBranch.mock.calls).toHaveLength(0);
     });
