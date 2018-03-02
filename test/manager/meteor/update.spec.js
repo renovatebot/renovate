dc9ce73a8583bc434d74bc8283c67066f7cd3c64
Rhys Arkins <rhys@arkins.net>
2018-03-02 16:12:48 +0100

    refactor setNewValue

--- test/manager/meteor/update.spec.js
+++ test/manager/meteor/update.spec.js
@@ -15,30 +15,30 @@ const input02Content = readFixture('package-2.js');
 describe('workers/branch/package-js', () => {
   describe('.setNewValue(currentFileContent, depName, currentVersion, newVersion)', () => {
     it('replaces a dependency value', () => {
-      const testContent = meteorUpdater.setNewValue(
-        input01Content,
-        'xmldom',
-        '0.1.19',
-        '0.22.1'
-      );
+      const upgrade = {
+        depName: 'xmldom',
+        currentVersion: '0.1.19',
+        newVersion: '0.22.1',
+      };
+      const testContent = meteorUpdater.setNewValue(input01Content, upgrade);
       expect(testContent).toMatchSnapshot();
     });
     it('handles alternative quotes and white space', () => {
-      const testContent = meteorUpdater.setNewValue(
-        input02Content,
-        'xmldom',
-        '0.1.19',
-        '0.22.1'
-      );
+      const upgrade = {
+        depName: 'xmldom',
+        currentVersion: '0.1.19',
+        newVersion: '0.22.1',
+      };
+      const testContent = meteorUpdater.setNewValue(input02Content, upgrade);
       expect(testContent).toMatchSnapshot();
     });
     it('handles the case where the desired version is already supported', () => {
-      const testContent = meteorUpdater.setNewValue(
-        input01Content,
-        'query-string',
-        '0.2.0',
-        '0.2.0'
-      );
+      const upgrade = {
+        depName: 'query-string',
+        currentVersion: '0.2.0',
+        newVersion: '0.2.0',
+      };
+      const testContent = meteorUpdater.setNewValue(input01Content, upgrade);
       testContent.should.equal(input01Content);
     });
   });
