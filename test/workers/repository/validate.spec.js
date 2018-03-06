8cda564a5e0564254373d16ce01d56f809d2e429
Rhys Arkins <rhys@arkins.net>
2018-03-06 15:06:11 +0100

    feat: validate renovate config PRs
    Checks for PRs mentioning renovate in the title and containing one or more renovate config files. Sets status check ‘renovate/validate’ to success or failure depending on whether any errors are found.
    
    GitHub-only for now due to missing getPrFiles() method in gitlab and vsts.
    
    Closes #1454

---
+++ test/workers/repository/validate.spec.js
@@ -0,0 +1,75 @@
+const validate = require('../../../lib/workers/repository/validate');
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('workers/repository/validate', () => {
+  describe('validatePrs()', () => {
+    it('catches error', async () => {
+      platform.getPrList.mockReturnValueOnce([
+        {
+          state: 'open',
+          branchName: 'some/branch',
+          title: 'Update Renovate',
+        },
+      ]);
+      await validate.validatePrs({});
+      expect(platform.setBranchStatus.mock.calls).toHaveLength(0);
+    });
+    it('returns if no matching files', async () => {
+      platform.getPrList.mockReturnValueOnce([
+        {
+          state: 'open',
+          branchName: 'some/branch',
+          title: 'Update Renovate',
+        },
+      ]);
+      platform.getPrFiles.mockReturnValueOnce(['readme.md']);
+      await validate.validatePrs({});
+      expect(platform.setBranchStatus.mock.calls).toHaveLength(0);
+    });
+    it('validates failures if cannot parse', async () => {
+      platform.getPrList.mockReturnValueOnce([
+        {
+          state: 'open',
+          branchName: 'some/branch',
+          title: 'Update Renovate',
+        },
+      ]);
+      platform.getPrFiles.mockReturnValueOnce(['renovate.json']);
+      platform.getFile.mockReturnValue('not JSON');
+      await validate.validatePrs({});
+      expect(platform.setBranchStatus.mock.calls).toHaveLength(1);
+      expect(platform.setBranchStatus.mock.calls[0][3]).toEqual('failure');
+    });
+    it('validates failures if config validation fails', async () => {
+      platform.getPrList.mockReturnValueOnce([
+        {
+          state: 'open',
+          branchName: 'some/branch',
+          title: 'Update Renovate',
+        },
+      ]);
+      platform.getPrFiles.mockReturnValueOnce(['renovate.json']);
+      platform.getFile.mockReturnValue('{"foo":1}');
+      await validate.validatePrs({});
+      expect(platform.setBranchStatus.mock.calls).toHaveLength(1);
+      expect(platform.setBranchStatus.mock.calls[0][3]).toEqual('failure');
+    });
+    it('validates successfully', async () => {
+      platform.getPrList.mockReturnValueOnce([
+        {
+          state: 'open',
+          branchName: 'some/branch',
+          title: 'Update Renovate',
+        },
+      ]);
+      platform.getPrFiles.mockReturnValueOnce(['renovate.json']);
+      platform.getFile.mockReturnValue('{}');
+      await validate.validatePrs({});
+      expect(platform.setBranchStatus.mock.calls).toHaveLength(1);
+      expect(platform.setBranchStatus.mock.calls[0][3]).toEqual('success');
+    });
+  });
+});
