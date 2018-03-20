c9f2d387fa8de25f17fad8cfe1a89d4f976efecb
Rhys Arkins <rhys@arkins.net>
2018-03-20 15:51:37 +0100

    fix: use github tags not releases to determine node versions
    Closes #1691

--- test/manager/_helpers/node/package.spec.js
+++ test/manager/_helpers/node/package.spec.js
@@ -1,5 +1,5 @@
 const nodeManager = require('../../../../lib/manager/_helpers/node/package');
-const { getRepoReleases } = require('../../../../lib/datasource/github');
+const { getRepoTags } = require('../../../../lib/datasource/github');
 
 jest.mock('../../../../lib/datasource/github');
 
@@ -17,20 +17,20 @@ describe('manager/npm/engines', () => {
   });
   it('returns empty', async () => {
     config.currentVersion = '8.9.0';
-    getRepoReleases.mockReturnValueOnce([]);
+    getRepoTags.mockReturnValueOnce([]);
     const res = await nodeManager.getPackageUpdates(config);
     expect(res).toEqual([]);
   });
   it('filters v', async () => {
     config.currentVersion = '8.9.0';
-    getRepoReleases.mockReturnValueOnce(['v8.0.0', 'v8.9.1']);
+    getRepoTags.mockReturnValueOnce(['v8.0.0', 'v8.9.1']);
     const res = await nodeManager.getPackageUpdates(config);
     expect(res).toHaveLength(1);
     expect(res[0].newVersion).toEqual('8.9.1');
   });
   it('skips major versions', async () => {
     config.currentVersion = '8.9.0';
-    getRepoReleases.mockReturnValueOnce(['v9.4.0']);
+    getRepoTags.mockReturnValueOnce(['v9.4.0']);
     const res = await nodeManager.getPackageUpdates(config);
     expect(res).toHaveLength(0);
   });
