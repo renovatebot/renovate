abb6aa799f55e3a746b837ca21afe418de4e14b9
Rhys Arkins <rhys@arkins.net>
2018-03-23 13:44:22 +0100

    add tests

--- test/datasource/docker.spec.js
+++ test/datasource/docker.spec.js
@@ -10,12 +10,12 @@ describe('api/docker', () => {
     });
     it('returns null if no token', async () => {
       got.mockReturnValueOnce({ body: {} });
-      const res = await docker.getDigest('some-name', undefined);
+      const res = await docker.getDigest(undefined, 'some-name', undefined);
       expect(res).toBe(null);
     });
     it('returns null if errored', async () => {
       got.mockReturnValueOnce({ body: { token: 'some-token' } });
-      const res = await docker.getDigest('some-name', undefined);
+      const res = await docker.getDigest(undefined, 'some-name', undefined);
       expect(res).toBe(null);
     });
     it('returns digest', async () => {
@@ -23,7 +23,7 @@ describe('api/docker', () => {
       got.mockReturnValueOnce({
         headers: { 'docker-content-digest': 'some-digest' },
       });
-      const res = await docker.getDigest('some-name', undefined);
+      const res = await docker.getDigest(undefined, 'some-name', undefined);
       expect(res).toBe('some-digest');
     });
     it('supports scoped names', async () => {
@@ -31,26 +31,26 @@ describe('api/docker', () => {
       got.mockReturnValueOnce({
         headers: { 'docker-content-digest': 'some-digest' },
       });
-      const res = await docker.getDigest('some/name', undefined);
+      const res = await docker.getDigest(undefined, 'some/name', undefined);
       expect(res).toBe('some-digest');
     });
   });
   describe('getTags', () => {
     it('returns null if no token', async () => {
       got.mockReturnValueOnce({ body: {} });
-      const res = await docker.getTags('node');
+      const res = await docker.getTags(undefined, 'node');
       expect(res).toBe(null);
     });
     it('returns tags', async () => {
       const tags = ['a', 'b'];
       got.mockReturnValueOnce({ body: { token: 'some-token ' } });
       got.mockReturnValueOnce({ body: { tags } });
-      const res = await docker.getTags('my/node');
+      const res = await docker.getTags(undefined, 'my/node');
       expect(res).toBe(tags);
     });
     it('returns null on error', async () => {
       got.mockReturnValueOnce({});
-      const res = await docker.getTags('node');
+      const res = await docker.getTags(undefined, 'node');
       expect(res).toBe(null);
     });
   });
