4c348d437da1c97127cb9be18cc3fa6f0fe1ed72
Rhys Arkins <rhys@arkins.net>
2018-03-28 09:55:10 +0200

    feat: validate packageRule selectors
    Validates that each packageRule must contain at least one selector. Resolves the rule first to allow for presets.
    
    Closes #1345, Closes #1693

--- test/config/validation.spec.js
+++ test/config/validation.spec.js
@@ -2,7 +2,7 @@ const configValidation = require('../../lib/config/validation.js');
 
 describe('config/validation', () => {
   describe('validateConfig(config)', () => {
-    it('returns nested errors', () => {
+    it('returns nested errors', async () => {
       const config = {
         foo: 1,
         schedule: ['after 5pm'],
@@ -12,12 +12,14 @@ describe('config/validation', () => {
           bar: 2,
         },
       };
-      const { warnings, errors } = configValidation.validateConfig(config);
+      const { warnings, errors } = await configValidation.validateConfig(
+        config
+      );
       expect(warnings).toHaveLength(0);
       expect(errors).toHaveLength(2);
       expect(errors).toMatchSnapshot();
     });
-    it('errors for all types', () => {
+    it('errors for all types', async () => {
       const config = {
         allowedVersions: 'foo',
         enabled: 1,
@@ -29,14 +31,22 @@ describe('config/validation', () => {
         extends: [':timezone(Europe/Brussel)'],
         packageRules: [
           {
+            excludePackageNames: ['foo'],
+            enabled: true,
+          },
+          {
             foo: 1,
           },
+          'what?',
         ],
       };
-      const { warnings, errors } = configValidation.validateConfig(config);
-      expect(warnings).toHaveLength(0);
+      const { warnings, errors } = await configValidation.validateConfig(
+        config
+      );
+      expect(warnings).toMatchSnapshot();
+      expect(warnings).toHaveLength(1);
       expect(errors).toMatchSnapshot();
-      expect(errors).toHaveLength(9);
+      expect(errors).toHaveLength(10);
     });
   });
 });
