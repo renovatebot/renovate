ced374ea24eb43da8cec31c5765fe59f8a755367
Rhys Arkins <rhys@arkins.net>
2018-04-03 06:52:04 +0200

    fix: disable manager if parentManager is disabled
    Closes #1778

--- test/manager/index.spec.js
+++ test/manager/index.spec.js
@@ -16,7 +16,7 @@ describe('manager', () => {
     let config;
     beforeEach(() => {
       config = {
-        ...defaultConfig,
+        ...JSON.parse(JSON.stringify(defaultConfig)),
         warnings: [],
       };
     });
@@ -28,6 +28,15 @@ describe('manager', () => {
       config.enabledManagers = ['docker'];
       const res = await manager.detectPackageFiles(config);
       expect(res).toHaveLength(0);
+    });
+    it('skips if parentManager is disabled', async () => {
+      platform.getFileList.mockReturnValueOnce([
+        'package.json',
+        '.circleci/config.yml',
+      ]);
+      config.docker.enabled = false;
+      const res = await manager.detectPackageFiles(config);
+      expect(res).toHaveLength(1);
     });
     it('adds package files to object', async () => {
       platform.getFileList.mockReturnValueOnce([
