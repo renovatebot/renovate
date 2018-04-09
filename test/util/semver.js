430f93f93bd70af8a2cf9c51004c5e719a4e63fd
Rhys Arkins <rhys@arkins.net>
2018-04-09 19:36:10 +0200

    renaming

--- test/util/semver.js
+++ test/util/semver.js
@@ -1,27 +1,25 @@
 const semver = require('../../lib/util/semver');
 
-describe('.isValidVersionOrRange(input)', () => {
+describe('.isValidSemver(input)', () => {
   it('should support simple semver', () => {
-    semver.isValidVersionOrRange('1.2.3').should.eql(true);
+    semver.isValidSemver('1.2.3').should.eql(true);
   });
   it('should support semver with dash', () => {
-    semver.isValidVersionOrRange('1.2.3-foo').should.eql(true);
+    semver.isValidSemver('1.2.3-foo').should.eql(true);
   });
   it('should reject semver without dash', () => {
-    semver.isValidVersionOrRange('1.2.3foo').should.eql(false);
+    semver.isValidSemver('1.2.3foo').should.eql(false);
   });
   it('should support ranges', () => {
-    semver.isValidVersionOrRange('~1.2.3').should.eql(true);
-    semver.isValidVersionOrRange('^1.2.3').should.eql(true);
-    semver.isValidVersionOrRange('>1.2.3').should.eql(true);
+    semver.isValidSemver('~1.2.3').should.eql(true);
+    semver.isValidSemver('^1.2.3').should.eql(true);
+    semver.isValidSemver('>1.2.3').should.eql(true);
   });
   it('should reject github repositories', () => {
-    semver.isValidVersionOrRange('renovateapp/renovate').should.eql(false);
+    semver.isValidSemver('renovateapp/renovate').should.eql(false);
+    semver.isValidSemver('renovateapp/renovate#master').should.eql(false);
     semver
-      .isValidVersionOrRange('renovateapp/renovate#master')
-      .should.eql(false);
-    semver
-      .isValidVersionOrRange('https://github.com/renovateapp/renovate.git')
+      .isValidSemver('https://github.com/renovateapp/renovate.git')
       .should.eql(false);
   });
 });
