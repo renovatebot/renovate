812fe227b855f37ec00774a9fb5f2c47b116bb1f
Rhys Arkins <rhys@arkins.net>
2018-05-07 20:14:00 +0200

    fix(onboarding): adjust packageFiles check when filtering

--- test/workers/repository/onboarding/pr/config-description.spec.js
+++ test/workers/repository/onboarding/pr/config-description.spec.js
@@ -19,29 +19,31 @@ describe('workers/repository/onboarding/pr/config-description', () => {
       expect(res).toMatchSnapshot();
     });
     it('returns a full list', () => {
-      config.packageFiles = [
-        { packageFile: 'package.json' },
-        { packageFile: 'Dockerfile' },
-      ];
+      const packageFiles = {
+        npm: [],
+        docker: [],
+      };
       config.description = [
         'description 1',
         'description two',
         'something else',
         'this is Docker-only',
       ];
-      const res = getConfigDesc(config);
+      const res = getConfigDesc(config, packageFiles);
       expect(res).toMatchSnapshot();
       expect(res.indexOf('Docker-only')).not.toBe(-1);
     });
     it('returns a filtered list', () => {
-      config.packageFiles = [{ packageFile: 'package.json' }];
+      const packageFiles = {
+        npm: [],
+      };
       config.description = [
         'description 1',
         'description two',
         'something else',
         'this is Docker-only',
       ];
-      const res = getConfigDesc(config);
+      const res = getConfigDesc(config, packageFiles);
       expect(res).toMatchSnapshot();
       expect(res.indexOf('Docker-only')).toBe(-1);
     });
