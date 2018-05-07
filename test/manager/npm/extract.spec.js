eb7596071a4b5a520808f76ae982a56a46389dca
Rhys Arkins <rhys@arkins.net>
2018-05-07 14:27:53 +0200

    npm extract

--- test/manager/npm/extract.spec.js
+++ test/manager/npm/extract.spec.js
@@ -13,111 +13,60 @@ const input01Content = readFixture('inputs/01.json');
 const input02Content = readFixture('inputs/02.json');
 
 describe('manager/npm/extract', () => {
-  describe('.extractDependencies(npmExtract, depType)', () => {
-    it('returns an array of correct length (dependencies)', () => {
-      const config = {
-        depType: 'dependencies',
-      };
-      const extractedDependencies = npmExtract.extractDependencies(
-        JSON.parse(input01Content),
-        config
-      ).deps;
-      extractedDependencies.should.be.instanceof(Array);
-      extractedDependencies.should.have.length(6);
+  describe('.extractDependencies()', () => {
+    beforeEach(() => {
+      platform.getFile.mockReturnValue(null);
     });
-    it('returns an array of correct length (devDependencies)', () => {
-      const config = {
-        depType: 'devDependencies',
-      };
-      const extractedDependencies = npmExtract.extractDependencies(
-        JSON.parse(input01Content),
-        config
-      ).deps;
-      extractedDependencies.should.be.instanceof(Array);
-      extractedDependencies.should.have.length(4);
+    it('returns null if cannot parse', async () => {
+      const res = await npmExtract.extractDependencies(
+        'not json',
+        'package.json'
+      );
+      expect(res).toBe(null);
     });
-    it('each element contains non-null depType, depName, currentVersion', () => {
-      const config = {
-        depType: 'dependencies',
-      };
-      const extractedDependencies = npmExtract.extractDependencies(
-        JSON.parse(input01Content),
-        config
-      ).deps;
-      expect(extractedDependencies).toMatchSnapshot();
-      extractedDependencies
-        .every(dep => dep.depType && dep.depName && dep.currentVersion)
-        .should.eql(true);
+    it('returns null if no deps', async () => {
+      const res = await npmExtract.extractDependencies('{}', 'package.json');
+      expect(res).toBe(null);
     });
-    it('supports null devDependencies indirect', () => {
-      const config = {
-        depType: 'dependencies',
-      };
-      const extractedDependencies = npmExtract.extractDependencies(
-        JSON.parse(input02Content),
-        config
-      ).deps;
-      extractedDependencies.should.be.instanceof(Array);
-      extractedDependencies.should.have.length(6);
+    it('handles invalid', async () => {
+      const res = await npmExtract.extractDependencies(
+        '{"dependencies": true, "devDependencies": []}',
+        'package.json'
+      );
+      expect(res).toBe(null);
     });
-    it('supports null', () => {
-      const config = {
-        depType: 'fooDpendencies',
-      };
-      const extractedDependencies = npmExtract.extractDependencies(
-        JSON.parse(input02Content),
-        config
-      ).deps;
-      extractedDependencies.should.be.instanceof(Array);
-      extractedDependencies.should.have.length(0);
+    it('returns an array of dependencies', async () => {
+      const res = await npmExtract.extractDependencies(
+        input01Content,
+        'package.json'
+      );
+      expect(res).toMatchSnapshot();
     });
-    it('finds a locked version in package-lock.json', () => {
-      const packageLockParsed = {
-        dependencies: { chalk: { version: '2.0.1' } },
-      };
-      const config = {
-        depType: 'dependencies',
-        packageLockParsed,
-      };
-      const extractedDependencies = npmExtract.extractDependencies(
-        { dependencies: { chalk: '^2.0.0', foo: '^1.0.0' } },
-        config
-      ).deps;
-      extractedDependencies.should.be.instanceof(Array);
-      extractedDependencies.should.have.length(2);
-      expect(extractedDependencies[0].lockedVersion).toBeDefined();
-      expect(extractedDependencies[1].lockedVersion).toBeUndefined();
+    it('finds a lock file', async () => {
+      platform.getFile = jest.fn(fileName => {
+        if (fileName === 'yarn.lock') {
+          return '# yarn.lock';
+        }
+        return null;
+      });
+      const res = await npmExtract.extractDependencies(
+        input01Content,
+        'package.json'
+      );
+      expect(res).toMatchSnapshot();
     });
-    it('finds a locked version in yarn.lock', () => {
-      const yarnLockParsed = {
-        object: { 'chalk@^2.0.0': { version: '2.0.1' } },
-      };
-      const config = {
-        depType: 'dependencies',
-        yarnLockParsed,
-      };
-      const extractedDependencies = npmExtract.extractDependencies(
-        { dependencies: { chalk: '^2.0.0', foo: '^1.0.0' } },
-        config
-      ).deps;
-      extractedDependencies.should.be.instanceof(Array);
-      extractedDependencies.should.have.length(2);
-      expect(extractedDependencies[0].lockedVersion).toBeDefined();
-      expect(extractedDependencies[1].lockedVersion).toBeUndefined();
-    });
-    it('handles lock error', () => {
-      const config = {
-        depType: 'dependencies',
-        packageLockParsed: true,
-      };
-      const extractedDependencies = npmExtract.extractDependencies(
-        { dependencies: { chalk: '^2.0.0', foo: '^1.0.0' } },
-        config
-      ).deps;
-      extractedDependencies.should.be.instanceof(Array);
-      extractedDependencies.should.have.length(2);
-      expect(extractedDependencies[0].lockedVersion).toBeUndefined();
-      expect(extractedDependencies[1].lockedVersion).toBeUndefined();
+    it('finds lerna', async () => {
+      platform.getFile = jest.fn(fileName => {
+        if (fileName === 'lerna.json') {
+          return '{}';
+        }
+        return null;
+      });
+      const res = await npmExtract.extractDependencies(
+        input01Content,
+        'package.json'
+      );
+      expect(res).toMatchSnapshot();
     });
   });
 });
