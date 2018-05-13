55c7121104650d73f7945c4ab417d24104d32bfa
Rhys Arkins <rhys@arkins.net>
2018-05-13 08:34:34 +0200

    fix(onboarding): rebase existing content

--- test/workers/repository/onboarding/branch/rebase.spec.js
+++ test/workers/repository/onboarding/branch/rebase.spec.js
@@ -12,12 +12,22 @@ describe('workers/repository/onboarding/branch/rebase', () => {
         ...defaultConfig,
       };
     });
+    it('does not rebase modified branch', async () => {
+      platform.getBranchPr.mockReturnValueOnce({
+        canRebase: false,
+      });
+      await rebaseOnboardingBranch(config);
+      expect(platform.commitFilesToBranch.mock.calls).toHaveLength(0);
+    });
     it('does nothing if branch is up to date', async () => {
       const contents =
         JSON.stringify(defaultConfig.onboardingConfig, null, 2) + '\n';
       platform.getFile.mockReturnValueOnce(contents); // package.json
       platform.getFile.mockReturnValueOnce(contents); // renovate.json
-      platform.getBranchPr.mockReturnValueOnce({ isStale: false });
+      platform.getBranchPr.mockReturnValueOnce({
+        canRebase: true,
+        isStale: false,
+      });
       await rebaseOnboardingBranch(config);
       expect(platform.commitFilesToBranch.mock.calls).toHaveLength(0);
     });
@@ -28,14 +38,6 @@ describe('workers/repository/onboarding/branch/rebase', () => {
       });
       await rebaseOnboardingBranch(config);
       expect(platform.commitFilesToBranch.mock.calls).toHaveLength(1);
-    });
-    it('does not rebase modified branch with additional files', async () => {
-      platform.getBranchPr.mockReturnValueOnce({
-        isStale: true,
-        canRebase: false,
-      });
-      await rebaseOnboardingBranch(config);
-      expect(platform.commitFilesToBranch.mock.calls).toHaveLength(0);
     });
   });
 });
