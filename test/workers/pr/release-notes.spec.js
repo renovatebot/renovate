6dbf9c52ad4b90781e014d5cc65fbfd8c97a8390
Rhys Arkins <rhys@arkins.net>
2018-05-16 07:04:45 +0200

    fix: Revert "feat: support retrieving changelog from ghe hosted repositories (#1968)"
    This reverts commit f2fb2e6bda1704a05f241ed7662dc05f88e36e4e.

--- test/workers/pr/release-notes.spec.js
+++ test/workers/pr/release-notes.spec.js
@@ -33,32 +33,20 @@ describe('workers/pr/release-notes', () => {
       ghGot.mockReturnValueOnce({
         body: [{ tag_name: 'v1.0.0' }, { tag_name: 'v1.0.1' }],
       });
-      const res = await getReleaseNotes(
-        'some/repository',
-        '1.0.0',
-        'https://github.com/'
-      );
+      const res = await getReleaseNotes('some/repository', '1.0.0');
       expect(res).toMatchSnapshot();
     });
   });
   describe('getReleaseNotesMd()', () => {
     it('handles not found', async () => {
-      const res = await getReleaseNotesMd(
-        'chalk',
-        '2.0.0',
-        'https://github.com/'
-      );
+      const res = await getReleaseNotesMd('chalk', '2.0.0');
       expect(res).toBe(null);
     });
     it('handles files mismatch', async () => {
       ghGot.mockReturnValueOnce({
         body: [{ name: 'lib' }, { name: 'README.md' }],
       });
-      const res = await getReleaseNotesMd(
-        'chalk',
-        '2.0.0',
-        'https://github.com/'
-      );
+      const res = await getReleaseNotesMd('chalk', '2.0.0');
       expect(res).toBe(null);
     });
     it('handles wrong format', async () => {
@@ -69,11 +57,7 @@ describe('workers/pr/release-notes', () => {
             content: Buffer.from('not really markdown').toString('base64'),
           },
         });
-      const res = await getReleaseNotesMd(
-        'some/repository1',
-        '1.0.0',
-        'https://github.com/'
-      );
+      const res = await getReleaseNotesMd('some/repository1', '1.0.0');
       expect(res).toBe(null);
     });
     it('handles bad markdown', async () => {
@@ -84,11 +68,7 @@ describe('workers/pr/release-notes', () => {
             content: Buffer.from(`#\nha\nha\n#\nha\nha`).toString('base64'),
           },
         });
-      const res = await getReleaseNotesMd(
-        'some/repository2',
-        '1.0.0',
-        'https://github.com/'
-      );
+      const res = await getReleaseNotesMd('some/repository2', '1.0.0');
       expect(res).toBe(null);
     });
     it('parses angular.js', async () => {
@@ -99,11 +79,7 @@ describe('workers/pr/release-notes', () => {
             content: Buffer.from(angularJsChangelogMd).toString('base64'),
           },
         });
-      const res = await getReleaseNotesMd(
-        'angular/angular.js',
-        '1.6.9',
-        'https://github.com/'
-      );
+      const res = await getReleaseNotesMd('angular/angular.js', '1.6.9');
       expect(res).not.toBe(null);
       expect(res).toMatchSnapshot();
     });
@@ -115,11 +91,7 @@ describe('workers/pr/release-notes', () => {
             content: Buffer.from(jestChangelogMd).toString('base64'),
           },
         });
-      const res = await getReleaseNotesMd(
-        'facebook/jest',
-        '22.0.0',
-        'https://github.com/'
-      );
+      const res = await getReleaseNotesMd('facebook/jest', '22.0.0');
       expect(res).not.toBe(null);
       expect(res).toMatchSnapshot();
     });
@@ -131,11 +103,7 @@ describe('workers/pr/release-notes', () => {
             content: Buffer.from(jsYamlChangelogMd).toString('base64'),
           },
         });
-      const res = await getReleaseNotesMd(
-        'nodeca/js-yaml',
-        '3.10.0',
-        'https://github.com/'
-      );
+      const res = await getReleaseNotesMd('nodeca/js-yaml', '3.10.0');
       expect(res).not.toBe(null);
       expect(res).toMatchSnapshot();
     });
