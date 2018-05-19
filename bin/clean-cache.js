c89929d3e252202e43346a591d0033f0a5db82fd
Rhys Arkins <rhys@arkins.net>
2018-05-19 08:22:37 +0200

    fix: rename changelog cache file

--- bin/clean-cache.js
+++ bin/clean-cache.js
@@ -2,6 +2,6 @@ const fs = require('fs-extra');
 const os = require('os');
 
 (async () => {
-  await fs.remove(os.tmpdir() + '/renovate-changelog-cache');
+  await fs.remove(os.tmpdir() + '/renovate-cache-changelog');
   await fs.remove(os.tmpdir() + '/renovate-npm-cache');
 })();
