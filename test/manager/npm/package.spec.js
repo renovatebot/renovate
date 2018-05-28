1c115ae6b67ae75f61f92a63c1833039ef55a0ef
Rhys Arkins <rhys@arkins.net>
2018-05-28 13:25:54 +0200

    refactor: move getRangeStrategy into npm/lookup

--- test/manager/npm/package.spec.js
+++ test/manager/npm/package.spec.js
@@ -8,48 +8,6 @@ jest.mock('../../../lib/manager/_helpers/node/package');
 npmApi.getDependency = jest.fn();
 
 describe('lib/manager/npm/package', () => {
-  describe('getRangeStrategy', () => {
-    it('returns same if not auto', () => {
-      const config = { rangeStrategy: 'widen' };
-      expect(npm.getRangeStrategy(config)).toEqual('widen');
-    });
-    it('pins devDependencies', () => {
-      const config = { rangeStrategy: 'auto', depType: 'devDependencies' };
-      expect(npm.getRangeStrategy(config)).toEqual('pin');
-    });
-    it('pins app dependencies', () => {
-      const config = {
-        rangeStrategy: 'auto',
-        depType: 'dependencies',
-        packageJsonType: 'app',
-      };
-      expect(npm.getRangeStrategy(config)).toEqual('pin');
-    });
-    it('widens peerDependencies', () => {
-      const config = { rangeStrategy: 'auto', depType: 'peerDependencies' };
-      expect(npm.getRangeStrategy(config)).toEqual('widen');
-    });
-    it('widens complex ranges', () => {
-      const config = {
-        rangeStrategy: 'auto',
-        depType: 'dependencies',
-        currentVersion: '^1.6.0 || ^2.0.0',
-      };
-      expect(npm.getRangeStrategy(config)).toEqual('widen');
-    });
-    it('widens complex bump', () => {
-      const config = {
-        rangeStrategy: 'bump',
-        depType: 'dependencies',
-        currentVersion: '^1.6.0 || ^2.0.0',
-      };
-      expect(npm.getRangeStrategy(config)).toEqual('widen');
-    });
-    it('defaults to replace', () => {
-      const config = { rangeStrategy: 'auto', depType: 'dependencies' };
-      expect(npm.getRangeStrategy(config)).toEqual('replace');
-    });
-  });
   describe('getPackageUpdates', () => {
     let config;
     beforeEach(() => {
