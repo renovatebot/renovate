784a0598b928faad758d0488691cfbf89cec705e
Rhys Arkins <rhys@arkins.net>
2018-06-01 15:32:17 +0200

    refactor: use options for github datasource

--- test/datasource/github.spec.js
+++ test/datasource/github.spec.js
@@ -6,7 +6,7 @@ jest.mock('got');
 
 describe('datasource/github', () => {
   describe('getDependency', () => {
-    it('returns stripped versions', async () => {
+    it('returns cleaned tags', async () => {
       const body = [
         { ref: 'refs/tags/a' },
         { ref: 'refs/tags/b' },
@@ -14,10 +14,28 @@ describe('datasource/github', () => {
         { ref: 'refs/tags/v1.1.0' },
       ];
       ghGot.mockReturnValueOnce({ headers: {}, body });
-      const res = await github.getDependency('some/dep');
+      const res = await github.getDependency('some/dep', { clean: 'true' });
       expect(res).toMatchSnapshot();
       expect(Object.keys(res.versions)).toHaveLength(2);
       expect(res.versions['1.1.0']).toBeDefined();
+    });
+    it('returns releases', async () => {
+      const body = [
+        { tag_name: 'a' },
+        { tag_name: 'b' },
+        { tag_name: '1.0.0' },
+        { tag_name: 'v1.1.0' },
+      ];
+      ghGot.mockReturnValueOnce({ headers: {}, body });
+      const res = await github.getDependency('some/dep', { ref: 'release' });
+      expect(res).toMatchSnapshot();
+      expect(Object.keys(res.versions)).toHaveLength(2);
+      expect(res.versions['v1.1.0']).toBeDefined();
+    });
+    it('returns null for invalid ref', async () => {
+      expect(
+        await github.getDependency('some/dep', { ref: 'invalid' })
+      ).toBeNull();
     });
   });
 });
