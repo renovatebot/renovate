85f17eed749100b42efa89eb103bc503fce8e45e
Ayoub Kaanich <kayoub5@live.com>
2018-06-03 18:24:27 +0200

    feat: pep440 versioning

--- test/versioning/index.spec.js
+++ test/versioning/index.spec.js
@@ -5,4 +5,12 @@ describe('versioning(versionScheme)', () => {
     expect(versioning(undefined)).toBe(versioning('semver'));
     expect(versioning('unknown')).toBe(versioning('semver'));
   });
+
+  it('should return the same interface', () => {
+    const semverApi = Object.keys(versioning('semver'));
+    const pep440Api = Object.keys(versioning('pep440')).concat(
+      'isLessThanRange'
+    );
+    expect(pep440Api.sort()).toEqual(semverApi.sort());
+  });
 });
