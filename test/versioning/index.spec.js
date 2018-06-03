515f297194de5cbd93c8d462ad7b009059d7d780
Ayoub Kaanich <kayoub5@live.com>
2018-06-03 09:31:41 +0100

    update

--- test/versioning/index.spec.js
+++ test/versioning/index.spec.js
@@ -8,8 +8,9 @@ describe('versioning(versionScheme)', () => {
 
   it('should return the same interface', () => {
     const semverApi = Object.keys(versioning('semver'));
-    const pep440Api = Object.keys(versioning('pep440'))
-      .concat('isLessThanRange');
+    const pep440Api = Object.keys(versioning('pep440')).concat(
+      'isLessThanRange'
+    );
     expect(pep440Api.sort()).toEqual(semverApi.sort());
   });
 });
