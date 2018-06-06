6fc87cbf6fb41ddb1139e550f38ace616dce0563
Rhys Arkins <rhys@arkins.net>
2018-06-06 14:32:22 +0200

    add test

---
+++ test/manager/composer/extract.spec.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const {
+  extractDependencies,
+} = require('../../../lib/manager/composer/extract');
+
+const requirements1 = fs.readFileSync(
+  'test/_fixtures/composer/composer1.json',
+  'utf8'
+);
+
+describe('lib/manager/pip_requirements/extract', () => {
+  describe('extractDependencies()', () => {
+    let config;
+    beforeEach(() => {
+      config = {};
+    });
+    it('returns null for invalid json', () => {
+      expect(extractDependencies('nothing here', config)).toBe(null);
+    });
+    it('extracts dependencies', () => {
+      const res = extractDependencies(requirements1, config);
+      expect(res).toMatchSnapshot();
+    });
+  });
+});
