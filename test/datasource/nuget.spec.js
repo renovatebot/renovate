bf54f31406587892a3d144f9f58a1493f9b4e932
Rhys Arkins <rhys@arkins.net>
2018-06-08 17:22:17 +0200

    feat: nuget datasource - versions only

---
+++ test/datasource/nuget.spec.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const datasource = require('../../lib/datasource');
+const got = require('got');
+
+jest.mock('got');
+
+const res1 = fs.readFileSync('test/_fixtures/nuget/nunit.json');
+
+describe('datasource/nuget', () => {
+  describe('getDependency', () => {
+    it('returns null for empty result', async () => {
+      got.mockReturnValueOnce({});
+      expect(await datasource.getDependency('pkg:nuget/something')).toBeNull();
+    });
+    it('returns null for 404', async () => {
+      got.mockImplementationOnce(() =>
+        Promise.reject({
+          statusCode: 404,
+        })
+      );
+      expect(await datasource.getDependency('pkg:nuget/something')).toBeNull();
+    });
+    it('returns null for unknown error', async () => {
+      got.mockImplementationOnce(() => {
+        throw new Error();
+      });
+      expect(await datasource.getDependency('pkg:nuget/something')).toBeNull();
+    });
+    it('processes real data', async () => {
+      got.mockReturnValueOnce({
+        body: JSON.parse(res1),
+      });
+      expect(
+        await datasource.getDependency('pkg:nuget/nunit')
+      ).toMatchSnapshot();
+    });
+  });
+});
