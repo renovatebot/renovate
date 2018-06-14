466cb07d02af616ec3f5d0f8c53517562277a9b6
Rhys Arkins <rhys@arkins.net>
2018-06-14 09:39:23 +0200

    refactor: changelog filename

--- bin/clean-cache.js
+++ bin/clean-cache.js
@@ -2,6 +2,6 @@ const fs = require('fs-extra');
 const os = require('os');
 
 (async () => {
-  await fs.remove(os.tmpdir() + '/renovate-cache-changelog');
+  await fs.remove(os.tmpdir() + '/renovate-cache-changelog-v2');
   await fs.remove(os.tmpdir() + '/renovate-npm-cache');
 })();
