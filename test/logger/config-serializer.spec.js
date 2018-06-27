d8b50d7b0924d2a30df0e6179ede708495a5389b
Rhys Arkins <rhys@arkins.net>
2018-06-27 07:21:17 +0200

    logs: simplify releases

--- test/logger/config-serializer.spec.js
+++ test/logger/config-serializer.spec.js
@@ -22,4 +22,23 @@ describe('logger/config-serializer', () => {
     };
     expect(configSerializer(config)).toMatchSnapshot();
   });
+  it('simplifies releases', () => {
+    const config = {
+      releases: [
+        {
+          version: '1.0.0-rc.2',
+          gitRef: 'f6fd3d463e4a1c70dd7c2b2e4ea8637ccb05f2c7',
+          time: '2018-06-18T21:18:58.840Z',
+          canBeUnpublished: false,
+        },
+        {
+          version: '1.0.0-rc.3',
+          gitRef: 'da10c705e55c03dbbeeefa7f30e86225b10a7b82',
+          time: '2018-06-26T19:12:38.659Z',
+          canBeUnpublished: true,
+        },
+      ],
+    };
+    expect(configSerializer(config)).toMatchSnapshot();
+  });
 });
