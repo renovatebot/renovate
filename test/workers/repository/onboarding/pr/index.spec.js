945353376b5ca7f9ef5375d71ebcb0fa5650df71
Rhys Arkins <rhys@arkins.net>
2018-07-22 22:18:11 +0200

    feat(onboarding): use <details><summary>
    Closes #2304

--- test/workers/repository/onboarding/pr/index.spec.js
+++ test/workers/repository/onboarding/pr/index.spec.js
@@ -19,6 +19,7 @@ describe('workers/repository/onboarding/pr', () => {
       };
       packageFiles = { npm: [{ packageFile: 'package.json' }] };
       branches = [];
+      platform.getPrBody = jest.fn(input => input);
       platform.createPr.mockReturnValue({});
     });
     let createPrBody;
