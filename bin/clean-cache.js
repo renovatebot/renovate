c3a4b90411c662c4fbb92f27b7b71eab016d6e31
Rhys Arkins <rhys@arkins.net>
2018-09-05 11:03:06 +0200

    chore: fix clean-cache paths

--- bin/clean-cache.js
+++ bin/clean-cache.js
@@ -2,6 +2,5 @@ const fs = require('fs-extra');
 const os = require('os');
 
 (async () => {
-  await fs.remove(os.tmpdir() + '/renovate-cache-changelog-v3');
-  await fs.remove(os.tmpdir() + '/renovate-npm-cache');
+  await fs.remove(os.tmpdir() + '/renovate-cache-v1');
 })();
