133505da092ae7a935baf0c3eba33aa85cf274a0
Rhys Arkins <rhys@arkins.net>
2018-09-11 14:02:06 +0200

    feat(docker): cache digests for 30 minutes

--- test/datasource/docker.spec.js
+++ test/datasource/docker.spec.js
@@ -13,6 +13,7 @@ describe('api/docker', () => {
         username: 'some-username',
         password: 'some-password',
       });
+      return global.renovateCache.rmAll();
     });
     it('returns null if no token', async () => {
       got.mockReturnValueOnce({ body: {} });
@@ -46,6 +47,28 @@ describe('api/docker', () => {
         'some-new-value'
       );
       expect(res).toBe('some-digest');
+    });
+    it('returns from cache', async () => {
+      got.mockReturnValueOnce({
+        headers: {
+          'www-authenticate':
+            'Bearer realm="https://auth.docker.io/token",service="registry.docker.io",scope="repository:samalba/my-app:pull  "',
+        },
+      });
+      got.mockReturnValueOnce({ body: { token: 'some-token' } });
+      got.mockReturnValueOnce({
+        headers: { 'docker-content-digest': 'some-digest' },
+      });
+      const res = await docker.getDigest(
+        { depName: 'some-dep-to-cache' },
+        'some-newer-value'
+      );
+      expect(res).toBe('some-digest');
+      const res2 = await docker.getDigest(
+        { depName: 'some-dep-to-cache' },
+        'some-newer-value'
+      );
+      expect(res2).toBe('some-digest');
     });
     it('falls back to body for digest', async () => {
       got.mockReturnValueOnce({
