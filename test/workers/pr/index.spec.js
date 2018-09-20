8769ae8e06e4a86919377e10fbabde6405dc5633
Rhys Arkins <rhys@arkins.net>
2018-09-20 17:34:23 +0200

    Revert "update tests"
    This reverts commit 90f6a39db522f2d4b5ac1bb70336ccb796bbf055.

--- test/workers/pr/index.spec.js
+++ test/workers/pr/index.spec.js
@@ -239,6 +239,21 @@ describe('workers/pr', () => {
       expect(platform.addAssignees.mock.calls.length).toBe(0);
       expect(platform.addReviewers.mock.calls.length).toBe(0);
     });
+    it('should add assignees and reviewers to existing PR', async () => {
+      config.assignees = ['bar'];
+      config.reviewers = ['baz'];
+      config.automerge = true;
+      config.schedule = 'before 5am';
+      platform.getBranchPr.mockReturnValueOnce(existingPr);
+      platform.getBranchStatus.mockReturnValueOnce('failure');
+      config.semanticCommitScope = null;
+      const pr = await prWorker.ensurePr(config);
+      expect(platform.updatePr.mock.calls).toMatchSnapshot();
+      expect(platform.updatePr.mock.calls.length).toBe(0);
+      expect(platform.addAssignees.mock.calls.length).toBe(1);
+      expect(platform.addReviewers.mock.calls.length).toBe(1);
+      expect(pr).toMatchObject(existingPr);
+    });
     it('should return unmodified existing PR', async () => {
       platform.getBranchPr.mockReturnValueOnce(existingPr);
       config.semanticCommitScope = null;
