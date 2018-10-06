73a54d55fa987ece58e08c743eeb3470d2e324db
Rhys Arkins <rhys@arkins.net>
2018-10-06 06:36:27 +0200

    update fixture

--- test/_fixtures/terraform/1.tf
+++ test/_fixtures/terraform/1.tf
@@ -31,3 +31,38 @@ module "task_definition" {
 module "consul" {
   source = "git@github.com:hashicorp/example.git?ref=v2.0.0"
 }
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}
+
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "2.4.0"
+
+  name        = "user-service"
+  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
