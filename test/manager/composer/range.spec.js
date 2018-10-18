85dd6b02db76f341a048412ca0935569982f49b5
Rhys Arkins <rhys@arkins.net>
2018-10-18 04:28:10 +0200

    feat(composer): support auto rangeStrategy

---
+++ test/manager/composer/range.spec.js
@@ -0,0 +1,32 @@
+const { getRangeStrategy } = require('../../../lib/manager/composer');
+
+describe('getRangeStrategy', () => {
+  it('returns same if not auto', () => {
+    const config = { rangeStrategy: 'widen' };
+    expect(getRangeStrategy(config)).toEqual('widen');
+  });
+  it('pins require-dev', () => {
+    const config = { rangeStrategy: 'auto', depType: 'require-dev' };
+    expect(getRangeStrategy(config)).toEqual('pin');
+  });
+  it('widens complex ranges', () => {
+    const config = {
+      rangeStrategy: 'auto',
+      depType: 'require',
+      currentValue: '^1.6.0 || ^2.0.0',
+    };
+    expect(getRangeStrategy(config)).toEqual('widen');
+  });
+  it('widens complex bump', () => {
+    const config = {
+      rangeStrategy: 'bump',
+      depType: 'require',
+      currentValue: '^1.6.0 || ^2.0.0',
+    };
+    expect(getRangeStrategy(config)).toEqual('widen');
+  });
+  it('defaults to replace', () => {
+    const config = { rangeStrategy: 'auto', depType: 'require' };
+    expect(getRangeStrategy(config)).toEqual('replace');
+  });
+});
