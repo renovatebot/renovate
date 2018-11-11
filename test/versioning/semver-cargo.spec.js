4feb9f913e49df1c2ea2d8556b6d22b8d28121d1
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-11-11 16:41:44 +0300

    semver-cargo: Add isLessThanRange function
    * Add test for isLessThanRange
    * Update/expand tests for other functions

--- test/versioning/semver-cargo.spec.js
+++ test/versioning/semver-cargo.spec.js
@@ -2,8 +2,11 @@ const semver = require('../../lib/versioning')('semverCargo');
 
 describe('semver.matches()', () => {
   it('handles comma', () => {
-    expect(semver.matches('4.2.0', '2.0, >= 3.0')).toBe(true);
+    expect(semver.matches('4.2.0', '2.0, >= 3.0 < 5.0.0')).toBe(true);
     expect(semver.matches('4.2.0', '4.0.0, 3.0.0')).toBe(false);
+    expect(semver.matches('4.2.0', '4.0.0, 3.0.0, > 5.0.0 <= 6.0.0')).toBe(
+      false
+    );
   });
 });
 describe('semver.maxSatisfyingVersion()', () => {
@@ -24,17 +27,16 @@ describe('semver.maxSatisfyingVersion()', () => {
 });
 describe('semver.isValid()', () => {
   it('handles comma', () => {
-    expect(semver.isValid('>= 1.0.0, <= 2.0.0')).toBeTruthy();
-    expect(semver.isValid('>= 1.0.0,<= 2.0.0')).toBeTruthy();
+    expect(semver.isValid('< 3.0, >= 1.0.0 <= 2.0.0')).toBeTruthy();
+    expect(semver.isValid('< 3.0, >= 1.0.0 <= 2.0.0')).toBeTruthy();
   });
 });
-/* TODO: Investigate why test fails */
-// describe('semver.isLessThanRange()', () => {
-//   it('handles comma', () => {
-//     expect(semver.isLessThanRange('0.9.0', '>= 1.0.0, <= 2.0.0')).toBe(true);
-//     expect(semver.isLessThanRange('1.9.0', '>= 1.0.0, <= 2.0.0')).toBe(false);
-//   });
-// });
+describe('semver.isLessThanRange()', () => {
+  it('handles comma', () => {
+    expect(semver.isLessThanRange('0.9.0', '>= 1.0.0 <= 2.0.0')).toBe(true);
+    expect(semver.isLessThanRange('1.9.0', '>= 1.0.0 <= 2.0.0')).toBe(false);
+  });
+});
 describe('semver.minSatisfyingVersion()', () => {
   it('handles comma', () => {
     expect(
