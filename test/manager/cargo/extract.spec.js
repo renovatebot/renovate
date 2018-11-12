6290bf74fa82ae748416600ffc5492b72158def4
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-11-12 09:26:16 +0300

    cargo: Add stub implementation for extractPackageFile and add tests
    * Add Cargo.1.toml and Cargo.2.toml fixtures
    * Implement two tests counting number of dependenceis in fixtures
    
    Stub implementation just returns a list of dependency names and line numbers.

--- test/manager/cargo/extract.spec.js
+++ test/manager/cargo/extract.spec.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
 const { extractPackageFile } = require('../../../lib/manager/cargo/extract');
+
+const cargo1toml = fs.readFileSync('test/_fixtures/cargo/Cargo.1.toml', 'utf8');
+const cargo2toml = fs.readFileSync('test/_fixtures/cargo/Cargo.2.toml', 'utf8');
 
 describe('lib/manager/cargo/extract', () => {
   describe('extractPackageFile()', () => {
@@ -8,6 +12,16 @@ describe('lib/manager/cargo/extract', () => {
     });
     it('returns null for empty', () => {
       expect(extractPackageFile('nothing here', config)).toBe(null);
+    });
+    it('extracts multiple dependencies', () => {
+      const res = extractPackageFile(cargo1toml, config);
+      // expect(res.deps).toMatchSnapshot();
+      expect(res.deps).toHaveLength(6);
+    });
+    it('extracts multiple dependencies', () => {
+      const res = extractPackageFile(cargo2toml, config);
+      // expect(res.deps).toMatchSnapshot();
+      expect(res.deps).toHaveLength(18 + 6 + 1);
     });
   });
 });
