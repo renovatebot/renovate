68837cd996482ba10d5b7dddeee081dd04a0a6c3
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-11-15 12:41:19 +0300

    cargo: Handle standard tables in extractPackageFile
    * For extractPackageFile test for matching the snapshot
    * Add test for standard table handling (Cargo.4.toml)

--- test/manager/cargo/extract.spec.js
+++ test/manager/cargo/extract.spec.js
@@ -4,6 +4,7 @@ const { extractPackageFile } = require('../../../lib/manager/cargo/extract');
 const cargo1toml = fs.readFileSync('test/_fixtures/cargo/Cargo.1.toml', 'utf8');
 const cargo2toml = fs.readFileSync('test/_fixtures/cargo/Cargo.2.toml', 'utf8');
 const cargo3toml = fs.readFileSync('test/_fixtures/cargo/Cargo.3.toml', 'utf8');
+const cargo4toml = fs.readFileSync('test/_fixtures/cargo/Cargo.4.toml', 'utf8');
 
 describe('lib/manager/cargo/extract', () => {
   describe('extractPackageFile()', () => {
@@ -16,18 +17,23 @@ describe('lib/manager/cargo/extract', () => {
     });
     it('extracts multiple dependencies', () => {
       const res = extractPackageFile(cargo1toml, config);
-      // expect(res.deps).toMatchSnapshot();
-      expect(res.deps).toHaveLength(6);
+      expect(res.deps).toMatchSnapshot();
+      expect(res.deps).toHaveLength(5);
     });
     it('extracts multiple dependencies', () => {
       const res = extractPackageFile(cargo2toml, config);
-      // expect(res.deps).toMatchSnapshot();
+      expect(res.deps).toMatchSnapshot();
       expect(res.deps).toHaveLength(18 + 6 + 1);
     });
-    it('extracts handles inline tables', () => {
+    it('handles inline tables', () => {
       const res = extractPackageFile(cargo3toml, config);
-      // expect(res.deps).toMatchSnapshot();
+      expect(res.deps).toMatchSnapshot();
       expect(res.deps).toHaveLength(6);
+    });
+    it('handles standard tables', () => {
+      const res = extractPackageFile(cargo4toml, config);
+      expect(res.deps).toMatchSnapshot();
+      expect(res.deps).toHaveLength(4);
     });
   });
 });
