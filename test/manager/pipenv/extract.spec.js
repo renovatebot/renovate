a4165ee140ebf0ed7744c5e74f25785441ce3ef9
Rhys Arkins <rhys@arkins.net>
2018-11-17 08:24:34 +0100

    update tests

--- test/manager/pipenv/extract.spec.js
+++ test/manager/pipenv/extract.spec.js
@@ -1,39 +1,39 @@
 const fs = require('fs');
-const { extractDependencies } = require('../../../lib/manager/pipenv/extract');
+const { extractPackageFile } = require('../../../lib/manager/pipenv/extract');
 
 const pipfile1 = fs.readFileSync('test/_fixtures/pipenv/Pipfile1', 'utf8');
 const pipfile2 = fs.readFileSync('test/_fixtures/pipenv/Pipfile2', 'utf8');
 
 describe('lib/manager/pipenv/extract', () => {
-  describe('extractDependencies()', () => {
+  describe('extractPackageFile()', () => {
     let config;
     beforeEach(() => {
       config = {};
     });
     it('returns null for empty', () => {
-      expect(extractDependencies('[packages]\r\n', config)).toBe(null);
+      expect(extractPackageFile('[packages]\r\n', config)).toBe(null);
     });
     it('returns null for invalid toml file', () => {
-      expect(extractDependencies('nothing here', config)).toBe(null);
+      expect(extractPackageFile('nothing here', config)).toBe(null);
     });
     it('extracts dependencies', () => {
-      const res = extractDependencies(pipfile1, config).deps;
+      const res = extractPackageFile(pipfile1, config).deps;
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(3);
     });
     it('extracts multiple dependencies', () => {
-      const res = extractDependencies(pipfile2, config).deps;
+      const res = extractPackageFile(pipfile2, config).deps;
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(5);
     });
     it('ignores invalid package names', () => {
       const content = '[packages]\r\nfoo = "==1.0.0"\r\n_invalid = "==1.0.0"';
-      const res = extractDependencies(content, config).deps;
+      const res = extractPackageFile(content, config).deps;
       expect(res).toHaveLength(1);
     });
     it('ignores invalid versions', () => {
       const content = '[packages]\r\nfoo = "==1.0.0"\r\nsome-package = "==0 0"';
-      const res = extractDependencies(content, config).deps;
+      const res = extractPackageFile(content, config).deps;
       expect(res).toHaveLength(1);
     });
     it('extracts all sources', () => {
@@ -41,7 +41,7 @@ describe('lib/manager/pipenv/extract', () => {
         '[[source]]\r\nurl = "source-url"\r\n' +
         '[[source]]\r\nurl = "other-source-url"\r\n' +
         '[packages]\r\nfoo = "==1.0.0"\r\n';
-      const res = extractDependencies(content, config).deps;
+      const res = extractPackageFile(content, config).deps;
       expect(res[0].registryUrls).toEqual(['source-url', 'other-source-url']);
     });
     it('converts simple-API URLs to JSON-API URLs', () => {
@@ -49,7 +49,7 @@ describe('lib/manager/pipenv/extract', () => {
         '[[source]]\r\nurl = "https://my-pypi/foo/simple/"\r\n' +
         '[[source]]\r\nurl = "https://other-pypi/foo/simple"\r\n' +
         '[packages]\r\nfoo = "==1.0.0"\r\n';
-      const res = extractDependencies(content, config).deps;
+      const res = extractPackageFile(content, config).deps;
       expect(res[0].registryUrls).toEqual([
         'https://my-pypi/foo/pypi/',
         'https://other-pypi/foo/pypi/',
