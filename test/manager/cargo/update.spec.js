dd1deeec9c2ff06b35db84c98b9e01917fc7b0c6
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-11-19 13:15:38 +0300

    cargo: Add initial version of updateDependency and a test

--- test/manager/cargo/update.spec.js
+++ test/manager/cargo/update.spec.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
 const { updateDependency } = require('../../../lib/manager/cargo/update');
+
+const cargo1toml = fs.readFileSync('test/_fixtures/cargo/Cargo.1.toml', 'utf8');
+const cargo5toml = fs.readFileSync('test/_fixtures/cargo/Cargo.5.toml', 'utf8');
 
 describe('lib/manager/cargo/update', () => {
   describe('updateDependency()', () => {
@@ -8,6 +12,15 @@ describe('lib/manager/cargo/update', () => {
     });
     it('returns same', () => {
       expect(updateDependency('abc', config)).toEqual('abc');
+    });
+    it('updates dependency', () => {
+      upgrade = {
+        depName : 'libc',
+        lineNumber : 16,
+        depType : 'normal',
+        newValue : '0.3.0',
+      };
+      expect(updateDependency(cargo1toml, upgrade)).toEqual(cargo5toml);
     });
   });
 });
