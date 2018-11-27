200c01dcde771add158208d4afdea5c0f9b608c1
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2018-11-27 18:55:07 +0300

    cargo: Add "catches error" test for getArtifacts function

--- test/manager/cargo/artifacts.spec.js
+++ test/manager/cargo/artifacts.spec.js
@@ -20,11 +20,21 @@ describe('cargo.getArtifacts()', () => {
   it('returns null by default', async () => {
     expect(await cargo.getArtifacts('Cargo.toml', [], '', config)).toBeNull();
   });
-  it('catches errors', async () => {
+  it('returns null by default', async () => {
     // NOTE: In this case cargo.getArtifacts() returns null, because
     // updatedDeps array is empty
+    // TODO: Figure out if this is correct behavior
     expect(await cargo.getArtifacts()).toBeNull();
-    // expect(await cargo.getArtifacts()).toMatchSnapshot();
+  });
+  it('catches errors', async () => {
+    const dep = {
+      depName: 'INVALID_DEP',
+      currentValue: 'INVALID_DEP',
+    };
+    // NOTE: If config.localDir is not set getArtifacts throws an exception
+    // TODO: Figure out if this is correct behavior, most likely not -- it should
+    // probably default to current working directory
+    expect(await cargo.getArtifacts('Cargo.toml', [dep], '', {})).toMatchSnapshot();
   });
   it('generates Cargo.lock', async () => {
     const updatedDeps = extractPackageFile(cargoToml, 'Cargo.toml').deps;
