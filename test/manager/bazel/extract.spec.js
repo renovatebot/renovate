bd311be43b9e1cabcf691ee0111c2ca946b7057e
Nyarum <nyarumilian@gmail.com>
2018-11-28 00:25:41 +0400

    chore: add support remote option in go_repository

--- test/manager/bazel/extract.spec.js
+++ test/manager/bazel/extract.spec.js
@@ -24,5 +24,19 @@ describe('lib/manager/bazel/extract', () => {
       const res = extractPackageFile(workspaceFile, config);
       expect(res.deps).toMatchSnapshot();
     });
+    it('check replace remote instead importpath', () => {
+      const res = extractPackageFile(
+        `
+go_repository(
+  name = "test_repository",
+  importpath = "github.com/google/uuid",
+  remote = "https://github.com/test/uuid",
+  commit = "dec09d789f3dba190787f8b4454c7d3c936fed9e"
+)
+        `,
+        config
+      );
+      expect(res.deps[0].purl).toBe('pkg:go/github.com/test/uuid');
+    });
   });
 });
