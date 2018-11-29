03e31ec21f141319a3eee703066ef7429df5a0fe
Rhys Arkins <rhys@arkins.net>
2018-11-29 12:22:48 +0100

    add failing test case

--- test/manager/buildkite/extract.spec.js
+++ test/manager/buildkite/extract.spec.js
@@ -15,6 +15,10 @@ const pipeline3 = fs.readFileSync(
   'test/_fixtures/buildkite/pipeline3.yml',
   'utf8'
 );
+const pipeline4 = fs.readFileSync(
+  'test/_fixtures/buildkite/pipeline4.yml',
+  'utf8'
+);
 
 describe('lib/manager/buildkite/extract', () => {
   describe('extractPackageFile()', () => {
@@ -39,6 +43,11 @@ describe('lib/manager/buildkite/extract', () => {
       const res = extractPackageFile(pipeline3, config).deps;
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(3);
+    });
+    it('extracts arrays of plugins', () => {
+      const res = extractPackageFile(pipeline4, config).deps;
+      expect(res).toMatchSnapshot();
+      expect(res).toHaveLength(4);
     });
   });
 });
