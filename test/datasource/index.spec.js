0f0bae4b65609bb6a2bd5e72c51d89f9907cedfa
Rhys Arkins <rhys@arkins.net>
2018-12-10 06:34:39 +0100

    refactor: datasource manual urls
    Provide a central location for mapping changelog and source URLs.

--- test/datasource/index.spec.js
+++ test/datasource/index.spec.js
@@ -1,6 +1,8 @@
 const datasource = require('../../lib/datasource');
+const npmDatasource = require('../../lib/datasource/npm');
 
 jest.mock('../../lib/datasource/docker');
+jest.mock('../../lib/datasource/npm');
 
 describe('datasource/index', () => {
   it('returns null for invalid purl', async () => {
@@ -10,5 +12,12 @@ describe('datasource/index', () => {
     expect(
       await datasource.getDigest({ purl: 'pkg:docker/node' })
     ).toBeUndefined();
+  });
+  it('adds changelogUrl', async () => {
+    npmDatasource.getPkgReleases.mockReturnValue({});
+    const res = await datasource.getPkgReleases('pkg:npm/react-native');
+    expect(res).toMatchSnapshot();
+    expect(res.changelogUrl).toBeDefined();
+    expect(res.sourceUrl).toBeDefined();
   });
 });
