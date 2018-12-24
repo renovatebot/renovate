5941eae63f07e81ba972433aba691f83ece3c198
corecanarias <ieb.core@gmail.com>
2018-12-24 11:41:09 +0000

    add tests

--- test/datasource/maven.spec.js
+++ test/datasource/maven.spec.js
@@ -6,14 +6,33 @@ const { initLogger } = require('../../lib/logger');
 
 initLogger();
 
+const MYSQL_VERSIONS = [
+  '8.0.12',
+  '8.0.11',
+  '8.0.9',
+  '8.0.8',
+  '8.0.7',
+  '6.0.6',
+  '6.0.5',
+];
+
+const MYSQL_MAVEN_METADATA = fs.readFileSync(
+  'test/_fixtures/gradle/maven/repo1.maven.org/maven2/mysql/mysql-connector-java/maven-metadata.xml',
+  'utf8'
+);
+
 describe('datasource/maven', () => {
-  afterEach(() => {
-    jest.resetAllMocks();
-    nock.cleanAll();
+  beforeEach(() => {
+    nock('http://central.maven.org')
+      .get('/maven2/mysql/mysql-connector-java/maven-metadata.xml')
+      .reply(200, MYSQL_MAVEN_METADATA);
+    nock('http://failed_repo')
+      .get('/mysql/mysql-connector-java/maven-metadata.xml')
+      .reply(404, null);
   });
 
   describe('getPkgReleases', () => {
-    it('should return empty if library not found', async () => {
+    it('should return empty if library is not found', async () => {
       const releases = await datasource.getPkgReleases(
         'pkg:maven/unknown/unknown@1.0.5?repository_url=file://test/_fixtures/gradle/maven/repo1.maven.org/maven2/'
       );
@@ -37,75 +56,34 @@ describe('datasource/maven', () => {
       );
     });
 
-    it('should return all versions of a specific library in all repositories', async () => {
+    it('should return versions in all repositories for a specific library', async () => {
       const releases = await datasource.getPkgReleases(
         'pkg:maven/mysql/mysql-connector-java@6.0.5?repository_url=file://test/_fixtures/gradle/maven/repo1.maven.org/maven2/,file://test/_fixtures/gradle/maven/custom_maven_repo/maven2/'
       );
       expect(releases.releases).toEqual(
-        generateReleases([
-          '8.0.12',
-          '8.0.11',
-          '8.0.9',
-          '8.0.8',
-          '8.0.7',
-          '6.0.6',
-          '6.0.5',
-          '6.0.4',
-        ])
+        generateReleases([...MYSQL_VERSIONS, '6.0.4'])
       );
     });
 
-    it('should return all versions of a specific library in all http repositories', async () => {
-      const mavenMetadata = fs.readFileSync(
-        'test/_fixtures/gradle/maven/repo1.maven.org/maven2/mysql/mysql-connector-java/maven-metadata.xml',
-        'utf8'
-      );
-      nock('http://central.maven.org')
-        .get('/maven2/mysql/mysql-connector-java/maven-metadata.xml')
-        .reply(200, mavenMetadata);
-
+    it('should return all versions of a specific library for http repositories', async () => {
       const releases = await datasource.getPkgReleases(
         'pkg:maven/mysql/mysql-connector-java@6.0.5?repository_url=http://central.maven.org/maven2/'
       );
-      expect(releases.releases).toEqual(
-        generateReleases([
-          '8.0.12',
-          '8.0.11',
-          '8.0.9',
-          '8.0.8',
-          '8.0.7',
-          '6.0.6',
-          '6.0.5',
-        ])
-      );
+      expect(releases.releases).toEqual(generateReleases(MYSQL_VERSIONS));
     });
 
     it('should return all versions of a specific library if a repository fails', async () => {
-      const mavenMetadata = fs.readFileSync(
-        'test/_fixtures/gradle/maven/repo1.maven.org/maven2/mysql/mysql-connector-java/maven-metadata.xml',
-        'utf8'
-      );
-      nock('http://central.maven.org')
-        .get('/maven2/mysql/mysql-connector-java/maven-metadata.xml')
-        .reply(200, mavenMetadata);
-      nock('http://failed_repo')
-        .get('/mysql/mysql-connector-java/maven-metadata.xml')
-        .reply(404, mavenMetadata);
-
       const releases = await datasource.getPkgReleases(
         'pkg:maven/mysql/mysql-connector-java@6.0.5?repository_url=http://central.maven.org/maven2/,http://failed_repo/,http://dns_error_repo'
       );
-      expect(releases.releases).toEqual(
-        generateReleases([
-          '8.0.12',
-          '8.0.11',
-          '8.0.9',
-          '8.0.8',
-          '8.0.7',
-          '6.0.6',
-          '6.0.5',
-        ])
+      expect(releases.releases).toEqual(generateReleases(MYSQL_VERSIONS));
+    });
+
+    it('should return all versions of a specific library if a repository fails because invalid protocol', async () => {
+      const releases = await datasource.getPkgReleases(
+        'pkg:maven/mysql/mysql-connector-java@6.0.5?repository_url=http://central.maven.org/maven2/,http://failed_repo/,ftp://protocol_error_repo'
       );
+      expect(releases.releases).toEqual(generateReleases(MYSQL_VERSIONS));
     });
   });
 });
