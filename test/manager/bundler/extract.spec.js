f2261faaf57e9291c46e55cd93b2f6e039133de3
Rhys Arkins <rhys@arkins.net>
2019-01-10 10:31:54 +0100

    Gemfile extract

--- test/manager/bundler/extract.spec.js
+++ test/manager/bundler/extract.spec.js
@@ -1,4 +1,21 @@
+const fs = require('fs');
 const { extractPackageFile } = require('../../../lib/manager/bundler/extract');
+
+const railsGemfile = fs.readFileSync(
+  'test/_fixtures/bundler/Gemfile.rails',
+  'utf8'
+);
+
+const sourceGroupGemfile = fs.readFileSync(
+  'test/_fixtures/bundler/Gemfile.sourceGroup',
+  'utf8'
+);
+
+function validateGems(raw, parsed) {
+  const gemfileGemCount = raw.match(/\n\s*gem\s+/g).length;
+  const parsedGemCount = parsed.deps.length;
+  expect(gemfileGemCount).toEqual(parsedGemCount);
+}
 
 describe('lib/manager/bundler/extract', () => {
   describe('extractPackageFile()', () => {
@@ -8,6 +25,16 @@ describe('lib/manager/bundler/extract', () => {
     });
     it('returns null for empty', () => {
       expect(extractPackageFile('nothing here', config)).toBeNull();
+    });
+    it('parses rails Gemfile', () => {
+      const res = extractPackageFile(railsGemfile);
+      expect(res).toMatchSnapshot();
+      validateGems(railsGemfile, res);
+    });
+    it('parses sourceGroups', () => {
+      const res = extractPackageFile(sourceGroupGemfile);
+      expect(res).toMatchSnapshot();
+      validateGems(sourceGroupGemfile, res);
     });
   });
 });
