9c68b88c5b129b6831cac9f16c4d860e5d1564f9
Daniel Hallqvist <daniel.hallqvist@nordnet.se>
2019-01-22 15:33:15 +0100

    initRepo test wip

--- test/platform/bitbucket-server/index.spec.js
+++ test/platform/bitbucket-server/index.spec.js
@@ -12,9 +12,15 @@ describe('platform/bitbucket', () => {
     // reset module
     jest.resetModules();
     jest.mock('../../../lib/platform/bitbucket-server/bb-got-wrapper');
+    jest.mock('../../../lib/platform/git/storage');
     hostRules = require('../../../lib/util/host-rules');
     api = require('../../../lib/platform/bitbucket-server/bb-got-wrapper');
     bitbucket = require('../../../lib/platform/bitbucket-server');
+    let gitStorage = require('../../../lib/platform/git/storage');
+    gitStorage.mockImplementation(() => ({
+      initRepo: () => {},
+      cleanRepo: () => {},
+    }));
 
     // clean up hostRules
     hostRules.clear();
@@ -28,6 +34,31 @@ describe('platform/bitbucket', () => {
   afterEach(() => {
     bitbucket.cleanRepo();
   });
+  async function mocked(fn) {
+    const oldGet = api.get;
+    try {
+      api.get = jest.fn().mockImplementation(mockedGet);
+      return await fn();
+    } finally {
+      api.get = oldGet;
+    }
+  }
+  async function mockedGet(path) {
+    let body = responses[URL.parse(path).pathname] || { values: [] };
+    if (typeof body === 'function') {
+      body = await body();
+    }
+    return { body };
+  }
+
+  function initRepo() {
+    return mocked(() =>
+      bitbucket.initRepo({
+        repository: 'some/repo',
+        localDir: '',
+      })
+    );
+  }
 
   describe('getRepos()', () => {
     it('returns repos', async () => {
@@ -39,6 +70,16 @@ describe('platform/bitbucket', () => {
           body: responses['/rest/api/1.0/projects/SOME/repos'],
         });
       expect(await bitbucket.getRepos()).toEqual(['some/repo']);
+    });
+  });
+
+  describe('initRepo()', () => {
+    it('works', async () => {
+      api.get.mockReturnValueOnce({
+        body: responses['/rest/api/1.0/projects/SOME/repos/repo'],
+      });
+
+      expect(await initRepo()).toMatchSnapshot();
     });
   });
 });
