762bfd118e1258cc738a798612cad8fc342dfeb2
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-03-24 18:02:51 +0300

    poetry: Add more tests for poetry versioning (get coverage to 100%)

--- test/versioning/poetry.spec.js
+++ test/versioning/poetry.spec.js
@@ -41,10 +41,107 @@ describe('semver.isSingleVersion()', () => {
     expect(!!semver.isSingleVersion('1.*')).toBe(false);
   });
 });
+describe('semver.matches()', () => {
+  it('handles comma', () => {
+    expect(semver.matches('4.2.0', '4.2, >= 3.0, < 5.0.0')).toBe(true);
+    expect(semver.matches('4.2.0', '2.0, >= 3.0, < 5.0.0')).toBe(false);
+    expect(semver.matches('4.2.2', '4.2.0, < 4.2.4')).toBe(false);
+    expect(semver.matches('4.2.2', '^4.2.0, < 4.2.4')).toBe(true);
+    expect(semver.matches('4.2.0', '4.3.0, 3.0.0')).toBe(false);
+    expect(semver.matches('4.2.0', '> 5.0.0, <= 6.0.0')).toBe(false);
+  });
+});
+describe('semver.isLessThanRange()', () => {
+  it('handles comma', () => {
+    expect(semver.isLessThanRange('0.9.0', '>= 1.0.0 <= 2.0.0')).toBe(true);
+    expect(semver.isLessThanRange('1.9.0', '>= 1.0.0 <= 2.0.0')).toBe(false);
+  });
+});
+describe('semver.minSatisfyingVersion()', () => {
+  it('handles comma', () => {
+    expect(
+      semver.minSatisfyingVersion(
+        ['0.4.0', '0.5.0', '4.2.0', '4.3.0', '5.0.0'],
+        '4.*, > 4.2'
+      )
+    ).toBe('4.3.0');
+    expect(
+      semver.minSatisfyingVersion(
+        ['0.4.0', '0.5.0', '4.2.0', '5.0.0'],
+        '^4.0.0'
+      )
+    ).toBe('4.2.0');
+    expect(
+      semver.minSatisfyingVersion(
+        ['0.4.0', '0.5.0', '4.2.0', '5.0.0'],
+        '^4.0.0, = 0.5.0'
+      )
+    ).toBe(null);
+    expect(
+      semver.minSatisfyingVersion(
+        ['0.4.0', '0.5.0', '4.2.0', '5.0.0'],
+        '^4.0.0, > 4.1.0, <= 4.3.5'
+      )
+    ).toBe('4.2.0');
+    expect(
+      semver.minSatisfyingVersion(
+        ['0.4.0', '0.5.0', '4.2.0', '5.0.0'],
+        '^6.2.0, 3.*'
+      )
+    ).toBe(null);
+  });
+});
+describe('semver.maxSatisfyingVersion()', () => {
+  it('handles comma', () => {
+    expect(
+      semver.maxSatisfyingVersion(
+        ['4.2.1', '0.4.0', '0.5.0', '4.0.0', '4.2.0', '5.0.0'],
+        '4.*.0, < 4.2.5'
+      )
+    ).toBe('4.2.1');
+    expect(
+      semver.maxSatisfyingVersion(
+        ['0.4.0', '0.5.0', '4.0.0', '4.2.0', '5.0.0', '5.0.3'],
+        '5.0, > 5.0.0'
+      )
+    ).toBe('5.0.3');
+  });
+});
+
 describe('semver.getNewValue()', () => {
+  it('bumps exact', () => {
+    expect(semver.getNewValue('1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
+      '1.1.0'
+    );
+    expect(semver.getNewValue('   1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
+      '1.1.0'
+    );
+  });
   it('bumps equals', () => {
     expect(semver.getNewValue('=1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
       '=1.1.0'
+    );
+    expect(semver.getNewValue('=  1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
+      '=1.1.0'
+    );
+  });
+  it('bumps equals space', () => {
+    expect(semver.getNewValue('= 1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
+      '=1.1.0'
+    );
+    expect(semver.getNewValue('  = 1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
+      '=1.1.0'
+    );
+    expect(semver.getNewValue('  =   1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
+      '=1.1.0'
+    );
+    expect(semver.getNewValue('=    1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
+      '=1.1.0'
+    );
+  });
+  it('bumps version range', () => {
+    expect(semver.getNewValue('1.0.0', 'bump', '1.0.0', '1.1.0')).toEqual(
+      '1.1.0'
     );
   });
   it('bumps short caret to same', () => {
@@ -52,36 +149,20 @@ describe('semver.getNewValue()', () => {
       '^1.0'
     );
   });
-  it('bumps caret to prerelease', () => {
-    expect(
-      semver.getNewValue('^1', 'bump', '1.0.0', '1.0.7-prerelease.1')
-    ).toEqual('^1.0.7-prerelease.1');
-  });
   it('replaces with newer', () => {
-    expect(semver.getNewValue('^1.0.0', 'replace', '1.0.0', '1.0.7')).toEqual(
-      '^1.0.7'
+    expect(semver.getNewValue('^1.0.0', 'replace', '1.0.0', '2.0.7')).toEqual(
+      '^2.0.0'
     );
   });
-  // TODO: Find out if poetry supports this.
-  // it('supports tilde greater than', () => {
-  //   expect(semver.getNewValue('~> 1.0.0', 'replace', '1.0.0', '1.1.7')).toEqual(
-  //     '~> 1.1.0'
-  //   );
-  // });
-  it('bumps short caret to new', () => {
-    expect(semver.getNewValue('^1.0', 'bump', '1.0.0', '1.1.7')).toEqual(
-      '^1.1'
+  it('replaces naked version', () => {
+    expect(semver.getNewValue('1.0.0', 'replace', '1.0.0', '2.0.7')).toEqual(
+      '2.0.7'
     );
   });
-  it('bumps short tilde', () => {
-    expect(semver.getNewValue('~1.0', 'bump', '1.0.0', '1.1.7')).toEqual(
-      '~1.1'
+  it('replaces with version range', () => {
+    expect(semver.getNewValue('1.0.0', 'replace', '1.0.0', '^2.0.7')).toEqual(
+      '^2.0.7'
     );
-  });
-  it('bumps tilde to prerelease', () => {
-    expect(
-      semver.getNewValue('~1.0', 'bump', '1.0.0', '1.0.7-prerelease.1')
-    ).toEqual('~1.0.7-prerelease.1');
   });
   it('updates naked caret', () => {
     expect(semver.getNewValue('^1', 'bump', '1.0.0', '2.1.7')).toEqual('^2');
@@ -98,9 +179,29 @@ describe('semver.getNewValue()', () => {
     expect(semver.getNewValue('5.0', 'bump', '5.0.0', '5.1.7')).toEqual('5.1');
     expect(semver.getNewValue('5.0', 'bump', '5.0.0', '6.1.7')).toEqual('6.1');
   });
+  it('replaces minor', () => {
+    expect(semver.getNewValue('5.0', 'replace', '5.0.0', '6.1.7')).toEqual(
+      '6.1'
+    );
+  });
   it('replaces equals', () => {
     expect(semver.getNewValue('=1.0.0', 'replace', '1.0.0', '1.1.0')).toEqual(
       '=1.1.0'
+    );
+  });
+  it('bumps caret to prerelease', () => {
+    expect(
+      semver.getNewValue('^1', 'bump', '1.0.0', '1.0.7-prerelease.1')
+    ).toEqual('^1.0.7-prerelease.1');
+  });
+  it('replaces with newer', () => {
+    expect(semver.getNewValue('^1.0.0', 'replace', '1.0.0', '1.0.7')).toEqual(
+      '^1.0.7'
+    );
+  });
+  it('bumps short tilde', () => {
+    expect(semver.getNewValue('~1.0', 'bump', '1.0.0', '1.1.7')).toEqual(
+      '~1.1'
     );
   });
   it('handles long asterisk', () => {
@@ -117,5 +218,27 @@ describe('semver.getNewValue()', () => {
     expect(
       semver.getNewValue('~0.6.1', 'replace', '0.6.8', '0.7.0-rc.2')
     ).toEqual('~0.7.0-rc');
+  });
+  it('handles less than version requirements', () => {
+    expect(semver.getNewValue('<1.3.4', 'replace', '1.2.3', '1.5.0')).toEqual(
+      '<1.5.1'
+    );
+    expect(semver.getNewValue('< 1.3.4', 'replace', '1.2.3', '1.5.0')).toEqual(
+      '< 1.5.1'
+    );
+    expect(
+      semver.getNewValue('<   1.3.4', 'replace', '1.2.3', '1.5.0')
+    ).toEqual('< 1.5.1');
+  });
+  it('handles less than equals version requirements', () => {
+    expect(semver.getNewValue('<=1.3.4', 'replace', '1.2.3', '1.5.0')).toEqual(
+      '<=1.5.0'
+    );
+    expect(semver.getNewValue('<= 1.3.4', 'replace', '1.2.3', '1.5.0')).toEqual(
+      '<= 1.5.0'
+    );
+    expect(
+      semver.getNewValue('<=   1.3.4', 'replace', '1.2.3', '1.5.0')
+    ).toEqual('<= 1.5.0');
   });
 });
