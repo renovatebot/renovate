447103614178bc8da8f9ee9f959c63a3ceaa1657
Rhys Arkins <rhys@arkins.net>
2019-04-08 17:13:17 +0200

    fix tmpdir

--- bin/clean-cache.js
+++ bin/clean-cache.js
@@ -3,7 +3,7 @@ const os = require('os');
 const path = require('path');
 
 (async () => {
-  const tmpDir = process.env.RENOVATE_TMPDIR || process.env.TMPDIR;
+  const tmpDir = process.env.RENOVATE_TMPDIR || os.tmpdir();
   const baseDir = path.join(tmpDir, 'renovate');
   console.log('Removing ' + baseDir);
   await fs.remove(baseDir);
