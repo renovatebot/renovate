a0bf368d725a6545bf5dd9d8e49fcf6144702f66
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-04-11 16:24:38 +0300

    homebrew: Add tests for extractPackageFile

--- test/manager/homebrew/extract.spec.js
+++ test/manager/homebrew/extract.spec.js
@@ -1,9 +1,39 @@
+const fs = require('fs');
 const { extractPackageFile } = require('../../../lib/manager/homebrew/extract');
+
+const aalib = fs.readFileSync(
+  'test/manager/homebrew/_fixtures/aalib.rb',
+  'utf8'
+);
+const aap = fs.readFileSync('test/manager/homebrew/_fixtures/aap.rb', 'utf8');
+const acmetool = fs.readFileSync(
+  'test/manager/homebrew/_fixtures/acmetool.rb',
+  'utf8'
+);
+const aide = fs.readFileSync('test/manager/homebrew/_fixtures/aide.rb', 'utf8');
+const ibazel = fs.readFileSync(
+  'test/manager/homebrew/_fixtures/ibazel.rb',
+  'utf8'
+);
 
 describe('lib/manager/homebrew/extract', () => {
   describe('extractPackageFile()', () => {
-    it('returns null (STUB)', () => {
-      expect(extractPackageFile('')).toBeNull();
+    it('skips non github dependencies', () => {
+      expect(extractPackageFile(aalib)).toBeNull();
+    });
+    it('skips non github dependencies', () => {
+      expect(extractPackageFile(aap)).toBeNull();
+    });
+    it('skips github dependency with wrong format', () => {
+      expect(extractPackageFile(acmetool)).toBeNull();
+    });
+    it('extracts github dependency', () => {
+      expect(extractPackageFile(aide)).not.toBeNull();
+      expect(extractPackageFile(aide)).toMatchSnapshot();
+    });
+    it('extracts github dependency', () => {
+      expect(extractPackageFile(ibazel)).not.toBeNull();
+      expect(extractPackageFile(ibazel)).toMatchSnapshot();
     });
   });
 });
