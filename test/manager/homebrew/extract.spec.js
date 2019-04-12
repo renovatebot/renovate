c4520263a6c4a2feafb2aeb4ff09334ff8340e80
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-04-12 13:37:20 +0300

    homebrew: Add initial implementation of extractPackageFile
    * It extracts "archive" and "releases" github dependencies
    * Note: It doesn't extract sha256 field yet

--- test/manager/homebrew/extract.spec.js
+++ test/manager/homebrew/extract.spec.js
@@ -18,21 +18,35 @@ const ibazel = fs.readFileSync(
 
 describe('lib/manager/homebrew/extract', () => {
   describe('extractPackageFile()', () => {
-    it('skips non github dependencies', () => {
-      expect(extractPackageFile(aalib)).toBeNull();
+    it('skips sourceforge dependency', () => {
+      expect(extractPackageFile(aalib)).not.toBeNull();
+      expect(extractPackageFile(aalib).deps[0].skipReason).toBe(
+        'unsupported-url'
+      );
+      expect(extractPackageFile(aalib)).toMatchSnapshot();
     });
-    it('skips non github dependencies', () => {
-      expect(extractPackageFile(aap)).toBeNull();
+    it('skips sourceforge dependency', () => {
+      expect(extractPackageFile(aap)).not.toBeNull();
+      expect(extractPackageFile(aap).deps[0].skipReason).toBe(
+        'unsupported-url'
+      );
+      expect(extractPackageFile(aap)).toMatchSnapshot();
     });
     it('skips github dependency with wrong format', () => {
-      expect(extractPackageFile(acmetool)).toBeNull();
+      expect(extractPackageFile(acmetool)).not.toBeNull();
+      expect(extractPackageFile(acmetool).deps[0].skipReason).toBe(
+        'unsupported-url'
+      );
+      expect(extractPackageFile(acmetool)).toMatchSnapshot();
     });
-    it('extracts github dependency', () => {
+    it('extracts "releases" github dependency', () => {
       expect(extractPackageFile(aide)).not.toBeNull();
+      expect(extractPackageFile(aide).deps[0].skipReason).toBeUndefined();
       expect(extractPackageFile(aide)).toMatchSnapshot();
     });
-    it('extracts github dependency', () => {
+    it('extracts "archive" github dependency', () => {
       expect(extractPackageFile(ibazel)).not.toBeNull();
+      expect(extractPackageFile(ibazel).deps[0].skipReason).toBeUndefined();
       expect(extractPackageFile(ibazel)).toMatchSnapshot();
     });
   });
