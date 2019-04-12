612db4436dba8fd2bb54f7f65b98ea3728ff5cb8
Dmitry9 <karpov.dmitry80@gmail.com>
2019-04-12 12:52:44 +0300

    added tests

--- test/manager/bazel/extract.spec.js
+++ test/manager/bazel/extract.spec.js
@@ -33,6 +33,23 @@ describe('lib/manager/bazel/extract', () => {
       const res = extractPackageFile(fileWithBzlExtension, config);
       expect(res.deps).toMatchSnapshot();
     });
+
+    it('extracts dependencies for container_pull deptype', () => {
+      const res = extractPackageFile(
+        `
+        container_pull(
+          name="hasura",
+          registry="index.docker.io",
+          repository="hasura/graphql-engine",
+          # v1.0.0-alpha31.cli-migrations 11/28
+          digest="sha256:a4e8d8c444ca04fe706649e82263c9f4c2a4229bc30d2a64561b5e1d20cc8548",
+          tag="v1.0.0-alpha31.cli-migrations"
+        )`,
+        config
+      );
+      expect(res.deps).toMatchSnapshot();
+    });
+
     it('check remote option in go_repository', () => {
       const successStory = extractPackageFile(
         `
