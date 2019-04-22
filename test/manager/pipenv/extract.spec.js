f79cd8c297d2c92cb476c2f027c7d7e804a717b1
Rhys Arkins <rhys@arkins.net>
2019-04-22 08:56:30 +0200

    refactor(pipenv): write registryUrls to packageFile level

--- test/manager/pipenv/extract.spec.js
+++ test/manager/pipenv/extract.spec.js
@@ -31,10 +31,10 @@ describe('lib/manager/pipenv/extract', () => {
       expect(extractPackageFile('nothing here', config)).toBeNull();
     });
     it('extracts dependencies', () => {
-      const res = extractPackageFile(pipfile1, config).deps;
+      const res = extractPackageFile(pipfile1, config);
       expect(res).toMatchSnapshot();
-      expect(res).toHaveLength(6);
-      expect(res.filter(dep => !dep.skipReason)).toHaveLength(4);
+      expect(res.deps).toHaveLength(6);
+      expect(res.deps.filter(dep => !dep.skipReason)).toHaveLength(4);
     });
     it('marks packages with "extras" as skipReason === any-version', () => {
       const res = extractPackageFile(pipfile3, {
@@ -42,45 +42,45 @@ describe('lib/manager/pipenv/extract', () => {
         pipenv: { enabled: true },
         pip_setup: { enabled: true },
         labels: ['dependencies'],
-      }).deps;
-      expect(res.filter(r => !r.skipReason)).toHaveLength(0);
-      expect(res.filter(r => r.skipReason)).toHaveLength(6);
+      });
+      expect(res.deps.filter(r => !r.skipReason)).toHaveLength(0);
+      expect(res.deps.filter(r => r.skipReason)).toHaveLength(6);
     });
     it('extracts multiple dependencies', () => {
-      const res = extractPackageFile(pipfile2, config).deps;
+      const res = extractPackageFile(pipfile2, config);
       expect(res).toMatchSnapshot();
-      expect(res).toHaveLength(5);
+      expect(res.deps).toHaveLength(5);
     });
     it('ignores git dependencies', () => {
       const content =
         '[packages]\r\nflask = {git = "https://github.com/pallets/flask.git"}\r\nwerkzeug = ">=0.14"';
-      const res = extractPackageFile(content, config).deps;
-      expect(res.filter(r => !r.skipReason)).toHaveLength(1);
+      const res = extractPackageFile(content, config);
+      expect(res.deps.filter(r => !r.skipReason)).toHaveLength(1);
     });
     it('ignores invalid package names', () => {
       const content = '[packages]\r\nfoo = "==1.0.0"\r\n_invalid = "==1.0.0"';
-      const res = extractPackageFile(content, config).deps;
-      expect(res).toHaveLength(2);
-      expect(res.filter(dep => !dep.skipReason)).toHaveLength(1);
+      const res = extractPackageFile(content, config);
+      expect(res.deps).toHaveLength(2);
+      expect(res.deps.filter(dep => !dep.skipReason)).toHaveLength(1);
     });
     it('ignores relative path dependencies', () => {
       const content = '[packages]\r\nfoo = "==1.0.0"\r\ntest = {path = "."}';
-      const res = extractPackageFile(content, config).deps;
-      expect(res.filter(r => !r.skipReason)).toHaveLength(1);
+      const res = extractPackageFile(content, config);
+      expect(res.deps.filter(r => !r.skipReason)).toHaveLength(1);
     });
     it('ignores invalid versions', () => {
       const content = '[packages]\r\nfoo = "==1.0.0"\r\nsome-package = "==0 0"';
-      const res = extractPackageFile(content, config).deps;
-      expect(res).toHaveLength(2);
-      expect(res.filter(dep => !dep.skipReason)).toHaveLength(1);
+      const res = extractPackageFile(content, config);
+      expect(res.deps).toHaveLength(2);
+      expect(res.deps.filter(dep => !dep.skipReason)).toHaveLength(1);
     });
     it('extracts all sources', () => {
       const content =
         '[[source]]\r\nurl = "source-url"\r\n' +
         '[[source]]\r\nurl = "other-source-url"\r\n' +
         '[packages]\r\nfoo = "==1.0.0"\r\n';
-      const res = extractPackageFile(content, config).deps;
-      expect(res[0].registryUrls).toEqual(['source-url', 'other-source-url']);
+      const res = extractPackageFile(content, config);
+      expect(res.registryUrls).toEqual(['source-url', 'other-source-url']);
     });
     it('extracts example pipfile', () => {
       const res = extractPackageFile(pipfile4, config);
