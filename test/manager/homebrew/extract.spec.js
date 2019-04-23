41fb4a52d99497cb536a3b6bc61c7b583f7a6057
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-04-23 11:52:50 +0300

    homebrew: Add more tests for extractPackageFile

--- test/manager/homebrew/extract.spec.js
+++ test/manager/homebrew/extract.spec.js
@@ -49,5 +49,129 @@ describe('lib/manager/homebrew/extract', () => {
       expect(extractPackageFile(ibazel).deps[0].skipReason).toBeUndefined();
       expect(extractPackageFile(ibazel)).toMatchSnapshot();
     });
+    it('handles no space before class header', () => {
+      const content = `class Ibazel < Formula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.tar.gz"
+          sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
+          end
+      `;
+      expect(extractPackageFile(content)).not.toBeNull();
+      expect(extractPackageFile(content).deps[0].skipReason).toBeUndefined();
+      expect(extractPackageFile(content)).toMatchSnapshot();
+    });
+    it('returns null for invalid class header', () => {
+      const content = `
+          class Ibazel !?# Formula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.tar.gz"
+          sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
+          end
+      `;
+      expect(extractPackageFile(content)).toBeNull();
+    });
+    it('returns null for invalid class header', () => {
+      const content = `
+          class Ibazel < NotFormula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.tar.gz"
+          sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
+          end
+      `;
+      expect(extractPackageFile(content)).toBeNull();
+    });
+    it('skips if there is no url field', () => {
+      const content = `
+          class Ibazel < Formula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          not_url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.tar.gz"
+          sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
+          end
+      `;
+      expect(extractPackageFile(content)).not.toBeNull();
+      expect(extractPackageFile(content).deps[0].skipReason).toBe(
+        'unsupported-url'
+      );
+      expect(extractPackageFile(content)).toMatchSnapshot();
+    });
+    it('skips if invalid url field', () => {
+      const content = `
+          class Ibazel < Formula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.not_tar.not_gz"
+          sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
+          end
+      `;
+      expect(extractPackageFile(content)).not.toBeNull();
+      expect(extractPackageFile(content).deps[0].skipReason).toBe(
+        'unsupported-url'
+      );
+      expect(extractPackageFile(content)).toMatchSnapshot();
+    });
+    it('skips if invalid url field', () => {
+      const content = `
+          class Ibazel < Formula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          url "https://github.com/bazelbuild/bazel-watcher/archive/vInvalid.version.2.tar.gz"
+          sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
+          end
+      `;
+      expect(extractPackageFile(content)).not.toBeNull();
+      expect(extractPackageFile(content).deps[0].skipReason).toBe(
+        'unsupported-url'
+      );
+      expect(extractPackageFile(content)).toMatchSnapshot();
+    });
+    it('skips if invalid url field', () => {
+      const content = `
+          class Ibazel < Formula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          url ??https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.tar.gz"
+          sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
+          end
+      `;
+      expect(extractPackageFile(content)).not.toBeNull();
+      expect(extractPackageFile(content).deps[0].skipReason).toBe(
+        'unsupported-url'
+      );
+      expect(extractPackageFile(content)).toMatchSnapshot();
+    });
+    it('skips if there is no sha256 field', () => {
+      const content = `
+          class Ibazel < Formula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.tar.gz"
+          not_sha256 '26f5125218fad2741d3caf937b02296d803900e5f153f5b1f733f15391b9f9b4'
+          end
+      `;
+      expect(extractPackageFile(content)).not.toBeNull();
+      expect(extractPackageFile(content).deps[0].skipReason).toBe(
+        'invalid-sha256'
+      );
+      expect(extractPackageFile(content)).toMatchSnapshot();
+    });
+    it('skips if sha256 field is invalid', () => {
+      const content = `
+          class Ibazel < Formula
+          desc 'IBazel is a tool for building Bazel targets when source files change.'
+          homepage 'https://github.com/bazelbuild/bazel-watcher'
+          url "https://github.com/bazelbuild/bazel-watcher/archive/v0.8.2.tar.gz"
+          sha256 '26f5125218fad2741d3caf937b0229'
+          end
+      `;
+      expect(extractPackageFile(content)).not.toBeNull();
+      expect(extractPackageFile(content).deps[0].skipReason).toBe(
+        'invalid-sha256'
+      );
+      expect(extractPackageFile(content)).toMatchSnapshot();
+    });
   });
 });
