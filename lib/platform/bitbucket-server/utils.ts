4c1d8f1625811ef6eb05626502009884d5f5a0f6
Michael Kriese <michael.kriese@visualon.de>
2019-05-19 20:30:58 +0200

    feat: convert utils

--- lib/platform/bitbucket-server/utils.ts
+++ lib/platform/bitbucket-server/utils.ts
@@ -1,15 +1,15 @@
 // SEE for the reference https://github.com/renovatebot/renovate/blob/c3e9e572b225085448d94aa121c7ec81c14d3955/lib/platform/bitbucket/utils.js
-const url = require('url');
-const api = require('./bb-got-wrapper');
+import url from 'url';
+import api from './bb-got-wrapper';
 
 // https://docs.atlassian.com/bitbucket-server/rest/6.0.0/bitbucket-rest.html#idp250
-const prStateMapping = {
+const prStateMapping: any = {
   MERGED: 'merged',
   DECLINED: 'closed',
   OPEN: 'open',
 };
 
-const prInfo = pr => ({
+const prInfo = (pr: any) => ({
   version: pr.version,
   number: pr.id,
   body: pr.description,
@@ -20,7 +20,7 @@ const prInfo = pr => ({
   canRebase: false,
 });
 
-const addMaxLength = (inputUrl, limit = 100) => {
+const addMaxLength = (inputUrl: string, limit = 100) => {
   const { search, ...parsedUrl } = url.parse(inputUrl, true); // eslint-disable-line @typescript-eslint/no-unused-vars
   const maxedUrl = url.format({
     ...parsedUrl,
@@ -29,13 +29,19 @@ const addMaxLength = (inputUrl, limit = 100) => {
   return maxedUrl;
 };
 
-const accumulateValues = async (reqUrl, method = 'get', options, limit) => {
-  let accumulator = [];
+const accumulateValues = async (
+  reqUrl: string,
+  method = 'get',
+  options: any,
+  limit?: number
+) => {
+  let accumulator: any = [];
   let nextUrl = addMaxLength(reqUrl, limit);
   const lowerCaseMethod = method.toLocaleLowerCase();
 
   while (typeof nextUrl !== 'undefined') {
-    const { body } = await api[lowerCaseMethod](nextUrl, options);
+    // TODO: fix typing
+    const { body } = await (api as any)[lowerCaseMethod](nextUrl, options);
     accumulator = [...accumulator, ...body.values];
     if (body.isLastPage !== false) break;
 
