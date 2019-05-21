314889031ee9270b08ef0b5051b9cf896772d012
Michael Kriese <michael.kriese@visualon.de>
2019-05-21 10:34:28 +0200

    feat(bitbucket): convert to typescript (#3767)

--- test/platform/bitbucket/bb-got-wrapper.spec.ts
+++ test/platform/bitbucket/bb-got-wrapper.spec.ts
@@ -1,14 +1,16 @@
+import { IGotApi } from '../../../lib/platform/common';
+
 describe('platform/gl-got-wrapper', () => {
-  let get;
-  let got;
-  let hostRules;
+  let api: IGotApi;
+  let got: jest.Mock<typeof import('got')>;
+  let hostRules: typeof import('../../../lib/util/host-rules');
   beforeEach(() => {
     // reset module
     jest.resetAllMocks();
     jest.mock('got');
     got = require('got');
     hostRules = require('../../../lib/util/host-rules');
-    get = require('../../../lib/platform/bitbucket/bb-got-wrapper');
+    api = require('../../../lib/platform/bitbucket/bb-got-wrapper').api;
 
     // clean up hostRules
     hostRules.clear();
@@ -20,19 +22,22 @@ describe('platform/gl-got-wrapper', () => {
   });
   it('posts', async () => {
     const body = ['a', 'b'];
-    got.mockImplementationOnce(() => ({
-      body,
-    }));
-    const res = await get.post('some-url');
+    got.mockImplementationOnce(
+      () =>
+        ({
+          body,
+        } as any)
+    );
+    const res = await api.post('some-url');
     expect(res.body).toEqual(body);
   });
   it('returns cached', async () => {
-    get.reset();
+    api.reset();
     got.mockReturnValueOnce({
       body: {},
-    });
-    const res1 = await get('projects/foo');
-    const res2 = await get('projects/foo');
+    } as any);
+    const res1 = await api.get('projects/foo');
+    const res2 = await api.get('projects/foo');
     expect(res1).toEqual(res2);
   });
 });
