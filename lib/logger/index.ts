72166b81fe80dc06baa39a8c77b5c4881ebe7762
Jamie Magee <jamie.magee@gmail.com>
2019-05-22 22:13:27 +0200

    fix: add setMeta method back
    I'm not super happy with how I ended up doing this, but it appears to be the recommended way. See https://github.com/DefinitelyTyped/DefinitelyTyped/pull/21080

--- lib/logger/index.ts
+++ lib/logger/index.ts
@@ -1,9 +1,12 @@
 import * as Logger from 'bunyan';
+
+const is = require('@sindresorhus/is');
 const PrettyStdout = require('./pretty-stdout').RenovateStream;
 const configSerializer = require('./config-serializer');
 const errSerializer = require('./err-serializer');
+const cmdSerializer = require('./cmd-serializer');
 
-let logger: Logger;
+let meta = {};
 
 const stdout: Logger.Stream = {
   name: 'stdout',
@@ -14,18 +17,48 @@ const stdout: Logger.Stream = {
 if (process.env.LOG_FORMAT !== 'json') {
   const prettyStdOut = new PrettyStdout();
   prettyStdOut.pipe(process.stdout);
-  stdout.type = 'raw';
   stdout.stream = prettyStdOut;
 }
 
-logger = Logger.createLogger({
+const bunyanLogger: any = Logger.createLogger({
   name: 'renovate',
   serializers: {
     body: configSerializer,
+    cmd: cmdSerializer,
     config: configSerializer,
     err: errSerializer,
   },
   streams: [stdout],
 });
 
-export { logger };
+const logFactory = (level: string): any => {
+  return (p1: any, p2: any): Logger => {
+    (global as any).renovateError =
+      (global as any).renovateError || level === 'error' || level === 'fatal';
+    if (p2) {
+      // meta and msg provided
+      return bunyanLogger[level]({ ...meta, ...p1 }, p2);
+    }
+    if (is.string(p1)) {
+      // only message provided
+      return bunyanLogger[level](meta, p1);
+    }
+    // only meta provided
+    return bunyanLogger[level]({ ...meta, ...p1 });
+  };
+};
+
+export const logger = {
+  trace: logFactory('trace'),
+  debug: logFactory('debug'),
+  info: logFactory('info'),
+  warn: logFactory('warn'),
+  error: logFactory('error'),
+  fatal: logFactory('fatal'),
+  child: logFactory('child'),
+};
+
+// setMeta overrides existing meta
+export function setMeta(obj: any) {
+  meta = { ...obj };
+}
