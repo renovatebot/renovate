9ffbce264fcf7ace34ea314e4d72c6ff9239cac2
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-05-24 11:47:34 +0300

    helm: Add initial implementation of extractPackage file
    * Change fileMatch in definitions.js from Chart.ya?ml to requirements.ya?ml

--- test/manager/helm/extract.spec.js
+++ test/manager/helm/extract.spec.js
@@ -3,8 +3,17 @@ const { extractPackageFile } = require('../../../lib/manager/helm/extract');
 describe('lib/manager/helm/extract', () => {
   describe('extractPackageFile()', () => {
     it('returns empty array', () => {
-      const content = 'some_content';
-      expect(extractPackageFile(content)).toBe([]);
+      const content = `
+      dependencies:
+        - name: redis
+          version: 0.9.0
+          repository: https://kubernetes-charts.storage.googleapis.com/
+        - name: postgresql
+          version: 0.8.1
+          repository: https://kubernetes-charts.storage.googleapis.com/
+      `;
+      expect(extractPackageFile(content)).not.toBeNull();
+      expect(extractPackageFile(content)).toMatchSnapshot();
     });
   });
 });
