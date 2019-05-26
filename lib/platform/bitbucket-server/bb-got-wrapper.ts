584539d422ed39c92991fc410f75dca9d28cce15
Rhys Arkins <rhys@arkins.net>
2019-05-26 20:24:17 +0200

    restore url

--- lib/platform/bitbucket-server/bb-got-wrapper.ts
+++ lib/platform/bitbucket-server/bb-got-wrapper.ts
@@ -1,3 +1,4 @@
+import URL from 'url';
 import { GotJSONOptions } from 'got';
 import got from '../../util/got';
 import { IGotApi, IGotApiOptions } from '../common';
@@ -5,17 +6,17 @@ import { IGotApi, IGotApiOptions } from '../common';
 let baseUrl: string;
 
 function get(path: string, options: IGotApiOptions & GotJSONOptions) {
+  const url = URL.resolve(baseUrl, path);
   const opts: IGotApiOptions & GotJSONOptions = {
     ...options,
     hostType: 'bitbucket-server',
-    baseUrl,
     json: true,
   };
   opts.headers = {
     ...opts.headers,
     'X-Atlassian-Token': 'no-check',
   };
-  return got(path, opts);
+  return got(url, opts);
 }
 
 const helpers = ['get', 'post', 'put', 'patch', 'head', 'delete'];
