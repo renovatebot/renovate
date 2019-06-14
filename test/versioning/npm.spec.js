bda1d15456829d14dd49e58904eb68e4f5d3eaf5
Sergio Zharinov <sergio.zharinov@gmail.com>
2019-06-14 19:13:20 +0400

    feat(npm): Fix coverage

--- test/versioning/npm.spec.js
+++ test/versioning/npm.spec.js
@@ -195,6 +195,7 @@ describe('semver.getNewValue()', () => {
         '6.0.0',
         '>1.0.0 <2.0.0 || >3.0.0 <=6.0.0',
       ],
+      ['>1.2.3', '1.2.3', '1.2.3', '1.2.3'],
     ].forEach(([currentValue, fromVersion, toVersion, result]) => {
       const newValue = semver.getNewValue(
         currentValue,
