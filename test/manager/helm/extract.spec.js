beb669de3332b4c5e0185c886d69f52721d6f749
Nikita Chashchinskii <nikita.chashchinskii@gmail.com>
2019-06-19 12:16:47 +0300

    helm: Skip local dependencies in extractPackageFile
    * Don't add chartName field if chartName is undefined

--- test/manager/helm/extract.spec.js
+++ test/manager/helm/extract.spec.js
@@ -27,6 +27,48 @@ describe('lib/manager/helm/extract', () => {
       expect(result).not.toBeNull();
       expect(result).toMatchSnapshot();
     });
+    it("doesn't fail if Chart.yaml is invalid", async () => {
+      platform.getFile.mockReturnValueOnce(`
+      Invalid Chart.yaml content.
+      arr:
+      [
+      `);
+      const content = `
+      dependencies:
+        - name: redis
+          version: 0.9.0
+          repository: https://kubernetes-charts.storage.googleapis.com/
+        - name: postgresql
+          version: 0.8.1
+          repository: https://kubernetes-charts.storage.googleapis.com/
+      `;
+      const fileName = 'requirements.yaml';
+      const result = await extractPackageFile(content, fileName);
+      expect(result).not.toBeNull();
+      expect(result).toMatchSnapshot();
+    });
+    it('skips local dependencies', async () => {
+      platform.getFile.mockReturnValueOnce(`
+      apiVersion: v1
+      appVersion: "1.0"
+      description: A Helm chart for Kubernetes
+      name: example
+      version: 0.1.0
+      `);
+      const content = `
+      dependencies:
+        - name: redis
+          version: 0.9.0
+          repository: https://kubernetes-charts.storage.googleapis.com/
+        - name: postgresql
+          version: 0.8.1
+          repository: file:///some/local/path/
+      `;
+      const fileName = 'requirements.yaml';
+      const result = await extractPackageFile(content, fileName);
+      expect(result).not.toBeNull();
+      expect(result).toMatchSnapshot();
+    });
     it('returns null if requirements.yaml is invalid', async () => {
       const content = `
       Invalid requirements.yaml content.
