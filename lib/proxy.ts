52657daa41d5214829cb232f96347157fe3838f9
Florian Greinacher <florian@greinacher.de>
2019-06-25 23:53:03 +0200

    Proxy logic back to JS

--- lib/proxy.ts
+++
@@ -1,8 +0,0 @@
-import { createGlobalProxyAgent } from 'global-agent';
-
-// eslint-disable-next-line import/prefer-default-export
-export function bootstrap() {
-  return createGlobalProxyAgent({
-    environmentVariableNamespace: '',
-  });
-}
