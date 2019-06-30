49b8e13874a3114917f7f080622ed7bc2b6c823e
Rhys Arkins <rhys@arkins.net>
2019-06-30 07:18:37 +0300

    fix(bazel): extract urls for new hashes (#3980)

--- test/manager/bazel/update.spec.js
+++ test/manager/bazel/update.spec.js
@@ -97,23 +97,17 @@ describe('manager/bazel/update', () => {
         res.includes('"aaa09d789f3dba190787f8b4454c7d3c936fe123",  # v1.0.3')
       ).toBe(true);
     });
-    it('updates WORKSPACE http archive', async () => {
+    it('updates commit-based http archive', async () => {
       const upgrade = {
-        depName: 'io_bazel_rules_go',
+        depName: 'distroless',
         depType: 'http_archive',
-        repo: 'bazelbuild/rules_go',
-        def: `http_archive(\n    name = "io_bazel_rules_go",\n    url = "https://github.com/bazelbuild/rules_go/releases/download/0.7.1/rules_go-0.7.1.tar.gz",\n    sha256 = "341d5eacef704415386974bc82a1783a8b7ffbff2ab6ba02375e1ca20d9b031c",\n)`,
-        currentValue: '0.7.1',
-        newValue: '0.8.1',
+        repo: 'GoogleContainerTools/distroless',
+        def: `http_archive(\n  name="distroless",\n  sha256="f7a6ecfb8174a1dd4713ea3b21621072996ada7e8f1a69e6ae7581be137c6dd6",\n  strip_prefix="distroless-446923c3756ceeaa75888f52fcbdd48bb314fbf8",\n  urls=["https://github.com/GoogleContainerTools/distroless/archive/446923c3756ceeaa75888f52fcbdd48bb314fbf8.tar.gz"]\n)`,
+        newDigest: '033387ac8853e6cc1cd47df6c346bc53cbc490d8',
       };
-      hasha.fromStream.mockImplementationOnce(() => {
-        throw new Error('not found');
-      });
-      hasha.fromStream.mockImplementationOnce(() => 'abc123');
+      hasha.fromStream.mockReturnValueOnce('abc123');
       const res = await bazelfile.updateDependency(content, upgrade);
       expect(res).not.toEqual(content);
-      expect(res.includes('abc123')).toBe(true);
-      expect(res.indexOf('0.8.1')).not.toBe(-1);
     });
     it('updates http archive with content other then WORKSPACE', async () => {
       const upgrade = {
@@ -137,36 +131,47 @@ describe('manager/bazel/update', () => {
       expect(res).not.toEqual(fileWithBzlExtension);
       expect(res.indexOf('0.8.0')).not.toBe(-1);
     });
-    it('updates commit-based http archive', async () => {
+    it('returns null if no urls resolve hashes', async () => {
       const upgrade = {
-        depName: 'distroless',
+        depName: 'bazel_skylib',
         depType: 'http_archive',
-        repo: 'GoogleContainerTools/distroless',
-        def: `http_archive(\n  name="distroless",\n  sha256="f7a6ecfb8174a1dd4713ea3b21621072996ada7e8f1a69e6ae7581be137c6dd6",\n  strip_prefix="distroless-446923c3756ceeaa75888f52fcbdd48bb314fbf8",\n  urls=["https://github.com/GoogleContainerTools/distroless/archive/446923c3756ceeaa75888f52fcbdd48bb314fbf8.tar.gz"]\n)`,
-        newDigest: '033387ac8853e6cc1cd47df6c346bc53cbc490d8',
+        repo: 'bazelbuild/bazel-skyfoo',
+        def: `http_archive(
+            name = "bazel_skyfoo",
+            sha256 = "eb5c57e4c12e68c0c20bc774bfbc60a568e800d025557bc4ea022c6479acc867",
+            strip_prefix = "bazel-skyfoo-0.6.0",
+            urls = ["https://github.com/bazelbuild/bazel-skyfoo/archive/0.6.0.tar.gz"],
+          )`,
+        currentValue: '0.6.0',
+        newValue: '0.8.0',
+      };
+      const res = await bazelfile.updateDependency(
+        fileWithBzlExtension,
+        upgrade
+      );
+      expect(res).toBeNull();
+    });
+    it('errors for http_archive without urls', async () => {
+      const upgrade = {
+        depName: 'bazel_skylib',
+        depType: 'http_archive',
+        repo: 'bazelbuild/bazel-skylib',
+        def:
+          `
+http_archive(
+  name = "bazel_skylib",
+  sha256 = "b5f6abe419da897b7901f90cbab08af958b97a8f3575b0d3dd062ac7ce78541f",
+  strip_prefix = "bazel-skylib-0.5.0",
+)
+        `.trim() + '\n',
+        currentValue: '0.5.0',
+        newValue: '0.6.2',
       };
       hasha.fromStream.mockReturnValueOnce('abc123');
       const res = await bazelfile.updateDependency(content, upgrade);
-      expect(res).not.toEqual(content);
+      expect(res).toBeNull();
     });
-    it('updates second time http archive', async () => {
-      const upgrade = {
-        depName: 'io_bazel_rules_go',
-        depType: 'http_archive',
-        repo: 'bazelbuild/rules_go',
-        def: `http_archive(\n    name = "io_bazel_rules_go",\n    url = "https://github.com/bazelbuild/rules_go/releases/download/0.7.1/rules_go-0.7.1.tar.gz",\n    sha256 = "341d5eacef704415386974bc82a1783a8b7ffbff2ab6ba02375e1ca20d9b031c",\n)`,
-        currentValue: '0.7.1',
-        newValue: '0.8.1',
-      };
-      hasha.fromStream.mockImplementationOnce(() => {
-        throw new Error('not found');
-      });
-      hasha.fromStream.mockImplementationOnce(() => 'abc123');
-      const res = await bazelfile.updateDependency(content, upgrade);
-      expect(res).not.toEqual(content);
-      expect(res.indexOf('0.8.1')).not.toBe(-1);
-    });
-    it('updates http urls array', async () => {
+    it('updates http_archive with urls array', async () => {
       const upgrade = {
         depName: 'bazel_skylib',
         depType: 'http_archive',
