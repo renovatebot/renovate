e3c46bb9a0ff8200ba060a36999e1962208903d0
Rhys Arkins <rhys@arkins.net>
2019-07-10 13:18:51 +0200

    feat: gitlab optimization with caching fix (#4033)
    Restores GitLab optimization but with caching and pagination fixes.
    
    Co-authored-by: Lukas Eipert <leipert@users.noreply.github.com>

--- lib/platform/gitlab/index.ts
+++ lib/platform/gitlab/index.ts
@@ -1,4 +1,4 @@
-import URL from 'url';
+import URL, { URLSearchParams } from 'url';
 import is from '@sindresorhus/is';
 
 import { api } from './gl-got-wrapper';
@@ -22,6 +22,8 @@ const defaults = {
   endpoint: 'https://gitlab.com/api/v4/',
 };
 
+let authorId: number;
+
 export async function initPlatform({
   endpoint,
   token,
@@ -42,7 +44,9 @@ export async function initPlatform({
     logger.info('Using default GitLab endpoint: ' + res.endpoint);
   }
   try {
-    res.gitAuthor = (await api.get(`user`, { token })).body.email;
+    const user = (await api.get(`user`, { token })).body;
+    res.gitAuthor = user.email;
+    authorId = user.id;
   } catch (err) {
     logger.info(
       { err },
@@ -203,7 +207,12 @@ export async function getBranchPr(branchName: string) {
   if (!(await branchExists(branchName))) {
     return null;
   }
-  const urlString = `projects/${config.repository}/merge_requests?state=opened&per_page=100`;
+  const query = new URLSearchParams({
+    per_page: '100',
+    state: 'opened',
+    source_branch: branchName,
+  }).toString();
+  const urlString = `projects/${config.repository}/merge_requests?${query}`;
   const res = await api.get(urlString, { paginate: true });
   logger.debug(`Got res with ${res.body.length} results`);
   let pr: any = null;
@@ -590,25 +599,33 @@ export async function ensureCommentRemoval(issueNo: number, topic: string) {
   }
 }
 
+const mapPullRequests = (pr: {
+  iid: number;
+  source_branch: string;
+  title: string;
+  state: string;
+  created_at: string;
+}) => ({
+  number: pr.iid,
+  branchName: pr.source_branch,
+  title: pr.title,
+  state: pr.state === 'opened' ? 'open' : pr.state,
+  createdAt: pr.created_at,
+});
+
+async function fetchPrList() {
+  const query = new URLSearchParams({
+    per_page: '100',
+    author_id: `${authorId}`,
+  }).toString();
+  const urlString = `projects/${config.repository}/merge_requests?${query}`;
+  const res = await api.get(urlString, { paginate: true });
+  return res.body.map(mapPullRequests);
+}
+
 export async function getPrList() {
   if (!config.prList) {
-    const urlString = `projects/${config.repository}/merge_requests?per_page=100`;
-    const res = await api.get(urlString, { paginate: true });
-    config.prList = res.body.map(
-      (pr: {
-        iid: number;
-        source_branch: string;
-        title: string;
-        state: string;
-        created_at: string;
-      }) => ({
-        number: pr.iid,
-        branchName: pr.source_branch,
-        title: pr.title,
-        state: pr.state === 'opened' ? 'open' : pr.state,
-        createdAt: pr.created_at,
-      })
-    );
+    config.prList = await fetchPrList();
   }
   return config.prList;
 }
