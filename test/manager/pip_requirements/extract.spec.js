c84b325cbec852bb206e606ba9d20e56c9ef7399
RmStorm <roaldstorm@gmail.com>
2019-07-12 13:31:03 +0200

    Correctly default to pypi or other url when --index-url is not supplied

--- test/manager/pip_requirements/extract.spec.js
+++ test/manager/pip_requirements/extract.spec.js
@@ -26,33 +26,40 @@ const requirements5 = fs.readFileSync(
   'utf8'
 );
 
+const requirements6 = fs.readFileSync(
+  'test/manager/pip_requirements/_fixtures/requirements6.txt',
+  'utf8'
+);
+
 describe('lib/manager/pip_requirements/extract', () => {
   describe('extractPackageFile()', () => {
     let config;
     beforeEach(() => {
-      config = {};
+      config = { registryUrls: ['AnExistingDefaultUrl'] };
     });
     it('returns null for empty', () => {
       expect(extractPackageFile('nothing here', config)).toBeNull();
     });
     it('extracts dependencies', () => {
-      const res = extractPackageFile(requirements1, config);
+      const res = extractPackageFile(requirements1, 'unused_file_name', config);
       expect(res).toMatchSnapshot();
       expect(res.registryUrls).toEqual(['http://example.com/private-pypi/']);
       expect(res.deps).toHaveLength(3);
     });
     it('extracts multiple dependencies', () => {
-      const res = extractPackageFile(requirements2, config).deps;
+      const res = extractPackageFile(requirements2, 'unused_file_name', config)
+        .deps;
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(5);
     });
     it('handles comments and commands', () => {
-      const res = extractPackageFile(requirements3, config).deps;
+      const res = extractPackageFile(requirements3, 'unused_file_name', config)
+        .deps;
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(5);
     });
     it('handles extras and complex index url', () => {
-      const res = extractPackageFile(requirements4, config);
+      const res = extractPackageFile(requirements4, 'unused_file_name', config);
       expect(res).toMatchSnapshot();
       expect(res.registryUrls).toEqual([
         'https://artifactory.company.com/artifactory/api/pypi/python/simple',
@@ -60,11 +67,17 @@ describe('lib/manager/pip_requirements/extract', () => {
       expect(res.deps).toHaveLength(3);
     });
     it('handles extra index url', () => {
-      const res = extractPackageFile(requirements5, config);
+      const res = extractPackageFile(requirements5, 'unused_file_name', config);
+      expect(res).toMatchSnapshot();
+      expect(res.registryUrls).toEqual(['http://example.com/private-pypi/']);
+      expect(res.deps).toHaveLength(6);
+    });
+    it('handles extra index url and defaults without index', () => {
+      const res = extractPackageFile(requirements6, 'unused_file_name', config);
       expect(res).toMatchSnapshot();
       expect(res.registryUrls).toEqual([
-        'https://artifactory.company.com/artifactory/api/pypi/python/simple',
         'http://example.com/private-pypi/',
+        'AnExistingDefaultUrl',
       ]);
       expect(res.deps).toHaveLength(6);
     });
