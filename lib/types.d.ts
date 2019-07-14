b397c5a6409b3708621ca14f9095adf3ca7dc32b
Rhys Arkins <rhys@arkins.net>
2019-07-14 22:30:31 +0200

    add back renovateError

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -9,7 +9,7 @@ declare namespace NodeJS {
   interface Global {
     appMode?: boolean;
     gitAuthor?: { name: string; email: string };
-
+    renovateError?: boolean;
     renovateVersion: string;
     // TODO: declare interface for all platforms
     platform: typeof import('./platform/github');
