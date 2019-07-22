e5da665e8391e21aad3489b1fd2d0face449f692
Michael Kriese <michael.kriese@visualon.de>
2019-07-22 06:56:08 +0200

    feat(versioning): convert entrypoint

--- lib/versioning/index.ts
+++ lib/versioning/index.ts
@@ -1,12 +1,14 @@
 import { logger } from '../logger';
 import { getOptions } from '../config/definitions';
+import { VersioningApi } from './common';
+
+export * from './common';
 
 const supportedSchemes = getOptions().find(
   option => option.name === 'versionScheme'
 ).allowedValues;
 
-/** @type Record<string, import('./common').VersioningApi> */
-const schemes = {};
+const schemes: Record<string, VersioningApi> = {};
 
 for (const scheme of supportedSchemes) {
   schemes[scheme] = require('./' + scheme).api; // eslint-disable-line
@@ -14,7 +16,7 @@ for (const scheme of supportedSchemes) {
 
 export { get };
 
-function get(versionScheme) {
+function get(versionScheme: string) {
   if (!versionScheme) {
     logger.debug('Missing versionScheme');
     return schemes.semver;
