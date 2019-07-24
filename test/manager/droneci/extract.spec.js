34c47e07c863032bb4b1ded56bdbd8181ee71d1a
Nihal <nihal9ns@gmail.com>
2019-07-24 09:23:51 +0530

    feat: added tests for droneci, updated renovate-schema

---
+++ test/manager/droneci/extract.spec.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const { extractPackageFile } = require('../../../lib/manager/droneci/extract');
+
+const droneYAML = fs.readFileSync(
+  'test/manager/droneci/_fixtures/.drone.yml',
+  'utf8'
+);
+
+describe('lib/manager/droneci/extract', () => {
+  describe('extractPackageFile()', () => {
+    it('returns null for empty', () => {
+      expect(extractPackageFile('nothing here')).toBeNull();
+    });
+
+    it('extracts multiple image lines', () => {
+      const res = extractPackageFile(droneYAML);
+      expect(res.deps).toMatchSnapshot();
+      expect(res.deps).toHaveLength(3);
+    });
+  });
+});
