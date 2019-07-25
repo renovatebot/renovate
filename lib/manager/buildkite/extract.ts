4b6a25e53df52a2de381cd7eca56bd5acf37e775
Sergio Zharinov <sergio.zharinov@gmail.com>
2019-07-25 18:29:29 +0400

    fix(buildkite): Destructure values from named regexes

--- lib/manager/buildkite/extract.ts
+++ lib/manager/buildkite/extract.ts
@@ -8,61 +8,67 @@ function extractPackageFile(content: string): PackageFile {
   const deps: PackageDependency[] = [];
   try {
     const lines = content.split('\n');
-    let pluginsSection = null;
+    let isPluginsSection = false;
     let pluginsIndent = '';
     for (let lineNumber = 1; lineNumber <= lines.length; lineNumber += 1) {
       const lineIdx = lineNumber - 1;
       const line = lines[lineIdx];
-      const plugins = line.match(/^(\s*-?\s*)plugins:/);
-      if (plugins) {
+      const pluginsSection = line.match(
+        /^(?<pluginsIndent>\s*)(-?\s*)plugins:/
+      );
+      if (pluginsSection) {
         logger.trace(`Matched plugins on line ${lineNumber}`);
-        [pluginsSection, pluginsIndent] = plugins;
-      } else if (pluginsSection) {
+        isPluginsSection = true;
+        pluginsIndent = pluginsSection.groups.pluginsIndent;
+      } else if (isPluginsSection) {
         logger.debug(`serviceImageLine: "${line}"`);
-        const depLineMatch = line.match(/^\s+(?:-\s+)?([^#]+)#([^:]+):/);
-        const [, currentIndent] = line.match(/^(\s*)/);
-        if (currentIndent.length <= pluginsIndent.length) {
-          pluginsSection = null;
-          pluginsIndent = '';
-        } else if (depLineMatch) {
-          logger.trace('depLineMatch');
-          const [, depName, currentValue] = depLineMatch;
-          let skipReason: string;
-          let repo: string;
-          if (depName.startsWith('https://') || depName.startsWith('git@')) {
-            logger.debug({ dependency: depName }, 'Skipping git plugin');
-            skipReason = 'git-plugin';
-          } else if (!isVersion(currentValue)) {
-            logger.debug(
-              { currentValue },
-              'Skipping non-pinned current version'
-            );
-            skipReason = 'invalid-version';
+        const depLineMatch = line.match(
+          /^(?<currentIndent>\s+)(?:-\s+)?(?<depName>[^#]+)#(?<currentValue>[^:]+):/
+        );
+        if (depLineMatch) {
+          const { currentIndent, depName, currentValue } = depLineMatch.groups;
+          if (currentIndent.length <= pluginsIndent.length) {
+            isPluginsSection = false;
+            pluginsIndent = '';
           } else {
-            const splitName = depName.split('/');
-            if (splitName.length === 1) {
-              repo = `buildkite-plugins/${depName}-buildkite-plugin`;
-            } else if (splitName.length === 2) {
-              repo = `${depName}-buildkite-plugin`;
-            } else {
-              logger.warn(
-                { dependency: depName },
-                'Something is wrong with buildkite plugin name'
+            logger.trace('depLineMatch');
+            let skipReason: string;
+            let repo: string;
+            if (depName.startsWith('https://') || depName.startsWith('git@')) {
+              logger.debug({ dependency: depName }, 'Skipping git plugin');
+              skipReason = 'git-plugin';
+            } else if (!isVersion(currentValue)) {
+              logger.debug(
+                { currentValue },
+                'Skipping non-pinned current version'
               );
-              skipReason = 'unknown';
+              skipReason = 'invalid-version';
+            } else {
+              const splitName = depName.split('/');
+              if (splitName.length === 1) {
+                repo = `buildkite-plugins/${depName}-buildkite-plugin`;
+              } else if (splitName.length === 2) {
+                repo = `${depName}-buildkite-plugin`;
+              } else {
+                logger.warn(
+                  { dependency: depName },
+                  'Something is wrong with buildkite plugin name'
+                );
+                skipReason = 'unknown';
+              }
             }
+            const dep: PackageDependency = {
+              managerData: { lineNumber },
+              depName,
+              currentValue,
+              skipReason,
+            };
+            if (repo) {
+              dep.datasource = 'github';
+              dep.lookupName = repo;
+            }
+            deps.push(dep);
           }
-          const dep: PackageDependency = {
-            managerData: { lineNumber },
-            depName,
-            currentValue,
-            skipReason,
-          };
-          if (repo) {
-            dep.datasource = 'github';
-            dep.lookupName = repo;
-          }
-          deps.push(dep);
         }
       }
     }
