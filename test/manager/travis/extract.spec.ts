60e917e48fdab84079081b4d87f74e48c93c13e6
Nihal <nihal9ns@gmail.com>
2019-07-26 12:53:18 +0530

    fix: added test to check the catch

--- test/manager/travis/extract.spec.ts
+++ test/manager/travis/extract.spec.ts
@@ -1,4 +1,11 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { extractPackageFile } from '../../../lib/manager/travis/extract';
+
+const invalidYAML = readFileSync(
+  resolve('test/manager/travis/_fixtures/invalid.yml'),
+  'utf8'
+);
 
 describe('lib/manager/travis/extract', () => {
   describe('extractPackageFile()', () => {
@@ -10,6 +17,10 @@ describe('lib/manager/travis/extract', () => {
       const res = extractPackageFile('node_js:\n  - 6\n  - 8\n');
       expect(res).toMatchSnapshot();
       expect(res.deps).toHaveLength(1);
+    });
+    it('should handle invalid YAML', () => {
+      const res = extractPackageFile(invalidYAML);
+      expect(res).toBeNull();
     });
   });
 });
