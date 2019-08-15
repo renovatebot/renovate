01918957b01bbaaca708118f48ff6e6ce35e7fda
Michael Kriese <michael.kriese@visualon.de>
2019-08-15 06:19:30 +0200

    feat(versioning): add class based versioning schemes

--- lib/versioning/loose/generic.ts
+++ lib/versioning/loose/generic.ts
@@ -5,7 +5,7 @@ export interface GenericVersion {
   suffix?: string;
 }
 export interface VersionParser {
-  (version: string);
+  (version: string): GenericVersion;
 }
 
 export interface VersionComparator {
@@ -118,3 +118,88 @@ export const comparer = (
     sortVersions,
   };
 };
+
+export abstract class GenericVersioningApi<
+  T extends GenericVersion = GenericVersion
+> implements VersioningApi {
+  private _getSection(version: string, index: number) {
+    const parsed = this._parse(version);
+    return parsed && parsed.release.length > index
+      ? parsed.release[index]
+      : null;
+  }
+
+  protected abstract _compare(version: string, other: string): number;
+
+  protected abstract _parse(version: string): T | null;
+
+  isValid(version: string): boolean {
+    return this._parse(version) !== null;
+  }
+
+  isCompatible(version: string, _range: string): boolean {
+    return this.isValid(version);
+  }
+
+  isStable(version: string): boolean {
+    return this.isValid(version);
+  }
+
+  isSingleVersion(version: string): string | boolean {
+    return this.isValid(version);
+  }
+
+  isVersion(version: string): string | boolean {
+    return this.isValid(version);
+  }
+
+  getMajor(version: string): number | null {
+    return this._getSection(version, 0);
+  }
+
+  getMinor(version: string): number | null {
+    return this._getSection(version, 1);
+  }
+
+  getPatch(version: string): number | null {
+    return this._getSection(version, 2);
+  }
+
+  equals(version: string, other: string): boolean {
+    return this._compare(version, other) === 0;
+  }
+
+  isGreaterThan(version: string, other: string): boolean {
+    return this._compare(version, other) > 0;
+  }
+
+  isLessThanRange(version: string, range: string): boolean {
+    return this._compare(version, range) < 0;
+  }
+
+  maxSatisfyingVersion(versions: string[], range: string): string | null {
+    return versions.find(v => this.equals(v, range)) || null;
+  }
+
+  minSatisfyingVersion(versions: string[], range: string): string | null {
+    return versions.find(v => this.equals(v, range)) || null;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  getNewValue(
+    _currentValue: string,
+    _rangeStrategy: RangeStrategy,
+    _fromVersion: string,
+    toVersion: string
+  ): string {
+    return toVersion;
+  }
+
+  sortVersions(version: string, other: string): number {
+    return this._compare(version, other);
+  }
+
+  matches(version: string, range: string): boolean {
+    return this.equals(version, range);
+  }
+}
--- test/versioning/index.spec.ts
+++ test/versioning/index.spec.ts
@@ -16,21 +16,55 @@ describe('versioning.get(versionScheme)', () => {
   });
 
   describe('should return the same interface', () => {
-    const optionalFunctions = ['isLessThanRange', 'valueToVersion'];
+    const optionalFunctions = [
+      'isLessThanRange',
+      'valueToVersion',
+      'constructor',
+      'hasOwnProperty',
+      'isPrototypeOf',
+      'propertyIsEnumerable',
+      'should',
+      'toLocaleString',
+      'toString',
+      'valueOf',
+    ];
     const npmApi = Object.keys(versioning.get('semver'))
       .filter(val => !optionalFunctions.includes(val))
       .sort();
+
+    function getAllPropertyNames(obj: any): string[] {
+      const props = [];
+      let o = obj;
+
+      // eslint-disable-next-line no-cond-assign
+      do {
+        Object.getOwnPropertyNames(o).forEach(prop => {
+          if (props.indexOf(prop) === -1) {
+            props.push(prop);
+          }
+        });
+      } while ((o = Object.getPrototypeOf(o)));
+
+      return props;
+    }
+
     for (const supportedScheme of supportedSchemes) {
       it(supportedScheme, () => {
-        const schemeKeys = Object.keys(versioning.get(supportedScheme))
-          .filter(val => !optionalFunctions.includes(val))
+        const schemeKeys = getAllPropertyNames(versioning.get(supportedScheme))
+          .filter(
+            val => !optionalFunctions.includes(val) && !val.startsWith('_')
+          )
           .sort();
+
         expect(schemeKeys).toEqual(npmApi);
-        expect(
-          Object.keys(
-            require('../../lib/versioning/' + supportedScheme).api
-          ).sort()
-        ).toEqual(Object.keys(versioning.get(supportedScheme)).sort());
+
+        const apiOrCtor = require('../../lib/versioning/' + supportedScheme)
+          .api;
+        if (versioning.isVersioningApiConstructor(apiOrCtor)) return;
+
+        expect(Object.keys(apiOrCtor).sort()).toEqual(
+          Object.keys(versioning.get(supportedScheme)).sort()
+        );
       });
     }
   });
