871f23edbff00ea95b373d573e1e06f03c7ab201
Julien Simon <julien.simon@karhoo.com>
2019-08-27 22:13:25 +0100

    update

--- lib/workers/global/limits.ts
+++ lib/workers/global/limits.ts
@@ -1,7 +1,6 @@
 import { logger } from '../../logger';
 
 const limitsToInit = ['prCommitsPerRunLimit'];
-
 const l = {};
 const v = {};
 
@@ -9,19 +8,8 @@ export function init() {
   let i;
   for (i = 0; i < limitsToInit.length; i += 1) {
     if (process.env[limitsToInit[i]]) {
-      logger.info(
-        `4279 feat - Limits.init ${
-          limitsToInit[i]
-        } environment variable is set to ${process.env[limitsToInit[i]]}`
-      );
-      logger.info(
-        `4279 feat - Limits.init ${limitsToInit[i]} = ${
-          process.env[limitsToInit[i]]
-        }`
-      );
       setLimit(limitsToInit[i], parseInt(process.env[limitsToInit[i]], 10));
       v[limitsToInit[i]] = 0;
-      logger.info(`4279 feat - Limits v[${limitsToInit[i]}] = 0`);
     } else {
       logger.info(
         `4279 feat - Limits.init ${limitsToInit[i]} environment variable is not set. Ignoring ${limitsToInit[i]}`
@@ -38,9 +26,6 @@ export function setLimit(name: string, value: number) {
 export function getLimitRemaining(name: string) {
   let result;
   if (typeof v[name] !== 'undefined') {
-    logger.info(
-      `4279 feat - Limits.getLimitRemaining for ${name} = ${l[name] - v[name]}`
-    );
     result = l[name] - v[name];
   } else {
     result = undefined;
@@ -50,10 +35,6 @@ export function getLimitRemaining(name: string) {
 
 export function incrementLimit(name: string, value: number) {
   if (typeof v[name] !== 'undefined') {
-    logger.info(
-      `4279 feat - Limits.incrementLimit for ${name} by ${value} = ${v[name] +
-        value}`
-    );
     v[name] += value;
   }
 }
