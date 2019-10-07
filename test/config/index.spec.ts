856518a98dc55d5a62e0e5994b3eef7576dc5907
Jamie Magee <jamie.magee@gmail.com>
2019-10-07 22:28:52 +0200

    fix: wrap mock in try/catch

--- test/config/index.spec.ts
+++ test/config/index.spec.ts
@@ -1,12 +1,14 @@
-import { env as _env } from 'process';
 import argv from './config/_fixtures/argv';
 import { getConfig } from '../../lib/config/defaults';
 import * as _npm from '../../lib/datasource/npm';
 import presetDefaults from './npm/_fixtures/renovate-config-default.json';
 
 jest.mock('../../lib/datasource/npm');
-jest.mock('../../config.js');
-jest.mock(_env.RENOVATE_CONFIG_FILE);
+try {
+  jest.mock('../../config.js');
+} catch (err) {
+  // file does not exist
+}
 
 const npm: any = _npm;
 const defaultConfig = getConfig();
