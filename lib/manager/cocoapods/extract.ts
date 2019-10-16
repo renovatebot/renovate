6ddfd5a4e74497535fb08386247b3a4f06d41781
Sergio Zharinov <sergio.zharinov@gmail.com>
2019-10-16 16:15:03 +0400

    feat(cocoapods): extractPackageFile

---
+++ lib/manager/cocoapods/extract.ts
@@ -0,0 +1,77 @@
+import { logger } from '../../logger';
+import { PackageDependency, PackageFile } from '../common';
+
+const regexMapping = {
+  depName: /^\s*pod\s+(['"])(?<depName>[^'"]+)\1/,
+  currentValue: /^\s*pod\s+(['"])(?<depName>[^'"]+)\1\s*,\s*(['"])(?<currentValue>[^'"]+)\3\s*$/,
+  git: /,\s*:git\s*=>\s*(['"])(?<git>[^'"]+)\1/,
+  tag: /,\s*:tag\s*=>\s*(['"])(?<tag>[^'"]+)\1/,
+  path: /,\s*:path\s*=>\s*(['"])(?<path>[^'"]+)\1/,
+};
+
+interface ParsedLine {
+  depName?: string;
+  currentValue?: string;
+  git?: string;
+  tag?: string;
+  path?: string;
+}
+
+function parseLine(line): ParsedLine {
+  const result = {};
+  for (const [key, regex] of Object.entries(regexMapping)) {
+    const match = line.replace(/#.*$/, '').match(regex);
+    const value = match && match.groups && match.groups[key];
+    if (value) {
+      result[key] = value;
+    }
+  }
+  return result;
+}
+
+export function extractPackageFile(content: string): PackageFile {
+  logger.trace('cocoapods.extractPackageFile()');
+  const deps: PackageDependency[] = [];
+  const lines = content.split('\n');
+
+  for (let idx = 0; idx < lines.length; idx += 1) {
+    const line = lines[idx];
+    const { depName, currentValue, git, tag, path } = parseLine(line);
+    if (depName) {
+      let dep: PackageDependency = {
+        depName,
+        skipReason: 'unknown-version',
+      };
+
+      if (currentValue) {
+        dep = {
+          depName,
+          datasource: 'cocoapods',
+          currentValue,
+        };
+      } else if (git) {
+        if (tag) {
+          dep = {
+            datasource: 'git-tags',
+            depName: git,
+            currentValue: tag,
+          };
+        } else {
+          dep = {
+            depName,
+            skipReason: 'git-dependency',
+          };
+        }
+      } else if (path) {
+        dep = {
+          depName,
+          skipReason: 'path-dependency',
+        };
+      }
+
+      deps.push(dep);
+    }
+  }
+
+  return { deps };
+}
---
+++ test/manager/cocoapods/extract.spec.ts
@@ -0,0 +1,17 @@
+import fs from 'fs-extra';
+import path from 'path';
+import { extractPackageFile } from '../../../lib/manager/cocoapods/extract';
+
+const sample = fs.readFileSync(
+  path.resolve(__dirname, './_fixtures/Podfile'),
+  'utf-8'
+);
+
+describe('lib/manager/cocoapods/extract', () => {
+  describe('extractPackageFile()', () => {
+    it('extracts all dependencies', () => {
+      const res = extractPackageFile(sample).deps;
+      expect(res).toMatchSnapshot();
+    });
+  });
+});
