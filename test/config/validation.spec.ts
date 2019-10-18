6892802fd7c96f3dee35fda7b8c0661ba51e6cb1
Rhys Arkins <rhys@arkins.net>
2019-10-18 14:41:38 +0200

    revert result

--- test/config/validation.spec.ts
+++ test/config/validation.spec.ts
@@ -145,14 +145,14 @@ describe('config/validation', () => {
           fileMatch: ['abc ([a-z]+) ([a-z]+))'],
         },
         docker: {
-          fileMatch: ['(x+x+)+y'],
+          fileMatch: ['x?+'],
         },
       };
       const { warnings, errors } = await configValidation.validateConfig(
         config
       );
       expect(warnings).toHaveLength(0);
-      expect(errors).toHaveLength(1);
+      expect(errors).toHaveLength(2);
       expect(errors).toMatchSnapshot();
     });
 
