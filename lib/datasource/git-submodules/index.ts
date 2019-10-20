ffa499f7c723fd2a9f8ed35f0c976a4df02854fc
Jamie Magee <jamie.magee@gmail.com>
2019-10-20 22:42:32 +0200

    test: add datasource tests

--- lib/datasource/git-submodules/index.ts
+++ lib/datasource/git-submodules/index.ts
@@ -1,26 +1,32 @@
 import Git from 'simple-git/promise';
 
 import { ReleaseResult, PkgReleaseConfig, DigestConfig } from '../common';
+import { logger } from '../../logger';
 
 export async function getPkgReleases({
   lookupName,
   registryUrls,
 }: PkgReleaseConfig): Promise<ReleaseResult | null> {
   const git = Git();
-  const newHash = (await git.listRemote([
-    '--refs',
-    registryUrls[0],
-    registryUrls[1],
-  ]))
-    .trim()
-    .split(/\t/)[0];
-  return {
-    releases: [
-      {
-        version: newHash,
-      },
-    ],
-  };
+  try {
+    const newHash = (await git.listRemote([
+      '--refs',
+      registryUrls[0],
+      registryUrls[1],
+    ]))
+      .trim()
+      .split(/\t/)[0];
+    return {
+      releases: [
+        {
+          version: newHash,
+        },
+      ],
+    };
+  } catch (err) {
+    logger.debug(`Error looking up tags in ${lookupName}`);
+  }
+  return null;
 }
 
 export const getDigest = (
