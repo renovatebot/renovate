0b199df918a4cd854c440796f5cf908918a426aa
Tim Jacomb <tim.jacomb@hmcts.net>
2019-11-21 09:06:48 +0000

    feat(helm): Add repo alias support

--- lib/manager/common.ts
+++ lib/manager/common.ts
@@ -18,6 +18,7 @@ export interface ExtractConfig extends ManagerConfig {
   endpoint?: string;
   global?: any;
   gradle?: { timeout?: number };
+  helm?: { aliases?: Record<string, string> };
   ignoreNpmrcFile?: boolean;
 
   skipInstalls?: boolean;
--- lib/manager/helm-requirements/extract.ts
+++ lib/manager/helm-requirements/extract.ts
@@ -3,12 +3,13 @@ import upath from 'upath';
 import yaml from 'js-yaml';
 
 import { logger } from '../../logger';
-import { PackageFile, PackageDependency } from '../common';
+import { PackageFile, PackageDependency, ExtractConfig } from '../common';
 import { platform } from '../../platform';
 
 export async function extractPackageFile(
   content: string,
-  fileName: string
+  fileName: string,
+  config: ExtractConfig
 ): Promise<PackageFile> {
   try {
     const baseDir = upath.parse(fileName).dir;
@@ -50,6 +51,15 @@ export async function extractPackageFile(
     if (dep.repository) {
       res.registryUrls = [dep.repository];
       if (dep.repository.startsWith('@')) {
+        const aliases =
+          config.helm && config.helm.aliases ? config.helm.aliases : null;
+        if (aliases) {
+          const alias = aliases[dep.repository.slice(1)];
+          if (alias) {
+            res.registryUrls = [alias];
+            return res;
+          }
+        }
         res.skipReason = 'placeholder-url';
       } else {
         try {
