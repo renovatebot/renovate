5b84b45516a69e9fe4bf5b2744885f6b8a9a4b37
Michael Kriese <michael.kriese@visualon.de>
2019-11-25 17:40:55 +0100

    test: fix coverage

--- lib/proxy.ts
+++ lib/proxy.ts
@@ -7,6 +7,7 @@ const envVars = ['HTTP_PROXY', 'HTTPS_PROXY', 'NO_PROXY'];
 
 export function bootstrap(): ProxyAgentConfigurationType {
   envVars.forEach(envVar => {
+    /* istanbul ignore if: env is case-insensitive on windows */
     if (
       typeof process.env[envVar] === 'undefined' &&
       typeof process.env[envVar.toLowerCase()] !== 'undefined'
