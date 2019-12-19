b0dbf9526b099fddeb79372baa923511e9f9e8a6
Sergio Zharinov <sergio.zharinov@gmail.com>
2019-12-19 18:58:05 +0400

    fix(pip_setup): Mock misc function tests as well

--- test/manager/pip_setup/extract.spec.ts
+++ test/manager/pip_setup/extract.spec.ts
@@ -1,14 +1,20 @@
-import { exec } from '../../../lib/util/exec';
+import { exec as _exec } from 'child_process';
 
 import {
+  resetModule,
   parsePythonVersion,
   getPythonAlias,
   pythonVersions,
 } from '../../../lib/manager/pip_setup/extract';
 
+const exec: jest.Mock<typeof _exec> = _exec as any;
+jest.mock('child_process');
+
 describe('lib/manager/pip_setup/extract', () => {
   beforeEach(() => {
+    jest.resetAllMocks();
     jest.resetModules();
+    resetModule();
   });
   describe('parsePythonVersion', () => {
     it('returns major and minor version numbers', () => {
@@ -17,42 +23,47 @@ describe('lib/manager/pip_setup/extract', () => {
   });
   describe('getPythonAlias', () => {
     it('returns the python alias to use', async () => {
-      expect(pythonVersions.includes(await getPythonAlias())).toBe(true);
-    });
-    it('finds python', async () => {
-      const fExec = jest.fn(() =>
-        Promise.resolve({ stderr: 'Python 3.7.15rc1' })
-      );
-      jest.doMock('../../../lib/util/exec', () => {
-        return {
-          exec: fExec,
-        };
+      const commands = [];
+      exec.mockImplementationOnce((cmd, _options, callback) => {
+        commands.push(cmd);
+        callback(null, { stdout: '', stderr: 'Python 2.7.17\\n' });
+        return undefined;
       });
-      const m = require('../../../lib/manager/pip_setup/extract');
-      expect(pythonVersions.includes(await m.getPythonAlias())).toBe(true);
-      expect(fExec).toMatchSnapshot();
+      exec.mockImplementationOnce((cmd, _options, _callback) => {
+        commands.push(cmd);
+        throw new Error();
+      });
+      exec.mockImplementationOnce((cmd, _options, callback) => {
+        commands.push(cmd);
+        callback(null, { stdout: 'Python 3.8.0\\n', stderr: '' });
+        return undefined;
+      });
+      const result = await getPythonAlias();
+      expect(pythonVersions.includes(result)).toBe(true);
+      expect(result).toMatchSnapshot();
+      expect(commands).toMatchSnapshot();
     });
   });
-  describe('Test for presence of mock lib', () => {
-    it('should test if python mock lib is installed', async () => {
-      const cp = jest.requireActual('../../../lib/util/exec');
-      let isMockInstalled = true;
-      // when binarysource === docker
-      try {
-        await cp.exec(`python -c "import mock"`);
-      } catch (err) {
-        isMockInstalled = false;
-      }
-      if (!isMockInstalled) {
-        try {
-          const pythonAlias = await getPythonAlias();
-          await exec(`${pythonAlias} -c "from unittest import mock"`);
-          isMockInstalled = true;
-        } catch (err) {
-          isMockInstalled = false;
-        }
-      }
-      expect(isMockInstalled).toBe(true);
-    });
-  });
+  // describe('Test for presence of mock lib', () => {
+  //   it('should test if python mock lib is installed', async () => {
+  //     const cp = jest.requireActual('../../../lib/util/exec');
+  //     let isMockInstalled = true;
+  //     // when binarysource === docker
+  //     try {
+  //       await cp.exec(`python -c "import mock"`);
+  //     } catch (err) {
+  //       isMockInstalled = false;
+  //     }
+  //     if (!isMockInstalled) {
+  //       try {
+  //         const pythonAlias = await getPythonAlias();
+  //         await exec(`${pythonAlias} -c "from unittest import mock"`);
+  //         isMockInstalled = true;
+  //       } catch (err) {
+  //         isMockInstalled = false;
+  //       }
+  //     }
+  //     expect(isMockInstalled).toBe(true);
+  //   });
+  // });
 });
