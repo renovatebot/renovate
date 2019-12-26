ff079793a576a29da25590d49d9c8b4bc5f27029
Sourav Das <souravdasslg95@gmail.com>
2019-12-26 14:38:43 +0530

    fix(config): log error and exit immediately when config.js file has syntax error (#5057)

--- test/config/file.spec.ts
+++ test/config/file.spec.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import fs from 'fs';
 import * as file from '../../lib/config/file';
 import customConfig from './config/_fixtures/file';
 
@@ -21,5 +22,31 @@ describe('config/file', () => {
       expect(res).toMatchSnapshot();
       expect(res.rangeStrategy).toEqual('bump');
     });
+    it('informs user when error in parsing config.js', () => {
+      const configFile = path.resolve(__dirname, './config/_fixtures/file3.js');
+      const fileContent = `module.exports = {
+        "platform": "github",
+        "token":"abcdef",
+        "logFileLevel": "warn",
+        "logLevel": "info",
+        "onboarding": false,
+        "gitAuthor": "Renovate Bot <bot@renovateapp.com>"
+        "onboardingConfig": {
+          "extends": ["config:base"],
+        },
+        "repositories": [ "test/test" ],
+      };`;
+      fs.writeFileSync(configFile, fileContent, { encoding: 'utf8' });
+      expect(
+        file.getConfig({ RENOVATE_CONFIG_FILE: configFile })
+      ).toStrictEqual({});
+      fs.unlinkSync(configFile);
+    });
+  });
+  it('handles when invalid file location is provided', () => {
+    const configFile = path.resolve(__dirname, './config/_fixtures/file4.js');
+    expect(file.getConfig({ RENOVATE_CONFIG_FILE: configFile })).toStrictEqual(
+      {}
+    );
   });
 });
