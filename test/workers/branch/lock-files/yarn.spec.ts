9e7fa2609fb999500b3b9158836dff49f7b3a370
Sergio Zharinov <sergio.zharinov@gmail.com>
2020-01-02 15:14:10 +0400

    Default yarn snapshots

--- test/workers/branch/lock-files/yarn.spec.ts
+++ test/workers/branch/lock-files/yarn.spec.ts
@@ -3,7 +3,7 @@ import _fs from 'fs-extra';
 import { exec as _exec } from 'child_process';
 import * as _yarnHelper from '../../../../lib/manager/npm/post-update/yarn';
 import { mocked } from '../../../util';
-import { ExecSnapshots, mockExecAll } from '../../../execUtil';
+import { mockExecAll } from '../../../execUtil';
 
 jest.mock('fs-extra');
 jest.mock('child_process');
@@ -16,15 +16,6 @@ const fs = mocked(_fs);
 const yarnHelper = mocked(_yarnHelper);
 
 let processEnv;
-
-const fixSnapshots = (snapshots: ExecSnapshots): ExecSnapshots =>
-  snapshots.map(snapshot => ({
-    ...snapshot,
-    cmd: snapshot.cmd.replace(
-      /^node .*?bin\/yarn.*?\.js /,
-      'node bin/yarn.js '
-    ),
-  }));
 
 describe('generateLockFile', () => {
   beforeEach(() => {
@@ -55,7 +46,7 @@ describe('generateLockFile', () => {
     const res = await yarnHelper.generateLockFile('some-dir', env, config);
     expect(fs.readFile).toHaveBeenCalledTimes(1);
     expect(res.lockFile).toEqual('package-lock-contents');
-    expect(fixSnapshots(execSnapshots)).toMatchSnapshot();
+    expect(execSnapshots).toMatchSnapshot();
   });
   it('performs lock file updates', async () => {
     const execSnapshots = mockExecAll(exec);
@@ -68,7 +59,7 @@ describe('generateLockFile', () => {
       { depName: 'some-dep', isLockfileUpdate: true },
     ]);
     expect(res.lockFile).toEqual('package-lock-contents');
-    expect(fixSnapshots(execSnapshots)).toMatchSnapshot();
+    expect(execSnapshots).toMatchSnapshot();
   });
   it('detects yarnIntegrity', async () => {
     const execSnapshots = mockExecAll(exec);
@@ -97,7 +88,7 @@ describe('generateLockFile', () => {
     expect(fs.readFile).toHaveBeenCalledTimes(1);
     expect(res.error).toBe(true);
     expect(res.lockFile).not.toBeDefined();
-    expect(fixSnapshots(execSnapshots)).toMatchSnapshot();
+    expect(execSnapshots).toMatchSnapshot();
   });
   it('finds yarn embedded in renovate', async () => {
     const execSnapshots = mockExecAll(exec);
