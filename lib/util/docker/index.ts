f1d703272c51b78b56566f5f94d5aee3410acb9b
Sergio Zharinov <zharinov@users.noreply.github.com>
2020-01-07 11:47:12 +0400

    refactor(docker): Use pre/post commands instead of wrapping (#5103)
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/util/docker/index.ts
+++ lib/util/docker/index.ts
@@ -1,3 +1,5 @@
+import { uniq } from 'lodash';
+
 type Opt<T> = T | null | undefined;
 
 export interface DockerOptions {
@@ -7,37 +9,53 @@ export interface DockerOptions {
   envVars?: Opt<Opt<string>[]>;
   cwd?: Opt<string>;
   tag?: Opt<string>;
-  cmdWrap?: Opt<string>;
+  preCommands?: Opt<string[]>;
+  postCommands?: Opt<string[]>;
 }
 
 export function dockerCmd(cmd: string, options: DockerOptions): string {
-  const { dockerUser, volumes, envVars, cwd, image, tag, cmdWrap } = options;
+  const {
+    dockerUser,
+    envVars,
+    cwd,
+    image,
+    tag,
+    preCommands = [],
+    postCommands = [],
+  } = options;
 
   const result = ['docker run --rm'];
   if (dockerUser) result.push(`--user=${dockerUser}`);
 
-  if (volumes)
-    result.push(...volumes.filter(x => !!x).map(vol => `-v "${vol}":"${vol}"`));
+  let volumes = options.volumes || [];
+  volumes = cwd ? [...volumes, options.cwd] : volumes;
+  if (volumes) {
+    result.push(
+      ...uniq(volumes)
+        .filter(x => typeof x === 'string')
+        .map(vol => `-v "${vol}":"${vol}"`)
+    );
+  }
 
-  if (envVars) result.push(...envVars.filter(x => !!x).map(e => `-e ${e}`));
+  if (envVars) {
+    result.push(
+      ...uniq(envVars)
+        .filter(x => typeof x === 'string')
+        .map(e => `-e ${e}`)
+    );
+  }
 
   if (cwd) result.push(`-w "${cwd}"`);
 
   const taggedImage = tag ? `${image}:${tag}` : `${image}`;
   result.push(taggedImage);
 
-  if (cmdWrap) {
-    const regex = /{{\s*cmd\s*}}/;
-    if (regex.test(cmdWrap)) {
-      result.push(cmdWrap.replace(regex, cmd));
-    } /* istanbul ignore next */ else {
-      throw new Error(
-        'dockerCmd(): Provide {{ cmd }} placeholder inside `wrapCmd` parameter'
-      );
-    }
-  } else {
-    result.push(cmd);
-  }
+  const commands = [...preCommands, cmd, ...postCommands];
+  const command =
+    commands.length === 1
+      ? cmd
+      : `bash -l -c "${commands.join(' && ').replace(/"/g, '\\"')}"`;
+  result.push(command);
 
   return result.join(' ');
 }
