55497ea251d4a0e78bae956f9b111680914de4a7
Rhys Arkins <rhys@arkins.net>
2020-01-21 16:35:09 +0100

    fix: drop unused disable-gitfs error

--- lib/workers/branch/index.ts
+++ lib/workers/branch/index.ts
@@ -445,7 +445,6 @@ export async function processBranch(
       throw new Error(PLATFORM_AUTHENTICATION_ERROR);
     } else if (
       err.message !== DATASOURCE_FAILURE &&
-      err.message !== 'disable-gitfs' &&
       err.message !== PLATFORM_FAILURE
     ) {
       logger.error({ err }, `Error updating branch: ${err.message}`);
