e2d0040bec3758015743362d5d266b51b05ba501
Rhys Arkins <rhys@arkins.net>
2020-02-15 06:56:39 +0100

    Update index.ts

--- lib/manager/kustomize/index.ts
+++ lib/manager/kustomize/index.ts
@@ -1,9 +1,5 @@
-import { LANGUAGE_DOCKER } from '../../constants/languages';
-
 export { extractPackageFile } from './extract';
 export { updateDependency } from './update';
-
-// export const language = LANGUAGE_DOCKER;
 
 export const defaultConfig = {
   fileMatch: ['(^|/)kustomization.yaml'],
