74f24d5b0c8f5b7d9b721f4215a72e62b610130b
Rhys Arkins <rhys@arkins.net>
2020-02-28 17:47:57 +0100

    fix: Revert "fix: Remove authorization header from core.windows.net requests… (#5274)"
    This reverts commit 4c258e27b65d6dd4810a5225fcbf9c8f8f31c1db.
    
    # Conflicts:
    #	lib/datasource/docker/index.ts

--- lib/datasource/docker/index.ts
+++ lib/datasource/docker/index.ts
@@ -1,7 +1,3 @@
-/**
- * @copyright 2020-present by Avid Technology, Inc.
- */
-
 import is from '@sindresorhus/is';
 import hasha from 'hasha';
 import URL from 'url';
@@ -414,32 +410,6 @@ async function getTags(
   }
 }
 
-export function getConfigResponseBeforeRedirectHook(options: any): void {
-  if (options.search?.includes('X-Amz-Algorithm')) {
-    // if there is no port in the redirect URL string, then delete it from the redirect options.
-    // This can be evaluated for removal after upgrading to Got v10
-    const portInUrl = options.href.split('/')[2].split(':')[1];
-    if (!portInUrl) {
-      // eslint-disable-next-line no-param-reassign
-      delete options.port; // Redirect will instead use 80 or 443 for HTTP or HTTPS respectively
-    }
-
-    // docker registry is hosted on amazon, redirect url includes authentication.
-    // eslint-disable-next-line no-param-reassign
-    delete options.headers.authorization;
-  }
-
-  if (options.href && options.headers?.authorization) {
-    const { host } = URL.parse(options.href);
-    // prettier-ignore
-    if (host && host.endsWith('blob.core.windows.net')) { // lgtm [js/incomplete-url-substring-sanitization]
-      // docker registry is hosted on Azure blob, redirect url includes authentication.
-      // eslint-disable-next-line no-param-reassign
-      delete options.headers.authorization;
-    }
-  }
-}
-
 export function getConfigResponse(
   url: string,
   headers: OutgoingHttpHeaders
@@ -447,7 +417,26 @@ export function getConfigResponse(
   return got(url, {
     headers,
     hooks: {
-      beforeRedirect: [getConfigResponseBeforeRedirectHook],
+      beforeRedirect: [
+        (options: any): void => {
+          if (
+            options.search &&
+            options.search.indexOf('X-Amz-Algorithm') !== -1
+          ) {
+            // if there is no port in the redirect URL string, then delete it from the redirect options.
+            // This can be evaluated for removal after upgrading to Got v10
+            const portInUrl = options.href.split('/')[2].split(':')[1];
+            if (!portInUrl) {
+              // eslint-disable-next-line no-param-reassign
+              delete options.port; // Redirect will instead use 80 or 443 for HTTP or HTTPS respectively
+            }
+
+            // docker registry is hosted on amazon, redirect url includes authentication.
+            // eslint-disable-next-line no-param-reassign
+            delete options.headers.authorization;
+          }
+        },
+      ],
     },
   });
 }
