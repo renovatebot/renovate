4f71eff4ab3ce613a0789343e42c28a024cfed98
Rhys Arkins <rhys@arkins.net>
2020-03-05 16:01:03 +0100

    chore: fix types (#5645)

--- lib/manager/npm/extract/common.ts
+++ lib/manager/npm/extract/common.ts
@@ -1,9 +1,10 @@
 // eslint-disable-next-line import/no-unresolved
 import { PackageJson } from 'type-fest';
 
+export type NpmPackageDependeny = PackageJson.Dependency;
+
 export interface NpmPackage extends PackageJson {
-  workspaces?: any;
-  renovate?: any;
+  renovate?: unknown;
   _from?: any;
   _args?: any;
   _id?: any;
