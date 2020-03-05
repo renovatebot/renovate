8d0f3f968296f0c02534708885d4bb54f7c9eed0
Daniel Tschinder <daniel@tschinder.de>
2020-03-05 14:01:57 +0100

    fix: Detect binary and module npm packages as libraries

--- lib/manager/npm/extract/type.ts
+++ lib/manager/npm/extract/type.ts
@@ -6,8 +6,12 @@ export function mightBeABrowserLibrary(packageJson: NpmPackage): boolean {
     // it's not published
     return false;
   }
-  if (packageJson.main === undefined) {
-    // it can't be required
+  if (
+    packageJson.main === undefined &&
+    packageJson.module === undefined &&
+    packageJson.bin === undefined
+  ) {
+    // it can't be required nor does it have a binary
     return false;
   }
   // TODO: how can we know if it's a node.js library only, and not browser?
