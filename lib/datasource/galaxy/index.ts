2ec935f8c0d07351a2a7e2318947ba1e4c3827b0
ViralRuparel <viralruparel95@gmail.com>
2020-03-07 17:08:02 +0530

    5xx error handling in galaxy

--- lib/datasource/galaxy/index.ts
+++ lib/datasource/galaxy/index.ts
@@ -35,65 +35,73 @@ export async function getPkgReleases({
     '&name=' +
     projectName;
   const galaxyProjectUrl = baseUrl + userName + '/' + projectName;
-
-  let res: any = await got(galaxyAPIUrl, {
-    hostType: id,
-  });
-  if (!res || !res.body) {
-    logger.warn(
-      { dependency: lookupName },
-      `Received invalid crate data from ${galaxyAPIUrl}`
-    );
-    return null;
-  }
-
-  res = res.body;
-  const response = JSON.parse(res);
-
-  // istanbul ignore if
-  if (response.results.length > 1) {
-    logger.warn(
-      { dependency: lookupName },
-      `Received multiple results from ${galaxyAPIUrl}`
-    );
-    return null;
-  }
-  if (response.results.length === 0) {
-    logger.warn(
-      { dependency: lookupName },
-      `Received no results from ${galaxyAPIUrl}`
-    );
-    return null;
-  }
-
-  const resultObject = response.results[0];
-  const versions = resultObject.summary_fields.versions;
-
-  const result: ReleaseResult = {
-    releases: [],
-  };
-
-  result.dependencyUrl = galaxyProjectUrl;
-  if (resultObject.github_user && resultObject.github_repo) {
-    result.sourceUrl =
-      'https://github.com/' +
-      resultObject.github_user +
-      '/' +
-      resultObject.github_repo;
-  }
-
-  result.releases = versions.map(
-    (version: { name: string; release_date: string }) => {
-      const release: Release = {
-        version: version.name,
-        releaseTimestamp: version.release_date,
-      };
-
-      return release;
+  try {
+    let res: any = await got(galaxyAPIUrl, {
+      hostType: id,
+    });
+    if (!res || !res.body) {
+      logger.warn(
+        { dependency: lookupName },
+        `Received invalid crate data from ${galaxyAPIUrl}`
+      );
+      return null;
     }
-  );
 
-  const cacheMinutes = 10;
-  await renovateCache.set(cacheNamespace, cacheKey, result, cacheMinutes);
-  return result;
+    res = res.body;
+    const response = JSON.parse(res);
+
+    // istanbul ignore if
+    if (response.results.length > 1) {
+      logger.warn(
+        { dependency: lookupName },
+        `Received multiple results from ${galaxyAPIUrl}`
+      );
+      return null;
+    }
+    if (response.results.length === 0) {
+      logger.warn(
+        { dependency: lookupName },
+        `Received no results from ${galaxyAPIUrl}`
+      );
+      return null;
+    }
+
+    const resultObject = response.results[0];
+    const versions = resultObject.summary_fields.versions;
+
+    const result: ReleaseResult = {
+      releases: [],
+    };
+
+    result.dependencyUrl = galaxyProjectUrl;
+    if (resultObject.github_user && resultObject.github_repo) {
+      result.sourceUrl =
+        'https://github.com/' +
+        resultObject.github_user +
+        '/' +
+        resultObject.github_repo;
+    }
+
+    result.releases = versions.map(
+      (version: { name: string; release_date: string }) => {
+        const release: Release = {
+          version: version.name,
+          releaseTimestamp: version.release_date,
+        };
+
+        return release;
+      }
+    );
+    const cacheMinutes = 10;
+    await renovateCache.set(cacheNamespace, cacheKey, result, cacheMinutes);
+    return result;
+  } catch (err) {
+    if (
+      err.statusCode === 429 ||
+      (err.statusCode >= 500 && err.statusCode < 600)
+    ) {
+      throw new DatasourceError(err);
+    }
+    throw err;
+  }
 }
