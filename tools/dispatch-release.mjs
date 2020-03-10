752351628cd6c89755b183d11f65a45f5db41c0e
Michael Kriese <michael.kriese@visualon.de>
2020-03-10 18:25:17 +0100

    build: add new event dispatcher

---
+++ tools/dispatch-release.mjs
@@ -0,0 +1,23 @@
+import got from 'got';
+
+const baseUrl = 'https://api.github.com/';
+
+(async () => {
+  await got(`repos/${process.env.GITHUB_REPOSITORY}/dispatches`, {
+    baseUrl,
+    headers: {
+      'user-agent': 'Renovate release helper',
+      authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+    json: true,
+    retry: 5,
+    body: {
+      event_type: 'renovate-release',
+      // max 10 keys here, https://github.com/peter-evans/repository-dispatch#client-payload
+      client_payload: {
+        sha: process.env.GITHUB_SHA,
+        ref: process.env.GITHUB_REF,
+      },
+    },
+  });
+})();
