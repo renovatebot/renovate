90defdb50f7551751ac8c5e89d0be181b9929b90
Michael Kriese <michael.kriese@visualon.de>
2020-03-11 17:09:35 +0100

    build: refactor release (#5695)

--- tools/release.mjs
+++ tools/release.mjs
@@ -8,15 +8,15 @@ let err = false;
 
 shell.echo(`Publishing version: ${version}`);
 
-shell.echo('Publishing docker images ...');
-if (!exec(`./.github/workflows/release-docker.sh ${version} ${sha}`)) {
-  err = true;
-}
-
 shell.echo('Publishing npm package ...');
 if (
   !exec(`yarn publish --non-interactive --new-version ${version} --verbose`)
 ) {
+  err = true;
+}
+
+shell.echo('Publishing docker images ...');
+if (!exec(`./.github/workflows/release-docker.sh ${version} ${sha}`)) {
   err = true;
 }
 
