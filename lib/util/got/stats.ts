eee910a51254f3ac77a754e807a9ad072a210c88
Rhys Arkins <rhys@arkins.net>
2020-03-12 21:41:51 +0100

    feat: log outstandingRequests

--- lib/util/got/stats.ts
+++ lib/util/got/stats.ts
@@ -10,9 +10,12 @@ interface HostStats {
 
 let stats: Record<string, number[]> = {};
 
+let outstandingRequests = {};
+
 // istanbul ignore next
 export const resetStats = (): void => {
   stats = {};
+  outstandingRequests = {};
 };
 
 // istanbul ignore next
@@ -30,15 +33,22 @@ export const printStats = (): void => {
     res.median = entries[Math.floor(entries.length / 2)];
     hostStats[hostname] = res;
   }
-  logger.debug({ hostStats }, 'Host request stats (milliseconds)');
+  logger.debug(
+    { hostStats, outstandingRequests },
+    'Host request stats (milliseconds)'
+  );
 };
 
 export const instance = create({
   options: {},
   handler: (options, next) => {
+    const request = `${options.method} ${options.href}`;
+    logger.info(request);
+    outstandingRequests[request] = true;
     const start = new Date();
     const nextPromise = next(options);
     nextPromise.on('response', () => {
+      delete outstandingRequests[request];
       const elapsed = new Date().getTime() - start.getTime();
       stats[options.hostname] = stats[options.hostname] || [];
       stats[options.hostname].push(elapsed);
