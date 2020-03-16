374df8de8f31d22db891bc4f6ceca35af5bc122a
ViralRuparel <viralruparel95@gmail.com>
2020-03-16 13:34:05 +0530

    feat/git-ref added

--- lib/datasource/git-refs/index.ts
+++ lib/datasource/git-refs/index.ts
@@ -1,11 +1,10 @@
-import * as semver from '../../versioning/semver';
+import simpleGit from 'simple-git/promise';
 import { logger } from '../../logger';
 import { ReleaseResult, GetReleasesConfig } from '../common';
-import * as gitRefs from '../git-refs';
 
-export const id = 'git-tags';
+export const id = 'git-refs';
 
-const cacheNamespace = 'git-tags';
+const cacheNamespace = 'git-refs';
 const cacheMinutes = 10;
 
 // git will prompt for known hosts or passwords, unless we activate BatchMode
@@ -14,6 +13,7 @@ process.env.GIT_SSH_COMMAND = 'ssh -o BatchMode=yes';
 export async function getPkgReleases({
   lookupName,
 }: GetReleasesConfig): Promise<ReleaseResult | null> {
+  const git = simpleGit();
   try {
     const cachedResult = await renovateCache.get<ReleaseResult>(
       cacheNamespace,
@@ -23,28 +23,28 @@ export async function getPkgReleases({
     if (cachedResult) return cachedResult;
 
     // fetch remote tags
-    const lsRemote = await gitRefs.getPkgReleases({ lookupName });
+    const lsRemote = await git.listRemote([lookupName, '--sort=-v:refname']);
 
-    // extract valid tags from git ls-remote which looks like 'commithash\trefs/tags/1.2.3
-    const refs = lsRemote.releases.map(release => release.gitRef);
+    if (!lsRemote) {
+      return null;
+    }
 
-    const tags = refs
-      .map(ref => ref.replace(/^tags\//gm, ''))
-      .filter(tag => semver.isVersion(tag));
+    const refs = lsRemote.replace(/^.+?refs\//gm, '').split('\n');
 
     const sourceUrl = lookupName.replace(/\.git$/, '').replace(/\/$/, '');
+
     const result: ReleaseResult = {
       sourceUrl,
-      releases: tags.map(tag => ({
-        version: tag,
-        gitRef: tag,
+      releases: refs.map(ref => ({
+        version: ref,
+        gitRef: ref,
       })),
     };
 
     await renovateCache.set(cacheNamespace, lookupName, result, cacheMinutes);
     return result;
   } catch (e) {
-    logger.debug(`Error looking up tags in ${lookupName}`);
+    logger.debug(`Error looking up refs in ${lookupName}`);
   }
   return null;
 }
