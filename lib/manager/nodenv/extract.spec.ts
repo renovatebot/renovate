8c3b8d21c71c1ce4d96126cabe8711313e6c6b06
Viral Ruparel <viralruparel95@gmail.com>
2020-03-18 14:25:43 +0530

    feat(node): nodenv(.node-version) support (#5738)

---
+++ lib/manager/nodenv/extract.spec.ts
@@ -0,0 +1,18 @@
+import { extractPackageFile } from './extract';
+
+describe('lib/manager/nvm/extract', () => {
+  describe('extractPackageFile()', () => {
+    it('returns a result', () => {
+      const res = extractPackageFile('8.4.0\n');
+      expect(res.deps).toMatchSnapshot();
+    });
+    it('supports ranges', () => {
+      const res = extractPackageFile('8.4\n');
+      expect(res.deps).toMatchSnapshot();
+    });
+    it('skips non ranges', () => {
+      const res = extractPackageFile('latestn');
+      expect(res.deps).toMatchSnapshot();
+    });
+  });
+});
