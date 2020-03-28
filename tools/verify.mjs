2c4635d6de5f2edceb106ff9579ed165c2f88516
Michael Kriese <michael.kriese@visualon.de>
2020-03-28 12:19:57 +0100

    testing

--- tools/verify.mjs
+++ tools/verify.mjs
@@ -2,11 +2,6 @@ import shell from 'shelljs';
 
 shell.echo(`Verifying ...`);
 
-shell.exec(`npm config ls`);
-shell.exec(`cat /home/runner/work/_temp/.npmrc | grep NODE_`);
-shell.exec(`env | grep NPM_`);
-shell.exec(`env | grep NODE_`);
-
 const res = shell.exec(`npm whoami`);
 if (res.code !== 0) {
   shell.exit(2);
