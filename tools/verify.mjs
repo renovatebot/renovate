797421822efa08648ee70c549ffeb2e035f10595
Michael Kriese <michael.kriese@visualon.de>
2020-03-28 10:47:17 +0100

    fixes

--- tools/verify.mjs
+++ tools/verify.mjs
@@ -1,14 +1,8 @@
 import shell from 'shelljs';
-import { exec } from './utils.mjs';
-
-let err = false;
 
 shell.echo(`Verifying ...`);
 
-if (!exec(`npm whoami`)) {
-  err = true;
-}
-
-if (err) {
-  shell.exit(1);
+const res = shell.exec(`npm whoami`);
+if (res.code !== 0) {
+  shell.exit(2);
 }
