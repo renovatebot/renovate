ab6eb78dd9ba4bc711b5aea39b608cd19acfc351
Rhys Arkins <rhys@arkins.net>
2020-03-29 06:50:46 +0200

    Revert "chore(ci): ignore duplicate publish error (#5815)"
    This reverts commit dde2988289094829e3ecf73da79b0fd7cff54ac0.

--- tools/dispatch-release.mjs
+++ tools/dispatch-release.mjs
@@ -1,5 +1,5 @@
 import got from 'got';
-import core from '@actions/core';
+import shell from 'shelljs';
 import { program } from './utils.mjs';
 
 const version = program.release;
@@ -7,34 +7,32 @@ const dry = program.dryRun;
 
 const baseUrl = 'https://api.github.com/';
 
-export default async function run() {
-  core.info(`Dispatching version: ${version}`);
+shell.echo(`Dispatching version: ${version}`);
 
+(async () => {
   if (dry) {
-    core.warning('DRY-RUN: done.');
+    shell.echo('DRY-RUN: done.');
     return;
   }
-  try {
-    await got(`repos/${process.env.GITHUB_REPOSITORY}/dispatches`, {
-      baseUrl,
-      headers: {
-        'user-agent': 'Renovate release helper',
-        authorization: `token ${process.env.GITHUB_TOKEN}`,
+  await got(`repos/${process.env.GITHUB_REPOSITORY}/dispatches`, {
+    baseUrl,
+    headers: {
+      'user-agent': 'Renovate release helper',
+      authorization: `token ${process.env.GITHUB_TOKEN}`,
+    },
+    json: true,
+    retry: 5,
+    body: {
+      event_type: 'renovate-release',
+      // max 10 keys here, https://github.com/peter-evans/repository-dispatch#client-payload
+      client_payload: {
+        sha: process.env.GITHUB_SHA,
+        ref: process.env.GITHUB_REF,
+        version,
       },
-      json: true,
-      retry: 5,
-      body: {
-        event_type: 'renovate-release',
-        // max 10 keys here, https://github.com/peter-evans/repository-dispatch#client-payload
-        client_payload: {
-          sha: process.env.GITHUB_SHA,
-          ref: process.env.GITHUB_REF,
-          version,
-        },
-      },
-    });
-  } catch (e) {
-    // Ignore for now
-    core.error(e.toString());
-  }
-}
+    },
+  });
+})().catch(e => {
+  // Ignore for now
+  shell.echo(e.toString());
+});
--- tools/verify.mjs
+++ tools/verify.mjs
@@ -1,14 +1,8 @@
-import core from '@actions/core';
-import exec from '@actions/exec';
+import shell from 'shelljs';
 
-core.info(`Verifying ...`);
-(async () => {
-  try {
-    core.groupStart('npm whoami');
-    await exec.exec(`npm`, ['whoami']);
-  } catch (e) {
-    core.setFailed('npm auth error');
-  } finally {
-    core.endGroup('npm whoami');
-  }
-})();
+shell.echo(`Verifying ...`);
+
+const res = shell.exec(`npm whoami`);
+if (res.code !== 0) {
+  shell.exit(2);
+}
