0df61e2fc742fa65d74d9110f3426d443f13b56b
Tanuel <tanuel.mategi@gmail.com>
2020-04-01 14:53:20 +0200

    fix(git-tags): remove try-catch
    this is no longer needed as a null check is done above

--- lib/datasource/git-tags/index.ts
+++ lib/datasource/git-tags/index.ts
@@ -1,6 +1,5 @@
 import { ReleaseResult, GetReleasesConfig } from '../common';
 import * as semver from '../../versioning/semver';
-import { logger } from '../../logger';
 import * as gitRefs from '../git-refs';
 
 export const id = 'git-tags';
@@ -8,31 +7,25 @@ export const id = 'git-tags';
 export async function getPkgReleases({
   lookupName,
 }: GetReleasesConfig): Promise<ReleaseResult | null> {
-  try {
-    // fetch remote tags
-    const rawRefs: gitRefs.RawRefs[] = await gitRefs.getRawRefs({ lookupName });
+  const rawRefs: gitRefs.RawRefs[] = await gitRefs.getRawRefs({ lookupName });
 
-    if (rawRefs === null) {
-      return null;
-    }
-    const tags = rawRefs
-      .filter(ref => ref.type === 'tags')
-      .map(ref => ref.value)
-      .filter(tag => semver.isVersion(tag));
-
-    const sourceUrl = lookupName.replace(/\.git$/, '').replace(/\/$/, '');
-
-    const result: ReleaseResult = {
-      sourceUrl,
-      releases: tags.map(tag => ({
-        version: tag,
-        gitRef: tag,
-      })),
-    };
-
-    return result;
-  } catch (err) {
-    logger.error({ err }, `Git-Tags lookup error in ${lookupName}`);
+  if (rawRefs === null) {
+    return null;
   }
-  return null;
+  const tags = rawRefs
+    .filter(ref => ref.type === 'tags')
+    .map(ref => ref.value)
+    .filter(tag => semver.isVersion(tag));
+
+  const sourceUrl = lookupName.replace(/\.git$/, '').replace(/\/$/, '');
+
+  const result: ReleaseResult = {
+    sourceUrl,
+    releases: tags.map(tag => ({
+      version: tag,
+      gitRef: tag,
+    })),
+  };
+
+  return result;
 }
