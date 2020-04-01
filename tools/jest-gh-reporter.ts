0a69a5ec31c867bc1a6d60f1f4b1aa4cd8054e4d
Michael Kriese <michael.kriese@visualon.de>
2020-04-01 10:02:15 +0200

    chore: chunk annotations

--- tools/jest-gh-reporter.ts
+++ tools/jest-gh-reporter.ts
@@ -1,13 +1,6 @@
-import { ConsoleBuffer } from '@jest/console';
-import {
-  AggregatedResult,
-  Test,
-  BaseReporter,
-  Context,
-  TestResult,
-} from '@jest/reporters';
+import { AggregatedResult, BaseReporter, Context } from '@jest/reporters';
 import { AssertionResult } from '@jest/test-result';
-import { error, warning, info } from '@actions/core';
+import { error, info } from '@actions/core';
 import { GitHub } from '@actions/github';
 import { Octokit } from '@octokit/rest';
 import { getEnv } from './utils';
@@ -20,32 +13,30 @@ function getLevel(test: AssertionResult): Level {
   switch (test.status) {
     case 'failed':
       return 'failure';
-    case 'skipped':
+    case 'pending':
+    case 'todo':
       return 'warning';
     default:
       return 'notice';
   }
 }
 
-class GitHubReporter extends BaseReporter {
-  private consoleLogs: { filePath: string; logs: ConsoleBuffer }[];
+const ignoreStates = new Set(['passed']);
+const MAX_ANNOTATIONS = 50;
 
-  onTestResult(_data: Test, result: TestResult): void {
-    if (getEnv('GITHUB_ACTIONS') !== 'true') {
-      info('');
-      return;
-    }
-    // Catch console logs per test
-    // TestResult will only contain console logs if Jest is run with verbose=false
-    if (result.console) {
-      this.consoleLogs.push({
-        filePath: result.testFilePath,
-        logs: result.console,
-      });
+class GitHubReporter extends BaseReporter {
+  private readonly _api: GitHub | null;
+
+  constructor() {
+    super();
+    try {
+      this._api = new GitHub(getEnv('GITHUB_TOKEN'));
+    } catch (e) {
+      error(`Unexpected error: ${e}`);
+      this._api = null;
     }
   }
 
-  // eslint-disable-next-line class-methods-use-this
   async onRunComplete(
     _contexts: Set<Context>,
     testResult: AggregatedResult
@@ -54,24 +45,19 @@ class GitHubReporter extends BaseReporter {
       if (getEnv('GITHUB_ACTIONS') !== 'true') {
         return;
       }
-      const ref = getEnv('GITHUB_SHA');
-      const [owner, repo] = getEnv('GITHUB_REPOSITORY').split('/');
-      const checkArgs = {
-        name,
-        owner,
-        repo,
-      };
 
-      const api = new GitHub(getEnv('GITHUB_TOKEN'));
-      const output: Octokit.ChecksCreateParamsOutput = {
-        title: 'Jest test results',
-        summary: '',
-        annotations: [],
-      };
+      const annotations: Octokit.ChecksCreateParamsOutputAnnotations[] = [];
 
       for (const suite of testResult.testResults) {
-        for (const test of suite.testResults) {
-          output.annotations.push({
+        for (const test of suite.testResults.filter(t =>
+          ignoreStates.has(t.status)
+        )) {
+          if (annotations.length === MAX_ANNOTATIONS) {
+            await this._createOrUpdate(testResult.success, annotations);
+            annotations.length = 0;
+            break;
+          }
+          annotations.push({
             title: test.fullName,
             message: test.failureMessages?.join('\n '),
             path: suite.testFilePath,
@@ -82,31 +68,60 @@ class GitHubReporter extends BaseReporter {
           });
         }
       }
-
-      const checks = await api.checks.listForRef({
-        ...checkArgs,
-        ref,
-        filter: 'latest',
-      });
-      if (checks.data.total_count && checks.data.check_runs.length) {
-        const [run] = checks.data.check_runs;
-        if (run.conclusion === 'failure' && testResult.success) {
-          warning('Skipping report, because other run failed');
-          return;
-        }
-      }
-
-      await api.checks.create({
-        ...checkArgs,
-        head_sha: ref,
-        completed_at: new Date().toISOString(),
-        conclusion: testResult.success ? 'success' : 'failure',
-        status: 'completed',
-        output,
-      });
     } catch (e) {
       error(`Unexpected error: ${e}`);
     }
+  }
+
+  private async _createOrUpdate(
+    status: boolean,
+    annotations: Octokit.ChecksCreateParamsOutputAnnotations[]
+  ): Promise<void> {
+    if (this._api == null) {
+      return;
+    }
+    const ref = getEnv('GITHUB_SHA');
+    const [owner, repo] = getEnv('GITHUB_REPOSITORY').split('/');
+    const checkArgs = {
+      name,
+      owner,
+      repo,
+    };
+    const output: Octokit.ChecksCreateParamsOutput = {
+      title: 'Jest test results',
+      summary: '',
+      annotations,
+    };
+    const checks = await this._api.checks.listForRef({
+      ...checkArgs,
+      ref,
+      filter: 'latest',
+    });
+    if (checks.data.check_runs.length) {
+      const [run] = checks.data.check_runs;
+      info(`Update check run: ${run.id}`);
+
+      await this._api.checks.update({
+        ...checkArgs,
+        check_run_id: run.id,
+        completed_at: new Date().toISOString(),
+        conclusion: status ? 'success' : 'failure',
+        status: 'completed',
+        output: { ...run.output, annotations },
+      });
+
+      return;
+    }
+
+    info(`Create check run`);
+    await this._api.checks.create({
+      ...checkArgs,
+      head_sha: ref,
+      completed_at: new Date().toISOString(),
+      conclusion: status ? 'success' : 'failure',
+      status: 'completed',
+      output,
+    });
   }
 }
 
