3c045e199825cb3106e10f35909655bbb205b7b2
Michael Kriese <michael.kriese@visualon.de>
2020-04-01 13:09:37 +0200

    test(manager): fix coverage

--- lib/manager/index.spec.ts
+++ lib/manager/index.spec.ts
@@ -45,26 +45,100 @@ describe('manager', () => {
 
   describe('extractAllPackageFiles()', () => {
     it('returns null', () => {
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+      });
       expect(
-        manager.extractAllPackageFiles('dockerfile', {} as any, [])
+        manager.extractAllPackageFiles('unknown', {} as any, [])
       ).toBeNull();
+      expect(manager.extractAllPackageFiles('dummy', {} as any, [])).toBeNull();
     });
     it('returns non-null', () => {
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+        extractAllPackageFiles: () => Promise.resolve([]),
+      });
       expect(
-        manager.extractAllPackageFiles('npm', {} as any, [])
+        manager.extractAllPackageFiles('dummy', {} as any, [])
       ).not.toBeNull();
+    });
+    afterEach(() => {
+      manager.getManagers().delete('dummy');
     });
   });
 
   describe('extractPackageFile()', () => {
     it('returns null', () => {
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+      });
       expect(manager.extractPackageFile('unknown', null)).toBeNull();
+      expect(manager.extractPackageFile('dummy', null)).toBeNull();
+    });
+    it('returns non-null', () => {
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+        extractPackageFile: () => Promise.resolve({ deps: [] }),
+      });
+
+      expect(manager.extractPackageFile('dummy', null)).not.toBeNull();
+    });
+    afterEach(() => {
+      manager.getManagers().delete('dummy');
     });
   });
 
   describe('getPackageUpdates', () => {
     it('returns null', () => {
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+      });
       expect(manager.getPackageUpdates('unknown', null)).toBeNull();
+      expect(manager.getPackageUpdates('dummy', null)).toBeNull();
+    });
+    it('returns non-null', () => {
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+        getPackageUpdates: () => Promise.resolve([]),
+      });
+      expect(manager.getPackageUpdates('dummy', {} as any)).not.toBeNull();
+    });
+    afterEach(() => {
+      manager.getManagers().delete('dummy');
+    });
+  });
+
+  describe('getRangeStrategy', () => {
+    it('returns null', () => {
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+      });
+      expect(
+        manager.getRangeStrategy({ manager: 'unknown', rangeStrategy: 'auto' })
+      ).toBeNull();
+    });
+    it('returns non-null', () => {
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+        getRangeStrategy: () => 'replace',
+      });
+      expect(
+        manager.getRangeStrategy({ manager: 'dummy', rangeStrategy: 'auto' })
+      ).not.toBeNull();
+
+      manager.getManagers().set('dummy', {
+        defaultConfig: {},
+      });
+      expect(
+        manager.getRangeStrategy({ manager: 'dummy', rangeStrategy: 'auto' })
+      ).not.toBeNull();
+
+      expect(
+        manager.getRangeStrategy({ manager: 'dummy', rangeStrategy: 'bump' })
+      ).not.toBeNull();
+    });
+    afterEach(() => {
+      manager.getManagers().delete('dummy');
     });
   });
 });
