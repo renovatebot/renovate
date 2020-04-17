e581d59da61183bac37803f0bd031c0f47805413
Michael Kriese <michael.kriese@visualon.de>
2020-04-17 13:46:10 +0200

    chore: remove obsolete regex

--- lib/manager/gradle-wrapper/search.ts
+++ lib/manager/gradle-wrapper/search.ts
@@ -1,4 +1,3 @@
 export const DISTRIBUTION_URL_REGEX = /^(?<assignment>distributionUrl\s*=\s*)\S*-(?<version>(\d|\.)+)-(?<type>bin|all)\.zip\s*$/;
 export const DISTRIBUTION_CHECKSUM_REGEX = /^(?<assignment>distributionSha256Sum\s*=\s*)(?<checksum>(\w){64}).*$/;
-export const DOWNLOAD_URL_REGEX = /^(?<http>http)\S*-(?<version>(\d|\.)+)-(?<type>bin|all)\.zip\s*$/;
 export const VERSION_REGEX = /-(?<version>(\d|\.)+)-/;
