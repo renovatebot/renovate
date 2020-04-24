1ee8007c8abcc4d9d2741c240ccfa375c42d1519
Christophe Hurpeau <christophe@hurpeau.com>
2020-04-24 11:31:06 +0200

    feat: yarn2 lockfile support

--- lib/manager/npm/extract/yarn.spec.ts
+++ lib/manager/npm/extract/yarn.spec.ts
@@ -11,12 +11,24 @@ describe('manager/npm/extract/yarn', () => {
       const res = await getYarnLock('package.json');
       expect(Object.keys(res)).toHaveLength(0);
     });
-    it('extracts', async () => {
+
+    it('extracts yarn 1', async () => {
       const plocktest1Lock = readFileSync(
         'lib/manager/npm/__fixtures__/plocktest1/yarn.lock',
         'utf8'
       );
       fs.readLocalFile.mockResolvedValueOnce(plocktest1Lock);
+      const res = await getYarnLock('package.json');
+      expect(res).toMatchSnapshot();
+      expect(Object.keys(res)).toHaveLength(7);
+    });
+
+    it('extracts yarn 2', async () => {
+      const plocktest1Lock = readFileSync(
+        'lib/manager/npm/__fixtures__/yarn2/yarn.lock',
+        'utf8'
+      );
+      fs.readLocalFile.mockReturnValueOnce(plocktest1Lock);
       const res = await getYarnLock('package.json');
       expect(res).toMatchSnapshot();
       expect(Object.keys(res)).toHaveLength(7);
--- lib/manager/npm/extract/yarn.ts
+++ lib/manager/npm/extract/yarn.ts
@@ -1,30 +1,81 @@
-import { parse } from '@yarnpkg/lockfile';
+import { parseSyml } from '@yarnpkg/parsers';
 import { logger } from '../../../logger';
 import { readLocalFile } from '../../../util/fs';
 import { LockFileEntry } from './common';
 
 export type YarnLock = Record<string, string>;
 
+interface ParseRangeReturnType {
+  protocol: string | null;
+  source: string | null;
+  selector: string | null;
+  params: string | null;
+}
+
+// https:// github.com/yarnpkg/berry/blob/b128988379a1f257b2406ebf5058b67861403bd4/packages/yarnpkg-core/sources/structUtils.ts#L260-L303
+/* istanbul ignore next */
+export function parseRange(range: string): ParseRangeReturnType {
+  const match = /^([^#:]*:)?((?:(?!::)[^#])*)(?:#((?:(?!::).)*))?(?:::(.*))?$/.exec(
+    range
+  );
+  if (match === null) {
+    throw new Error(`Invalid range (${range})`);
+  }
+
+  const protocol = typeof match[1] !== `undefined` ? match[1] : null;
+
+  const source =
+    typeof match[3] !== `undefined` ? decodeURIComponent(match[2]) : null;
+
+  const rawSelector =
+    typeof match[3] !== `undefined`
+      ? decodeURIComponent(match[3])
+      : decodeURIComponent(match[2]);
+
+  const selector = rawSelector;
+
+  const params = typeof match[4] !== `undefined` ? match[4] : null;
+
+  return {
+    protocol,
+    source,
+    selector,
+    params,
+  };
+}
+
 export async function getYarnLock(filePath: string): Promise<YarnLock> {
   const yarnLockRaw = await readLocalFile(filePath, 'utf8');
   try {
-    const yarnLockParsed = parse(yarnLockRaw);
-    // istanbul ignore if
-    if (yarnLockParsed.type !== 'success') {
-      logger.debug(
-        { filePath, parseType: yarnLockParsed.type },
-        'Error parsing yarn.lock - not success'
-      );
-      return {};
-    }
+    const parsed = parseSyml(yarnLockRaw);
     const lockFile: YarnLock = {};
 
-    for (const [entry, val] of Object.entries(
-      yarnLockParsed.object as LockFileEntry
-    )) {
-      logger.trace({ entry, version: val.version });
-      lockFile[entry] = val.version;
-    }
+    Object.keys(parsed).forEach((key) => {
+      if (key === '__metadata') {
+        return;
+      }
+
+      const val: LockFileEntry = parsed[key];
+
+      key.split(', ').forEach((entry) => {
+        // https:// github.com/yarnpkg/berry/blob/b128988379a1f257b2406ebf5058b67861403bd4/packages/yarnpkg-core/sources/structUtils.ts#L193-L214
+        const [
+          ,
+          packageName,
+          range,
+        ] = /^((?:(?:@[^/]+?)\/)?(?:[^/]+?))@(.+)$/i.exec(entry);
+
+        const { selector } = parseRange(range);
+
+        logger.trace({ entry, version: val.version });
+        lockFile[packageName + '@' + selector] = parsed[key].version;
+      });
+
+      // istanbul ignore if
+      if (val.integrity) {
+        lockFile['@renovate_yarn_integrity'] = true;
+      }
+    });
     return lockFile;
   } catch (err) {
     logger.debug({ filePath, err }, 'Warning: Exception parsing yarn.lock');
