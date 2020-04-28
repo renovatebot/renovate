6a9d36166a6aa6222aef20c8cad0c363edf8b8e7
hw <hussainweb@gmail.com>
2020-04-28 16:10:56 -0400

    test: expand test case inputs

--- lib/versioning/composer/index.spec.ts
+++ lib/versioning/composer/index.spec.ts
@@ -106,13 +106,14 @@ describe('semver.maxSatisfyingVersion()', () => {
           '0.5.0',
           '4.0.0-beta1',
           '4.0.0',
+          '4.0.1-beta1',
           '4.2.0-beta1',
           '4.2.0-beta2',
           '5.0.0',
         ],
         '~4@beta'
       )
-    ).toBe('4.2.0-beta2');
+    ).toBe('4.0.1-beta1');
   });
 });
 describe('semver.minSatisfyingVersion()', () => {
