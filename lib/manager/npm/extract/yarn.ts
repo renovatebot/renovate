4f12a46be448e5677f17fc559162edd8012bc3d3
Christophe Hurpeau <christophe@hurpeau.com>
2020-05-01 11:48:54 +0200

    refactor: use yarnpkg/core

--- lib/manager/npm/extract/yarn.ts
+++ lib/manager/npm/extract/yarn.ts
@@ -1,3 +1,4 @@
+import { structUtils } from '@yarnpkg/core';
 import { parseSyml } from '@yarnpkg/parsers';
 import { logger } from '../../../logger';
 import { readLocalFile } from '../../../util/fs';
@@ -10,38 +11,6 @@ interface ParseRangeReturnType {
   source: string | null;
   selector: string | null;
   params: string | null;
-}
-
-// https:// github.com/yarnpkg/berry/blob/b128988379a1f257b2406ebf5058b67861403bd4/packages/yarnpkg-core/sources/structUtils.ts#L260-L303
-/* istanbul ignore next */
-export function parseRange(range: string): ParseRangeReturnType {
-  const match = /^([^#:]*:)?((?:(?!::)[^#])*)(?:#((?:(?!::).)*))?(?:::(.*))?$/.exec(
-    range
-  );
-  if (match === null) {
-    throw new Error(`Invalid range (${range})`);
-  }
-
-  const protocol = typeof match[1] !== `undefined` ? match[1] : null;
-
-  const source =
-    typeof match[3] !== `undefined` ? decodeURIComponent(match[2]) : null;
-
-  const rawSelector =
-    typeof match[3] !== `undefined`
-      ? decodeURIComponent(match[3])
-      : decodeURIComponent(match[2]);
-
-  const selector = rawSelector;
-
-  const params = typeof match[4] !== `undefined` ? match[4] : null;
-
-  return {
-    protocol,
-    source,
-    selector,
-    params,
-  };
 }
 
 export async function getYarnLock(filePath: string): Promise<YarnLock> {
@@ -58,14 +27,9 @@ export async function getYarnLock(filePath: string): Promise<YarnLock> {
       const val: LockFileEntry = parsed[key];
 
       key.split(', ').forEach((entry) => {
-        // https:// github.com/yarnpkg/berry/blob/b128988379a1f257b2406ebf5058b67861403bd4/packages/yarnpkg-core/sources/structUtils.ts#L193-L214
-        const [
-          ,
-          packageName,
-          range,
-        ] = /^((?:(?:@[^/]+?)\/)?(?:[^/]+?))@(.+)$/i.exec(entry);
-
-        const { selector } = parseRange(range);
+        const { scope, name, range } = structUtils.parseDescriptor(entry);
+        const packageName = scope ? `${scope}/${name}` : name;
+        const { selector } = structUtils.parseRange(range);
 
         logger.trace({ entry, version: val.version });
         lockFile[packageName + '@' + selector] = parsed[key].version;
