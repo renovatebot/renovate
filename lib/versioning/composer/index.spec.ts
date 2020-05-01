8f66b5ec3c8b2a01a89bb16901ccece4cadd8fad
hussainweb <hussainweb@gmail.com>
2020-05-01 07:03:50 -0400

    feat(composer): support stability modifiers (#6050) (#6057)

--- lib/versioning/composer/index.spec.ts
+++ lib/versioning/composer/index.spec.ts
@@ -12,6 +12,11 @@ describe('semver.equals(a, b)', () => {
   it('should pad really short version', () => {
     expect(semver.equals('v1.0.0', '1')).toBe(true);
   });
+  it('should translate stability modifier', () => {
+    expect(semver.equals('1.0@alpha3', '1.0.0-alpha.3')).toBe(true);
+    expect(semver.equals('1.0@beta', '1.0.0-beta')).toBe(true);
+    expect(semver.equals('1.0@rc2', '1.0.0-rc.2')).toBe(true);
+  });
 });
 describe('semver.isGreaterThan(a, b)', () => {
   it('should pad short version', () => {
@@ -48,6 +53,11 @@ describe('semver.isValid(input)', () => {
     expect(semver.isValid('~1.2.3')).toBeTruthy();
     expect(semver.isValid('^1.2.3')).toBeTruthy();
     expect(semver.isValid('>1.2.3')).toBeTruthy();
+  });
+  it('should support ranges with stability modifiers', () => {
+    expect(semver.isValid('~1.2.3@beta1')).toBeTruthy();
+    expect(semver.isValid('^1.2.3@alpha')).toBeTruthy();
+    expect(semver.isValid('>1.2.3@rc2')).toBeTruthy();
   });
 });
 describe('semver.isVersion(input)', () => {
@@ -88,6 +98,22 @@ describe('semver.maxSatisfyingVersion()', () => {
       )
     ).toBe('0.5.0');
   });
+  it('handles prereleases', () => {
+    expect(
+      semver.maxSatisfyingVersion(
+        [
+          '0.4.0',
+          '0.5.0',
+          '4.0.0-beta1',
+          '4.0.0-beta2',
+          '4.2.0-beta1',
+          '4.2.0-beta2',
+          '5.0.0',
+        ],
+        '~4@beta'
+      )
+    ).toBe('4.0.0-beta2');
+  });
 });
 describe('semver.minSatisfyingVersion()', () => {
   it('handles massaged tilde', () => {
@@ -109,6 +135,22 @@ describe('semver.minSatisfyingVersion()', () => {
         '~0.4'
       )
     ).toBe('0.4.0');
+  });
+  it('handles prereleases', () => {
+    expect(
+      semver.minSatisfyingVersion(
+        [
+          '0.4.0',
+          '0.5.0',
+          '4.0.0-beta1',
+          '4.0.0',
+          '4.2.0-beta1',
+          '4.2.0-beta2',
+          '5.0.0',
+        ],
+        '~4@beta'
+      )
+    ).toBe('4.0.0-beta1');
   });
 });
 describe('semver.matches()', () => {
@@ -285,6 +327,26 @@ describe('semver.getNewValue()', () => {
         toVersion: '1.1.7',
       })
     ).toEqual('^v1.1');
+  });
+  it('bumps short caret with stability modifiers', () => {
+    expect(
+      semver.getNewValue({
+        currentValue: '^v1.0@beta',
+        rangeStrategy: 'bump',
+        fromVersion: '1.0.0-beta3',
+        toVersion: '1.0.0-beta5',
+      })
+    ).toEqual('^v1.0.0@beta5');
+  });
+  it('replaces short caret with stability modifiers', () => {
+    expect(
+      semver.getNewValue({
+        currentValue: '^v1.0@beta',
+        rangeStrategy: 'replace',
+        fromVersion: '1.0.0-beta3',
+        toVersion: '2.0.0-beta5',
+      })
+    ).toEqual('^v2.0.0@beta5');
   });
   it('handles differing lengths', () => {
     expect(
