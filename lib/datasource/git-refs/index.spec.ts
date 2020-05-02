1826fa8f74c9ee0b21f2294c8cd7aff1953c3a94
Rhys Arkins <rhys@arkins.net>
2020-05-02 07:42:40 +0200

    test: add snapshots for git-refs and git-tags

--- lib/datasource/git-refs/index.spec.ts
+++ lib/datasource/git-refs/index.spec.ts
@@ -39,7 +39,7 @@ describe('datasource/git-refs', () => {
       const versions = await getReleases({
         lookupName,
       });
-
+      expect(versions).toMatchSnapshot();
       const result = versions.releases.map((x) => x.version).sort();
       expect(result).toEqual(['0.0.1', 'v0.0.2', 'v0.0.3']);
     });
