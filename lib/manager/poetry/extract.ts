e0fa50123ff74f10f068d50ad24fa251efcfe751
Michael Kriese <michael.kriese@visualon.de>
2020-05-05 11:43:09 +0200

    feat(manager): dynamic poetry install (#6119)

--- lib/manager/poetry/extract.ts
+++ lib/manager/poetry/extract.ts
@@ -1,3 +1,4 @@
+import is from '@sindresorhus/is';
 import { parse } from 'toml';
 import * as datasourcePypi from '../../datasource/pypi';
 import { logger } from '../../logger';
@@ -111,5 +112,22 @@ export function extractPackageFile(
     return null;
   }
 
-  return { deps, registryUrls: extractRegistries(pyprojectfile) };
+  const compatibility: Record<string, any> = {};
+
+  // https://python-poetry.org/docs/pyproject/#poetry-and-pep-517
+  if (
+    pyprojectfile['build-system']?.['build-backend'] === 'poetry.masonry.api'
+  ) {
+    compatibility.poetry = pyprojectfile['build-system']?.requires.join(' ');
+  }
+
+  if (is.nonEmptyString(pyprojectfile.tool?.poetry?.['dependencies']?.python)) {
+    compatibility.python = pyprojectfile.tool?.poetry?.['dependencies']?.python;
+  }
+
+  return {
+    deps,
+    registryUrls: extractRegistries(pyprojectfile),
+    compatibility,
+  };
 }
