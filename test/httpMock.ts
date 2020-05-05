aa38ddf4283eb2fb6299105e61829734a094118b
Sergio Zharinov <zharinov@users.noreply.github.com>
2020-05-05 16:57:05 +0400

    test: Better GraphQL snapshots (#6128)

--- test/httpMock.ts
+++ test/httpMock.ts
@@ -1,4 +1,6 @@
 import { Url } from 'url';
+import is from '@sindresorhus/is';
+import { parse as parseGraphqlQuery } from 'graphql/language';
 import nock from 'nock';
 
 export type { Scope } from 'nock';
@@ -8,12 +10,77 @@ interface RequestLogItem {
   method: string;
   url: string;
   body?: any;
+  graphql?: any;
 }
 
 type BasePath = string | RegExp | Url;
 
 let requestLog: RequestLogItem[] = [];
 let missingLog: string[] = [];
+
+function simplifyGraphqlAST(tree: any): any {
+  if (!tree || is.emptyArray(tree) || is.emptyObject(tree)) {
+    return null;
+  }
+
+  if (is.array(tree)) {
+    return tree.map(simplifyGraphqlAST);
+  }
+  if (is.object(tree)) {
+    return [
+      'operation',
+      'definitions',
+      'selectionSet',
+      'arguments',
+      'value',
+      'alias',
+      'directives',
+    ].reduce((acc: Record<string, any>, field) => {
+      const value = tree[field];
+      let simplifiedValue;
+
+      if (field === 'definitions') {
+        return (value || []).reduce((defsAcc, def) => {
+          const name = def?.operation;
+          const defValue = simplifyGraphqlAST(def);
+          if (name && defValue) {
+            return { ...defsAcc, [name]: defValue };
+          }
+          return defsAcc;
+        }, {});
+      }
+
+      if (field === 'arguments') {
+        const args = (value || []).reduce((argsAcc, arg) => {
+          const name = arg?.name?.value;
+          const argValue = arg?.value?.value;
+          if (name && argValue) {
+            return { ...argsAcc, [name]: argValue };
+          }
+          return argsAcc;
+        }, {});
+        if (!is.emptyObject(args)) {
+          acc.__args = args;
+        }
+      } else if (field === 'selectionSet') {
+        (value?.selections || []).forEach((selection) => {
+          const name = selection?.name?.value;
+          const selValue = simplifyGraphqlAST(selection);
+          if (name && selValue) {
+            acc[name] = is.emptyObject(selValue) ? null : selValue;
+          }
+        });
+      } else {
+        simplifiedValue = simplifyGraphqlAST(value);
+        if (simplifiedValue) {
+          acc[`__${field}`] = simplifiedValue;
+        }
+      }
+      return acc;
+    }, {});
+  }
+  return tree;
+}
 
 function onMissing(_: any, opts: any): void /* istanbul ignore next */ {
   missingLog.push(`  ${opts.method} ${opts.href}`);
@@ -46,7 +113,15 @@ export function scope(basePath: BasePath, options?: nock.Options): nock.Scope {
     const result: RequestLogItem = { headers, method, url };
     const body = req.options?.body;
     if (body) {
-      result.body = body;
+      try {
+        const strQuery = JSON.parse(body).query;
+        const rawQuery = parseGraphqlQuery(strQuery, {
+          noLocation: true,
+        });
+        result.graphql = simplifyGraphqlAST(rawQuery);
+      } catch (ex) {
+        result.body = body;
+      }
     }
     requestLog.push(result);
   });
