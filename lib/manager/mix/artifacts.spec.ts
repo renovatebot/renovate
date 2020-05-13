b4767d7e6bc64bbf5c5b16916b03225ff2dd3495
Rhys Arkins <rhys@arkins.net>
2020-05-13 10:59:03 +0200

    refactor: mix artifacts read file

--- lib/manager/mix/artifacts.spec.ts
+++ lib/manager/mix/artifacts.spec.ts
@@ -2,19 +2,16 @@ import { exec as _exec } from 'child_process';
 import _fs from 'fs-extra';
 import { envMock, mockExecAll } from '../../../test/execUtil';
 import { mocked } from '../../../test/util';
-import { platform as _platform } from '../../platform';
 import { BinarySource } from '../../util/exec/common';
 import * as _env from '../../util/exec/env';
 import { updateArtifacts } from '.';
 
 const fs: jest.Mocked<typeof _fs> = _fs as any;
 const exec: jest.Mock<typeof _exec> = _exec as any;
-const platform = mocked(_platform);
 const env = mocked(_env);
 
 jest.mock('fs-extra');
 jest.mock('child_process');
-jest.mock('../../platform');
 jest.mock('../../util/exec/env');
 
 const config = {
@@ -72,7 +69,7 @@ describe('.updateArtifacts()', () => {
     ).toBeNull();
   });
   it('returns null if unchanged', async () => {
-    platform.getFile.mockResolvedValueOnce('Current mix.lock');
+    fs.readFile.mockResolvedValueOnce('Current mix.lock' as any);
     const execSnapshots = mockExecAll(exec);
     fs.readFile.mockResolvedValueOnce('Current mix.lock' as any);
     expect(
@@ -86,7 +83,7 @@ describe('.updateArtifacts()', () => {
     expect(execSnapshots).toMatchSnapshot();
   });
   it('returns updated mix.lock', async () => {
-    platform.getFile.mockResolvedValueOnce('Old mix.lock');
+    fs.readFile.mockResolvedValueOnce('Old mix.lock' as any);
     const execSnapshots = mockExecAll(exec);
     fs.readFile.mockResolvedValueOnce('New mix.lock' as any);
     expect(
@@ -103,7 +100,7 @@ describe('.updateArtifacts()', () => {
     expect(execSnapshots).toMatchSnapshot();
   });
   it('catches errors', async () => {
-    platform.getFile.mockResolvedValueOnce('Current mix.lock');
+    fs.readFile.mockResolvedValueOnce('Current mix.lock' as any);
     fs.outputFile.mockImplementationOnce(() => {
       throw new Error('not found');
     });
