5ce284d714e0c8a3e22b460873853f4b395a1d3d
Michael Kriese <michael.kriese@visualon.de>
2020-05-15 13:29:36 +0200

    chore: revert to reduce conflicts

--- lib/manager/gradle-wrapper/artifacts.spec.ts
+++ lib/manager/gradle-wrapper/artifacts.spec.ts
@@ -19,6 +19,8 @@ const config = {
   toVersion: '5.6.4',
 };
 
+jest.mock('../../platform');
+
 expect.addSnapshotSerializer(bufferSerializer());
 
 function readString(...paths: string[]): Promise<string> {
