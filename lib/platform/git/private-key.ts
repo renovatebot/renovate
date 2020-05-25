557b4b6d7ed1d43a002dfae5797a7a9a8b7e4458
Rhys Arkins <rhys@arkins.net>
2020-05-25 16:25:49 +0200

    Revert "use child_process"
    This reverts commit 86525aae649eb07fb8b47ac8cecd720a03c0e560.

--- lib/platform/git/private-key.ts
+++ lib/platform/git/private-key.ts
@@ -1,12 +1,9 @@
-import childProcess from 'child_process';
 import os from 'os';
 import path from 'path';
-import util from 'util';
 import fs from 'fs-extra';
 import { PLATFORM_GPG_FAILED } from '../../constants/error-messages';
 import { logger } from '../../logger';
-
-const exec = util.promisify(childProcess.exec);
+import { exec } from '../../util/exec';
 
 let gitPrivateKey: string;
 let keyId: string;
