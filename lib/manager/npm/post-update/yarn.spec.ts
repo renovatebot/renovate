0d6a0a0aaa73bcc633042d57602438a3c3c8709f
Rhys Arkins <rhys@arkins.net>
2020-05-29 08:43:09 +0200

    feat(internal): yarn use util/env/docker (#6286)
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/manager/npm/post-update/yarn.spec.ts
+++ lib/manager/npm/post-update/yarn.spec.ts
@@ -2,9 +2,8 @@ import { exec as _exec } from 'child_process';
 import _fs from 'fs-extra';
 import { ExecSnapshots, envMock, mockExecAll } from '../../../../test/execUtil';
 import { getName, mocked } from '../../../../test/util';
-import * as _yarnHelper from '../../../manager/npm/post-update/yarn';
-import { BinarySource } from '../../../util/exec/common';
 import * as _env from '../../../util/exec/env';
+import * as _yarnHelper from './yarn';
 
 jest.mock('fs-extra');
 jest.mock('child_process');
@@ -14,6 +13,8 @@ const exec: jest.Mock<typeof _exec> = _exec as any;
 const env = mocked(_env);
 const fs = mocked(_fs);
 const yarnHelper = mocked(_yarnHelper);
+
+delete process.env.NPM_CONFIG_CACHE;
 
 // TODO: figure out snapshot similarity for each CI platform
 const fixSnapshots = (snapshots: ExecSnapshots): ExecSnapshots =>
@@ -27,7 +28,7 @@ describe(getName(__filename), () => {
     jest.resetAllMocks();
     env.getChildProcessEnv.mockReturnValue(envMock.basic);
   });
-  it.each([['1.0.0']])(
+  it.each([['1.22.0']])(
     'generates lock files using yarn v%s',
     async (yarnVersion) => {
       const execSnapshots = mockExecAll(exec, {
@@ -36,6 +37,7 @@ describe(getName(__filename), () => {
       });
       fs.readFile = jest.fn(() => 'package-lock-contents') as never;
       const config = {
+        dockerMapDotfiles: true,
         postUpdateOptions: ['yarnDedupeFewer', 'yarnDedupeHighest'],
       };
       const res = await yarnHelper.generateLockFile('some-dir', {}, config);
@@ -44,7 +46,7 @@ describe(getName(__filename), () => {
       expect(fixSnapshots(execSnapshots)).toMatchSnapshot();
     }
   );
-  it.each([['1.0.0']])(
+  it.each([['1.22.0']])(
     'performs lock file updates using yarn v%s',
     async (yarnVersion) => {
       const execSnapshots = mockExecAll(exec, {
@@ -54,30 +56,15 @@ describe(getName(__filename), () => {
 
       fs.readFile = jest.fn(() => 'package-lock-contents') as never;
       const res = await yarnHelper.generateLockFile('some-dir', {}, {}, [
-        { depName: 'some-dep', isLockfileUpdate: true },
+        {
+          depName: 'some-dep',
+          isLockfileUpdate: true,
+        },
       ]);
       expect(res.lockFile).toEqual('package-lock-contents');
       expect(fixSnapshots(execSnapshots)).toMatchSnapshot();
     }
   );
-  it.each([['1.0.0']])('docker using yarn v%s', async (yarnVersion) => {
-    const execSnapshots = mockExecAll(exec, {
-      stdout: yarnVersion,
-      stderr: '',
-    });
-
-    fs.readFile = jest.fn(() => 'package-lock-contents') as never;
-    const config = {
-      upgrades: [{}],
-      binarySource: BinarySource.Docker,
-      cacheDir: 'dummy/dir',
-    };
-    const res = await yarnHelper.generateLockFile('some-dir', {}, config, [
-      { depName: 'some-dep', isLockfileUpdate: true },
-    ]);
-    expect(res.lockFile).toEqual('package-lock-contents');
-    expect(fixSnapshots(execSnapshots)).toMatchSnapshot();
-  });
   it('catches errors', async () => {
     const execSnapshots = mockExecAll(exec, {
       stdout: '1.9.4',
@@ -90,19 +77,6 @@ describe(getName(__filename), () => {
     expect(fs.readFile).toHaveBeenCalledTimes(1);
     expect(res.error).toBe(true);
     expect(res.lockFile).not.toBeDefined();
-    expect(fixSnapshots(execSnapshots)).toMatchSnapshot();
-  });
-
-  it.each([['1.0.0']])('finds yarn v%s globally', async (yarnVersion) => {
-    const execSnapshots = mockExecAll(exec, {
-      stdout: yarnVersion,
-      stderr: '',
-    });
-
-    fs.readFile = jest.fn(() => 'package-lock-contents') as never;
-    const res = await yarnHelper.generateLockFile('some-dir');
-    expect(fs.readFile).toHaveBeenCalledTimes(1);
-    expect(res.lockFile).toEqual('package-lock-contents');
     expect(fixSnapshots(execSnapshots)).toMatchSnapshot();
   });
 });
