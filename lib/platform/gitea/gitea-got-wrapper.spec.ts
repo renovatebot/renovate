dfcefcc519e529149e0c1e17beef3557a90bbfcb
Sergio Zharinov <sergio.zharinov@gmail.com>
2020-05-30 22:13:23 +0400

    refactor(gitea): Use nock for tests

--- lib/platform/gitea/gitea-got-wrapper.spec.ts
+++ lib/platform/gitea/gitea-got-wrapper.spec.ts
@@ -1,86 +1,62 @@
-import { GotResponse } from '..';
-import { partial } from '../../../test/util';
-import { GotFn } from '../../util/got';
-import { GiteaGotApi } from './gitea-got-wrapper';
+import * as httpMock from '../../../test/httpMock';
+import { api } from './gitea-got-wrapper';
 
 describe('platform/gitea/gitea-got-wrapper', () => {
-  let api: GiteaGotApi;
-  let got: jest.Mocked<GotFn> & jest.Mock;
+  const baseUrl = 'https://gitea.renovatebot.com/api/v1';
 
-  const baseURL = 'https://gitea.renovatebot.com/api/v1';
-
-  beforeEach(async () => {
+  beforeEach(() => {
     jest.resetAllMocks();
-    jest.mock('../../util/got');
 
-    api = (await import('./gitea-got-wrapper')).api as any;
-    got = (await import('../../util/got')).api as any;
-    api.setBaseUrl(baseURL);
+    httpMock.reset();
+    httpMock.setup();
+
+    api.setBaseUrl(baseUrl);
   });
 
   it('supports responses without pagination when enabled', async () => {
-    got.mockResolvedValueOnce(
-      partial<GotResponse>({
-        body: { hello: 'world' },
-      })
-    );
+    httpMock
+      .scope(baseUrl)
+      .get('/pagination-example-1')
+      .reply(200, { hello: 'world' });
 
     const res = await api.get('pagination-example-1', { paginate: true });
     expect(res.body).toEqual({ hello: 'world' });
+    expect(httpMock.getTrace()).toMatchSnapshot();
   });
 
   it('supports root-level pagination', async () => {
-    got.mockResolvedValueOnce(
-      partial<GotResponse>({
-        body: ['abc', 'def', 'ghi'],
-        headers: { 'x-total-count': '5' },
-        url: `${baseURL}/pagination-example-1`,
-      })
-    );
-    got.mockResolvedValueOnce(
-      partial<GotResponse>({
-        body: ['jkl'],
-      })
-    );
-    got.mockResolvedValueOnce(
-      partial<GotResponse>({
-        body: ['mno', 'pqr'],
-      })
-    );
+    httpMock
+      .scope(baseUrl)
+      .get('/pagination-example-1')
+      .reply(200, ['abc', 'def', 'ghi'], { 'x-total-count': '5' })
+      .get('/pagination-example-1?page=2')
+      .reply(200, ['jkl'])
+      .get('/pagination-example-1?page=3')
+      .reply(200, ['mno', 'pqr']);
 
-    const res = await api.get(`${baseURL}/pagination-example-1`, {
+    const res = await api.get(`${baseUrl}/pagination-example-1`, {
       paginate: true,
     });
+    httpMock.getTrace();
 
     expect(res.body).toHaveLength(6);
     expect(res.body).toEqual(['abc', 'def', 'ghi', 'jkl', 'mno', 'pqr']);
-    expect(got.mock.calls).toMatchSnapshot();
-    expect(got).toHaveBeenCalledTimes(3);
+    expect(httpMock.getTrace()).toMatchSnapshot();
   });
 
   it('supports pagination on data property', async () => {
-    got.mockResolvedValueOnce(
-      partial<GotResponse>({
-        body: { data: ['abc', 'def', 'ghi'] },
-        headers: { 'x-total-count': '5' },
-        url: `${baseURL}/pagination-example-2`,
-      })
-    );
-    got.mockResolvedValueOnce(
-      partial<GotResponse>({
-        body: { data: ['jkl'] },
-      })
-    );
-    got.mockResolvedValueOnce(
-      partial<GotResponse>({
-        body: { data: ['mno', 'pqr'] },
-      })
-    );
+    httpMock
+      .scope(baseUrl)
+      .get('/pagination-example-2')
+      .reply(200, { data: ['abc', 'def', 'ghi'] }, { 'x-total-count': '5' })
+      .get('/pagination-example-2?page=2')
+      .reply(200, { data: ['jkl'] })
+      .get('/pagination-example-2?page=3')
+      .reply(200, { data: ['mno', 'pqr'] });
 
     const res = await api.get('pagination-example-2', { paginate: true });
-
     expect(res.body.data).toHaveLength(6);
     expect(res.body.data).toEqual(['abc', 'def', 'ghi', 'jkl', 'mno', 'pqr']);
-    expect(got).toHaveBeenCalledTimes(3);
+    expect(httpMock.getTrace()).toMatchSnapshot();
   });
 });
