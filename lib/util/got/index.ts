b62c3b13f5c3ebc72e218a9d5f5e146ffa371aca
Rhys Arkins <rhys@arkins.net>
2020-06-12 09:16:31 +0200

    refactor: move user agent insertion into util/got

--- lib/util/got/index.ts
+++ lib/util/got/index.ts
@@ -1,7 +1,6 @@
 import auth from './auth';
 import cacheGet from './cache-get';
 import hostRules from './host-rules';
-import renovateAgent from './renovate-agent';
 import { mergeInstances } from './util';
 
 export * from './common';
@@ -12,6 +11,6 @@ export * from './common';
  *  - Cache all GET requests for the lifetime of the repo
  *
  */
-export const api = mergeInstances(cacheGet, renovateAgent, hostRules, auth);
+export const api = mergeInstances(cacheGet, hostRules, auth);
 
 export default api;
