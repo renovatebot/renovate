2f767ea3904c39bec21b474151b6f58a4eb94464
Rhys Arkins <rhys@arkins.net>
2020-06-16 07:40:26 +0200

    test: Revert "test: remove util/got mocks"
    This reverts commit 9b51f0ec2911569dbbf477b51e7288a51fda270b.

--- lib/manager/bazel/update.spec.ts
+++ lib/manager/bazel/update.spec.ts
@@ -5,6 +5,7 @@ import { UpdateType } from '../../config';
 import { updateDependency } from './update';
 
 jest.mock('hasha');
+jest.mock('../../util/got');
 
 const fromStream: jest.Mock<Promise<string>> = _fromStream as any;
 
--- lib/manager/homebrew/update.spec.ts
+++ lib/manager/homebrew/update.spec.ts
@@ -3,6 +3,7 @@ import { fromStream as _fromStream } from 'hasha';
 import { updateDependency } from './update';
 
 jest.mock('hasha');
+jest.mock('../../util/got');
 
 const fromStream: jest.Mock<Promise<string>> = _fromStream as any;
 
