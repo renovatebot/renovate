013c63664b0d83fa8d493edc21d612de074566f1
Rhys Arkins <rhys@arkins.net>
2020-06-24 22:33:27 +0200

    refactor: remove global.renovateCache (#6579)

--- lib/util/cache/global/file.spec.ts
+++ lib/util/cache/global/file.spec.ts
@@ -1,24 +1,25 @@
 import os from 'os';
-import { init } from './file';
+import { get, init, set } from './file';
 
 describe('lib/util/cache/global/file', () => {
-  beforeAll(() => {
-    init(os.tmpdir());
+  it('returns if uninitiated', async () => {
+    await set('test', 'key', 1234);
+    expect(await get('test', 'key')).toBeUndefined();
   });
-
   it('gets null', async () => {
-    expect(
-      await global.renovateCache.get('test', 'missing-key')
-    ).toBeUndefined();
+    init(os.tmpdir());
+    expect(await get('test', 'missing-key')).toBeUndefined();
   });
 
   it('sets and gets', async () => {
-    await global.renovateCache.set('test', 'key', 1234);
-    expect(await global.renovateCache.get('test', 'key')).toBe(1234);
+    init(os.tmpdir());
+    await set('test', 'key', 1234);
+    expect(await get('test', 'key')).toBe(1234);
   });
 
   it('expires', async () => {
-    await global.renovateCache.set('test', 'key', 1234, -5);
-    expect(await global.renovateCache.get('test', 'key')).toBeUndefined();
+    init(os.tmpdir());
+    await set('test', 'key', 1234, -5);
+    expect(await get('test', 'key')).toBeUndefined();
   });
 });
