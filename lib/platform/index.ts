3c3f259aef35c4b5e6a7b3c6a7259268c0a643dc
Rhys Arkins <rhys@arkins.net>
2020-06-27 08:03:46 +0200

    fix: gitAuthor parsing (#6594)

--- lib/platform/index.ts
+++ lib/platform/index.ts
@@ -37,6 +37,48 @@ export function setPlatformApi(name: string): void {
   _platform = platforms.get(name);
 }
 
+interface GitAuthor {
+  name?: string;
+  address?: string;
+}
+
+export function parseGitAuthor(input: string): GitAuthor | null {
+  let result: GitAuthor = null;
+  if (!input) {
+    return null;
+  }
+  try {
+    result = addrs.parseOneAddress(input);
+    if (result) {
+      return result;
+    }
+    if (input.includes('[bot]@')) {
+      // invalid github app/bot addresses
+      const parsed = addrs.parseOneAddress(
+        input.replace('[bot]@', '@')
+      ) as addrs.ParsedMailbox;
+      if (parsed?.address) {
+        result = {
+          name: parsed.name || input.replace(/@.*/, ''),
+          address: parsed.address.replace('@', '[bot]@'),
+        };
+        return result;
+      }
+    }
+    if (input.includes('<') && input.includes('>')) {
+      // try wrapping the name part in quotations
+      result = addrs.parseOneAddress('"' + input.replace(/(\s?<)/, '"$1'));
+      if (result) {
+        return result;
+      }
+    }
+  } catch (err) /* istanbul ignore next */ {
+    logger.error({ err }, 'Unknown error parsing gitAuthor');
+  }
+  // give up
+  return null;
+}
+
 export async function initPlatform(
   config: RenovateConfig
 ): Promise<RenovateConfig> {
@@ -56,12 +98,7 @@ export async function initPlatform(
     logger.debug('Using platform gitAuthor: ' + platformInfo.gitAuthor);
     gitAuthor = platformInfo.gitAuthor;
   }
-  let gitAuthorParsed: addrs.ParsedMailbox | null = null;
-  try {
-    gitAuthorParsed = addrs.parseOneAddress(gitAuthor) as addrs.ParsedMailbox;
-  } catch (err) /* istanbul ignore next */ {
-    logger.debug({ gitAuthor, err }, 'Error parsing gitAuthor');
-  }
+  const gitAuthorParsed = parseGitAuthor(gitAuthor);
   // istanbul ignore if
   if (!gitAuthorParsed) {
     throw new Error('Init: gitAuthor is not parsed as valid RFC5322 format');
