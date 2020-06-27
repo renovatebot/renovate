6ff8a28bb851101f08b92e5409a61a0376a06211
Rhys Arkins <rhys@arkins.net>
2020-06-27 09:30:26 +0200

    remove gitAuthor from globals

--- lib/globals.d.ts
+++ lib/globals.d.ts
@@ -14,7 +14,6 @@ declare interface Error {
 declare namespace NodeJS {
   interface Global {
     appMode?: boolean;
-    gitAuthor?: { name: string; email: string };
 
     trustLevel?: string;
   }
