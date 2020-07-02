f3d920c4546a96a416c57a1c48776b1445ea0695
Sebastian Poxhofer <secustor@users.noreply.github.com>
2020-07-02 19:20:30 +0200

    feat(terraform-provider): Implement terraform helm chart update (#6631)
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/manager/terraform/util.ts
+++ lib/manager/terraform/util.ts
@@ -12,6 +12,7 @@ export enum TerraformDependencyTypes {
   module = 'module',
   provider = 'provider',
   required_providers = 'required_providers',
+  resource = 'resource',
 }
 
 export function getTerraformDependencyType(
@@ -27,6 +28,9 @@ export function getTerraformDependencyType(
     case 'required_providers': {
       return TerraformDependencyTypes.required_providers;
     }
+    case 'resource': {
+      return TerraformDependencyTypes.resource;
+    }
     default: {
       return TerraformDependencyTypes.unknown;
     }
@@ -40,4 +44,10 @@ export function checkFileContainsDependency(
   return checkList.some((check) => {
     return content.includes(check);
   });
+}
+
+const pathStringRegex = /(.|..)?(\/[^/])+/;
+export function checkIfStringIsPath(path: string): boolean {
+  const match = pathStringRegex.exec(path);
+  return !!match;
 }
