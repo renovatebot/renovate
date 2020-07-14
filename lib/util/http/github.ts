76d8574581f862afbff027a53d2f2153f2192ce2
Michael Kriese <michael.kriese@visualon.de>
2020-07-14 09:51:27 +0200

    chore(internal): fix http coverage (#6746)

--- lib/util/http/github.ts
+++ lib/util/http/github.ts
@@ -44,7 +44,7 @@ interface GithubGraphqlResponse<T = unknown> {
   errors?: { message: string; locations: unknown }[];
 }
 
-export function handleGotError(
+function handleGotError(
   err: GotRequestError,
   url: string | URL,
   opts: GithubHttpOptions
@@ -97,7 +97,7 @@ export function handleGotError(
     throw new Error(PLATFORM_INTEGRATION_UNAUTHORIZED);
   }
   if (err.statusCode === 401 && message.includes('Bad credentials')) {
-    const rateLimit = err.headers ? err.headers['x-ratelimit-limit'] : -1;
+    const rateLimit = err.headers?.['x-ratelimit-limit'] ?? -1;
     logger.debug(
       {
         token: maskToken(opts.token),
@@ -191,6 +191,7 @@ export class GithubHttp extends Http<GithubHttpOptions, GithubHttpOptions> {
     try {
       result = await super.request<T>(url, opts);
 
+      // istanbul ignore else: Can result be null ???
       if (result !== null) {
         if (opts.paginate) {
           // Check if result is paginated
@@ -200,6 +201,7 @@ export class GithubHttp extends Http<GithubHttpOptions, GithubHttpOptions> {
             parseLinkHeader(result.headers.link as string);
           if (linkHeader && linkHeader.next && linkHeader.last) {
             let lastPage = +linkHeader.last.page;
+            // istanbul ignore else: needs a test
             if (!process.env.RENOVATE_PAGINATE_ALL && opts.paginate !== 'all') {
               lastPage = Math.min(pageLimit, lastPage);
             }
