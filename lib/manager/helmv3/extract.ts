692abb421f1fb6470360cc77a87c96f52ff2f1a6
chgl <chgl@users.noreply.github.com>
2020-07-22 20:56:32 +0200

    feat: implement support for Helm v3

---
+++ lib/manager/helmv3/extract.ts
@@ -0,0 +1,76 @@
+import is from '@sindresorhus/is';
+import yaml from 'js-yaml';
+import * as datasourceHelm from '../../datasource/helm';
+import { logger } from '../../logger';
+import { SkipReason } from '../../types';
+import { ExtractConfig, PackageDependency, PackageFile } from '../common';
+
+export function extractPackageFile(
+  content: string,
+  fileName: string,
+  config: ExtractConfig
+): PackageFile | null {
+  let chart;
+  try {
+    chart = yaml.safeLoad(content, { json: true });
+    if (!(chart?.apiVersion && chart.name && chart.version)) {
+      logger.debug(
+        { fileName },
+        'Failed to find required fields in Chart.yaml'
+      );
+      return null;
+    }
+    if (chart.apiVersion !== 'v2') {
+      logger.debug(
+        { fileName },
+        'Unsupported Chart apiVersion. Only v2 is supported.'
+      );
+      return null;
+    }
+  } catch (err) {
+    logger.debug({ fileName }, 'Failed to parse helm Chart.yaml');
+    return null;
+  }
+  let deps = [];
+  if (!(chart && is.array(chart.dependencies))) {
+    logger.debug({ fileName }, 'Chart has no dependencies');
+    return null;
+  }
+  deps = chart.dependencies.map((dep) => {
+    const res: PackageDependency = {
+      depName: dep.name,
+      currentValue: dep.version,
+    };
+    if (dep.repository) {
+      res.registryUrls = [dep.repository];
+      if (dep.repository.startsWith('@')) {
+        const repoWithAtRemoved = dep.repository.slice(1);
+        const alias = config.aliases[repoWithAtRemoved];
+        if (alias) {
+          res.registryUrls = [alias];
+          return res;
+        }
+
+        res.skipReason = SkipReason.PlaceholderUrl;
+      } else {
+        try {
+          const url = new URL(dep.repository);
+          if (url.protocol === 'file:') {
+            res.skipReason = SkipReason.LocalDependency;
+          }
+        } catch (err) {
+          logger.debug({ err }, 'Error parsing url');
+          res.skipReason = SkipReason.InvalidUrl;
+        }
+      }
+    } else {
+      res.skipReason = SkipReason.NoRepository;
+    }
+    return res;
+  });
+  const res = {
+    deps,
+    datasource: datasourceHelm.id,
+  };
+  return res;
+}
