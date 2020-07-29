f4510a86c4e56a8063632d8abb184e67d3bdf412
Michael Kriese <michael.kriese@visualon.de>
2020-07-29 13:23:34 +0200

    fix: revert meta change

--- lib/datasource/metadata.ts
+++ lib/datasource/metadata.ts
@@ -34,12 +34,6 @@ const manualChangelogUrls = {
     requests: 'https://github.com/psf/requests/blob/master/HISTORY.md',
     wagtail: 'https://github.com/wagtail/wagtail/tree/master/docs/releases',
   },
-  docker: {
-    'gitlab/gitlab-ce':
-      'https://gitlab.com/gitlab-org/omnibus-gitlab/-/blob/master/CHANGELOG.md',
-    'gitlab/gitlab-runner':
-      'https://gitlab.com/gitlab-org/gitlab-runner/-/blob/master/CHANGELOG.md',
-  },
 };
 
 // Use this object to define manual source URLs for packages
