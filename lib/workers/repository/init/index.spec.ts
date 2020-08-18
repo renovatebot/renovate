f8780143885c5bc05ef4c5862a47a7ed14d10913
Rhys Arkins <rhys@arkins.net>
2020-08-18 14:35:35 +0200

    fix: don’t error when baseBranches are not found
    Log a warning when baseBranches are missing but otherwise just skip over and keep going.
    
    Closes #6669

--- lib/workers/repository/init/index.spec.ts
+++ lib/workers/repository/init/index.spec.ts
@@ -1,6 +1,5 @@
 import { mocked } from '../../../../test/util';
 import * as _apis from './apis';
-import * as _base from './base';
 import * as _config from './config';
 import { initRepo } from '.';
 
@@ -8,18 +7,15 @@ jest.mock('../../../util/git');
 jest.mock('../../../workers/repository/onboarding/branch');
 jest.mock('../../../workers/repository/configured');
 jest.mock('../../../workers/repository/init/apis');
-jest.mock('../../../workers/repository/init/base');
 jest.mock('../../../workers/repository/init/config');
 jest.mock('../../../workers/repository/init/semantic');
 
-const base = mocked(_base);
 const apis = mocked(_apis);
 const config = mocked(_config);
 
 describe('workers/repository/init', () => {
   describe('initRepo', () => {
     it('runs', async () => {
-      base.checkBaseBranch.mockResolvedValue({});
       apis.initApis.mockResolvedValue({} as never);
       config.mergeRenovateConfig.mockResolvedValueOnce({});
       const renovateConfig = await initRepo({});
