d16f66b20995c9c69b70e026397c79a9857a46f7
Michée Lengronne <michee.lengronne@coppint.com>
2020-08-26 17:31:41 +0200

    unit tests problems

--- lib/datasource/rubygems/get.ts
+++ lib/datasource/rubygems/get.ts
@@ -74,6 +74,8 @@ export async function getDependency(
   let releases;
 
   if (testendpoint.statusCode === 404) {
+    logger.debug({ dependency }, '/api/v1/versions does not exist. Using /api/v1/dependencies.json');
+
     // Let's try with the dependencies.json endpoint then
     const versions = (await fetch_deps(dependency, registry, DEPENDENCIES_PATH)) || [];
 
