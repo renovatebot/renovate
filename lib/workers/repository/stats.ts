b7f2503c9a491393f7e83f21d4d1fff4fb903057
Sergio Zharinov <sergio.zharinov@gmail.com>
2020-08-27 14:38:05 +0400

    refactor(http): Prepare util/http for queues

--- lib/workers/repository/stats.ts
+++ lib/workers/repository/stats.ts
@@ -2,6 +2,11 @@ import URL from 'url';
 import { logger } from '../../logger';
 import * as memCache from '../../util/cache/memory';
 
+interface RequestStats {
+  duration: number;
+  queueDuration: number;
+}
+
 export function printRequestStats(): void {
   const httpRequests = memCache.get('http-requests');
   if (!httpRequests) {
@@ -17,14 +22,14 @@ export function printRequestStats(): void {
     return 1;
   });
   const allRequests: string[] = [];
-  const requestHosts: Record<string, number[]> = {};
-  for (const request of httpRequests) {
+  const requestHosts: Record<string, RequestStats[]> = {};
+  for (const { method, url, duration, queueDuration } of httpRequests) {
     allRequests.push(
-      `${request.method.toUpperCase()} ${request.url} ${request.duration}`
+      `${method.toUpperCase()} ${url} ${duration} ${queueDuration}`
     );
-    const { hostname } = URL.parse(request.url);
+    const { hostname } = URL.parse(url);
     requestHosts[hostname] = requestHosts[hostname] || [];
-    requestHosts[hostname].push(request.duration);
+    requestHosts[hostname].push({ duration, queueDuration });
   }
   logger.trace({ allRequests, requestHosts }, 'full stats');
   const hostStats: string[] = [];
@@ -32,11 +37,20 @@ export function printRequestStats(): void {
   for (const [hostname, requests] of Object.entries(requestHosts)) {
     const hostRequests = requests.length;
     totalRequests += hostRequests;
-    const requestSum = requests.reduce((a, b) => a + b, 0);
-    const avg = Math.round(requestSum / hostRequests);
+    const requestSum = requests
+      .map(({ duration }) => duration)
+      .reduce((a, b) => a + b, 0);
+    const requestAvg = Math.round(requestSum / hostRequests);
+
+    const queueSum = requests
+      .map(({ queueDuration }) => queueDuration)
+      .reduce((a, b) => a + b, 0);
+    const queueAvg = Math.round(queueSum / hostRequests);
     const requestCount =
       `${hostRequests} ` + (hostRequests > 1 ? 'requests' : 'request');
-    hostStats.push(`${hostname}, ${requestCount}, ${avg}ms average`);
+    hostStats.push(
+      `${hostname}, ${requestCount}, ${requestAvg}ms request average, ${queueAvg}ms queue average`
+    );
   }
   logger.debug({ hostStats, totalRequests }, 'http statistics');
 }
