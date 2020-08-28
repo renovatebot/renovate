21e1f1ff3edf975a26a4f2d846bc47bdad5ce5fd
Sergio Zharinov <zharinov@users.noreply.github.com>
2020-08-28 17:22:14 +0400

    feat(worker): Enforce limits at PR creation time (#7099)

--- lib/workers/common.ts
+++ lib/workers/common.ts
@@ -72,6 +72,7 @@ export enum PrResult {
   ErrorAlreadyExists = 'ErrorAlreadyExists',
   NotUpdated = 'NotUpdated',
   Updated = 'Updated',
+  LimitReached = 'LimitReached',
 }
 
 export type ProcessBranchResult =
