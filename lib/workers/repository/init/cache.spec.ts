cda4e7c21e6df953eebe6be38dc8dcfe7f27c82f
Rhys Arkins <rhys@arkins.net>
2020-09-01 14:23:28 +0200

    fix: revert config cache

--- lib/workers/repository/init/cache.spec.ts
+++ lib/workers/repository/init/cache.spec.ts
@@ -1,32 +1,14 @@
 import { RenovateConfig, getConfig } from '../../../../test/util';
-import {
-  getResolvedConfig,
-  initializeCaches,
-  setResolvedConfig,
-} from './cache';
+import { initializeCaches } from './cache';
 
 describe('workers/repository/init/cache', () => {
-  describe('getCachedConfig()', () => {
+  describe('initializeCaches()', () => {
     let config: RenovateConfig;
-    beforeEach(async () => {
+    beforeEach(() => {
       config = { ...getConfig() };
-      await initializeCaches(config);
     });
-    it('returns null if no sha provided', () => {
-      expect(getResolvedConfig(null)).toBeNull();
-    });
-    it('returns null if no match', () => {
-      expect(getResolvedConfig('abc123')).toBeNull();
-    });
-    it('returns null if resolved config sha does not match', () => {
-      config.defaultBranchSha = 'abc123';
-      setResolvedConfig(config);
-      expect(getResolvedConfig('def456')).toBeNull();
-    });
-    it('returns matching resolved config', () => {
-      config.defaultBranchSha = 'abc123';
-      setResolvedConfig(config);
-      expect(getResolvedConfig('abc123')).not.toBeNull();
+    it('initializes', async () => {
+      expect(await initializeCaches(config)).toBeUndefined();
     });
   });
 });
