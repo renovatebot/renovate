9d29517c570ce41dafc219c9d36d89cd48dccaa9
Michael Kriese <michael.kriese@visualon.de>
2020-09-04 16:10:16 +0200

    chore: refactor err serializer

--- lib/logger/err-serializer.spec.ts
+++ lib/logger/err-serializer.spec.ts
@@ -1,8 +1,13 @@
+import * as httpMock from '../../test/httpMock';
+import { partial } from '../../test/util';
+import * as hostRules from '../util/host-rules';
+import { Http } from '../util/http';
 import configSerializer from './err-serializer';
+import { sanitizeValue } from './utils';
 
 describe('logger/err-serializer', () => {
   it('expands errors', () => {
-    const err = {
+    const err = partial<Error & Record<string, unknown>>({
       a: 1,
       b: 2,
       message: 'some message',
@@ -10,17 +15,59 @@ describe('logger/err-serializer', () => {
         body: 'some response body',
         url: 'some/path',
       },
-      gotOptions: {
+      options: {
         headers: {
           authorization: 'Bearer abc',
         },
-        auth: 'test:token',
       },
-    };
+    });
     expect(configSerializer(err)).toMatchSnapshot();
   });
   it('handles missing fields', () => {
-    const err = { a: 1, stack: 'foo', body: 'some body' };
+    const err = partial<Error & Record<string, unknown>>({
+      a: 1,
+      stack: 'foo',
+      body: 'some body',
+    });
     expect(configSerializer(err)).toMatchSnapshot();
+  });
+
+  describe('got', () => {
+    const baseUrl = 'https://github.com';
+
+    beforeEach(() => {
+      // reset module
+      jest.resetAllMocks();
+      httpMock.setup();
+      // clean up hostRules
+      hostRules.clear();
+      hostRules.add({
+        hostType: 'any',
+        baseUrl,
+        token: 'token',
+      });
+    });
+    afterEach(() => httpMock.reset());
+
+    it('handles http error', async () => {
+      httpMock
+        .scope(baseUrl)
+        .post('/api')
+        .reply(412, { err: { message: 'failed' } });
+      let err: any;
+      try {
+        await new Http('any').postJson('https://:token@github.com/api');
+      } catch (error) {
+        err = configSerializer(error);
+      }
+
+      expect(httpMock.getTrace()).toMatchSnapshot();
+      expect(err).toBeDefined();
+      delete err.timings;
+      expect(err.response.body).toBeDefined();
+      expect(err.options).toBeDefined();
+      // sanitize like Bunyan
+      expect(sanitizeValue(err)).toMatchSnapshot();
+    });
   });
 });
--- lib/logger/err-serializer.ts
+++ lib/logger/err-serializer.ts
@@ -1,60 +1,47 @@
 import is from '@sindresorhus/is';
+import { RequestError } from 'got';
+import { clone } from '../util/clone';
 
 Error.stackTraceLimit = 20;
 
-interface Err {
-  body?: unknown;
-  response?: {
-    body?: unknown;
-    url?: unknown;
-  };
-  message?: unknown;
-  stack?: unknown;
-  gotOptions?: {
-    auth?: unknown;
-    headers?: unknown;
-  };
-}
-
-export default function errSerializer(err: Err): any {
+export default function errSerializer(err: Error): any {
   const response: Record<string, unknown> = {
     ...err,
   };
-  if (err.body) {
-    response.body = err.body;
-  } else if (err.response?.body) {
-    response.body = err.response.body;
-  }
-  if (err.message) {
+
+  // Can maybe removed?
+  if (!response.message && err.message) {
     response.message = err.message;
   }
-  if (err.stack) {
+
+  // Can maybe removed?
+  if (!response.stack && err.stack) {
     response.stack = err.stack;
   }
-  if (err.response?.url) {
-    response.url = err.response.url;
-  }
-  if (is.plainObject(response.gotOptions)) {
-    if (is.string(response.gotOptions.auth)) {
-      response.gotOptions.auth = response.gotOptions.auth.replace(
-        /:.*/,
-        ':***********'
-      );
+
+  // handle got error
+  if (err instanceof RequestError) {
+    const options: Record<string, unknown> = {
+      headers: clone(err.options.headers),
+      url: err.options.url?.toString(),
+    };
+    response.options = options;
+
+    for (const k of ['username', 'password', 'method', 'http2']) {
+      options[k] = err.options[k];
     }
-    if (is.plainObject(response.gotOptions.headers)) {
-      const headers = response.gotOptions.headers;
-      const redactedHeaders = [
-        'authorization',
-        'private-header',
-        'Private-header',
-      ];
-      redactedHeaders.forEach((header) => {
-        if (headers[header]) {
-          headers[header] = '** redacted **';
-        }
-      });
+
+    if (err.response) {
+      response.response = {
+        statusCode: err.response?.statusCode,
+        statusMessage: err.response?.statusMessage,
+        body: clone(err.response.body),
+        headers: clone(err.response.headers),
+      };
     }
   }
+
+  // already done by `sanitizeValue` ?
   const redactedFields = ['message', 'stack', 'stdout', 'stderr'];
   for (const field of redactedFields) {
     const val = response[field];
