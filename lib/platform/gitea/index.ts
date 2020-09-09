18c1c0b7cb6568e983f5315435dcfc8116938467
Sergio Zharinov <sergio.zharinov@gmail.com>
2020-09-09 14:10:57 +0400

    fix(gitea): Return only PRs created by Renovate

--- lib/platform/gitea/index.ts
+++ lib/platform/gitea/index.ts
@@ -57,6 +57,7 @@ const defaultConfigFile = configFileNames[0];
 
 let config: GiteaRepoConfig = {} as any;
 let botUserID: number;
+let botUserName: string;
 
 function toRenovateIssue(data: helper.Issue): Issue {
   return {
@@ -81,6 +82,11 @@ function toRenovatePR(data: helper.PR): Pr | null {
     logger.trace(
       `Skipping Pull Request #${data.number} due to missing base and/or head branch`
     );
+    return null;
+  }
+
+  const createdBy = data.user?.username;
+  if (createdBy && botUserName && createdBy !== botUserName) {
     return null;
   }
 
@@ -199,6 +205,7 @@ const platform: Platform = {
       const user = await helper.getCurrentUser({ token });
       gitAuthor = `${user.full_name || user.username} <${user.email}>`;
       botUserID = user.id;
+      botUserName = user.username;
     } catch (err) {
       logger.debug(
         { err },
