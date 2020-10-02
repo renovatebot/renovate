7f41d65f1cebf899bb56dcb3e03d3a1d6e95c850
Rhys Arkins <rhys@arkins.net>
2020-10-02 08:46:02 +0200

    move functions

--- lib/workers/repository/init/apis.ts
+++ lib/workers/repository/init/apis.ts
@@ -15,9 +15,9 @@ export type WorkerPlatformConfig = RepoResult &
 const defaultConfigFile = configFileNames[0];
 
 async function validateOptimizeForDisabled(
-  repoParams: RepoParams
+  config: RenovateConfig
 ): Promise<void> {
-  if (repoParams.optimizeForDisabled) {
+  if (config.optimizeForDisabled) {
     const renovateConfig = await platform.getJsonFile(defaultConfigFile);
     if (renovateConfig?.enabled === false) {
       throw new Error(REPOSITORY_DISABLED);
@@ -25,11 +25,8 @@ async function validateOptimizeForDisabled(
   }
 }
 
-async function validateIncludeForks(
-  repoParams: RepoParams,
-  repoResult: RepoResult
-): Promise<void> {
-  if (!repoParams.includeForks && repoResult.isFork) {
+async function validateIncludeForks(config: RenovateConfig): Promise<void> {
+  if (!config.includeForks && config.isFork) {
     const renovateConfig = await platform.getJsonFile(defaultConfigFile);
     if (!renovateConfig?.includeForks) {
       throw new Error(REPOSITORY_FORKED);
@@ -42,8 +39,6 @@ async function getPlatformConfig(
   config: RepoParams
 ): Promise<WorkerPlatformConfig> {
   const platformConfig = await platform.initRepo(config);
-  await validateOptimizeForDisabled(config);
-  await validateIncludeForks(config, platformConfig);
   return {
     ...config,
     ...platformConfig,
@@ -56,6 +51,8 @@ export async function initApis(
 ): Promise<WorkerPlatformConfig> {
   let config: WorkerPlatformConfig = { ...input } as never;
   config = await getPlatformConfig(config as never);
+  await validateOptimizeForDisabled(config);
+  await validateIncludeForks(config);
   npmApi.resetMemCache();
   npmApi.setNpmrc(config.npmrc);
   return config;
