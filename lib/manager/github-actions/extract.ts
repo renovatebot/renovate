3957efb8d79a8337db58cb9dda514aef20ef2030
Richard Simpson <rsimpson@uship.com>
2020-10-08 23:36:44 -0500

    feat: add github-actions tag support

--- lib/manager/github-actions/extract.ts
+++ lib/manager/github-actions/extract.ts
@@ -1,5 +1,7 @@
+import * as githubTagsDatasource from '../../datasource/github-tags';
 import { logger } from '../../logger';
 import * as dockerVersioning from '../../versioning/docker';
+import * as semverVersioning from '../../versioning/semver';
 import { PackageDependency, PackageFile } from '../common';
 import { getDep } from '../dockerfile/extract';
 
@@ -7,13 +9,13 @@ export function extractPackageFile(content: string): PackageFile | null {
   logger.debug('github-actions.extractPackageFile()');
   const deps: PackageDependency[] = [];
   for (const line of content.split('\n')) {
-    // old github actions syntax will be deprecated on September 30, 2019
-    // after that, the first line can be removed
-    const match =
-      /^\s+uses = "docker:\/\/([^"]+)"\s*$/.exec(line) ||
-      /^\s+uses: docker:\/\/([^"]+)\s*$/.exec(line);
-    if (match) {
-      const [, currentFrom] = match;
+    if (line.trim().startsWith('#')) {
+      continue; // eslint-disable-line no-continue
+    }
+
+    const dockerMatch = /^\s+uses: docker:\/\/([^"]+)\s*$/.exec(line);
+    if (dockerMatch) {
+      const [, currentFrom] = dockerMatch;
       const dep = getDep(currentFrom);
       logger.debug(
         {
@@ -21,10 +23,28 @@ export function extractPackageFile(content: string): PackageFile | null {
           currentValue: dep.currentValue,
           currentDigest: dep.currentDigest,
         },
-        'Docker image inside GitHub Actions'
+        'Docker image inside GitHub Workflow'
       );
       dep.versioning = dockerVersioning.id;
       deps.push(dep);
+      continue; // eslint-disable-line no-continue
+    }
+
+    const tagMatch = /^\s+-?\s+?uses: (?<depName>[a-z-/]+?)@(?<currentValue>.+?)\s*?$/.exec(
+      line
+    );
+    if (tagMatch) {
+      const { depName, currentValue } = tagMatch.groups;
+      // Only allow tagged versions for now
+      if (semverVersioning.isVersion(currentValue)) {
+        const dep = {
+          depName,
+          currentValue,
+          datasource: githubTagsDatasource.id,
+        };
+        logger.debug(dep, 'GitHub Action inside GitHub Workflow');
+        deps.push(dep);
+      }
     }
   }
   if (!deps.length) {
