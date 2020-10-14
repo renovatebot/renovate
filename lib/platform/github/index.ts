d858d85205cc99d7ecf617754612d2a17ff4eda0
Rhys Arkins <rhys@arkins.net>
2020-10-14 08:50:51 +0200

    logs: trace ensureIssueClosing

--- lib/platform/github/index.ts
+++ lib/platform/github/index.ts
@@ -1111,7 +1111,7 @@ export async function ensureIssue({
 }
 
 export async function ensureIssueClosing(title: string): Promise<void> {
-  logger.debug(`ensureIssueClosing(${title})`);
+  logger.trace(`ensureIssueClosing(${title})`);
   const issueList = await getIssueList();
   for (const issue of issueList) {
     if (issue.state === 'open' && issue.title === title) {
