af3f5ee084f31f4e5aa4d02c9f104cc241689544
Michael Kriese <michael.kriese@visualon.de>
2020-10-27 17:56:32 +0100

    fix: add simple generator

--- docs/website/tools/generate.ts
+++ docs/website/tools/generate.ts
@@ -1,11 +1,14 @@
-import { setUncaughtExceptionCaptureCallback } from 'process';
+// import fs from 'fs-extra';
 import shell from 'shelljs';
 
 shell.echo('generating docs');
 shell.echo('===============');
 
-shell.echo('copy assets');
-shell.cp('-r', '../usage/assets', 'static/');
+// shell.echo('copy assets');
+shell.cp('-r', '../usage/*', 'docs/');
 
-shell.echo('copy usage docs');
-shell.cp('-r', '../usage/**/*.md', 'docs/');
+// shell.echo('copy usage docs');
+// shell.ls('../usage/**/*.md').forEach(f => {
+//   const tgt = `./docs/${f.substr(9)}`
+//   fs.copySync(f, tgt);
+// })
