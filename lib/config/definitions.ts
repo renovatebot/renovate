3dc0aa9ce6cddd50ffae2a020b2e261656e04657
Stefan <5585744+casdevs@users.noreply.github.com>
2020-11-02 09:10:07 +0100

    Update definitions.ts
    I noticed that `updateTypes: ['patch']` only matches if `separateMinorPatch` is set to true.
    This comes somewhat unexpected and does not seem to be documented anywhere at the moment, as opposed to the documentation of the `patch` setting, where the docs correctly state `Only applies if separateMinorPatch is set to true`.
    
    Maybe related to https://github.com/renovatebot/renovate/issues/2818
    
    But without reading the docs, I would expect `updateTypes: ['patch']` to always match on patch updates (e.g. 1.0.0 to 1.0.1) regardless of the `separateMinorPatch` setting (which is only set if we want separate PRs or branches for patch AND minor updates)

--- lib/config/definitions.ts
+++ lib/config/definitions.ts
@@ -900,7 +900,7 @@ const options: RenovateOptions[] = [
   {
     name: 'updateTypes',
     description:
-      'Update types to match against (major, minor, pin, etc). Valid only within `packageRules` object.',
+      'Update types to match against (major, minor, pin, etc). Valid only within `packageRules` object. `patch` only matches if `separateMinorPatch` is set to true',
     type: 'array',
     // TODO: add allowedValues
     subType: 'string',
