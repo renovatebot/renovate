035e24580172defd6d11adb5822bcd3995677856
Gerben Oostra <gerben.oostra@gmail.com>
2020-11-05 21:20:38 +0100

    Changed debug statement to print the full repository url in detect datasource

--- lib/manager/pre-commit/extract.ts
+++ lib/manager/pre-commit/extract.ts
@@ -12,20 +12,76 @@ import {
   matchesPrecommitGitHeuristic,
 } from './util';
 
-function detectDatasource(hostName: string): { [key: string]: string } {
+function determineDatasource(
+  repository: string,
+  hostName: string
+): { [key: string]: string } {
   let datasource;
   let skipReason;
   if (hostName.endsWith('github.com')) {
     datasource = githubTagsId;
-    logger.debug({ hostName }, 'Found github dependency');
+    logger.debug({ repository }, 'Found github dependency');
   } else if (hostName.endsWith('gitlab.com')) {
     datasource = gitlabTagsId;
-    logger.debug({ hostName }, 'Found gitlab dependency');
+    logger.debug({ repository }, 'Found gitlab dependency');
   } else {
-    logger.debug({ hostName }, 'No datasource specified for hostname');
+    logger.debug({ repository }, 'No datasource specified for hostname');
     skipReason = SkipReason.UnsupportedUrl;
   }
   return { datasource, skipReason };
+}
+
+function extractDependency(
+  tag: string,
+  repository: string
+): {
+  depName: any;
+  depType: string;
+  datasource: any;
+  lookupName: any;
+  skipReason: any;
+  currentValue: string;
+} {
+  let skipReason;
+  let datasource;
+  let lookupName;
+  const currentValue = tag;
+  logger.debug({ tag }, 'Found version');
+
+  const urlMatchers = [
+    // This splits "http://github.com/user/repo" -> "http://github" ".com" "user/repo
+    /^(?<hostNamePrefix>.*)(?<domain>\.\w+)\/(?<depName>\S*)*/,
+    // This splits "git@hostNamePrefix.com:user/repo" -> "hostNamePrefix" ".com" "user/repo
+    /^git@(?<hostNamePrefix>.*)(?<domain>\.\w+):(?<depName>\S*)*/,
+  ];
+  let matched = false;
+  for (const urlMatcher of urlMatchers) {
+    const match = urlMatcher.exec(repository);
+    if (match) {
+      const { hostNamePrefix, domain, depName } = match.groups;
+      const hostName = hostNamePrefix.concat(domain);
+      lookupName = depName;
+      const sourceDef = determineDatasource(repository, hostName);
+      datasource = sourceDef.datasource;
+      skipReason = sourceDef.skipReason;
+      matched = true;
+      break;
+    }
+  }
+  if (!matched) {
+    logger.info({ repository }, 'Could not separate host from lookup name.');
+    skipReason = SkipReason.InvalidUrl;
+  }
+
+  const dep = {
+    depName: lookupName,
+    depType: 'repository',
+    datasource,
+    lookupName,
+    skipReason,
+    currentValue,
+  };
+  return dep;
 }
 
 /**
@@ -49,49 +105,7 @@ function findDependencies(
         logger.trace(item, 'Matched pre-commit repo spec');
         const repository = String(item.repo);
         const tag = String(item.rev);
-
-        let skipReason;
-        let datasource;
-        let lookupName;
-        const currentValue = tag;
-        logger.debug({ tag }, 'Found version');
-
-        const urlMatchers = [
-          // This splits "http://github.com/user/repo" -> "http://github" ".com" "user/repo
-          /^(?<hostNamePrefix>.*)(?<domain>\.\w+)\/(?<depName>\S*)*/,
-          // This splits "git@hostNamePrefix.com:user/repo" -> "hostNamePrefix" ".com" "user/repo
-          /^git@(?<hostNamePrefix>.*)(?<domain>\.\w+):(?<depName>\S*)*/,
-        ];
-        let matched = false;
-        for (const urlMatcher of urlMatchers) {
-          const match = urlMatcher.exec(repository);
-          if (match) {
-            const { hostNamePrefix, domain, depName } = match.groups;
-            const hostName = hostNamePrefix.concat(domain);
-            lookupName = depName;
-            const sourceDef = detectDatasource(hostName);
-            datasource = sourceDef.datasource;
-            skipReason = sourceDef.skipReason;
-            matched = true;
-            break;
-          }
-        }
-        if (!matched) {
-          logger.info(
-            { repository },
-            'Could not separate host from lookup name.'
-          );
-          skipReason = SkipReason.InvalidUrl;
-        }
-
-        const dep = {
-          depName: lookupName,
-          depType: 'repository',
-          datasource,
-          lookupName,
-          skipReason,
-          currentValue,
-        };
+        const dep = extractDependency(tag, repository);
 
         packageDependencies.push(dep);
       } else {
