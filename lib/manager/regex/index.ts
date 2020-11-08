176ffbbe751f9ec3247a1a86267e6456483a7753
sebastian.poxhofer <sebastian.poxhofer@itsdone.at>
2020-11-08 22:21:23 +0100

    chore(regex): prepare strategy implementation

--- lib/manager/regex/index.ts
+++ lib/manager/regex/index.ts
@@ -2,61 +2,93 @@ import url from 'url';
 import { logger } from '../../logger';
 import { regEx } from '../../util/regex';
 import * as template from '../../util/template';
-import { CustomExtractConfig, PackageFile, Result } from '../common';
+import {
+  CustomExtractConfig,
+  PackageDependency,
+  PackageFile,
+  Result,
+} from '../common';
 
 export const defaultConfig = {
   pinDigests: false,
 };
+
+function regexMatchAll(regex: RegExp, content: string): RegExpMatchArray[] {
+  const matches: RegExpMatchArray[] = [];
+  let matchResult;
+  do {
+    matchResult = regex.exec(content);
+    if (matchResult) {
+      matches.push(matchResult);
+    }
+  } while (matchResult);
+  return matches;
+}
+
+function createDependency(
+  matchResult: RegExpMatchArray,
+  config: CustomExtractConfig,
+  dep?: PackageDependency
+): PackageDependency {
+  const dependency = dep || {};
+  const { groups } = matchResult;
+  const fields = [
+    'depName',
+    'lookupName',
+    'currentValue',
+    'currentDigest',
+    'datasource',
+    'versioning',
+    'registryUrl',
+  ];
+  for (const field of fields) {
+    const fieldTemplate = `${field}Template`;
+    if (config[fieldTemplate]) {
+      try {
+        dependency[field] = template.compile(config[fieldTemplate], groups);
+      } catch (err) {
+        logger.warn(
+          { template: config[fieldTemplate] },
+          'Error compiling template for custom manager'
+        );
+        return null;
+      }
+    } else if (groups[field]) {
+      dependency[field] = groups[field];
+    }
+  }
+  dependency.replaceString = String(matchResult[0]);
+  if (dependency.registryUrl) {
+    if (url.parse(dep.registryUrl)) {
+      dependency.registryUrls = [dependency.registryUrl];
+    }
+    delete dependency.registryUrl;
+  }
+  return dependency;
+}
+
+function handleFlat(
+  content: string,
+  packageFile: string,
+  config: CustomExtractConfig
+): PackageDependency[] {
+  return config.matchStrings
+    .map((matchString) => regEx(matchString, 'g'))
+    .flatMap((regex) => regexMatchAll(regex, content)) // match all regex to content, get all matches, reduce to single array
+    .map((matchResult) => createDependency(matchResult, config));
+}
 
 export function extractPackageFile(
   content: string,
   packageFile: string,
   config: CustomExtractConfig
 ): Result<PackageFile | null> {
-  const regexMatch = regEx(config.matchStrings[0], 'g');
-  const deps = [];
-  let matchResult;
-  do {
-    matchResult = regexMatch.exec(content);
-    if (matchResult) {
-      const dep: any = {};
-      const { groups } = matchResult;
-      const fields = [
-        'depName',
-        'lookupName',
-        'currentValue',
-        'currentDigest',
-        'datasource',
-        'versioning',
-        'registryUrl',
-      ];
-      for (const field of fields) {
-        const fieldTemplate = `${field}Template`;
-        if (config[fieldTemplate]) {
-          try {
-            dep[field] = template.compile(config[fieldTemplate], groups);
-          } catch (err) {
-            logger.warn(
-              { template: config[fieldTemplate] },
-              'Error compiling template for custom manager'
-            );
-            return null;
-          }
-        } else if (groups[field]) {
-          dep[field] = groups[field];
-        }
-      }
-      dep.replaceString = String(matchResult[0]);
-      if (dep.registryUrl) {
-        if (url.parse(dep.registryUrl)) {
-          dep.registryUrls = [dep.registryUrl];
-        }
-        delete dep.registryUrl;
-      }
-      deps.push(dep);
-    }
-  } while (matchResult);
-  if (deps.length) {
+  let deps = handleFlat(content, packageFile, config);
+
+  deps = deps.filter((dep) => {
+    return dep != null;
+  });
+  if (deps?.length) {
     return { deps, matchStrings: config.matchStrings };
   }
   return null;
