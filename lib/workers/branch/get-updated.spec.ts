1e5ce2adae6aaf818d5d11529d27e8dea448f826
Mike Bryant <m@omg.network>
2020-11-09 17:27:46 +0000

    Fixing coverage

--- lib/workers/branch/get-updated.spec.ts
+++ lib/workers/branch/get-updated.spec.ts
@@ -2,6 +2,7 @@ import { defaultConfig, git, mocked } from '../../../test/util';
 import * as datasourceGitSubmodules from '../../datasource/git-submodules';
 import * as _composer from '../../manager/composer';
 import * as _gitSubmodules from '../../manager/git-submodules';
+import * as _helmv3 from '../../manager/helmv3';
 import * as _npm from '../../manager/npm';
 import { BranchConfig } from '../common';
 import * as _autoReplace from './auto-replace';
@@ -9,10 +10,12 @@ import { getUpdatedPackageFiles } from './get-updated';
 
 const composer = mocked(_composer);
 const gitSubmodules = mocked(_gitSubmodules);
+const helmv3 = mocked(_helmv3);
 const npm = mocked(_npm);
 const autoReplace = mocked(_autoReplace);
 
 jest.mock('../../manager/composer');
+jest.mock('../../manager/helmv3');
 jest.mock('../../manager/npm');
 jest.mock('../../manager/git-submodules');
 jest.mock('../../util/git');
@@ -166,6 +169,17 @@ describe('workers/branch/get-updated', () => {
           },
         },
       ]);
+      const res = await getUpdatedPackageFiles(config);
+      expect(res).toMatchSnapshot();
+    });
+    it('bumps versions in autoReplace managers', async () => {
+      config.upgrades.push({
+        branchName: undefined,
+        bumpVersion: true,
+        manager: 'helmv3',
+      });
+      autoReplace.doAutoReplace.mockResolvedValueOnce('version: 0.0.1');
+      helmv3.bumpPackageVersion.mockResolvedValueOnce('version: 0.0.2');
       const res = await getUpdatedPackageFiles(config);
       expect(res).toMatchSnapshot();
     });
