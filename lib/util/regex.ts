ef4adac3497ad9a5177fea6e73e5f44ca18816c0
Sergio Zharinov <sergio.zharinov@gmail.com>
2020-11-09 18:44:43 +0300

    refactor(config): Generalize regex handling used for allowedVersions

--- lib/util/regex.ts
+++ lib/util/regex.ts
@@ -1,3 +1,4 @@
+import is from '@sindresorhus/is';
 import { CONFIG_VALIDATION } from '../constants/error-messages';
 import { logger } from '../logger';
 
@@ -28,4 +29,43 @@ export function regEx(pattern: string, flags?: string): RegExp {
 
 export function escapeRegExp(input: string): string {
   return input.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+const posConfigValStart = /^\s*\//;
+const negConfigValStart = /^\s*!\s*\//;
+const configValEnd = /\/\s*$/;
+
+export function isConfigRegex(input: unknown): input is string {
+  return (
+    is.string(input) &&
+    (posConfigValStart.test(input) || negConfigValStart.test(input)) &&
+    configValEnd.test(input)
+  );
+}
+
+function parseConfigRegex(input: string): RegExp | null {
+  try {
+    const regexString = input
+      .replace(posConfigValStart, '')
+      .replace(negConfigValStart, '')
+      .replace(configValEnd, '');
+    return regEx(regexString);
+  } catch (err) {
+    // no-op
+  }
+  return null;
+}
+
+type ConfigRegexPredicate = (string) => boolean;
+
+export function configRegexPredicate(input: string): ConfigRegexPredicate {
+  const configRegex = parseConfigRegex(input);
+  if (configRegex) {
+    const isPositive = posConfigValStart.test(input);
+    return (x: string): boolean => {
+      const res = configRegex.test(x);
+      return isPositive ? res : !res;
+    };
+  }
+  return null;
 }
