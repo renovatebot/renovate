0a5f877eb6ee0c323433f456213ce797255785e6
Bogdan Alecu <bogdalecu@gmail.com>
2020-11-10 13:15:04 +0000

    fix: prettier fix

--- lib/manager/terragrunt/index.ts
+++ lib/manager/terragrunt/index.ts
@@ -3,8 +3,7 @@ import * as hashicorpVersioning from '../../versioning/hashicorp';
 export { extractPackageFile } from './extract';
 
 export const defaultConfig = {
-  commitMessageTopic:
-    'Terragrunt dependency {{depNameShort}}',
+  commitMessageTopic: 'Terragrunt dependency {{depNameShort}}',
   fileMatch: ['\\.hcl$'],
   versioning: hashicorpVersioning.id,
 };
