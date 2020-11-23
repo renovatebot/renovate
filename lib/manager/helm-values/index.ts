d7d4d3ae1099b11564782a0cd6fc43b971d18267
Michael Kriese <michael.kriese@visualon.de>
2020-11-23 16:13:45 +0100

    fix: use explicit versionig

--- lib/manager/helm-values/index.ts
+++ lib/manager/helm-values/index.ts
@@ -1,8 +1,4 @@
-import { LANGUAGE_DOCKER } from '../../constants/languages';
-
 export { extractPackageFile } from './extract';
-
-export const language = LANGUAGE_DOCKER;
 
 export const defaultConfig = {
   commitMessageTopic: 'helm values {{depName}}',
