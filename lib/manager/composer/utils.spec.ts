c4ffb0d3e46ae8e73ce59c2b39ad0159d1f22cdc
Michael Kriese <michael.kriese@visualon.de>
2020-11-26 08:13:55 +0100

    chore: fix tests

--- lib/manager/composer/utils.spec.ts
+++ lib/manager/composer/utils.spec.ts
@@ -1,32 +1,19 @@
-import { getName, partial } from '../../../test/util';
-import { UpdateArtifact } from '../common';
+import { getName } from '../../../test/util';
 import { extractContraints, getConstraint } from './utils';
 
 describe(getName(__filename), () => {
   describe('getConstraint', () => {
-    const config = partial<UpdateArtifact>({
-      packageFileName: 'composer.json',
-      config: {},
-    });
-
     it('returns from config', () => {
-      expect(
-        getConstraint({
-          ...config,
-          config: { constraints: { composer: '1.1.0' } },
-        })
-      ).toEqual('1.1.0');
+      expect(getConstraint({ constraints: { composer: '1.1.0' } })).toEqual(
+        '1.1.0'
+      );
     });
 
     it('returns from null', () => {
-      expect(
-        getConstraint({
-          ...config,
-          newPackageFileContent: JSON.stringify({}),
-        })
-      ).toBeNull();
+      expect(getConstraint({})).toBeNull();
     });
   });
+
   describe('extractContraints', () => {
     it('returns from require', () => {
       expect(
