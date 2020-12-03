e287195c2616813512b5a5c9482d1d42a638163d
Rhys Arkins <rhys@arkins.net>
2020-12-03 08:33:36 +0100

    Update lib/manager/gradle-lite/index.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/manager/gradle-lite/index.ts
+++ lib/manager/gradle-lite/index.ts
@@ -6,4 +6,5 @@ export { updateDependency } from './update';
 export const defaultConfig = {
   fileMatch: ['(^|/)gradle.properties$', '\\.gradle(\\.kts)?$'],
   versioning: gradleVersioning.id,
+  enabled: false,
 };
