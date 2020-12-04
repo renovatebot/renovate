c2bc70a5d8f96f8039629ff9e920b363168f5db6
ruffoa <ruffo.alex@gmail.com>
2020-12-04 11:32:17 -0500

    Fix bad fix

--- lib/workers/repository/init/apis.spec.ts
+++ lib/workers/repository/init/apis.spec.ts
@@ -89,7 +89,7 @@ describe('workers/repository/init/apis', () => {
         onboardingConfigFileName: 'foo.bar',
       });
       expect(workerPlatformConfig).toBeTruthy();
-      expect(workerPlatformConfig.onboardingConfigFileName).toBe('foo.bar');
+      expect(workerPlatformConfig.onboardingConfigFileName).toBe(undefined);
     });
   });
 });
