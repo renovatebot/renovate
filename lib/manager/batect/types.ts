eb07a3a22512576e875dd3d96d8ecb8b5ed6b56a
Charles Korn <me@charleskorn.com>
2020-12-13 09:19:59 +1100

    Add support for updating Batect bundle references.

--- lib/manager/batect/types.ts
+++ lib/manager/batect/types.ts
@@ -1,7 +1,20 @@
 export interface BatectConfig {
   containers?: Record<string, BatectContainer>;
+  include?: BatectInclude[];
 }
 
 export interface BatectContainer {
   image?: string;
+}
+
+export type BatectInclude = string | BatectFileInclude | BatectGitInclude;
+
+export interface BatectFileInclude {
+  type: 'file';
+}
+
+export interface BatectGitInclude {
+  type: 'git';
+  repo: string;
+  ref: string;
 }
