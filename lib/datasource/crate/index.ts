1ae082212deaac9b356499cd6abca761c117f50f
Amos Wenger <amoswenger@gmail.com>
2020-12-18 22:55:03 +0100

    crate datasource: read data from http or cloned repository

--- lib/datasource/crate/index.ts
+++ lib/datasource/crate/index.ts
@@ -1,10 +1,11 @@
 import { ExternalHostError } from '../../types/errors/external-host-error';
+import * as fs from 'fs-extra';
 import * as packageCache from '../../util/cache/package';
 import { Http } from '../../util/http';
 import { GetReleasesConfig, Release, ReleaseResult } from '../common';
-import { logger } from '../../logger';
 import { ensureCacheDir } from '../../util/fs';
-import Git, { SimpleGitTaskCallback } from 'simple-git';
+import Git from 'simple-git';
+import { join } from 'path';
 
 export const id = 'crate';
 
@@ -17,22 +18,20 @@ const http = new Http(id);
 const CRATES_IO_BASE_URL =
   'https://raw.githubusercontent.com/rust-lang/crates.io-index/master/';
 
-export function getIndexSuffix(lookupName: string): string {
+export function getIndexSuffix(lookupName: string): string[] {
   const len = lookupName.length;
 
   if (len === 1) {
-    return '1/' + lookupName;
+    return ['1', lookupName];
   }
   if (len === 2) {
-    return '2/' + lookupName;
+    return ['2', lookupName];
   }
   if (len === 3) {
-    return '3/' + lookupName[0] + '/' + lookupName;
+    return ['3', lookupName[0], lookupName];
   }
 
-  return (
-    lookupName.slice(0, 2) + '/' + lookupName.slice(2, 4) + '/' + lookupName
-  );
+  return [lookupName.slice(0, 2), lookupName.slice(2, 4), lookupName];
 }
 
 interface CrateRecord {
@@ -62,62 +61,36 @@ export async function getReleases({
   let registryInfo = await fetchRegistryInfo(registryUrl);
   console.log(`registryInfo: ${JSON.stringify(registryInfo, null, 2)}`);
 
-  const crateUrl = CRATES_IO_BASE_URL + getIndexSuffix(lookupName);
-  let dependencyUrl = `https://crates.io/crates/${lookupName}`;
-  let parsedRegistryUrl: URL | undefined;
-  try {
-    parsedRegistryUrl = new URL(registryUrl);
-  } catch (err) {
-    logger.debug({ err }, 'failed to parse crate registry URL');
-  }
+  let dependencyUrl = getDependencyUrl(registryInfo, lookupName);
 
-  if (parsedRegistryUrl) {
-    if (parsedRegistryUrl.hostname == 'dl.cloudsmith.io') {
-      // input: https://dl.cloudsmith.io/basic/$org/$repo/cargo/index.git
-      // output: https://cloudsmith.io/~$org/repos/$repo/packages/detail/cargo/$package/
-      let tokens = parsedRegistryUrl.pathname.split('/');
-      let org = tokens[2];
-      let repo = tokens[3];
-      dependencyUrl = `https://cloudsmith.io/~${org}/repos/${repo}/packages/detail/cargo/${lookupName}`;
-    }
+  let payload = await fetchCrateRecordsPayload(registryInfo, lookupName);
+  console.log(`payload =\n${payload}`);
+  const lines = payload
+    .split('\n') // break into lines
+    .map((line) => line.trim()) // remove whitespace
+    .filter((line) => line.length !== 0) // remove empty lines
+    .map((line) => JSON.parse(line) as CrateRecord); // parse
+  const result: ReleaseResult = {
+    dependencyUrl,
+    releases: [],
+  };
+  result.releases = lines
+    .map((version) => {
+      const release: Release = {
+        version: version.vers,
+      };
+      if (version.yanked) {
+        release.isDeprecated = true;
+      }
+      return release;
+    })
+    .filter((release) => release.version);
+  if (!result.releases.length) {
+    return null;
   }
-
-  try {
-    const lines = (await http.get(crateUrl)).body
-      .split('\n') // break into lines
-      .map((line) => line.trim()) // remove whitespace
-      .filter((line) => line.length !== 0) // remove empty lines
-      .map((line) => JSON.parse(line) as CrateRecord); // parse
-    const result: ReleaseResult = {
-      dependencyUrl,
-      releases: [],
-    };
-    result.releases = lines
-      .map((version) => {
-        const release: Release = {
-          version: version.vers,
-        };
-        if (version.yanked) {
-          release.isDeprecated = true;
-        }
-        return release;
-      })
-      .filter((release) => release.version);
-    if (!result.releases.length) {
-      return null;
-    }
-    const cacheMinutes = 10;
-    await packageCache.set(cacheNamespace, cacheKey, result, cacheMinutes);
-    return result;
-  } catch (err) {
-    if (
-      err.statusCode === 429 ||
-      (err.statusCode >= 500 && err.statusCode < 600)
-    ) {
-      throw new ExternalHostError(err);
-    }
-    throw err;
-  }
+  const cacheMinutes = 10;
+  await packageCache.set(cacheNamespace, cacheKey, result, cacheMinutes);
+  return result;
 }
 
 enum RegistryFlavor {
@@ -189,4 +162,43 @@ function defaultRegistry(): RegistryInfo {
   return {
     flavor: RegistryFlavor.CratesIo,
   };
+}
+
+/// Computes the dependency URL for a crate, given
+/// registry informatoin
+function getDependencyUrl(info: RegistryInfo, lookupName: string): string {
+  switch (info.flavor) {
+    case RegistryFlavor.CratesIo:
+      return `https://crates.io/crates/${lookupName}`;
+    case RegistryFlavor.Cloudsmith:
+      // input: https://dl.cloudsmith.io/basic/$org/$repo/cargo/index.git
+      let tokens = info.url.pathname.split('/');
+      let org = tokens[2];
+      let repo = tokens[3];
+      return `https://cloudsmith.io/~${org}/repos/${repo}/packages/detail/cargo/${lookupName}`;
+    default:
+      return `${info.rawUrl}/${lookupName}`;
+  }
+}
+
+async function fetchCrateRecordsPayload(
+  info: RegistryInfo,
+  lookupName: string
+): Promise<string> {
+  if (info.clonePath) {
+    let path = join(info.clonePath, ...getIndexSuffix(lookupName));
+    return await fs.readFile(path, { encoding: 'utf8' });
+  } else {
+    const crateUrl = CRATES_IO_BASE_URL + getIndexSuffix(lookupName).join('/');
+    try {
+      return (await http.get(crateUrl)).body;
+    } catch (err) {
+      if (
+        err.statusCode === 429 ||
+        (err.statusCode >= 500 && err.statusCode < 600)
+      ) {
+        throw new ExternalHostError(err);
+      }
+    }
+  }
 }
