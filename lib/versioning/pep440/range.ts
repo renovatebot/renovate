2dd96cf0ad3ab1039ceddd747e1573393c7010f7
Rhys Arkins <rhys@arkins.net>
2021-01-13 17:35:06 +0100

    Update range.ts

--- lib/versioning/pep440/range.ts
+++ lib/versioning/pep440/range.ts
@@ -64,7 +64,7 @@ export function getNewValue({
       return currentValue;
     }
   }
-  if (!['replace', 'bump'].includes(rangeStrategy)) {
+  if (!['extend', 'bump'].includes(rangeStrategy)) {
     logger.debug(
       'Unsupported rangeStrategy: ' +
         rangeStrategy +
