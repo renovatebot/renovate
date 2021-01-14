305c48fa13f63645ffd671482393326d9ea75de7
frunza <marius.carp@assist.ro>
2021-01-14 13:06:03 +0200

    sbtBumpVersion remove try catch

--- lib/manager/sbt/update.ts
+++ lib/manager/sbt/update.ts
@@ -11,33 +11,22 @@ export function bumpPackageVersion(
     { bumpVersion, currentValue },
     'Checking if we should bump build.sbt version'
   );
-  let newVersion: string;
   let bumpedContent = content;
-  try {
-    newVersion = inc(currentValue, bumpVersion as ReleaseType);
-    if (!newVersion) {
-      logger.warn('Version incremental failed');
-      return { bumpedContent };
-    }
-    bumpedContent = content.replace(
-      /^(version\s*:=\s*).*$/m,
-      `$1"${newVersion}"`
-    );
-
-    if (bumpedContent === content) {
-      logger.debug('Version was already bumped');
-    } else {
-      logger.debug({ newVersion }, 'Bumped build.sbt version');
-    }
-  } catch (err) {
-    logger.warn(
-      {
-        content,
-        currentValue,
-        bumpVersion,
-      },
-      'Failed to sbt.bumpVersion'
-    );
+  const newVersion = inc(currentValue, bumpVersion as ReleaseType);
+  if (!newVersion) {
+    logger.warn('Version incremental failed');
+    return { bumpedContent };
   }
+  bumpedContent = content.replace(
+    /^(version\s*:=\s*).*$/m,
+    `$1"${newVersion}"`
+  );
+
+  if (bumpedContent === content) {
+    logger.debug('Version was already bumped');
+  } else {
+    logger.debug({ newVersion }, 'Bumped build.sbt version');
+  }
+
   return { bumpedContent };
 }
