33d1de75e271ce8643b49d7fa2dc249747c25998
Rhys Arkins <rhys@arkins.net>
2021-02-05 09:03:08 +0100

    no sort, add todo

--- lib/config/admin.ts
+++ lib/config/admin.ts
@@ -9,9 +9,10 @@ export function setAdminConfig(
   adminOptions: string[]
 ): void {
   adminConfig = {};
-  const repoAdminOptions = adminOptions.concat(derivedAdminOptions).sort();
+  const repoAdminOptions = adminOptions.concat(derivedAdminOptions);
   for (const option of repoAdminOptions) {
     adminConfig[option] = config[option];
+    // TODO: delete from config
   }
 }
 
