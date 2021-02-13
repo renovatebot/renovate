b3506050986e5bf07d09707bcc2d7c2df0bdc1bb
Michael Kriese <michael.kriese@visualon.de>
2021-02-13 10:23:55 +0100

    fix: add missing platform

---
+++ lib/platform/api.ts
@@ -0,0 +1,18 @@
+import * as azure from './azure';
+import * as bitbucket from './bitbucket';
+import * as bitbucketServer from './bitbucket-server';
+import { Platform } from './common';
+import * as gitea from './gitea';
+import * as github from './github';
+import * as gitlab from './gitlab';
+
+const api = new Map<string, Platform>();
+
+export default api;
+
+api.set('azure', azure);
+api.set('bitbucket', bitbucket);
+api.set('bitbucket-server', bitbucketServer);
+api.set('gitea', gitea);
+api.set('github', github);
+api.set('gitlab', gitlab);
