c1c90bb9cd99b4963668cfa8197f1635bf8af879
Sergei Zharinov <sergio.zharinov@gmail.com>
2021-02-18 12:58:20 +0300

    feat(datasource): Normalize releaseTimestamp field

--- lib/datasource/metadata.ts
+++ lib/datasource/metadata.ts
@@ -1,6 +1,7 @@
 import URL from 'url';
 import is from '@sindresorhus/is';
 import parse from 'github-url-from-git';
+import { DateTime } from 'luxon';
 import * as hostRules from '../util/host-rules';
 import { ReleaseResult } from './common';
 
@@ -117,6 +118,43 @@ function massageGitlabUrl(url: string): string {
     .replace('.git', '');
 }
 
+function normalizeDate(input: any): string | null {
+  if (
+    typeof input === 'number' &&
+    !Number.isNaN(input) &&
+    input > 0 &&
+    input <= Date.now() + 24 * 60 * 60 * 1000
+  ) {
+    return new Date(input).toISOString();
+  }
+
+  if (typeof input === 'string') {
+    const luxonDate = DateTime.fromISO(input, { zone: 'UTC' });
+    if (luxonDate.isValid) {
+      return luxonDate.toISO();
+    }
+
+    return normalizeDate(Date.parse(input));
+  }
+
+  if (input instanceof Date) {
+    return input.toISOString();
+  }
+
+  return null;
+}
+
+function massageTimestamps(dep: ReleaseResult): void {
+  for (const release of dep.releases || []) {
+    let { releaseTimestamp } = release;
+    delete release.releaseTimestamp;
+    releaseTimestamp = normalizeDate(releaseTimestamp);
+    if (releaseTimestamp) {
+      release.releaseTimestamp = releaseTimestamp;
+    }
+  }
+}
+
 /* eslint-disable no-param-reassign */
 export function addMetaData(
   dep?: ReleaseResult,
@@ -126,6 +164,9 @@ export function addMetaData(
   if (!dep) {
     return;
   }
+
+  massageTimestamps(dep);
+
   const lookupNameLowercase = lookupName ? lookupName.toLowerCase() : null;
   if (manualChangelogUrls[datasource]?.[lookupNameLowercase]) {
     dep.changelogUrl = manualChangelogUrls[datasource][lookupNameLowercase];
