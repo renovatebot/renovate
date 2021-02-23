3714e1e3780e82fce6088f245d5c3e6bd05ba142
Rhys Arkins <rhys@arkins.net>
2021-02-23 21:30:39 +0100

    update-lockfile not bump

--- lib/config/definitions.ts
+++ lib/config/definitions.ts
@@ -1271,7 +1271,7 @@ const options: RenovateOptions[] = [
       groupName: null,
       schedule: [],
       dependencyDashboardApproval: false,
-      rangeStrategy: 'bump',
+      rangeStrategy: 'update-lockfile',
       commitMessageSuffix: '[SECURITY]',
       branchTopic: `{{{datasource}}}-{{{depName}}}-vulnerability`,
       prCreation: 'immediate',
