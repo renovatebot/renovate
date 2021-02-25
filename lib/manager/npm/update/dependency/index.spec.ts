7a0ade04c64a9f12388d0525483bd0dd01ffd34b
Rhys Arkins <rhys@arkins.net>
2021-02-25 09:42:39 +0100

    refactor: nested npm/update

--- lib/manager/npm/update/dependency/index.spec.ts
+++ lib/manager/npm/update/dependency/index.spec.ts
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import upath from 'upath';
 
-import * as npmUpdater from './update';
+import * as npmUpdater from '.';
 
 function readFixture(fixture: string) {
   return fs.readFileSync(
-    upath.resolve(__dirname, `./__fixtures__/${fixture}`),
+    upath.resolve(__dirname, `../../__fixtures__/${fixture}`),
     'utf8'
   );
 }
@@ -13,7 +13,7 @@ function readFixture(fixture: string) {
 const input01Content = readFixture('inputs/01.json');
 const input01GlobContent = readFixture('inputs/01-glob.json');
 
-describe('workers/branch/package-json', () => {
+describe(__dirname, () => {
   describe('.updateDependency(fileContent, depType, depName, newValue)', () => {
     it('replaces a dependency value', () => {
       const upgrade = {
@@ -198,70 +198,6 @@ describe('workers/branch/package-json', () => {
         upgrade,
       });
       expect(testContent).toBeNull();
-    });
-  });
-  describe('.bumpPackageVersion()', () => {
-    const content = JSON.stringify({
-      name: 'some-package',
-      version: '0.0.2',
-      dependencies: { chalk: '2.4.2' },
-    });
-    it('mirrors', () => {
-      const { bumpedContent } = npmUpdater.bumpPackageVersion(
-        content,
-        '0.0.2',
-        'mirror:chalk'
-      );
-      expect(bumpedContent).toMatchSnapshot();
-      expect(bumpedContent).not.toEqual(content);
-    });
-    it('aborts mirror', () => {
-      const { bumpedContent } = npmUpdater.bumpPackageVersion(
-        content,
-        '0.0.2',
-        'mirror:a'
-      );
-      expect(bumpedContent).toEqual(content);
-    });
-    it('increments', () => {
-      const { bumpedContent } = npmUpdater.bumpPackageVersion(
-        content,
-        '0.0.2',
-        'patch'
-      );
-      expect(bumpedContent).toMatchSnapshot();
-      expect(bumpedContent).not.toEqual(content);
-    });
-    it('no ops', () => {
-      const { bumpedContent } = npmUpdater.bumpPackageVersion(
-        content,
-        '0.0.1',
-        'patch'
-      );
-      expect(bumpedContent).toEqual(content);
-    });
-    it('updates', () => {
-      const { bumpedContent } = npmUpdater.bumpPackageVersion(
-        content,
-        '0.0.1',
-        'minor'
-      );
-      expect(bumpedContent).toMatchSnapshot();
-      expect(bumpedContent).not.toEqual(content);
-    });
-    it('returns content if bumping errors', async () => {
-      jest.mock('semver', () => ({
-        inc: () => {
-          throw new Error('semver inc');
-        },
-      }));
-      const npmUpdater1 = await import('./update');
-      const { bumpedContent } = npmUpdater1.bumpPackageVersion(
-        content,
-        '0.0.2',
-        true as any
-      );
-      expect(bumpedContent).toEqual(content);
     });
   });
 });
