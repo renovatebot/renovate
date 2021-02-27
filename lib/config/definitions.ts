4292f51739a9a6a7508138eb1b58b762679b0840
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2021-02-27 13:34:44 +0100

    docs: list allowed values for matchStringsStrategy (#8896)

--- lib/config/definitions.ts
+++ lib/config/definitions.ts
@@ -1870,6 +1870,7 @@ const options: RenovateOptions[] = [
     description: 'Strategy how to interpret matchStrings.',
     type: 'string',
     default: 'any',
+    allowedValues: ['any', 'recursive', 'combination'],
     parent: 'regexManagers',
     cli: false,
     env: false,
