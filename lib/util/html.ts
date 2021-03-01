842c69c01b7120f00cc462425d77b979428d880c
Michael Kriese <michael.kriese@visualon.de>
2021-03-01 14:47:27 +0100

    fix: remove old stuff

--- lib/util/html.ts
+++ lib/util/html.ts
@@ -1,9 +1,7 @@
-// import { JSDOM } from 'jsdom';
 import { HTMLElement, parse as _parse } from 'node-html-parser';
 
 export { HTMLElement };
 
 export function parse(html: string): HTMLElement {
   return _parse(html);
-  // return new JSDOM(html).window.document.body;
 }
