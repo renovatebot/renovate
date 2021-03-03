7d52199ee2c0c124c43c2763ac1443a4fe7a5019
Michael Kriese <michael.kriese@visualon.de>
2021-03-03 17:33:02 +0100

    test: temporary coverage fix

--- lib/manager/npm/update/locked-dependency/parent-version.ts
+++ lib/manager/npm/update/locked-dependency/parent-version.ts
@@ -64,6 +64,7 @@ export async function findFirstParentVersion(
         return parentVersion;
       }
       if (semver.isVersion(constraint)) {
+        // istanbul ignore if: TODO FIX ME
         if (semver.isGreaterThan(constraint, targetVersion)) {
           // it's not the version we were after - the parent skipped to a higher version
           logger.debug(
