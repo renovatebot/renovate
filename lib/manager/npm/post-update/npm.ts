4637ba4c4ea4d89ea7ae48704114d3fc26d72869
Michael Kriese <michael.kriese@visualon.de>
2021-03-15 07:42:50 +0100

    feat: use short removate image names

--- lib/manager/npm/post-update/npm.ts
+++ lib/manager/npm/post-update/npm.ts
@@ -64,7 +64,7 @@ export async function generateLockFile(
         npm_config_store: env.npm_config_store,
       },
       docker: {
-        image: 'renovate/node',
+        image: 'node',
         tagScheme: 'npm',
         tagConstraint,
         preCommands,
