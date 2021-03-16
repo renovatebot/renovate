8717d2fc385358fe0074ea23461409b511c32d26
Michael Kriese <michael.kriese@visualon.de>
2021-03-16 16:33:25 +0100

    Update lib/manager/gitlabci/utils.ts
    Co-authored-by: HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>

--- lib/manager/gitlabci/utils.ts
+++ lib/manager/gitlabci/utils.ts
@@ -2,7 +2,7 @@
 const re = /!reference \[\.\w+?(?:, \w+?)\]/g;
 
 /**
- * Replaces gitlab reference tags before parsing, because we our yaml parser doesn't support that.
+ * Replaces GitLab reference tags before parsing, because our yaml parser doesn't support that.
  * @param content pipeline yaml
  * @returns replaced pipeline content
  * https://docs.gitlab.com/ee/ci/yaml/#reference-tags
