fc6928077d84fe714ed430f03d02226ef5921c5e
Michael Kriese <michael.kriese@visualon.de>
2021-03-16 16:47:59 +0100

    Apply suggestions from code review
    Co-authored-by: HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>

--- lib/manager/gitlabci/utils.ts
+++ lib/manager/gitlabci/utils.ts
@@ -1,8 +1,7 @@
-// !reference [.setup, script]
 const re = /!reference \[\.\w+?(?:, \w+?)\]/g;
 
 /**
- * Replaces GitLab reference tags before parsing, because our yaml parser doesn't support that.
+ * Replaces GitLab reference tags before parsing, because our yaml parser cannot process them anyway.
  * @param content pipeline yaml
  * @returns replaced pipeline content
  * https://docs.gitlab.com/ee/ci/yaml/#reference-tags
