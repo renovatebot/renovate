5815140872e6235a63fd830e9b19246371e08c88
Tom Jenkinson <tom@tjenkinson.me>
2021-04-03 12:30:43 +0100

    use emojify

--- lib/workers/pr/body/controls.ts
+++ lib/workers/pr/body/controls.ts
@@ -1,9 +1,10 @@
+import { emojify } from 'node-emoji';
 import { isBranchModified } from '../../../util/git';
 import { BranchConfig } from '../../types';
 
 export async function getControls(config: BranchConfig): Promise<string> {
   const warning = (await isBranchModified(config.branchName))
-    ? ' :warning: **Warning**: custom changes will be lost.'
+    ? emojify(' :warning: **Warning**: custom changes will be lost.')
     : '';
   return `\n\n---\n\n - [ ] <!-- rebase-check -->If you want to rebase/retry this PR, check this box.${warning}\n\n`;
 }
