2f111cac8c105c406978b784af46dfe5cc8e727a
sebastian.poxhofer <sebastian.poxhofer@itsdone.at>
2021-04-09 00:13:35 +0200

    fix(ansible-galaxy): do not catch trailing whitespace in source/name

--- lib/manager/ansible-galaxy/util.ts
+++ lib/manager/ansible-galaxy/util.ts
@@ -3,4 +3,4 @@ export const blockLineRegEx = /^\s*((\w+):\s*(.*))$/;
 export const galaxyDepRegex = /[\w-]+\.[\w-]+/;
 export const dependencyRegex = /^dependencies:/;
 export const galaxyRegEx = /^\s+(?<lookupName>[\w.]+):\s*["'](?<version>.+)["']\s*/;
-export const nameMatchRegex = /(?<source>((git\+)?(?:(git|ssh|https?):\/\/)?(.*@)?(?<hostname>[\w.-]+)(?:(:\d+)?\/|:))(?<depName>[\w./-]+)(?:\.git)?)(,(?<version>\w.*))?/;
+export const nameMatchRegex = /(?<source>((git\+)?(?:(git|ssh|https?):\/\/)?(.*@)?(?<hostname>[\w.-]+)(?:(:\d+)?\/|:))(?<depName>[\w./-]+)(?:\.git)?)(,(?<version>[\w.]*))?/;
