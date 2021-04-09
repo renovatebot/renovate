d565e44562e890aa7707d09d61f37559eb43cb2f
sebastian.poxhofer <sebastian.poxhofer@itsdone.at>
2021-04-10 01:52:28 +0200

    feat(terraform): add platforms and local dependency mirror

--- lib/manager/terraform/artifacts.ts
+++ lib/manager/terraform/artifacts.ts
@@ -6,20 +6,37 @@ import {
   readLocalFile,
   writeLocalFile,
 } from '../../util/fs';
-import { UpdateArtifact, UpdateArtifactsResult } from '../common';
+import type {
+  UpdateArtifact,
+  UpdateArtifactsConfig,
+  UpdateArtifactsResult,
+} from '../types';
 
 const lockFile = '.terraform.lock.hcl';
 
-async function terraformLockUpdate(): Promise<void> {
-  await deleteLocalFile(lockFile);
-  const cmd = 'terraform providers lock';
+async function terraformLockUpdate(
+  config: UpdateArtifactsConfig
+): Promise<void> {
+  // TODO do not hardcode
+  const platformArch = ['linux_amd64', 'darwin_amd64', 'windows_amd64'];
+  // const tempPathProvider = "/tmp/terraform/providers"
+  const tempPathProvider = `${config.cacheDir}/terraform/providers`;
 
+  await deleteLocalFile(lockFile);
   const execOptions: ExecOptions = {
     docker: {
       image: 'renovate/terraform',
     },
   };
-  await exec(cmd, execOptions);
+
+  const platformParameters = platformArch
+    .map((value) => `-platform=${value}`)
+    .join(' ');
+  const providerMirrorCMD = `terraform providers mirror ${platformParameters} ${tempPathProvider}`;
+  await exec(providerMirrorCMD, execOptions);
+
+  const providerLockCMD = `terraform providers lock -fs-mirror=${tempPathProvider} ${platformParameters}`;
+  await exec(providerLockCMD, execOptions);
 }
 
 export async function updateArtifacts({
@@ -30,16 +47,6 @@ export async function updateArtifacts({
 }: UpdateArtifact): Promise<UpdateArtifactsResult[] | null> {
   logger.debug(`terraform.updateArtifacts(${packageFileName})`);
 
-  const isLockFileMaintenance = config.updateType === 'lockFileMaintenance';
-
-  if (
-    !isLockFileMaintenance &&
-    (updatedDeps === undefined || updatedDeps.length < 1)
-  ) {
-    logger.debug('No updated terraform deps - returning null');
-    return null;
-  }
-
   const lockFileName = getSiblingFileName(packageFileName, lockFile);
   const existingLockFileContent = await readLocalFile(lockFileName);
   if (!existingLockFileContent) {
@@ -49,7 +56,7 @@ export async function updateArtifacts({
   try {
     await writeLocalFile(packageFileName, newPackageFileContent);
     logger.debug('Updating ' + lockFileName);
-    await terraformLockUpdate();
+    await terraformLockUpdate(config);
     logger.debug(`Returning updated ${lockFile}`);
     const terraformProviderLockContent = await readLocalFile(lockFileName);
     if (existingLockFileContent === terraformProviderLockContent) {
