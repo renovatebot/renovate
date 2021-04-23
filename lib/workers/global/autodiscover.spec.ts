4c76b9e090e84f5d508f016f42b529e52c0a2e08
Emanuel Bennici <eb@fabmation.de>
2021-04-23 19:23:30 +0200

    fix(config): Massage config everytime
    The config has only been "massaged" when presets where used.
    This lead into problems as described in #9639.
    
    Ref #8763

--- lib/workers/global/autodiscover.spec.ts
+++ lib/workers/global/autodiscover.spec.ts
@@ -5,6 +5,7 @@ import * as platform from '../../platform';
 import * as _ghApi from '../../platform/github';
 import * as _hostRules from '../../util/host-rules';
 import { autodiscoverRepositories } from './autodiscover';
+import { massageConfig } from '../../config/massage';
 
 jest.mock('../../platform/github');
 jest.unmock('../../platform');
@@ -34,6 +35,7 @@ describe(getName(), () => {
       token: 'abc',
     }));
     ghApi.getRepos = jest.fn(() => Promise.resolve([]));
+    config = massageConfig(config);
     const res = await autodiscoverRepositories(config);
     expect(res).toEqual(config);
   });
@@ -44,6 +46,7 @@ describe(getName(), () => {
       token: 'abc',
     }));
     ghApi.getRepos = jest.fn(() => Promise.resolve(['a', 'b']));
+    config = massageConfig(config);
     const res = await autodiscoverRepositories(config);
     expect(res.repositories).toHaveLength(2);
   });
@@ -57,12 +60,13 @@ describe(getName(), () => {
     ghApi.getRepos = jest.fn(() =>
       Promise.resolve(['project/repo', 'project/another-repo'])
     );
+    config = massageConfig(config);
     const res = await autodiscoverRepositories(config);
     expect(res.repositories).toEqual(['project/repo']);
   });
   it('filters autodiscovered github repos but nothing matches', async () => {
     config.autodiscover = true;
-    config.autodiscoverFilter = 'project/re*';
+    config.autodiscoverFilter = ['project/re*'];
     config.platform = 'github';
     hostRules.find = jest.fn(() => ({
       token: 'abc',
@@ -70,7 +74,53 @@ describe(getName(), () => {
     ghApi.getRepos = jest.fn(() =>
       Promise.resolve(['another-project/repo', 'another-project/another-repo'])
     );
+    config = massageConfig(config);
     const res = await autodiscoverRepositories(config);
     expect(res).toEqual(config);
+  });
+  it('filters autodiscovered github repos with string variable', async () => {
+    config.autodiscover = true;
+    config.autodiscoverFilter = 'project/re*';
+    config.platform = 'github';
+    hostRules.find = jest.fn(() => ({
+      token: 'abc',
+    }));
+    ghApi.getRepos = jest.fn(() =>
+      Promise.resolve(['project/repo', 'another-project/another-repo'])
+    );
+    config = massageConfig(config);
+    const res = await autodiscoverRepositories(config);
+    expect(res.repositories).toEqual(['project/repo']);
+  });
+  it('filters autodiscovered github repos with string variable but empty', async () => {
+    config.autodiscover = true;
+    config.autodiscoverFilter = 'project/re*';
+    config.platform = 'github';
+    hostRules.find = jest.fn(() => ({
+      token: 'abc',
+    }));
+    ghApi.getRepos = jest.fn(() =>
+      Promise.resolve(['not-a-target/repo', 'another-project/another-repo'])
+    );
+    config = massageConfig(config);
+    const res = await autodiscoverRepositories(config);
+    expect(res).toEqual(config);
+  });
+  it('filters autodiscoverd repos with string variable deeper', async () => {
+    config.autodiscover = true;
+    config.autodiscoverFilter = 'myprj/**/*';
+    config.platform = 'github';
+    hostRules.find = jest.fn(() => ({
+      token: 'abc',
+    }));
+    ghApi.getRepos = jest.fn(() =>
+      Promise.resolve([
+        'myprj/subgroup-1/renovate-test-3',
+        'another-project/another-repo',
+      ])
+    );
+    config = massageConfig(config);
+    const res = await autodiscoverRepositories(config);
+    expect(res.repositories).toEqual(['myprj/subgroup-1/renovate-test-3']);
   });
 });
