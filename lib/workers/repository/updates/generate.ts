b7d1284562a4c61b8ea7cd0c3bf41a4381851861
Rhys Arkins <rhys@arkins.net>
2021-04-27 15:41:14 +0200

    fix: patch grouping when separateMinorPatch=false

--- lib/workers/repository/updates/generate.ts
+++ lib/workers/repository/updates/generate.ts
@@ -221,7 +221,10 @@ export function generateBranchConfig(
         upgrade.updateType === 'minor' && upgrade.separateMinorPatch
           ? ' (minor)'
           : '';
-      upgrade.prTitle += upgrade.updateType === 'patch' ? ' (patch)' : '';
+      upgrade.prTitle +=
+        upgrade.updateType === 'patch' && upgrade.separateMinorPatch
+          ? ' (patch)'
+          : '';
     }
     // Compile again to allow for nested templates
     upgrade.prTitle = template.compile(upgrade.prTitle, upgrade);
