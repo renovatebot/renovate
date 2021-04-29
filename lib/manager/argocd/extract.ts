7b3401b275cfdd621936b96756cd47fcec3be4c8
secustor <sebastian@poxhofer.at>
2021-04-29 10:10:28 +0200

    fix(argocd): apply requested changes

--- lib/manager/argocd/extract.ts
+++ lib/manager/argocd/extract.ts
@@ -5,15 +5,10 @@ import type { ExtractConfig, PackageDependency, PackageFile } from '../types';
 import type { ApplicationDefinition } from './types';
 import { fileTestRegex } from './util';
 
-function loadYaml(content: string): ApplicationDefinition[] {
-  return safeLoadAll(content);
-}
 function createDependency(
   definition: ApplicationDefinition
 ): PackageDependency {
-  let result: PackageDependency;
-
-  const source = definition.spec.source;
+  const source = definition.spec?.source;
 
   if (source == null) {
     return null;
@@ -21,20 +16,18 @@ function createDependency(
 
   // a chart variable is defined this is helm declaration
   if (source.chart) {
-    result = {
+    return {
       depName: source.chart,
       registryUrls: [source.repoURL],
       currentValue: source.targetRevision,
       datasource: helm.id,
     };
-  } else {
-    result = {
-      depName: source.repoURL,
-      currentValue: source.targetRevision,
-      datasource: gitTags.id,
-    };
   }
-  return result;
+  return {
+    depName: source.repoURL,
+    currentValue: source.targetRevision,
+    datasource: gitTags.id,
+  };
 }
 
 export function extractPackageFile(
@@ -47,15 +40,11 @@ export function extractPackageFile(
     return null;
   }
 
-  const definitions = loadYaml(content);
+  const definitions: ApplicationDefinition[] = safeLoadAll(content);
 
-  const deps = definitions.map((definition) => createDependency(definition));
+  const deps = definitions
+    .map((definition) => createDependency(definition))
+    .filter(Boolean);
 
-  const filteredDeps = deps.filter((value) => value);
-
-  if (filteredDeps.length === 0) {
-    return null;
-  }
-
-  return { deps: filteredDeps };
+  return deps.length ? { deps } : null;
 }
