cf8bd72b13cb3322c62477f33f5927647758866a
Sergei Zharinov <zharinov@users.noreply.github.com>
2021-04-30 13:26:51 +0400

    feat(maven): Obtain "last-modified" timestamps for GitLab (#9791)

--- lib/datasource/maven/util.ts
+++ lib/datasource/maven/util.ts
@@ -116,7 +116,7 @@ export async function isHttpResourceExists(
     const httpClient = httpByHostType(hostType);
     const res = await httpClient.head(pkgUrl.toString());
     const pkgUrlHost = url.parse(pkgUrl.toString()).host;
-    if (pkgUrlHost === 'repo.maven.apache.org') {
+    if (['repo.maven.apache.org', 'gitlab.com'].includes(pkgUrlHost)) {
       const timestamp = res?.headers?.['last-modified'] as string;
       return timestamp || true;
     }
