95b0a9f7cb9fb2735102daeef56433538f43008a
Michael Kriese <michael.kriese@visualon.de>
2021-05-05 08:42:30 +0200

    Update lib/util/git/private-key.ts

--- lib/util/git/private-key.ts
+++ lib/util/git/private-key.ts
@@ -9,7 +9,7 @@ let gitPrivateKey: string;
 let keyId: string;
 
 export function setPrivateKey(key: string): void {
-  gitPrivateKey = key.trim();
+  gitPrivateKey = key?.trim();
 }
 
 async function importKey(): Promise<void> {
