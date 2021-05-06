9ef17df01c3485fc4bde7fda72829c7303757bb1
Matthew Palmer <matthew.palmer@helpsystems.com>
2021-05-06 17:31:29 +0100

    fix: Use local scoped onboardingConfig

--- lib/workers/repository/onboarding/branch/config.spec.ts
+++ lib/workers/repository/onboarding/branch/config.spec.ts
@@ -1,7 +1,6 @@
 import { RenovateConfig, getConfig, getName } from '../../../../../test/util';
 import * as presets from '../../../../config/presets/local';
 import { PRESET_DEP_NOT_FOUND } from '../../../../config/presets/util';
-import { RenovateSharedConfig } from '../../../../config/types';
 import { getOnboardingConfig, getOnboardingConfigContents } from './config';
 
 jest.mock('../../../../config/presets/local');
@@ -10,7 +9,6 @@ const mockedPresets = presets as jest.Mocked<typeof presets>;
 
 describe(getName(), () => {
   let config: RenovateConfig;
-  let onboardingConfig: RenovateSharedConfig;
   beforeEach(() => {
     jest.clearAllMocks();
     config = getConfig();
@@ -35,7 +33,7 @@ describe(getName(), () => {
   describe('getOnboardingConfig', () => {
     it('handles finding an organization preset', async () => {
       mockedPresets.getPreset.mockResolvedValueOnce({ enabled: true });
-      onboardingConfig = await getOnboardingConfig(config);
+      const onboardingConfig = await getOnboardingConfig(config);
       expect(mockedPresets.getPreset).toHaveBeenCalledTimes(1);
       expect(onboardingConfig).toEqual({
         $schema: 'https://docs.renovatebot.com/renovate-schema.json',
@@ -47,7 +45,7 @@ describe(getName(), () => {
         new Error(PRESET_DEP_NOT_FOUND)
       );
       mockedPresets.getPreset.mockResolvedValueOnce({ enabled: true });
-      onboardingConfig = await getOnboardingConfig(config);
+      const onboardingConfig = await getOnboardingConfig(config);
       expect(mockedPresets.getPreset).toHaveBeenCalledTimes(2);
       expect(onboardingConfig).toEqual({
         $schema: 'https://docs.renovatebot.com/renovate-schema.json',
@@ -58,7 +56,7 @@ describe(getName(), () => {
       mockedPresets.getPreset.mockRejectedValue(
         new Error(PRESET_DEP_NOT_FOUND)
       );
-      onboardingConfig = await getOnboardingConfig(config);
+      const onboardingConfig = await getOnboardingConfig(config);
       expect(mockedPresets.getPreset).toHaveBeenCalledTimes(2);
       expect(onboardingConfig).toEqual(config.onboardingConfig);
     });
@@ -66,7 +64,7 @@ describe(getName(), () => {
       mockedPresets.getPreset.mockRejectedValue(
         new Error('unknown error for test')
       );
-      onboardingConfig = await getOnboardingConfig(config);
+      const onboardingConfig = await getOnboardingConfig(config);
       expect(mockedPresets.getPreset).toHaveBeenCalledTimes(2);
       expect(onboardingConfig).toEqual(config.onboardingConfig);
     });
@@ -74,7 +72,7 @@ describe(getName(), () => {
       mockedPresets.getPreset.mockRejectedValue(
         new Error(`Unsupported platform 'dummy' for local preset.`)
       );
-      onboardingConfig = await getOnboardingConfig(config);
+      const onboardingConfig = await getOnboardingConfig(config);
       expect(mockedPresets.getPreset).toHaveBeenCalledTimes(2);
       expect(onboardingConfig).toEqual(config.onboardingConfig);
     });
