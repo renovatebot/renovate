0fe5ca3090f2847f3ac14c9e3c83c4ad1a2d3703
Florian Mueller <f.l.o.mueller@web.de>
2021-05-09 11:48:36 +0200

    Allow port numbers in git URLs

--- lib/manager/kustomize/extract.ts
+++ lib/manager/kustomize/extract.ts
@@ -20,7 +20,7 @@ interface Kustomize {
 
 // URL specifications should follow the hashicorp URL format
 // https://github.com/hashicorp/go-getter#url-format
-const gitUrl = /^(?:git::)?(?<url>(?:(?:(?:http|https|ssh):\/\/)?(?:.*@)?)?(?<path>(?:[^:/]+[:/])?(?<project>[^/]+\/[^/]+)))(?<subdir>[^?]*)\?ref=(?<currentValue>.+)$/;
+const gitUrl = /^(?:git::)?(?<url>(?:(?:(?:http|https|ssh):\/\/)?(?:.*@)?)?(?<path>(?:[^:/]+[:/])?([0-9]+\/)?(?<project>[^/]+\/[^/]+)))(?<subdir>[^?]*)\?ref=(?<currentValue>.+)$/;
 
 export function extractBase(base: string): PackageDependency | null {
   const match = gitUrl.exec(base);
