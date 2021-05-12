10ea22bc8672bd1e80f32115216a26ffc927e198
jose-ws <jose.morales@whitesourcesoftware.com>
2021-05-11 23:49:31 -0400

    feat(proxy): proxy environment variables available in lowercase for child proecess.

--- lib/proxy.ts
+++ lib/proxy.ts
@@ -14,6 +14,10 @@ export function bootstrap(): void {
     ) {
       process.env[envVar] = process.env[envVar.toLowerCase()];
     }
+
+    if (process.env[envVar]) {
+      process.env[envVar.toLowerCase()] = process.env[envVar];
+    }
   });
 
   if (
