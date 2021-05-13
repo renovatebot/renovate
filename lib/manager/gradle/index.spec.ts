680670e613b9f2dc3cef4a99ea46e6aa3441daa9
Sergei Zharinov <sergio.zharinov@gmail.com>
2021-05-13 10:42:41 +0300

    refactor: Stop use setUtilConfig in tests

--- lib/manager/gradle/index.spec.ts
+++ lib/manager/gradle/index.spec.ts
@@ -10,11 +10,11 @@ import {
   loadFixture,
   mocked,
 } from '../../../test/util';
-import { setUtilConfig } from '../../util';
 import { setExecConfig } from '../../util/exec';
 import { BinarySource } from '../../util/exec/common';
 import * as docker from '../../util/exec/docker';
 import * as _env from '../../util/exec/env';
+import { setFsConfig } from '../../util/fs';
 import { extractAllPackageFiles, updateDependency } from '.';
 
 jest.mock('child_process');
@@ -83,7 +83,8 @@ describe(getName(), () => {
   }
 
   beforeAll(async () => {
-    await setUtilConfig(utilConfig);
+    await setExecConfig(utilConfig);
+    setFsConfig(utilConfig);
   });
 
   beforeEach(() => {
