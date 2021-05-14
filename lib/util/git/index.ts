241a4bf24c50066d8fa3098eceb6ffd5db9a973d
Rhys Arkins <rhys@arkins.net>
2021-05-14 10:31:30 +0200

    fix(git): gracefully handle rev-parse error when checking out branch

--- lib/util/git/index.ts
+++ lib/util/git/index.ts
@@ -420,6 +420,10 @@ export async function checkoutBranch(branchName: string): Promise<CommitSha> {
     return config.currentBranchSha;
   } catch (err) /* istanbul ignore next */ {
     checkForPlatformFailure(err);
+    if (err.message?.includes('fatal: ambiguous argument')) {
+      logger.warn({ err }, 'Failed to checkout branch');
+      throw new Error(TEMPORARY_ERROR);
+    }
     throw err;
   }
 }
