899422214b1d1de3c782b331ea03dd4211d5921b
Rhys Arkins <rhys@arkins.net>
2021-05-21 15:43:51 +0200

    fix

--- lib/manager/npm/post-update/rules.ts
+++ lib/manager/npm/post-update/rules.ts
@@ -23,16 +23,22 @@ export function processHostRules(): HostRulesResult {
         const key = hostRule.authType === 'Basic' ? '_auth' : '_authToken';
         additionalNpmrcContent.push(`${uri}:${key}=${hostRule.token}`);
         additionalYarnRcYml ||= { npmRegistries: {} };
-        additionalYarnRcYml.npmRegistries[uri] = {
-          npmAuthToken: hostRule.token,
-        };
+        if (hostRule.authType === 'Basic') {
+          additionalYarnRcYml.npmRegistries[uri] = {
+            npmAuthIdent: hostRule.token,
+          };
+        } else {
+          additionalYarnRcYml.npmRegistries[uri] = {
+            npmAuthToken: hostRule.token,
+          };
+        }
       } else if (is.string(hostRule.username) && is.string(hostRule.password)) {
         const password = Buffer.from(hostRule.password).toString('base64');
         additionalNpmrcContent.push(`${uri}:username=${hostRule.username}`);
         additionalNpmrcContent.push(`${uri}:_password=${password}`);
         additionalYarnRcYml ||= { npmRegistries: {} };
         additionalYarnRcYml.npmRegistries[uri] = {
-          npmAuthIdent: `${hostRule.username}:${hostRule.password}`,
+          npmAuthIdent: `Buffer.from(${hostRule.username}:${hostRule.password}).toString('base64)`,
         };
       }
     }
