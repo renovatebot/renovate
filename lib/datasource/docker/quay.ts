b53b18d00a3b04aa147a342293a75a449bb9fd8c
Giorgos Karagounis <giorgos.karagounis@oqton.com>
2021-05-26 21:03:02 +0200

    fix quay authentication

--- lib/datasource/docker/quay.ts
+++ lib/datasource/docker/quay.ts
@@ -1,4 +1,5 @@
-import { http } from './common';
+import { logger } from '../../logger';
+import { getAuthHeaders, http } from './common';
 
 export async function getTagsQuayRegistry(
   repository: string
@@ -6,6 +7,11 @@ export async function getTagsQuayRegistry(
   const registry = 'https://quay.io';
   let tags: string[] = [];
   const limit = 100;
+  const headers = await getAuthHeaders(registry, repository);
+  if (!headers) {
+    logger.debug('Failed to get authHeaders for getTagsQuayRegistry lookup');
+    return null;
+  }
 
   const pageUrl = (page: number): string =>
     `${registry}/api/v1/repository/${repository}/tag/?limit=${limit}&page=${page}&onlyActiveTags=true`;
@@ -16,7 +22,7 @@ export async function getTagsQuayRegistry(
     const res = await http.getJson<{
       tags: { name: string }[];
       has_additional: boolean;
-    }>(url, {});
+    }>(url, { headers });
     const pageTags = res.body.tags.map((tag) => tag.name);
     tags = tags.concat(pageTags);
     page += 1;
