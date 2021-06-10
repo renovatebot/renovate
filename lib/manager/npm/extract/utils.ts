33061edde54adee247e86725919b16ec7bcaa6e3
Lukas Hass <lukas@slucky.de>
2021-06-10 20:57:04 +0200

    fix(pnpm): match package patterns that are matched by pnpm

--- lib/manager/npm/extract/utils.ts
+++ lib/manager/npm/extract/utils.ts
@@ -3,7 +3,8 @@ import { logger } from '../../../logger';
 
 export function matchesAnyPattern(val: string, patterns: string[]): boolean {
   const res = patterns.some(
-    (pattern) => pattern === val + '/' || minimatch(val, pattern, { dot: true })
+    (pattern) =>
+      pattern === val + '/' || minimatch(val, `${pattern}/*`, { dot: true })
   );
   logger.trace({ val, patterns, res }, `matchesAnyPattern`);
   return res;
