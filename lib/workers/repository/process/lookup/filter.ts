740c5518053bdd4e6070366a1e0dc4b7b67e2c5f
Michael Kriese <michael.kriese@visualon.de>
2021-06-18 14:18:20 +0200

    fix: reduce deprecation noise

--- lib/workers/repository/process/lookup/filter.ts
+++ lib/workers/repository/process/lookup/filter.ts
@@ -50,7 +50,7 @@ export function filterVersions(
         (release) => release.version === v.version
       );
       if (versionRelease.isDeprecated) {
-        logger.debug(
+        logger.trace(
           `Skipping ${config.depName}@${v.version} because it is deprecated`
         );
         return false;
