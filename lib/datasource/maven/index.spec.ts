7fd1e2f9ce95ff005b011682d0c0c1134d4814ce
Thorsten Wendelmuth <thorsten.wendelmuth@gmx.de>
2021-06-25 14:39:28 +0200

    feat(maven): lookup parent homepage/scm information from pom (#9722)

--- lib/datasource/maven/index.spec.ts
+++ lib/datasource/maven/index.spec.ts
@@ -305,4 +305,191 @@ describe(getName(), () => {
     expect(res).toMatchSnapshot();
     expect(httpMock.getTrace()).toMatchSnapshot();
   });
+
+  describe('fetching parent info', () => {
+    const parentPackage = {
+      dep: 'org.example:parent',
+      meta: null,
+      pom: loadFixture('parent-scm-homepage/pom.xml'),
+      latest: '1.0.0',
+      jars: null,
+    };
+
+    it('should get source and homepage from parent', async () => {
+      mockGenericPackage({
+        meta: loadFixture('child-no-info/meta.xml'),
+        pom: loadFixture('child-no-info/pom.xml'),
+        latest: '2.0.0',
+        jars: { '2.0.0': 200 },
+      });
+      mockGenericPackage(parentPackage);
+
+      const res = await get();
+
+      expect(res).toMatchObject({
+        sourceUrl: 'https://github.com/parent-scm/parent',
+        homepage: 'https://parent-home.example.com',
+      });
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('should deal with missing parent fields', async () => {
+      mockGenericPackage({
+        meta: loadFixture('child-empty/meta.xml'),
+        pom: loadFixture('child-empty/pom.xml'),
+        latest: '2.0.0',
+        jars: { '2.0.0': 200 },
+      });
+
+      const res = await get();
+
+      expect(res).toMatchObject({
+        display: 'org.example:package',
+        group: 'org.example',
+        name: 'package',
+      });
+      expect(res).not.toHaveProperty('homepage');
+      expect(res).not.toHaveProperty('sourceUrl');
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('should deal with circular hierarchy', async () => {
+      const parentPom = loadFixture('child-parent-cycle/parent.pom.xml');
+      const parentPomMock = {
+        dep: 'org.example:parent',
+        meta: null,
+        pom: parentPom,
+        latest: '2.0.0',
+        jars: null,
+      };
+
+      const childMeta = loadFixture('child-parent-cycle/child.meta.xml');
+      const childPom = loadFixture('child-parent-cycle/child.pom.xml');
+      const childPomMock = {
+        dep: 'org.example:child',
+        meta: null,
+        pom: childPom,
+        latest: '2.0.0',
+        jars: null,
+      };
+
+      mockGenericPackage({
+        ...childPomMock,
+        meta: childMeta,
+        jars: { '2.0.0': 200 },
+      });
+      mockGenericPackage(parentPomMock);
+      mockGenericPackage(childPomMock);
+      mockGenericPackage(parentPomMock);
+      mockGenericPackage(childPomMock);
+      mockGenericPackage(parentPomMock);
+
+      const res = await get('org.example:child');
+
+      expect(res).toMatchObject({
+        homepage: 'https://parent-home.example.com',
+      });
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('should get source from own pom and homepage from parent', async () => {
+      mockGenericPackage({
+        meta: loadFixture('child-scm/meta.xml'),
+        pom: loadFixture('child-scm/pom.xml'),
+        latest: '2.0.0',
+        jars: { '2.0.0': 200 },
+      });
+      mockGenericPackage(parentPackage);
+
+      const res = await get();
+
+      expect(res).toMatchObject({
+        sourceUrl: 'https://github.com/child-scm/child',
+        homepage: 'https://parent-home.example.com',
+      });
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('should get homepage from own pom and source from parent', async () => {
+      mockGenericPackage({
+        meta: loadFixture('child-url/meta.xml'),
+        pom: loadFixture('child-url/pom.xml'),
+        latest: '2.0.0',
+        jars: { '2.0.0': 200 },
+      });
+      mockGenericPackage(parentPackage);
+
+      const res = await get();
+
+      expect(res).toMatchObject({
+        sourceUrl: 'https://github.com/parent-scm/parent',
+        homepage: 'https://child-home.example.com',
+      });
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('should get homepage and source from own pom', async () => {
+      mockGenericPackage({
+        meta: loadFixture('child-all-info/meta.xml'),
+        pom: loadFixture('child-all-info/pom.xml'),
+        latest: '2.0.0',
+        jars: { '2.0.0': 200 },
+      });
+
+      const res = await get();
+
+      expect(res).toMatchObject({
+        sourceUrl: 'https://github.com/child-scm/child',
+        homepage: 'https://child-home.example.com',
+      });
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('should be able to detect git@github.com:child-scm as valid sourceUrl', async () => {
+      mockGenericPackage({
+        meta: loadFixture('child-scm-gitatcolon/meta.xml'),
+        pom: loadFixture('child-scm-gitatcolon/pom.xml'),
+        latest: '2.0.0',
+        jars: { '2.0.0': 200 },
+      });
+
+      const res = await get();
+
+      expect(res).toMatchObject({
+        sourceUrl: 'https://github.com/child-scm/child',
+      });
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('should be able to detect git@github.com/child-scm as valid sourceUrl', async () => {
+      mockGenericPackage({
+        meta: loadFixture('child-scm-gitatslash/meta.xml'),
+        pom: loadFixture('child-scm-gitatslash/pom.xml'),
+        latest: '2.0.0',
+        jars: { '2.0.0': 200 },
+      });
+
+      const res = await get();
+
+      expect(res).toMatchObject({
+        sourceUrl: 'https://github.com/child-scm/child',
+      });
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+    it('should be able to detect git://@github.com/child-scm as valid sourceUrl', async () => {
+      mockGenericPackage({
+        meta: loadFixture('child-scm-gitprotocol/meta.xml'),
+        pom: loadFixture('child-scm-gitprotocol/pom.xml'),
+        latest: '2.0.0',
+        jars: { '2.0.0': 200 },
+      });
+
+      const res = await get();
+
+      expect(res).toMatchObject({
+        sourceUrl: 'https://github.com/child-scm/child',
+      });
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+  });
 });
