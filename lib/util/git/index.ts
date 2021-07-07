036858c11f447746c22ab3bdaf97fc2d20397d24
Michael Kriese <michael.kriese@visualon.de>
2021-07-07 07:12:05 +0200

    feat: disable git-lfs pull temporary

--- lib/util/git/index.ts
+++ lib/util/git/index.ts
@@ -299,7 +299,7 @@ export async function syncGit(): Promise<void> {
     const cloneStart = Date.now();
     try {
       // clone only the default branch
-      const opts = ['--depth=10'];
+      const opts = ['--depth=10', '-c', 'lfs.fetchexclude=*'];
       if (config.extraCloneOpts) {
         Object.entries(config.extraCloneOpts).forEach((e) =>
           opts.push(e[0], `${e[1]}`)
