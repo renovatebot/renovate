d43b1d9734de1721ef43fb9e55f5926735684041
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-07-23 09:16:50 -0400

    github-releases: getDigest()

--- lib/datasource/github-releases/index.spec.ts
+++ lib/datasource/github-releases/index.spec.ts
@@ -1,4 +1,4 @@
-import { getPkgReleases } from '..';
+import { getDigest, getPkgReleases } from '..';
 import * as httpMock from '../../../test/http-mock';
 import { getName } from '../../../test/util';
 import * as _hostRules from '../../util/host-rules';
@@ -65,6 +65,126 @@ describe(getName(), () => {
       httpMock.getTrace();
       expect(res).toMatchSnapshot();
       expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+  });
+
+  describe('getDigest', () => {
+    const lookupName = 'some/dep';
+    const currentValue = 'v1.0.0';
+    const currentDigest = 'v1.0.0-digest';
+
+    const mockReleaseWithDigestFile = (version: string, digests: string[]) => {
+      const digestFile = digests.join('\n');
+      const digestFilePath = `/repos/${lookupName}/releases/download/${version}/SHASUMS.txt`;
+      const releaseData = {
+        tag_name: version,
+        published_at: '2020-03-09T11:00:00Z',
+        assets: [
+          {
+            name: 'SHASUMS.txt',
+            size: digestFile.length,
+            browser_download_url: `${githubApiHost}${digestFilePath}`,
+          },
+        ],
+      };
+      httpMock
+        .scope(githubApiHost)
+        .get(`/repos/${lookupName}/releases/tags/${version}`)
+        .reply(200, releaseData);
+      httpMock.scope(githubApiHost).get(digestFilePath).reply(200, digestFile);
+    };
+
+    it('requires currentDigest', async () => {
+      const digest = await getDigest({ datasource, lookupName });
+      expect(digest).toBeNull();
+    });
+
+    it('defaults to currentDigest when currentVersion is missing', async () => {
+      const digest = await getDigest({
+        datasource,
+        lookupName,
+        currentDigest,
+      });
+      expect(digest).toEqual(currentDigest);
+    });
+
+    it('verifies currentDigest from digest file', async () => {
+      mockReleaseWithDigestFile(currentValue, [
+        `${currentDigest} linux-amd64.tar.gz`,
+        `another-digest   linux-arm64.tar.gz`,
+      ]);
+      const digest = await getDigest(
+        {
+          datasource,
+          lookupName,
+          currentValue,
+          currentDigest,
+        },
+        currentValue
+      );
+      expect(digest).toEqual(currentDigest);
+    });
+
+    // TODO: reviewers - this is awkward, but I found returning `null` in this case to not produce an update
+    // I'd prefer a PR with the old digest (that I can manually patch) to no PR, so I made this decision.
+    it('ignores failures verifying currentDigest', async () => {
+      mockReleaseWithDigestFile(currentValue, []);
+      const digest = await getDigest(
+        {
+          datasource,
+          lookupName,
+          currentValue,
+          currentDigest,
+        },
+        currentValue
+      );
+      expect(digest).toEqual(currentDigest);
+    });
+
+    it('parses digest file in new release', async () => {
+      mockReleaseWithDigestFile(currentValue, [
+        `${currentDigest} linux-amd64.tar.gz`,
+        `another-digest   linux-arm64.tar.gz`,
+      ]);
+      const nextValue = 'v1.0.1';
+      const nextDigest = 'v1.0.1-digest';
+      mockReleaseWithDigestFile(nextValue, [
+        `a-next-digest  linux-arm64.tar.gz`,
+        `${nextDigest}  linux-amd64.tar.gz`,
+      ]);
+      const digest = await getDigest(
+        {
+          datasource,
+          lookupName,
+          currentValue,
+          currentDigest,
+        },
+        nextValue
+      );
+      expect(digest).toEqual(nextDigest);
+    });
+
+    it('parses digest file in new release that embeds version in digested file', async () => {
+      mockReleaseWithDigestFile(currentValue, [
+        `${currentDigest} some-dep-1.0.0/linux-amd64.tar.gz`,
+        `another-digest   some-dep-1.0.0/linux-arm64.tar.gz`,
+      ]);
+      const nextValue = 'v1.0.1';
+      const nextDigest = 'v1.0.1-digest';
+      mockReleaseWithDigestFile(nextValue, [
+        `a-next-digest  some-dep-1.0.1/linux-arm64.tar.gz`,
+        `${nextDigest}  some-dep-1.0.1/linux-amd64.tar.gz`,
+      ]);
+      const digest = await getDigest(
+        {
+          datasource,
+          lookupName,
+          currentValue,
+          currentDigest,
+        },
+        nextValue
+      );
+      expect(digest).toEqual(nextDigest);
     });
   });
 });
--- lib/datasource/github-releases/index.ts
+++ lib/datasource/github-releases/index.ts
@@ -1,8 +1,9 @@
+import { logger } from '../../logger';
 import * as packageCache from '../../util/cache/package';
 import { GithubHttp } from '../../util/http/github';
 import { ensureTrailingSlash } from '../../util/url';
-import type { GetReleasesConfig, ReleaseResult } from '../types';
-import type { GithubRelease } from './types';
+import type { DigestConfig, GetReleasesConfig, ReleaseResult } from '../types';
+import type { GithubRelease, GithubReleaseAsset } from './types';
 
 export const id = 'github-releases';
 export const customRegistrySupport = true;
@@ -16,6 +17,17 @@ const http = new GithubHttp();
 function getCacheKey(depHost: string, repo: string): string {
   const type = 'tags';
   return `${depHost}:${repo}:${type}`;
+}
+
+function getSourceUrlBase(registryUrl: string): string {
+  // default to GitHub.com if no GHE host is specified.
+  return ensureTrailingSlash(registryUrl ?? 'https://github.com/');
+}
+
+function getApiBaseUrl(sourceUrlBase: string): string {
+  return sourceUrlBase === 'https://github.com/'
+    ? `https://api.github.com/`
+    : `${sourceUrlBase}api/v3/`;
 }
 
 /**
@@ -40,14 +52,8 @@ export async function getReleases({
   if (cachedResult) {
     return cachedResult;
   }
-  // default to GitHub.com if no GHE host is specified.
-  const sourceUrlBase = ensureTrailingSlash(
-    registryUrl ?? 'https://github.com/'
-  );
-  const apiBaseUrl =
-    sourceUrlBase === 'https://github.com/'
-      ? `https://api.github.com/`
-      : `${sourceUrlBase}api/v3/`;
+  const sourceUrlBase = getSourceUrlBase(registryUrl);
+  const apiBaseUrl = getApiBaseUrl(sourceUrlBase);
   const url = `${apiBaseUrl}repos/${repo}/releases?per_page=100`;
   const res = await http.getJson<GithubRelease[]>(url, {
     paginate: true,
@@ -73,4 +79,115 @@ export async function getReleases({
     cacheMinutes
   );
   return dependency;
+}
+
+type DigestAsset = {
+  assetName: string;
+  digestedFileName: string;
+};
+
+async function findDigestAsset(
+  release: GithubRelease | null,
+  digest: string | null
+): Promise<DigestAsset | null> {
+  if (!release || !digest) {
+    return null;
+  }
+  const smallAssets = release.assets.filter(
+    (a: GithubReleaseAsset) => a.size < 5 * 1024
+  );
+  for (const asset of smallAssets) {
+    const res = await http.get(asset.browser_download_url);
+    for (const line of res.body.split('\n')) {
+      const [lineDigest, lineFn] = line.split(/\s+/, 2);
+      if (lineDigest === digest) {
+        return {
+          assetName: asset.name,
+          digestedFileName: lineFn,
+        };
+      }
+    }
+  }
+  // TODO: it's not unreasonable to download assets to find a match
+  return null;
+}
+
+async function findNewDigest(
+  currentVersion: string,
+  digestAsset: DigestAsset | null,
+  release: GithubRelease
+): Promise<string | null> {
+  if (!digestAsset) {
+    return null;
+  }
+  const current = currentVersion.replace(/^v/, '');
+  const next = release.tag_name.replace(/^v/, '');
+  const releaseChecksumAssetName = digestAsset.assetName.replace(current, next);
+  const releaseAsset = release.assets.find(
+    (a: GithubReleaseAsset) => a.name === releaseChecksumAssetName
+  );
+  if (!releaseAsset) {
+    return null;
+  }
+  const releaseFilename = digestAsset.digestedFileName.replace(current, next);
+  const res = await http.get(releaseAsset.browser_download_url);
+  for (const line of res.body.split('\n')) {
+    const [lineDigest, lineFn] = line.split(/\s+/, 2);
+    if (lineFn === releaseFilename) {
+      return lineDigest;
+    }
+  }
+  return null;
+}
+
+export async function getGithubRelease(
+  apiBaseUrl: string,
+  repo: string,
+  version: string
+): Promise<GithubRelease> {
+  const url = `${apiBaseUrl}repos/${repo}/releases/tags/${version}`;
+  const res = await http.getJson<GithubRelease>(url);
+  return res.body;
+}
+
+/**
+ * github.getDigest
+ *
+ * The `newValue` supplied here should be a valid tag for the GitHub release.
+ * Requires `currentValue` and `currentDigest`.
+ *
+ * There may be many assets attached to the release. This function will:
+ *  - Identify the asset pinned by `currentDigest` in the `currentValue` release
+ *     - Download small release assets, parse as checksum manifests (e.g. `SHASUMS.txt`).
+ *  - Map the hashed asset to `newValue` and return the updated digest as a string
+ */
+export async function getDigest(
+  { lookupName: repo, currentValue, currentDigest, registryUrl }: DigestConfig,
+  newValue?: string
+): Promise<string | null> {
+  logger.debug(
+    { repo, currentValue, currentDigest, registryUrl, newValue },
+    'getDigest'
+  );
+  if (!currentDigest) {
+    return null;
+  }
+  if (!currentValue) {
+    return currentDigest;
+  }
+  // TODO: cache.get()
+
+  const apiBaseUrl = getApiBaseUrl(getSourceUrlBase(registryUrl));
+  const currentRelease = await getGithubRelease(apiBaseUrl, repo, currentValue);
+  const digestAsset = await findDigestAsset(currentRelease, currentDigest);
+  let newDigest: string;
+  if (!digestAsset || newValue === currentValue) {
+    newDigest = currentDigest;
+  } else {
+    const newRelease = await getGithubRelease(apiBaseUrl, repo, newValue);
+    newDigest = await findNewDigest(currentValue, digestAsset, newRelease);
+  }
+
+  // TODO: cache.set()
+  return newDigest;
 }
