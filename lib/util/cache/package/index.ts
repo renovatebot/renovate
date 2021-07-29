2f93e70c8def9663e52ab2b4e599eb9c53376d77
secustor <sebastian@poxhofer.at>
2021-07-29 20:14:23 +0200

    feat(cache): allow to cache null values

--- lib/util/cache/package/index.ts
+++ lib/util/cache/package/index.ts
@@ -15,7 +15,7 @@ export function get<T = any>(namespace: string, key: string): Promise<T> {
     return undefined;
   }
   const globalKey = getGlobalKey(namespace, key);
-  if (!memCache.get(globalKey)) {
+  if (memCache.get(globalKey) === undefined) {
     memCache.set(globalKey, cacheProxy.get(namespace, key));
   }
   return memCache.get(globalKey);
