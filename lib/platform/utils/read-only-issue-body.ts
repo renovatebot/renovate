0a09bb920c1af1ad230aedccd78fef06a7d8b315
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2021-08-02 13:41:44 +0200

    fix: improve dependency dashboard text

--- lib/platform/utils/read-only-issue-body.ts
+++ lib/platform/utils/read-only-issue-body.ts
@@ -2,7 +2,7 @@ export function readOnlyIssueBody(body: string): string {
   return body
     .replace(' only once you click their checkbox below', '')
     .replace(' unless you click a checkbox below', '')
-    .replace(' To discard all commits and start over, check the box below.', '')
+    .replace(' To discard all commits and start over, click on a checkbox.', '')
     .replace(/ Click (?:on |)a checkbox.*\./g, '')
     .replace(/\[ ] <!-- \w*-branch.*-->/g, '');
 }
