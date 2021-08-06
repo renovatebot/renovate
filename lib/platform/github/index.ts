4a1f3e2df766cbe057c2f0b6709a04ae3be64f7c
Sergei Zharinov <sergio.zharinov@gmail.com>
2021-08-06 14:18:48 +0300

    Preserve old replacements just in case

--- lib/platform/github/index.ts
+++ lib/platform/github/index.ts
@@ -1573,7 +1573,11 @@ export function massageMarkdown(input: string): string {
   if (config.isGhe) {
     return smartTruncate(input, 60000);
   }
-  const massagedInput = massageMarkdownLinks(input);
+  const massagedInput = massageMarkdownLinks(input)
+    // to be safe, replace all github.com links with renovatebot redirector
+    .replace(/href="https?:\/\/github.com\//g, 'href="https://togithub.com/')
+    .replace(/]\(https:\/\/github\.com\//g, '](https://togithub.com/')
+    .replace(/]: https:\/\/github\.com\//g, ']: https://togithub.com/');
   return smartTruncate(massagedInput, 60000);
 }
 
