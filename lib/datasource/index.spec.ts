7dc8a95d153c34e95579ef75c192b0f07325b2c1
Sergei Zharinov <sergio.zharinov@gmail.com>
2021-08-10 10:09:12 +0300

    refactor(datasource): More explicit snapshot tests

--- lib/datasource/index.spec.ts
+++ lib/datasource/index.spec.ts
@@ -116,10 +116,9 @@ describe(getName(), () => {
       datasource: datasourceNpm.id,
       depName: 'react-native',
     });
-    // FIXME: explicit assert condition
-    expect(res).toMatchSnapshot();
-    expect(res.changelogUrl).toBeDefined();
-    expect(res.sourceUrl).toBeDefined();
+    expect(res).toMatchSnapshot({
+      changelogUrl: expect.any(String),
+    });
   });
   it('applies extractVersion', async () => {
     npmDatasource.getReleases.mockResolvedValue({
@@ -146,9 +145,7 @@ describe(getName(), () => {
       datasource: datasourceNpm.id,
       depName: 'node',
     });
-    // FIXME: explicit assert condition
-    expect(res).toMatchSnapshot();
-    expect(res.sourceUrl).toBeDefined();
+    expect(res).toMatchSnapshot({ sourceUrl: expect.any(String) });
   });
   it('ignores and warns for registryUrls', async () => {
     await datasource.getPkgReleases({
@@ -165,8 +162,7 @@ describe(getName(), () => {
       depName: 'something',
       registryUrls: ['https://docker.com', 'https://docker.io'],
     });
-    // FIXME: explicit assert condition
-    expect(res).toMatchSnapshot();
+    expect(res).toBeNull();
   });
   it('hunts registries and returns success', async () => {
     packagistDatasource.getReleases.mockResolvedValueOnce(null);
@@ -231,9 +227,18 @@ describe(getName(), () => {
       depName: 'something',
       registryUrls: ['https://reg1.com', 'https://reg2.io'],
     });
-    // FIXME: explicit assert condition
-    expect(res).toMatchSnapshot();
-    expect(res.releases).toHaveLength(2);
+    expect(res).toEqual({
+      releases: [
+        {
+          registryUrl: 'https://reg1.com',
+          version: '1.0.0',
+        },
+        {
+          registryUrl: 'https://reg1.com',
+          version: '1.1.0',
+        },
+      ],
+    });
   });
   it('merges registries and aborts on ExternalHostError', async () => {
     mavenDatasource.getReleases.mockImplementationOnce(() => {
