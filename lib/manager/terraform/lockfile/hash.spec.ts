0418d922046622fcc3a3431a5f7bb04307ca4e69
Michael Kriese <michael.kriese@visualon.de>
2021-08-10 17:27:12 +0200

    chore: use separate temp folders for tests

--- lib/manager/terraform/lockfile/hash.spec.ts
+++ lib/manager/terraform/lockfile/hash.spec.ts
@@ -20,12 +20,12 @@ const log = logger.logger as jest.Mocked<Logger>;
 describe(getName(), () => {
   let cacheDir: DirectoryResult;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     cacheDir = await dir({ unsafeCleanup: true });
     setAdminConfig({ cacheDir: cacheDir.path });
   });
 
-  afterAll(() => cacheDir.cleanup());
+  afterEach(() => cacheDir.cleanup());
 
   it('returns null if getBuilds returns null', async () => {
     httpMock
