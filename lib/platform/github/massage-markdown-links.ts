1e2700ae2147a0e6179ffbd872757c3092f6f588
Sergei Zharinov <sergio.zharinov@gmail.com>
2021-08-11 12:54:32 +0300

    fix(github): Massage links to issues, pulls or discussions

---
+++ lib/platform/github/massage-markdown-links.ts
@@ -0,0 +1,56 @@
+import findAndReplace from 'mdast-util-find-and-replace';
+import remark from 'remark';
+import type { Plugin, Transformer } from 'unified';
+// eslint-disable-next-line import/no-unresolved
+import type { Node } from 'unist';
+import visit from 'unist-util-visit';
+import { logger } from '../../logger';
+
+const urlRegex =
+  /(?:https?:)?(?:\/\/)?(?:www\.)?(?<!api\.)(?:to)?github\.com\/[-_a-z0-9]+\/[-_a-z0-9]+\/(?:discussions|issues|pull)\/[0-9]+(?:#[-_a-z0-9]+)?/i;
+
+function massageLink(input: string): string {
+  return urlRegex.test(input)
+    ? input.replace(/(?:to)?github\.com/, 'togithub.com')
+    : /* istanbul ignore next */
+      input;
+}
+
+function linkifyText(url: string): Node | boolean {
+  const newUrl = massageLink(url);
+  if (newUrl !== url) {
+    const content = { type: 'text', value: url };
+    return {
+      type: 'link',
+      url: newUrl,
+      title: null,
+      children: [content],
+    } as Node;
+  }
+  return false;
+}
+
+function transformer(tree: Node): void {
+  findAndReplace(tree, urlRegex, linkifyText, {
+    ignore: ['link', 'linkReference'],
+  });
+  visit(tree, 'link', (node: any) => {
+    if (node?.url) {
+      // eslint-disable-next-line no-param-reassign
+      node.url = massageLink(node.url);
+    }
+  });
+}
+
+const githubExtra: Plugin<any> = (): Transformer => transformer;
+
+export function massageMarkdownLinks(content: string): string {
+  try {
+    const output = remark().use(githubExtra).processSync(content);
+    const rightSpaces = content.replace(content.trimRight(), '');
+    return output.toString().trimRight() + rightSpaces;
+  } catch (err) /* istanbul ignore next */ {
+    logger.warn({ err }, `Unable to massage markdown text`);
+    return content;
+  }
+}
