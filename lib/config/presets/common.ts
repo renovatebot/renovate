7b08fb5041e5587f4a28be2047454bafff380a4b
Rhys Arkins <rhys@arkins.net>
2021-08-18 11:05:41 +0200

    fix(presets): migrate renamed monorepos

--- lib/config/presets/common.ts
+++ lib/config/presets/common.ts
@@ -1,4 +1,4 @@
-export const removedPresets = {
+export const removedPresets: Record<string, string | null> = {
   ':automergeBranchMergeCommit': ':automergeBranch',
   ':automergeBranchPush': ':automergeBranch',
   ':base': 'config:base',
@@ -23,3 +23,39 @@ export const removedPresets = {
   'helpers:oddIsUnstable': null,
   'helpers:oddIsUnstablePackages': null,
 };
+
+const renamedMonorepos: Record<string, string> = {
+  'arcus event-grid': 'arcus.event-grid',
+  'arcus security': 'arcus.security',
+  'arcus messaging': 'arcus.messaging',
+  'arcus observability': 'arcus.observability',
+  'arcus webap': 'arcus.webap',
+  'arcus background-jobs': 'arcus.background-jobs',
+  'aspnet AspNetWebStack': 'aspnet aspnetwebstack',
+  'aspnet Extensions': 'aspnet extensions',
+  'System.IO.Abstractions': 'system.io.abstractions',
+  angular1: 'angularjs',
+  angularcli: 'angular-cli',
+  Fontsource: 'fontsource',
+  hamcrest: 'javahamcrest',
+  HotChocolate: 'hotchocolate',
+  infrastructure: 'infrastructure-ui',
+  lingui: 'linguijs',
+  MassTransit: 'masstransit',
+  material: 'material-components-web',
+  mui: 'material-ui',
+  openfeign: 'feign',
+  opentelemetry: 'opentelemetry-js',
+  OpenTelemetryDotnet: 'opentelemetry-dotnet',
+  picasso: 'picassojs',
+  reactrouter: 'react-router',
+  sentry: 'sentry-javascript',
+  Steeltoe: 'steeltoe',
+  stryker: 'stryker-js',
+  Swashbuckle: 'swashbuckle-aspnetcore',
+};
+
+for (const [from, to] of Object.entries(renamedMonorepos)) {
+  removedPresets[`monorepo:${from}`] = `monorepo:${to}`;
+  removedPresets[`group:${from}Monorepo`] = `group:${to}Monorepo`;
+}
