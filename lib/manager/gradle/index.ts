0e7f3ed9bd85c506212ecc2a8b66b27a12ad4d47
Danilo Pianini <danilo.pianini@gmail.com>
2021-08-30 19:11:18 +0200

    Match all TOML files located in folders called 'gradle'

--- lib/manager/gradle/index.ts
+++ lib/manager/gradle/index.ts
@@ -32,7 +32,7 @@ export const defaultConfig = {
   fileMatch: [
     '\\.gradle(\\.kts)?$',
     '(^|/)gradle.properties$',
-    '\\.versions\\.toml$',
+    'gradle\\/.+\\.toml$',
   ],
   timeout: 600,
   versioning: gradleVersioning.id,
