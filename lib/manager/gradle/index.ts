39df97cd0dce1e7cf10709a9aa422e62068d7337
Danilo Pianini <danilo.pianini@gmail.com>
2021-08-31 11:09:51 +0200

    Fix borked regex

--- lib/manager/gradle/index.ts
+++ lib/manager/gradle/index.ts
@@ -32,7 +32,7 @@ export const defaultConfig = {
   fileMatch: [
     '\\.gradle(\\.kts)?$',
     '(^|/)gradle.properties$',
-    '(^|\\//gradle\\/.+\\.toml$',
+    '(^|\\/)gradle\\/.+\\.toml$',
     '\\.versions\\.toml$',
   ],
   timeout: 600,
