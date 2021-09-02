d6a09eb41e459e60930c79fae0358392d8bec0b9
t-kulmburg <89128736+t-kulmburg@users.noreply.github.com>
2021-09-02 14:30:14 +0200

    fix: formatting

--- lib/manager/gradle/shallow/utils.ts
+++ lib/manager/gradle/shallow/utils.ts
@@ -25,10 +25,7 @@ export function isDependencyString(input: string): boolean {
   }
   // eslint-disable-next-line prefer-const
   let [tempGroupId, tempArtifactId, tempVersionPart] = split;
-  if (
-    tempVersionPart !== versionLikeSubstring(tempVersionPart) &&
-    tempVersionPart.includes('@')
-  ) {
+  if (tempVersionPart !== versionLikeSubstring(tempVersionPart) && tempVersionPart.includes('@')) {
     const versionSplit = tempVersionPart?.split('@');
     if (versionSplit?.length !== 2) {
       return false;
