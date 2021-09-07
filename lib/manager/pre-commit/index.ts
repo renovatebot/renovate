52e11fea5832c0138ff0fe4a82a5de500148e81f
Rhys Arkins <rhys@arkins.net>
2021-09-07 10:14:20 +0200

    feat: disable pre-commit manager
    Disables pre-commit manager by default.
    
    BREAKING CHANGE: pre-commit manager is no longer enabled by default, and must be opted into manually.

--- lib/manager/pre-commit/index.ts
+++ lib/manager/pre-commit/index.ts
@@ -2,5 +2,6 @@ export { extractPackageFile } from './extract';
 
 export const defaultConfig = {
   commitMessageTopic: 'pre-commit hook {{depName}}',
+  enabled: false,
   fileMatch: ['(^|/)\\.pre-commit-config\\.yaml$'],
 };
