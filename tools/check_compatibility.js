5c2e25827f91d3c21c760eac7de6bbb8b4172b30
RahulGautamSingh <rahultesnik@gmail.com>
2021-09-08 15:07:45 +0000

    test script

---
+++ tools/check_compatibility.js
@@ -0,0 +1,25 @@
+const shell = require('shelljs');
+
+function checkGitVersion() {
+  try {
+    const gitVersion = shell
+      .exec('git --version', { silent: true })
+      .stdout.toString()
+      .slice(12)
+      .split('.');
+    if (
+      parseInt(gitVersion[0], 10) >= 2 &&
+      parseInt(gitVersion[1], 10) >= 22 &&
+      parseInt(gitVersion[2], 10) >= 0
+    ) {
+      shell.echo('VERSION OK');
+    } else {
+      // shell.echo('WARNING: GIT VERSION NOT COMAPTIBLE');
+      throw new Error('GIT VERSION NOT COMAPTIBLE');
+    }
+  } catch (err) {
+    shell.echo('ERROR:', err.message);
+    process.exit(1);
+  }
+}
+checkGitVersion();
