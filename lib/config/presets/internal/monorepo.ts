b525527236d2b5581b29759cc2d0ffaace4ae18c
Samuel Bodin <samuel.bodin@algolia.com>
2021-09-09 10:57:38 +0200

    Update monorepo.ts

--- lib/config/presets/internal/monorepo.ts
+++ lib/config/presets/internal/monorepo.ts
@@ -14,8 +14,7 @@ const repoGroups = {
     'https://github.com/algolia/react-instantsearch',
   'algoliasearch-client-javascript':
     'https://github.com/algolia/algoliasearch-client-javascript',
-  'algoliasearch-autocomplete':
-    'https://github.com/algolia/autocomplete',
+  'algoliasearch-autocomplete': 'https://github.com/algolia/autocomplete',
   'angular-eslint': 'https://github.com/angular-eslint/angular-eslint',
   'apollo-server': 'https://github.com/apollographql/apollo-server',
   'aspnet-api-versioning': 'https://github.com/Microsoft/aspnet-api-versioning',
