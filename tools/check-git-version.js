c1452308325836f2f60c70beb75584ea0abeacbf
RahulGautamSingh <rahul_ug@cse.nits.ac.in>
2021-09-12 21:13:07 +0545

    changed code logic

--- tools/check-git-version.js
+++ tools/check-git-version.js
@@ -7,12 +7,20 @@ function checkGitVersion() {
       .exec('git --version', { silent: true })
       .stdout.toString();
     
-    if (
-      parseInt(gitVersion[0], 10) >= 2 &&
-      parseInt(gitVersion[1], 10) >= 22 &&
-      parseInt(gitVersion[2], 10) >= 0
-    ) {
-      process.exit(0);
+   const [major, minor, revision] = gitVersion.match(regex);
+    const [reqMajor, reqMinor, reqRevision] = GIT_MINIMUM_VERSION.match(regex);
+    if (major < reqMajor) {
+      throw new Error('Minimum git version 2.33.0 is required');
+    } else if (major === reqMajor) {
+      if (minor < reqMinor) {
+        throw new Error('Minimum git version 2.33.0 is required');
+      } else if (minor === reqMinor) {
+        if (revision < reqRevision) {
+          throw new Error('Minimum git version 2.33.0 is required');
+        }
+      }
+    }
+    process.exit(0);
     } else {
       throw new Error('Minimum git version 2.33.0 is required');
     }
