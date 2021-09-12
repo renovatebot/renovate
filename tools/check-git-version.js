d3836edbd6a7e6ae2dbebad253d5d89761923ef5
RahulGautamSingh <rahul_ug@cse.nits.ac.in>
2021-09-12 21:12:32 +0545

    Added regex to comapre git version

--- tools/check-git-version.js
+++ tools/check-git-version.js
@@ -2,11 +2,11 @@ const shell = require('shelljs');
 
 function checkGitVersion() {
   try {
-    const gitVersion = shell
+        const regex = /[\d]+(?=.)/g;
+        const gitVersion = shell
       .exec('git --version', { silent: true })
-      .stdout.toString()
-      .slice(12)
-      .split('.');
+      .stdout.toString();
+    
     if (
       parseInt(gitVersion[0], 10) >= 2 &&
       parseInt(gitVersion[1], 10) >= 22 &&
