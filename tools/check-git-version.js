3c61bba4a21cc6adaf50011fec8f108f9389025b
RahulGautamSingh <rahultesnik@gmail.com>
2021-09-13 07:13:05 +0000

    changed code logic

--- tools/check-git-version.js
+++ tools/check-git-version.js
@@ -1,27 +1,16 @@
+const semver = require('semver');
 const shell = require('shelljs');
 
 const GIT_MINIMUM_VERSION = '2.33.0';
 function checkGitVersion() {
   try {
-    const regex = /[\d]+(?=.)/g;
-    const gitVersion = shell
+    const regex = /[\d.]+(?=)/g;
+    let gitVersion = shell
       .exec('git --version', { silent: true })
       .stdout.toString();
-    const GIT_VERSION_NOT_COMPATIBLE_ERROR = new Error(
-      'Minimum git version 2.33.0 is required'
-    );
-    const [major, minor, revision] = gitVersion.match(regex);
-    const [reqMajor, reqMinor, reqRevision] = GIT_MINIMUM_VERSION.match(regex);
-    if (major < reqMajor) {
-      throw GIT_VERSION_NOT_COMPATIBLE_ERROR;
-    } else if (major === reqMajor) {
-      if (minor < reqMinor) {
-        throw GIT_VERSION_NOT_COMPATIBLE_ERROR;
-      } else if (minor === reqMinor) {
-        if (revision < reqRevision) {
-          throw GIT_VERSION_NOT_COMPATIBLE_ERROR;
-        }
-      }
+    gitVersion = gitVersion.match(regex)[0];
+    if (semver.lt(gitVersion, GIT_MINIMUM_VERSION)) {
+      throw new Error('Minimum git version 2.33.0 is required');
     }
     process.exit(0);
   } catch (err) {
