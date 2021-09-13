a41f104e5c48d8ede7c5c4c9ffa0efd244beafe9
RahulGautamSingh <rahultesnik@gmail.com>
2021-09-13 15:47:27 +0000

    added deleted files and using simple-git

--- tools/check-git-version.js
+++ tools/check-git-version.js
@@ -1,19 +1,21 @@
 import semver from 'semver';
 import shell from 'shelljs';
+import simpleGit from 'simple-git';
 
 const GIT_MINIMUM_VERSION = '2.33.0';
-
-try {
-  const regex = /\d+\.\d+\.\d+/;
-  const { stdout } = shell.exec('git --version', { silent: true });
-  const [gitVersion] = regex.exec(stdout);
-  if (semver.lt(gitVersion, GIT_MINIMUM_VERSION)) {
-    throw new Error(
-      `Minimum git version ${GIT_MINIMUM_VERSION} is required`
-    );
+const git = simpleGit();
+async function checkGitVersion() {
+  try {
+    const regex = /\d+\.\d+\.\d+/;
+    const stdout = await git.raw('--version');
+    const [gitVersion] = regex.exec(stdout);
+    if (semver.lt(gitVersion, GIT_MINIMUM_VERSION)) {
+      throw new Error(`Minimum git version ${GIT_MINIMUM_VERSION} is required`);
+    }
+    process.exit(0);
+  } catch (err) {
+    shell.echo('ERROR:', err.message);
+    process.exit(1);
   }
-  process.exit(0);
-} catch (err) {
-  shell.echo('ERROR:', err.message);
-  process.exit(1);
 }
+checkGitVersion();
