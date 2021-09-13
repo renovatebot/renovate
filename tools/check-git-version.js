43fb3c97ccc6cbf7e56d9d207a5ee895376281ee
RahulGautamSingh <rahultesnik@gmail.com>
2021-09-13 08:25:00 +0000

    reverted unwanted changes

--- tools/check-git-version.js
+++ tools/check-git-version.js
@@ -1,21 +1,19 @@
-const semver = require('semver');
-const shell = require('shelljs');
+import semver from 'semver';
+import shell from 'shelljs';
 
 const GIT_MINIMUM_VERSION = '2.33.0';
-function checkGitVersion() {
-  try {
-    const regex = /\d+\.\d+\.\d+/;
-    const { stdout } = shell.exec('git --version', { silent: true });
-    const [ gitVersion ] = regex.exec(stdout);
-    if (semver.lt(gitVersion, GIT_MINIMUM_VERSION)) {
-      throw new Error(
-        'Minimum Git version ' + GIT_MINIMUM_VERSION + ' is required'
-      );
-    }
-    process.exit(0);
-  } catch (err) {
-    shell.echo('ERROR:', err.message);
-    process.exit(1);
+
+try {
+  const regex = /\d+\.\d+\.\d+/;
+  const { stdout } = shell.exec('git --version', { silent: true });
+  const [gitVersion] = regex.exec(stdout);
+  if (semver.lt(gitVersion, GIT_MINIMUM_VERSION)) {
+    throw new Error(
+      'Minimum Git version ' + GIT_MINIMUM_VERSION + ' is required'
+    );
   }
+  process.exit(0);
+} catch (err) {
+  shell.echo('ERROR:', err.message);
+  process.exit(1);
 }
-checkGitVersion();
