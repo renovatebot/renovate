fd293b11d11fc03708b67ee3af920814f32ab9b2
Sebastian Poxhofer <secustor@users.noreply.github.com>
2021-09-13 10:04:09 +0200

    require at least a single whitespace char after key declaaration
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/manager/gitlabci/extract.ts
+++ lib/manager/gitlabci/extract.ts
@@ -9,9 +9,9 @@ import { replaceReferenceTags } from './utils';
 
 const commentsRe = /^\s*#/;
 const whitespaceRe = /^(?<whitespace>\s*)/;
-const imageRe = /^(?<whitespace>\s*)image:\s*'?"?(?<image>[^\s'"]+|)'?"?\s*$/;
-const nameRe = /^\s*name:\s*'?"?(?<depName>[^\s'"]+|)'?"?\s*$/;
-const serviceRe = /^\s*-\s*(?:name:\s*)?'?"?(?<depName>[^\s'"]+)'?"?\s*$/;
+const imageRe = /^(?<whitespace>\s*)image:\s+'?"?(?<image>[^\s'"]+|)'?"?\s*$/;
+const nameRe = /^\s*name:\s+'?"?(?<depName>[^\s'"]+|)'?"?\s*$/;
+const serviceRe = /^\s*-\s*(?:name:\s+)?'?"?(?<depName>[^\s'"]+)'?"?\s*$/;
 
 function skipCommentLines(
   lines: string[],
