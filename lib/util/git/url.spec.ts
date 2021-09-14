d750e0f79d7dd23a4658dee417e54e2424a5acdb
Frédéric Marchand <frederic.marchand@kudelskisecurity.com>
2021-09-14 12:05:39 +0000

    Add URL test

--- lib/util/git/url.spec.ts
+++ lib/util/git/url.spec.ts
@@ -39,6 +39,13 @@ describe('util/git/url', () => {
       );
     });
 
+    it('returns https url with token with included username', () => {
+      hostRules.find.mockReturnValueOnce({ token: 'token' });
+      expect(
+        getRemoteUrlWithToken('https://oauth2:token@foo.bar/bar/foo')
+      ).toBe('https://oauth2:token@foo.bar/bar/foo');
+    });
+
     it('returns https url with token for non-http protocols', () => {
       hostRules.find.mockReturnValueOnce({ token: 'token' });
       expect(getRemoteUrlWithToken('ssh://foo.bar/')).toBe(
