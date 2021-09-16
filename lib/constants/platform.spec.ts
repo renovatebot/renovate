d676ce5b1297373db3767235cb798d4ac4fd154a
Samuel Dolt <samuel.dolt@foxtron.ch>
2021-09-16 12:47:30 +0200

    feat(datasource): add Gitlab Package support (#11672)
    Co-authored-by: HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>
    Co-authored-by: Rhys Arkins <rhys@arkins.net>

--- lib/constants/platform.spec.ts
+++ lib/constants/platform.spec.ts
@@ -1,5 +1,6 @@
 import { id as GH_RELEASES_DS } from '../datasource/github-releases';
 import { id as GH_TAGS_DS } from '../datasource/github-tags';
+import { GitlabPackagesDatasource } from '../datasource/gitlab-packages';
 import { GitlabReleasesDatasource } from '../datasource/gitlab-releases';
 import { id as GL_TAGS_DS } from '../datasource/gitlab-tags';
 import { id as POD_DS } from '../datasource/pod';
@@ -15,6 +16,9 @@ describe('constants/platform', () => {
     expect(GITLAB_API_USING_HOST_TYPES.includes(GL_TAGS_DS)).toBeTrue();
     expect(
       GITLAB_API_USING_HOST_TYPES.includes(GitlabReleasesDatasource.id)
+    ).toBeTrue();
+    expect(
+      GITLAB_API_USING_HOST_TYPES.includes(GitlabPackagesDatasource.id)
     ).toBeTrue();
     expect(
       GITLAB_API_USING_HOST_TYPES.includes(PLATFORM_TYPE_GITLAB)
