f3cef7d88364185a67f0bc858ec33663ce4e2189
Markus Siebert <mail@markussiebert.com>
2021-10-02 18:01:09 +0200

    fix: don't remove artifactory datasource

--- lib/datasource/api.ts
+++ lib/datasource/api.ts
@@ -1,4 +1,5 @@
 import { AdoptiumJavaDatasource } from './adoptium-java';
+import { ArtifactoryDatasource } from './artifactory';
 import { AwsMachineImageDataSource } from './aws-machine-image';
 import { BitBucketTagsDatasource } from './bitbucket-tags';
 import { CdnJsDatasource } from './cdnjs';
@@ -41,6 +42,7 @@ const api = new Map<string, DatasourceApi>();
 export default api;
 
 api.set(AdoptiumJavaDatasource.id, new AdoptiumJavaDatasource());
+api.set(ArtifactoryDatasource.id, new ArtifactoryDatasource());
 api.set(AwsMachineImageDataSource.id, new AwsMachineImageDataSource());
 api.set('bitbucket-tags', new BitBucketTagsDatasource());
 api.set('cdnjs', new CdnJsDatasource());
