be6eb477e63dce9a414e3d7129ac84b92c4cb878
Rhys Arkins <rhys@arkins.net>
2021-10-22 09:06:34 +0200

    chore: add test whitespace

--- lib/manager/dockerfile/extract.spec.ts
+++ lib/manager/dockerfile/extract.spec.ts
@@ -10,6 +10,7 @@ describe('manager/dockerfile/extract', () => {
       const res = extractPackageFile('no from!');
       expect(res).toBeNull();
     });
+
     it('handles naked dep', () => {
       const res = extractPackageFile('FROM node\n').deps;
       expect(res).toMatchSnapshot([
@@ -25,6 +26,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('is case insensitive', () => {
       const res = extractPackageFile('From node\n').deps;
       expect(res).toMatchSnapshot([
@@ -35,6 +37,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles tag', () => {
       const res = extractPackageFile('FROM node:8.9.0-alpine\n').deps;
       expect(res).toMatchSnapshot([
@@ -45,6 +48,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles digest', () => {
       const res = extractPackageFile(
         'FROM node@sha256:eb85fc5b1198f5e1ec025ea07586bdbbf397e7d82df66c90d7511f533517e063\n'
@@ -59,6 +63,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles tag and digest', () => {
       const res = extractPackageFile(
         'FROM node:8.9.0@sha256:eb85fc5b1198f5e1ec025ea07586bdbbf397e7d82df66c90d7511f533517e063\n'
@@ -74,6 +79,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles from as', () => {
       const res = extractPackageFile('FROM node:8.9.0-alpine as base\n').deps;
       expect(res).toMatchSnapshot([
@@ -84,6 +90,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles comments', () => {
       const res = extractPackageFile(
         '# some comment\n# another\n\nFROM node\n'
@@ -96,6 +103,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles custom hosts', () => {
       const res = extractPackageFile(
         'FROM registry2.something.info/node:8\n'
@@ -108,6 +116,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles custom hosts and suffix', () => {
       const res = extractPackageFile(
         'FROM registry2.something.info/node:8-alpine\n'
@@ -120,6 +129,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles custom hosts with port', () => {
       const res = extractPackageFile(
         'FROM registry2.something.info:5005/node:8\n'
@@ -128,6 +138,7 @@ describe('manager/dockerfile/extract', () => {
       expect(res[0].depName).toEqual('registry2.something.info:5005/node');
       expect(res[0].currentValue).toEqual('8');
     });
+
     it('handles custom hosts with port without tag', () => {
       const res = extractPackageFile(
         'FROM registry2.something.info:5005/node\n'
@@ -135,6 +146,7 @@ describe('manager/dockerfile/extract', () => {
       expect(res).toMatchSnapshot();
       expect(res[0].depName).toEqual('registry2.something.info:5005/node');
     });
+
     it('handles quay hosts with port', () => {
       const res = extractPackageFile('FROM quay.io:1234/node\n').deps;
       expect(res[0]).toMatchSnapshot({
@@ -144,6 +156,7 @@ describe('manager/dockerfile/extract', () => {
           '{{lookupName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}',
       });
     });
+
     it('handles namespaced images', () => {
       const res = extractPackageFile('FROM mynamespace/node:8\n').deps;
       expect(res).toMatchSnapshot([
@@ -154,6 +167,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles custom hosts with namespace', () => {
       const res = extractPackageFile(
         'FROM registry2.something.info/someaccount/node:8\n'
@@ -167,6 +181,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles abnormal spacing', () => {
       const res = extractPackageFile(
         'FROM    registry.allmine.info:5005/node:8.7.0\n\n'
@@ -179,6 +194,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('extracts multiple FROM tags', () => {
       const res = extractPackageFile(
         'FROM node:6.12.3 as frontend\n\n# comment\nENV foo=bar\nFROM python:3.6-slim\n'
@@ -186,10 +202,12 @@ describe('manager/dockerfile/extract', () => {
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(2);
     });
+
     it('skips scratches', () => {
       const res = extractPackageFile('FROM scratch\nADD foo\n');
       expect(res).toBeNull();
     });
+
     it('skips named multistage FROM tags', () => {
       const res = extractPackageFile(
         'FROM node:6.12.3 as frontend\n\n# comment\nENV foo=bar\nFROM frontend\n'
@@ -197,6 +215,7 @@ describe('manager/dockerfile/extract', () => {
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(1);
     });
+
     it('handles COPY --from', () => {
       const res = extractPackageFile(
         'FROM scratch\nCOPY --from=gcr.io/k8s-skaffold/skaffold:v0.11.0 /usr/bin/skaffold /usr/bin/skaffold\n'
@@ -209,6 +228,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('skips named multistage COPY --from tags', () => {
       const res = extractPackageFile(
         'FROM node:6.12.3 as frontend\n\n# comment\nENV foo=bar\nCOPY --from=frontend /usr/bin/node /usr/bin/node\n'
@@ -216,6 +236,7 @@ describe('manager/dockerfile/extract', () => {
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(1);
     });
+
     it('skips index reference COPY --from tags', () => {
       const res = extractPackageFile(
         'FROM node:6.12.3 as frontend\n\n# comment\nENV foo=bar\nCOPY --from=0 /usr/bin/node /usr/bin/node\n'
@@ -223,6 +244,7 @@ describe('manager/dockerfile/extract', () => {
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(1);
     });
+
     it('detects ["stage"] and ["final"] deps of docker multi-stage build.', () => {
       const res = extractPackageFile(
         'FROM node:8.15.1-alpine as skippedfrom\nFROM golang:1.7.3 as builder\n\n# comment\nWORKDIR /go/src/github.com/alexellis/href-counter/\nRUN go get -d -v golang.org/x/net/html  \nCOPY app.go    .\nRUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .\n\nFROM alpine:latest  \nRUN apk --no-cache add ca-certificates\nWORKDIR /root/\nCOPY --from=builder /go/src/github.com/alexellis/href-counter/app .\nCMD ["./app"]\n'
@@ -235,16 +257,19 @@ describe('manager/dockerfile/extract', () => {
       ].every(Boolean);
       expect(passed).toBe(true);
     });
+
     it('extracts images on adjacent lines', () => {
       const res = extractPackageFile(d1).deps;
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(2);
     });
+
     it('extracts images from all sorts of (maybe multiline) FROM and COPY --from statements', () => {
       const res = extractPackageFile(d2).deps;
       expect(res).toMatchSnapshot();
       expect(res).toHaveLength(9);
     });
+
     it('handles calico/node', () => {
       const res = extractPackageFile('FROM calico/node\n').deps;
       expect(res).toMatchSnapshot([
@@ -255,6 +280,7 @@ describe('manager/dockerfile/extract', () => {
         },
       ]);
     });
+
     it('handles ubuntu', () => {
       const res = extractPackageFile('FROM ubuntu:18.04\n').deps;
       expect(res).toMatchSnapshot([
