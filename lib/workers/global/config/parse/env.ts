549aa691de613aec8247fc258423d64fb3b8bc79
RahulGautamSingh <rahultesnik@gmail.com>
2021-10-23 11:16:29 +0000

    resolved merge problems

--- lib/workers/global/config/parse/env.ts
+++ lib/workers/global/config/parse/env.ts
@@ -1,11 +1,8 @@
 import is from '@sindresorhus/is';
-
 import { getOptions } from '../../../../config/options';
 import type { AllConfig, RenovateOptions } from '../../../../config/types';
 import { PlatformId } from '../../../../constants';
 import { logger } from '../../../../logger';
-import type { HostRule } from '../../../../types';
-import { regEx } from '../../../../util/regex';
 import { hostRulesFromEnv } from './host-rules-from-env';
 
 function normalizePrefixes(
@@ -32,7 +29,7 @@ export function getEnvName(option: Partial<RenovateOptions>): string {
   if (option.env) {
     return option.env;
   }
-  const nameWithUnderscores = option.name.replace(regEx(/([A-Z])/g), '_$1');
+  const nameWithUnderscores = option.name.replace(/([A-Z])/g, '_$1');
   return `RENOVATE_${nameWithUnderscores.toUpperCase()}`;
 }
 
@@ -78,7 +75,7 @@ export function getConfig(inputEnv: NodeJS.ProcessEnv): AllConfig {
   const coersions = {
     boolean: (val: string): boolean => val === 'true',
     array: (val: string): string[] => val.split(',').map((el) => el.trim()),
-    string: (val: string): string => val.replace(regEx(/\\n/g), '\n'),
+    string: (val: string): string => val.replace(/\\n/g, '\n'),
     object: (val: string): any => JSON.parse(val),
     integer: parseInt,
   };
@@ -121,59 +118,6 @@ export function getConfig(inputEnv: NodeJS.ProcessEnv): AllConfig {
     });
   }
 
-  const datasources = new Set(getDatasourceList());
-  const fields = ['token', 'username', 'password'];
-
-  const hostRules: HostRule[] = [];
-
-  const npmEnvPrefixes = ['npm_config_', 'npm_lifecycle_', 'npm_package_'];
-
-  for (const envName of Object.keys(env).sort()) {
-    if (npmEnvPrefixes.some((prefix) => envName.startsWith(prefix))) {
-      logger.trace('Ignoring npm env: ' + envName);
-      continue; // eslint-disable-line no-continue
-    }
-    // Double underscore __ is used in place of hyphen -
-    const splitEnv = envName
-      .toLowerCase()
-      .replace(regEx(/__/g), '-')
-      .split('_'); // TODO #12071
-    const hostType = splitEnv.shift();
-    if (datasources.has(hostType)) {
-      const suffix = splitEnv.pop();
-      if (fields.includes(suffix)) {
-        let matchHost: string;
-        const rule: HostRule = {};
-        rule[suffix] = env[envName];
-        if (splitEnv.length === 0) {
-          // host-less rule
-        } else if (splitEnv.length === 1) {
-          logger.warn(`Cannot parse ${envName} env`);
-          continue; // eslint-disable-line no-continue
-        } else {
-          matchHost = splitEnv.join('.');
-        }
-        const existingRule = hostRules.find(
-          (hr) => hr.hostType === hostType && hr.matchHost === matchHost
-        );
-        logger.debug(`Converting ${envName} into a global host rule`);
-        if (existingRule) {
-          // Add current field to existing rule
-          existingRule[suffix] = env[envName];
-        } else {
-          // Create a new rule
-          const newRule: HostRule = {
-            hostType,
-          };
-          if (matchHost) {
-            newRule.matchHost = matchHost;
-          }
-          newRule[suffix] = env[envName];
-          hostRules.push(newRule);
-        }
-      }
-    }
-  }
   config.hostRules = [...config.hostRules, ...hostRulesFromEnv(env)];
 
   // These env vars are deprecated and deleted to make sure they're not used
