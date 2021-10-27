f405350105fa8e84148ef825399e38ff0bd89da0
Michael Kriese <michael.kriese@visualon.de>
2021-10-27 08:22:02 +0200

    test: fix regression (#12347)

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,5 @@
 // Check for missing or pending http mocks
 import './http-mock';
-import 'expect-more-jest';
 
 jest.mock('../lib/platform', () => ({
   platform: jest.createMockFromModule('../lib/platform/github'),
