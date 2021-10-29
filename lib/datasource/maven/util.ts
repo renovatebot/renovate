170820e05f81bbda3fa4c388a28d57f89fc611bd
Anton Mostovoy <anton@mostovoy.net>
2021-10-29 11:01:38 -0500

    fix prettier linter violations

--- lib/datasource/maven/util.ts
+++ lib/datasource/maven/util.ts
@@ -115,7 +115,10 @@ export async function isHttpResourceExists(
     }
 
     const failedUrl = pkgUrl.toString();
-    logger.debug({ failedUrl, statusCode: err.statusCode }, `Can't check HTTP resource existence`);
+    logger.debug(
+      { failedUrl, statusCode: err.statusCode },
+      `Can't check HTTP resource existence`
+    );
     return null;
   }
 }
@@ -145,9 +148,11 @@ export async function downloadMavenXml(
   switch (pkgUrl.protocol) {
     case 'http:':
     case 'https:':
-      ({ authorization, body: rawContent, statusCode } = await downloadHttpProtocol(
-        pkgUrl
-      ));
+      ({
+        authorization,
+        body: rawContent,
+        statusCode,
+      } = await downloadHttpProtocol(pkgUrl));
       break;
     case 's3:':
       logger.debug('Skipping s3 dependency');
@@ -158,7 +163,10 @@ export async function downloadMavenXml(
   }
 
   if (!rawContent) {
-    logger.debug({ url: pkgUrl.toString(), statusCode}, `Content is not found for Maven url`);
+    logger.debug(
+      { url: pkgUrl.toString(), statusCode },
+      `Content is not found for Maven url`
+    );
     return {};
   }
 
