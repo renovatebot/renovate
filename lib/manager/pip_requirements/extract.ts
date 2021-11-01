131e9b0f30b4079333ade1f98a96907b4e91eb0e
Rhys Arkins <rhys@arkins.net>
2021-11-01 21:08:23 +0100

    refactor(pip_requirements): simplify extract regex

--- lib/manager/pip_requirements/extract.ts
+++ lib/manager/pip_requirements/extract.ts
@@ -53,7 +53,7 @@ export function extractPackageFile(
   }
   registryUrls = registryUrls.concat(extraUrls);
 
-  const regex = regEx(`^${dependencyPattern}$`, 'g');
+  const pkgValRegex = regEx(`^${dependencyPattern}$`);
   const deps = content
     .split('\n')
     .map((rawline) => {
@@ -62,13 +62,12 @@ export function extractPackageFile(
       if (isSkipComment(comment)) {
         dep.skipReason = SkipReason.Ignored;
       }
-      regex.lastIndex = 0;
-      const matches = regex.exec(line.split(' \\')[0]);
+      const matches = pkgValRegex.exec(line.split(' \\')[0]);
       if (!matches) {
         return null;
       }
       const [, depName, , currVal] = matches;
-      const currentValue = currVal.trim();
+      const currentValue = currVal?.trim();
       dep = {
         ...dep,
         depName,
