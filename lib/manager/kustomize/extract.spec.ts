c559f0ec0417662bd7aa2e546dff80c504b039bc
Maxime Brunet <max@brnt.mx>
2021-11-02 12:59:56 -0700

    Remove bases (likely not supported); Add images

--- lib/manager/kustomize/extract.spec.ts
+++ lib/manager/kustomize/extract.spec.ts
@@ -255,11 +255,11 @@ describe('manager/kustomize/extract', () => {
       expect(res).not.toBeNull();
       expect(res.deps).toMatchSnapshot();
       expect(res.deps).toHaveLength(3);
-      expect(res.deps[0].currentValue).toEqual('v0.0.1');
       expect(res.deps[1].currentValue).toEqual('1.19.0');
       expect(res.deps[2].currentValue).toEqual('1.18.0');
+      expect(res.deps[2].currentValue).toEqual('v0.1.0');
       expect(res.deps[1].depName).toEqual('fluxcd/flux');
-      expect(res.deps[2].depName).toEqual('fluxcd/flux');
+      expect(res.deps[2].depName).toEqual('node');
     });
 
     const postgresDigest =
