338fb6e0bf8cff40acd36de602ea91f054dff17a
Sergei Zharinov <zharinov@users.noreply.github.com>
2021-11-05 15:30:45 +0300

    refactor(go): Extract datasource detection to separate module (#12513)

--- lib/datasource/go/common.ts
+++ lib/datasource/go/common.ts
@@ -1,8 +1,11 @@
 import { Http } from '../../util/http';
+import { BitBucketTagsDatasource } from '../bitbucket-tags';
 
 export const id = 'go';
 
 export const http = new Http(id);
+
+export const bitbucket = new BitBucketTagsDatasource();
 
 export enum GoproxyFallback {
   WhenNotFoundOrGone = ',',
