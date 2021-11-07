14fcd5eb94c59dbd39032261d429b2311264b239
Nejc Habjan <hab.nejc@gmail.com>
2021-11-07 13:43:13 +0100

    chore(tools): apply review suggestions

--- tools/check-fenced-code.mjs
+++ tools/check-fenced-code.mjs
@@ -1,9 +1,14 @@
+import { promisify } from 'util';
 import fs from 'fs-extra';
-import glob from 'glob';
+import g from 'glob';
 import MarkdownIt from 'markdown-it';
 import shell from 'shelljs';
 
+const glob = promisify(g);
+
 const errorTitle = 'Invalid JSON in fenced code block';
+const errorBody =
+  'Fix this manually by ensuring each block is a valid, complete JSON document';
 const markdownGlob = 'docs/**/*.md';
 const markdown = new MarkdownIt('zero');
 
@@ -21,7 +26,7 @@ function checkValidJson(file, token) {
     issues += 1;
     if (process.env.CI) {
       shell.echo(
-        `::error file=${file},line=${start},endLine=${end},title=${errorTitle}::${err.message}`
+        `::error file=${file},line=${start},endLine=${end},title=${errorTitle}::${err.message}. ${errorBody}`
       );
     } else {
       shell.echo(
@@ -31,8 +36,8 @@ function checkValidJson(file, token) {
   }
 }
 
-function processFile(file) {
-  const text = fs.readFileSync(file, 'utf8');
+async function processFile(file) {
+  const text = await fs.readFile(file, 'utf8');
   const tokens = markdown.parse(text, undefined);
   shell.echo(`Linting ${file}..`);
 
@@ -43,12 +48,18 @@ function processFile(file) {
   });
 }
 
-glob(markdownGlob, (err, files) => {
-  files.forEach((file) => {
-    processFile(file);
-  });
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async () => {
+  const files = await glob(markdownGlob);
+
+  for (const file of files) {
+    await processFile(file);
+  }
+
   if (issues) {
-    shell.echo(`${issues} issues found.`);
+    shell.echo(
+      `${issues} issues found. ${errorBody}. See above for lines affected.`
+    );
     shell.exit(1);
   }
-});
+})();
