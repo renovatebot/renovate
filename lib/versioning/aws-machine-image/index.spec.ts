feae2b17b892b7474dad1943234ffbd96f4f9db2
Markus Siebert <mail@markussiebert.com>
2021-11-08 22:04:11 +0100

    fix: version coverage

--- lib/versioning/aws-machine-image/index.spec.ts
+++ lib/versioning/aws-machine-image/index.spec.ts
@@ -11,10 +11,20 @@ describe('versioning/aws-machine-image/index', () => {
   });
   describe('isCompatible(version)', () => {
     it('should return true', () => {
-      expect(aws.isValid('ami-00e1b2c30011d4e5f')).toBeTruthy();
+      expect(aws.isCompatible('ami-00e1b2c30011d4e5f')).toBeTruthy();
     });
     it('should return false', () => {
-      expect(aws.isValid('ami-1')).toBeFalsy();
+      expect(aws.isCompatible('ami-1')).toBeFalsy();
+    });
+  });
+  describe('isCompatible(version,range)', () => {
+    it('should return true', () => {
+      expect(
+        aws.isCompatible('ami-00e1b2c30011d4e5f', 'anything')
+      ).toBeTruthy();
+    });
+    it('should return false', () => {
+      expect(aws.isCompatible('ami-1', 'anything')).toBeFalsy();
     });
   });
   describe('isGreaterThan(version1, version2)', () => {
