1fe93e7505fedd401bac758fd516d08de7e77802
Rhys Arkins <rhys@arkins.net>
2021-11-09 07:30:22 +0100

    chore: remove unused eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,16 +33,11 @@ module.exports = {
     'import/named': 0,
     'import/namespace': 0,
     'import/no-named-as-default-member': 0,
+    'import/prefer-default-export': 0, // no benefit
 
     // other rules
-    'import/prefer-default-export': 0, // no benefit
-    'no-param-reassign': 'error',
-    'no-restricted-syntax': 0,
-    'no-await-in-loop': 0,
-    'prefer-destructuring': 0,
-    'prefer-template': 0,
-    'no-underscore-dangle': 0,
     'no-negated-condition': 'error',
+    'no-param-reassign': 'error',
     'sort-imports': [
       'error',
       {
