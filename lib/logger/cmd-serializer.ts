cb745dc23614b5c95fddc2be40df501ba03f359d
RahulGautamSingh <rahultesnik@gmail.com>
2021-11-25 05:21:41 +0000

    updated comment

--- lib/logger/cmd-serializer.ts
+++ lib/logger/cmd-serializer.ts
@@ -3,7 +3,7 @@ export default function cmdSerializer(
   cmd: string | string[]
 ): string | string[] {
   if (typeof cmd === 'string') {
-    return cmd.replace(/https:\/\/[^@]*@/g, 'https://**redacted**@'); // TODO #12070 RE2 causes circular dependency
+    return cmd.replace(/https:\/\/[^@]*@/g, 'https://**redacted**@'); // TODO #12070 using re2 cause problems in running looger.warn
   }
   return cmd;
 }
