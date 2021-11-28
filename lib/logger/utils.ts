0a3076bedba418a91e093f2d1ef19943dd6fede4
RahulGautamSingh <rahultesnik@gmail.com>
2021-11-28 14:17:07 +0000

    flagged regex with #12874 issue

--- lib/logger/utils.ts
+++ lib/logger/utils.ts
@@ -161,7 +161,7 @@ export function withSanitizer(streamConfig: bunyan.Stream): bunyan.Stream {
       const result =
         streamConfig.type === 'raw'
           ? raw
-          : JSON.stringify(raw, bunyan.safeCycles()).replace(/\n?$/, '\n'); // TODO #12070 cyclic dependency
+          : JSON.stringify(raw, bunyan.safeCycles()).replace(/\n?$/, '\n'); // TODO #12874
       stream.write(result, enc, cb);
     };
 
