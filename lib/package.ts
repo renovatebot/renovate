1896c4be0db448e55aafd9802b3628add0eff3e0
Michael Kriese <michael.kriese@visualon.de>
2021-12-18 16:50:13 +0100

    fix: add reference path

--- lib/package.ts
+++ lib/package.ts
@@ -1,10 +1,11 @@
 import fs from 'fs-extra';
 import type { PackageJson } from 'type-fest';
+import expose from './expose.cjs';
 
 export type RenovatPackageJson = PackageJson & {
   'engines-next': Record<string, string>;
 };
 
 export const pkg = JSON.parse(
-  fs.readFileSync('../package.json', 'utf-8')
+  fs.readFileSync(`${expose}/../package.json`, 'utf-8')
 ) as RenovatPackageJson;
