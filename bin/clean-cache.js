6a7dc309f119dfea0c45864c886c5d66ce358246
Michael Kriese <michael.kriese@visualon.de>
2021-12-22 11:59:55 +0100

    chore: update eslint config

--- bin/clean-cache.js
+++ bin/clean-cache.js
@@ -1,10 +1,11 @@
-const fs = require('fs-extra');
 const os = require('os');
 const path = require('path');
+const fs = require('fs-extra');
 
-(async () => {
+void (async () => {
   const tmpDir = process.env.RENOVATE_TMPDIR || os.tmpdir();
   const renovateDir = path.join(tmpDir, 'renovate');
+  // eslint-disable-next-line no-console
   console.log('Removing ' + renovateDir);
   await fs.remove(renovateDir);
 })();
--- bin/create-json-schema.js
+++ bin/create-json-schema.js
@@ -10,8 +10,12 @@ const schema = {
 };
 const options = getOptions();
 options.sort((a, b) => {
-  if (a.name < b.name) return -1;
-  if (a.name > b.name) return 1;
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
   return 0;
 });
 const properties = {};
@@ -34,7 +38,7 @@ function createSingleConfig(option) {
         temp.items.enum = option.allowedValues;
       }
     }
-    if (option.subType == 'string' && option.allowString === true) {
+    if (option.subType === 'string' && option.allowString === true) {
       const items = temp.items;
       delete temp.items;
       delete temp.type;
