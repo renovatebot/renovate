8a1aa5de6b9f9ef8960fcfb60fa7454cf68ceb53
Michael Kriese <michael.kriese@visualon.de>
2021-12-22 11:17:18 +0100

    chore: fix import

--- lib/util/html.ts
+++ lib/util/html.ts
@@ -1,12 +1,12 @@
 import type { HTMLElement, Options } from 'node-html-parser';
-import * as parser from 'node-html-parser';
+import _parse from 'node-html-parser';
 
 export type { HTMLElement, Options };
 
 export function parse(html: string, config?: Partial<Options>): HTMLElement {
   if (typeof config !== 'undefined') {
-    return parser.parse(html, config);
+    return _parse(html, config);
   }
 
-  return parser.parse(html);
+  return _parse(html);
 }
