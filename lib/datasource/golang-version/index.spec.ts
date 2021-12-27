0a6e13feffb05eca90256ff375e165b50131bdab
Oleg Krivtsov <olegkrivtsov@gmail.com>
2021-12-27 18:48:56 +0700

    Implement feature

---
+++ lib/datasource/golang-version/index.spec.ts
@@ -0,0 +1,47 @@
+import { getPkgReleases } from '..';
+import * as httpMock from '../../../test/http-mock';
+import { loadFixture } from '../../../test/util';
+import { GolangVersionDatasource } from '.';
+
+const golangReleasesContent = loadFixture('releases.go');
+
+const datasource = GolangVersionDatasource.id;
+
+describe('datasource/golang-version/index', () => {
+  describe('getReleases', () => {
+    it('parses real data', async () => {
+      httpMock
+        .scope('https://raw.githubusercontent.com')
+        .get('/golang/website/master/internal/history/release.go')
+        .reply(200, golangReleasesContent);
+      const res = await getPkgReleases({
+        datasource,
+        depName: 'golang',
+      });
+      expect(res).toMatchSnapshot();
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('throws for empty result', async () => {
+      httpMock
+        .scope('https://raw.githubusercontent.com')
+        .get('/golang/website/master/internal/history/release.go')
+        .reply(200, {});
+      await expect(
+        getPkgReleases({ datasource, depName: 'golang' })
+      ).rejects.toThrow();
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+
+    it('throws for 404', async () => {
+      httpMock
+        .scope('https://raw.githubusercontent.com')
+        .get('/golang/website/master/internal/history/release.go')
+        .reply(404);
+      await expect(
+        getPkgReleases({ datasource, depName: 'golang' })
+      ).rejects.toThrow();
+      expect(httpMock.getTrace()).toMatchSnapshot();
+    });
+  });
+});
---
+++ lib/datasource/golang-version/index.ts
@@ -0,0 +1,110 @@
+import { ExternalHostError } from '../../types/errors/external-host-error';
+import { cache } from '../../util/cache/package/decorator';
+import { HttpError } from '../../util/http/types';
+import { regEx } from '../../util/regex';
+import { isVersion, id as semverVersioningId } from '../../versioning/semver';
+import { Datasource } from '../datasource';
+import type { GetReleasesConfig, Release, ReleaseResult } from '../types';
+
+const lineTerminationRegex = regEx(`\r?\n`);
+const releaseBeginningChar = '{';
+const releaseTerminationChar = '},';
+const releaseDateRegex = regEx(
+  `Date\\{(?<year>\\d+),\\s+(?<month>\\d+),\\s+(?<day>\\d+)\\}`
+);
+const releaseVersionRegex = regEx(
+  `Version\\{(?<versionMajor>\\d+),\\s+(?<versionMinor>\\d+),\\s+(?<patch>\\d+)\\}`
+);
+
+export class GolangVersionDatasource extends Datasource {
+  static readonly id = 'golang-version';
+
+  constructor() {
+    super(GolangVersionDatasource.id);
+  }
+
+  override readonly defaultRegistryUrls = [
+    'https://raw.githubusercontent.com/golang/website/',
+  ];
+
+  override readonly customRegistrySupport = false;
+
+  override readonly defaultVersioning = semverVersioningId;
+
+  @cache({ namespace: `datasource-${GolangVersionDatasource.id}`, key: 'all' })
+  async getReleases({
+    registryUrl,
+  }: GetReleasesConfig): Promise<ReleaseResult | null> {
+    const res: ReleaseResult = {
+      homepage: 'https://go.dev/',
+      sourceUrl:
+        'https://github.com/golang/website/blob/master/internal/history/release.go',
+      releases: [],
+    };
+    const golangVersionsUrl = `${registryUrl}master/internal/history/release.go`;
+
+    try {
+      const response = await this.http.get(golangVersionsUrl);
+
+      const lines = response.body.split(lineTerminationRegex);
+
+      const startOfReleases = lines.indexOf('var Releases = []*Release{');
+      if (startOfReleases === -1) {
+        throw new Error('Invalid file - could not find the Releases section');
+      }
+
+      // Remove part before releases
+      lines.splice(0, startOfReleases + 1);
+
+      // Parse the release list
+      let release: Release = { version: undefined };
+      while (lines.length !== 0) {
+        const line = lines.shift().trim();
+
+        if (line === releaseBeginningChar) {
+          // istanbul ignore if
+          if (release.version !== undefined) {
+            throw new Error(
+              'Invalid file - unexpected error while parsing a release'
+            );
+          }
+        } else if (line === releaseTerminationChar) {
+          // istanbul ignore if
+          if (release.version === undefined) {
+            throw new Error('Invalid file - release has empty version');
+          }
+          res.releases.push(release);
+          release = { version: undefined };
+        } else {
+          const releaseDateMatch = releaseDateRegex.exec(line);
+          if (releaseDateMatch) {
+            release.releaseTimestamp = `${releaseDateMatch.groups.year}-${releaseDateMatch.groups.month}-${releaseDateMatch.groups.day}`;
+          }
+          const releaseVersionMatch = releaseVersionRegex.exec(line);
+          if (releaseVersionMatch) {
+            release.version = `${releaseVersionMatch.groups.versionMajor}.${releaseVersionMatch.groups.versionMinor}.${releaseVersionMatch.groups.patch}`;
+            // istanbul ignore if
+            if (!isVersion(release.version)) {
+              throw new Error(
+                `Version ${release.version} is not a valid semver`
+              );
+            }
+          }
+        }
+      }
+
+      // istanbul ignore if
+      if (res.releases.length === 0) {
+        throw new Error(`Invalid file - zero releases extracted`);
+      }
+    } catch (err) {
+      this.handleGenericErrors(err);
+    }
+
+    return res;
+  }
+
+  override handleSpecificErrors(err: HttpError): never | void {
+    throw new ExternalHostError(err);
+  }
+}
