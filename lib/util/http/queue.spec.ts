5e70c3079e455985d0e456f7b93b7319b10663da
Sergei Zharinov <sergio.zharinov@gmail.com>
2022-01-07 11:11:47 +0300

    refactor(util/http): Strict null checks for http utils

--- lib/util/http/queue.spec.ts
+++ lib/util/http/queue.spec.ts
@@ -2,6 +2,6 @@ import { getQueue } from './queue';
 
 describe('util/http/queue', () => {
   it('returns null for invalid URL', () => {
-    expect(getQueue(null)).toBeNull();
+    expect(getQueue('$#@!')).toBeNull();
   });
 });
