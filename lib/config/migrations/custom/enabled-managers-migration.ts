728ee61ac27ff58865e4fc26067ab19ce1851198
Maksim Sharipov <m.v.sharipov@gmail.com>
2022-01-11 21:42:24 +0100

    feat: add override modifier

--- lib/config/migrations/custom/enabled-managers-migration.ts
+++ lib/config/migrations/custom/enabled-managers-migration.ts
@@ -4,7 +4,7 @@ import { AbstractMigration } from '../base/abstract-migration';
 export class EnabledManagersMigration extends AbstractMigration {
   readonly propertyName = 'enabledManagers';
 
-  run(value: string[]): void {
+  override run(value: string[]): void {
     if (is.array(value)) {
       const newValue = value.map((manager) =>
         manager === 'yarn' ? 'npm' : manager
