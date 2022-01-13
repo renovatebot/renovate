cc2936dc35074f532bcd8ce5a31e9c15e77d0f63
Sergei Zharinov <sergio.zharinov@gmail.com>
2022-01-13 09:20:36 +0300

    Simplify recursion

--- lib/util/range.ts
+++ lib/util/range.ts
@@ -2,12 +2,8 @@ export function* range(
   start: number,
   end: number
 ): Generator<number, void, void> {
-  if (start > end) {
-    return;
+  for (let i = start; i <= end; i += 1) {
+    yield i;
   }
-  yield start;
-  if (start === end) {
-    return;
-  }
-  yield* range(start + 1, end);
+  return;
 }
