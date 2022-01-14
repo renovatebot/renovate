60ac435c45191030e0e3759cd20e1570c678f066
Sergei Zharinov <sergio.zharinov@gmail.com>
2022-01-14 08:52:10 +0300

    Add tests for MNG-6964

--- lib/versioning/maven/compare.spec.ts
+++ lib/versioning/maven/compare.spec.ts
@@ -181,6 +181,9 @@ describe('versioning/maven/compare', () => {
         ${'2.0'}                                        | ${'2.0.SP1'}
         ${'2.0-PFD2'}                                   | ${'2.0.SP1'}
         ${'1.3.9'}                                      | ${'1.3.9.fix-log4j2'}
+        ${'1-0.alpha'}                                  | ${'1'}
+        ${'1-0.beta'}                                   | ${'1'}
+        ${'1-0.alpha'}                                  | ${'1-0.beta'}
       `('$x < $y', ({ x, y }) => {
         expect(compare(x, y)).toBe(-1);
         expect(compare(y, x)).toBe(1);
