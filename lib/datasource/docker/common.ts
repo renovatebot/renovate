85810340faf23024ed5cc38c199f952cc6266f6c
Heyman <michaelheyman@users.noreply.github.com>
2022-01-18 14:51:04 -0800

    Make error check more robust and add tests

--- lib/datasource/docker/common.ts
+++ lib/datasource/docker/common.ts
@@ -512,12 +512,12 @@ export async function getLabels(
   }
 }
 
-export function isECRMaxResultsError(err): boolean {
+export function isECRMaxResultsError(err): boolean | undefined {
   return (
     err.response?.statusCode === 405 &&
-    err.response?.headers['docker-distribution-api-version'] &&
+    err.response?.headers?.['docker-distribution-api-version'] &&
     // https://docs.aws.amazon.com/AmazonECR/latest/APIReference/API_DescribeRepositories.html#ECR-DescribeRepositories-request-maxResults
-    err.response.body?.errors[0]?.message?.includes(
+    err.response.body?.errors?.[0]?.message?.includes(
       'Member must have value less than or equal to 1000'
     )
   );
