62b79baf0fc15af077b3b5d2676ffe6920bd8719
marcovmun <16606883+marcovmun@users.noreply.github.com>
2022-01-20 15:19:52 +0100

    bbUseDefaultReviewers also used on bitbucket-server platform

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -1378,7 +1378,7 @@ const options: RenovateOptions[] = [
     description: 'Use the default reviewers (Bitbucket only).',
     type: 'boolean',
     default: true,
-    supportedPlatforms: ['bitbucket'],
+    supportedPlatforms: ['bitbucket', 'bitbucket-server'],
   },
   // Automatic merging
   {
