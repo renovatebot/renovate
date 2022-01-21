a133bb96afe193679e487552519f27589dfd8496
Oleg Krivtsov <olegkrivtsov@gmail.com>
2022-01-21 15:33:22 +0700

    feat(workers/branch): allow to define a blocked label (#12164)
    Co-authored-by: Rhys Arkins <rhys@arkins.net>

--- lib/workers/branch/index.ts
+++ lib/workers/branch/index.ts
@@ -440,6 +440,26 @@ export async function processBranch(
     }
     const forcedManually = userRebaseRequested || !branchExists;
     config.forceCommit = forcedManually || branchPr?.isConflicted;
+
+    config.stopUpdating = branchPr?.labels?.includes(config.stopUpdatingLabel);
+
+    const prRebaseChecked = branchPr?.body?.includes(
+      `- [x] <!-- rebase-check -->`
+    );
+
+    if (branchExists && dependencyDashboardCheck && config.stopUpdating) {
+      if (!prRebaseChecked) {
+        logger.info(
+          'Branch updating is skipped because stopUpdatingLabel is present in config'
+        );
+        return {
+          branchExists: true,
+          prNo: branchPr?.number,
+          result: BranchResult.NoWork,
+        };
+      }
+    }
+
     const commitSha = await commitFilesToBranch(config);
     // istanbul ignore if
     if (branchPr && platform.refreshPr) {
