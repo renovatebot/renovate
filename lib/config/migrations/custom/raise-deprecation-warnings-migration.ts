4c9a4a84542cd908199e269e28691027292ba1b9
Maksim Sharipov <m.v.sharipov@gmail.com>
2022-01-23 23:30:19 +0100

    feat(migrations): raiseDeprecationWarnings

---
+++ lib/config/migrations/custom/raise-deprecation-warnings-migration.ts
@@ -0,0 +1,19 @@
+import { AbstractMigration } from '../base/abstract-migration';
+
+export class RaiseDeprecationWarningsMigration extends AbstractMigration {
+  override readonly deprecated = true;
+  override readonly propertyName = 'raiseDeprecationWarnings';
+
+  override run(value: unknown): void {
+    const suppressNotifications = this.get('suppressNotifications');
+
+    if (value === false) {
+      this.setHard(
+        'suppressNotifications',
+        Array.isArray(suppressNotifications)
+          ? suppressNotifications.concat(['deprecationWarningIssues'])
+          : ['deprecationWarningIssues']
+      );
+    }
+  }
+}
