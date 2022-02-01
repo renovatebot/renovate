9e0fc42c92343302756e2f37f46f3896c6ce0ecd
Jonas Havers <mail@jonas-havers.de>
2022-02-01 19:55:09 +0100

    feat(util): handle root relative urls for sbt-package repos
    fixes issue #13344

--- lib/datasource/sbt-plugin/util.ts
+++ lib/datasource/sbt-plugin/util.ts
@@ -1,4 +1,7 @@
+import url from 'url';
 import { regEx } from '../../util/regex';
+
+const linkRegExp = /(?<=href=['"])[^'"]*(?=\/['"])/g;
 
 export const SBT_PLUGINS_REPO =
   'https://dl.bintray.com/sbt/sbt-plugin-releases';
@@ -7,6 +10,16 @@ export function parseIndexDir(
   content: string,
   filterFn = (x: string): boolean => !regEx(/^\.+/).test(x)
 ): string[] {
-  const unfiltered = content.match(/(?<=href=['"])[^'"]*(?=\/['"])/g) || [];
+  const unfiltered = content.match(linkRegExp) || [];
   return unfiltered.filter(filterFn);
+}
+
+export function normalizeRootRelativeUrls(
+  content: string,
+  rootUrl: string | url.URL
+): string {
+  const rootRelativePath = new url.URL(rootUrl.toString()).pathname;
+  return content.replaceAll(linkRegExp, (href) =>
+    href.replace(rootRelativePath, '')
+  );
 }
