f44daca15b02b34030c1cb22d63042626d00c21a
LeoniePhiline <22329650+LeoniePhiline@users.noreply.github.com>
2022-02-10 16:47:35 +0100

    feat(composer): detect `typo3-cms-extension` type
    Detect composer.json `"type": "typo3-cms-extension"` as library,
    and thus use `"rangeStrategy": "replace"` for TYPO3 extensions
    by default.
    
    See also:
    https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ExtensionArchitecture/ComposerJson/Index.html#type

--- lib/manager/composer/range.ts
+++ lib/manager/composer/range.ts
@@ -30,7 +30,7 @@ export function getRangeStrategy(config: RangeConfig): RangeStrategy {
   }
   const isApp =
     composerJsonType &&
-    !['library', 'metapackage', 'composer-plugin'].includes(composerJsonType);
+    !['library', 'metapackage', 'composer-plugin', 'typo3-cms-extension'].includes(composerJsonType);
   if (isApp && depType === 'require') {
     // Pin dependencies if it's an app/project
     logger.trace({ dependency: depName }, 'Pinning app require');
