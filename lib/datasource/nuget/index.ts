b0ce30b59a2c31a58b2f71972b89ad13832cde9f
Sergei Zharinov <zharinov@users.noreply.github.com>
2022-02-13 16:55:03 +0300

    refactor(datasource/nuget): Convert to class (#14140)
    * refactor(datasource/nuget): Convert to class
    
    * Fix strict nulls and obsolete URL
    
    * Fixes
    
    * Fix mutability
    
    Co-authored-by: Rhys Arkins <rhys@arkins.net>
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/datasource/nuget/index.ts
+++ lib/datasource/nuget/index.ts
@@ -1,54 +1,42 @@
-import urlApi from 'url';
 import { logger } from '../../logger';
-import { regEx } from '../../util/regex';
 import * as nugetVersioning from '../../versioning/nuget';
+import { Datasource } from '../datasource';
 import type { GetReleasesConfig, ReleaseResult } from '../types';
+import { parseRegistryUrl } from './common';
 import * as v2 from './v2';
 import * as v3 from './v3';
 
-export { id } from './common';
+// https://api.nuget.org/v3/index.json is a default official nuget feed
+export const defaultRegistryUrls = ['https://api.nuget.org/v3/index.json'];
 
-export const customRegistrySupport = true;
-export const defaultRegistryUrls = [v3.getDefaultFeed()];
-export const defaultVersioning = nugetVersioning.id;
-export const registryStrategy = 'merge';
+export class NugetDatasource extends Datasource {
+  static readonly id = 'nuget';
 
-export function parseRegistryUrl(registryUrl: string): {
-  feedUrl: string;
-  protocolVersion: number;
-} {
-  try {
-    const parsedUrl = urlApi.parse(registryUrl);
-    let protocolVersion = 2;
-    const protocolVersionRegExp = regEx(/#protocolVersion=(2|3)/);
-    const protocolVersionMatch = protocolVersionRegExp.exec(parsedUrl.hash);
-    if (protocolVersionMatch) {
-      parsedUrl.hash = '';
-      protocolVersion = Number.parseInt(protocolVersionMatch[1], 10);
-    } else if (parsedUrl.pathname.endsWith('.json')) {
-      protocolVersion = 3;
+  override readonly defaultRegistryUrls = defaultRegistryUrls;
+
+  override readonly defaultVersioning = nugetVersioning.id;
+
+  override readonly registryStrategy = 'merge';
+
+  constructor() {
+    super(NugetDatasource.id);
+  }
+
+  async getReleases({
+    lookupName,
+    registryUrl,
+  }: GetReleasesConfig): Promise<ReleaseResult> {
+    logger.trace(`nuget.getReleases(${lookupName})`);
+    const { feedUrl, protocolVersion } = parseRegistryUrl(registryUrl);
+    if (protocolVersion === 2) {
+      return v2.getReleases(this.http, feedUrl, lookupName);
     }
-    return { feedUrl: urlApi.format(parsedUrl), protocolVersion };
-  } catch (err) {
-    logger.debug({ err }, `nuget registry failure: can't parse ${registryUrl}`);
-    return { feedUrl: registryUrl, protocolVersion: null };
-  }
-}
-
-export async function getReleases({
-  lookupName,
-  registryUrl,
-}: GetReleasesConfig): Promise<ReleaseResult> {
-  logger.trace(`nuget.getReleases(${lookupName})`);
-  const { feedUrl, protocolVersion } = parseRegistryUrl(registryUrl);
-  if (protocolVersion === 2) {
-    return v2.getReleases(feedUrl, lookupName);
-  }
-  if (protocolVersion === 3) {
-    const queryUrl = await v3.getResourceUrl(feedUrl);
-    if (queryUrl) {
-      return v3.getReleases(feedUrl, queryUrl, lookupName);
+    if (protocolVersion === 3) {
+      const queryUrl = await v3.getResourceUrl(this.http, feedUrl);
+      if (queryUrl) {
+        return v3.getReleases(this.http, feedUrl, queryUrl, lookupName);
+      }
     }
+    return null;
   }
-  return null;
 }
