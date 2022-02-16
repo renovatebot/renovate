8e93addfa5cb6e3c4d8b9f732d713883a06fb6c8
Timm Krause <timm.krause1@gmail.com>
2022-02-16 08:31:32 +0100

    Revert unneeded changes

--- lib/util/markdown.ts
+++ lib/util/markdown.ts
@@ -5,10 +5,8 @@ import { regEx } from './regex';
 // Generic replacements/link-breakers
 export function sanitizeMarkdown(markdown: string): string {
   let res = markdown;
-
   // Put a zero width space after every # followed by a digit
   res = res.replace(regEx(/#(\d)/gi), '#&#8203;$1');
-
   // Put a zero width space after every @ symbol to prevent unintended hyperlinking
   res = res.replace(regEx(/@/g), '@&#8203;');
   res = res.replace(regEx(/(`\[?@)&#8203;/g), '$1');
@@ -16,12 +14,10 @@ export function sanitizeMarkdown(markdown: string): string {
   res = res.replace(regEx(/\/compare\/@&#8203;/g), '/compare/@');
   res = res.replace(regEx(/(\(https:\/\/[^)]*?)\.\.\.@&#8203;/g), '$1...@');
   res = res.replace(regEx(/([\s(])#(\d+)([)\s]?)/g), '$1#&#8203;$2$3');
-
   // convert escaped backticks back to `
   const backTickRe = regEx(/&#x60;([^/]*?)&#x60;/g);
   res = res.replace(backTickRe, '`$1`');
   res = res.replace(regEx(/`#&#8203;(\d+)`/g), '`#$1`');
-
   return res;
 }
 
