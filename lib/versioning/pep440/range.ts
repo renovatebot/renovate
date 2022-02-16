fc7e15422cfd8a576821952328992f734b7d3823
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2022-02-16 16:54:49 +0200

    added precision detection function

--- lib/versioning/pep440/range.ts
+++ lib/versioning/pep440/range.ts
@@ -5,6 +5,96 @@ import { logger } from '../../logger';
 import { regEx } from '../../util/regex';
 import type { NewValueConfig } from '../types';
 
+// eslint-disable-next-line typescript-enum/no-enum
+enum ReplaceUserPolicy {
+  Major = 0,
+  Minor,
+  Micro,
+  Bug,
+}
+
+/**
+ *
+ * @param ranges A {@link Range} array consists of user's allowed range
+ * @param newVersion The newly introduced new version
+ * @returns A {@link ReplaceUserPolicy} set by the user
+ * example:
+ * let >=19.12.2,<19.13.0 be user's range of excepted updates.
+ * the corresponding return value will be "Minor".
+ * let >=19.12.2,<20.12.9, "Major" will be returned.
+ */
+function getRangePrecision(
+  ranges: Range[],
+  newVersion: string
+): ReplaceUserPolicy | null {
+  let upperBound: number[];
+  const toRelease: number[] = parseVersion(newVersion)?.release ?? [];
+  let rangePrecision: number;
+  // range is defined by a single bound.
+  // ie. <1.2.2.3;  >=7.
+  if (ranges.length !== 2) {
+    upperBound = parseVersion(ranges[0].version)?.release ?? [];
+    rangePrecision = toRelease.findIndex((el, index) => el > upperBound[index]);
+  }
+  // range is defined by both upper and lower bounds.
+  if (ranges.length === 2) {
+    const lowerBound: number[] = parseVersion(ranges[0].version)?.release ?? [];
+    upperBound = parseVersion(ranges[1].version)?.release ?? [];
+    rangePrecision = upperBound.findIndex(
+      (el, index) => el > lowerBound[index]
+    );
+  }
+  // Could not find user precision, default to the smallest possible.
+  // The following will result in a "patch"("micro") level precision,
+  //     currentValue          | rangeStrategy    | currentVersion | newVersion | expected
+  //     ${'<1.2.3'}           | ${'replace'}     | ${'1.0.0'}     | ${'1.2.3'} | ${'<1.2.4'}
+  if (rangePrecision === -1) {
+    rangePrecision = upperBound.length - 1;
+  }
+  // Tune down precision if followed by a zero
+  // The following will result in a "patch"("micro") level precision,
+  //    currentValue          | rangeStrategy    | currentVersion | newVersion | expected
+  //     ${'7+<8.0'}          | ${'replace'}     | ${'7.2.3'}     | ${'8.2.5'} | ${'7+<8.3'}
+  if (
+    rangePrecision + 1 < upperBound.length &&
+    upperBound[rangePrecision + 1] === 0
+  ) {
+    rangePrecision++;
+  }
+  return ReplaceUserPolicy[ReplaceUserPolicy[rangePrecision]];
+}
+
+/**
+ *
+ * @param baseVersion Current upper bound
+ * @param newVersion A newly accepted update version
+ * @param policy The user's range update precision
+ * @returns A string represents a future version upper bound.
+ *
+ * example: newVersion set to be 20.3.2 and policy is "Minor".
+ * 20.4.0 will be returned.
+ * if policy == "Major", 21.0.0 will be returned.
+ */
+function getPreciseFutureVersion(
+  baseVersion: string,
+  newVersion: string,
+  policy: ReplaceUserPolicy
+): string {
+  const toRelease: number[] = parseVersion(newVersion)?.release ?? [];
+  const baseRelease: number[] = parseVersion(baseVersion)?.release ?? [];
+  const futureVersion = baseRelease.map((num, index) => {
+    const toPart = toRelease[index] || 0;
+    if (index < policy) {
+      return toPart;
+    }
+    if (index === policy) {
+      return toPart + 1;
+    }
+    return 0;
+  });
+  return futureVersion.join('.');
+}
+
 function getFutureVersion(
   baseVersion: string,
   newVersion: string,
@@ -252,8 +342,17 @@ function handleReplaceStrategy(
   if (satisfies(newVersion, currentValue)) {
     return [currentValue];
   }
-  return ranges.map((range) =>
-    updateRangeValue(
+  const rangePrecision = getRangePrecision(ranges, newVersion);
+  return ranges.map((range) => {
+    if (range.operator === '<' && gte(newVersion, range.version)) {
+      const futureVersion = getPreciseFutureVersion(
+        range.version,
+        newVersion,
+        rangePrecision
+      );
+      return range.operator + futureVersion;
+    }
+    return updateRangeValue(
       {
         currentValue,
         rangeStrategy,
@@ -261,8 +360,8 @@ function handleReplaceStrategy(
         newVersion,
       },
       range
-    )
-  );
+    );
+  });
 }
 
 function handleBumpStrategy(
