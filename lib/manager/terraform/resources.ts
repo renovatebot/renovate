aa32d1ca59642075b171a75248cbd84d90689754
secustor <sebastian@poxhofer.at>
2022-02-16 23:58:43 +0100

    fix test

--- lib/manager/terraform/resources.ts
+++ lib/manager/terraform/resources.ts
@@ -56,6 +56,7 @@ export function analyseTerraformResource(
       dep.datasource = HelmDatasource.id;
       break;
 
+    /* istanbul ignore next */
     default:
       dep.skipReason = 'invalid-value';
       break;
