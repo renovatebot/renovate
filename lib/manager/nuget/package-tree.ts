93fb89bd0fe0c4f5dc4e5f01e34ab13d2d79f087
Matthew Vaughan <mattvaughan38@gmail.com>
2022-02-17 02:28:47 -0700

    Saving again

--- lib/manager/nuget/package-tree.ts
+++ lib/manager/nuget/package-tree.ts
@@ -2,6 +2,8 @@ import glob from 'glob';
 import { GlobalConfig } from '../../config/global';
 import Graph from 'graph-data-structure';
 import { logger } from '../../logger';
+import path from 'path';
+import upath from 'upath';
 import { regEx } from '../../util/regex';
 import util from 'util';
 import { readLocalFile } from '../../util/fs';
@@ -18,12 +20,11 @@ export async function getDependentPackageFiles(
   const packageFiles = await getAllPackageFiles();
   const graph = Graph();
 
-  for (const f in packageFiles) {
+  for (const f of packageFiles) {
     graph.addNode(f);
   }
 
   for (const f of packageFiles) {
-    logger.info(`Reading local file ${f}`);
     const packageFileContent = (await readLocalFile(f, 'utf8')).toString();
 
     const doc = new dom().parseFromString(packageFileContent);
@@ -31,26 +32,41 @@ export async function getDependentPackageFiles(
       '//ProjectReference/@Include',
       doc
     );
-    const projectReferences = projectReferenceAttributes.map((a) => a.value);
+    const projectReferences = projectReferenceAttributes.map((a) =>
+      upath.normalize(a.value)
+    );
+    const normalizedRelativeProjectReferences = projectReferences.map((r) =>
+      normalizeRelativePath(f, r)
+    );
 
-    logger.info(projectReferences, `Project references in ${f}`);
+    for (const ref of normalizedRelativeProjectReferences) {
+      graph.addEdge(f, ref);
+    }
   }
 
+  logger.info(graph.serialize(), 'Dependency graph');
+
   return packageFiles;
+}
+
+function normalizeRelativePath(fromPackageFile, toPackageFile): string {
+  const { localDir } = GlobalConfig.get();
+  const fromFullPath = `${localDir}/${fromPackageFile}`;
+  const toFullPath = path.resolve(path.dirname(fromFullPath), toPackageFile);
+  const relativeToPackageFile = path.relative(localDir, toFullPath);
+
+  return relativeToPackageFile;
 }
 
 // Get a list of package files in `localDir`
 async function getAllPackageFiles() {
   const { localDir } = GlobalConfig.get();
   const possiblePackageFiles = await globAsync(`${localDir}/**/*proj`);
-  logger.info(possiblePackageFiles, 'Package files in repo');
   const filteredPackageFiles = possiblePackageFiles.filter((f) =>
     regEx(/(?:cs|vb|fs)proj$/i).test(f)
   );
-  logger.info(filteredPackageFiles, 'Package files in repo');
   const relativePackageFiles = filteredPackageFiles.map((f) =>
     f.substring(localDir.length + 1)
   );
-  logger.info(relativePackageFiles, 'Package files in repo');
   return relativePackageFiles;
 }
