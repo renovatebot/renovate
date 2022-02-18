aa3e17dd4215830705bf7f6c37ac19d3eaf6f8f0
Michael Kriese <michael.kriese@visualon.de>
2022-02-18 14:08:04 +0100

    Update lib/util/fs/index.spec.ts

--- lib/util/fs/index.spec.ts
+++ lib/util/fs/index.spec.ts
@@ -22,7 +22,7 @@ import {
 jest.mock('../../util/exec/env');
 jest.mock('find-up');
 const env = mocked(_env);
-const { findUp } = mocked({ findUp: _findUp });
+const findUp = _findUp as any;
 
 describe('util/fs/index', () => {
   describe('readLocalFile', () => {
