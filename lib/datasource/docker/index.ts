5423b1ab50a36968348fd7dcafb7c0a6c8180669
MaronHatoum <maron.hatoum@whitesourcesoftware.com>
2022-02-22 09:55:44 +0200

    refactor:revert white spaces

--- lib/datasource/docker/index.ts
+++ lib/datasource/docker/index.ts
@@ -707,7 +707,7 @@ export class DockerDatasource extends Datasource {
       // prettier-ignore
       if (err.statusCode === 429 && isDockerHost(registryHost)) {
         logger.warn(
-          {registryHost, dockerRepository, err},
+          { registryHost, dockerRepository, err },
           'docker registry failure: too many requests'
         );
         throw new ExternalHostError(err);
@@ -715,7 +715,7 @@ export class DockerDatasource extends Datasource {
       // prettier-ignore
       if (err.statusCode === 401 && isDockerHost(registryHost)) {
         logger.warn(
-          {registryHost, dockerRepository, err},
+          { registryHost, dockerRepository, err },
           'docker registry failure: unauthorized'
         );
         throw new ExternalHostError(err);
