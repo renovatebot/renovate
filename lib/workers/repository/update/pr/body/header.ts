69e25452a36d6cf77f0a1dedd8facb7c5bfc5c1f
Rhys Arkins <rhys@arkins.net>
2022-03-03 10:53:23 +0100

    refactor: repository worker (#14489)
    Refactors branch and pr worker to be under repository worker.

---
+++ lib/workers/repository/update/pr/body/header.ts
@@ -0,0 +1,10 @@
+import * as template from '../../../../../util/template';
+import type { BranchConfig } from '../../../../types';
+
+// istanbul ignore next
+export function getPrHeader(config: BranchConfig): string {
+  if (!config.prHeader) {
+    return '';
+  }
+  return template.compile(config.prHeader, config) + '\n\n';
+}
