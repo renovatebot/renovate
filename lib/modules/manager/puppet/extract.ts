dd89d62c20bc339b8c4860cdc3a43813e5f6afc7
enoy19 <enoy19+github@gmail.com>
2022-03-22 16:33:12 +0100

    wip git dependency

--- lib/modules/manager/puppet/extract.ts
+++ lib/modules/manager/puppet/extract.ts
@@ -1,7 +1,12 @@
 import { logger } from '../../../logger';
+import * as gitVersioning from '../../versioning/git';
+import { GitRefsDatasource } from '../../datasource/git-refs';
 import { PuppetDatasource } from '../../datasource/puppet';
 import type { PackageDependency, PackageFile } from '../types';
-import { simpleModuleLineRegexFactory } from './constants';
+import {
+  gitModuleRegexFactory,
+  simpleModuleLineRegexFactory,
+} from './constants';
 
 export function extractPackageFile(
   content: string,
@@ -11,11 +16,11 @@ export function extractPackageFile(
 
   const deps: PackageDependency[] = [];
   const simpleModuleLineRegex = simpleModuleLineRegexFactory();
+  const gitModuleRegex = gitModuleRegexFactory();
 
   let line: RegExpExecArray;
   while ((line = simpleModuleLineRegex.exec(content)) !== null) {
     const module = line[1];
-
     const version = line[2];
 
     const dep: PackageDependency = {
@@ -24,6 +29,40 @@ export function extractPackageFile(
       packageName: module,
       currentValue: version,
       registryUrls: ['https://forgeapi.puppet.com'],
+    };
+
+    deps.push(dep);
+  }
+
+  while ((line = gitModuleRegex.exec(content)) !== null) {
+    const map = new Map();
+
+    const packageName = line.groups.packageName;
+
+    [1, 2, 3].forEach((i) => {
+      const key = line.groups[`key${i}`];
+      const value = line.groups[`value${i}`];
+
+      if (key && value) {
+        map.set(key, value);
+      }
+    });
+
+    const git = map.get('git');
+    const branch = map.get('branch');
+    const commit = map.get('commit');
+    const tag = map.get('tag');
+
+    // TODO: find out how to create proper git/github/gitlab PackageDependency
+
+    const dep: PackageDependency = {
+      depName: packageName,
+      packageName: packageName,
+      datasource: GitRefsDatasource.id,
+      currentDigest: commit,
+      currentValue: tag || branch || commit,
+      registryUrls: [git],
+      versioning: commit ? gitVersioning.id : undefined,
     };
 
     deps.push(dep);
