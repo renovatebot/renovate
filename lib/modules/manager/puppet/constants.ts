de2f13a9ab18bfdac4236de4255d684de2297c51
enoy19 <enoy19+github@gmail.com>
2022-03-22 16:15:24 +0100

    added git module regex

--- lib/modules/manager/puppet/constants.ts
+++ lib/modules/manager/puppet/constants.ts
@@ -1,2 +1,2 @@
-export const simpleModuleLineRegexFactory = () =>
-  /^mod\s*'([^']+)',\s*'([^']+)'$/gm;
+export const simpleModuleLineRegexFactory = (): RegExp => /^mod\s*'([^']+)',\s*'([^']+)'$/gm;
+export const gitModuleRegexFactory = (): RegExp => /^mod '(?<packageName>[^']+)',(?![^\n])(?:\n\s+:(?<key1>\w+)\s+=>\s+'(?<value1>[^']+)',?)(?![^\n])(?:\n\s+:(?<key2>\w+)\s+=>\s+'(?<value2>[^']+)',?)?(?![^\n])(?:\n\s+:(?<key3>\w+)\s+=>\s+'(?<value3>[^']+)',?)?/gm;
