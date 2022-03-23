aaed6a44a31c7b4dd3907902085a1a9f99d4ddca
tofferman <tofferman@bol.com>
2022-03-23 16:14:39 +0100

    3566-maven-wrapper:make sure if one of the urls is missing it is parsed correctly

--- lib/modules/manager/maven-wrapper/extract.spec.ts
+++ lib/modules/manager/maven-wrapper/extract.spec.ts
@@ -1,7 +1,13 @@
 import { loadFixture } from '../../../../test/util';
 import { extractPackageFile } from './extract';
 
-const typeBinFileContent = loadFixture('maven-wrapper.properties');
+const typeBinFileContent = loadFixture(
+  'wrapper-and-maven/maven-wrapper.properties'
+);
+const onlyWrapperProperties = loadFixture(
+  'only-wrapper/maven-wrapper.properties'
+);
+const onlyMavenProperties = loadFixture('only-maven/maven-wrapper.properties');
 
 describe('modules/manager/maven-wrapper/extract', () => {
   describe('extractPackageFile()', () => {
@@ -19,10 +25,38 @@ describe('modules/manager/maven-wrapper/extract', () => {
         {
           currentValue: '3.1.0',
           replaceString:
-            'https://artifactory.tools.bol.com/artifactory/maven-bol/org/apache/maven/wrapper/maven-wrapper/3.1.0/maven-wrapper-3.1.0',
-          datasource: 'maven-wrapper-version',
+            'https://artifactory.tools.bol.com/artifactory/maven-bol/org/apache/maven/wrapper/maven-wrapper/3.1.0/maven-wrapper-3.1.0.jar',
+          datasource: 'maven',
           depName: 'maven-wrapper',
-          versioning: 'maven-wrapper',
+          versioning: 'maven',
+        },
+      ]);
+    });
+    // takari or maven wrapper ??
+    it('extracts version for property file with only a wrapper url', () => {
+      const res = extractPackageFile(onlyWrapperProperties);
+      expect(res.deps).toEqual([
+        {
+          currentValue: '0.5.6',
+          replaceString:
+            'https://repo.maven.apache.org/maven2/io/takari/maven-wrapper/0.5.6/maven-wrapper-0.5.6.jar',
+          datasource: 'maven',
+          depName: 'maven-wrapper',
+          versioning: 'maven',
+        },
+      ]);
+    });
+
+    it('extracts version for property file with only a maven url', () => {
+      const res = extractPackageFile(onlyMavenProperties);
+      expect(res.deps).toEqual([
+        {
+          currentValue: '3.5.4',
+          replaceString:
+            'https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.5.4/apache-maven-3.5.4-bin.zip',
+          datasource: 'maven',
+          depName: 'maven',
+          versioning: 'maven',
         },
       ]);
     });
--- lib/modules/manager/maven-wrapper/extract.ts
+++ lib/modules/manager/maven-wrapper/extract.ts
@@ -1,5 +1,7 @@
 import { logger } from '../../../logger';
 import { newlineRegex, regEx } from '../../../util/regex';
+import { MavenDatasource } from '../../datasource/maven';
+import { id as versioning } from '../../versioning/maven';
 import type {
   /*PackageDependency, */ PackageDependency,
   PackageFile,
@@ -12,59 +14,58 @@ const DISTRIBUTION_URL_REGEX = regEx(
 );
 
 const WRAPPER_URL_REGEX = regEx(
-  '^(?:wrapperUrl\\s*=\\s*)(?<url>\\S*-(?<version>\\d+\\.\\d+(?:\\.\\d+)?(?:-\\w+)*))'
+  '^(?:wrapperUrl\\s*=\\s*)(?<url>\\S*-(?<version>\\d+\\.\\d+(?:\\.\\d+)?(?:-\\w+)*)(?:.jar))'
 );
 
 function extractVersions(fileContent: string): MavenVersionExtract {
   const lines = fileContent?.split(newlineRegex) ?? [];
-  const lineInfo = extractLineInfo(lines, [
-    DISTRIBUTION_URL_REGEX,
-    WRAPPER_URL_REGEX,
-  ]);
-
-  const [maven, wrapper] = lineInfo;
+  const maven = extractLineInfo(lines, DISTRIBUTION_URL_REGEX);
+  const wrapper = extractLineInfo(lines, WRAPPER_URL_REGEX);
   return { maven, wrapper };
 }
 
-function extractLineInfo(lines: string[], regexes: RegExp[]): any {
-  const result = regexes.map((regex) =>
-    lines
-      .filter((line) => line.match(regex))
-      .map((line) => {
-        const match = regex.exec(line);
-        if (match?.groups) {
-          return {
-            url: match.groups.url,
-            version: match.groups.version,
-          };
-        }
-        return null;
-      })
-  );
-
-  return [...result[0], ...result[1]];
+function extractLineInfo(lines: string[], regex: RegExp): any {
+  return lines
+    .filter((line) => line.match(regex))
+    .map((line) => {
+      const match = regex.exec(line);
+      if (match?.groups) {
+        return {
+          url: match.groups.url,
+          version: match.groups.version,
+        };
+      }
+      return null;
+    })[0];
 }
 
 export function extractPackageFile(fileContent: string): PackageFile | null {
   logger.trace('gradle-wrapper.extractPackageFile()');
   const extractResult = extractVersions(fileContent);
+  const deps = [];
   if (extractResult) {
-    const maven: PackageDependency = {
-      depName: 'maven',
-      currentValue: extractResult.maven?.version,
-      replaceString: extractResult.maven?.url,
-      datasource: 'maven',
-      versioning: 'maven',
-    };
+    if (extractResult.maven?.version) {
+      const maven: PackageDependency = {
+        depName: 'maven',
+        currentValue: extractResult.maven?.version,
+        replaceString: extractResult.maven?.url,
+        datasource: MavenDatasource.id,
+        versioning,
+      };
+      deps.push(maven);
+    }
 
-    const wrapper: PackageDependency = {
-      depName: 'maven-wrapper',
-      currentValue: extractResult.wrapper?.version,
-      replaceString: extractResult.wrapper?.url,
-      datasource: 'maven',
-      versioning: 'maven',
-    };
-    return { deps: [maven, wrapper] };
+    if (extractResult.wrapper?.version) {
+      const wrapper: PackageDependency = {
+        depName: 'maven-wrapper',
+        currentValue: extractResult.wrapper?.version,
+        replaceString: extractResult.wrapper?.url,
+        datasource: MavenDatasource.id,
+        versioning,
+      };
+      deps.push(wrapper);
+    }
+    return { deps };
   }
   return null;
 }
