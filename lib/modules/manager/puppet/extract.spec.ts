c6299078aa7c563f7442cb8128f31d82f8e22f4f
Marius Mies <mies@synyx.de>
2022-03-23 13:43:51 +0100

    Add tests for Puppet manager

---
+++ lib/modules/manager/puppet/extract.spec.ts
@@ -0,0 +1,15 @@
+import { Fixtures } from '../../../../test/fixtures';
+import { extractPackageFile } from './extract';
+
+describe('modules/manager/puppet/extract', () => {
+  describe('extractPackageFile()', () => {
+    it('returns null for empty Puppetfile', () => {
+      expect(extractPackageFile('')).toBeNull();
+    });
+    it('extracts multiple modules from Puppetfile', () => {
+      const res = extractPackageFile(Fixtures.get('Puppetfile'));
+      expect(res.deps).toMatchSnapshot();
+      expect(res.deps).toHaveLength(3);
+    });
+  });
+});
