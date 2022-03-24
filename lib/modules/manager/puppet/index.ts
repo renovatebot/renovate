fc2dc9c6db88e599125b21f4a752c18c48db58ad
lange <lange@synyx.de>
2022-03-24 09:47:42 +0100

    remove git support (made problems)

--- lib/modules/manager/puppet/index.ts
+++ lib/modules/manager/puppet/index.ts
@@ -1,5 +1,4 @@
 import { ProgrammingLanguage } from '../../../constants';
-import { GitRefsDatasource } from '../../datasource/git-refs';
 import { PuppetDatasource } from '../../datasource/puppet';
 
 export { extractPackageFile } from './extract';
@@ -10,4 +9,4 @@ export const defaultConfig = {
   fileMatch: ['^Puppetfile$'],
 };
 
-export const supportedDatasources = [PuppetDatasource.id, GitRefsDatasource.id];
+export const supportedDatasources = [PuppetDatasource.id];
