249fae4dd98028dd75c755764bea50362af5642d
lange <lange@synyx.de>
2022-03-24 10:13:17 +0100

    added registryUrls for forge

--- lib/modules/datasource/puppet/index.spec.ts
+++ lib/modules/datasource/puppet/index.spec.ts
@@ -1,7 +1,7 @@
-import {getPkgReleases} from '..';
+import { getPkgReleases } from '..';
 import * as httpMock from '../../../../test/http-mock';
-import {loadFixture} from '../../../../test/util';
-import {PuppetDatasource} from "./index";
+import { loadFixture } from '../../../../test/util';
+import { PuppetDatasource } from './index';
 
 const puppetforgeReleases = loadFixture('puppetforge-response.json');
 
@@ -18,12 +18,16 @@ describe('modules/datasource/puppet/index', () => {
       const res = await getPkgReleases({
         datasource,
         depName: 'puppetlabs/apache',
+        packageName: 'puppetlabs/apache',
+        registryUrls: ['https://forgeapi.puppet.com'],
       });
       const release = res.releases[0];
 
       expect(res.releases).toHaveLength(4);
       expect(release.version).toEqual('7.0.0');
-      expect(release.downloadUrl).toEqual('/v3/files/puppetlabs-apache-7.0.0.tar.gz');
+      expect(release.downloadUrl).toEqual(
+        '/v3/files/puppetlabs-apache-7.0.0.tar.gz'
+      );
       expect(release.releaseTimestamp).toEqual('2021-10-11 07:47:24 -0700');
       expect(release.registryUrl).toEqual('https://forgeapi.puppet.com');
       expect(res).toMatchSnapshot();
