a486c6e6a700429a16bf375bf4027a5493342883
Maksim Sharipov <m.v.sharipov@gmail.com>
2022-03-25 18:37:32 +0100

    refactor(migrations): ignoreNpmrcFile

---
+++ lib/config/migrations/custom/ignore-npmrc-file-migration.spec.ts
@@ -0,0 +1,26 @@
+import { IgnoreNpmrcFileMigration } from './ignore-npmrc-file-migration';
+
+describe('config/migrations/custom/ignore-npmrc-file-migration', () => {
+  it('should init npmrc field', () => {
+    expect(IgnoreNpmrcFileMigration).toMigrate(
+      {
+        ignoreNpmrcFile: true,
+      },
+      {
+        npmrc: '',
+      }
+    );
+  });
+
+  it('should not change npmrc field if it represents string value', () => {
+    expect(IgnoreNpmrcFileMigration).toMigrate(
+      {
+        ignoreNpmrcFile: true,
+        npmrc: '',
+      },
+      {
+        npmrc: '',
+      }
+    );
+  });
+});
