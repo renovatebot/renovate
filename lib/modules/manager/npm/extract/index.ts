d9b46689e25b7539d74d89f01f725abfd15772d1
philipabed <philip.abed@whitesourcesoftware.com>
2022-03-30 14:22:17 +0300

    remove setting pnpm constraint from extract

--- lib/modules/manager/npm/extract/index.ts
+++ lib/modules/manager/npm/extract/index.ts
@@ -209,6 +209,7 @@ export async function extractPackageFile(
       } else if (depName === 'pnpm') {
         dep.datasource = NpmDatasource.id;
         dep.commitMessageTopic = 'pnpm';
+        // constraints.pnpm = dep.currentValue;
       } else if (depName === 'vscode') {
         dep.datasource = GithubTagsDatasource.id;
         dep.packageName = 'microsoft/vscode';
