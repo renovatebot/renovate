b2051429a6d37bc21e21079c627a09544d208881
Maksim Sharipov <m.v.sharipov@gmail.com>
2022-04-02 20:12:09 +0200

    refactor(migrations): separateMajorReleases

---
+++ lib/config/migrations/custom/separate-major-release-migration.spec.ts
@@ -0,0 +1,15 @@
+import { SeparateMajorReleasesMigration } from './separate-major-release-migration';
+
+describe('config/migrations/custom/separate-major-release-migration', () => {
+  it('should migrate', () => {
+    expect(SeparateMajorReleasesMigration).toMigrate(
+      {
+        separateMajorReleases: true,
+      },
+      {
+        separateMajorMinor: true,
+        separateMajorReleases: true,
+      }
+    );
+  });
+});
