cf4d6e12902197aeeaebf2db0138733b7bd8376e
RahulGautamSingh <rahultesnik@gmail.com>
2022-04-04 17:58:22 +0530

    fix re2 issue

--- lib/modules/manager/fingerprint.spec.ts
+++ lib/modules/manager/fingerprint.spec.ts
@@ -3,11 +3,12 @@ import { getManagers, hashMap } from '.';
 
 describe('modules/manager/fingerprint', () => {
   it('validate manager hash', () => {
+    const regex = regEx(/^[a-f0-9]{64}$/gi);
     const managers = getManagers();
     for (const [manager] of managers) {
       const managerHash = hashMap.get(manager);
       expect(managerHash).toBeDefined();
-      expect(regEx(/^[a-f0-9]{64}$/gi).test(managerHash)).toBe(true);
+      expect(managerHash.match(regex)).toHaveLength(1);
     }
   });
 });
