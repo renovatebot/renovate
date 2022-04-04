0bd43f66d2ed122d4a431662b8aadea5aa593c55
RahulGautamSingh <rahultesnik@gmail.com>
2022-04-04 17:14:36 +0530

    validate sha256 format

--- lib/modules/manager/fingerprint.spec.ts
+++ lib/modules/manager/fingerprint.spec.ts
@@ -1,10 +1,13 @@
+import { regEx } from '../../util/regex';
 import { getManagers, hashMap } from '.';
 
 describe('modules/manager/fingerprint', () => {
   it('validate manager hash', () => {
     const managers = getManagers();
     for (const [manager] of managers) {
-      expect(hashMap.get(manager)).toBeDefined();
+      const managerHash = hashMap.get(manager);
+      expect(managerHash).toBeDefined();
+      expect(regEx(/^[a-f0-9]{64}$/gi).test(managerHash)).toBe(true);
     }
   });
 });
