81dfab44343f980da3553098af52741411bd544c
Sergei Zharinov <s@jrnv.me>
2022-04-06 17:40:16 +0300

    Fix

--- lib/modules/platform/github/index.ts
+++ lib/modules/platform/github/index.ts
@@ -618,7 +618,7 @@ export async function getPrList(): Promise<Pr[]> {
 
         const opts: GithubHttpOptions = { paginate: false };
         if (pageIdx === 1) {
-          const etag = config.prCache.getEtag();
+          const etag = config.prCache.etag;
           if (etag) {
             opts.headers = { 'If-None-Match': etag };
           }
@@ -636,7 +636,7 @@ export async function getPrList(): Promise<Pr[]> {
 
           const etag = res.headers.etag;
           if (etag) {
-            config.prCache.setEtag(etag);
+            config.prCache.etag = etag;
           }
         }
 
