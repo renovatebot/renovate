e1816476947230e5a85ead31be0c2ceaca4c4cc5
Sergei Zharinov <s@jrnv.me>
2022-04-06 11:08:24 +0300

    Refactor

--- lib/modules/platform/github/api-cache.spec.ts
+++ lib/modules/platform/github/api-cache.spec.ts
@@ -1,8 +1,7 @@
 import { DateTime } from 'luxon';
-import { getItem, reconcileWithPage, setItem } from './api-page-cache';
-import type { ApiPageCache } from './types';
+import { ApiCache } from './api-cache';
 
-describe('modules/platform/github/api-page-cache', () => {
+describe('modules/platform/github/api-cache', () => {
   const now = DateTime.now();
   const t1 = now.plus({ hours: 1 }).toISO();
   const t2 = now.plus({ hours: 2 }).toISO();
@@ -13,118 +12,100 @@ describe('modules/platform/github/api-page-cache', () => {
   it('stores and retrieves items', () => {
     const item1 = { number: 1, updated_at: t1 };
     const item2 = { number: 2, updated_at: t2 };
-    const cache: ApiPageCache = {
+    const apiCache = new ApiCache({
       items: { 1: item1 },
       lastUpdated: t1,
-    };
+    });
 
-    expect(getItem(cache, 1)).toBe(item1);
-    expect(getItem(cache, 2)).toBeNull();
+    expect(apiCache.getItem(1)).toBe(item1);
+    expect(apiCache.getItem(2)).toBeNull();
 
-    setItem(cache, item2);
-    expect(getItem(cache, 2)).toBe(item2);
-    expect(cache.lastUpdated).toBe(t1); // @see `setItem` jsdoc
+    apiCache.setItem(item2);
+    expect(apiCache.getItem(2)).toBe(item2);
+    expect(apiCache.lastUpdated()).toBe(t1); // Not `t2`, see jsdoc for `setItem`
+    expect(apiCache.getItems()).toEqual([item1, item2]);
   });
 
-  describe('reconcileWithPage', () => {
+  describe('reconcile', () => {
     it('appends new items', () => {
-      const cache: ApiPageCache = {
+      const apiCache = new ApiCache({
         items: {
           1: { number: 1, updated_at: t1 },
           2: { number: 2, updated_at: t2 },
         },
         lastUpdated: t2,
-      };
+      });
 
-      const needNextPage = reconcileWithPage(cache, [
+      const needNextPage = apiCache.reconcile([
         { number: 4, updated_at: t4 },
         { number: 3, updated_at: t3 },
       ]);
 
-      expect(cache).toEqual({
-        items: {
-          1: { number: 1, updated_at: t1 },
-          2: { number: 2, updated_at: t2 },
-          3: { number: 3, updated_at: t3 },
-          4: { number: 4, updated_at: t4 },
-        },
-        lastUpdated: t4,
-      });
+      expect(apiCache.getItems()).toEqual([
+        { number: 1, updated_at: t1 },
+        { number: 2, updated_at: t2 },
+        { number: 3, updated_at: t3 },
+        { number: 4, updated_at: t4 },
+      ]);
+      expect(apiCache.lastUpdated()).toBe(t4);
       expect(needNextPage).toBeTrue();
     });
 
     it('handles updated items', () => {
-      const cache: ApiPageCache = {
+      const apiCache = new ApiCache({
         items: {
           1: { number: 1, updated_at: t1 },
           2: { number: 2, updated_at: t2 },
           3: { number: 3, updated_at: t3 },
         },
         lastUpdated: t3,
-      };
+      });
 
-      const needNextPage = reconcileWithPage(cache, [
+      const needNextPage = apiCache.reconcile([
         { number: 1, updated_at: t5 },
         { number: 2, updated_at: t4 },
         { number: 3, updated_at: t3 },
       ]);
 
-      expect(cache).toEqual({
-        items: {
-          1: { number: 1, updated_at: t5 },
-          2: { number: 2, updated_at: t4 },
-          3: { number: 3, updated_at: t3 },
-        },
-        lastUpdated: t5,
-      });
+      expect(apiCache.getItems()).toEqual([
+        { number: 1, updated_at: t5 },
+        { number: 2, updated_at: t4 },
+        { number: 3, updated_at: t3 },
+      ]);
+      expect(apiCache.lastUpdated()).toBe(t5);
       expect(needNextPage).toBeFalse();
     });
 
     it('ignores page overlap', () => {
-      const cache: ApiPageCache = { items: {} };
+      const apiCache = new ApiCache({
+        items: {},
+      });
 
-      const res1 = reconcileWithPage(cache, [
+      const res1 = apiCache.reconcile([
         { number: 5, updated_at: t5 },
         { number: 4, updated_at: t4 },
         { number: 3, updated_at: t3 },
       ]);
-      const res2 = reconcileWithPage(cache, [
+      const res2 = apiCache.reconcile([
         { number: 3, updated_at: t3 },
         { number: 2, updated_at: t2 },
         { number: 1, updated_at: t1 },
       ]);
 
-      expect(cache).toEqual({
-        items: {
-          1: { number: 1, updated_at: t1 },
-          2: { number: 2, updated_at: t2 },
-          3: { number: 3, updated_at: t3 },
-          4: { number: 4, updated_at: t4 },
-          5: { number: 5, updated_at: t5 },
-        },
-        lastUpdated: t5,
-      });
+      expect(apiCache.getItems()).toEqual([
+        { number: 1, updated_at: t1 },
+        { number: 2, updated_at: t2 },
+        { number: 3, updated_at: t3 },
+        { number: 4, updated_at: t4 },
+        { number: 5, updated_at: t5 },
+      ]);
+      expect(apiCache.lastUpdated()).toBe(t5);
       expect(res1).toBeTrue();
       expect(res2).toBeTrue();
     });
 
     it('does not require new page if all items are old', () => {
-      const cache: ApiPageCache = {
-        items: {
-          1: { number: 1, updated_at: t1 },
-          2: { number: 2, updated_at: t2 },
-          3: { number: 3, updated_at: t3 },
-        },
-        lastUpdated: t3,
-      };
-
-      const needNextPage = reconcileWithPage(cache, [
-        { number: 3, updated_at: t3 },
-        { number: 2, updated_at: t2 },
-        { number: 1, updated_at: t1 },
-      ]);
-
-      expect(cache).toEqual({
+      const apiCache = new ApiCache({
         items: {
           1: { number: 1, updated_at: t1 },
           2: { number: 2, updated_at: t2 },
@@ -132,6 +113,19 @@ describe('modules/platform/github/api-page-cache', () => {
         },
         lastUpdated: t3,
       });
+
+      const needNextPage = apiCache.reconcile([
+        { number: 3, updated_at: t3 },
+        { number: 2, updated_at: t2 },
+        { number: 1, updated_at: t1 },
+      ]);
+
+      expect(apiCache.getItems()).toEqual([
+        { number: 1, updated_at: t1 },
+        { number: 2, updated_at: t2 },
+        { number: 3, updated_at: t3 },
+      ]);
+      expect(apiCache.lastUpdated()).toBe(t3);
       expect(needNextPage).toBeFalse();
     });
   });
---
+++ lib/modules/platform/github/api-cache.ts
@@ -0,0 +1,68 @@
+import { DateTime } from 'luxon';
+import type { ApiPageCache, ApiPageItem } from './types';
+
+export class ApiCache<T extends ApiPageItem> {
+  constructor(private cache: ApiPageCache<T>) {}
+
+  lastUpdated(): string | undefined {
+    return this.cache.lastUpdated;
+  }
+
+  getItems(): T[] {
+    return Object.values(this.cache.items);
+  }
+
+  getItem(number: number): T | null {
+    return this.cache.items[number] ?? null;
+  }
+
+  /**
+   * It intentionally doesn't alter `lastUpdated` cache field.
+   *
+   * The point is to allow cache modifications during run, but
+   * force fetching and refreshing of modified items next run.
+   */
+  setItem(item: T): void {
+    this.cache.items[item.number] = item;
+  }
+
+  /**
+   * Copies items from `page` to `cache`.
+   * Updates internal cache timestamp.
+   *
+   * @param cache Cache object
+   * @param page List of cacheable items, sorted by `updated_at` field
+   * starting from the most recently updated.
+   * @returns `true` when the next page is likely to contain fresh items,
+   * otherwise `false`.
+   */
+  reconcile(page: T[]): boolean {
+    const { items } = this.cache;
+    let { lastUpdated } = this.cache;
+
+    let needNextPage = true;
+
+    for (const newItem of page) {
+      const number = newItem.number;
+      const oldItem = items[number];
+
+      const itemNewTime = DateTime.fromISO(newItem.updated_at);
+      const itemOldTime = oldItem?.updated_at
+        ? DateTime.fromISO(oldItem.updated_at)
+        : null;
+
+      items[number] = newItem;
+
+      needNextPage = itemOldTime ? itemOldTime < itemNewTime : true;
+
+      const cacheOldTime = lastUpdated ? DateTime.fromISO(lastUpdated) : null;
+      if (!cacheOldTime || itemNewTime > cacheOldTime) {
+        lastUpdated = newItem.updated_at;
+      }
+    }
+
+    this.cache.lastUpdated = lastUpdated;
+
+    return needNextPage;
+  }
+}
