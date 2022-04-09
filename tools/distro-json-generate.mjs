974fa7b2a72f3ffa04e0811b7fd20dcbb232555f
Gabriel-Ladzaretti <97394622+Gabriel-Ladzaretti@users.noreply.github.com>
2022-04-09 14:38:06 +0300

    refactor(versioning/distro-info): for future use in debian and ubuntu versioning (#14880)
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    distro-info refactor
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    styling
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    distro-info refactor
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    styling
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    use https://debian.pages.debian.net/distro-info-data/ubuntu.csv as distro info data
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    cr changes and migrated from mjs to ts
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    added a crude way to prevent writing corrupted data
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    lint fixes
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    cr changes and migrated from mjs to ts
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    cr changes
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    dynamically check if stable/released.
    
    22.04 release for example
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    fixed JSON order
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    removed ts.ignore
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    cr changes
    
    * refactor(tools/distro-info): for later use in ubuntu and debian versioning
    
    cr changes
    
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- tools/distro-json-generate.mjs
+++ tools/distro-json-generate.mjs
@@ -1,73 +1,77 @@
 import fs from 'fs-extra';
+import got from 'got';
 import shell from 'shelljs';
 
-shell.echo(`Verifying required packages...`);
-
-if (!shell.which(`distro-info`)) {
-  shell.echo('This script requires distro-info, exiting...');
-  shell.exit(2);
-}
-
-if (!shell.which(`sed`)) {
-  shell.echo('This script requires sed, exiting...');
-  shell.exit(2);
-}
-
-shell.echo(`OK`);
-
-const ubuntuDistroInfo = shell.exec(
-  `ubuntu-distro-info --all -f | sed -r 's/Ubuntu|"|LTS |Debian //g; s/([0-9]+.[0-9]+) /\\1 /; s/.*/\\L&/; s/( [a-z]*) [a-z]*/\\1/g; s/^[ \\t]*//'`,
-  { silent: true }
-);
+const url = 'https://debian.pages.debian.net/distro-info-data/ubuntu.csv';
 
 /**
- * @param {string} str
- * @returns {{}}
+ * Converts valid CSV string into JSON.
+ * @param {string} raw CSV string
+ * @returns {string} JSON representation of input CSV
  */
-function objectify(str) {
-  let obj = {};
+function csvToJson(raw) {
+  const lines = raw.split(/\r?\n/);
 
-  for (const line of str.split(/\r?\n/)) {
-    let [ver, codename] = line.split(' ');
-    // eslint-disable-next-line
-    // @ts-ignore
-    obj[ver] = codename;
+  /** @type {Record<string, any>} */
+  const res = {};
+  const headers = lines[0].split(',');
+
+  // drop headers
+  lines.shift();
+
+  // drop "version" header
+  headers.shift();
+
+  for (const l of lines) {
+    if (!l) {
+      continue;
+    }
+
+    /** @type {Record<string, any>} */
+    const obj = {};
+    const line = l.split(',');
+    let ver = line?.shift()?.replace(/LTS|\s/g, '');
+
+    for (const [i, h] of headers.entries()) {
+      obj[h.replace('-', '_')] = line[i];
+    }
+
+    if (ver) {
+      // Debian related, example codename "hamm" version is 2.0
+      // change 2.0 -> 2
+      ver = ver.endsWith('.0') ? ver.replace('.0', '') : ver;
+      res[`v${ver}`] = obj;
+    }
   }
-
-  return obj;
+  return JSON.stringify(res, undefined, 2);
 }
 
 /**
- * @param {string} file
- * @param {string} newData
+ * Update given file with new provided data.
+ * @param {string} file Path to a data file
+ * @param {string} newData New data to be written
  */
 async function updateJsonFile(file, newData) {
-  let oldData;
-
-  try {
-    oldData = fs.existsSync(file) ? await fs.readFile(file, 'utf8') : null;
-    // Eliminate formatting
-    oldData = oldData?.replace(/\s/g, '') ?? null;
-  } catch (e) {
-    shell.echo(e.toString());
-    shell.exit(1);
-  }
-
-  const parsedData = JSON.stringify(objectify(newData), undefined, 2);
-
-  if (oldData === parsedData) {
-    shell.echo(`${file} is up to date.`);
-    return;
-  }
-
   try {
     shell.echo(`Updating ${file}`);
-    await fs.writeFile(file, parsedData);
+    await fs.writeFile(file, newData);
   } catch (e) {
     shell.echo(e.toString());
     shell.exit(1);
   }
+}
+
+/**
+ * Fetch CSV and update data file.
+ * @param {string} url Url to CSV
+ * @param {string} file File path to update
+ */
+async function update(url, file) {
+  const res = await got(url);
+  const csv = res.body;
+  const json = csvToJson(csv);
+  await updateJsonFile(file, json);
 }
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-updateJsonFile(`../data/ubuntu-distro-info.json`, ubuntuDistroInfo.toString());
+update(url, `./data/ubuntu-distro-info.json`);
