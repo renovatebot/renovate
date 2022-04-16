1fb352b21d85387267be288f6efaf638e72232a9
Sergei Zharinov <s@jrnv.me>
2022-04-16 10:46:50 +0300

    Move npm api init/reset

--- lib/workers/repository/init/cache.ts
+++ lib/workers/repository/init/cache.ts
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
 import type { RenovateConfig } from '../../../config/types';
+import * as npmApi from '../../../modules/datasource/npm';
 import * as memCache from '../../../util/cache/memory';
 import * as repositoryCache from '../../../util/cache/repository';
 import { privateCacheDir } from '../../../util/fs';
@@ -8,10 +9,12 @@ export async function resetCaches(): Promise<void> {
   memCache.reset();
   repositoryCache.reset();
   await fs.remove(privateCacheDir());
+  npmApi.resetMemCache();
 }
 
 export async function initializeCaches(config: RenovateConfig): Promise<void> {
   memCache.init();
   await repositoryCache.initialize(config);
   await fs.ensureDir(privateCacheDir());
+  npmApi.setNpmrc(config.npmrc);
 }
