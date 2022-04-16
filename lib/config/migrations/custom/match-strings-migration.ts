e33b0b6f71bdd53085b688874d95b53d8ff5ec7d
Maksim Sharipov <m.v.sharipov@gmail.com>
2022-04-16 20:04:33 +0200

    refactor(migrations): use regEx helper

--- lib/config/migrations/custom/match-strings-migration.ts
+++ lib/config/migrations/custom/match-strings-migration.ts
@@ -1,4 +1,5 @@
 import is from '@sindresorhus/is';
+import { regEx } from '../../../util/regex';
 import { AbstractMigration } from '../base/abstract-migration';
 
 export class MatchStringsMigration extends AbstractMigration {
@@ -9,12 +10,10 @@ export class MatchStringsMigration extends AbstractMigration {
       const newValue = value
         .filter(is.nonEmptyString)
         .map((matchString) =>
-          matchString.replace(this.#lookupName, '(?<packageName>')
+          matchString.replace(regEx(/\(\?<lookupName>/g), '(?<packageName>')
         );
 
       this.rewrite(newValue);
     }
   }
-
-  #lookupName = /\(\?<lookupName>/g;
 }
