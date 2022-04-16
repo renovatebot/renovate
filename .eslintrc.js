5cf9123017dff463ed48bafcd7507c90065c850a
Michael Kriese <michael.kriese@visualon.de>
2022-04-16 08:32:17 +0200

    refactor: more strict null checks (#15140)

--- .eslintrc.js
+++ .eslintrc.js
@@ -81,7 +81,8 @@ module.exports = {
 
     // TODO: fix lint
     '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-non-null-assertion': 2,
+    // TODO: https://github.com/renovatebot/renovate/issues/7154
+    '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/no-unused-vars': [
       2,
       {
