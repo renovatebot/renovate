653d0e9aad091c25eacff2bcc92fdeeece465f48
Sergei Zharinov <s@jrnv.me>
2022-04-16 10:37:57 +0300

    refactor: Separate repository caches init and reset phases

--- lib/workers/repository/init/cache.ts
+++ lib/workers/repository/init/cache.ts
@@ -4,10 +4,14 @@ import * as memCache from '../../../util/cache/memory';
 import * as repositoryCache from '../../../util/cache/repository';
 import { privateCacheDir } from '../../../util/fs';
 
+export async function resetCaches(): Promise<void> {
+  memCache.reset();
+  repositoryCache.reset();
+  await fs.remove(privateCacheDir());
+}
+
 export async function initializeCaches(config: RenovateConfig): Promise<void> {
   memCache.init();
   await repositoryCache.initialize(config);
-
-  await fs.remove(privateCacheDir());
   await fs.ensureDir(privateCacheDir());
 }
