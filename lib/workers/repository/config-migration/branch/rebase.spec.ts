8fe5ab30c5dd94a0d90a6d401df900e82877e754
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2022-04-19 16:28:50 +0300

    feat(config/migration): migrate config with a PR
     - added unit tests for branch/rebase

---
+++ lib/workers/repository/config-migration/branch/rebase.spec.ts
@@ -0,0 +1,79 @@
+import {
+  RenovateConfig,
+  defaultConfig,
+  git,
+  loadJsonFixture,
+  platform,
+} from '../../../../../test/util';
+import { GlobalConfig } from '../../../../config/global';
+import { MigratedData } from './migrated-data';
+import { rebaseMigrationBranch } from './rebase';
+
+jest.mock('../../../../util/git');
+
+describe('workers/repository/config-migration/branch/rebase', () => {
+  beforeAll(() => {
+    GlobalConfig.set({
+      localDir: '',
+    });
+  });
+
+  describe('rebaseMigrationBranch()', () => {
+    const raw = loadJsonFixture('./renovate.json');
+    const indent = '  ';
+    const renovateConfig = JSON.stringify(raw, undefined, indent) + '\n';
+    const fileName = 'renovate.json';
+
+    let config: RenovateConfig;
+    let migratedConfigData: MigratedData;
+
+    beforeEach(() => {
+      jest.resetAllMocks();
+      GlobalConfig.set({
+        dryRun: false,
+      });
+      migratedConfigData = new MigratedData(renovateConfig, fileName);
+      config = {
+        ...defaultConfig,
+        repository: 'some/repo',
+      };
+    });
+
+    it('does not rebase modified branch', async () => {
+      git.isBranchModified.mockResolvedValueOnce(true);
+      await rebaseMigrationBranch(config, migratedConfigData);
+      expect(git.commitFiles).toHaveBeenCalledTimes(0);
+    });
+
+    it('does nothing if branch is up to date', async () => {
+      git.getFile
+        .mockResolvedValueOnce(renovateConfig) // package.json
+        .mockResolvedValueOnce(renovateConfig); // renovate.json
+      await rebaseMigrationBranch(config, migratedConfigData);
+      expect(git.commitFiles).toHaveBeenCalledTimes(0);
+    });
+
+    it('rebases migration branch', async () => {
+      git.isBranchStale.mockResolvedValueOnce(true);
+      await rebaseMigrationBranch(config, migratedConfigData);
+      expect(git.commitFiles).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not rebases migration branch when in dryRun is on', async () => {
+      GlobalConfig.set({
+        dryRun: true,
+      });
+      git.isBranchStale.mockResolvedValueOnce(true);
+      await rebaseMigrationBranch(config, migratedConfigData);
+      expect(git.commitFiles).toHaveBeenCalledTimes(0);
+    });
+
+    it('rebases via platform', async () => {
+      platform.commitFiles = jest.fn();
+      config.platformCommit = true;
+      git.isBranchStale.mockResolvedValueOnce(true);
+      await rebaseMigrationBranch(config, migratedConfigData);
+      expect(platform.commitFiles).toHaveBeenCalledTimes(1);
+    });
+  });
+});
