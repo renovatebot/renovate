868ebbef636de7a78b719dc607001e3de391f32c
Michael Kriese <michael.kriese@visualon.de>
2022-04-20 08:40:20 +0200

    refactor(manager): final strict null checks (#15185)
    * refactor(manager): final strict null checks
    
    * refactor: fix type issues
    
    * test: fix mocking

--- lib/modules/manager/poetry/update-locked.spec.ts
+++ lib/modules/manager/poetry/update-locked.spec.ts
@@ -2,11 +2,16 @@ import { loadFixture } from '../../../../test/util';
 import type { UpdateLockedConfig } from '../types';
 import { updateLockedDependency } from '.';
 
-const lockFileContent = loadFixture('pyproject.11.toml.lock');
+const lockFile = 'pyproject.11.toml.lock';
+const packageFile = 'pyproject.11.toml';
+
+const lockFileContent = loadFixture(lockFile);
 
 describe('modules/manager/poetry/update-locked', () => {
   it('detects already updated', () => {
     const config: UpdateLockedConfig = {
+      packageFile,
+      lockFile,
       lockFileContent,
       depName: 'urllib3',
       newVersion: '1.26.3',
@@ -16,6 +21,8 @@ describe('modules/manager/poetry/update-locked', () => {
 
   it('returns unsupported', () => {
     const config: UpdateLockedConfig = {
+      packageFile,
+      lockFile,
       lockFileContent,
       depName: 'urllib3',
       newVersion: '1.26.4',
--- lib/modules/manager/types.ts
+++ lib/modules/manager/types.ts
@@ -123,7 +123,7 @@ export interface Package<T> extends ManagerData<T> {
   pinDigests?: boolean;
   currentRawValue?: string;
   major?: { enabled?: boolean };
-  prettyDepType?: any;
+  prettyDepType?: string;
 }
 
 export interface LookupUpdate {
@@ -159,7 +159,7 @@ export interface PackageDependency<T = Record<string, any>> extends Package<T> {
   digestOneAndOnly?: boolean;
   fixedVersion?: string;
   currentVersion?: string;
-  lockedVersion?: string;
+  lockedVersion?: string | null;
   propSource?: string;
   registryUrls?: string[] | null;
   rangeStrategy?: RangeStrategy;
@@ -224,13 +224,13 @@ export interface BumpPackageVersionResult {
 }
 
 export interface UpdateLockedConfig {
-  packageFile?: string;
+  packageFile: string;
   packageFileContent?: string;
-  lockFile?: string;
+  lockFile: string;
   lockFileContent?: string;
-  depName?: string;
+  depName: string;
   currentVersion?: string;
-  newVersion?: string;
+  newVersion: string;
   allowParentUpdates?: boolean;
   allowHigherOrRemoved?: boolean;
 }
@@ -296,9 +296,9 @@ export interface PostUpdateConfig<T = Record<string, any>>
   ignoreScripts?: boolean;
 
   platform?: string;
-  upgrades?: Upgrade[];
+  upgrades: Upgrade[];
   npmLock?: string;
   yarnLock?: string;
-  branchName?: string;
+  branchName: string;
   reuseExistingBranch?: boolean;
 }
