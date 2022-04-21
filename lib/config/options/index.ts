affda143098433a19f6d249d01ac0cf21528fd3c
Rhys Arkins <rhys@arkins.net>
2022-04-21 14:12:13 +0200

    Add supportedPlatforms

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2230,6 +2230,7 @@ const options: RenovateOptions[] = [
     description:
       'Overrides the default resolution for Git remote, e.g. to switch GitLab from HTTPS to SSH-based.',
     type: 'string',
+    supportedPlatforms: ['gitlab', 'bitbucket-server'],
     allowedValues: ['default', 'ssh', 'endpoint'],
     default: 'default',
     stage: 'repository',
