44070b4fce8a0d8f19c8d3dd5189a5de7a32dbd5
Sergei Zharinov <s@jrnv.me>
2022-04-26 18:28:00 +0300

    More fixes

--- lib/util/streams.ts
+++ lib/util/streams.ts
@@ -1,21 +1,6 @@
-import { Blob } from 'buffer';
-import { Readable } from 'stream';
-
 export async function streamToString(
-  stream: Readable | NodeJS.ReadableStream | ReadableStream | Blob
+  readable: NodeJS.ReadableStream
 ): Promise<string> {
-  if (stream instanceof Blob) {
-    return stream.text();
-  }
-
-  // istanbul ignore if
-  if (stream instanceof ReadableStream) {
-    const readerResult = await stream.getReader().read();
-    const readerValue = readerResult.value;
-    return `${readerValue ?? ''}`;
-  }
-
-  const readable = Readable.from(stream);
   const chunks: Uint8Array[] = [];
   const p = await new Promise<string>((resolve, reject) => {
     readable.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
