7ec22947c53880c999d8b98cb247b8349ff6b10c
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2022-04-29 22:26:08 +0900

    Added testing for singleton S3 instance.

--- lib/util/s3.spec.ts
+++ lib/util/s3.spec.ts
@@ -1,4 +1,4 @@
-import { parseS3Url } from './s3';
+import {getS3Client, parseS3Url} from './s3';
 
 describe('util/s3', () => {
   it('parses S3 URLs', () => {
@@ -14,5 +14,11 @@ describe('util/s3', () => {
 
   it('returns null for invalid URLs', () => {
     expect(parseS3Url('thisisnotaurl')).toBeNull();
+  });
+
+  it('returns a singleton S3 client instance', () => {
+    const client1 = getS3Client();
+    const client2 = getS3Client();
+    expect(client1).toBe(client2);
   });
 });
