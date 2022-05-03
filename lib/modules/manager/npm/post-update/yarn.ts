601a2dfc39e0890b126d7e5b0a9078a45419c8c4
Michael Kriese <michael.kriese@visualon.de>
2022-05-03 10:59:15 +0200

    fix: revert change (used by lerna)

--- lib/modules/manager/npm/post-update/yarn.ts
+++ lib/modules/manager/npm/post-update/yarn.ts
@@ -71,7 +71,9 @@ export async function checkYarnrc(
   return { offlineMirror, yarnPath };
 }
 
-export function getOptimizeCommand(fileName: string): string {
+export function getOptimizeCommand(
+  fileName = '/home/ubuntu/.npm-global/lib/node_modules/yarn/lib/cli.js'
+): string {
   return `sed -i 's/ steps,/ steps.slice(0,1),/' ${quote(fileName)}`;
 }
 
