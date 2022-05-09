c4f7888f2b138667bdcab3085e6548fca911aee3
Martin Herndl <martin.herndl@icis.com>
2022-05-09 22:37:44 +0200

    only include phpstan itself and extensions

--- lib/config/presets/internal/group.ts
+++ lib/config/presets/internal/group.ts
@@ -187,7 +187,7 @@ const staticGroups = {
     packageRules: [
       {
         matchDatasources: ['packagist'],
-        matchPackagePatterns: ['phpstan'],
+        matchPackagePatterns: ['^phpstan/phpstan$', '/phpstan-'],
         groupName: 'phpstan packages',
       },
     ],
