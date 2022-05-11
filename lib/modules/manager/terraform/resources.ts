cfa1f2ee9611c612df83a5947b3cc85829451479
Maurice Meyer <mmeyer@anaconda.com>
2022-05-11 13:06:15 +0200

    fixup! fix(terraform): resources can contain blocks

--- lib/modules/manager/terraform/resources.ts
+++ lib/modules/manager/terraform/resources.ts
@@ -57,7 +57,7 @@ export function extractTerraformResource(
 
     // istanbul ignore else
     if (is.string(line)) {
-      // `{` will be counted wit +1 and `}` with -1. Therefore if we reach braceCounter == 0. We have found the end of the terraform block
+      // `{` will be counted with +1 and `}` with -1. Therefore if we reach braceCounter == 0. We have found the end of the terraform block
       const openBrackets = (line.match(regEx(/\{/g)) || []).length;
       const closedBrackets = (line.match(regEx(/\}/g)) || []).length;
       braceCounter = braceCounter + openBrackets - closedBrackets;
