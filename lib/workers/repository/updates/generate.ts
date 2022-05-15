88c089e54a1ac5a0576a9ef4d8d6e836a0669237
Rhys Arkins <rhys@arkins.net>
2022-05-15 06:39:40 +0200

    fix(grouping): group update with same major version should be ignorable
    Closes #10710

--- lib/workers/repository/updates/generate.ts
+++ lib/workers/repository/updates/generate.ts
@@ -148,8 +148,12 @@ export function generateBranchConfig(
     // Delete group config regardless of whether it was applied
     delete upgrade.group;
 
-    // istanbul ignore else
-    if (toVersions.length > 1 && toValues.size > 1 && !typesGroup) {
+    if (
+      toVersions.length > 1 &&
+      toValues.size > 1 &&
+      newValue.length > 1 &&
+      !typesGroup
+    ) {
       logger.trace({ toVersions });
       logger.trace({ toValues });
       delete upgrade.commitMessageExtra;
