7e3b595943d305b07576a803eb7e219b71dbda3c
MaronHatoum <maron.hatoum@whitesourcesoftware.com>
2022-05-17 12:46:11 +0300

    feat: update

--- lib/config/types.ts
+++ lib/config/types.ts
@@ -135,7 +135,7 @@ export interface LegacyAdminConfig {
   onboardingConfigFileName?: string;
 
   platform?: string;
-  requireConfig: RequiredConfig;
+  requireConfig?: RequiredConfig;
 }
 export type ExecutionMode = 'branch' | 'update';
 
