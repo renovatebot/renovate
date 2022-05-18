cf60a57cd4002fd8490518b3e2ace79ebbc48fe4
Rhys Arkins <rhys@arkins.net>
2022-05-18 21:42:33 +0200

    fix(config): migrate requireConfig=false (#15640)

--- lib/config/migrations/custom/require-config-migration.ts
+++ lib/config/migrations/custom/require-config-migration.ts
@@ -4,9 +4,9 @@ export class RequireConfigMigration extends AbstractMigration {
   override readonly propertyName = 'requireConfig';
 
   override run(value: unknown): void {
-    if (value === 'false') {
+    if (value === false || value === 'false') {
       this.rewrite('optional');
-    } else {
+    } else if (value === true || value === 'true') {
       this.rewrite('required');
     }
   }
