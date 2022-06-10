996e4a6e579746d816220681f1ecbe5ec12f4376
RahulGautamSingh <rahultesnik@gmail.com>
2022-06-10 10:44:49 +0530

    feat: rename aliases to registryAliases (#15877)

--- lib/modules/manager/helmv3/utils.ts
+++ lib/modules/manager/helmv3/utils.ts
@@ -33,20 +33,20 @@ export function parseRepository(
  * Resolves alias in repository string.
  *
  * @param repository to be resolved string
- * @param aliases Records containing aliases as key and to be resolved URLs as values
+ * @param registryAliases Records containing registryAliases as key and to be resolved URLs as values
  *
- * @returns  resolved alias. If repository does not contain an alias the repository string will be returned. Should it contain an alias which can not be resolved using `aliases`, null will be returned
+ * @returns  resolved alias. If repository does not contain an alias the repository string will be returned. Should it contain an alias which can not be resolved using `registryAliases`, null will be returned
  */
 export function resolveAlias(
   repository: string,
-  aliases: Record<string, string>
+  registryAliases: Record<string, string>
 ): string | null {
   if (!isAlias(repository)) {
     return repository;
   }
 
   const repoWithPrefixRemoved = repository.slice(repository[0] === '@' ? 1 : 6);
-  const alias = aliases[repoWithPrefixRemoved];
+  const alias = registryAliases[repoWithPrefixRemoved];
   if (alias) {
     return alias;
   }
@@ -57,7 +57,7 @@ export function getRepositories(definitions: ChartDefinition[]): Repository[] {
   const repositoryList = definitions
     .flatMap((value) => value.dependencies)
     .filter((dependency) => dependency.repository) // only keep non-local references --> if no repository is defined the chart will be searched in charts/<name>
-    .filter((dependency) => !isAlias(dependency.repository)) // do not add aliases
+    .filter((dependency) => !isAlias(dependency.repository)) // do not add registryAliases
     .filter((dependency) => !dependency.repository.startsWith('file:')) // skip repositories which are locally referenced
     .map((dependency) => {
       // remove additional keys to prevent interference at deduplication
@@ -86,9 +86,9 @@ export function isOCIRegistry(repository: Repository): boolean {
 }
 
 export function aliasRecordToRepositories(
-  aliases: Record<string, string>
+  registryAliases: Record<string, string>
 ): Repository[] {
-  return Object.entries(aliases).map(([alias, url]) => {
+  return Object.entries(registryAliases).map(([alias, url]) => {
     return {
       name: alias,
       repository: url,
