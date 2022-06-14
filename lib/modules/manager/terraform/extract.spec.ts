a4205761c8ccb6a177b08f15e8647e27d35d62ae
Johannes Feichtner <Churro@users.noreply.github.com>
2022-06-14 21:51:11 +0200

    feat(terraform): kubernetes image resources support (#16029)
    * add tests
    
    * add support for kubernetes image resources
    
    * Update lib/modules/manager/terraform/extract.ts
    
    Co-authored-by: Sebastian Poxhofer <secustor@users.noreply.github.com>
    
    * added unknown resource
    
    * added Kubernetes to Readme
    
    * missed one spot in the Readme
    
    * Update lib/modules/manager/terraform/readme.md
    
    Co-authored-by: Sebastian Poxhofer <secustor@users.noreply.github.com>
    
    * switch to toMatchObject() in tests
    
    Co-authored-by: Sebastian Poxhofer <secustor@users.noreply.github.com>
    Co-authored-by: Rhys Arkins <rhys@arkins.net>
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/modules/manager/terraform/extract.spec.ts
+++ lib/modules/manager/terraform/extract.spec.ts
@@ -10,6 +10,7 @@ const bitbucketModules = Fixtures.get('bitbucketModules.tf');
 const azureDevOpsModules = Fixtures.get('azureDevOpsModules.tf');
 const providers = Fixtures.get('providers.tf');
 const docker = Fixtures.get('docker.tf');
+const kubernetes = Fixtures.get('kubernetes.tf');
 
 const tf2 = `module "relative" {
   source = "../fe"
@@ -102,6 +103,90 @@ describe('modules/manager/terraform/extract', () => {
       expect(res.deps).toHaveLength(8);
       expect(res.deps.filter((dep) => dep.skipReason)).toHaveLength(5);
       expect(res).toMatchSnapshot();
+    });
+
+    it('extracts kubernetes resources', async () => {
+      const res = await extractPackageFile(kubernetes, 'kubernetes.tf', {});
+      expect(res.deps).toHaveLength(16);
+      expect(res.deps.filter((dep) => dep.skipReason)).toHaveLength(2);
+      expect(res.deps).toMatchObject([
+        {
+          depName: 'gcr.io/kaniko-project/executor',
+          currentValue: 'v1.7.0',
+          currentDigest:
+            'sha256:8504bde9a9a8c9c4e9a4fe659703d265697a36ff13607b7669a4caa4407baa52',
+          depType: 'kubernetes_cron_job_v1',
+        },
+        {
+          depName: 'gcr.io/kaniko-project/executor',
+          currentValue: 'v1.8.0',
+          currentDigest:
+            'sha256:8504bde9a9a8c9c4e9a4fe659703d265697a36ff13607b7669a4caa4407baa52',
+          depType: 'kubernetes_cron_job',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.1',
+          depType: 'kubernetes_daemon_set_v1',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.2',
+          depType: 'kubernetes_daemonset',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.3',
+          depType: 'kubernetes_deployment',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.4',
+          depType: 'kubernetes_deployment_v1',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.5',
+          depType: 'kubernetes_job',
+        },
+        { skipReason: 'invalid-dependency-specification' },
+        { skipReason: 'invalid-value' },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.6',
+          depType: 'kubernetes_job_v1',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.7',
+          depType: 'kubernetes_pod',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.8',
+          depType: 'kubernetes_pod_v1',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.9',
+          depType: 'kubernetes_replication_controller',
+        },
+        {
+          depName: 'nginx',
+          currentValue: '1.21.10',
+          depType: 'kubernetes_replication_controller_v1',
+        },
+        {
+          depName: 'prom/prometheus',
+          currentValue: 'v2.2.1',
+          depType: 'kubernetes_stateful_set',
+        },
+        {
+          depName: 'prom/prometheus',
+          currentValue: 'v2.2.2',
+          depType: 'kubernetes_stateful_set_v1',
+        },
+      ]);
     });
 
     it('returns null if only local deps', async () => {
