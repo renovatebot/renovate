971ae85acf1231a1dd5b2bc118dbe53fce539c7d
Jason Bedard <jason+github@jbedard.ca>
2022-06-18 01:16:53 -0400

    remove "versioning: semverVersioning"

--- lib/modules/manager/bazelisk/index.ts
+++ lib/modules/manager/bazelisk/index.ts
@@ -1,11 +1,9 @@
 import { GithubReleasesDatasource } from '../../datasource/github-releases';
-import * as semverVersioning from '../../versioning/semver';
 
 export { extractPackageFile } from './extract';
 
 export const defaultConfig = {
   fileMatch: ['(^|/)\\.bazelversion$'],
-  versioning: semverVersioning.id,
   pinDigests: false,
 };
 
