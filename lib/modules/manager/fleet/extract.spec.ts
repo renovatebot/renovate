0f8005e111760a813e00cb724a61f7283c8bc5ec
Sebastian Poxhofer <secustor@users.noreply.github.com>
2022-06-21 22:10:24 +0200

    Update lib/modules/manager/fleet/extract.spec.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/modules/manager/fleet/extract.spec.ts
+++ lib/modules/manager/fleet/extract.spec.ts
@@ -1,5 +1,5 @@
 import { Fixtures } from '../../../../test/fixtures';
-import { extractPackageFile } from './extract';
+import { extractPackageFile } from '.';
 
 const validFleetYaml = Fixtures.get('valid_fleet.yaml');
 const inValidFleetYaml = Fixtures.get('invalid_fleet.yaml');
