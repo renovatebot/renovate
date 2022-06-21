b328a12850f16dc1eae8def5c6fe6b2807bda153
philipabed <philip.abed@whitesourcesoftware.com>
2022-06-21 14:00:39 +0300

    pr body renovate version

--- lib/modules/platform/bitbucket-server/index.ts
+++ lib/modules/platform/bitbucket-server/index.ts
@@ -973,7 +973,6 @@ export function massageMarkdown(input: string): string {
     .replace(regEx(/<\/?summary>/g), '**')
     .replace(regEx(/<\/?details>/g), '')
     .replace(regEx(`\n---\n\n.*?<!-- rebase-check -->.*?(\n|$)`), '')
-    .replace(regEx(`\n---\n\n.*?<!-- rebase-check -->.*?\n`), '')
     .replace(regEx('<!--.*?-->', 'g'), '')
     .replace(regEx(/<!--renovate-pr-data:.*?-->/), '');
 }
