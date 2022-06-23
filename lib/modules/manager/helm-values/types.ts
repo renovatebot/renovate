2e895cd7c7ff5d8fcf17281db155e33092d0d1c3
Karl-Johan Grahn <kjgrahn@kth.se>
2022-06-23 12:13:03 +0200

    add spaces for lint

--- lib/modules/manager/helm-values/types.ts
+++ lib/modules/manager/helm-values/types.ts
@@ -2,13 +2,16 @@ export interface HelmDockerImageDependencyBasic {
   registry?: string;
   repository: string;
 }
+
 export interface HelmDockerImageDependencyTag extends HelmDockerImageDependencyBasic {
   tag: string;
   version?: never;
 }
+
 export interface HelmDockerImageDependencyVersion extends HelmDockerImageDependencyBasic {
   version: string;
   tag?: never;
 }
+
 export type HelmDockerImageDependency = HelmDockerImageDependencyTag |
 HelmDockerImageDependencyVersion;
