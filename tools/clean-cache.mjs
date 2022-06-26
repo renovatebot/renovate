5803eb6f5d8051086aa8b5c413dba1abffc0d676
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2022-06-26 12:23:41 +0300

    fix(tools/clean-cache): fix upath import

--- tools/clean-cache.mjs
+++ tools/clean-cache.mjs
@@ -1,7 +1,8 @@
 import { tmpdir } from 'os';
 import { remove } from 'fs-extra';
-import { join } from 'upath';
+import upath from 'upath';
 
+const { join } = upath;
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   const tmpDir = process.env.RENOVATE_TMPDIR ?? tmpdir();
