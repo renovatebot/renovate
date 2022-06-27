b40fd6a32cca92c3302170f95c9a953353070965
Yun Lai <ylai@squareup.com>
2022-06-27 15:08:06 +1000

    feat: add versioning for Hermit package manager

---
+++ lib/modules/versioning/hermit/index.spec.ts
@@ -0,0 +1,276 @@
+import { HermitVersioning } from './index';
+
+describe('modules/versioning/hermit/index', () => {
+  const versioning = new HermitVersioning();
+
+  describe('_parseVersion', () => {
+    test.each`
+      version           | release           | compatibility | prerelease
+      ${'17'}           | ${[17, 0, 0]}     | ${undefined}  | ${undefined}
+      ${'17.2'}         | ${[17, 2, 0]}     | ${undefined}  | ${undefined}
+      ${'17.2.3a1'}     | ${[17, 2, 3]}     | ${undefined}  | ${'a1'}
+      ${'17.2.3-foo'}   | ${[17, 2, 3]}     | ${undefined}  | ${'foo'}
+      ${'17.2.3+m1'}    | ${[17, 2, 3]}     | ${'m1'}       | ${undefined}
+      ${'17.2.3_40+m1'} | ${[17, 2, 3, 40]} | ${'m1'}       | ${undefined}
+      ${'@17'}          | ${[17]}           | ${undefined}  | ${undefined}
+      ${'@17.2'}        | ${[17, 2]}        | ${undefined}  | ${undefined}
+      ${'@17.2.3_40'}   | ${[17, 2, 3, 40]} | ${undefined}  | ${undefined}
+    `(
+      'getReleases for "$version"',
+      ({ version, release, compatibility, prerelease }) => {
+        const res = versioning._parseVersion(version);
+        if (res === null) {
+          throw new Error('res is null');
+        }
+        expect(res.release).toStrictEqual(release);
+        expect(res.compatibility).toStrictEqual(compatibility);
+        expect(res.prerelease).toStrictEqual(prerelease);
+      }
+    );
+
+    it('non semver channel will receive a null', () => {
+      expect(versioning._parseVersion('@stable')).toBeNull();
+    });
+  });
+
+  describe('_isChannel', () => {
+    test.each`
+      version                     | expected
+      ${'1'}                      | ${false}
+      ${'1rc1'}                   | ${false}
+      ${'1-foo'}                  | ${false}
+      ${'1+bar'}                  | ${false}
+      ${'1.2'}                    | ${false}
+      ${'1.2-foo'}                | ${false}
+      ${'1.2+bar'}                | ${false}
+      ${'1.2.3'}                  | ${false}
+      ${'1.2.3rc1'}               | ${false}
+      ${'1.2.3-foo'}              | ${false}
+      ${'1.2.3+bar'}              | ${false}
+      ${'17.0.1_12'}              | ${false}
+      ${'17.0.1_12+m1'}           | ${false}
+      ${'17.0.1_12+m1'}           | ${false}
+      ${'11.0.11_9-zulu11.48.21'} | ${false}
+      ${'1.2-kotlin.3'}           | ${false}
+      ${'@1'}                     | ${true}
+      ${'@1.2'}                   | ${true}
+      ${'@1.2.3'}                 | ${true}
+      ${'@latest'}                | ${true}
+      ${'@stable'}                | ${true}
+    `('isChannel("$version") === $expected', ({ version, expected }) => {
+      expect(!!versioning._isChannel(version)).toBe(expected);
+    });
+  });
+
+  describe('_isSemverChannel', () => {
+    test.each`
+      version      | expected
+      ${'@1'}      | ${true}
+      ${'@1.2'}    | ${true}
+      ${'@1.2.3'}  | ${true}
+      ${'@latest'} | ${false}
+      ${'@stable'} | ${false}
+    `('isSemverChannel("$version") === $expected', ({ version, expected }) => {
+      expect(!!versioning._isSemverChannel(version)).toBe(expected);
+    });
+  });
+
+  describe('isStable', () => {
+    test.each`
+      version      | expected
+      ${'1'}       | ${true}
+      ${'1.2'}     | ${true}
+      ${'@1'}      | ${false}
+      ${'@1.2'}    | ${false}
+      ${'@1.2.3'}  | ${false}
+      ${'@latest'} | ${false}
+      ${'@stable'} | ${false}
+    `('isStable("$version") === $expected', ({ version, expected }) => {
+      expect(!!versioning.isStable(version)).toBe(expected);
+    });
+  });
+
+  describe('isValid', () => {
+    test.each`
+      version                     | expected
+      ${'1'}                      | ${true}
+      ${'1rc1'}                   | ${true}
+      ${'1-foo'}                  | ${true}
+      ${'1+bar'}                  | ${true}
+      ${'1.2'}                    | ${true}
+      ${'1.2-foo'}                | ${true}
+      ${'1.2+bar'}                | ${true}
+      ${'1.2.3'}                  | ${true}
+      ${'1.2.3rc1'}               | ${true}
+      ${'1.2.3-foo'}              | ${true}
+      ${'1.2.3+bar'}              | ${true}
+      ${'17.0.1_12'}              | ${true}
+      ${'17.0.1_12+m1'}           | ${true}
+      ${'17.0.1_12+m1'}           | ${true}
+      ${'11.0.11_9-zulu11.48.21'} | ${true}
+      ${'1.2-kotlin.3'}           | ${true}
+      ${'@1'}                     | ${true}
+      ${'@1.2'}                   | ${true}
+      ${'@1.2.3'}                 | ${true}
+      ${'@latest'}                | ${true}
+      ${'@stable'}                | ${true}
+    `('isValid("$version") === $expected', ({ version, expected }) => {
+      expect(!!versioning.isValid(version)).toBe(expected);
+    });
+  });
+
+  describe('getMajor, getMinor, getPatch', () => {
+    test.each`
+      version         | major   | minor   | patch
+      ${'17'}         | ${17}   | ${0}    | ${0}
+      ${'17.2'}       | ${17}   | ${2}    | ${0}
+      ${'17.2.3a1'}   | ${17}   | ${2}    | ${3}
+      ${'17.2.3-foo'} | ${17}   | ${2}    | ${3}
+      ${'17.2.3+m1'}  | ${17}   | ${2}    | ${3}
+      ${'@17'}        | ${17}   | ${null} | ${null}
+      ${'@17.2'}      | ${17}   | ${2}    | ${null}
+      ${'@stable'}    | ${null} | ${null} | ${null}
+    `(
+      'getMajor, getMinor, getPatch for "$version"',
+      ({ version, major, minor, patch }) => {
+        expect(versioning.getMajor(version)).toBe(major);
+        expect(versioning.getMinor(version)).toBe(minor);
+        expect(versioning.getPatch(version)).toBe(patch);
+      }
+    );
+  });
+
+  describe('equals', () => {
+    test.each`
+      version       | other        | expected
+      ${'1'}        | ${'1.2'}     | ${false}
+      ${'@1'}       | ${'@1.2'}    | ${false}
+      ${'@1.2'}     | ${'@1.2'}    | ${true}
+      ${'@1.2'}     | ${'@1.3'}    | ${false}
+      ${'@1.2.3'}   | ${'@1.2'}    | ${false}
+      ${'@1.2.3_4'} | ${'@1.2.3'}  | ${false}
+      ${'@latest'}  | ${'@1'}      | ${false}
+      ${'@stable'}  | ${'@stable'} | ${true}
+      ${'stable'}   | ${'stable'}  | ${true}
+    `('equals("$version") === $expected', ({ version, other, expected }) => {
+      expect(!!versioning.equals(version, other)).toBe(expected);
+    });
+  });
+
+  describe('matches', () => {
+    test.each`
+      version      | other        | expected
+      ${'@1'}      | ${'@1.2'}    | ${false}
+      ${'@1.2'}    | ${'@1.2'}    | ${true}
+      ${'@1.2.3'}  | ${'@1.2'}    | ${false}
+      ${'@latest'} | ${'@1'}      | ${false}
+      ${'@stable'} | ${'@stable'} | ${true}
+    `('matches("$version") === $expected', ({ version, other, expected }) => {
+      expect(!!versioning.matches(version, other)).toBe(expected);
+    });
+  });
+
+  describe('isGreaterThan', () => {
+    test.each`
+      version      | other        | expected
+      ${'@1'}      | ${'@1.2'}    | ${true}
+      ${'@1.2'}    | ${'@1.2'}    | ${false}
+      ${'@1.2'}    | ${'@1.3'}    | ${false}
+      ${'@1.2.3'}  | ${'@1.2'}    | ${false}
+      ${'1.2.3'}   | ${'@latest'} | ${true}
+      ${'@latest'} | ${'@1'}      | ${false}
+      ${'@stable'} | ${'@latest'} | ${true}
+      ${'@latest'} | ${'@stable'} | ${false}
+    `(
+      'isGreaterThan("$version", "$other") === $expected',
+      ({ version, other, expected }) => {
+        expect(!!versioning.isGreaterThan(version, other)).toBe(expected);
+      }
+    );
+  });
+
+  describe('isLesserThanRange', () => {
+    test.each`
+      version      | other        | expected
+      ${'@1'}      | ${'@1.2'}    | ${false}
+      ${'@1.2'}    | ${'@1.2'}    | ${true}
+      ${'@1.2.3'}  | ${'@1.2'}    | ${true}
+      ${'@latest'} | ${'@1'}      | ${true}
+      ${'@stable'} | ${'@latest'} | ${false}
+      ${'@latest'} | ${'@stable'} | ${true}
+    `(
+      'isLessThanRange("$version") === $expected',
+      ({ version, other, expected }) => {
+        expect(!!versioning.isLessThanRange(version, other)).toBe(expected);
+      }
+    );
+  });
+
+  it('getSatisfyingVersion', () => {
+    expect(versioning.getSatisfyingVersion(['@1.1.1', '1.2.3'], '1.2.3')).toBe(
+      '1.2.3'
+    );
+    expect(
+      versioning.getSatisfyingVersion(
+        ['1.1.1', '@2.2.1', '2.2.2', '3.3.3'],
+        '2.2.2'
+      )
+    ).toBe('2.2.2');
+    expect(
+      versioning.getSatisfyingVersion(
+        ['1.1.1', '@1.3.3', '2.2.2', '3.3.3'],
+        '1.2.3'
+      )
+    ).toBeNull();
+  });
+
+  it('minSatisfyingVersion', () => {
+    expect(versioning.minSatisfyingVersion(['@1.1.1', '1.2.3'], '1.2.3')).toBe(
+      '1.2.3'
+    );
+    expect(
+      versioning.minSatisfyingVersion(
+        ['1.1.1', '@1.2.3', '2.2.2', '3.3.3'],
+        '2.2.2'
+      )
+    ).toBe('2.2.2');
+    expect(
+      versioning.minSatisfyingVersion(
+        ['1.1.1', '@1.2.2', '2.2.2', '3.3.3'],
+        '1.2.3'
+      )
+    ).toBeNull();
+  });
+
+  describe('sortVresions', () => {
+    it('sorts versions in an ascending order', () => {
+      expect(
+        [
+          '@1',
+          '1.1',
+          '1.2',
+          '1.2.3',
+          '1.3',
+          '@1.2',
+          '@2',
+          '2',
+          '2.1',
+          '@stable',
+          '@latest',
+        ].sort(versioning.sortVersions.bind(versioning))
+      ).toEqual([
+        '@latest',
+        '@stable',
+        '1.1',
+        '1.2',
+        '1.2.3',
+        '@1.2',
+        '1.3',
+        '@1',
+        '2',
+        '2.1',
+        '@2',
+      ]);
+    });
+  });
+});
---
+++ lib/modules/versioning/hermit/index.ts
@@ -0,0 +1,233 @@
+import { regEx } from '../../../util/regex';
+import { RegExpVersion, RegExpVersioningApi } from '../regex';
+import type { VersioningApiConstructor } from '../types';
+
+export class HermitVersioning extends RegExpVersioningApi {
+  static versionRegex =
+    '^(?<major>\\d+)(\\.(?<minor>\\d+))?(\\.(?<patch>\\d+))?(_(?<build>\\d+))?([-]?(?<prerelease>[^.+][^+]*))?([+](?<compatibility>[^.-][^+]*))?$';
+  private _hermitConfig: RegExp | null = null;
+
+  public constructor() {
+    super(HermitVersioning.versionRegex);
+    this._hermitConfig = regEx(HermitVersioning.versionRegex);
+  }
+
+  _parseVersion(version: string): RegExpVersion | null {
+    return this._parse(version);
+  }
+
+  private _isValidSemver(version: string): boolean {
+    const parsed = super._parse(version);
+    return parsed !== null;
+  }
+
+  protected override _parse(version: string): RegExpVersion | null {
+    const parsed = super._parse(version);
+    if (parsed) {
+      return parsed;
+    }
+    const channelVer = this._getChannel(version);
+
+    const groups = this._hermitConfig?.exec(channelVer)?.groups;
+
+    if (!groups) {
+      return null;
+    }
+
+    const { major, minor, patch, build, prerelease, compatibility } = groups;
+    const release = [];
+
+    if (major) {
+      release.push(Number.parseInt(major, 10));
+    }
+
+    if (minor) {
+      release.push(Number.parseInt(minor, 10));
+    }
+    if (patch) {
+      release.push(Number.parseInt(patch, 10));
+    }
+    if (build) {
+      release.push(Number.parseInt(build, 10));
+    }
+
+    return {
+      release,
+      prerelease: prerelease,
+      compatibility: compatibility,
+    };
+  }
+
+  _isChannel(version: string): boolean {
+    return version.startsWith('@');
+  }
+
+  private _getChannel(version: string): string {
+    return version.substring(1);
+  }
+
+  _isSemverChannel(version: string): boolean {
+    return this._isChannel(version) && this.isValid(this._getChannel(version));
+  }
+
+  override isStable(version: string): boolean {
+    if (this._isValidSemver(version)) {
+      return super.isStable(version);
+    }
+
+    // channel and the rest should be considered unstable version
+    // as channels are changing values
+    return false;
+  }
+
+  override isValid(version: string): boolean {
+    if (this._isValidSemver(version)) {
+      return true;
+    }
+
+    // if a version starts with @ then it is a channel
+    return this._isChannel(version);
+  }
+
+  private _getVersionPart(
+    partFn: (version: string) => null | number,
+    version: string
+  ): null | number {
+    if (this._isValidSemver(version) || this._isSemverChannel(version)) {
+      return partFn(version);
+    }
+
+    return null;
+  }
+
+  override getMajor(version: string): null | number {
+    return this._getVersionPart((version) => super.getMajor(version), version);
+  }
+
+  override getMinor(version: string): null | number {
+    return this._getVersionPart((version) => super.getMinor(version), version);
+  }
+
+  override getPatch(version: string): null | number {
+    return this._getVersionPart((version) => super.getPatch(version), version);
+  }
+
+  override equals(version: string, other: string): boolean {
+    // compare semver when both are
+    if (this._isValidSemver(version) && this._isValidSemver(other)) {
+      return super.equals(version, other);
+    }
+    const parsedVersion = this._parse(version);
+    const parsedOther = this._parse(other);
+
+    if (parsedVersion !== null && parsedOther !== null) {
+      const versionReleases = parsedVersion.release;
+      const otherReleases = parsedOther.release;
+
+      if (versionReleases.length !== otherReleases.length) {
+        return false;
+      }
+
+      for (let i = 0; i < versionReleases.length; i++) {
+        if (versionReleases[i] !== otherReleases[i]) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    return version === other;
+  }
+
+  override isGreaterThan(version: string, other: string): boolean {
+    return this.sortVersions(version, other) > 0;
+  }
+
+  override isLessThanRange(version: string, range: string): boolean {
+    return !this.isGreaterThan(version, range);
+  }
+
+  private _filterVersions(versions: string[]): string[] {
+    return versions
+      .filter((v) => {
+        return this._isValidSemver(v) || this._isSemverChannel(v);
+      })
+      .map((v) => {
+        if (this._isSemverChannel(v)) {
+          return this._getChannel(v);
+        }
+
+        return v;
+      });
+  }
+
+  override getSatisfyingVersion(
+    versions: string[],
+    range: string
+  ): string | null {
+    return super.getSatisfyingVersion(this._filterVersions(versions), range);
+  }
+
+  override minSatisfyingVersion(
+    versions: string[],
+    range: string
+  ): string | null {
+    return super.minSatisfyingVersion(this._filterVersions(versions), range);
+  }
+
+  override sortVersions(version: string, other: string): number {
+    if (this._isValidSemver(version) && this._isValidSemver(other)) {
+      return super.sortVersions(version, other);
+    }
+
+    const parsedVersion = this._parse(version);
+    const parsedOther = this._parse(other);
+
+    if (parsedVersion === null || parsedOther === null) {
+      if (parsedVersion === null && parsedOther === null) {
+        return version.localeCompare(other);
+      }
+      return parsedVersion === null ? -1 : 1;
+    }
+
+    const versionReleases = parsedVersion.release;
+    const otherReleases = parsedOther.release;
+
+    const maxLength =
+      versionReleases.length > otherReleases.length
+        ? versionReleases.length
+        : otherReleases.length;
+
+    for (let i = 0; i < maxLength; i++) {
+      const verVal = versionReleases[i];
+      const otherVal = otherReleases[i];
+
+      if (
+        verVal !== undefined &&
+        otherVal !== undefined &&
+        verVal !== otherVal
+      ) {
+        return verVal - otherVal;
+      } else if (verVal === undefined) {
+        return 1;
+      } else if (otherVal === undefined) {
+        return -1;
+      }
+    }
+
+    return 0;
+  }
+
+  override matches(version: string, range: string): boolean {
+    return this.equals(version, range);
+  }
+}
+
+export const api: VersioningApiConstructor = HermitVersioning;
+export const id = 'hermit';
+export const displayName = 'Hermit versioning';
+export const urls = [
+  'https://cashapp.github.io/hermit/packaging/reference/#versions',
+];
+export const supportsRanges = false;
