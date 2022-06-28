3c873af2fa032eacb2c9be5de56cc5a80bbf1c53
Sergei Zharinov <s@jrnv.me>
2022-06-28 18:11:55 +0300

    Fix tests

--- lib/modules/manager/gradle-wrapper/artifacts.spec.ts
+++ lib/modules/manager/gradle-wrapper/artifacts.spec.ts
@@ -1,4 +1,4 @@
-import { readFile, stat } from 'fs-extra';
+import { readFile } from 'fs-extra';
 import { resolve } from 'upath';
 import { envMock, exec, mockExecAll } from '../../../../test/exec-util';
 import * as httpMock from '../../../../test/http-mock';
@@ -54,7 +54,10 @@ describe('modules/manager/gradle-wrapper/artifacts', () => {
     resetPrefetchedImages();
 
     fs.readLocalFile.mockResolvedValue('test');
-    fs.stat.mockImplementation((p) => stat(p));
+    fs.statLocalFile.mockResolvedValue({
+      isFile: () => true,
+      mode: 0o555,
+    } as never);
   });
 
   afterEach(() => {
@@ -98,6 +101,10 @@ describe('modules/manager/gradle-wrapper/artifacts', () => {
   });
 
   it('gradlew not found', async () => {
+    fs.statLocalFile.mockResolvedValue({
+      isFile: () => false,
+      mode: 0o555,
+    } as never);
     GlobalConfig.set({ ...adminConfig, localDir: 'some-dir' });
     const res = await gradleWrapper.updateArtifacts({
       packageFileName: 'gradle-wrapper.properties',
