4fa28d828db45c694d02b455d81dcc0a527d26e9
RahulGautamSingh <rahultesnik@gmail.com>
2022-06-28 14:28:12 +0530

    revert SemanticCommmitMessage class

--- lib/config/migrations/custom/semantic-prefix-migration.ts
+++ lib/config/migrations/custom/semantic-prefix-migration.ts
@@ -1,5 +1,4 @@
 import is from '@sindresorhus/is';
-import { SemanticCommitMessage } from '../../../workers/repository/model/semantic-commit-message';
 import { AbstractMigration } from '../base/abstract-migration';
 
 export class SemanticPrefixMigration extends AbstractMigration {
@@ -8,18 +7,10 @@ export class SemanticPrefixMigration extends AbstractMigration {
 
   override run(value: unknown): void {
     if (is.string(value)) {
-      const semanticCommitMessage = SemanticCommitMessage.fromString(value);
-      if (SemanticCommitMessage.is(semanticCommitMessage)) {
-        const { scope, type } = semanticCommitMessage.toJSON();
-        this.setSafely('semanticCommitType', type);
-        this.setSafely(
-          'semanticCommitScope',
-          is.emptyString(scope) ? null : scope
-        );
-      } else {
-        this.setSafely('semanticCommitType', value);
-        this.setSafely('semanticCommitScope', null);
-      }
+      const [text] = value.split(':');
+      const [type, scope] = text.split('(');
+      this.setSafely('semanticCommitType', type);
+      this.setSafely('semanticCommitScope', scope ? scope.split(')')[0] : null);
     }
   }
 }
