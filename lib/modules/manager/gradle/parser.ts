db6de2475632e4932862882e96987af2b6a3c6d2
Johannes Feichtner <Churro@users.noreply.github.com>
2022-07-04 00:53:43 +0200

    fix(gradle): resolve escaped registry URLs in gradle.properties (#16403)

--- lib/modules/manager/gradle/parser.ts
+++ lib/modules/manager/gradle/parser.ts
@@ -298,6 +298,7 @@ function processCustomRegistryUrl({
 
   try {
     if (registryUrl) {
+      registryUrl = registryUrl.replace(regEx(/\\/g), '');
       const { host, protocol } = url.parse(registryUrl);
       if (host && protocol) {
         return { urls: [registryUrl] };
