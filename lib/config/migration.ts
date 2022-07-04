3d632a043b374a2cbf3554257f121d09b0bb0caa
RahulGautamSingh <rahultesnik@gmail.com>
2022-07-04 18:04:12 +0530

    fix type

--- lib/config/migration.ts
+++ lib/config/migration.ts
@@ -200,8 +200,10 @@ export function migrateConfig(config: RenovateConfig): MigratedConfig {
           logger.debug('Flattening nested packageRules');
           // merge each subrule and add to the parent list
           for (const subrule of packageRule.packageRules) {
-            // TODO: fix types #7154
-            const combinedRule = mergeChildConfig(packageRule, subrule as any);
+            const combinedRule = mergeChildConfig(
+              packageRule,
+              subrule as PackageRule
+            );
             delete combinedRule.packageRules;
             migratedConfig.packageRules.push(combinedRule);
           }
