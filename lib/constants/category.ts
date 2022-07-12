06438d7968a8cca574256d55f71ace7a43def48b
secustor <sebastian@poxhofer.at>
2022-07-12 17:32:03 +0200

    chore: remove commented code

--- lib/constants/category.ts
+++ lib/constants/category.ts
@@ -1,30 +1,3 @@
-// // eslint-disable-next-line typescript-enum/no-enum
-// export enum test {
-//   Ansible = 'ansible',
-//   Batect = 'batect',
-//   Bazel = 'bazel',
-//   CD = 'cd',
-//   CI = 'ci',
-//   C = 'c',
-//   Kubernetes = 'kubernetes',
-//   Ruby = 'ruby',
-//   Dotnet = 'dotnet',
-//   Rust = 'rust',
-//   Swift = 'swift',
-//   PHP = 'php',
-//   JVM = 'jvm',
-//   Docker = 'docker',
-//   Golang = 'golang',
-//   Helm = 'helm',
-//   JavaScript = 'javascript',
-//   Elixir = 'elixir',
-//   NodeJS = 'node',
-//   Python = 'python',
-//   Dart = 'dart',
-//   Terraform = 'terraform',
-//   IaC = 'iac',
-// }
-
 export type Category =
   | 'ansible'
   | 'batect'
