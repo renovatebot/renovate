1d34f8673cc6736ef6fd9ab3b4713a52ce6b1404
Jamie Magee <jamie.magee@gmail.com>
2022-07-13 08:25:05 -0700

    coverage

--- lib/modules/manager/kubernetes/extract.spec.ts
+++ lib/modules/manager/kubernetes/extract.spec.ts
@@ -5,6 +5,7 @@ const kubernetesImagesFile = Fixtures.get('kubernetes.yaml');
 const kubernetesConfigMapFile = Fixtures.get('configmap.yaml');
 const kubernetesArraySyntaxFile = Fixtures.get('array-syntax.yaml');
 const otherYamlFile = Fixtures.get('gitlab-ci.yaml');
+const invalidYamlFile = Fixtures.get('invalid.yaml');
 
 describe('modules/manager/kubernetes/extract', () => {
   describe('extractPackageFile()', () => {
@@ -14,24 +15,72 @@ describe('modules/manager/kubernetes/extract', () => {
 
     it('returns only API version', () => {
       const res = extractPackageFile(kubernetesConfigMapFile);
-      expect(res?.deps).toMatchSnapshot();
-      expect(res?.deps).toHaveLength(1);
+      expect(res?.deps).toStrictEqual([
+        {
+          currentValue: 'v1',
+          depName: 'ConfigMap',
+        },
+      ]);
     });
 
     it('extracts multiple Kubernetes configurations', () => {
       const res = extractPackageFile(kubernetesImagesFile);
-      expect(res?.deps).toMatchSnapshot();
-      expect(res?.deps).toHaveLength(4);
+      expect(res?.deps).toStrictEqual([
+        {
+          autoReplaceStringTemplate:
+            '{{depName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}',
+          currentDigest: undefined,
+          currentValue: '1.7.9',
+          datasource: 'docker',
+          depName: 'nginx',
+          replaceString: 'nginx:1.7.9',
+        },
+        {
+          autoReplaceStringTemplate:
+            '{{depName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}',
+          currentDigest: undefined,
+          currentValue: 'v1.11.1',
+          datasource: 'docker',
+          depName: 'k8s.gcr.io/kube-proxy-amd64',
+          replaceString: 'k8s.gcr.io/kube-proxy-amd64:v1.11.1',
+        },
+        {
+          currentValue: 'apps/v1',
+          depName: 'Deployment',
+        },
+        {
+          currentValue: 'extensions/v1beta1',
+          depName: 'DaemonSet',
+        },
+      ]);
     });
 
     it('extracts image line in a YAML array', () => {
       const res = extractPackageFile(kubernetesArraySyntaxFile);
-      expect(res?.deps).toMatchSnapshot();
-      expect(res?.deps).toHaveLength(2);
+      expect(res?.deps).toStrictEqual([
+        {
+          autoReplaceStringTemplate:
+            '{{depName}}{{#if newValue}}:{{newValue}}{{/if}}{{#if newDigest}}@{{newDigest}}{{/if}}',
+          currentDigest: undefined,
+          currentValue: 'v2.1.0',
+          datasource: 'docker',
+          depName: 'quay.io/external_storage/local-volume-provisioner',
+          replaceString:
+            'quay.io/external_storage/local-volume-provisioner:v2.1.0',
+        },
+        {
+          currentValue: 'apps/v1',
+          depName: 'DaemonSet',
+        },
+      ]);
     });
 
     it('ignores non-Kubernetes YAML files', () => {
       expect(extractPackageFile(otherYamlFile)).toBeNull();
+    });
+
+    it('handles invalid YAML files', () => {
+      expect(extractPackageFile(invalidYamlFile)).toBeNull();
     });
   });
 });
