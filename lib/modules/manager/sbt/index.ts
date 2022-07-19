b8c9a680b4573559beaebe43ad483e307b2f556c
cjtim <47479090+cjtim@users.noreply.github.com>
2022-07-19 11:59:01 +0000

    fix export

--- lib/modules/manager/sbt/index.ts
+++ lib/modules/manager/sbt/index.ts
@@ -3,10 +3,7 @@ import { SbtPackageDatasource } from '../../datasource/sbt-package';
 import { SbtPluginDatasource } from '../../datasource/sbt-plugin';
 import * as ivyVersioning from '../../versioning/ivy';
 
-// extractPackageFile is used only for test and within extractAllPackageFiles FN
-// extractAllPackageFiles has more higher priority
-// lib/workers/repository/extract/manager-files.ts:32:0
-export { extractPackageFile, extractAllPackageFiles } from './extract';
+export { extractAllPackageFiles } from './extract';
 export { bumpPackageVersion, updateDependency } from './update';
 
 export const supportedDatasources = [
