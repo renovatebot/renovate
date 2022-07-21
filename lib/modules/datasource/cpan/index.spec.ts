acf5bf2f58eddaf120051ca4a224f94e82089e21
IKEDA Sho <suicaicoca@gmail.com>
2022-07-21 16:26:37 +0900

    Improve tests

--- lib/modules/datasource/cpan/index.spec.ts
+++ lib/modules/datasource/cpan/index.spec.ts
@@ -72,78 +72,21 @@ describe('modules/datasource/cpan/index', () => {
         changelogUrl: 'https://metacpan.org/dist/Plack/changes',
         homepage: 'https://metacpan.org/pod/Plack',
         registryUrl: 'https://fastapi.metacpan.org/',
-        releases: [
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2015-12-06T11:29:40.000Z',
-            version: '1.0039',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: false,
-            releaseTimestamp: '2016-04-01T16:58:21.000Z',
-            version: '1.0040',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2016-09-25T21:25:47.000Z',
-            version: '1.0041',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2016-09-29T05:38:42.000Z',
-            version: '1.0042',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2017-02-22T03:02:05.000Z',
-            version: '1.0043',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2017-04-27T17:48:20.000Z',
-            version: '1.0044',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2017-12-31T20:42:50.000Z',
-            version: '1.0045',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2018-02-10T07:52:31.000Z',
-            version: '1.0046',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2018-02-10T09:25:30.000Z',
-            version: '1.0047',
-          },
-          {
-            distribution: 'Plack',
-            isDeprecated: false,
-            isStable: true,
-            releaseTimestamp: '2020-11-30T00:21:36.000Z',
-            version: '1.0048',
-          },
-        ],
+        releases: expect.toBeArrayOfSize(10),
+      });
+      expect(res?.releases[1]).toMatchObject({
+        distribution: 'Plack',
+        isDeprecated: false,
+        isStable: false,
+        releaseTimestamp: '2016-04-01T16:58:21.000Z',
+        version: '1.0040',
+      });
+      expect(res?.releases[9]).toMatchObject({
+        distribution: 'Plack',
+        isDeprecated: false,
+        isStable: true,
+        releaseTimestamp: '2020-11-30T00:21:36.000Z',
+        version: '1.0048',
       });
     });
   });
