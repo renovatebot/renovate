729690d99c9af283e24b178bdc2beed2f56e7f9a
IKEDA Sho <suicaicoca@gmail.com>
2022-07-25 21:58:30 +0900

    docs(manager/cocoapods): Configure `ModuleApi`'s displayName and url

--- lib/modules/manager/cocoapods/index.ts
+++ lib/modules/manager/cocoapods/index.ts
@@ -4,8 +4,11 @@ import { GitlabTagsDatasource } from '../../datasource/gitlab-tags';
 import { PodDatasource } from '../../datasource/pod';
 import * as rubyVersioning from '../../versioning/ruby';
 
-export { extractPackageFile } from './extract';
 export { updateArtifacts } from './artifacts';
+export { extractPackageFile } from './extract';
+
+export const displayName = 'CocoaPods';
+export const url = 'https://cocoapods.org';
 
 export const defaultConfig = {
   fileMatch: ['(^|/)Podfile$'],
