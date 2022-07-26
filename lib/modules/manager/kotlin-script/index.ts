ca0eac03eeb31ea45b1cc15ab42c6cd60120af14
Piotr Krzemiński <git@krzeminski.it>
2022-07-26 07:49:10 +0200

    Remove explicit Maven versioning - it's taken from Datasource

--- lib/modules/manager/kotlin-script/index.ts
+++ lib/modules/manager/kotlin-script/index.ts
@@ -1,11 +1,9 @@
 import { MavenDatasource } from '../../datasource/maven';
-import * as mavenVersioning from '../../versioning/maven';
 
 export { extractPackageFile } from './extract';
 
 export const defaultConfig = {
   fileMatch: ['^.*\\.main\\.kts$'],
-  versioning: mavenVersioning.id,
 };
 
 export const supportedDatasources = [MavenDatasource.id];
