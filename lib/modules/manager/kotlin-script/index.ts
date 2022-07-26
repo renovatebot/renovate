94e13e3333d6763a9211ee380d1ab6e34d08c976
Piotr Krzemiński <3110813+krzema12@users.noreply.github.com>
2022-07-26 10:34:55 +0200

    Don't match with '.main.kts' (without file name)
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/modules/manager/kotlin-script/index.ts
+++ lib/modules/manager/kotlin-script/index.ts
@@ -3,7 +3,7 @@ import { MavenDatasource } from '../../datasource/maven';
 export { extractPackageFile } from './extract';
 
 export const defaultConfig = {
-  fileMatch: ['^.*\\.main\\.kts$'],
+  fileMatch: ['^.+\\.main\\.kts$'],
 };
 
 export const supportedDatasources = [MavenDatasource.id];
