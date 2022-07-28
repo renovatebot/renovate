cd2a8ec3d8dbcaaff7a25478f5be6b1128e58bd0
Jamie Magee <jamie.magee@gmail.com>
2022-07-28 14:25:53 -0700

    feat(manager/azure-pipelines): extract azure-pipelines tasks

--- lib/modules/manager/azure-pipelines/extract.spec.ts
+++ lib/modules/manager/azure-pipelines/extract.spec.ts
@@ -7,21 +7,25 @@ import {
 import { extractPackageFile } from '.';
 
 const azurePipelines = Fixtures.get('azure-pipelines.yaml');
-
 const azurePipelinesInvalid = Fixtures.get('azure-pipelines-invalid.yaml');
-
 const azurePipelinesNoDependency = Fixtures.get(
   'azure-pipelines-no-dependency.yaml'
 );
+const azurePipelinesStages = Fixtures.get('azure-pipelines-stages.yaml');
+const azurePipelinesJobs = Fixtures.get('azure-pipelines-jobs.yaml');
+const azurePipelinesSteps = Fixtures.get('azure-pipelines-steps.yaml');
 
 describe('modules/manager/azure-pipelines/extract', () => {
   it('should parse a valid azure-pipelines file', () => {
-    const file = parseAzurePipelines(azurePipelines, 'some-file');
+    const file = parseAzurePipelines(azurePipelines, 'azure-pipelines.yaml');
     expect(file).not.toBeNull();
   });
 
   it('return null on an invalid file', () => {
-    const file = parseAzurePipelines(azurePipelinesInvalid, 'some-file');
+    const file = parseAzurePipelines(
+      azurePipelinesInvalid,
+      'azure-pipelines.yaml'
+    );
     expect(file).toBeNull();
   });
 
@@ -90,19 +94,46 @@ describe('modules/manager/azure-pipelines/extract', () => {
 
   describe('extractPackageFile()', () => {
     it('returns null for invalid azure pipelines files', () => {
-      expect(extractPackageFile('', 'some-file')).toBeNull();
+      expect(extractPackageFile('', 'azure-pipelines.yaml')).toBeNull();
     });
 
     it('extracts dependencies', () => {
-      const res = extractPackageFile(azurePipelines, 'some-file');
+      const res = extractPackageFile(azurePipelines, 'azure-pipelines.yaml');
       expect(res?.deps).toMatchSnapshot();
       expect(res?.deps).toHaveLength(3);
     });
 
     it('should return null when there is no dependency found', () => {
       expect(
-        extractPackageFile(azurePipelinesNoDependency, 'some-file')
+        extractPackageFile(azurePipelinesNoDependency, 'azure-pipelines.yaml')
       ).toBeNull();
+    });
+
+    it('should extract stages', () => {
+      const res = extractPackageFile(
+        azurePipelinesStages,
+        'azure-pipelines.yaml'
+      );
+      expect(res?.deps).toMatchSnapshot();
+      expect(res?.deps).toHaveLength(1);
+    });
+
+    it('should extract jobs', () => {
+      const res = extractPackageFile(
+        azurePipelinesJobs,
+        'azure-pipelines.yaml'
+      );
+      expect(res?.deps).toMatchSnapshot();
+      expect(res?.deps).toHaveLength(1);
+    });
+
+    it('should extract steps', () => {
+      const res = extractPackageFile(
+        azurePipelinesSteps,
+        'azure-pipelines.yaml'
+      );
+      expect(res?.deps).toMatchSnapshot();
+      expect(res?.deps).toHaveLength(1);
     });
   });
 });
--- lib/modules/manager/azure-pipelines/extract.ts
+++ lib/modules/manager/azure-pipelines/extract.ts
@@ -1,9 +1,13 @@
+import is from '@sindresorhus/is';
 import { load } from 'js-yaml';
 import { logger } from '../../../logger';
+import { regEx } from '../../../util/regex';
 import { GitTagsDatasource } from '../../datasource/git-tags';
 import { getDep } from '../dockerfile/extract';
 import type { PackageDependency, PackageFile } from '../types';
 import type { AzurePipelines, Container, Repository } from './types';
+
+const AzurePipelinesTaskRegex = regEx(/^(?<name>[^@]+)@(?<version>.*)$/);
 
 export function extractRepository(
   repository: Repository
@@ -48,6 +52,19 @@ export function extractContainer(
   return dep;
 }
 
+export function extractAzurePipelinesTasks(
+  task: string
+): PackageDependency | null {
+  const match = AzurePipelinesTaskRegex.exec(task);
+  if (match?.groups) {
+    return {
+      depName: match.groups.name,
+      currentValue: match.groups.version,
+    };
+  }
+  return null;
+}
+
 export function parseAzurePipelines(
   content: string,
   filename: string
@@ -59,13 +76,6 @@ export function parseAzurePipelines(
     logger.info({ filename, err }, 'Error parsing azure-pipelines content');
     return null;
   }
-
-  if (!pkg || !pkg.resources) {
-    return null;
-  }
-
-  pkg.resources.containers = pkg.resources.containers || [];
-  pkg.resources.repositories = pkg.resources.repositories || [];
 
   return pkg;
 }
@@ -83,18 +93,64 @@ export function extractPackageFile(
   }
 
   // grab the repositories tags
-  for (const repository of pkg.resources.repositories) {
-    const dep = extractRepository(repository);
-    if (dep) {
-      deps.push(dep);
+  if (
+    !is.nullOrUndefined(pkg.resources) &&
+    is.nonEmptyArray(pkg.resources.repositories)
+  ) {
+    for (const repository of pkg.resources.repositories) {
+      const dep = extractRepository(repository);
+      if (dep) {
+        deps.push(dep);
+      }
     }
   }
 
   // grab the containers tags
-  for (const container of pkg.resources.containers) {
-    const dep = extractContainer(container);
-    if (dep) {
-      deps.push(dep);
+  if (
+    !is.nullOrUndefined(pkg.resources) &&
+    is.nonEmptyArray(pkg.resources.containers)
+  ) {
+    for (const container of pkg.resources.containers) {
+      const dep = extractContainer(container);
+      if (dep) {
+        deps.push(dep);
+      }
+    }
+  }
+
+  if (!is.nullOrUndefined(pkg.stages)) {
+    const tasks = pkg.stages
+      .flatMap((stage) =>
+        stage.jobs?.flatMap((job) =>
+          job.steps?.flatMap((step) => extractAzurePipelinesTasks(step.task))
+        )
+      )
+      .filter((task) => !is.nullOrUndefined(task));
+
+    if (is.array<PackageDependency>(tasks)) {
+      deps.push(...tasks);
+    }
+  }
+
+  if (!is.nullOrUndefined(pkg.jobs)) {
+    const tasks = pkg.jobs
+      .flatMap((job) =>
+        job.steps?.flatMap((step) => extractAzurePipelinesTasks(step.task))
+      )
+      .filter((dep) => !is.nullOrUndefined(dep));
+
+    if (is.array<PackageDependency>(tasks)) {
+      deps.push(...tasks);
+    }
+  }
+
+  if (!is.nullOrUndefined(pkg.steps)) {
+    const tasks = pkg.steps
+      ?.flatMap((step) => extractAzurePipelinesTasks(step.task))
+      .filter((dep) => !is.nullOrUndefined(dep));
+
+    if (is.array<PackageDependency>(tasks)) {
+      deps.push(...tasks);
     }
   }
 
