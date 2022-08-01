2dbc15e6793916f75579d0a66126982d76304a3d
IKEDA Sho <suicaicoca@gmail.com>
2022-08-01 13:48:45 +0900

    docs(manager): Set displayName and url for nodenv and nvm managers (#16878)

--- lib/modules/manager/nodenv/index.ts
+++ lib/modules/manager/nodenv/index.ts
@@ -4,6 +4,9 @@ import * as nodeVersioning from '../../versioning/node';
 
 export { extractPackageFile } from './extract';
 
+export const displayName = 'nodenv';
+export const url = 'https://github.com/nodenv/nodenv';
+
 export const language = ProgrammingLanguage.NodeJS;
 
 export const defaultConfig = {
