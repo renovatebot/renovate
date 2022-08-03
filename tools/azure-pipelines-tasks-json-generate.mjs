4eb8536f8f767f44c0fa19db0c90eb78e6e6fbe4
Jamie Magee <jamie.magee@gmail.com>
2022-08-03 14:02:56 -0700

    feat: add azure pipelines built-in tasks version data

---
+++ tools/azure-pipelines-tasks-json-generate.mjs
@@ -0,0 +1,60 @@
+import os from 'os';
+import { promisify } from 'util';
+import fs from 'fs-extra';
+import g from 'glob';
+import shell from 'shelljs';
+import Git from 'simple-git';
+import path from 'upath';
+
+const glob = promisify(g);
+
+const localPath = path.join(os.tmpdir(), 'azure-pipelines-tasks');
+
+await (async () => {
+  await fs.ensureDir(localPath);
+  const git = Git(localPath);
+
+  if (await git.checkIsRepo()) {
+    await git.pull();
+  } else {
+    await git.clone(
+      'https://github.com/microsoft/azure-pipelines-tasks.git',
+      '.'
+    );
+  }
+
+  const files = (await glob(path.join(localPath, '**/task.json'))).map((file) =>
+    file.replace(`${localPath}/`, '')
+  );
+
+  /** @type {Record<string, Set<string>>} */
+  const tasks = {};
+
+  for (const file of files) {
+    const revs = (await git.raw(['rev-list', 'master', '--', file])).split(
+      '\n'
+    );
+    shell.echo(`Parsing ${file}`);
+    for (const rev of revs) {
+      try {
+        const content = await git.show([`${rev}:${file}`]);
+        /** @type {{name: string, version: {Major: number, Minor: number, Patch: number}}} */
+        const parsedContent = JSON.parse(content);
+        const version = `${parsedContent.version.Major}.${parsedContent.version.Minor}.${parsedContent.version.Patch}`;
+        tasks[parsedContent.name] =
+          tasks[parsedContent.name]?.add(version) ?? new Set([version]);
+      } catch (e) {
+        shell.echo(`Failed to parse ${file} at ${rev}`);
+        shell.echo(e.toString());
+      }
+    }
+  }
+
+  const data = JSON.stringify(
+    tasks,
+    (_, value) => (value instanceof Set ? [...value] : value),
+    2
+  );
+
+  await fs.writeFile(`./data/azure-pipelines-tasks-info.json`, data);
+})();
