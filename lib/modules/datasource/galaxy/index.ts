6a9648fc2a644e4dfd2465eeceeb4ee07f6bb7b8
IKEDA Sho <suicaicoca@gmail.com>
2022-08-10 01:02:08 +0900

    Revert "Split arguments"
    This reverts commit 7041fe9313b889d5bb098e93e629fbe197d4660d.

--- lib/modules/datasource/galaxy/index.ts
+++ lib/modules/datasource/galaxy/index.ts
@@ -40,7 +40,10 @@ export class GalaxyDatasource extends Datasource {
       registryUrl,
       'api/v1/roles'
     )}/?owner__username=${userName}&name=${projectName}`;
-    const galaxyProjectUrl = joinUrlParts(registryUrl, userName, projectName);
+    const galaxyProjectUrl = joinUrlParts(
+      registryUrl,
+      `${userName}/${projectName}`
+    );
 
     let raw: HttpResponse<GalaxyResult> | null = null;
     try {
