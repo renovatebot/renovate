b474ffb5808003ba156b9b28a5e62d5bfbe0a467
hasanwhitesource <hasan.awad@whitesourcesoftware.com>
2022-08-10 15:36:53 +0300

    update test

--- lib/modules/manager/gomod/extract.spec.ts
+++ lib/modules/manager/gomod/extract.spec.ts
@@ -43,20 +43,37 @@ describe('modules/manager/gomod/extract', () => {
 
     it('extracts replace directives from multi-line and single line', () => {
       const goMod = `
+module github.com/renovate-tests/gomod
+go 1.18
 replace golang.org/x/foo => github.com/pravesht/gocql v0.0.0
 replace (
       k8s.io/client-go => k8s.io/client-go v0.21.9
-      k8s.io/cloud-provider => k8s.io/cloud-provider v0.17.3
-      k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.17.3
-      k8s.io/code-generator => k8s.io/code-generator v0.17.3
-      )`;
+      )
+replace (
+  k8s.io/cloud-provider => k8s.io/cloud-provider v0.17.3
+  k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.17.3 // indirect
+  k8s.io/code-generator => k8s.io/code-generator v0.17.3
+)`;
       const res = extractPackageFile(goMod);
       expect(res).toEqual({
-        constraints: {},
+        constraints: {
+          go: '^1.18',
+        },
         deps: [
           {
             managerData: {
-              lineNumber: 1,
+              lineNumber: 2,
+            },
+            depName: 'go',
+            depType: 'golang',
+            currentValue: '1.18',
+            datasource: 'golang-version',
+            versioning: 'npm',
+            rangeStrategy: 'replace',
+          },
+          {
+            managerData: {
+              lineNumber: 3,
             },
             depName: 'github.com/pravesht/gocql',
             depType: 'replace',
@@ -65,7 +82,7 @@ replace (
           },
           {
             managerData: {
-              lineNumber: 3,
+              lineNumber: 5,
               multiLine: true,
             },
             depName: 'k8s.io/client-go',
@@ -75,7 +92,7 @@ replace (
           },
           {
             managerData: {
-              lineNumber: 4,
+              lineNumber: 8,
               multiLine: true,
             },
             depName: 'k8s.io/cloud-provider',
@@ -85,17 +102,7 @@ replace (
           },
           {
             managerData: {
-              lineNumber: 5,
-              multiLine: true,
-            },
-            depName: 'k8s.io/cluster-bootstrap',
-            depType: 'replace',
-            currentValue: 'v0.17.3',
-            datasource: 'go',
-          },
-          {
-            managerData: {
-              lineNumber: 6,
+              lineNumber: 10,
               multiLine: true,
             },
             depName: 'k8s.io/code-generator',
