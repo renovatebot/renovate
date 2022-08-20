87f792813c8a0913ee9cbaefed79fd7bfc83e5e8
Jamie Magee <jamie.magee@gmail.com>
2022-08-19 21:33:44 -0700

    PR comments

--- lib/modules/versioning/kubernetes-api/index.ts
+++ lib/modules/versioning/kubernetes-api/index.ts
@@ -1,8 +1,8 @@
-import { RegExpVersion, RegExpVersioningApi } from '../regex';
+import { RegExpVersioningApi } from '../regex';
 import type { VersioningApi } from '../types';
 
 export const id = 'kubernetes-api';
-export const displayName = 'kubernetes-api';
+export const displayName = 'Kubernetes API';
 export const urls = [
   'https://kubernetes.io/docs/reference/using-api/#api-versioning',
 ];
@@ -14,30 +14,6 @@ export class KubernetesApiVersioningApi extends RegExpVersioningApi {
 
   public constructor() {
     super(KubernetesApiVersioningApi.versionRegex);
-  }
-
-  protected override _parse(version: string): RegExpVersion | null {
-    const parsed = super._parse(version);
-    if (parsed) {
-      return parsed;
-    }
-
-    const groups = this._config?.exec(version)?.groups;
-
-    if (!groups) {
-      return null;
-    }
-
-    if (groups) {
-      const { compatibility, version, prerelease } = groups;
-      return {
-        compatibility,
-        release: [parseInt(version, 10)],
-        prerelease,
-      };
-    }
-
-    return null;
   }
 }
 
