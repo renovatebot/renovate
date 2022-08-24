169610de82c12c7932e0ee18606d54149698c191
coffmark <come1234560@gmail.com>
2022-08-25 08:25:47 +0900

    feat(manager/mint): Inline complex.Mintfile

--- lib/modules/manager/mint/extract.spec.ts
+++ lib/modules/manager/mint/extract.spec.ts
@@ -3,32 +3,33 @@ import { extractPackageFile } from '.';
 
 const simpleMintfile = Fixtures.get('simple.Mintfile');
 const noVersionMintfile = Fixtures.get('noVersion.Mintfile');
-const complexMintfile = Fixtures.get('complex.Mintfile');
 
 describe('modules/manager/mint/extract', () => {
   describe('extractPackageFile()', () => {
     it('Makefile With Version Description', () => {
       const res = extractPackageFile(simpleMintfile);
-      expect(res).toEqual({ deps: [
-        {
-          depName: 'SwiftGen/SwiftGen',
-          currentValue: '6.6.1',
-          datasource: 'git-tags',
-          packageName: 'https://github.com/SwiftGen/SwiftGen.git',
-        },
-        {
-          depName: 'yonaskolb/xcodegen',
-          currentValue: '2.30.0',
-          datasource: 'git-tags',
-          packageName: 'https://github.com/yonaskolb/xcodegen.git',
-        },
-        {
-          depName: 'realm/SwiftLint',
-          currentValue: '0.48.0',
-          datasource: 'git-tags',
-          packageName: 'https://github.com/realm/SwiftLint.git',
-        },
-      ]});
+      expect(res).toEqual({
+        deps: [
+          {
+            depName: 'SwiftGen/SwiftGen',
+            currentValue: '6.6.1',
+            datasource: 'git-tags',
+            packageName: 'https://github.com/SwiftGen/SwiftGen.git',
+          },
+          {
+            depName: 'yonaskolb/xcodegen',
+            currentValue: '2.30.0',
+            datasource: 'git-tags',
+            packageName: 'https://github.com/yonaskolb/xcodegen.git',
+          },
+          {
+            depName: 'realm/SwiftLint',
+            currentValue: '0.48.0',
+            datasource: 'git-tags',
+            packageName: 'https://github.com/realm/SwiftLint.git',
+          },
+        ],
+      });
     });
 
     it('Makefile Without Version Description', () => {
@@ -37,7 +38,10 @@ describe('modules/manager/mint/extract', () => {
     });
 
     it('Complex Makefile', () => {
-      const res = extractPackageFile(complexMintfile);
+      const res = extractPackageFile(`
+        SwiftGen/SwiftGen@6.6.1
+        yonaskolb/xcodegen
+        realm/SwiftLint @ 0.48.0`);
       expect(res!.deps).toEqual([
         {
           depName: 'SwiftGen/SwiftGen',
