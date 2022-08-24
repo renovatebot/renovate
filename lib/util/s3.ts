c8bca54046a11f403579031560c3c6b44c1174a4
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2022-08-24 13:27:29 +0300

    document new cache related renovate env vars

--- lib/util/s3.ts
+++ lib/util/s3.ts
@@ -5,9 +5,11 @@ import { parseUrl } from './url';
 let s3Instance: S3 | undefined;
 export function getS3Client(): S3 {
   if (!s3Instance) {
+    const endpoint = process.env.RENOVATE_X_S3_ENDPOINT;
+    const forcePathStyle = process.env.RENOVATE_X_S3_PATH_STYLE;
     s3Instance = new S3({
-      ...(process.env.S3_ENDPOINT && { endpoint: process.env.S3_ENDPOINT }),
-      ...(process.env.S3_FORCE_PATH_STYLE && { forcePathStyle: true }),
+      ...(endpoint && { endpoint }),
+      ...(forcePathStyle && { forcePathStyle: true }),
     });
   }
   return s3Instance;
