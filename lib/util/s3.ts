88c9ca6aa37aa56e785ab3b451d394f5c3126999
Michael Kriese <michael.kriese@visualon.de>
2022-08-25 12:00:54 +0200

    fix(datasource/maven): use sdk v3 api (#17408)

--- lib/util/s3.ts
+++ lib/util/s3.ts
@@ -1,11 +1,11 @@
 // Singleton S3 instance initialized on-demand.
-import { S3 } from '@aws-sdk/client-s3';
+import { S3Client } from '@aws-sdk/client-s3';
 import { parseUrl } from './url';
 
-let s3Instance: S3 | undefined;
-export function getS3Client(): S3 {
+let s3Instance: S3Client | undefined;
+export function getS3Client(): S3Client {
   if (!s3Instance) {
-    s3Instance = new S3({});
+    s3Instance = new S3Client({});
   }
   return s3Instance;
 }
