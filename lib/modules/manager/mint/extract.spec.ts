a7ca7864ad58ef09bf9d9929a830e35db2ceba63
coffmark <come1234560@gmail.com>
2022-08-27 11:55:07 +0900

    feat(manager/mint): handle comment & add skipReason when no version

--- lib/modules/manager/mint/extract.spec.ts
+++ lib/modules/manager/mint/extract.spec.ts
@@ -4,6 +4,18 @@ import { extractPackageFile } from '.';
 const simpleMintfile = Fixtures.get('simple.Mintfile');
 const noVersionMintfile = Fixtures.get('noVersion.Mintfile');
 
+const complexMintFileContent = `
+SwiftGen/SwiftGen@6.6.1
+yonaskolb/xcodegen
+realm/SwiftLint @ 0.48.0`;
+
+const includesCommentOutMintFileContent = `
+SwiftGen/SwiftGen@6.6.1
+yonaskolb/xcodegen
+#yonaskolb/xcodegen
+#realm/SwiftLint@0.48.0#commented out
+`;
+
 describe('modules/manager/mint/extract', () => {
   describe('extractPackageFile()', () => {
     it('Makefile With Version Description', () => {
@@ -34,14 +46,26 @@ describe('modules/manager/mint/extract', () => {
 
     it('Makefile Without Version Description', () => {
       const res = extractPackageFile(noVersionMintfile);
-      expect(res).toBeNull();
+      expect(res).toEqual({
+        deps: [
+          {
+            depName: 'yonaskolb/xcodegen',
+            currentValue: null,
+            skipReason: 'no-version',
+            packageName: 'https://github.com/yonaskolb/xcodegen.git',
+          },
+          {
+            depName: 'realm/SwiftLint',
+            currentValue: null,
+            skipReason: 'no-version',
+            packageName: 'https://github.com/realm/SwiftLint.git',
+          },
+        ],
+      });
     });
 
     it('Complex Makefile', () => {
-      const res = extractPackageFile(`
-        SwiftGen/SwiftGen@6.6.1
-        yonaskolb/xcodegen
-        realm/SwiftLint @ 0.48.0`);
+      const res = extractPackageFile(complexMintFileContent);
       expect(res).toEqual({
         deps: [
           {
@@ -51,10 +75,36 @@ describe('modules/manager/mint/extract', () => {
             packageName: 'https://github.com/SwiftGen/SwiftGen.git',
           },
           {
+            depName: 'yonaskolb/xcodegen',
+            currentValue: null,
+            skipReason: 'no-version',
+            packageName: 'https://github.com/yonaskolb/xcodegen.git',
+          },
+          {
             depName: 'realm/SwiftLint',
             currentValue: '0.48.0',
             datasource: 'git-tags',
             packageName: 'https://github.com/realm/SwiftLint.git',
+          },
+        ],
+      });
+    });
+
+    it('Mintfile Includes Commented Out', () => {
+      const res = extractPackageFile(includesCommentOutMintFileContent);
+      expect(res).toEqual({
+        deps: [
+          {
+            depName: 'SwiftGen/SwiftGen',
+            currentValue: '6.6.1',
+            datasource: 'git-tags',
+            packageName: 'https://github.com/SwiftGen/SwiftGen.git',
+          },
+          {
+            currentValue: null,
+            depName: 'yonaskolb/xcodegen',
+            packageName: 'https://github.com/yonaskolb/xcodegen.git',
+            skipReason: 'no-version',
           },
         ],
       });
--- lib/modules/manager/mint/extract.ts
+++ lib/modules/manager/mint/extract.ts
@@ -5,16 +5,48 @@ export function extractPackageFile(content: string): PackageFile | null {
   const deps: PackageDependency[] = [];
 
   content.split('\n').forEach((line) => {
-    if (line.includes('@')) {
-      const [depName, currentVersion] = line.replace(/\s+/g, '').split('@');
-      const dep: PackageDependency = {
-        depName: depName,
-        currentValue: currentVersion,
-        datasource: GitTagsDatasource.id,
-        packageName: `https://github.com/${depName}.git`,
-      };
-      deps.push(dep);
+    if (line === '') {
+      return;
     }
+
+    // commented out line
+    if (line.startsWith('#')) {
+      return;
+    }
+
+    // exclude blanks
+    const noBlanksLine = line.replace(/\s+/g, '');
+    if (noBlanksLine === '') {
+      return;
+    }
+
+    // commented out line after package name
+    if (noBlanksLine.includes('#')) {
+      const [uncommentLine] = line.split('#');
+      deps.push(handleDepInMintfile(uncommentLine));
+      return;
+    }
+
+    deps.push(handleDepInMintfile(noBlanksLine));
+    return;
   });
   return deps.length ? { deps } : null;
+}
+
+function handleDepInMintfile(line: string): PackageDependency {
+  if (!line.includes('@')) {
+    return {
+      depName: line,
+      currentValue: null,
+      skipReason: 'no-version',
+      packageName: `https://github.com/${line}.git`,
+    };
+  }
+  const [depName, currentVersion] = line.split('@');
+  return {
+    depName: depName,
+    currentValue: currentVersion,
+    datasource: GitTagsDatasource.id,
+    packageName: `https://github.com/${depName}.git`,
+  };
 }
