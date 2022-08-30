6a93997863e767c83eec89a4e1d807ef802c19ab
Sergei Zharinov <zharinov@users.noreply.github.com>
2022-08-30 07:58:57 +0300

    feat(cache): Repository cache fingerprinting (#17367)

--- lib/util/cache/repository/impl/cache-factory.ts
+++ lib/util/cache/repository/impl/cache-factory.ts
@@ -7,20 +7,21 @@ import { RepoCacheS3 } from './s3';
 export class CacheFactory {
   static get(
     repository: string,
-    cacheType: RepositoryCacheType = 'local'
+    repoFingerprint: string,
+    cacheType: RepositoryCacheType
   ): RepoCache {
     const type = cacheType.split('://')[0].trim().toLowerCase();
     switch (type) {
       case 'local':
-        return new RepoCacheLocal(repository);
+        return new RepoCacheLocal(repository, repoFingerprint);
       case 's3':
-        return new RepoCacheS3(repository, cacheType);
+        return new RepoCacheS3(repository, repoFingerprint, cacheType);
       default:
         logger.warn(
           { cacheType },
           `Repository cache type not supported using type "local" instead`
         );
-        return new RepoCacheLocal(repository);
+        return new RepoCacheLocal(repository, repoFingerprint);
     }
   }
 }
