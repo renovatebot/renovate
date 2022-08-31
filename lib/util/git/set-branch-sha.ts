9ea555051c5769b79a9c49e00193005b64b7b954
RahulGautamSingh <rahultesnik@gmail.com>
2022-08-31 19:52:01 +0530

    disapply partial<T>

--- lib/util/git/set-branch-sha.ts
+++ lib/util/git/set-branch-sha.ts
@@ -1,4 +1,3 @@
-import { partial } from '../../../test/util';
 import { getCache } from '../cache/repository';
 import type { BranchCache } from '../cache/repository/types';
 import { getBranchCommit } from '.';
@@ -10,16 +9,14 @@ export function setBranchShas(
 ): void {
   const cache = getCache();
   const baseBranchSha = getBranchCommit(baseBranchName);
-  // eslint-disable-next-line no-console
-  console.log('BEFORE UPDATE:', cache);
   cache.branches ??= [];
 
   let branch = cache.branches.find((br) => br.branchName === branchName);
   if (!branch) {
-    branch = partial<BranchCache>({
+    branch = {
       branchName: branchName,
       baseBranchName: baseBranchName,
-    });
+    } as BranchCache;
     cache.branches.push(branch);
   }
   branch.sha = commitSha;
@@ -27,6 +24,4 @@ export function setBranchShas(
   branch.parentSha = baseBranchSha;
   branch.isConflicted = false;
   branch.isModified = false;
-  // eslint-disable-next-line no-console
-  console.log('AFTER UPDATE:', cache);
 }
