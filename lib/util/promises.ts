8d2d287a9442a43ce9af2a74baa1551a2533d11c
Sergei Zharinov <zharinov@users.noreply.github.com>
2022-09-06 12:54:40 +0300

    refactor: Create promises utilities

---
+++ lib/util/promises.ts
@@ -0,0 +1,27 @@
+import pAll from 'p-all';
+import pMap from 'p-map';
+
+type PromiseFactory<T> = () => Promise<T>;
+
+export function all<T>(
+  tasks: PromiseFactory<T>[],
+  options?: pAll.Options
+): Promise<T[]> {
+  return pAll(tasks, {
+    concurrency: 5,
+    ...options,
+    stopOnError: false,
+  });
+}
+
+export function map<Element, NewElement>(
+  input: Iterable<Element>,
+  mapper: pMap.Mapper<Element, NewElement>,
+  options?: pMap.Options
+): Promise<NewElement[]> {
+  return pMap(input, mapper, {
+    concurrency: 5,
+    ...options,
+    stopOnError: false,
+  });
+}
