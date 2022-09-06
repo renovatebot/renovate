200925680a019fb7f95b0329201c1b188e36309d
Sergei Zharinov <zharinov@users.noreply.github.com>
2022-09-06 14:14:39 +0300

    fix(util/promises): Disable `stopOnError` option by default

--- lib/util/promises.ts
+++ lib/util/promises.ts
@@ -1,27 +1,74 @@
+import is from '@sindresorhus/is';
 import pAll from 'p-all';
 import pMap from 'p-map';
+import { ExternalHostError } from '../types/errors/external-host-error';
 
 type PromiseFactory<T> = () => Promise<T>;
 
-export function all<T>(
+interface AggregateError {
+  _errors: Error[];
+}
+
+function isAggregateError(err: any): err is AggregateError {
+  return is.array(err?._errors, is.error);
+}
+
+function isExternalHostError(err: any): err is ExternalHostError {
+  return err instanceof ExternalHostError;
+}
+
+function handleError(err: any): never {
+  if (!isAggregateError(err)) {
+    throw err;
+  }
+
+  const errors = err._errors;
+
+  const hostError = errors.find(isExternalHostError);
+  if (hostError) {
+    throw hostError;
+  }
+
+  if (
+    errors.length === 1 ||
+    new Set(errors.map(({ message }) => message)).size === 1
+  ) {
+    const [error] = errors;
+    throw error;
+  }
+
+  throw err;
+}
+
+export async function all<T>(
   tasks: PromiseFactory<T>[],
   options?: pAll.Options
 ): Promise<T[]> {
-  return pAll(tasks, {
-    concurrency: 5,
-    ...options,
-    stopOnError: true,
-  });
+  try {
+    const res = await pAll(tasks, {
+      concurrency: 5,
+      stopOnError: false,
+      ...options,
+    });
+    return res;
+  } catch (err) {
+    return handleError(err);
+  }
 }
 
-export function map<Element, NewElement>(
+export async function map<Element, NewElement>(
   input: Iterable<Element>,
   mapper: pMap.Mapper<Element, NewElement>,
   options?: pMap.Options
 ): Promise<NewElement[]> {
-  return pMap(input, mapper, {
-    concurrency: 5,
-    ...options,
-    stopOnError: true,
-  });
+  try {
+    const res = await pMap(input, mapper, {
+      concurrency: 5,
+      stopOnError: false,
+      ...options,
+    });
+    return res;
+  } catch (err) {
+    return handleError(err);
+  }
 }
