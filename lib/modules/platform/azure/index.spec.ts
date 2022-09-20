4a87b71c8100cc370cee13f2de1a0e6a000ab29e
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2022-09-20 09:28:56 +0300

    prettier fix

--- lib/modules/platform/azure/index.spec.ts
+++ lib/modules/platform/azure/index.spec.ts
@@ -1091,7 +1091,7 @@ describe('modules/platform/azure/index', () => {
         '\n---\n\n - [ ] <!-- rebase-check --> rebase\n<!--renovate-config-hash:-->' +
         'plus also [a link](https://github.com/foo/bar/issues/5)';
       expect(azure.massageMarkdown(prBody)).toBe(
-       'plus also [a link](https://github.com/foo/bar/issues/5)'
+        'plus also [a link](https://github.com/foo/bar/issues/5)'
       );
     });
   });
