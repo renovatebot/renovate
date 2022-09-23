f1d75ca2b6b46ff45762cd10ea7ab5b263aa9fd0
Tharaka De Silva <tharaka.uo@gmail.com>
2022-09-23 18:04:51 +0200

    fix(Docker): Remove unnecessary header key declaration

--- lib/util/http/types.ts
+++ lib/util/http/types.ts
@@ -77,7 +77,6 @@ export interface InternalHttpOptions extends HttpOptions {
 
 export interface HttpHeaders extends IncomingHttpHeaders {
   link?: string | undefined;
-  'x-artifactory-id'?: string | undefined;
 }
 
 export interface HttpResponse<T = string> {
