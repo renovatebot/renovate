33edbf9fa53995e80a82e58e66780bb6f7196f06
Jamie Magee <jamie.magee@gmail.com>
2022-09-26 21:51:57 -0700

    PR comments

--- lib/modules/datasource/kubernetes-api/index.ts
+++ lib/modules/datasource/kubernetes-api/index.ts
@@ -1,13 +1,18 @@
+import JSON5 from 'json5';
+import dataFiles from '../../../data-files.generated';
 import * as kubernetesApiVersioning from '../../versioning/kubernetes-api';
 import { Datasource } from '../datasource';
 import type { GetReleasesConfig, ReleaseResult } from '../types';
-import { KubernetesApiVersions } from './mapping';
 
 export class KubernetesApiDatasource extends Datasource {
   static readonly id = 'kubernetes-api';
+  private readonly kubernetesApiVersions: Record<string, string[]>;
 
   constructor() {
     super(KubernetesApiDatasource.id);
+    this.kubernetesApiVersions = JSON5.parse(
+      dataFiles.get('data/kubernetes-api.json5')!
+    );
   }
 
   override defaultVersioning = kubernetesApiVersioning.id;
@@ -15,7 +20,7 @@ export class KubernetesApiDatasource extends Datasource {
   getReleases({
     packageName,
   }: GetReleasesConfig): Promise<ReleaseResult | null> {
-    const versions = KubernetesApiVersions[packageName];
+    const versions = this.kubernetesApiVersions[packageName];
     if (versions) {
       const releases = versions.map((version) => ({ version }));
       return Promise.resolve({ releases });
