00cc0e43761ed8871440fae3919318d8e94a5837
Rhys Arkins <rhys@arkins.net>
2022-09-27 17:11:22 +0200

    Update lib/util/array.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/util/array.ts
+++ lib/util/array.ts
@@ -1,6 +1,6 @@
 import is from '@sindresorhus/is';
 
-export function coerceArray(input: unknown): any[] {
+export function coerceArray<T>(input: T[] | null | undefined): T[] {
   if (is.array(input)) {
     return input;
   }
