eb5181e217a718d9c6b25fe6b4407fa3e6bc50b2
Sergei Zharinov <zharinov@users.noreply.github.com>
2022-09-28 14:35:16 +0300

    refactor: Make invalid schema reporting optional (#18024)

--- lib/util/schema.spec.ts
+++ lib/util/schema.spec.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { logger } from '../../test/util';
 import * as memCache from './cache/memory';
-import { checkSchema, reportErrors } from './schema';
+import * as schema from './schema';
 
 describe('util/schema', () => {
   beforeEach(() => {
@@ -10,33 +10,41 @@ describe('util/schema', () => {
   });
 
   it('validates data', () => {
-    const schema = z.object({ foo: z.string() });
+    const testSchema = z.object({ foo: z.string() });
     const validData = { foo: 'bar' };
 
-    const res = checkSchema(schema, validData);
+    const res = schema.match(testSchema, validData);
     expect(res).toBeTrue();
-
-    reportErrors();
-    expect(logger.logger.warn).not.toHaveBeenCalledOnce();
   });
 
-  it('reports nothing if there are no any reports', () => {
-    reportErrors();
+  it('returns false for invalid data', () => {
+    const testSchema = z.object({ foo: z.string() });
+    const invalidData = { foo: 123 };
+
+    const res = schema.match(testSchema, invalidData);
+    expect(res).toBeFalse();
+
+    schema.reportErrors();
     expect(logger.logger.warn).not.toHaveBeenCalled();
   });
 
-  it('reports same warning once', () => {
-    const schema = z.object(
+  it('reports nothing if there are no any reports', () => {
+    schema.reportErrors();
+    expect(logger.logger.warn).not.toHaveBeenCalled();
+  });
+
+  it('reports same warning one time', () => {
+    const testSchema = z.object(
       { foo: z.string() },
       { description: 'Some test schema' }
     );
     const invalidData = { foo: 42 };
 
-    checkSchema(schema, invalidData);
-    checkSchema(schema, invalidData);
-    checkSchema(schema, invalidData);
-    checkSchema(schema, invalidData);
-    reportErrors();
+    schema.match(testSchema, invalidData, true);
+    schema.match(testSchema, invalidData, true);
+    schema.match(testSchema, invalidData, true);
+    schema.match(testSchema, invalidData, true);
+    schema.reportErrors();
 
     expect(logger.logger.warn).toHaveBeenCalledOnce();
     expect(logger.logger.warn.mock.calls[0]).toMatchObject([
@@ -46,11 +54,11 @@ describe('util/schema', () => {
   });
 
   it('reports unspecified schema', () => {
-    const schema = z.object({ foo: z.string() });
+    const testSchema = z.object({ foo: z.string() });
     const invalidData = { foo: 42 };
 
-    checkSchema(schema, invalidData);
-    reportErrors();
+    schema.match(testSchema, invalidData, true);
+    schema.reportErrors();
 
     expect(logger.logger.warn).toHaveBeenCalledOnce();
     expect(logger.logger.warn.mock.calls[0]).toMatchObject([
