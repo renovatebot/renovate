1906391cdde9996d085187719d9bd895feb4c370
Jamie Magee <jamie.magee@gmail.com>
2022-10-02 23:10:53 -0700

    `in` now type checks

--- lib/util/http/github.ts
+++ lib/util/http/github.ts
@@ -431,9 +431,8 @@ export class GithubHttp extends Http<GithubHttpOptions, GithubHttpOptions> {
       });
       const repositoryData = res?.data?.repository;
       if (
-        repositoryData &&
-        is.plainObject(repositoryData) &&
-        repositoryData[fieldName]
+        is.nonEmptyObject(repositoryData) &&
+        !is.nullOrUndefined(repositoryData[fieldName])
       ) {
         optimalCount = count;
 
