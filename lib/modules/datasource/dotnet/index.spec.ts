981408281c3d0f63f1462b0d39cf5e1530de3cb9
Jamie Magee <jamie.magee@gmail.com>
2022-10-05 21:31:39 -0700

    PR comments

--- lib/modules/datasource/dotnet/index.spec.ts
+++ lib/modules/datasource/dotnet/index.spec.ts
@@ -2,7 +2,7 @@ import { getPkgReleases } from '..';
 import { Fixtures } from '../../../../test/fixtures';
 import * as httpMock from '../../../../test/http-mock';
 import { EXTERNAL_HOST_ERROR } from '../../../constants/error-messages';
-import { DotnetSdkDatasource } from '.';
+import { DotnetDatasource } from '.';
 
 const releasesIndex = Fixtures.getJson('releases-index.json');
 const releases7_0 = Fixtures.getJson('releases-7.0.json');
@@ -13,7 +13,7 @@ const releases3_1 = Fixtures.getJson('releases-3.1.json');
 const baseUrl =
   'https://dotnetcli.blob.core.windows.net/dotnet/release-metadata';
 
-describe('modules/datasource/dotnet-sdk/index', () => {
+describe('modules/datasource/dotnet/index', () => {
   describe('getReleases', () => {
     afterEach(() => httpMock.clear(false));
 
@@ -22,7 +22,7 @@ describe('modules/datasource/dotnet-sdk/index', () => {
 
       expect(
         await getPkgReleases({
-          datasource: DotnetSdkDatasource.id,
+          datasource: DotnetDatasource.id,
           depName: 'dotnet-sdk',
         })
       ).toBeNull();
@@ -38,7 +38,7 @@ describe('modules/datasource/dotnet-sdk/index', () => {
 
       expect(
         await getPkgReleases({
-          datasource: DotnetSdkDatasource.id,
+          datasource: DotnetDatasource.id,
           depName: 'dotnet-sdk',
         })
       ).toBeNull();
@@ -49,7 +49,7 @@ describe('modules/datasource/dotnet-sdk/index', () => {
 
       await expect(
         getPkgReleases({
-          datasource: DotnetSdkDatasource.id,
+          datasource: DotnetDatasource.id,
           depName: 'dotnet-sdk',
         })
       ).rejects.toThrow(EXTERNAL_HOST_ERROR);
@@ -71,7 +71,7 @@ describe('modules/datasource/dotnet-sdk/index', () => {
 
       await expect(
         getPkgReleases({
-          datasource: DotnetSdkDatasource.id,
+          datasource: DotnetDatasource.id,
           depName: 'dotnet-sdk',
         })
       ).rejects.toThrow(EXTERNAL_HOST_ERROR);
@@ -82,7 +82,7 @@ describe('modules/datasource/dotnet-sdk/index', () => {
 
       expect(
         await getPkgReleases({
-          datasource: DotnetSdkDatasource.id,
+          datasource: DotnetDatasource.id,
           depName: 'dotnet-sdk',
         })
       ).toBeNull();
@@ -98,13 +98,13 @@ describe('modules/datasource/dotnet-sdk/index', () => {
 
       expect(
         await getPkgReleases({
-          datasource: DotnetSdkDatasource.id,
+          datasource: DotnetDatasource.id,
           depName: 'dotnet-sdk',
         })
       ).toBeNull();
     });
 
-    it('returns real data', async () => {
+    it('returns real data for sdk', async () => {
       httpMock
         .scope(baseUrl)
         .get('/releases-index.json')
@@ -119,10 +119,55 @@ describe('modules/datasource/dotnet-sdk/index', () => {
         .reply(200, releases3_1);
 
       const res = await getPkgReleases({
-        datasource: DotnetSdkDatasource.id,
+        datasource: DotnetDatasource.id,
         depName: 'dotnet-sdk',
       });
+
       expect(res).toBeDefined();
+      expect(res?.releases).toHaveLength(88);
+      expect(res?.releases).toIncludeAllPartialMembers([
+        { version: '3.1.100-preview1-014459' },
+        { version: '3.1.423' },
+        { version: '5.0.100-preview.1.20155.7' },
+        { version: '5.0.408' },
+        { version: '6.0.100-preview.1.21103.13' },
+        { version: '6.0.401' },
+        { version: '7.0.100-preview.1.22110.4' },
+        { version: '7.0.100-rc.1.22431.12' },
+      ]);
+    });
+
+    it('returns real data for runtime', async () => {
+      httpMock
+        .scope(baseUrl)
+        .get('/releases-index.json')
+        .reply(200, releasesIndex)
+        .get('/7.0/releases.json')
+        .reply(200, releases7_0)
+        .get('/6.0/releases.json')
+        .reply(200, releases6_0)
+        .get('/5.0/releases.json')
+        .reply(200, releases5_0)
+        .get('/3.1/releases.json')
+        .reply(200, releases3_1);
+
+      const res = await getPkgReleases({
+        datasource: DotnetDatasource.id,
+        depName: 'dotnet-runtime',
+      });
+
+      expect(res).toBeDefined();
+      expect(res?.releases).toHaveLength(88);
+      expect(res?.releases).toIncludeAllPartialMembers([
+        { version: '3.1.0-preview1.19506.1' },
+        { version: '3.1.29' },
+        { version: '5.0.0-preview.1.20120.5' },
+        { version: '5.0.17' },
+        { version: '6.0.0-preview.1.21102.12' },
+        { version: '6.0.9' },
+        { version: '7.0.0-preview.1.22076.8' },
+        { version: '7.0.0-rc.1.22426.10' },
+      ]);
     });
   });
 });
