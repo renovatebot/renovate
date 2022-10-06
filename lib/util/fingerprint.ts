cc7ae05f5f0bf5b2e53997ac96dbd33adee2077b
Rhys Arkins <rhys@arkins.net>
2022-10-06 14:48:11 +0200

    sha512

--- lib/util/fingerprint.ts
+++ lib/util/fingerprint.ts
@@ -2,5 +2,5 @@ import hasha from 'hasha';
 import { safeStringify } from './stringify';
 
 export function fingerprint(input: unknown): string {
-  return hasha(safeStringify(input), { algorithm: 'sha256' });
+  return hasha(safeStringify(input));
 }
