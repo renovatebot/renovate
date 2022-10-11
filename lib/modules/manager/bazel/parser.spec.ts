2825a156de8e541243386f55aa6f709a4efd7e23
Sergei Zharinov <zharinov@users.noreply.github.com>
2022-10-11 15:23:09 +0300

    refactor(bazel): Simplify parser output structure

--- lib/modules/manager/bazel/parser.spec.ts
+++ lib/modules/manager/bazel/parser.spec.ts
@@ -9,42 +9,36 @@ describe('modules/manager/bazel/parser', () => {
 
     const res = parse(input);
 
-    expect(res).toEqual({
-      type: 'array',
-      value:
-        'go_repository(name = "foo")\nmaybe(go_repository, name = "bar", deps = ["baz", "qux"])',
-      offset: 0,
-      children: [
-        {
-          type: 'record',
-          value: 'go_repository(name = "foo")',
-          offset: 0,
-          children: {
-            rule: { type: 'string', value: 'go_repository', offset: 0 },
-            name: { type: 'string', value: 'foo', offset: 22 },
+    expect(res).toEqual([
+      {
+        type: 'record',
+        value: 'go_repository(name = "foo")',
+        offset: 0,
+        children: {
+          rule: { type: 'string', value: 'go_repository', offset: 0 },
+          name: { type: 'string', value: 'foo', offset: 22 },
+        },
+      },
+      {
+        type: 'record',
+        value: 'maybe(go_repository, name = "bar", deps = ["baz", "qux"])',
+        offset: 28,
+        children: {
+          rule: { type: 'string', value: 'go_repository', offset: 34 },
+          name: { type: 'string', value: 'bar', offset: 57 },
+          deps: {
+            type: 'array',
+            value: '["baz", "qux"]',
+            offset: 70,
+            children: [
+              { type: 'string', value: 'baz', offset: 72 },
+              { type: 'string', value: 'qux', offset: 79 },
+            ],
           },
         },
-        {
-          type: 'record',
-          value: 'maybe(go_repository, name = "bar", deps = ["baz", "qux"])',
-          offset: 28,
-          children: {
-            rule: { type: 'string', value: 'go_repository', offset: 34 },
-            name: { type: 'string', value: 'bar', offset: 57 },
-            deps: {
-              type: 'array',
-              value: '["baz", "qux"]',
-              offset: 70,
-              children: [
-                { type: 'string', value: 'baz', offset: 72 },
-                { type: 'string', value: 'qux', offset: 79 },
-              ],
-            },
-          },
-        },
-      ],
-    });
-    expect(extract(res!)).toMatchObject([
+      },
+    ]);
+    expect(res?.map(extract)).toMatchObject([
       { rule: 'go_repository', name: 'foo' },
       { rule: 'go_repository', name: 'bar', deps: ['baz', 'qux'] },
     ]);
@@ -66,45 +60,44 @@ describe('modules/manager/bazel/parser', () => {
 
     const res = parse(input);
 
-    expect(res).toMatchObject({
-      children: [
-        {
-          children: {
-            rule: { value: 'http_archive' },
-            name: { value: 'aspect_rules_js' },
-            sha256: {
-              value:
-                'db9f446752fe4100320cf8487e8fd476b9af0adf6b99b601bcfd70b289bb0598',
-            },
-            strip_prefix: { value: 'rules_js-1.1.2' },
-            url: {
-              value:
-                'https://github.com/aspect-build/rules_js/archive/refs/tags/v1.1.2.tar.gz',
-            },
+    expect(res).toMatchObject([
+      {
+        children: {
+          rule: { value: 'http_archive' },
+          name: { value: 'aspect_rules_js' },
+          sha256: {
+            value:
+              'db9f446752fe4100320cf8487e8fd476b9af0adf6b99b601bcfd70b289bb0598',
+          },
+          strip_prefix: { value: 'rules_js-1.1.2' },
+          url: {
+            value:
+              'https://github.com/aspect-build/rules_js/archive/refs/tags/v1.1.2.tar.gz',
           },
         },
-        {
-          children: {
-            rule: { value: 'http_archive' },
-            name: { value: 'rules_nodejs' },
-            sha256: {
-              value:
-                '5aef09ed3279aa01d5c928e3beb248f9ad32dde6aafe6373a8c994c3ce643064',
-            },
-            urls: {
-              type: 'array',
-              children: [
-                {
-                  value:
-                    'https://github.com/bazelbuild/rules_nodejs/releases/download/5.5.3/rules_nodejs-core-5.5.3.tar.gz',
-                },
-              ],
-            },
+      },
+      {
+        children: {
+          rule: { value: 'http_archive' },
+          name: { value: 'rules_nodejs' },
+          sha256: {
+            value:
+              '5aef09ed3279aa01d5c928e3beb248f9ad32dde6aafe6373a8c994c3ce643064',
+          },
+          urls: {
+            type: 'array',
+            children: [
+              {
+                value:
+                  'https://github.com/bazelbuild/rules_nodejs/releases/download/5.5.3/rules_nodejs-core-5.5.3.tar.gz',
+              },
+            ],
           },
         },
-      ],
-    });
-    expect(extract(res!)).toMatchObject([
+      },
+    ]);
+
+    expect(res?.map(extract)).toMatchObject([
       {
         rule: 'http_archive',
         name: 'aspect_rules_js',
@@ -134,25 +127,24 @@ describe('modules/manager/bazel/parser', () => {
 
     const res = parse(input);
 
-    expect(res).toMatchObject({
-      children: [
-        {
-          children: {
-            name: { value: 'rules_nodejs' },
-            rule: { value: 'http_archive' },
-            sha256: {
-              value:
-                '5aef09ed3279aa01d5c928e3beb248f9ad32dde6aafe6373a8c994c3ce643064',
-            },
-            url: {
-              value:
-                'https://github.com/bazelbuild/rules_nodejs/releases/download/5.5.3/rules_nodejs-core-5.5.3.tar.gz',
-            },
+    expect(res).toMatchObject([
+      {
+        children: {
+          name: { value: 'rules_nodejs' },
+          rule: { value: 'http_archive' },
+          sha256: {
+            value:
+              '5aef09ed3279aa01d5c928e3beb248f9ad32dde6aafe6373a8c994c3ce643064',
+          },
+          url: {
+            value:
+              'https://github.com/bazelbuild/rules_nodejs/releases/download/5.5.3/rules_nodejs-core-5.5.3.tar.gz',
           },
         },
-      ],
-    });
-    expect(extract(res!)).toMatchObject([
+      },
+    ]);
+
+    expect(res?.map(extract)).toMatchObject([
       {
         rule: 'http_archive',
         name: 'rules_nodejs',
@@ -176,37 +168,35 @@ describe('modules/manager/bazel/parser', () => {
 
     const res = parse(input);
 
-    expect(res).toMatchObject({
-      children: [
-        {
-          children: {
-            name: { value: 'bazel_toolchains' },
-            rule: { value: 'http_archive' },
-            sha256: {
-              value:
-                '4b1468b254a572dbe134cc1fd7c6eab1618a72acd339749ea343bd8f55c3b7eb',
-            },
-            strip_prefix: {
-              value:
-                'bazel-toolchains-d665ccfa3e9c90fa789671bf4ef5f7c19c5715c4',
-            },
-            urls: {
-              children: [
-                {
-                  value:
-                    'https://mirror.bazel.build/github.com/bazelbuild/bazel-toolchains/archive/d665ccfa3e9c90fa789671bf4ef5f7c19c5715c4.tar.gz',
-                },
-                {
-                  value:
-                    'https://github.com/bazelbuild/bazel-toolchains/archive/d665ccfa3e9c90fa789671bf4ef5f7c19c5715c4.tar.gz',
-                },
-              ],
-            },
+    expect(res).toMatchObject([
+      {
+        children: {
+          name: { value: 'bazel_toolchains' },
+          rule: { value: 'http_archive' },
+          sha256: {
+            value:
+              '4b1468b254a572dbe134cc1fd7c6eab1618a72acd339749ea343bd8f55c3b7eb',
+          },
+          strip_prefix: {
+            value: 'bazel-toolchains-d665ccfa3e9c90fa789671bf4ef5f7c19c5715c4',
+          },
+          urls: {
+            children: [
+              {
+                value:
+                  'https://mirror.bazel.build/github.com/bazelbuild/bazel-toolchains/archive/d665ccfa3e9c90fa789671bf4ef5f7c19c5715c4.tar.gz',
+              },
+              {
+                value:
+                  'https://github.com/bazelbuild/bazel-toolchains/archive/d665ccfa3e9c90fa789671bf4ef5f7c19c5715c4.tar.gz',
+              },
+            ],
           },
         },
-      ],
-    });
-    expect(extract(res!)).toMatchObject([
+      },
+    ]);
+
+    expect(res?.map(extract)).toMatchObject([
       {
         name: 'bazel_toolchains',
         rule: 'http_archive',
