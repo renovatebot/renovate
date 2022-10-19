9ec41b90300a969339bf35d58ae7b1a4f888653f
philipabed <philip.abed@whitesourcesoftware.com>
2022-10-19 14:56:41 +0300

    refactor text decoder

--- lib/modules/platform/codecommit/index.ts
+++ lib/modules/platform/codecommit/index.ts
@@ -331,7 +331,7 @@ export async function getRawFile(
     fileName,
     branchOrTag
   );
-  if (!fileRes || !fileRes.fileContent) {
+  if (!fileRes?.fileContent) {
     return null;
   }
   const buf = Buffer.from(fileRes.fileContent);
