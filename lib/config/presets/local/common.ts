194e6bbcc9ab170d467a04a7cf6c2f223b4bbb87
Rhys Arkins <rhys@arkins.net>
2022-10-21 13:00:37 +0300

    Update lib/config/presets/local/common.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/config/presets/local/common.ts
+++ lib/config/presets/local/common.ts
@@ -17,7 +17,7 @@ export async function fetchJSONFile(
       throw err;
     }
 
-    logger.debug(`Could not fetch ${fileName} from repo ${repo}`);
+    logger.debug(`Preset file ${fileName} not found in ${repo}`);
 
     throw new Error(PRESET_DEP_NOT_FOUND);
   }
