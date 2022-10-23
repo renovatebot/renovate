b53c581e5c4e3da46a478d6bcd000d3dff34ac3d
Sebastian Poxhofer <secustor@users.noreply.github.com>
2022-10-23 06:36:35 +0200

    feat(telemetry): add OpenTelemetry tracing MVC (#15731)

--- lib/workers/repository/index.ts
+++ lib/workers/repository/index.ts
@@ -3,6 +3,7 @@ import { GlobalConfig } from '../../config/global';
 import { applySecretsToConfig } from '../../config/secrets';
 import type { RenovateConfig } from '../../config/types';
 import { pkg } from '../../expose.cjs';
+import { instrument } from '../../instrumentation';
 import { logger, setMeta } from '../../logger';
 import { removeDanglingContainers } from '../../util/exec/docker';
 import { deleteLocalFile, privateCacheDir } from '../../util/fs';
@@ -42,16 +43,22 @@ export async function renovateRepository(
     logger.debug('Using localDir: ' + localDir);
     config = await initRepo(config);
     addSplit('init');
-    const { branches, branchList, packageFiles } = await extractDependencies(
-      config
+    const { branches, branchList, packageFiles } = await instrument(
+      'extract',
+      () => extractDependencies(config)
     );
     if (
       GlobalConfig.get('dryRun') !== 'lookup' &&
       GlobalConfig.get('dryRun') !== 'extract'
     ) {
-      await ensureOnboardingPr(config, packageFiles, branches);
+      await instrument('onboarding', () =>
+        ensureOnboardingPr(config, packageFiles, branches)
+      );
       addSplit('onboarding');
-      const res = await updateRepo(config, branches);
+
+      const res = await instrument('update', () =>
+        updateRepo(config, branches)
+      );
       setMeta({ repository: config.repository });
       addSplit('update');
       await setBranchCache(branches);
