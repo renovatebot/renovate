a89d5078c043924a3e58ca8295041ba873ab6a0c
philipabed <philip.abed@whitesourcesoftware.com>
2022-10-25 15:08:46 +0300

    error gracefully when editor parsing fails

--- lib/util/json-writer/editor-config.ts
+++ lib/util/json-writer/editor-config.ts
@@ -8,19 +8,15 @@ import { IndentationType } from './indentation-type';
 export class EditorConfig {
   public static async getCodeFormat(fileName: string): Promise<CodeFormat> {
     const { localDir } = GlobalConfig.get();
-    let knownProps = undefined;
     try {
-      knownProps = await parse(upath.join(localDir, fileName));
+      const knownProps = await parse(upath.join(localDir, fileName));
       return {
         indentationSize: EditorConfig.getIndentationSize(knownProps),
         indentationType: EditorConfig.getIndentationType(knownProps),
       };
     } catch (err) {
       logger.warn({ err }, 'Failed to parse editor config');
-      return {
-        indentationSize: undefined,
-        indentationType: undefined,
-      };
+      return {};
     }
   }
 
