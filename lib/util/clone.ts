c3a00b0c7c68b6d3cf6931e6c899f19cd26ef4dd
RahulGautamSingh <rahultesnik@gmail.com>
2022-10-26 15:13:30 +0530

    Update lib/util/clone.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/util/clone.ts
+++ lib/util/clone.ts
@@ -2,5 +2,5 @@ import { quickStringify } from './stringify';
 
 export function clone<T>(input: T | null = null): T {
   const sortedinput = quickStringify(input);
-  return sortedinput ? JSON.parse(sortedinput) : {};
+  return sortedinput ? JSON.parse(sortedinput) : null;
 }
