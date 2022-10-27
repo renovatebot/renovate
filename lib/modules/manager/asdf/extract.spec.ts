2c7f8271bbec973c72cd570d180e05e8d05f2cd2
Jonatan Männchen <jonatan@maennchen.ch>
2022-10-27 19:47:33 +0100

    feat(manager/asdf): add additional asdf supported tools
    Adds support for:
    awscli, bun, cargo-make, clojure, crystal, deno, direnv,
    dprint, elixir, elm, erlang, gauche, golang, haskell, helmfile,
    helm, hugo, idris, java, julia, just, kotlin, kustomize, lua, nim,
    nodejs, ocaml, perl, php, python, ruby, rust, scala, shellcheck,
    shfmt, terraform, trivy, zig
    
    Code is largely extracted from:
    https://github.com/kachick/renovate-config-asdf/tree/1.8.0/plugins
    
    Thanks @kachick!

--- lib/modules/manager/asdf/extract.spec.ts
+++ lib/modules/manager/asdf/extract.spec.ts
@@ -8,10 +8,11 @@ describe('modules/manager/asdf/extract', () => {
         deps: [
           {
             currentValue: '16.16.0',
-            datasource: 'github-tags',
+            datasource: 'github-releases',
             depName: 'node',
             packageName: 'nodejs/node',
             versioning: 'node',
+            extractVersion: '^v(?<version>\\S+)',
           },
         ],
       });
@@ -35,25 +36,353 @@ describe('modules/manager/asdf/extract', () => {
         deps: [
           {
             currentValue: '16.16.0',
-            datasource: 'github-tags',
+            datasource: 'github-releases',
             depName: 'node',
             packageName: 'nodejs/node',
             versioning: 'node',
+            extractVersion: '^v(?<version>\\S+)',
           },
         ],
       });
     });
 
     it('can handle multiple tools in one file', () => {
-      const res = extractPackageFile('nodejs 16.16.0\ndummy 1.2.3');
+      const res = extractPackageFile(
+        `awscli 2.8.6
+bun 0.2.2
+cargo-make 0.36.2
+clojure 1.11.1.1182
+crystal 1.6.1
+deno 1.26.2
+direnv 2.32.1
+dprint 0.32.2
+elixir 1.14.1
+elm 0.19.1
+erlang 25.1.2
+gauche 0.9.12
+golang 1.19.2
+haskell 9.4.2
+helm 3.10.1
+helmfile 0.147.0
+hugo 0.104.3
+idris 1.3.4
+java adoptopenjdk-16.0.0+36
+julia 1.8.2
+just 1.7.0
+kotlin 1.7.20
+kustomize 4.5.7
+lua 5.4.4
+nim 1.6.8
+nodejs 18.12.0
+ocaml 4.14.0
+perl 5.37.5
+php 8.1.12
+python 3.11.0
+ruby 3.1.2
+rust 1.64.0
+scala 3.2.1
+shellcheck 0.8.0
+shfmt 3.5.1
+terraform 1.3.3
+trivy 0.33.0
+zig 0.9.1
+dummy 1.2.3
+`
+      );
       expect(res).toEqual({
         deps: [
           {
-            currentValue: '16.16.0',
+            currentValue: '2.8.6',
             datasource: 'github-tags',
-            depName: 'node',
+            packageName: 'aws/aws-cli',
+            versioning: 'semver',
+            depName: 'awscli',
+          },
+          {
+            currentValue: '0.2.2',
+            datasource: 'github-releases',
+            packageName: 'oven-sh/bun',
+            versioning: 'semver',
+            depName: 'bun',
+            extractVersion: '^bun-v(?<version>\\S+)',
+          },
+          {
+            currentValue: '0.36.2',
+            datasource: 'github-releases',
+            packageName: 'sagiegurari/cargo-make',
+            versioning: 'semver',
+            depName: 'cargo-make',
+          },
+          {
+            currentValue: '1.11.1.1182',
+            datasource: 'github-tags',
+            packageName: 'clojure/brew-install',
+            versioning:
+              'regex:^(?<major>\\d+?)\\.(?<minor>\\d+?)\\.(?<patch>\\d+)\\.(?<build>\\d+)$',
+            depName: 'clojure',
+          },
+          {
+            currentValue: '1.6.1',
+            datasource: 'github-tags',
+            packageName: 'crystal-lang/crystal',
+            versioning: 'semver',
+            depName: 'crystal',
+          },
+          {
+            currentValue: '1.26.2',
+            datasource: 'github-releases',
+            packageName: 'denoland/deno',
+            versioning: 'semver',
+            depName: 'deno',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '2.32.1',
+            datasource: 'github-releases',
+            packageName: 'direnv/direnv',
+            versioning: 'semver',
+            depName: 'direnv',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '0.32.2',
+            datasource: 'github-releases',
+            packageName: 'dprint/dprint',
+            versioning: 'semver',
+            depName: 'dprint',
+          },
+          {
+            currentValue: '1.14.1',
+            datasource: 'github-tags',
+            packageName: 'elixir-lang/elixir',
+            versioning: 'semver',
+            depName: 'elixir',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '0.19.1',
+            datasource: 'github-releases',
+            packageName: 'elm/compiler',
+            versioning: 'semver',
+            depName: 'elm',
+          },
+          {
+            currentValue: '25.1.2',
+            datasource: 'github-tags',
+            packageName: 'erlang/otp',
+            extractVersion: '^OTP-(?<version>\\S+)',
+            versioning:
+              'regex:^(?<major>\\d+?)\\.(?<minor>\\d+?)(\\.(?<patch>\\d+))?$',
+            depName: 'erlang',
+          },
+          {
+            currentValue: '0.9.12',
+            datasource: 'docker',
+            packageName: 'practicalscheme/gauche',
+            versioning: 'semver',
+            depName: 'gauche',
+          },
+          {
+            currentValue: '1.19.2',
+            datasource: 'github-tags',
+            packageName: 'golang/go',
+            versioning: 'semver',
+            depName: 'golang',
+            extractVersion: '^go(?<version>\\S+)',
+          },
+          {
+            currentValue: '9.4.2',
+            datasource: 'github-tags',
+            packageName: 'ghc/ghc',
+            versioning: 'semver',
+            depName: 'haskell',
+            extractVersion: '^ghc-(?<version>\\S+?)-release',
+          },
+          {
+            currentValue: '3.10.1',
+            datasource: 'github-tags',
+            packageName: 'helm/helm',
+            versioning: 'semver',
+            depName: 'helm',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '0.147.0',
+            datasource: 'github-releases',
+            packageName: 'helmfile/helmfile',
+            versioning: 'semver',
+            depName: 'helmfile',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '0.104.3',
+            datasource: 'github-releases',
+            packageName: 'gohugoio/hugo',
+            versioning: 'semver',
+            depName: 'hugo',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '1.3.4',
+            datasource: 'github-tags',
+            packageName: 'idris-lang/Idris-dev',
+            versioning: 'semver',
+            depName: 'idris',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: 'adoptopenjdk-16.0.0+36',
+            datasource: 'github-releases',
+            packageName: 'adoptium/temurin16-binaries',
+            versioning: 'hermit',
+            depName: 'java',
+            extractVersion: '^adoptopenjdk-(?<version>\\S+)',
+          },
+          {
+            currentValue: '1.8.2',
+            datasource: 'github-releases',
+            packageName: 'JuliaLang/julia',
+            versioning: 'semver',
+            depName: 'julia',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '1.7.0',
+            datasource: 'github-releases',
+            packageName: 'casey/just',
+            versioning: 'semver',
+            depName: 'just',
+          },
+          {
+            currentValue: '1.7.20',
+            datasource: 'github-releases',
+            packageName: 'JetBrains/kotlin',
+            versioning: 'semver',
+            depName: 'kotlin',
+            extractVersion: '^(Kotlin |v)(?<version>\\S+)',
+          },
+          {
+            currentValue: '4.5.7',
+            datasource: 'github-tags',
+            packageName: 'kubernetes-sigs/kustomize',
+            versioning: 'semver',
+            depName: 'kustomize',
+            extractVersion: '^kustomize/v(?<version>\\S+)',
+          },
+          {
+            currentValue: '5.4.4',
+            datasource: 'github-releases',
+            packageName: 'lua/lua',
+            versioning: 'semver',
+            depName: 'lua',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '1.6.8',
+            datasource: 'github-tags',
+            packageName: 'nim-lang/Nim',
+            versioning: 'semver',
+            depName: 'nim',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '18.12.0',
+            datasource: 'github-releases',
             packageName: 'nodejs/node',
             versioning: 'node',
+            depName: 'node',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '4.14.0',
+            datasource: 'github-releases',
+            packageName: 'ocaml/ocaml',
+            versioning: 'semver',
+            depName: 'ocaml',
+          },
+          {
+            currentValue: '5.37.5',
+            datasource: 'github-tags',
+            packageName: 'Perl/perl5',
+            versioning: 'semver',
+            depName: 'perl',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '8.1.12',
+            datasource: 'github-tags',
+            packageName: 'php/php-src',
+            versioning: 'semver',
+            depName: 'php',
+            extractVersion: '^php-(?<version>\\S+)',
+          },
+          {
+            currentValue: '3.11.0',
+            datasource: 'github-tags',
+            packageName: 'python/cpython',
+            versioning: 'semver',
+            depName: 'python',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '3.1.2',
+            datasource: 'ruby-version',
+            packageName: 'ruby-version',
+            versioning: 'semver',
+            depName: 'ruby',
+          },
+          {
+            currentValue: '1.64.0',
+            datasource: 'github-tags',
+            packageName: 'rust-lang/rust',
+            versioning: 'semver',
+            depName: 'rust',
+          },
+          {
+            currentValue: '3.2.1',
+            datasource: 'github-tags',
+            packageName: 'lampepfl/dotty',
+            versioning: 'semver',
+            depName: 'scala',
+          },
+          {
+            currentValue: '0.8.0',
+            datasource: 'github-releases',
+            packageName: 'koalaman/shellcheck',
+            versioning: 'semver',
+            depName: 'shellcheck',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '3.5.1',
+            datasource: 'github-releases',
+            packageName: 'mvdan/sh',
+            versioning: 'semver',
+            depName: 'shfmt',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '1.3.3',
+            datasource: 'github-tags',
+            packageName: 'hashicorp/terraform',
+            versioning: 'semver',
+            depName: 'terraform',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '0.33.0',
+            datasource: 'github-tags',
+            packageName: 'aquasecurity/trivy',
+            versioning: 'semver',
+            depName: 'trivy',
+            extractVersion: '^v(?<version>\\S+)',
+          },
+          {
+            currentValue: '0.9.1',
+            datasource: 'github-tags',
+            packageName: 'ziglang/zig',
+            versioning: 'semver',
+            depName: 'zig',
           },
           {
             depName: 'dummy',
@@ -84,10 +413,11 @@ describe('modules/manager/asdf/extract', () => {
               deps: [
                 {
                   currentValue: data.expect,
-                  datasource: 'github-tags',
+                  datasource: 'github-releases',
                   depName: 'node',
                   packageName: 'nodejs/node',
                   versioning: 'node',
+                  extractVersion: '^v(?<version>\\S+)',
                 },
               ],
             });
@@ -115,10 +445,11 @@ describe('modules/manager/asdf/extract', () => {
           deps: [
             {
               currentValue: '16.16.0',
-              datasource: 'github-tags',
+              datasource: 'github-releases',
               depName: 'node',
               packageName: 'nodejs/node',
               versioning: 'node',
+              extractVersion: '^v(?<version>\\S+)',
             },
           ],
         });
@@ -130,11 +461,12 @@ describe('modules/manager/asdf/extract', () => {
           deps: [
             {
               currentValue: '16.16.0',
-              datasource: 'github-tags',
+              datasource: 'github-releases',
               depName: 'node',
               packageName: 'nodejs/node',
               versioning: 'node',
               skipReason: 'ignored',
+              extractVersion: '^v(?<version>\\S+)',
             },
           ],
         });
---
+++ lib/modules/manager/asdf/upgradeable-tooling.ts
@@ -0,0 +1,260 @@
+import { DockerDatasource } from '../../datasource/docker';
+import { GithubReleasesDatasource } from '../../datasource/github-releases';
+import { GithubTagsDatasource } from '../../datasource/github-tags';
+import * as nodeVersioning from '../../versioning/node';
+import * as regexVersioning from '../../versioning/regex';
+import * as semverVersioning from '../../versioning/semver';
+import * as hermitVersioning from '../../versioning/hermit';
+import type { PackageDependency } from '../types';
+
+type StaticTooling = Pick<
+  PackageDependency,
+  'depName' | 'datasource' | 'packageName' | 'versioning' | 'extractVersion'
+>;
+
+type DynamicTooling = (version: string) => StaticTooling | undefined;
+
+export const upgradeableTooling: Record<
+  string,
+  StaticTooling | DynamicTooling
+> = {
+  awscli: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'aws/aws-cli',
+    versioning: semverVersioning.id,
+  },
+  bun: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'oven-sh/bun',
+    versioning: semverVersioning.id,
+    extractVersion: '^bun-v(?<version>\\S+)',
+  },
+  'cargo-make': {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'sagiegurari/cargo-make',
+    versioning: semverVersioning.id,
+  },
+  clojure: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'clojure/brew-install',
+    versioning: `${regexVersioning.id}:^(?<major>\\d+?)\\.(?<minor>\\d+?)\\.(?<patch>\\d+)\\.(?<build>\\d+)$`,
+  },
+  crystal: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'crystal-lang/crystal',
+    versioning: semverVersioning.id,
+  },
+  deno: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'denoland/deno',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  direnv: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'direnv/direnv',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  dprint: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'dprint/dprint',
+    versioning: semverVersioning.id,
+  },
+  elixir: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'elixir-lang/elixir',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  elm: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'elm/compiler',
+    versioning: semverVersioning.id,
+  },
+  erlang: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'erlang/otp',
+    extractVersion: '^OTP-(?<version>\\S+)',
+    versioning: `${regexVersioning.id}:^(?<major>\\d+?)\\.(?<minor>\\d+?)(\\.(?<patch>\\d+))?$`,
+  },
+  gauche: {
+    datasource: DockerDatasource.id,
+    packageName: 'practicalscheme/gauche',
+    versioning: semverVersioning.id,
+  },
+  golang: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'golang/go',
+    versioning: semverVersioning.id,
+    extractVersion: '^go(?<version>\\S+)',
+  },
+  haskell: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'ghc/ghc',
+    versioning: semverVersioning.id,
+    extractVersion: '^ghc-(?<version>\\S+?)-release',
+  },
+  helm: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'helm/helm',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  helmfile: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'helmfile/helmfile',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  hugo: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'gohugoio/hugo',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  idris: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'idris-lang/Idris-dev',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  java: (version) => {
+    const supportedMajorVersions = ['16', '17', '18'];
+    for (const majorVersion of supportedMajorVersions) {
+      if (!version.startsWith(`adoptopenjdk-${majorVersion}`)) {
+        continue;
+      }
+      return {
+        datasource: GithubReleasesDatasource.id,
+        packageName: `adoptium/temurin${majorVersion}-binaries`,
+        versioning: hermitVersioning.id,
+        extractVersion: '^adoptopenjdk-(?<version>\\S+)',
+      };
+    }
+    return undefined;
+  },
+  julia: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'JuliaLang/julia',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  just: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'casey/just',
+    versioning: semverVersioning.id,
+  },
+  kotlin: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'JetBrains/kotlin',
+    versioning: semverVersioning.id,
+    extractVersion: '^(Kotlin |v)(?<version>\\S+)',
+  },
+  kustomize: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'kubernetes-sigs/kustomize',
+    versioning: semverVersioning.id,
+    extractVersion: '^kustomize/v(?<version>\\S+)',
+  },
+  lua: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'lua/lua',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  nim: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'nim-lang/Nim',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  nodejs: {
+    depName: 'node',
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'nodejs/node',
+    versioning: nodeVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  ocaml: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'ocaml/ocaml',
+    versioning: semverVersioning.id,
+  },
+  perl: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'Perl/perl5',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  php: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'php/php-src',
+    versioning: semverVersioning.id,
+    extractVersion: '^php-(?<version>\\S+)',
+  },
+  python: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'python/cpython',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  ruby: {
+    datasource: 'ruby-version',
+    packageName: 'ruby-version',
+    versioning: semverVersioning.id,
+  },
+  rust: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'rust-lang/rust',
+    versioning: semverVersioning.id,
+  },
+  scala: (version) => {
+    if (version.startsWith('2')) {
+      return {
+        datasource: GithubTagsDatasource.id,
+        packageName: 'scala/scala',
+        versioning: semverVersioning.id,
+        extractVersion: '^v(?<version>\\S+)',
+      };
+    }
+    if (version.startsWith('3')) {
+      return {
+        datasource: GithubTagsDatasource.id,
+        packageName: 'lampepfl/dotty',
+        versioning: semverVersioning.id,
+      };
+    }
+
+    return undefined;
+  },
+  shellcheck: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'koalaman/shellcheck',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  shfmt: {
+    datasource: GithubReleasesDatasource.id,
+    packageName: 'mvdan/sh',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  terraform: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'hashicorp/terraform',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  trivy: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'aquasecurity/trivy',
+    versioning: semverVersioning.id,
+    extractVersion: '^v(?<version>\\S+)',
+  },
+  zig: {
+    datasource: GithubTagsDatasource.id,
+    packageName: 'ziglang/zig',
+    versioning: semverVersioning.id,
+  },
+};
