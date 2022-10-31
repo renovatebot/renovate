d58bd8495b06f3c381f9800b2e55c8d63f8bdc0e
Usman Akeju <usman.akeju@blablacar.com>
2022-10-31 11:23:50 +0100

    Apply suggestion

--- lib/modules/manager/flux/common.ts
+++ lib/modules/manager/flux/common.ts
@@ -2,6 +2,7 @@ import { regEx } from '../../../util/regex';
 
 export const systemManifestRegex =
   '(^|\\/)flux-system\\/(?:.+\\/)?gotk-components\\.yaml$';
+
 export function isSystemManifest(file: string): boolean {
   return regEx(systemManifestRegex).test(file);
 }
