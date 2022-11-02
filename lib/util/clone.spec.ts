6d9718adadbf8633a72041cfc3b7b7f9aee7a46e
RahulGautamSingh <rahultesnik@gmail.com>
2022-11-02 08:48:58 +0530

    add new tests

--- lib/util/clone.spec.ts
+++ lib/util/clone.spec.ts
@@ -7,6 +7,11 @@ describe('util/clone', () => {
     isObject: true,
   };
 
+  it('returns null', () => {
+    const res = clone(null);
+    expect(res).toBeNull();
+  });
+
   it('maintains same order', () => {
     const res = clone(obj);
     expect(Object.keys(res)).toEqual(Object.keys(obj));
