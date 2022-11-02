e6724cc35b1389ab7e968bb741b3479b807ac9fb
xingxing3 <xingxing@jd.com>
2022-11-02 19:39:25 +0800

    format(i18n): address lint issue

--- lib/workers/repository/update/pr/body/controls.ts
+++ lib/workers/repository/update/pr/body/controls.ts
@@ -1,5 +1,9 @@
 import { _ } from '../../../../../i18n';
 
 export function getControls(): string {
-  return `\n\n---\n\n - [ ] <!-- rebase-check -->` + _('If you want to rebase/retry this PR, check this box') + '\n\n';
+  return (
+    `\n\n---\n\n - [ ] <!-- rebase-check -->` +
+    _('If you want to rebase/retry this PR, check this box') +
+    '\n\n'
+  );
 }
