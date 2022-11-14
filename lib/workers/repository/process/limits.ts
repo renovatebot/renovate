c7878e61306e4ee3918f088f8cb8e7881188b43c
RahulGautamSingh <rahultesnik@gmail.com>
2022-11-14 19:50:19 +0530

    re-add comment

--- lib/workers/repository/process/limits.ts
+++ lib/workers/repository/process/limits.ts
@@ -119,6 +119,7 @@ export function getConcurrentBranchesRemaining(
 
       return concurrentRemaining;
     } catch (err) {
+      // TODO: #7154 should never throw
       logger.error({ err }, 'Error checking concurrent branches');
       return limit;
     }
