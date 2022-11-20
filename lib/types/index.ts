4720b687367f88c1116d18d4df9d5de2c27a77d7
RahulGautamSingh <rahultesnik@gmail.com>
2022-11-20 06:51:36 +0530

    export types from lib/types

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,10 +1,10 @@
 export type { CommitMessageJSON } from './commit-message-json';
 export * from './host-rules';
-export * from './skip-reason';
-export * from './versioning';
-export * from './branch-status';
+export type { SkipReason } from './skip-reason';
+export type { RangeStrategy } from './versioning';
+export type { BranchStatus } from './branch-status';
 export * from './vulnerability-alert';
-export * from './pr-state';
+export type { PrState } from './pr-state';
 export * from './base';
 
 export type AutoMergeType = 'branch' | 'pr' | 'pr-comment';
