f239aa35bc1276ffaa352e305bc683a5b3984371
RahulGautamSingh <rahultesnik@gmail.com>
2022-11-20 07:17:23 +0530

    fix(lib/types): convert exports to type exports

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,10 +1,15 @@
 export type { CommitMessageJSON } from './commit-message-json';
-export * from './host-rules';
+export type { HostRule, HostRuleSearchResult } from './host-rules';
 export type { SkipReason } from './skip-reason';
 export type { RangeStrategy } from './versioning';
 export type { BranchStatus } from './branch-status';
-export * from './vulnerability-alert';
+export type {
+  VulnerabilityPackage,
+  SecurityVulnerability,
+  SecurityAdvisory,
+  VulnerabilityAlert,
+} from './vulnerability-alert';
 export type { PrState } from './pr-state';
-export * from './base';
+export type { ModuleApi, RenovatePackageJson } from './base';
 
 export type AutoMergeType = 'branch' | 'pr' | 'pr-comment';
