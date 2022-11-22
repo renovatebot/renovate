eb8d6337abe954175f5b753e0e56a825ee36e817
Devin Buhl <devin@buhl.casa>
2022-11-22 07:51:49 -0500

    fix: update order of imports
    Signed-off-by: Devin Buhl <devin@buhl.casa>

--- lib/modules/manager/flux/index.ts
+++ lib/modules/manager/flux/index.ts
@@ -8,8 +8,8 @@ import { GitlabTagsDatasource } from '../../datasource/gitlab-tags';
 import { HelmDatasource } from '../../datasource/helm';
 import { systemManifestRegex } from './common';
 
-export { updateArtifacts } from './artifacts';
 export { extractAllPackageFiles, extractPackageFile } from './extract';
+export { updateArtifacts } from './artifacts';
 
 export const defaultConfig = {
   fileMatch: [systemManifestRegex],
