63d2fce68de2946de221cca42247e74655a32398
Daniel Petró <daniel.petro@icloud.com>
2022-11-22 10:32:51 +0100

    chore(prettier)

--- lib/constants/platforms.ts
+++ lib/constants/platforms.ts
@@ -23,6 +23,9 @@ export const GITLAB_API_USING_HOST_TYPES = [
   'gitlab-changelog',
 ];
 
-export const AZURE_API_USING_HOST_TYPES = ['PlatformId.Azure', 'azure-changelog'];
+export const AZURE_API_USING_HOST_TYPES = [
+  'PlatformId.Azure',
+  'azure-changelog',
+];
 
 export const BITBUCKET_API_USING_HOST_TYPES = ['bitbucket', 'bitbucket-tags'];
