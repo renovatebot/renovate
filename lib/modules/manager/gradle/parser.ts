dc0c02716d279a0df9d50b07851e45c6811d5189
Johannes Feichtner <Churro@users.noreply.github.com>
2022-11-26 08:27:39 +0100

    feat(manager/gradle): add support for alternative Gradle plugin syntax (#19113)

--- lib/modules/manager/gradle/parser.ts
+++ lib/modules/manager/gradle/parser.ts
@@ -205,24 +205,58 @@ const qKotlinMapNotationDependencies = q
   .handler(handleLongFormDep)
   .handler(cleanupTempVars);
 
-// id "foo.bar" version "1.2.3"
 // kotlin("jvm") version "1.3.71"
 const qPlugins = q
   .sym(regEx(/^(?:id|kotlin)$/), storeVarToken)
   .handler((ctx) => storeInTokenMap(ctx, 'methodName'))
   .alt(
-    qStringValue,
-    q.tree({
-      type: 'wrapped-tree',
-      maxDepth: 1,
-      startsWith: '(',
-      endsWith: ')',
-      search: q.begin<Ctx>().join(qStringValue).end(),
-    })
+    // id "foo.bar" version "1.2.3"
+    qStringValue
+      .handler((ctx) => storeInTokenMap(ctx, 'pluginName'))
+      .sym('version')
+      .alt(
+        qTemplateString,
+        qPropertyAccessIdentifier,
+        qVariableAccessIdentifier
+      ),
+    q
+      .tree({
+        type: 'wrapped-tree',
+        maxDepth: 1,
+        startsWith: '(',
+        endsWith: ')',
+        search: q.begin<Ctx>().join(qStringValue).end(),
+      })
+      .handler((ctx) => storeInTokenMap(ctx, 'pluginName'))
+      .alt(
+        // id("foo.bar") version "1.2.3"
+        q
+          .sym<Ctx>('version')
+          .alt(
+            qTemplateString,
+            qPropertyAccessIdentifier,
+            qVariableAccessIdentifier
+          ),
+        // id("foo.bar").version("1.2.3")
+        q
+          .op<Ctx>('.')
+          .sym('version')
+          .tree({
+            maxDepth: 1,
+            startsWith: '(',
+            endsWith: ')',
+            search: q
+              .begin<Ctx>()
+              .alt(
+                qTemplateString,
+                qPropertyAccessIdentifier,
+                qVariableAccessIdentifier
+              )
+              .end(),
+          })
+      )
   )
-  .handler((ctx) => storeInTokenMap(ctx, 'pluginName'))
-  .sym('version')
-  .alt(qTemplateString, qPropertyAccessIdentifier, qVariableAccessIdentifier)
+
   .handler((ctx) => storeInTokenMap(ctx, 'version'))
   .handler(handlePlugin)
   .handler(cleanupTempVars);
