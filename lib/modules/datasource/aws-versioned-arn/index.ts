9edb2e053cc6fe136f6b55fd375621ef2d3266ac
kayma <kayma@hlag.com>
2022-11-29 21:36:13 +0100

    retrieve layer list from AWS

--- lib/modules/datasource/aws-versioned-arn/index.ts
+++ lib/modules/datasource/aws-versioned-arn/index.ts
@@ -1,4 +1,8 @@
-import { LambdaClient } from '@aws-sdk/client-lambda';
+import {
+  LambdaClient,
+  LayerVersionsListItem,
+  ListLayerVersionsCommand,
+} from '@aws-sdk/client-lambda';
 import { cache } from '../../../util/cache/package/decorator';
 import { Lazy } from '../../../util/lazy';
 import { Datasource } from '../datasource';
@@ -18,13 +22,45 @@ export class AwsVersionedArnDataSource extends Datasource {
 
   @cache({
     namespace: `datasource-${AwsVersionedArnDataSource.id}`,
+    key: (serializedArnFilter: string) =>
+      `getSortedLambdaLayerVersions:${serializedArnFilter}`,
+  })
+  async getSortedLambdaLayerVersions(
+    serializedLambdaLayerFilter: string
+  ): Promise<LayerVersionsListItem[]> {
+    const cmd = new ListLayerVersionsCommand({
+      LayerName: JSON.parse(serializedLambdaLayerFilter),
+    });
+
+    const matchingLayerVersions = await this.lambda.getValue().send(cmd);
+    matchingLayerVersions.LayerVersions =
+      matchingLayerVersions.LayerVersions ?? [];
+
+    return matchingLayerVersions.LayerVersions.sort((layer1, layer2) => {
+      return (layer1.Version ?? 0) - (layer2.Version ?? 0);
+    });
+  }
+
+  @cache({
+    namespace: `datasource-${AwsVersionedArnDataSource.id}`,
     key: ({ packageName }: GetReleasesConfig) => `getReleases:${packageName}`,
   })
-  async getReleases(
-    getReleasesConfig: GetReleasesConfig
-  ): Promise<ReleaseResult | null> {
+  async getReleases({
+    packageName: serializedArnFilter,
+  }: GetReleasesConfig): Promise<ReleaseResult | null> {
+    const lambdaLayerVersions = await this.getSortedLambdaLayerVersions(
+      serializedArnFilter
+    );
+    if (lambdaLayerVersions.length === 0) {
+      return null;
+    }
+
     return {
-      releases: [],
+      releases: lambdaLayerVersions.map((layer) => ({
+        version: layer.Version?.toString() ?? '0',
+        releaseTimestamp: layer.CreatedDate,
+        newDigest: layer.LayerVersionArn,
+      })),
     };
   }
 }
