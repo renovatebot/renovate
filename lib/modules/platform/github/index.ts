8e51bfd4bd1652ac1e5c8a524798ad0292c25b32
Rhys Arkins <rhys@arkins.net>
2022-12-15 11:04:09 +0100

    looseEquals

--- lib/modules/platform/github/index.ts
+++ lib/modules/platform/github/index.ts
@@ -34,7 +34,7 @@ import * as hostRules from '../../../util/host-rules';
 import * as githubHttp from '../../../util/http/github';
 import { regEx } from '../../../util/regex';
 import { sanitize } from '../../../util/sanitize';
-import { fromBase64 } from '../../../util/string';
+import { looseEquals, fromBase64 } from '../../../util/string';
 import { ensureTrailingSlash } from '../../../util/url';
 import type {
   AggregatedVulnerabilities,
@@ -719,10 +719,7 @@ export async function findPr({
       return false;
     }
 
-    if (
-      !config.forkToken &&
-      config.repository?.toLowerCase() !== p.sourceRepo?.toLowerCase()
-    ) {
+    if (!config.forkToken && !looseEquals(config.repository, p.sourceRepo)) {
       return false;
     }
 
