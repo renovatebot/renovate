b46e52c353541440d8b2f25d27a2e8314ba73566
Joshua Tang <joshuaystang@gmail.com>
2022-12-25 05:11:25 +1100

    feat(pub): Support updating lock files (#19116)

--- lib/util/exec/containerbase.ts
+++ lib/util/exec/containerbase.ts
@@ -136,6 +136,16 @@ const allToolConfig: Record<string, ToolConfig> = {
     depName: 'yarn',
     versioning: npmVersioningId,
   },
+  dart: {
+    datasource: 'dart-version',
+    depName: 'dart',
+    versioning: semverVersioningId,
+  },
+  flutter: {
+    datasource: 'flutter-version',
+    depName: 'flutter',
+    versioning: semverVersioningId,
+  },
 };
 
 export function supportsDynamicInstall(toolName: string): boolean {
