bbe26dc7fac712590df198b9063521d9bd360d04
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2022-12-24 22:51:32 +0200

    update package-names.ts use is.string

--- lib/util/package-rules/package-names.ts
+++ lib/util/package-rules/package-names.ts
@@ -10,9 +10,9 @@ export class PackageNameMatcher extends Matcher {
     if (is.undefined(matchPackageNames)) {
       return null;
     }
-    return !!(
-      (depName && matchPackageNames.includes(depName)) ||
-      (packageName && matchPackageNames.includes(packageName))
+    return (
+      (is.string(depName) && matchPackageNames.includes(depName)) ||
+      (is.string(packageName) && matchPackageNames.includes(packageName))
     );
   }
 
@@ -23,9 +23,9 @@ export class PackageNameMatcher extends Matcher {
     if (is.undefined(excludePackageNames)) {
       return null;
     }
-    return !!(
-      (depName && excludePackageNames.includes(depName)) ||
-      (packageName && excludePackageNames.includes(packageName))
+    return (
+      (is.string(depName) && excludePackageNames.includes(depName)) ||
+      (is.string(packageName) && excludePackageNames.includes(packageName))
     );
   }
 }
