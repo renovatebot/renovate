ebff08ac5ede4e378ef17585aba230a6c0097cd6
Johannes Feichtner <johannes@web-wack.at>
2022-12-27 13:50:52 +0100

    replace forEach, rename variables

--- lib/modules/manager/gradle/extract.ts
+++ lib/modules/manager/gradle/extract.ts
@@ -26,29 +26,14 @@ import {
 
 const datasource = MavenDatasource.id;
 
-// Enables reverse sorting in generateBranchConfig()
-//
-// Required for grouped dependencies to be upgraded
-// correctly in single branch.
-//
-// https://github.com/renovatebot/renovate/issues/8224
-function elevateFileReplacePositionField(
-  deps: PackageDependency<GradleManagerData>[]
-): PackageDependency<GradleManagerData>[] {
-  return deps.map((dep) => ({
-    ...dep,
-    fileReplacePosition: dep?.managerData?.fileReplacePosition,
-  }));
-}
-
 export async function extractAllPackageFiles(
   config: ExtractConfig,
   packageFiles: string[]
 ): Promise<PackageFile[] | null> {
   const extractedDeps: PackageDependency<GradleManagerData>[] = [];
-  const registry: VariableRegistry = {};
+  const varRegistry: VariableRegistry = {};
   const packageFilesByName: Record<string, PackageFile> = {};
-  const registryUrls: string[] = [];
+  const packageRegistries: string[] = [];
   const reorderedFiles = reorderFiles(packageFiles);
   const fileContents = await getFileContentMap(packageFiles, true);
 
@@ -62,11 +47,11 @@ export async function extractAllPackageFiles(
     try {
       // TODO #7154
       const content = fileContents[packageFile]!;
-      const dir = upath.dirname(toAbsolutePath(packageFile));
+      const packageFileDir = upath.dirname(toAbsolutePath(packageFile));
 
       const updateVars = (newVars: PackageVariables): void => {
-        const oldVars = registry[dir] || {};
-        registry[dir] = { ...oldVars, ...newVars };
+        const oldVars = varRegistry[packageFileDir] || {};
+        varRegistry[packageFileDir] = { ...oldVars, ...newVars };
       };
 
       if (isPropsFile(packageFile)) {
@@ -83,18 +68,21 @@ export async function extractAllPackageFiles(
         const updatesFromGcv = parseGcv(packageFile, fileContents);
         extractedDeps.push(...updatesFromGcv);
       } else if (isGradleScriptFile(packageFile)) {
-        const vars = getVars(registry, dir);
+        const vars = getVars(varRegistry, packageFileDir);
         const {
           deps,
           urls,
           vars: gradleVars,
         } = parseGradle(content, vars, packageFile, fileContents);
-        urls.forEach((url) => {
-          if (!registryUrls.includes(url)) {
-            registryUrls.push(url);
+        for (const url of urls) {
+          if (!packageRegistries.includes(url)) {
+            packageRegistries.push(url);
           }
-        });
-        registry[dir] = { ...registry[dir], ...gradleVars };
+        }
+        varRegistry[packageFileDir] = {
+          ...varRegistry[packageFileDir],
+          ...gradleVars,
+        };
         updateVars(gradleVars);
         extractedDeps.push(...deps);
       }
@@ -110,22 +98,24 @@ export async function extractAllPackageFiles(
     return null;
   }
 
-  elevateFileReplacePositionField(extractedDeps).forEach((dep) => {
+  for (const dep of extractedDeps) {
+    dep.fileReplacePosition = dep?.managerData?.fileReplacePosition; // #8224
+
     const key = dep.managerData?.packageFile;
     // istanbul ignore else
     if (key) {
-      let pkgFile = packageFilesByName[key];
+      let pkgFile: PackageFile = packageFilesByName[key];
       // istanbul ignore if: won't happen if "apply from" processes only initially known files
       if (!pkgFile) {
         pkgFile = {
           packageFile: key,
           datasource,
           deps: [],
-        } as PackageFile;
+        };
       }
 
       dep.registryUrls = [
-        ...new Set([...registryUrls, ...(dep.registryUrls ?? [])]),
+        ...new Set([...packageRegistries, ...(dep.registryUrls ?? [])]),
       ];
 
       if (!dep.depType) {
@@ -148,8 +138,7 @@ export async function extractAllPackageFiles(
     } else {
       logger.warn({ dep }, `Failed to process Gradle dependency`);
     }
-  });
+  }
 
-  const result = Object.values(packageFilesByName);
-  return result;
+  return Object.values(packageFilesByName);
 }
