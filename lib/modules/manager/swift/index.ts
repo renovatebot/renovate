971e55e1e570ffc3e7d27f39fe35f2d69cd9b065
Janosch Hildebrand <jnosh@jnosh.com>
2023-01-10 22:54:12 +0100

    Apply prettier fixes

--- lib/modules/manager/swift/index.ts
+++ lib/modules/manager/swift/index.ts
@@ -3,7 +3,6 @@ import * as swiftVersioning from '../../versioning/swift';
 export { updateArtifacts } from './artifacts';
 export { extractPackageFile } from './extract';
 
-
 export const displayName = 'Swift Package Manager';
 export const url = 'https://www.swift.org/package-manager/';
 
