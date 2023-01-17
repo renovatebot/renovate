c6e2fc198135250de870307e865582bec6f1833e
Rhys Arkins <rhys@arkins.net>
2023-01-17 16:28:29 +0100

    comment

--- lib/util/git/index.ts
+++ lib/util/git/index.ts
@@ -426,6 +426,7 @@ export async function syncGit(): Promise<void> {
       const emptyDirAndClone = async (): Promise<void> => {
         await fs.emptyDir(localDir);
         await git.clone(config.url, '.', opts);
+        // unset hooks to avoid post-checkout hooks, etc.
         await git.raw(['config', '--unset', 'core.hooksPath']);
       };
       await gitRetry(() => emptyDirAndClone());
