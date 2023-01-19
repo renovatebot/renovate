690dc0d497e98fb2be167ed85d7c37f023454c70
Morre <morre@mor.re>
2023-01-19 14:38:13 +0100

    Update lib/modules/manager/terraform/extractors/resources/helm-release.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/modules/manager/terraform/extractors/resources/helm-release.ts
+++ lib/modules/manager/terraform/extractors/resources/helm-release.ts
@@ -34,7 +34,7 @@ export class HelmReleaseExtractor extends DependencyExtractor {
           dep.skipReason = 'invalid-name';
         } else if (isOCIRegistry(helmRelease.chart)) {
           // For oci charts, we remove the oci:// and use the docker datasource
-          dep.depName = helmRelease.chart.replace(ociRegex, '');
+          dep.depName = helmRelease.chart.replace('oci://', '');
           dep.datasource = DockerDatasource.id;
         } else if (checkIfStringIsPath(helmRelease.chart)) {
           dep.skipReason = 'local-chart';
