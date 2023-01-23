27648d7bd7798b4354cd930ab31dca966af9525e
RahulGautamSingh <rahultesnik@gmail.com>
2023-01-23 23:08:23 +0530

    update old tests

--- lib/modules/datasource/pypi/index.spec.ts
+++ lib/modules/datasource/pypi/index.spec.ts
@@ -271,6 +271,7 @@ describe('modules/datasource/pypi/index', () => {
           datasource,
           constraints: { python: '2.7' },
           depName: 'doit',
+          releaseConstraintFiltering: 'strict',
         })
       ).toMatchSnapshot();
     });
@@ -518,6 +519,7 @@ describe('modules/datasource/pypi/index', () => {
           constraints: { python: '2.7' },
           ...config,
           depName: 'dj-database-url',
+          releaseConstraintFiltering: 'strict',
         })
       ).toMatchSnapshot();
     });
