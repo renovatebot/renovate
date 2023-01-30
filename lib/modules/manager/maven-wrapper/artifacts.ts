22a6bab27f6b8e240e6daaefe7ef9a25224acd20
tofferman <tofferman@bol.com>
2023-01-30 09:00:12 +0100

    fix(manager/maven-wrapper): lint ifx

--- lib/modules/manager/maven-wrapper/artifacts.ts
+++ lib/modules/manager/maven-wrapper/artifacts.ts
@@ -165,13 +165,12 @@ async function executeWrapperCommand(
 }
 
 function getCustomMavenWrapperUrl(deps: PackageDependency<Record<string, unknown>>[]) : string | null {
-   const replaceString = deps.filter(dep => dep.depName === 'maven-wrapper').map(dep => dep.replaceString)[0];
-   const match = replaceString?.match(/^(.*?)org\/apache\/maven\/wrapper\//);
-
-   if (match) {
-    return match[1] !== DEFAULT_MAVEN_REPO_URL ? match[1] : null;
-   }
-   return null;
+  const replaceString = deps.filter(dep => dep.depName === 'maven-wrapper').map(dep => dep.replaceString)[0];
+  const match = replaceString?.match(/^(.*?)org\/apache\/maven\/wrapper\//);
+  if (match) {
+   return match[1] === DEFAULT_MAVEN_REPO_URL ? null : match[1];
+  }
+  return null;
 }
 
 async function createWrapperCommand(
