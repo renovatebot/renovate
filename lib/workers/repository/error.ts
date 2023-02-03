fe8fd3e44e08299930301066ee377ea26c9f9d75
Jesse Houwing <jesse.houwing@gmail.com>
2023-02-03 21:22:10 +0100

    Cleaned up whitespace using prettier

--- lib/workers/repository/error.ts
+++ lib/workers/repository/error.ts
@@ -90,7 +90,9 @@ export default async function handleError(
     return err.message;
   }
   if (err.message === REPOSITORY_FORKED) {
-    logger.info('Repository is a fork and not manually configured - skipping - did you want to run with flag --include-forks?');
+    logger.info(
+      'Repository is a fork and not manually configured - skipping - did you want to run with flag --include-forks?'
+    );
     return err.message;
   }
   if (err.message === REPOSITORY_CANNOT_FORK) {
