24df80d5a37c5d1ee23c591f39a2ff53200b8eff
RahulGautamSingh <rahultesnik@gmail.com>
2023-02-06 12:00:51 +0530

    Update dep-types-migration.ts

--- lib/config/migrations/custom/dep-types-migration.ts
+++ lib/config/migrations/custom/dep-types-migration.ts
@@ -15,7 +15,7 @@ export class DepTypesMigration extends AbstractMigration {
       packageRules.push({
         matchDepTypes: [key],
         ...value,
-      } satisfies PackageRule);
+      } as PackageRule);
     }
 
     if (is.array(value)) {
@@ -25,7 +25,7 @@ export class DepTypesMigration extends AbstractMigration {
           if (depTypeName) {
             delete depType.depType;
             depType.matchDepTypes = [depTypeName];
-            packageRules.push({ ...(depType satisfies PackageRule) });
+            packageRules.push({ ...(depType as PackageRule) });
           }
         }
       }
