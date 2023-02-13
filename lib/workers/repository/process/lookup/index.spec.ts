8c0fa309c618705cca115089b22d195f5a653b62
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-02-13 15:21:32 +0000

    remove trailing dot like with other warnings

--- lib/workers/repository/process/lookup/index.spec.ts
+++ lib/workers/repository/process/lookup/index.spec.ts
@@ -1205,7 +1205,7 @@ describe('workers/repository/process/lookup/index', () => {
       expect(res.warnings).toHaveLength(1);
       expect(res.warnings[0]).toEqual({
         message:
-          'Could not determine new digest for update (datasource: github-tags).',
+          'Could not determine new digest for update (datasource: github-tags)',
         topic: 'angular/angular',
       });
     });
