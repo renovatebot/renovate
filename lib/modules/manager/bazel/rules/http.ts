0b855859a5693feb83cbbdbba057bd7b7ee62ca1
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-02-14 10:37:51 +0300

    Fixes

--- lib/modules/manager/bazel/rules/http.ts
+++ lib/modules/manager/bazel/rules/http.ts
@@ -5,11 +5,15 @@ import { parseUrl } from '../../../../util/url';
 import { GithubReleasesDatasource } from '../../../datasource/github-releases';
 import { GithubTagsDatasource } from '../../../datasource/github-tags';
 import type { PackageDependency } from '../../types';
-import type { UrlParsedResult } from '../types';
 
 const archives = [
   '.zip',
   '.tar',
+  '.jar',
+  '.war',
+  '.aar',
+  '.ar',
+  '.deb',
 
   '.gz',
   '.tar.gz',
@@ -22,6 +26,10 @@ const archives = [
   '.xz',
   '.tar.xz',
   '.txz',
+
+  '.zst',
+  '.tar.zst',
+  '.tzst',
 ];
 
 const archiveSuffixRegex = regEx(
@@ -32,47 +40,52 @@ function stripArchiveSuffix(value: string): string {
   return value.replace(archiveSuffixRegex, '');
 }
 
-export function parseGithubUrl(url: URL): UrlParsedResult | null {
-  const [p0, p1, p2, p3, p4, p5] = url.pathname.split('/').slice(1);
-  const repo = p0 + '/' + p1;
+function isHash(value: unknown): value is string {
+  return is.string(value) && regEx(/[0-9a-z]{40}/i).test(value);
+}
+
+export function parseGithubPath(
+  pathname: string
+): Partial<PackageDependency> | null {
+  const [p0, p1, p2, p3, p4, p5] = pathname.split('/').slice(1);
+  const packageName = p0 + '/' + p1;
   let datasource = '';
-  let currentValue: string | null = null;
+  let value: string | null = null;
   if (p2 === 'releases' && p3 === 'download') {
     // https://github.com/foo/bar/releases/download/1.2.3/bar-1.2.3.tar.gz
     datasource = GithubReleasesDatasource.id;
-    currentValue = p4;
+    value = p4;
   } else if (p2 === 'archive' && p3 === 'refs' && p4 === 'tags') {
     // https://github.com/foo/bar/archive/refs/tags/v1.2.3.tar.gz
     datasource = GithubTagsDatasource.id;
-    currentValue = p5;
+    value = p5;
   } else if (p2 === 'archive') {
     // https://github.com/foo/bar/archive/1.2.3.tar.gz
     datasource = GithubTagsDatasource.id;
-    currentValue = p3;
+    value = p3;
   }
 
-  if (!currentValue) {
+  if (!value) {
     return null;
   }
 
-  currentValue = stripArchiveSuffix(currentValue);
-  return { datasource, repo, currentValue };
+  value = stripArchiveSuffix(value);
+  return isHash(value)
+    ? { datasource, packageName, currentDigest: value }
+    : { datasource, packageName, currentValue: value };
 }
 
 export function parseArchiveUrl(
   urlString: string | undefined | null
-): UrlParsedResult | null {
+): Partial<PackageDependency> | null {
   if (!urlString) {
     return null;
   }
 
   const url = parseUrl(urlString);
-  if (!url?.pathname) {
-    return null;
-  }
 
-  if (url.host === 'github.com') {
-    return parseGithubUrl(url);
+  if (url?.host === 'github.com') {
+    return parseGithubPath(url.pathname);
   }
 
   return null;
@@ -99,13 +112,20 @@ export const HttpTarget = z
       datasource: parsedUrl.datasource,
       depType: rule,
       depName: name,
-      packageName: parsedUrl.repo,
+      packageName: parsedUrl.packageName,
     };
 
-    if (regEx(/^[a-f0-9]{40}$/i).test(parsedUrl.currentValue)) {
-      dep.currentDigest = parsedUrl.currentValue;
-    } else {
+    // We don't want to set both `currentValue` and `currentDigest`,
+    // relying on artifact updates instead.
+    //
+    // However, we want to auto-replace to be run successfully
+    // against digest-only dependencies.
+    //
+    // Hence, `if-else-if` is being used here.
+    if (parsedUrl.currentValue) {
       dep.currentValue = parsedUrl.currentValue;
+    } else if (parsedUrl.currentDigest) {
+      dep.currentDigest = parsedUrl.currentDigest;
     }
 
     return [dep];
