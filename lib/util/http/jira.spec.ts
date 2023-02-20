83ac1711c772838ce8f01c786aa98b5324a2f471
Adam Setch <adam.setch@outlook.com>
2023-02-20 16:31:31 -0500

    client: add jira http client

---
+++ lib/util/http/jira.spec.ts
@@ -0,0 +1,58 @@
+import * as httpMock from '../../../test/http-mock';
+import * as hostRules from '../host-rules';
+import { JiraHttp, setBaseUrl } from './jira';
+
+const baseUrl = 'https://api.atlassian.com';
+
+describe('util/http/jira', () => {
+  let api: JiraHttp;
+
+  beforeEach(() => {
+    api = new JiraHttp();
+
+    // reset module
+    jest.resetAllMocks();
+
+    // clean up hostRules
+    hostRules.clear();
+    hostRules.add({
+      hostType: 'jira',
+      matchHost: baseUrl,
+      token: 'token',
+    });
+
+    setBaseUrl(baseUrl);
+  });
+
+  it('posts', async () => {
+    const body = ['a', 'b'];
+    httpMock.scope(baseUrl).post('/some-url').reply(200, body);
+    const res = await api.postJson('some-url');
+    expect(res.body).toEqual(body);
+  });
+
+  it('accepts custom baseUrl', async () => {
+    const customBaseUrl = 'https://api-test.atlassian.com';
+    httpMock.scope(baseUrl).post('/some-url').reply(200, {});
+    httpMock.scope(customBaseUrl).post('/some-url').reply(200, {});
+
+    expect(await api.postJson('some-url')).toEqual({
+      authorization: true,
+      body: {},
+      headers: {
+        'content-type': 'application/json',
+      },
+      statusCode: 200,
+    });
+
+    setBaseUrl(customBaseUrl);
+    expect(await api.postJson('some-url')).toEqual({
+      authorization: false,
+      body: {},
+      headers: {
+        'content-type': 'application/json',
+      },
+      statusCode: 200,
+    });
+  });
+});
