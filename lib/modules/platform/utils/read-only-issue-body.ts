6427f6d135c4198133348a4a6c6ae99676b6a82d
Adam Setch <adam.setch@outlook.com>
2023-02-21 08:22:09 -0500

    regex: remove whitespace

--- lib/modules/platform/utils/read-only-issue-body.ts
+++ lib/modules/platform/utils/read-only-issue-body.ts
@@ -6,7 +6,7 @@ export function readOnlyIssueBody(body: string): string {
     .replace(' unless you click a checkbox below', '')
     .replace(' To discard all commits and start over, click on a checkbox.', '')
     .replace(regEx(/ Click (?:on |)a checkbox.*\./g), '')
-    .replace(regEx(/ \[ ] <!-- \w*-branch.*-->/g), '')
+    .replace(regEx(/\[ ] <!-- \w*-branch.*-->/g), '')
     .replace('**Click on this checkbox to rebase all open PRs at once**', '')
     .replace(regEx(/\[ ] <!-- rebase-all-open-prs -->/g), '');
 }
