c09382d082cbd68ab6ea7fa54822f0758fa54c48
Simon Eßlinger <simon.esslinger@tapio.one>
2023-03-03 00:53:28 +0100

    wip

--- lib/modules/manager/bicep/index.ts
+++ lib/modules/manager/bicep/index.ts
@@ -1,9 +1,9 @@
 import { AzureBicepTypesDatasource } from '../../datasource/azure-bicep-types';
 
+export { extractPackageFile } from './extract';
+
 export const defaultConfig = {
   fileMatch: ['\\.bicep$'],
 };
-
-export { extractPackageFile } from './extract';
 
 export const supportedDatasources = [AzureBicepTypesDatasource.id];
