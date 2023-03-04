c090e9763694f75a18be397ab46f20bd6ca68728
github@esslinger.dev <simon@esslinger.dev>
2023-03-04 19:25:39 +0100

    wip

--- lib/modules/versioning/azure-rest-api/index.ts
+++ lib/modules/versioning/azure-rest-api/index.ts
@@ -1,3 +1,4 @@
+import { regEx } from '../../../util/regex';
 import { GenericVersion, GenericVersioningApi } from '../generic';
 import type { VersioningApi } from '../types';
 
@@ -8,20 +9,35 @@ export const urls = [];
 
 export const supportsRanges = false;
 
+const AZURE_REST_API_VERSION_REGEX = regEx(
+  /^(?<version>(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2}))(?<prerelease>-[a-z]+)?$/
+);
+
 class AzureRestApiVersioningApi extends GenericVersioningApi {
   protected _parse(version: string): GenericVersion | null {
-    if (version) {
-      return {
-        release: [1, 0, 0],
-        suffix: version.substring(0, 10),
-        prerelease: version.length > 10 ? version.substring(9) : undefined,
-      };
+    if (!version) {
+      return null;
     }
-    return null;
+
+    const matchGroups = AZURE_REST_API_VERSION_REGEX.exec(version)?.groups;
+
+    if (!matchGroups) {
+      return null;
+    }
+
+    return {
+      release: [1, 0, 0], // static because there is no semantic versioning
+      suffix: matchGroups.version,
+      prerelease: matchGroups.prerelease,
+    };
   }
 
   protected override _compare(_version: string, _other: string): number {
-    return 1;
+    if (_version === _other) {
+      return 0;
+    }
+
+    return _version > _other ? 1 : -1;
   }
 }
 
