1fe1e731a7482e8dc59738d398740d0b5e63ab8b
Adam Setch <adam.setch@outlook.com>
2023-03-04 16:54:05 -0500

    refactor: remove nested if statements and make some early returns

--- lib/workers/repository/update/branch/auto-replace.ts
+++ lib/workers/repository/update/branch/auto-replace.ts
@@ -81,25 +81,23 @@ export async function confirmIfDepUpdated(
     return false;
   }
 
-  if (
-    upgrade.updateType !== 'replacement' &&
-    (upgrade.pinDigests || upgrade.currentDigest)
-  ) {
-    if (upgrade.newDigest && upgrade.newDigest !== newUpgrade.currentDigest) {
-      logger.debug(
-        {
-          manager,
-          packageFile,
-          expectedValue: upgrade.newDigest,
-          foundValue: newUpgrade.currentDigest,
-        },
-        'Digest is not updated'
-      );
-      return false;
-    }
+  // if (!upgrade.currentDigest && !upgrade.isPinDigest) {
+  // return true;
+  // }
+
+  if (upgrade.newDigest && upgrade.newDigest !== newUpgrade.currentDigest) {
+    logger.debug(
+      {
+        manager,
+        packageFile,
+        expectedValue: upgrade.newDigest,
+        foundValue: newUpgrade.currentDigest,
+      },
+      'Digest is not updated'
+    );
+    return false;
   }
 
-  // istanbul ignore next
   return true;
 }
 
