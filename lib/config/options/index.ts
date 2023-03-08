9d2704b60ce4dc0f5a14036e8ea5e65f0be14bd7
Adam Setch <adam.setch@outlook.com>
2023-03-08 16:36:56 -0500

    remove global option

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -1629,7 +1629,6 @@ const options: RenovateOptions[] = [
     type: 'boolean',
     default: false,
     supportedPlatforms: ['bitbucket'],
-    globalOnly: true,
   },
   // Automatic merging
   {
