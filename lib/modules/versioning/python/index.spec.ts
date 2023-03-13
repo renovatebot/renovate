b2ec1de971be2a60ee627f19224e611fb1da56bc
Lctrs <jerome@prmntr.me>
2023-03-13 11:34:09 +0100

    fix coverage

--- lib/modules/versioning/python/index.spec.ts
+++ lib/modules/versioning/python/index.spec.ts
@@ -117,6 +117,7 @@ test.each`
   ${'^1.0.0'}           | ${'^0.9.0'}           | ${false}
   ${'^1.1.0 || ^2.0.0'} | ${'^1.0.0 || ^2.0.0'} | ${true}
   ${'^1.0.0 || ^2.0.0'} | ${'^1.1.0 || ^2.0.0'} | ${false}
+  ${'1.2.3foo'}         | ${'~1.1.0'}           | ${undefined}
 `('subset("$a", "$b") === $expected', ({ a, b, expected }) => {
   expect(versioning.subset!(a, b)).toBe(expected);
 });
