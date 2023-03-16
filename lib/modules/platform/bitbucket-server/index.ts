d739141f7d93daf97b3bf80a5b32907b84a82964
​Faizaan <aulisius@users.noreply.github.com>
2023-03-16 11:20:08 +0530

    fix: Add support for PAT in Bitbucket Server. Closes #14900 (#20974)

--- lib/modules/platform/bitbucket-server/index.ts
+++ lib/modules/platform/bitbucket-server/index.ts
@@ -81,13 +81,14 @@ export function initPlatform({
   endpoint,
   username,
   password,
+  token,
 }: PlatformParams): Promise<PlatformResult> {
   if (!endpoint) {
     throw new Error('Init: You must configure a Bitbucket Server endpoint');
   }
-  if (!(username && password)) {
+  if (!(username && password) && !token) {
     throw new Error(
-      'Init: You must configure a Bitbucket Server username/password'
+      'Init: You must configure either a Bitbucket Server token or username and password'
     );
   }
   // TODO: Add a connection check that endpoint/username/password combination are valid (#9595)
