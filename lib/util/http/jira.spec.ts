34f83042366be86a0de6c857274824a60eca0b94
Adam Setch <adam.setch@outlook.com>
2023-03-26 13:34:06 -0400

    Update lib/util/http/jira.spec.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/util/http/jira.spec.ts
+++ lib/util/http/jira.spec.ts
@@ -7,7 +7,7 @@ describe('util/http/jira', () => {
 
   it('throws error if setBaseUrl not called', async () => {
     await expect(api.postJson('some-path')).rejects.toThrow(
-      TypeError('Invalid URL')
+      new TypeError('Invalid URL')
     );
   });
 
