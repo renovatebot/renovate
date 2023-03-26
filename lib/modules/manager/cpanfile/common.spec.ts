0c5627c6d4029fd6fccd81523ed6f8330a5e0ab0
IKEDA Sho <suicaicoca@gmail.com>
2023-03-26 15:15:49 +0900

    Use template literal

--- lib/modules/manager/cpanfile/common.spec.ts
+++ lib/modules/manager/cpanfile/common.spec.ts
@@ -5,8 +5,8 @@ describe('modules/manager/cpanfile/common', () => {
     test.each`
       version         | expected
       ${5.012005}     | ${'5.012005'}
-      ${"'5.008001'"} | ${'5.008001'}
-      ${'"5.008001"'} | ${'5.008001'}
+      ${`'5.008001'`} | ${'5.008001'}
+      ${`"5.008001"`} | ${'5.008001'}
     `('$version', ({ version, expected }) => {
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       const result = extractPerlVersion(`requires 'perl', ${version};`);
