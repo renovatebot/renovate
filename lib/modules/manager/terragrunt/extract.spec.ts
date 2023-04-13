4d41c2a55492f3693d843e327ae15ed0f5a57c71
Emil Davtyan <emil2k@gmail.com>
2023-04-13 16:38:53 +0200

    feat: support depth URL argument in Terragrunt modules

--- lib/modules/manager/terragrunt/extract.spec.ts
+++ lib/modules/manager/terragrunt/extract.spec.ts
@@ -14,6 +14,20 @@ describe('modules/manager/terragrunt/extract', () => {
       expect(res?.deps.filter((dep) => dep.skipReason)).toHaveLength(5);
     });
 
+    it('extracts terragrunt sources with depth specified after the branch', () => {
+      const res = extractPackageFile(Fixtures?.get('3.hcl'));
+      expect(res).toMatchSnapshot();
+      expect(res?.deps).toHaveLength(30);
+      expect(res?.deps.filter((dep) => dep.skipReason)).toHaveLength(5);
+    });
+
+    it('extracts terragrunt sources with depth specified before the branch', () => {
+      const res = extractPackageFile(Fixtures?.get('4.hcl'));
+      expect(res).toMatchSnapshot();
+      expect(res?.deps).toHaveLength(30);
+      expect(res?.deps.filter((dep) => dep.skipReason)).toHaveLength(5);
+    });
+
     it('returns null if only local terragrunt deps', () => {
       expect(
         extractPackageFile(`terragrunt {
