62b57aa27c890c4ebfe25822bc2df6b76ac0fcf4
DjordyKoert <djordy.koert@live.nl>
2023-04-17 08:16:02 +0200

    feat: disable setting COMPOSER_AUTH for gitlab (#20634)
    Co-authored-by: Rhys Arkins <rhys@arkins.net>
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2303,6 +2303,20 @@ const options: RenovateOptions[] = [
     experimental: true,
   },
   {
+    name: 'artifactAuth',
+    description:
+      'A list of package managers to enable artifact auth. Only managers on the list are enabled. All are enabled if `null`',
+    experimental: true,
+    type: 'array',
+    subType: 'string',
+    stage: 'repository',
+    parent: 'hostRules',
+    allowedValues: ['composer'],
+    default: null,
+    cli: false,
+    env: false,
+  },
+  {
     name: 'cacheHardTtlMinutes',
     description:
       'Maximum duration in minutes to keep datasource cache entries.',
