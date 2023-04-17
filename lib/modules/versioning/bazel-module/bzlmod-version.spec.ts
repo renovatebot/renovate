d867195d68add680822bba7e8dfa57fe13a4547b
Chuck Grindel <chuck.grindel@gmail.com>
2023-04-17 13:44:52 -0600

    Initial BzlmodVersion files.

--- lib/modules/versioning/bazel-module/bzlmod-version.spec.ts
+++ lib/modules/versioning/bazel-module/bzlmod-version.spec.ts
@@ -1,8 +1,57 @@
-import { BzlmodVersion } from './bzlmod-version';
+import { BzlmodVersion, Identifier, VersionPart } from './bzlmod-version';
 
 describe('modules/versioning/bazel-module/bzlmod-version', () => {
-  it('constructor', () => {
-    const bzlmodVer = new BzlmodVersion('1.2.3');
-    expect(bzlmodVer.release).toHaveLength(3);
+  describe('Identifier', () => {
+    it('when all digits', () => {
+      const ident = new Identifier('123');
+      expect(ident.asString).toBe('123');
+      expect(ident.asNumber).toBe(123);
+      expect(ident.isDigitsOnly).toBe(true);
+    });
+
+    it('when not all digits', () => {
+      const ident = new Identifier('foo123');
+      expect(ident.asString).toBe('foo123');
+      expect(ident.asNumber).toBe(0);
+      expect(ident.isDigitsOnly).toBe(false);
+    });
+  });
+
+  describe('VersionPart', () => {
+    it.each([
+      { vp: VersionPart.create(), exp: '' },
+      { vp: VersionPart.create('1', '2', '3'), exp: '1.2.3' },
+    ])('.asString', ({ vp, exp }) => {
+      expect(vp.asString).toBe(exp);
+    });
+  });
+
+  describe('BzlmodVersion', () => {
+    it.each([
+      { v: '1.2.3', rexp: '1.2.3', pexp: '', bexp: '' },
+      {
+        v: '1.2.3-pre.20230417.1',
+        rexp: '1.2.3',
+        pexp: 'pre.20230417.1',
+        bexp: '',
+      },
+      {
+        v: '1.2.3+build5',
+        rexp: '1.2.3',
+        pexp: '',
+        bexp: 'build5',
+      },
+      {
+        v: '1.2.3-pre.20230417.1+build5',
+        rexp: '1.2.3',
+        pexp: 'pre.20230417.1',
+        bexp: 'build5',
+      },
+    ])('constructor($v)', ({ v, rexp, pexp, bexp }) => {
+      const bv = new BzlmodVersion(v);
+      expect(bv.release.asString).toBe(rexp);
+      expect(bv.prerelease.asString).toBe(pexp);
+      expect(bv.build.asString).toBe(bexp);
+    });
   });
 });
--- lib/modules/versioning/bazel-module/bzlmod-version.ts
+++ lib/modules/versioning/bazel-module/bzlmod-version.ts
@@ -1,15 +1,83 @@
+/**
+ * A single value in a VersionPart.
+ */
 export class Identifier {
-  value: string;
+  asString: string;
+  asNumber: number;
+  isDigitsOnly: boolean;
+
+  static digitsOnlyMatcher = /^[0-9]+$/;
+
+  constructor(value: string) {
+    this.asString = value;
+    if (Identifier.digitsOnlyMatcher.test(value)) {
+      this.isDigitsOnly = true;
+      this.asNumber = parseInt(value);
+    } else {
+      this.isDigitsOnly = false;
+      this.asNumber = 0;
+    }
+  }
 }
 
-export class BzlmodVersion {
-  release: Identifier[];
-  prerelease: Identifier[];
-  build: Identifier[];
+/**
+ * A collection of Identifier values that represent a portion of a Bazel module version.
+ */
+export class VersionPart extends Array<Identifier> {
+  static create(...items: Array<Identifier | string>): VersionPart {
+    const idents = items.map((item) => {
+      if (typeof item === 'string') {
+        return new Identifier(item);
+      }
+      return item;
+    });
+    const vp = new VersionPart();
+    vp.push(...idents);
+    return vp;
+  }
 
-  constructor(version) {
-    this.release = [];
-    this.prerelease = [];
-    this.build = [];
+  get asString(): string {
+    return this.map((ident) => ident.asString).join('.');
+  }
+}
+
+interface VersionRegexResult {
+  release: string;
+  prerelease: string | undefined;
+  build: string | undefined;
+}
+
+/**
+ * Represents a Bazel module version.
+ */
+export class BzlmodVersion {
+  release: VersionPart;
+  prerelease: VersionPart;
+  build: VersionPart;
+
+  // Supported version pattern
+  static versionMatcher =
+    /(?<release>[a-zA-Z0-9.]+)(?:-(?<prerelease>[a-zA-Z0-9.-]+))?(?:\+(?<build>[a-zA-Z0-9.-]+))?/;
+
+  constructor(version: string) {
+    this.release = new VersionPart();
+    this.prerelease = new VersionPart();
+    this.build = new VersionPart();
+
+    const vparts: Partial<VersionRegexResult> | undefined =
+      BzlmodVersion.versionMatcher.exec(version)?.groups;
+    if (!vparts) {
+      throw new Error(`Invalid Bazel module version: ${version}`);
+    }
+    if (!vparts.release) {
+      throw new Error(`Missing release: ${version}`);
+    }
+    const rparts = vparts.release.split('.');
+    this.release = VersionPart.create(...rparts);
+    const pparts = vparts.prerelease ? vparts.prerelease.split('.') : [];
+    this.prerelease = VersionPart.create(...pparts);
+    // Do not parse the build value. Treat it as a single value.
+    const bparts = vparts.build ? [vparts.build] : [];
+    this.build = VersionPart.create(...bparts);
   }
 }
