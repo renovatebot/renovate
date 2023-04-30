70633c9dcbad197543509fcd17250ea7c13e467f
Chuck Grindel <chuck.grindel@gmail.com>
2023-04-30 08:32:47 -0600

    Disable the manager

--- lib/modules/manager/bazel-module/index.ts
+++ lib/modules/manager/bazel-module/index.ts
@@ -5,6 +5,10 @@ export { extractPackageFile };
 
 export const defaultConfig = {
   fileMatch: ['(^|/)MODULE\\.bazel$'],
+  // The bazel-module manager is still under development. The milestone
+  // tracking the release of this manager is at
+  // https://github.com/cgrindel/renovate_bzlmod_support/milestone/2.
+  enabled: false,
 };
 
 export const supportedDatasources = [BazelDatasource.id];
