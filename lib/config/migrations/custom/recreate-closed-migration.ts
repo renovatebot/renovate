e07a18b5307427dbddfdc6f27fb65ccbb1c2956d
RahulGautamSingh <rahultesnik@gmail.com>
2023-05-02 19:23:59 +0530

    update test

--- lib/config/migrations/custom/recreate-closed-migration.ts
+++ lib/config/migrations/custom/recreate-closed-migration.ts
@@ -2,6 +2,7 @@ import is from '@sindresorhus/is';
 import { AbstractMigration } from '../base/abstract-migration';
 
 export class RecreateClosedMigration extends AbstractMigration {
+  override readonly deprecated = true;
   override readonly propertyName = 'recreateClosed';
 
   override run(value: unknown): void {
