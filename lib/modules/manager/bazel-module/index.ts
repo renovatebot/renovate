cf883b4e1ac3b6549b149f203369bfe7382295d5
Chuck Grindel <chuck.grindel@gmail.com>
2023-05-02 07:54:52 -0600

    Update WIP comment with link to renovate tracking issue.

--- lib/modules/manager/bazel-module/index.ts
+++ lib/modules/manager/bazel-module/index.ts
@@ -7,7 +7,7 @@ export const defaultConfig = {
   fileMatch: ['(^|/)MODULE\\.bazel$'],
   // The bazel-module manager is still under development. The milestone
   // tracking the release of this manager is at
-  // https://github.com/cgrindel/renovate_bzlmod_support/milestone/2.
+  // https://github.com/renovatebot/renovate/issues/13658.
   enabled: false,
 };
 
