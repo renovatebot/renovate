85bd24ae582083fd925ce067823eab59d290d442
lluiscab <lluiscab@gmail.com>
2023-05-03 15:10:43 +0200

    feat(codeowners): improve codeowners assignment with score based matching

--- lib/workers/repository/update/pr/code-owners.spec.ts
+++ lib/workers/repository/update/pr/code-owners.spec.ts
@@ -22,13 +22,142 @@ describe('workers/repository/update/pr/code-owners', () => {
       expect(codeOwners).toEqual(['@jimmy']);
     });
 
+    it('respects orphan files', async () => {
+      fs.readLocalFile.mockResolvedValueOnce(
+        ['* @jimmy', 'yarn.lock'].join('\n')
+      );
+      git.getBranchFiles.mockResolvedValueOnce(['yarn.lock']);
+      const codeOwners = await codeOwnersForPr(pr);
+      expect(codeOwners).toEqual([]);
+    });
+
     it('returns more specific code owners', async () => {
       fs.readLocalFile.mockResolvedValueOnce(
         ['* @jimmy', 'package.json @john @maria'].join('\n')
       );
       git.getBranchFiles.mockResolvedValueOnce(['package.json']);
       const codeOwners = await codeOwnersForPr(pr);
-      expect(codeOwners).toEqual(['@john', '@maria']);
+      expect(codeOwners).toEqual(['@john', '@maria', '@jimmy']);
+    });
+
+    describe('returns more specific code owners in monorepos', () => {
+      const mockCodeOwners = `
+        # By default, assign to @john
+        #
+        * @john
+
+        # Lockfiles are not owned by anyone, any package dependency update may modify them.
+        # Assigning lockfiles an owner will cause issues as merge requests to be assigned to incorrect users
+        yarn.lock
+
+        # Assign each package to it's respective user
+        #
+        packages/a/ @maria
+        packages/b/ @jimmy
+        packages/c/ @dan
+        packages/d/ @maria @jimmy
+        packages/e/ @jimmy
+
+      `;
+
+      it('does not assign changes for yarn.lock', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce(['yarn.lock']);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual([]);
+      });
+
+      it('assigns root changes to @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce(['package.json', 'yarn.lock']);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@john']);
+      });
+
+      it('assigns changes in package A to @maria (a), @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce([
+          'packages/a/package.json',
+          'yarn.lock',
+        ]);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@maria', '@john']);
+      });
+
+      it('assigns changes in package B to @jimmy (b), @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce([
+          'packages/b/package.json',
+          'yarn.lock',
+        ]);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@jimmy', '@john']);
+      });
+
+      it('assigns changes in package C to @dan (c), @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce([
+          'packages/c/package.json',
+          'yarn.lock',
+        ]);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@dan', '@john']);
+      });
+
+      it('assigns changes in package D to @maria (d), @jimmy (d), @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce([
+          'packages/d/package.json',
+          'yarn.lock',
+        ]);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@maria', '@jimmy', '@john']);
+      });
+
+      it('assigns changes in package A and B to @maria (a), @jimmy (b), @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce([
+          'packages/a/package.json',
+          'packages/b/package.json',
+          'yarn.lock',
+        ]);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@maria', '@jimmy', '@john']);
+      });
+
+      it('assigns changes in package A, B and C to @john, @maria (a), @jimmy (b), @dan (c), @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce([
+          'packages/a/package.json',
+          'packages/b/package.json',
+          'packages/c/package.json',
+          'yarn.lock',
+        ]);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@maria', '@jimmy', '@dan', '@john']);
+      });
+
+      it('assigns changes in package C and D to @dan (c), @maria (d), @jimmy (e), @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce([
+          'packages/c/package.json',
+          'packages/d/package.json',
+          'yarn.lock',
+        ]);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@dan', '@maria', '@jimmy', '@john']);
+      });
+
+      it('assigns changes in package D and E to @jimmy (d, e), @maria (d), @john (*)', async () => {
+        fs.readLocalFile.mockResolvedValueOnce(mockCodeOwners);
+        git.getBranchFiles.mockResolvedValueOnce([
+          'packages/d/package.json',
+          'packages/e/package.json',
+          'yarn.lock',
+        ]);
+        const codeOwners = await codeOwnersForPr(pr);
+        expect(codeOwners).toEqual(['@jimmy', '@maria', '@john']);
+      });
     });
 
     it('ignores comments and leading/trailing whitespace', async () => {
@@ -43,7 +172,7 @@ describe('workers/repository/update/pr/code-owners', () => {
       );
       git.getBranchFiles.mockResolvedValueOnce(['package.json']);
       const codeOwners = await codeOwnersForPr(pr);
-      expect(codeOwners).toEqual(['@john', '@maria']);
+      expect(codeOwners).toEqual(['@john', '@maria', '@jimmy']);
     });
 
     it('returns empty array when no code owners set', async () => {
--- lib/workers/repository/update/pr/code-owners.ts
+++ lib/workers/repository/update/pr/code-owners.ts
@@ -3,11 +3,22 @@ import { logger } from '../../../../logger';
 import type { Pr } from '../../../../modules/platform';
 import { readLocalFile } from '../../../../util/fs';
 import { getBranchFiles } from '../../../../util/git';
-import { newlineRegex, regEx } from '../../../../util/regex';
+import { regEx } from '../../../../util/regex';
+
+type UserScore = {
+  username: string;
+  score: number;
+};
+
+type FileScore = {
+  file: string;
+  score: number;
+};
 
 export async function codeOwnersForPr(pr: Pr): Promise<string[]> {
   logger.debug('Searching for CODEOWNERS file');
   try {
+    // Find CODEOWNERS file
     const codeOwnersFile =
       (await readLocalFile('CODEOWNERS', 'utf8')) ??
       (await readLocalFile('.github/CODEOWNERS', 'utf8')) ??
@@ -21,37 +32,130 @@ export async function codeOwnersForPr(pr: Pr): Promise<string[]> {
 
     logger.debug(`Found CODEOWNERS file: ${codeOwnersFile}`);
 
+    // Get list of modified files in PR
     const prFiles = await getBranchFiles(pr.sourceBranch);
-    const rules = codeOwnersFile
-      .split(newlineRegex)
+
+    if (!prFiles) {
+      logger.debug('PR includes no files');
+      return [];
+    }
+
+    // Convert CODEOWNERS file into list of matching rules
+    const fileOwnerRules = codeOwnersFile
+      .toString()
+      .split(/\r\n|\r|\n/)
+      // Remove empty and commented lines
       .map((line) => line.trim())
       .filter((line) => line && !line.startsWith('#'))
+
+      // Extract pattern & usernames
       .map((line) => {
         const [pattern, ...usernames] = line.split(regEx(/\s+/));
         return {
           usernames,
-          match: (path: string) => {
-            const matcher = ignore().add(pattern);
-            return matcher.ignores(path);
-          },
+          pattern,
+          match: (path: string) => ignore().add(pattern).ignores(path),
         };
-      })
-      .reverse();
+      });
+
     logger.debug(
-      { prFiles, rules },
+      { prFiles, fileOwnerRules },
       'PR files and rules to match for CODEOWNERS'
     );
-    const matchingRule = rules.find((rule) => prFiles?.every(rule.match));
-    if (!matchingRule) {
-      logger.debug('No matching CODEOWNERS rule found');
-      return [];
-    }
-    logger.debug(
-      `CODEOWNERS matched the following usernames: ${JSON.stringify(
-        matchingRule.usernames
-      )}`
+
+    // Apply rules & get list of owners for each prFile
+    const emptyRules = fileOwnerRules.filter(
+      (rule) => rule.usernames.length === 0
     );
-    return matchingRule.usernames;
+    const fileOwners =
+      // Map through all prFiles and match said file with all the rules
+      prFiles
+        .map<{
+          file: string;
+          usernames: UserScore[];
+        }>((file) => {
+          const usersWithScore = fileOwnerRules
+            .map((rule) =>
+              rule.match(file)
+                ? { score: rule.pattern.length, usernames: rule.usernames }
+                : null
+            )
+            .reduce<UserScore[]>((acc, match) => {
+              if (!match) {
+                return acc;
+              }
+              for (const user of match.usernames) {
+                if (!acc.find((a) => a.username === user)) {
+                  acc.push({ score: match.score, username: user });
+                }
+              }
+
+              return acc;
+            }, []);
+
+          return { file, usernames: usersWithScore };
+        })
+
+        // Match file again but this time only with emptyRules, this makes sure that files that have been set as having no owner remain as such
+        .map((fileMatch) => {
+          const matchEmpty = emptyRules.find((rule) =>
+            rule.match(fileMatch.file)
+          );
+          if (matchEmpty) {
+            return { ...fileMatch, usernames: [] };
+          }
+          return fileMatch;
+        });
+
+    logger.debug(
+      `CODEOWNERS matched the following files: ${JSON.stringify(fileOwners)}`
+    );
+
+    // Get list of all matched users and the files they own (reverse keys of fileOwners)
+    const usersWithOwnedFiles = fileOwners.reduce<
+      {
+        username: string;
+        files: FileScore[];
+      }[]
+    >((acc, fileMatch) => {
+      for (const userScore of fileMatch.usernames) {
+        // Get / create user file score
+        let userAcc = acc.find((u) => u.username === userScore.username);
+        if (!userAcc) {
+          userAcc = {
+            username: userScore.username,
+            files: [],
+          };
+          acc.push(userAcc);
+        }
+
+        // Add file to user
+        let file = userAcc.files.find((f) => f.file === fileMatch.file);
+        if (!file) {
+          file = {
+            file: fileMatch.file,
+            score: 0,
+          };
+          userAcc.files.push(file);
+        }
+        file.score += userScore.score;
+      }
+      return acc;
+    }, []);
+
+    // Calculate a match score for each user. This allows sorting of the final user array in a way that guarantees that users matched with more precise patterns are first and users matched with less precise patterns are last (wildcards)
+    const userScore = usersWithOwnedFiles
+      .map((userMatch) => ({
+        user: userMatch.username,
+        score: userMatch.files.reduce((acc, file) => acc + file.score, 0),
+      }))
+      .sort((a, b) => b.score - a.score);
+
+    logger.debug(
+      `CODEOWNERS matched the following users: ${JSON.stringify(userScore)}`
+    );
+
+    return userScore.map((u) => u.user);
   } catch (err) {
     logger.warn({ err, pr }, 'Failed to determine CODEOWNERS for PR.');
     return [];
