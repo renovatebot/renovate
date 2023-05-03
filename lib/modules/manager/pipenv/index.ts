395c34417ad8a842f39c5675114379cd0a8e1e22
Hugo Ferrando Seage <hugoseage@gmail.com>
2023-05-03 13:17:02 +0200

    Updated supported datasources in index.ts

--- lib/modules/manager/pipenv/index.ts
+++ lib/modules/manager/pipenv/index.ts
@@ -1,4 +1,6 @@
 import type { ProgrammingLanguage } from '../../../constants';
+import { GitTagsDatasource } from '../../datasource/git-tags';
+import { GithubTagsDatasource } from '../../datasource/github-tags';
 import { PypiDatasource } from '../../datasource/pypi';
 
 export { extractPackageFile } from './extract';
@@ -7,7 +9,11 @@ export { updateArtifacts } from './artifacts';
 export const language: ProgrammingLanguage = 'python';
 export const supportsLockFileMaintenance = true;
 
-export const supportedDatasources = [PypiDatasource.id];
+export const supportedDatasources = [
+  PypiDatasource.id,
+  GitTagsDatasource.id,
+  GithubTagsDatasource.id,
+];
 
 export const defaultConfig = {
   fileMatch: ['(^|/)Pipfile$'],
