f310996b5ac9085fa41ff94fccbcb9cbf6b23b62
Alex Hatzenbuhler <hatz@hey.com>
2023-05-04 11:17:17 -0500

    Move to sourceUrl

--- lib/modules/datasource/metadata-manual.ts
+++ lib/modules/datasource/metadata-manual.ts
@@ -53,10 +53,6 @@ export const manualChangelogUrls: Record<string, Record<string, string>> = {
     'google/cloud-sdk': 'https://cloud.google.com/sdk/docs/release-notes',
     neo4j: 'https://neo4j.com/release-notes/',
     'whitesource/renovate': 'https://github.com/whitesource/renovate-on-prem',
-  },
-  maven: {
-    'com.figure.gradle.semver-plugin:com.figure.gradle.semver-plugin.gradle.plugin': 
-      'https://github.com/FigureTechnologies/gradle-semver-plugin',
   }
 };
 
@@ -98,6 +94,10 @@ export const manualSourceUrls: Record<string, Record<string, string>> = {
   kubernetes: {
     node: 'https://github.com/nodejs/node',
   },
+  maven: {
+    'com.figure.gradle.semver-plugin:com.figure.gradle.semver-plugin.gradle.plugin': 
+      'https://github.com/FigureTechnologies/gradle-semver-plugin',
+  }
   npm: {
     node: 'https://github.com/nodejs/node',
   },
