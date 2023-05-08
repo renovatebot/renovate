eb77be123c8575eb2722d6a3412a678488df800e
Chuck Grindel <chuck.grindel@gmail.com>
2023-05-08 06:23:58 -0600

    Switch to is.boolean

--- lib/modules/manager/bazel-module/starlark.ts
+++ lib/modules/manager/bazel-module/starlark.ts
@@ -1,3 +1,5 @@
+import is from '@sindresorhus/is';
+
 const stringMapping: ReadonlyMap<string, boolean> = new Map<string, boolean>([
   ['True', true],
   ['False', false],
@@ -7,7 +9,7 @@ export const booleanStringValues = Array.from(stringMapping.keys());
 
 export function asBoolean(value: string): boolean {
   const result = stringMapping.get(value);
-  if (result !== undefined) {
+  if (is.boolean(result)) {
     return result;
   }
   throw new Error(`Invalid Starlark boolean string: ${value}`);
