9907ff096b929ed6ac04ec299f1367441c04fc60
Chuck Grindel <chuck.grindel@gmail.com>
2023-05-08 11:54:43 -0600

    Add mock test for extractPackageFile

--- lib/modules/manager/bazel-module/extract.ts
+++ lib/modules/manager/bazel-module/extract.ts
@@ -14,7 +14,6 @@ export function extractPackageFile(
   const deps = fragments
     .map((record) => {
       const result = BazelDepRecordToPackageDependency.safeParse(record);
-      // istanbul ignore next: cannot reach undefined without an additional rule
       return result.success ? result.data : undefined;
     })
     .filter(is.plainObject);
