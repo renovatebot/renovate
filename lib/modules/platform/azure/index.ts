6dab5cc16fd2f1e3480e8d5f88aee15dfa4b6722
Heiko Helmle <heiko.helmle@horiba.com>
2023-05-08 08:34:31 +0200

    update approval when a PR gets updated
    Signed-off-by: Heiko Helmle <heiko.helmle@horiba.com>

--- lib/modules/platform/azure/index.ts
+++ lib/modules/platform/azure/index.ts
@@ -506,6 +506,7 @@ export async function updatePr({
   prTitle: title,
   prBody: body,
   state,
+  platformOptions,
 }: UpdatePrConfig): Promise<void> {
   logger.debug(`updatePr(${prNo}, ${title}, body)`);
 
@@ -526,6 +527,24 @@ export async function updatePr({
     );
   } else if (state === 'closed') {
     objToUpdate.status = PullRequestStatus.Abandoned;
+  }
+  if (platformOptions?.autoApprove) {
+    const pr: GitPullRequest = await azureApiGit.getPullRequest(
+      config.repoId,
+      prNo
+    );
+    await azureApiGit.createPullRequestReviewer(
+      {
+        reviewerUrl: pr.createdBy!.url,
+        vote: AzurePrVote.Approved,
+        isFlagged: false,
+        isRequired: false,
+      },
+      config.repoId,
+      // TODO #7154
+      pr.pullRequestId!,
+      pr.createdBy!.id!
+    );
   }
 
   await azureApiGit.updatePullRequest(objToUpdate, config.repoId, prNo);
