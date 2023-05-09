228b74c904a88f62806782fda8215ae23c094f4f
Rhys Arkins <rhys@arkins.net>
2023-05-09 12:22:55 +0200

    Update lib/modules/manager/poetry/artifacts.ts
    Co-authored-by: Mathieu Kniewallner <mathieu.kniewallner@gmail.com>

--- lib/modules/manager/poetry/artifacts.ts
+++ lib/modules/manager/poetry/artifacts.ts
@@ -52,7 +52,7 @@ export function getPoetryRequirement(
     const lockVersion = data?.metadata?.['lock-version'];
     if (is.string(lockVersion)) {
       const poetryConstraint: Record<string, string> = {
-        '1.0': '<1.10.0',
+        '1.0': '<1.1.0',
         '1.1': '<1.3.0',
         '2.0': '>=1.3.0',
       };
