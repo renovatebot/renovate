712c98f811e2323db869b1ca443a4d715478f17c
Chuck Grindel <chuck.grindel@gmail.com>
2023-05-10 16:29:56 -0600

    Update import

--- lib/modules/manager/bazel-module/extract.ts
+++ lib/modules/manager/bazel-module/extract.ts
@@ -1,5 +1,5 @@
 import type { PackageFileContent } from '../types';
-import * as bazelDep from './bazel-dep';
+import { toPackageDependency } from './bazel-dep';
 import { isNotNullOrUndefined } from './filters';
 import { parse } from './parser';
 
@@ -12,7 +12,7 @@ export function extractPackageFile(
     return null;
   }
   const deps = fragments
-    .map((frag) => bazelDep.toPackageDependency(frag))
+    .map((frag) => toPackageDependency(frag))
     .filter(isNotNullOrUndefined);
   // istanbul ignore next: cannot reach null without introducing fake rule
   return deps.length ? { deps } : null;
