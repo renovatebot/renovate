5b3594a079b4c07c876d633b8961d90619095f34
Rhys Arkins <rhys@arkins.net>
2023-05-10 07:38:50 +0200

    Update lib/workers/global/config/parse/cli.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/workers/global/config/parse/cli.ts
+++ lib/workers/global/config/parse/cli.ts
@@ -36,7 +36,7 @@ export function getConfig(input: string[]): AllConfig {
         .replace('--include-forks', '--fork-processing=enabled')
         .replace('--recreate-closed=false', '--recreate-when=auto')
         .replace('--recreate-closed=true', '--recreate-when=always')
-        .replace(regEx(/--recreate-closed$/), '--recreate-when=always')
+        .replace('--recreate-closed', '--recreate-when=always')
     )
     .filter((a) => !a.startsWith('--git-fs'));
   const options = getOptions();
