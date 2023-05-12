93027b0976a3aba748f5ef5fefac82bd0f178b44
secustor <sebastian@poxhofer.at>
2023-05-12 22:53:13 +0200

    refactor: extract reoccurring schemas

--- lib/modules/manager/pep621/schema.ts
+++ lib/modules/manager/pep621/schema.ts
@@ -2,22 +2,23 @@ import { z } from 'zod';
 
 export type PyProject = z.infer<typeof PyProjectSchema>;
 
+const DependencyListSchema = z.array(z.string()).optional();
+const DependencyRecordSchema = z
+  .record(z.string(), z.array(z.string()))
+  .optional();
+
 export const PyProjectSchema = z.object({
   project: z
     .object({
-      dependencies: z.array(z.string()).optional(),
-      'optional-dependencies': z
-        .record(z.string(), z.array(z.string()))
-        .optional(),
+      dependencies: DependencyListSchema,
+      'optional-dependencies': DependencyRecordSchema,
     })
     .optional(),
   tool: z
     .object({
       pdm: z
         .object({
-          'dev-dependencies': z
-            .record(z.string(), z.array(z.string()))
-            .optional(),
+          'dev-dependencies': DependencyRecordSchema,
           source: z
             .array(
               z.object({
