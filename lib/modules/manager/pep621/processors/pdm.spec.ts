3c7f01100c930044273cbc7eedd2e40d4428bb4e
secustor <sebastian@poxhofer.at>
2023-05-15 22:58:57 +0200

    chore: add additional tests and reference to lock file support

--- lib/modules/manager/pep621/processors/pdm.spec.ts
+++ lib/modules/manager/pep621/processors/pdm.spec.ts
@@ -57,28 +57,12 @@ describe('modules/manager/pep621/processors/pdm', () => {
       expect(execSnapshots).toMatchObject([
         {
           cmd: 'docker pull containerbase/sidecar',
-          options: {
-            encoding: 'utf-8',
-          },
         },
         {
           cmd: 'docker ps --filter name=renovate_sidecar -aq',
-          options: {
-            encoding: 'utf-8',
-          },
         },
         {
           cmd: 'docker run --rm --name=renovate_sidecar --label=renovate_child -v "/tmp/github/some/repo":"/tmp/github/some/repo" -v "/tmp/cache":"/tmp/cache" -e BUILDPACK_CACHE_DIR -e CONTAINERBASE_CACHE_DIR -w "/tmp/github/some/repo" containerbase/sidecar bash -l -c "install-tool pdm v2.5.0 && pdm update dep1"',
-          options: {
-            cwd: '/tmp/github/some/repo',
-            encoding: 'utf-8',
-            env: {
-              BUILDPACK_CACHE_DIR: '/tmp/cache/containerbase',
-              CONTAINERBASE_CACHE_DIR: '/tmp/cache/containerbase',
-            },
-            maxBuffer: 10485760,
-            timeout: 900000,
-          },
         },
       ]);
     });
@@ -102,6 +86,75 @@ describe('modules/manager/pep621/processors/pdm', () => {
         { artifactError: { lockFile: 'pdm.lock', stderr: 'test error' } },
       ]);
       expect(execSnapshots).toEqual([]);
+    });
+
+    it('return update dep update', async () => {
+      const execSnapshots = mockExecAll();
+      GlobalConfig.set(adminConfig);
+      fs.getSiblingFileName.mockReturnValueOnce('pdm.lock');
+      fs.readLocalFile.mockResolvedValueOnce('test content');
+      fs.readLocalFile.mockResolvedValueOnce('changed test content');
+      // pdm
+      getPkgReleases.mockResolvedValueOnce({
+        releases: [{ version: 'v2.6.1' }, { version: 'v2.5.0' }],
+      });
+
+      const updatedDeps = [{ depName: 'dep1' }, { depName: 'dep2' }];
+      const result = await processor.updateArtifacts({
+        packageFileName: 'pyproject.toml',
+        newPackageFileContent: '',
+        config: {},
+        updatedDeps,
+      });
+      expect(result).toEqual([
+        {
+          file: {
+            contents: 'changed test content',
+            path: 'pdm.lock',
+            type: 'addition',
+          },
+        },
+      ]);
+      expect(execSnapshots).toMatchObject([
+        {
+          cmd: 'pdm update dep1 dep2',
+        },
+      ]);
+    });
+
+    it('return update on lockfileMaintenance', async () => {
+      const execSnapshots = mockExecAll();
+      GlobalConfig.set(adminConfig);
+      fs.getSiblingFileName.mockReturnValueOnce('pdm.lock');
+      fs.readLocalFile.mockResolvedValueOnce('test content');
+      fs.readLocalFile.mockResolvedValueOnce('changed test content');
+      // pdm
+      getPkgReleases.mockResolvedValueOnce({
+        releases: [{ version: 'v2.6.1' }, { version: 'v2.5.0' }],
+      });
+
+      const result = await processor.updateArtifacts({
+        packageFileName: 'pyproject.toml',
+        newPackageFileContent: '',
+        config: {
+          updateType: 'lockFileMaintenance',
+        },
+        updatedDeps: [],
+      });
+      expect(result).toEqual([
+        {
+          file: {
+            contents: 'changed test content',
+            path: 'pdm.lock',
+            type: 'addition',
+          },
+        },
+      ]);
+      expect(execSnapshots).toMatchObject([
+        {
+          cmd: 'pdm update ',
+        },
+      ]);
     });
   });
 });
