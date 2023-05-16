fceaeaff84b71547984863843ab46ed4c766bb28
Maurice Meyer <mmeyer@anaconda.com>
2023-05-16 11:54:29 +0200

    fix: implement review comments

--- lib/modules/datasource/endoflife-date/schema.ts
+++ lib/modules/datasource/endoflife-date/schema.ts
@@ -7,10 +7,6 @@ const EndoflifeDateVersionScheme = z
     cycle: z.string(),
     releaseDate: z.optional(z.string()),
     eol: z.optional(z.union([z.string(), z.boolean()])),
-    latest: z.optional(z.string()),
-    link: z.optional(z.string()),
-    lts: z.optional(z.union([z.string(), z.boolean()])),
-    support: z.optional(z.union([z.string(), z.boolean()])),
     discontinued: z.optional(z.union([z.string(), z.boolean()])),
   })
   .transform(({ cycle, releaseDate, eol, discontinued }): Release => {
