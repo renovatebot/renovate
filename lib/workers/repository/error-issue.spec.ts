493e3195e168eebe0f5ad4c81e4e0fc933c18fa4
Gabriel-Ladzaretti <Gabriel.ladzaretti@whitesourcesoftware.com>
2023-05-17 17:02:20 +0300

    generalize error issue creation

--- lib/workers/repository/error-issue.spec.ts
+++ lib/workers/repository/error-issue.spec.ts
@@ -1,7 +1,13 @@
 import { mock } from 'jest-mock-extended';
-import { RenovateConfig, getConfig, platform } from '../../../test/util';
+import {
+  RenovateConfig,
+  getConfig,
+  partial,
+  platform,
+} from '../../../test/util';
 import { GlobalConfig } from '../../config/global';
 import { CONFIG_VALIDATION } from '../../constants/error-messages';
+import { logger } from '../../logger';
 import type { Pr } from '../../modules/platform';
 import { raiseConfigWarningIssue } from './error-issue';
 
@@ -24,9 +30,16 @@ describe('workers/repository/error-issue', () => {
       const error = new Error(CONFIG_VALIDATION);
       error.validationSource = 'package.json';
       error.validationMessage = 'some-message';
+      error.validationError = 'some-error';
       platform.ensureIssue.mockResolvedValueOnce('created');
+
       const res = await raiseConfigWarningIssue(config, error);
+
       expect(res).toBeUndefined();
+      expect(logger.warn).toHaveBeenCalledWith(
+        { configError: error, res: 'created' },
+        'Configuration Warning'
+      );
     });
 
     it('creates issues (dryRun)', async () => {
@@ -35,50 +48,74 @@ describe('workers/repository/error-issue', () => {
       error.validationMessage = 'some-message';
       platform.ensureIssue.mockResolvedValueOnce('created');
       GlobalConfig.set({ dryRun: 'full' });
+
       const res = await raiseConfigWarningIssue(config, error);
+
       expect(res).toBeUndefined();
+      expect(logger.info).toHaveBeenCalledWith(
+        { configError: error },
+        'DRY-RUN: Would ensure configuration error issue'
+      );
     });
 
     it('handles onboarding', async () => {
       const error = new Error(CONFIG_VALIDATION);
       error.validationSource = 'package.json';
       error.validationMessage = 'some-message';
-      platform.getBranchPr.mockResolvedValue({
-        ...mock<Pr>(),
+      const pr = partial<Pr>({
+        title: 'onboarding',
         number: 1,
         state: 'open',
       });
+      platform.getBranchPr.mockResolvedValue(pr);
+
       const res = await raiseConfigWarningIssue(config, error);
+
       expect(res).toBeUndefined();
+      expect(platform.updatePr).toHaveBeenCalledWith(
+        expect.objectContaining({ prTitle: pr.title, number: pr.number })
+      );
     });
 
     it('handles onboarding (dryRun)', async () => {
       const error = new Error(CONFIG_VALIDATION);
       error.validationSource = 'package.json';
       error.validationMessage = 'some-message';
-      platform.getBranchPr.mockResolvedValue({
-        ...mock<Pr>(),
+      const pr = partial<Pr>({
         number: 1,
         state: 'open',
       });
+      platform.getBranchPr.mockResolvedValue(pr);
       GlobalConfig.set({ dryRun: 'full' });
+
       const res = await raiseConfigWarningIssue(config, error);
+
       expect(res).toBeUndefined();
+      expect(logger.info).toHaveBeenCalledWith(
+        `DRY-RUN: Would update PR #${pr.number}`
+      );
     });
 
     it('disable issue creation on config failure', async () => {
+      const notificationName = 'configErrorIssue';
       const error = new Error(CONFIG_VALIDATION);
       error.validationSource = 'package.json';
       error.validationMessage = 'some-message';
       // config.suppressNotifications = ['deprecationWarningIssues']
-      config.suppressNotifications = ['configErrorIssue'];
+      config.suppressNotifications = [notificationName];
       platform.getBranchPr.mockResolvedValueOnce({
         ...mock<Pr>(),
         number: 1,
         state: '!open',
       });
+
       const res = await raiseConfigWarningIssue(config, error);
+
       expect(res).toBeUndefined();
+      expect(logger.info).toHaveBeenCalledWith(
+        { notificationName },
+        'Configuration failure, issues will be suppressed'
+      );
     });
   });
 });
