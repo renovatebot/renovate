a109143df10f766312f212d7dd145b84eec2f8f4
Chuck Grindel <chuck.grindel@gmail.com>
2023-05-22 08:16:27 -0600

    More of the revert

--- lib/modules/manager/bazel-module/parser.ts
+++ lib/modules/manager/bazel-module/parser.ts
@@ -2,6 +2,7 @@ import { lang, query as q } from 'good-enough-parser';
 import { regEx } from '../../../util/regex';
 import { Ctx } from './context';
 import type { RecordFragment } from './fragments';
+import * as starlark from './starlark';
 
 const booleanValuesRegex = regEx(`^${starlark.booleanStringValues.join('|')}$`);
 const supportedRules = ['bazel_dep', 'git_override'];
@@ -14,7 +15,10 @@ const supportedRulesRegex = regEx(`^${supportedRules.join('|')}$`);
 const kvParams = q
   .sym<Ctx>((ctx, token) => ctx.startAttribute(token.value))
   .op('=')
-  .str((ctx, token) => ctx.addString(token.value));
+  .alt(
+    q.str((ctx, token) => ctx.addString(token.value)),
+    q.sym<Ctx>(booleanValuesRegex, (ctx, token) => ctx.addBoolean(token.value))
+  );
 
 const moduleRules = q
   .sym<Ctx>(supportedRulesRegex, (ctx, token) => ctx.startRule(token.value))
