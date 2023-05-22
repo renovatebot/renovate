b9d7231e06b370c1b6732691ab54a4b4b7d36145
Chuck Grindel <chuck.grindel@gmail.com>
2023-05-22 09:16:11 -0600

    Copy files over

---
+++ lib/modules/manager/bazel-module/bazelrc.spec.ts
@@ -0,0 +1,140 @@
+import { codeBlock } from 'common-tags';
+import upath from 'upath';
+import { GlobalConfig } from '../../../config/global';
+import type { RepoGlobalConfig } from '../../../config/types';
+import { BazelOption, CommandEntry, ImportEntry, parse, read } from './bazelrc';
+
+const adminConfig: RepoGlobalConfig = {
+  localDir: upath.resolve('lib/modules/manager/bazel-module/__fixtures__'),
+};
+
+describe('modules/manager/bazel-module/bazelrc', () => {
+  describe('BazelOption', () => {
+    it.each`
+      a                                       | expArgs
+      ${'--show_timestamps'}                  | ${[['show_timestamps', undefined]]}
+      ${'--host_jvm_args=-XX:-UseParallelGC'} | ${[['host_jvm_args', '-XX:-UseParallelGC']]}
+      ${'--host_jvm_args='}                   | ${[['host_jvm_args', '']]}
+      ${'--jobs 600'}                         | ${[['jobs', '600']]}
+    `('parse($a)', ({ a, expArgs }) => {
+      const exp = (expArgs as [string, string | undefined][]).map(
+        (args) => new BazelOption(...args)
+      );
+      const result = BazelOption.parse(a);
+      expect(result).toEqual(exp);
+    });
+  });
+
+  describe('CommandEntry', () => {
+    it('getOption', () => {
+      const opt0 = new BazelOption('show_timestamps');
+      const opt1 = new BazelOption('keep_going');
+      const opt2 = new BazelOption('jobs', '600');
+      const cmdEntry = new CommandEntry('build', [opt0, opt1, opt2]);
+      expect(cmdEntry.getOption('does_not_exist')).toBeUndefined();
+      expect(cmdEntry.getOption(opt0.name)).toEqual(opt0);
+      expect(cmdEntry.getOption(opt2.name)).toEqual(opt2);
+    });
+  });
+
+  it('parse', () => {
+    const input = codeBlock`
+        # Bob's Bazel option defaults
+        
+        startup --host_jvm_args=-XX:-UseParallelGC
+        import /home/bobs_project/bazelrc
+        build --show_timestamps --keep_going --jobs 600
+        build --color=yes
+        query --keep_going
+        
+        # Definition of --config=memcheck
+        build:memcheck --strip=never --test_timeout=3600
+
+        try-import %workspace%/local.bazelrc
+      `;
+    const res = parse(input);
+    expect(res).toEqual([
+      new CommandEntry('startup', [
+        new BazelOption('host_jvm_args', '-XX:-UseParallelGC'),
+      ]),
+      new ImportEntry('/home/bobs_project/bazelrc', false),
+      new CommandEntry('build', [
+        new BazelOption('show_timestamps'),
+        new BazelOption('keep_going'),
+        new BazelOption('jobs', '600'),
+      ]),
+      new CommandEntry('build', [new BazelOption('color', 'yes')]),
+      new CommandEntry('query', [new BazelOption('keep_going')]),
+      new CommandEntry(
+        'build',
+        [
+          new BazelOption('strip', 'never'),
+          new BazelOption('test_timeout', '3600'),
+        ],
+        'memcheck'
+      ),
+      new ImportEntry('%workspace%/local.bazelrc', true),
+    ]);
+  });
+
+  describe('read()', () => {
+    beforeEach(() => {
+      GlobalConfig.set(adminConfig);
+    });
+
+    afterEach(() => {
+      GlobalConfig.reset();
+    });
+
+    it('when .bazelrc does not exist', async () => {
+      const result = await read('.');
+      expect(result).toHaveLength(0);
+    });
+
+    it('when .bazelrc has invalid lines', async () => {
+      const result = await read('bazelrc/invalid-bazelrc');
+      expect(result).toEqual([
+        new CommandEntry('build', [
+          new BazelOption('show_timestamps'),
+          new BazelOption('keep_going'),
+          new BazelOption('jobs', '600'),
+        ]),
+      ]);
+    });
+
+    it('when .bazelrc has no imports', async () => {
+      const result = await read('bazelrc/no-imports');
+      expect(result).toEqual([
+        new CommandEntry('build', [
+          new BazelOption('show_timestamps'),
+          new BazelOption('keep_going'),
+          new BazelOption('jobs', '600'),
+        ]),
+        new CommandEntry('build', [new BazelOption('color', 'yes')]),
+      ]);
+    });
+
+    it('when .bazelrc has import and try-import, try-import exists', async () => {
+      const result = await read('bazelrc/single-level-imports');
+      expect(result).toEqual([
+        new CommandEntry('build', [new BazelOption('show_timestamps')]),
+        new CommandEntry('build', [new BazelOption('color', 'yes')]),
+      ]);
+    });
+
+    it('when .bazelrc has import and try-import, try-import does not exist', async () => {
+      const result = await read('bazelrc/try-import-does-not-exist');
+      expect(result).toEqual([
+        new CommandEntry('build', [new BazelOption('jobs', '600')]),
+      ]);
+    });
+
+    it('when .bazelrc multi-level import', async () => {
+      const result = await read('bazelrc/multi-level-imports');
+      expect(result).toEqual([
+        new CommandEntry('build', [new BazelOption('show_timestamps')]),
+        new CommandEntry('build', [new BazelOption('jobs', '600')]),
+      ]);
+    });
+  });
+});
--- lib/modules/manager/bazel-module/extract.ts
+++ lib/modules/manager/bazel-module/extract.ts
@@ -1,22 +1,39 @@
+import { dirname } from 'upath';
 import { logger } from '../../../logger';
+import { isNotNullOrUndefined } from '../../../util/array';
 import { LooseArray } from '../../../util/schema-utils';
 import type { PackageFileContent } from '../types';
+import * as bazelrc from './bazelrc';
 import { parse } from './parser';
 import { RuleToBazelModulePackageDep } from './rules';
 import * as rules from './rules';
 
-export function extractPackageFile(
+export async function extractPackageFile(
   content: string,
-  packageFile: string
-): PackageFileContent | null {
+  filename: string
+): Promise<PackageFileContent | null> {
   try {
     const records = parse(content);
-    return LooseArray(RuleToBazelModulePackageDep)
+    const pfc: PackageFileContent = LooseArray(RuleToBazelModulePackageDep)
       .transform(rules.toPackageDependencies)
-      .transform((deps) => (deps.length ? { deps } : null))
+      .transform((deps) => ({ deps }))
       .parse(records);
+    if (!pfc.deps.length) {
+      return null;
+    }
+
+    const registryUrls = (await bazelrc.read(dirname(filename)))
+      // Ignore any entries for custom configurations
+      .filter((ce) => ce.config === undefined)
+      .map((ce) => ce.getOption('registry')?.value)
+      .filter(isNotNullOrUndefined);
+    if (registryUrls.length) {
+      pfc.registryUrls = registryUrls;
+    }
+
+    return pfc;
   } catch (err) {
-    logger.debug({ err, packageFile }, 'Failed to parse bazel module file.');
+    logger.debug({ err, filename }, 'Failed to parse bazel module file.');
     return null;
   }
 }
