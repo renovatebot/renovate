db2b3652b3bdc1516467f2d8ecf898726aefeda5
Adam Setch <adam.setch@outlook.com>
2023-05-22 14:27:50 -0400

    tests: update to use existing range function

--- lib/util/http/bitbucket.spec.ts
+++ lib/util/http/bitbucket.spec.ts
@@ -1,9 +1,9 @@
 import * as httpMock from '../../../test/http-mock';
 import * as hostRules from '../host-rules';
+import { range } from '../range';
 import { BitbucketHttp, setBaseUrl } from './bitbucket';
 
 const baseUrl = 'https://api.bitbucket.org';
-const range = (count: number) => [...Array(count).keys()];
 
 describe('util/http/bitbucket', () => {
   let api: BitbucketHttp;
@@ -58,23 +58,28 @@ describe('util/http/bitbucket', () => {
   });
 
   it('paginates: adds default pagelen if non is present', async () => {
+
+    const valuesPageOne = [...range(1, 100)];
+    const valuesPageTwo = [...range(101, 200)];
+    const valuesPageThree = [...range(201, 210)];
+
     httpMock
       .scope(baseUrl)
       .get('/some-url?foo=bar&pagelen=100')
       .reply(200, {
-        values: range(100),
+        values: valuesPageOne,
         page: '1',
         next: `${baseUrl}/some-url?foo=bar&pagelen=100&page=2`,
       })
       .get('/some-url?foo=bar&pagelen=100&page=2')
       .reply(200, {
-        values: range(100),
+        values: valuesPageTwo,
         page: '2',
         next: `${baseUrl}/some-url?foo=bar&pagelen=100&page=3`,
       })
       .get('/some-url?foo=bar&pagelen=100&page=3')
       .reply(200, {
-        values: range(10),
+        values: valuesPageThree,
         page: '3',
       });
     const res = await api.getJson('/some-url?foo=bar', { paginate: true });
@@ -82,29 +87,33 @@ describe('util/http/bitbucket', () => {
       page: '1',
       pagelen: 210,
       size: 210,
-      values: expect.arrayContaining(Array(210)),
+      values: [...valuesPageOne, ...valuesPageTwo, ...valuesPageThree],
       next: undefined,
     });
   });
 
   it('paginates: respects pagelen if already set in path', async () => {
+    const valuesPageOne = [...range(1, 10)];
+    const valuesPageTwo = [...range(11, 20)];
+    const valuesPageThree = [...range(21, 21)];
+
     httpMock
       .scope(baseUrl)
       .get('/some-url?pagelen=10')
       .reply(200, {
-        values: range(10),
+        values: valuesPageOne,
         page: '1',
         next: `${baseUrl}/some-url?pagelen=10&page=2`,
       })
       .get('/some-url?pagelen=10&page=2')
       .reply(200, {
-        values: range(10),
+        values: valuesPageTwo,
         page: '2',
         next: `${baseUrl}/some-url?pagelen=10&page=3`,
       })
       .get('/some-url?pagelen=10&page=3')
       .reply(200, {
-        values: range(1),
+        values: valuesPageThree,
         page: '3',
       });
     const res = await api.getJson('some-url?pagelen=10', { paginate: true });
@@ -112,29 +121,34 @@ describe('util/http/bitbucket', () => {
       page: '1',
       pagelen: 21,
       size: 21,
-      values: expect.arrayContaining(Array(21)),
+      values: [...valuesPageOne, ...valuesPageTwo, ...valuesPageThree],
       next: undefined,
     });
   });
 
   it('paginates: respects pagelen if set in options', async () => {
+    const valuesPageOne = [...range(1, 20)];
+    const valuesPageTwo = [...range(21, 40)];
+    const valuesPageThree = [...range(41, 44)];
+
+
     httpMock
       .scope(baseUrl)
       .get('/some-url?pagelen=20')
       .reply(200, {
-        values: range(20),
+        values: valuesPageOne,
         page: '1',
         next: `${baseUrl}/some-url?pagelen=20&page=2`,
       })
       .get('/some-url?pagelen=20&page=2')
       .reply(200, {
-        values: range(20),
+        values: valuesPageTwo,
         page: '2',
         next: `${baseUrl}/some-url?pagelen=20&page=3`,
       })
       .get('/some-url?pagelen=20&page=3')
       .reply(200, {
-        values: range(4),
+        values: valuesPageThree,
         page: '3',
       });
     const res = await api.getJson('some-url', {
@@ -145,7 +159,7 @@ describe('util/http/bitbucket', () => {
       page: '1',
       pagelen: 44,
       size: 44,
-      values: expect.arrayContaining(Array(44)),
+      values: [...valuesPageOne, ...valuesPageTwo, ...valuesPageThree],
       next: undefined,
     });
   });
