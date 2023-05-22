1661b59108d24d9b552307f5c9072056412c703b
Chuck Grindel <chuck.grindel@gmail.com>
2023-05-22 09:16:11 -0600

    Copy files over

--- lib/modules/manager/bazel-module/extract.ts
+++ lib/modules/manager/bazel-module/extract.ts
@@ -18,7 +18,7 @@ export async function extractPackageFile(
       RuleToBazelModulePackageDep
     )
       .transform(rules.toPackageDependencies)
-      .transform((deps) => (deps.length ? { deps } : null))
+      .transform((deps) => ({ deps }))
       .parse(records);
     if (!pfc) {
       return null;
