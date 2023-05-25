3de23a19839913dc515ce265e5da77de3f61cab2
Rhys Arkins <rhys@arkins.net>
2023-05-25 17:46:28 +0200

    feat!: merge matchPaths and matchFiles into matchFileNames (#22406)
    Closes #22395
    
    BREAKING CHANGE: matchPaths and matchFiles are now combined into matchFileNames, supporting exact match and glob-only

--- lib/util/package-rules/files.ts
+++ lib/util/package-rules/files.ts
@@ -1,23 +1,27 @@
 import is from '@sindresorhus/is';
+import { minimatch } from 'minimatch';
 import type { PackageRule, PackageRuleInputConfig } from '../../config/types';
 import { Matcher } from './base';
 
-export class FilesMatcher extends Matcher {
+export class FileNamesMatcher extends Matcher {
   override matches(
     { packageFile, lockFiles }: PackageRuleInputConfig,
-    { matchFiles }: PackageRule
+    { matchFileNames }: PackageRule
   ): boolean | null {
-    if (is.undefined(matchFiles)) {
+    if (is.undefined(matchFileNames)) {
       return null;
     }
     if (is.undefined(packageFile)) {
       return false;
     }
 
-    return matchFiles.some(
-      (fileName) =>
-        packageFile === fileName ||
-        (is.array(lockFiles) && lockFiles?.includes(fileName))
+    return matchFileNames.some(
+      (matchFileName) =>
+        minimatch(packageFile, matchFileName, { dot: true }) ||
+        (is.array(lockFiles) &&
+          lockFiles.some((lockFile) =>
+            minimatch(lockFile, matchFileName, { dot: true })
+          ))
     );
   }
 }
