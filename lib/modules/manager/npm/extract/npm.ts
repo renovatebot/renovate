4f12ce9a0d1416c94a4aa17a8699ecc6559b10bf
Matthias Junker <matthias.junker@mobi.ch>
2023-06-02 09:44:25 +0200

    chore: fix test after merge issue

--- lib/modules/manager/npm/extract/npm.ts
+++ lib/modules/manager/npm/extract/npm.ts
@@ -3,11 +3,11 @@ import { readLocalFile } from '../../../../util/fs';
 import { PackageLock } from './schema';
 import type { LockFile } from './types';
 
-export async function getNpmLock(filePath: string): Promise<LockFile | null> {
+export async function getNpmLock(filePath: string): Promise<LockFile> {
   const lockfileContent = await readLocalFile(filePath, 'utf8');
   if (!lockfileContent) {
     logger.debug({ filePath }, 'Npm: unable to read lockfile');
-    return null;
+    return { lockedVersions: {} };
   }
 
   const parsedLockfile = PackageLock.safeParse(lockfileContent);
@@ -16,7 +16,7 @@ export async function getNpmLock(filePath: string): Promise<LockFile | null> {
       { filePath, err: parsedLockfile.error },
       'Npm: unable to parse lockfile'
     );
-    return null;
+    return { lockedVersions: {} };
   }
 
   return parsedLockfile.data;
