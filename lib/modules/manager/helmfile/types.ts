31b7b31d680f06a3e8a25851a60e9690f0ab7331
mugioka <okamugi0722@gmail.com>
2023-06-02 12:54:03 +0900

    chore: remove unnecessary code.

--- lib/modules/manager/helmfile/types.ts
+++ lib/modules/manager/helmfile/types.ts
@@ -1,5 +1,5 @@
 import type { z } from 'zod';
-import type { HostRule } from '../../../types';
+
 import type { DocSchema, ReleaseSchema, RepositorySchema } from './schema';
 
 export type Release = z.infer<typeof ReleaseSchema>;
@@ -7,7 +7,3 @@ export type Release = z.infer<typeof ReleaseSchema>;
 export type Repository = z.infer<typeof RepositorySchema>;
 
 export type Doc = z.infer<typeof DocSchema>;
-
-export interface RepositoryRule extends Repository {
-  hostRule: HostRule;
-}
