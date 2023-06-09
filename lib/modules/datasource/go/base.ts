989e0572fc08231dd59e93bd1d82138fa827a742
Hilário Coelho <5185271+hilariocoelho@users.noreply.github.com>
2023-06-09 23:06:50 +0100

    fix go-import meta header support for Azure DevOps

--- lib/modules/datasource/go/base.ts
+++ lib/modules/datasource/go/base.ts
@@ -172,7 +172,7 @@ export class BaseGoDatasource {
     goModule: string
   ): DataSource | null {
     const importMatch = regEx(
-      `<meta\\s+name="?go-import"?\\s+content="([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)">`
+      `<meta\\s+name="?go-import"?\\s+content="([^\\s]+)\\s+([^\\s]+)\\s+([^\\s]+)".*?>`
     ).exec(res);
 
     if (!importMatch) {
