f6112172891f378bb0d169aed5fb3f99b771a442
MartijnLeijssen-TomTom <46988880+MartijnLeijssen-TomTom@users.noreply.github.com>
2023-06-15 17:56:54 +0200

    feat(datasource/conan): add sourceUrl when artifactory is used (#22656)
    Co-authored-by: Rhys Arkins <rhys@arkins.net>
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/modules/datasource/common.ts
+++ lib/modules/datasource/common.ts
@@ -1,4 +1,5 @@
 import is from '@sindresorhus/is';
+import type { HttpResponse } from '../../util/http/types';
 import type { GetPkgReleasesConfig } from './types';
 
 export function isGetPkgReleasesConfig(
@@ -12,4 +13,12 @@ export function isGetPkgReleasesConfig(
       (input as GetPkgReleasesConfig).packageName
     )
   );
+}
+
+const JFROG_ARTIFACTORY_RES_HEADER = 'x-jfrog-version';
+
+export function isArtifactoryServer<T = unknown>(
+  res: HttpResponse<T> | undefined
+): boolean {
+  return is.string(res?.headers[JFROG_ARTIFACTORY_RES_HEADER]);
 }
