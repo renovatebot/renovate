fa6e5df8052fb3677ac3fe549438829ebd8940f5
RahulGautamSingh <rahultesnik@gmail.com>
2023-06-21 16:40:57 +0545

    feat(onboarding): skip processing onboarding branch (#22490)
    Co-authored-by: Rhys Arkins <rhys@arkins.net>
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/workers/repository/onboarding/pr/index.ts
+++ lib/workers/repository/onboarding/pr/index.ts
@@ -33,6 +33,7 @@ export async function ensureOnboardingPr(
 ): Promise<void> {
   if (
     config.repoIsOnboarded ||
+    OnboardingState.onboardingCacheValid ||
     (config.onboardingRebaseCheckbox && !OnboardingState.prUpdateRequested)
   ) {
     return;
