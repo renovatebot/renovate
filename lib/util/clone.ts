c403c0e4376561cc71fa1253620c431b19d3b3c1
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-06-25 21:32:04 +0300

    Fixes

--- lib/util/clone.ts
+++ lib/util/clone.ts
@@ -2,8 +2,10 @@
  * Creates a deep clone of an object.
  * @param input The object to clone.
  */
-export function clone<T>(input: T): T {
+export function clone<T = unknown>(input: T): T {
   const res = structuredClone(input);
-  Object.setPrototypeOf(res, Object.getPrototypeOf(input));
+  if (typeof input === 'object' && input !== null) {
+    Object.setPrototypeOf(res, Object.getPrototypeOf(input));
+  }
   return res;
 }
