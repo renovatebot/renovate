570ee2a4014edae01c82334f1fbc8aa0ef73f69a
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-06-26 13:14:37 +0300

    Copy with `klona` library

--- lib/util/clone.ts
+++ lib/util/clone.ts
@@ -1,11 +1,9 @@
+import { klona } from 'klona/json';
+
 /**
  * Creates a deep clone of an object.
  * @param input The object to clone.
  */
 export function clone<T = unknown>(input: T): T {
-  const res = structuredClone(input);
-  if (typeof input === 'object' && input !== null) {
-    Object.setPrototypeOf(res, Object.getPrototypeOf(input));
-  }
-  return res;
+  return klona(input);
 }
