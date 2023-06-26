b00c2206853558680d60bf7368f7369bd3306974
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-06-26 15:49:13 +0300

    Error handling

--- lib/util/clone.spec.ts
+++ lib/util/clone.spec.ts
@@ -1,23 +1,35 @@
 import { clone } from './clone';
 
 describe('util/clone', () => {
-  const obj: any = {
-    name: 'object',
-    type: 'object',
-    isObject: true,
-  };
-
   it('returns null', () => {
     const res = clone(null);
     expect(res).toBeNull();
   });
 
   it('maintains same order', () => {
+    const obj: any = {
+      name: 'object',
+      type: 'object',
+      isObject: true,
+    };
+
     const res = clone(obj);
+
     expect(res).toMatchSnapshot(`{
       name: 'object',
       type: 'object',
       isObject: true,
     }`);
+  });
+
+  it('assigns "[Circular]" to circular references', () => {
+    const obj: any = {
+      name: 'object',
+      type: 'object',
+      isObject: true,
+    };
+    obj.circular = obj;
+
+    expect(() => clone(obj)).toThrow('Circular reference detected');
   });
 });
--- lib/util/clone.ts
+++ lib/util/clone.ts
@@ -6,5 +6,14 @@ import { klona } from 'klona/json';
  * @param input The object to clone.
  */
 export function clone<T = unknown>(input: T): T {
-  return klona(input);
+  try {
+    return klona(input);
+  } catch (error) {
+    if (error.name === 'RangeError') {
+      throw new Error('Circular reference detected');
+    }
+
+    // istanbul ignore next: not easily testable
+    throw error;
+  }
 }
