25d5cccd6d942db89166b82008cac3e5cd1b2931
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-06-27 19:00:19 +0300

    Test hooks

--- lib/util/http/hooks.ts
+++ lib/util/http/hooks.ts
@@ -3,25 +3,23 @@
 // From here: https://github.com/sindresorhus/got/issues/1489#issuecomment-805485731
 import type { Hooks, Response } from 'got';
 
-function isResponseOk(response: Response): boolean {
+export function isResponseOk(response: Response): boolean {
   const { statusCode } = response;
-  const limitStatusCode = response.request.options.followRedirect
-    ? 299
-    : /* istanbul ignore next */ 399;
+  const limitStatusCode = response.request.options.followRedirect ? 299 : 399;
 
   return (
     (statusCode >= 200 && statusCode <= limitStatusCode) || statusCode === 304
   );
 }
 
-export const hooks: Hooks = {
-  afterResponse: [
-    (response: Response): Response => {
-      if (isResponseOk(response)) {
-        response.request.destroy();
-      }
+export function hook(response: Response): Response {
+  if (isResponseOk(response)) {
+    response.request.destroy();
+  }
 
-      return response;
-    },
-  ],
+  return response;
+}
+
+export const hooks: Hooks = {
+  afterResponse: [hook],
 };
