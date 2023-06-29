a44856874581cb92064f4173299dbde712e78723
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-06-29 17:24:18 +0300

    Refactor

--- lib/modules/versioning/index.ts
+++ lib/modules/versioning/index.ts
@@ -1,12 +1,8 @@
-import { logger } from '../../logger';
 import versionings from './api';
-import { isVersioningApiConstructor } from './common';
-import * as semverCoerced from './semver-coerced';
+import { Versioning } from './schema';
 import type { VersioningApi, VersioningApiConstructor } from './types';
 
 export * from './types';
-
-const defaultVersioning = semverCoerced;
 
 export const getVersioningList = (): string[] => Array.from(versionings.keys());
 /**
@@ -18,27 +14,5 @@ export const getVersionings = (): Map<
 > => versionings;
 
 export function get(versioning: string | undefined): VersioningApi {
-  if (!versioning) {
-    logger.trace(
-      `Missing versioning, using ${defaultVersioning.id} as fallback.`
-    );
-    return defaultVersioning.api;
-  }
-  const [versioningName, ...versioningRest] = versioning.split(':');
-  const versioningConfig = versioningRest.length
-    ? versioningRest.join(':')
-    : undefined;
-
-  const theVersioning = versionings.get(versioningName);
-  if (!theVersioning) {
-    logger.info(
-      { versioning },
-      `Unknown versioning - defaulting to ${defaultVersioning.id}`
-    );
-    return defaultVersioning.api;
-  }
-  if (isVersioningApiConstructor(theVersioning)) {
-    return new theVersioning(versioningConfig);
-  }
-  return theVersioning;
+  return Versioning.parse(versioning);
 }
--- lib/modules/versioning/schema.ts
+++ lib/modules/versioning/schema.ts
@@ -1,12 +1,38 @@
+import is from '@sindresorhus/is';
 import { z } from 'zod';
-import api from './api';
+import { logger } from '../../logger';
+import versionings from './api';
+import * as defaultVersioning from './semver-coerced';
+import type { VersioningApi } from './types';
 
-export const Versioning = z.string().transform((key, ctx) => {
-  const versioning = api.get(key);
-  if (!versioning) {
-    ctx.addIssue({ code: 'custom', message: 'Versioning not found' });
-    return z.NEVER;
-  }
+export const Versioning = z
+  .unknown()
+  .transform((versioningSpec): VersioningApi => {
+    if (!is.string(versioningSpec)) {
+      logger.debug(
+        { versioning: versioningSpec },
+        `Versioning: invalid name, falling back to ${defaultVersioning.id}`
+      );
+      return defaultVersioning.api;
+    }
 
-  return versioning;
-});
+    const [versioningName, ...versioningRest] = versioningSpec.split(':');
+
+    let versioning = versionings.get(versioningName);
+    if (!versioning) {
+      logger.info(
+        { versioning: versioningSpec },
+        `Versioning: '${versioningSpec}' not found, falling back to ${defaultVersioning.id}`
+      );
+      return defaultVersioning.api;
+    }
+
+    if (is.function_(versioning)) {
+      const versioningConfig = versioningRest.length
+        ? versioningRest.join(':')
+        : undefined;
+      versioning = new versioning(versioningConfig);
+    }
+
+    return versioning;
+  });
