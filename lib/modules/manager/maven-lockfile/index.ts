b94a6a7f36d9322db588d0407d8fafbd5b4b7bcc
Martin <wittlinger.martin@gmail.com>
2023-06-30 13:20:24 +0200

    up

--- lib/modules/manager/maven-lockfile/index.ts
+++ lib/modules/manager/maven-lockfile/index.ts
@@ -1,12 +1,13 @@
+import type { StatusResult } from 'simple-git';
 import upath from 'upath';
 import type { ProgrammingLanguage } from '../../../constants';
 import { logger } from '../../../logger';
 import { exec } from '../../../util/exec';
+import type { ExecResult } from '../../../util/exec/types';
 import { readLocalFile } from '../../../util/fs';
 import { getRepoStatus } from '../../../util/git';
 import type { UpdateArtifact, UpdateArtifactsResult } from '../types';
 
-// key feature of maven-lockfile is to provide a way to maintain a lockfile
 export const supportsLockFileMaintenance = true;
 export const language: ProgrammingLanguage = 'java';
 
@@ -17,8 +18,10 @@ export async function updateArtifacts({
   config,
 }: UpdateArtifact): Promise<UpdateArtifactsResult[] | null> {
   try {
+    // Log the updated dependencies
     logger.debug({ updatedDeps }, 'maven-lockfile.updateArtifacts()');
 
+    // Check if any Maven dependencies were updated
     if (!updatedDeps.some((dep) => dep.datasource === 'maven')) {
       logger.info(
         'No Maven dependency version updated - skipping Artifacts update'
@@ -27,38 +30,44 @@ export async function updateArtifacts({
         'No Maven dependency version updated - skipping Artifacts update'
       );
     }
+
+    // Set the current working directory for the `exec` command
     const execOptions = {
       cwd: upath.resolve(upath.dirname(packageFileName)),
     };
-    const cmd = 'mvn io.github.chains-project:maven-lockfile:3.4.1:generate';
-    const result = await exec(cmd, execOptions);
-    logger.info({ result }, 'maven-lockfile.updateArtifacts() result');
-    const status = await getRepoStatus();
-    const res: UpdateArtifactsResult[] = [];
 
-    for (const f of [...status.modified]) {
-      logger.info(`modified: ${f}`);
-      if (f.match('**/lockfile.json')) {
-        logger.info(`lockfile.json updated`);
-        res.push({
-          file: {
-            type: 'addition',
-            path: f,
-            contents: await readLocalFile(f),
-          },
-        });
-      }
-    }
+    // Generate the Maven lockfile using the `mvn` command
+    const cmd = 'mvn io.github.chains-project:maven-lockfile:3.4.1:generate';
+    const result: ExecResult = await exec(cmd, execOptions);
+    logger.info({ result }, 'maven-lockfile.updateArtifacts() result');
+
+    // Get the repository status
+    const status = await getRepoStatus();
+    // Add the updated lockfile to the array of updated artifacts
+    const res: UpdateArtifactsResult[] = await addUpdatedLockfiles(status);
     return res;
   } catch (err) {
-    logger.debug({ err }, 'Error updating maven-lockfile');
-    return [
-      {
-        artifactError: {
-          lockFile: packageFileName,
-          stderr: err.message,
-        },
-      },
-    ];
+    logger.error({ err }, 'maven-lockfile.updateArtifacts() error');
+    throw err;
   }
+}
+
+async function addUpdatedLockfiles(
+  status: StatusResult
+): Promise<UpdateArtifactsResult[]> {
+  const res: UpdateArtifactsResult[] = [];
+  for (const f of [...status.modified]) {
+    logger.info(`modified: ${f}`);
+    if (/.*\/lockfile.json/.exec(f)) {
+      logger.info(`lockfile.json updated`);
+      res.push({
+        file: {
+          type: 'addition',
+          path: f,
+          contents: await readLocalFile(f),
+        },
+      });
+    }
+  }
+  return res;
 }
