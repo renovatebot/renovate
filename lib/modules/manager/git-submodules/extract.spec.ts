60349918669efb5829c0e2bd55413dcf0c9aaec2
lfcyja <cyjada@bluewin.ch>
2023-07-14 09:59:07 +0200

    add unit test for URL encoding behaviour

--- lib/modules/manager/git-submodules/extract.spec.ts
+++ lib/modules/manager/git-submodules/extract.spec.ts
@@ -89,6 +89,11 @@ describe('modules/manager/git-submodules/extract', () => {
         token: 'xyz',
         hostType: 'gitlab',
       });
+      hostRules.add({
+        matchHost: 'organization@dev.azure.com/organization',
+        token: 'pat',
+        hostType: 'azure',
+      });
       gitMock.listRemote.mockResolvedValueOnce(
         'ref: refs/heads/main  HEAD\n5701164b9f5edba1f6ca114c491a564ffb55a964        HEAD'
       );
@@ -137,6 +142,30 @@ describe('modules/manager/git-submodules/extract', () => {
             currentValue: 'dev',
             depName: 'some-gitlab',
             packageName: 'https://gitlab.com/some/repo.git',
+          },
+        ],
+      });
+    });
+
+    it('whitespaces in submodule URL are encoded properly', async () => {
+      hostRules.add({
+        matchHost: 'organization@dev.azure.com/organization',
+        token: 'pat',
+        hostType: 'azure',
+      });
+      gitMock.listRemote.mockResolvedValueOnce(
+        'ref: refs/heads/main  HEAD\n5701164b9f5edba1f6ca114c491a564ffb55a964        HEAD'
+      );
+      const res = await extractPackageFile('', '.gitmodules.6', {});
+      expect(res).toEqual({
+        datasource: 'git-refs',
+        deps: [
+          {
+            currentDigest: '4b825dc642cb6eb9a060e54bf8d69288fbee4904',
+            currentValue: 'main',
+            depName: 'some-azure',
+            packageName:
+              'https://organization@dev.azure.com/organization/whitespace%20project/_git/repo',
           },
         ],
       });
