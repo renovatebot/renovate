51e16bda876b217cee9b6b5eca0fea37b29c6d51
RahulGautamSingh <rahultesnik@gmail.com>
2023-07-14 23:27:48 +0545

    fix coverage

--- lib/workers/repository/extract/index.spec.ts
+++ lib/workers/repository/extract/index.spec.ts
@@ -26,6 +26,7 @@ describe('workers/repository/extract/index', () => {
       managerFiles.getManagerPackageFiles.mockResolvedValue([
         partial<PackageFile<Record<string, any>>>({}),
       ]);
+      config.regexManagers = undefined; // for coverage
       const res = await extractAllDependencies(config);
       expect(Object.keys(res.packageFiles)).toContain('ansible');
     });
