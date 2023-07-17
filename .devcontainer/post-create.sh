a003b62bfffa06cb3d040f8052d2ef6fc31184b9
Felipe Santos <felipecassiors@gmail.com>
2023-07-17 17:07:15 +0000

    chore: extract postCreate into a script
    So that the Codespaces workaround will only be applied on Codespaces.
    
    And also reorganize devcontainer.json.

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "${CODESPACES}" == true ]]; then
+  echo "Fixing permissions of /tmp for GitHub Codespaces..." >&2
+  sudo chmod 1777 /tmp
+fi
+
+set -x
+
+exec yarn install
