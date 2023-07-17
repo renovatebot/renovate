7337c874a30593daf5ef8c2bb36b85c0a7751797
Birger J. Nordølum <contact@mindtooth.no>
2023-07-18 00:32:06 +0200

    feat(presets): add rollup preset

--- lib/config/presets/internal/monorepo.ts
+++ lib/config/presets/internal/monorepo.ts
@@ -215,6 +215,10 @@ const repoGroups = {
   remark: 'https://github.com/remarkjs/remark',
   remix: 'https://github.com/remix-run/remix',
   rjsf: 'https://github.com/rjsf-team/react-jsonschema-form',
+  rollup: [
+    'https://github.com/rollup/rollup',
+    'https://github.com/rollup/plugins',
+  ],
   router5: 'https://github.com/router5/router5',
   'rust-futures': 'https://github.com/rust-lang/futures-rs',
   'rust-wasm-bindgen': 'https://github.com/rustwasm/wasm-bindgen',
