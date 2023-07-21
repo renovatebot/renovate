665a4aa06654bbf3e3c5502135621f6aed2d7a55
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-07-21 19:13:54 +0300

    Refactor

--- lib/modules/datasource/rubygems/metadata-cache.ts
+++ lib/modules/datasource/rubygems/metadata-cache.ts
@@ -1,6 +1,7 @@
 import hasha from 'hasha';
 import * as packageCache from '../../../util/cache/package';
 import type { Http } from '../../../util/http';
+import { AsyncResult, Result } from '../../../util/result';
 import { parseUrl } from '../../../util/url';
 import type { ReleaseResult } from '../types';
 import { getV1Releases } from './common';
@@ -18,33 +19,44 @@ export class MetadataCache {
     packageName: string,
     versions: string[]
   ): Promise<ReleaseResult> {
-    const hash = hasha(versions, { algorithm: 'sha256' });
     const cacheNs = `datasource-rubygems`;
     const cacheKey = `metadata-cache:${registryUrl}:${packageName}`;
-    const oldCache = await packageCache.get<CacheRecord>(cacheNs, cacheKey);
-    if (oldCache?.hash === hash) {
-      return oldCache.data;
-    }
+    const hash = hasha(versions, { algorithm: 'sha256' });
 
-    return getV1Releases(this.http, registryUrl, packageName)
-      .transform(async (result) => {
-        const registryHostname = parseUrl(registryUrl)?.hostname;
-        if (registryHostname === 'rubygems.org') {
-          const newCache: CacheRecord = { hash, data: result };
-          const ttlMinutes = 100 * 24 * 60;
-          const ttlRandomDelta = Math.floor(Math.random() * 10 * 24 * 60);
-          await packageCache.set(
-            cacheNs,
-            cacheKey,
-            newCache,
-            ttlMinutes + ttlRandomDelta
-          );
-        }
-
-        return result;
-      })
-      .unwrap({
-        releases: versions.map((version) => ({ version })),
+    const loadCache = (): AsyncResult<ReleaseResult, unknown> =>
+      Result.wrapNullable(
+        packageCache.get<CacheRecord>(cacheNs, cacheKey),
+        'cache-not-found'
+      ).transform((cache) => {
+        return hash === cache.hash
+          ? Result.ok(cache.data)
+          : Result.err('cache-outdated');
       });
+
+    const saveCache = async (result: ReleaseResult): Promise<ReleaseResult> => {
+      const registryHostname = parseUrl(registryUrl)?.hostname;
+      if (registryHostname === 'rubygems.org') {
+        const newCache: CacheRecord = { hash, data: result };
+        const ttlMinutes = 100 * 24 * 60;
+        const ttlRandomDelta = Math.floor(Math.random() * 10 * 24 * 60);
+        await packageCache.set(
+          cacheNs,
+          cacheKey,
+          newCache,
+          ttlMinutes + ttlRandomDelta
+        );
+      }
+
+      return result;
+    };
+
+    return await loadCache()
+      .catch(() =>
+        getV1Releases(this.http, registryUrl, packageName).transform(saveCache)
+      )
+      .catch(() =>
+        Result.ok({ releases: versions.map((version) => ({ version })) })
+      )
+      .unwrapOrThrow();
   }
 }
