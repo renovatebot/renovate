9de151fb4ab50af1d7dfde5d6d1cec76168753ef
Adam Setch <adam.setch@outlook.com>
2023-07-23 07:11:27 +1000

    feat: add minimatch wrapper to support caching

--- lib/util/minimatch.spec.ts
+++ lib/util/minimatch.spec.ts
@@ -1,10 +1,6 @@
 import { minimatch } from './minimatch';
 
 describe('util/minimatch', () => {
-  beforeEach(() => {
-    jest.resetModules();
-  });
-
   it('caches minimatch', () => {
     expect(minimatch('foo')).toBe(minimatch('foo'));
   });
