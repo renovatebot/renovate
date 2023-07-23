bbb334ebf1313ec26208e94dec6515ff6ed380ad
Adam Setch <adam.setch@outlook.com>
2023-07-24 06:47:58 +1000

    feat: add hash util

---
+++ lib/util/hash.spec.ts
@@ -0,0 +1,9 @@
+import { hash } from './hash';
+
+describe('util/hash', () => {
+  test('should hash data with sha256', () => {
+    expect(hash('https://example.com/test.txt', 'sha256')).toBe(
+      'd1dc63218c42abba594fff6450457dc8c4bfdd7c22acf835a50ca0e5d2693020'
+    );
+  });
+});
---
+++ lib/util/hash.ts
@@ -0,0 +1,13 @@
+import crypto from 'node:crypto';
+import type { LiteralUnion } from 'type-fest';
+
+export type AlgorithmName = LiteralUnion<
+  'sha1' | 'sha224' | 'sha256' | 'sha384' | 'sha512',
+  string
+>;
+
+export function hash(data: string | Buffer, algorithm: AlgorithmName): string {
+  const hash = crypto.createHash(algorithm);
+  hash.update(data);
+  return hash.digest('hex');
+}
