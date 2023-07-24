e24ca0eb3d57067c1a81c6cb018b769c9b57f070
Adam Setch <adam.setch@outlook.com>
2023-07-24 16:07:52 -0400

    feat: add minimatch wrapper to support caching (#23515)

---
+++ lib/util/minimatch.spec.ts
@@ -0,0 +1,18 @@
+import { minimatch } from './minimatch';
+
+describe('util/minimatch', () => {
+  it('caches minimatch', () => {
+    expect(minimatch('foo')).toBe(minimatch('foo'));
+    expect(minimatch('foo', { dot: true })).toBe(
+      minimatch('foo', { dot: true })
+    );
+  });
+
+  it('does not cache minimatch', () => {
+    expect(minimatch('foo', undefined, false)).not.toBe(
+      minimatch('foo', undefined, false)
+    );
+    expect(minimatch('foo')).not.toBe(minimatch('foo', undefined, false));
+    expect(minimatch('foo', { dot: true })).not.toBe(minimatch('foo'));
+  });
+});
