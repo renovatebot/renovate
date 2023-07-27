0ed22ffe95a2331025bc1153ab9b5d860759c338
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-07-28 00:23:41 +0300

    log

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+echo "1"
+
 set -e
 
+echo "2"
+
 if [[ "${CODESPACES}" == true ]]; then
+  echo "3"
+
   echo "Fixing permissions of /tmp for GitHub Codespaces..." >&2
+
+  echo "4"
+
   sudo chmod 1777 /tmp
+
+  echo "5"
 fi
+
+echo "6"
 
 set -x
 
+echo "7"
+
 exec yarn install
+
+echo "8"
