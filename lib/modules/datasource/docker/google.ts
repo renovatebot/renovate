595b6591144e21430de20153251a7ef8dec8c2a2
kvanzuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2023-08-18 08:15:32 +0200

    Made regex more strict

--- lib/modules/datasource/docker/google.ts
+++ lib/modules/datasource/docker/google.ts
@@ -3,7 +3,7 @@ import { logger } from '../../../logger';
 import { regEx } from '../../../util/regex';
 import { addSecretForSanitizing } from '../../../util/sanitize';
 
-export const googleRegex = regEx(/([a-z-]+\.)?(gcr.io|pkg.dev)/);
+export const googleRegex = regEx(/(((eu|us|asia)\.)?gcr\.io|[a-z0-9-]+-docker\.pkg\.dev)/);
 
 export async function getGoogleAccessToken(): Promise<string | null> {
   const googleAuth: GoogleAuth = new GoogleAuth({
