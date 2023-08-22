6f3721e6b154e789b89b7f7649a1741188927a2b
RahulGautamSingh <rahultesnik@gmail.com>
2023-08-22 17:49:34 +0545

    fix: update logic to add missing custom manager readme (#24018)

--- lib/modules/manager/custom/index.ts
+++ lib/modules/manager/custom/index.ts
@@ -5,5 +5,5 @@ export const customManagerList = Array.from(customManagers.keys());
 export const getCustomManagers = (): Map<string, ManagerApi> => customManagers;
 
 export function isCustomManager(manager: string): boolean {
-  return !!customManagers.has(manager);
+  return customManagers.has(manager);
 }
