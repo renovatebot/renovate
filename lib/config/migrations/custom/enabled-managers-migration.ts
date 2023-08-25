20a26e1efd5d5e9b42c6f209dd3986a52ec881aa
RahulGautamSingh <rahultesnik@gmail.com>
2023-08-26 02:34:23 +0545

    revert early return

--- lib/config/migrations/custom/enabled-managers-migration.ts
+++ lib/config/migrations/custom/enabled-managers-migration.ts
@@ -6,21 +6,19 @@ export class EnabledManagersMigration extends AbstractMigration {
   override readonly propertyName = 'enabledManagers';
 
   override run(value: unknown): void {
-    if (!is.array<string>(value, is.string)) {
-      return;
+    if (is.array<string>(value, is.string)) {
+      const newValue = value.map((manager) => {
+        if (manager === 'yarn') {
+          return 'npm';
+        }
+
+        if (isCustomManager(manager)) {
+          return `custom.${manager}`;
+        }
+
+        return manager;
+      });
+      this.rewrite(newValue);
     }
-
-    const newValue = value.map((manager) => {
-      if (manager === 'yarn') {
-        return 'npm';
-      }
-
-      if (isCustomManager(manager)) {
-        return `custom.${manager}`;
-      }
-
-      return manager;
-    });
-    this.rewrite(newValue);
   }
 }
