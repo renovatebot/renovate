95d3a1db8860f8be97374ca9a29ba1a9191f9367
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-08-25 10:41:07 +0300

    refactor(poetry): Use transforms for dependencies parsing (#23965)

--- lib/modules/manager/poetry/schema.ts
+++ lib/modules/manager/poetry/schema.ts
@@ -1,17 +1,152 @@
 import { z } from 'zod';
+import { parseGitUrl } from '../../../util/git/url';
+import { regEx } from '../../../util/regex';
 import { LooseArray, LooseRecord, Toml } from '../../../util/schema-utils';
+import { GitRefsDatasource } from '../../datasource/git-refs';
+import { GithubTagsDatasource } from '../../datasource/github-tags';
+import { PypiDatasource } from '../../datasource/pypi';
+import * as pep440Versioning from '../../versioning/pep440';
+import * as poetryVersioning from '../../versioning/poetry';
+import type { PackageDependency } from '../types';
 
-const PoetryDependencySchema = z.object({
-  path: z.string().optional(),
-  git: z.string().optional(),
-  tag: z.string().optional(),
-  version: z.string().optional(),
-});
+const PoetryPathDependency = z
+  .object({
+    path: z.string(),
+    version: z.string().optional().catch(undefined),
+  })
+  .transform(({ version }): PackageDependency => {
+    const dep: PackageDependency = {
+      datasource: PypiDatasource.id,
+      skipReason: 'path-dependency',
+    };
+
+    if (version) {
+      dep.currentValue = version;
+    }
+
+    return dep;
+  });
+
+const PoetryGitDependency = z
+  .object({
+    git: z.string(),
+    tag: z.string().optional().catch(undefined),
+    version: z.string().optional().catch(undefined),
+  })
+  .transform(({ git, tag, version }): PackageDependency => {
+    if (!tag) {
+      const res: PackageDependency = {
+        datasource: GitRefsDatasource.id,
+        packageName: git,
+        skipReason: 'git-dependency',
+      };
+
+      if (version) {
+        res.currentValue = version;
+      }
+
+      return res;
+    }
+
+    const parsedUrl = parseGitUrl(git);
+    if (parsedUrl.source !== 'github.com') {
+      return {
+        datasource: GitRefsDatasource.id,
+        currentValue: tag,
+        packageName: git,
+        skipReason: 'git-dependency',
+      };
+    }
+
+    const { owner, name } = parsedUrl;
+    const repo = `${owner}/${name}`;
+    return {
+      datasource: GithubTagsDatasource.id,
+      currentValue: tag,
+      packageName: repo,
+    };
+  });
+
+const PoetryPypiDependency = z.union([
+  z
+    .object({ version: z.string().optional() })
+    .transform(({ version: currentValue }): PackageDependency => {
+      if (!currentValue) {
+        return { datasource: PypiDatasource.id };
+      }
+
+      return {
+        datasource: PypiDatasource.id,
+        managerData: { nestedVersion: true },
+        currentValue,
+      };
+    }),
+  z.string().transform(
+    (version): PackageDependency => ({
+      datasource: PypiDatasource.id,
+      currentValue: version,
+      managerData: { nestedVersion: false },
+    })
+  ),
+]);
+
+const PoetryDependencySchema = z.union([
+  PoetryPathDependency,
+  PoetryGitDependency,
+  PoetryPypiDependency,
+]);
+
+const PoetryArraySchema = z.array(z.unknown()).transform(
+  (): PackageDependency => ({
+    datasource: PypiDatasource.id,
+    skipReason: 'multiple-constraint-dep',
+  })
+);
+
+const PoetryValue = z.union([PoetryDependencySchema, PoetryArraySchema]);
+type PoetryValue = z.infer<typeof PoetryValue>;
 
 export const PoetryDependencyRecord = LooseRecord(
   z.string(),
-  z.union([PoetryDependencySchema, z.array(PoetryDependencySchema), z.string()])
-);
+  PoetryValue.transform((dep) => {
+    if (dep.skipReason) {
+      return dep;
+    }
+
+    // istanbul ignore if: normaly should not happen
+    if (!dep.currentValue) {
+      dep.skipReason = 'unspecified-version';
+      return dep;
+    }
+
+    if (pep440Versioning.isValid(dep.currentValue)) {
+      dep.versioning = pep440Versioning.id;
+      return dep;
+    }
+
+    if (poetryVersioning.isValid(dep.currentValue)) {
+      dep.versioning = poetryVersioning.id;
+      return dep;
+    }
+
+    dep.skipReason = 'invalid-version';
+    return dep;
+  })
+).transform((record) => {
+  for (const [depName, dep] of Object.entries(record)) {
+    dep.depName = depName;
+    if (!dep.packageName) {
+      const pep503NormalizeRegex = regEx(/[-_.]+/g);
+      const packageName = depName
+        .toLowerCase()
+        .replace(pep503NormalizeRegex, '-');
+      if (depName !== packageName) {
+        dep.packageName = packageName;
+      }
+    }
+  }
+  return record;
+});
 
 export type PoetryDependencyRecord = z.infer<typeof PoetryDependencyRecord>;
 
