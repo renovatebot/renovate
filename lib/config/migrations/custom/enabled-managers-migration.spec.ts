2b12118f2effb883cb5384cda3a98bf6170b5130
RahulGautamSingh <rahultesnik@gmail.com>
2023-08-27 18:44:57 +0545

    Update lib/config/migrations/custom/enabled-managers-migration.spec.ts

--- lib/config/migrations/custom/enabled-managers-migration.spec.ts
+++ lib/config/migrations/custom/enabled-managers-migration.spec.ts
@@ -1,7 +1,7 @@
 import { EnabledManagersMigration } from './enabled-managers-migration';
 
 describe('config/migrations/custom/enabled-managers-migration', () => {
-  it('should replace yarn with npm', () => {
+  it('migrates', () => {
     expect(EnabledManagersMigration).toMigrate(
       {
         enabledManagers: ['test1', 'yarn', 'test2', 'regex'],
