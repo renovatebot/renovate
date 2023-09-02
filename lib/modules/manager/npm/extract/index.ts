da6706845d02edbc716388639f9cd925d3a38afb
Rhys Arkins <rhys@arkins.net>
2023-09-02 09:14:04 +0200

    postExtractPackageFile

--- lib/modules/manager/npm/extract/index.ts
+++ lib/modules/manager/npm/extract/index.ts
@@ -39,7 +39,54 @@ export async function extractPackageFile(
   if (!res) {
     return res;
   }
+  await postExtractPackageFile(packageFile, config, res);
   return res;
+}
+
+export async function postExtractPackageFile(
+  packageFile: string,
+  config: ExtractConfig,
+  res: PackageFileContent<NpmManagerData>
+): Promise<void> {
+  const lockFiles: NpmLockFiles = {
+    packageLock: 'package-lock.json',
+    shrinkwrapJson: 'npm-shrinkwrap.json',
+  };
+
+  for (const [key, val] of Object.entries(lockFiles) as [
+    'packageLock' | 'shrinkwrapJson',
+    string
+  ][]) {
+    const filePath = getSiblingFileName(packageFile, val);
+    if (await readLocalFile(filePath, 'utf8')) {
+      lockFiles[key] = filePath;
+    } else {
+      lockFiles[key] = undefined;
+    }
+  }
+  lockFiles.npmLock = lockFiles.packageLock ?? lockFiles.shrinkwrapJson;
+  delete lockFiles.packageLock;
+
+  const hasFancyRefs = res.deps.some(
+    (dep) => dep.npmPackageAlias ?? dep.currentValue?.startsWith('file:')
+  );
+
+  let skipInstalls = config.skipInstalls;
+  if (skipInstalls === null) {
+    if (hasFancyRefs && lockFiles.npmLock) {
+      // https://github.com/npm/cli/issues/1432
+      // Explanation:
+      //  - npm install --package-lock-only is buggy for transitive deps in file: and npm: references
+      //  - So we set skipInstalls to false if file: or npm: refs are found *and* the user hasn't explicitly set the value already
+      //  - Also, do not skip install if Yarn zero-install is used
+      logger.debug('Automatically setting skipInstalls to false');
+      skipInstalls = false;
+    } else {
+      skipInstalls = true;
+    }
+  }
+  res.skipInstalls = skipInstalls;
+  res.managerData = { ...lockFiles, ...res.managerData };
 }
 
 export async function extractPackageJson(
@@ -71,25 +118,6 @@ export async function extractPackageJson(
   } else {
     workspacesPackages = packageJson.workspaces?.packages;
   }
-
-  const lockFiles: NpmLockFiles = {
-    packageLock: 'package-lock.json',
-    shrinkwrapJson: 'npm-shrinkwrap.json',
-  };
-
-  for (const [key, val] of Object.entries(lockFiles) as [
-    'packageLock' | 'shrinkwrapJson',
-    string
-  ][]) {
-    const filePath = getSiblingFileName(packageFile, val);
-    if (await readLocalFile(filePath, 'utf8')) {
-      lockFiles[key] = filePath;
-    } else {
-      lockFiles[key] = undefined;
-    }
-  }
-  lockFiles.npmLock = lockFiles.packageLock ?? lockFiles.shrinkwrapJson;
-  delete lockFiles.packageLock;
 
   let npmrc: string | undefined;
   const npmrcFileName = getSiblingFileName(packageFile, '.npmrc');
@@ -215,38 +243,18 @@ export async function extractPackageJson(
       extractedConstraints[engine] = constraint.currentValue;
     }
   }
-  const hasFancyRefs = deps.some(
-    (dep) => dep.npmPackageAlias ?? dep.currentValue?.startsWith('file:')
-  );
-
-  let skipInstalls = config.skipInstalls;
-  if (skipInstalls === null) {
-    if (hasFancyRefs && lockFiles.npmLock) {
-      // https://github.com/npm/cli/issues/1432
-      // Explanation:
-      //  - npm install --package-lock-only is buggy for transitive deps in file: and npm: references
-      //  - So we set skipInstalls to false if file: or npm: refs are found *and* the user hasn't explicitly set the value already
-      //  - Also, do not skip install if Yarn zero-install is used
-      logger.debug('Automatically setting skipInstalls to false');
-      skipInstalls = false;
-    } else {
-      skipInstalls = true;
-    }
-  }
 
   return {
     deps,
     packageFileVersion,
     npmrc,
     managerData: {
-      ...lockFiles,
       packageJsonName,
       hasPackageManager: is.nonEmptyStringAndNotWhitespace(
         packageJson.packageManager
       ),
       workspacesPackages,
     },
-    skipInstalls,
     extractedConstraints,
   };
 }
