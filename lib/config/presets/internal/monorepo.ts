07fbeeade481a919fbe624572eebee5d5f1b177b
Ілля Мазуряк <illay1994@gmail.com>
2023-09-11 22:15:07 +0200

    Update monorepo.ts

--- lib/config/presets/internal/monorepo.ts
+++ lib/config/presets/internal/monorepo.ts
@@ -34,7 +34,8 @@ const repoGroups = {
   'arcus.webapi': 'https://github.com/arcus-azure/arcus.webapi',
   'aspnet aspnetwebstack': 'https://github.com/aspnet/AspNetWebStack',
   'aspnet extensions': 'https://github.com/aspnet/Extensions',
-  'aspnet health checks': 'https://github.com/xabaril/AspNetCore.Diagnostics.HealthChecks',
+  'aspnet health checks':
+    'https://github.com/xabaril/AspNetCore.Diagnostics.HealthChecks',
   'aspnet-api-versioning': 'https://github.com/Microsoft/aspnet-api-versioning',
   'automapper-dotnet': [
     'https://github.com/AutoMapper/AutoMapper',
