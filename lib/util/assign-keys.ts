c1aa7f6938eee6dac2062b60262a29e5b5883020
RahulGautamSingh <rahultesnik@gmail.com>
2023-09-13 02:55:32 +0545

    Update lib/util/assign-keys.ts

--- lib/util/assign-keys.ts
+++ lib/util/assign-keys.ts
@@ -5,7 +5,7 @@ import is from '@sindresorhus/is';
  */
 export function assignKeys<
   Left extends { [key in K]?: unknown },
-  Right extends { [key in K]?: Left[K] },
+  Right extends { [key in K]?: Left[key] },
   K extends keyof Right
 >(left: Left, right: Right, keys: K[]): Left {
   for (const key of keys) {
