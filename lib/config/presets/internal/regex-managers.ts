e1d294d96ea7a64dd1cb96052d5570fbcfcc8d77
Robert Munteanu <robert@lmn.ro>
2023-09-14 17:44:20 +0200

    feat: mavenPropertyVersions regexManager

--- lib/config/presets/internal/regex-managers.ts
+++ lib/config/presets/internal/regex-managers.ts
@@ -44,6 +44,21 @@ export const presets: Record<string, Preset> = {
       },
     ],
   },
+  mavenPropertyVersions: {
+    description: 'Update `*.version` properties in `pom.xml` files.',
+    regexManagers: [
+      {
+        customType: 'regex',
+        datasourceTemplate:
+          '{{#if datasource}}{{{datasource}}}{{else}}maven{{/if}}',
+        fileMatch: ['^pom\\.xml$'],
+        matchStrings: [
+          '<!--\\s?renovate:( datasource=(?<datasource>.*?))? depName=(?<depName>.*?)\\s?(?: (?:lookupName|packageName)=(?<packageName>[^\\s]+?))?(?: versioning=(?<versioning>[a-z-0-9]+?))?\\s+-->\\s+<.*?\\.version>(?<currentValue>.*?)<\\/.*?\\.version>',
+        ],
+        versioningTemplate: '{{#if versioning}}{{{versioning}}}{{/if}}',
+      },
+    ],
+  },
   tfvarsVersions: {
     description: 'Update `*_version` variables in `.tfvars` files.',
     regexManagers: [
