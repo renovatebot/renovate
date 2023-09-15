b5f8c076d93b19d889e5fcb37ee71fdcb52fcae9
RahulGautamSingh <rahultesnik@gmail.com>
2023-09-15 08:11:15 +0545

    fix tests

--- lib/workers/repository/update/branch/index.ts
+++ lib/workers/repository/update/branch/index.ts
@@ -853,7 +853,8 @@ export async function processBranch(
       } else if (config.automerge) {
         logger.debug('PR is configured for automerge');
         // skip automerge if there is a new commit since status checks aren't done yet
-        if (config.ignoreTests ?? !commitSha) {
+        /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+        if (config.ignoreTests || !commitSha) {
           logger.debug('checking auto-merge');
           const prAutomergeResult = await checkAutoMerge(pr, config);
           if (prAutomergeResult?.automerged) {
