9624637f2b15e5e5119fdf0b925f6d80e335b347
Alessandro Lai <alessandro.lai85@gmail.com>
2023-10-06 09:21:41 +0200

    Add PHP linters to preset

--- lib/config/presets/internal/packages.ts
+++ lib/config/presets/internal/packages.ts
@@ -85,6 +85,7 @@ export const presets: Record<string, Preset> = {
     extends: [
       'packages:emberTemplateLint',
       'packages:eslint',
+      'packages:phpLinters',
       'packages:stylelint',
       'packages:tslint',
     ],
@@ -94,6 +95,10 @@ export const presets: Record<string, Preset> = {
     description: 'All Mapbox-related packages.',
     matchPackagePrefixes: ['leaflet', 'mapbox'],
   },
+  phpLinters: {
+    description: 'All PHP lint-related packages.',
+    matchPackageNames: ['friendsofphp/php-cs-fixer', 'squizlabs/php_codesniffer', 'symplify/easy-coding-standard'],
+  }
   postcss: {
     description: 'All PostCSS packages.',
     matchPackageNames: ['postcss'],
