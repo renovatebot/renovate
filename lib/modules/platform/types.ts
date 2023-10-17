c2cb93ce44aa3319dff703f2041e47bfe033c1f4
Alex Kessock <akessock@gmail.com>
2023-10-17 02:45:53 -0600

    feat: compare all branch authors when deciding if a branch is modified (#21558)
    Co-authored-by: Rhys Arkins <rhys@arkins.net>

--- lib/modules/platform/types.ts
+++ lib/modules/platform/types.ts
@@ -233,7 +233,7 @@ export interface Platform {
 
 export interface PlatformScm {
   isBranchBehindBase(branchName: string, baseBranch: string): Promise<boolean>;
-  isBranchModified(branchName: string): Promise<boolean>;
+  isBranchModified(branchName: string, baseBranch?: string): Promise<boolean>;
   isBranchConflicted(baseBranch: string, branch: string): Promise<boolean>;
   branchExists(branchName: string): Promise<boolean>;
   getBranchCommit(branchName: string): Promise<CommitSha | null>;
--- lib/workers/repository/update/pr/automerge.ts
+++ lib/workers/repository/update/pr/automerge.ts
@@ -82,7 +82,7 @@ export async function checkAutoMerge(
     };
   }
   // Check if it's been touched
-  if (await scm.isBranchModified(branchName)) {
+  if (await scm.isBranchModified(branchName, config.baseBranch)) {
     logger.debug('PR is ready for automerge but has been modified');
     return {
       automerged: false,
