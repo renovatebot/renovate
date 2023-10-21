2b1354aff980bf3cd1ee5f62a6dae374afe49613
Simon Žlender <pub.git@zlender.si>
2023-10-21 15:13:31 +0200

    chore(cargo): fix update-lockfile comment

--- lib/modules/manager/cargo/artifacts.ts
+++ lib/modules/manager/cargo/artifacts.ts
@@ -44,7 +44,7 @@ async function cargoUpdatePrecise(
       ` --manifest-path ${quote(manifestPath)} --workspace`,
   ].concat(
     // Update individual dependencies to their `newVersion`. Necessary when
-    // using the `lockfile-update` rangeStrategy which doesn't touch Cargo.lock.
+    // using the `update-lockfile` rangeStrategy which doesn't touch Cargo.lock.
     updatedDeps
       .filter((dep) => !!dep.lockedVersion)
       .map(
