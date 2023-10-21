064447738f92e2077a80ba9d582a88117239fa85
HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
2023-10-21 10:20:10 +0200

    fix: monospace version in `ignoreOther` PR close comment

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2699,7 +2699,7 @@ const options: RenovateOptions[] = [
       ignoreDigest:
         'Because you closed this PR without merging, Renovate will ignore this update. You will not get PRs for the `{{{depName}}}` `{{{newDigestShort}}}` update again.',
       ignoreOther:
-        'Because you closed this PR without merging, Renovate will ignore this update ({{{newValue}}}). You will get a PR once a newer version is released. To ignore this dependency forever, add it to the `ignoreDeps` array of your Renovate config.',
+        'Because you closed this PR without merging, Renovate will ignore this update (`{{{newValue}}}`). You will get a PR once a newer version is released. To ignore this dependency forever, add it to the `ignoreDeps` array of your Renovate config.',
     },
   },
   {
