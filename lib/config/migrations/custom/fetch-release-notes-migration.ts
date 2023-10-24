4aecd91abd1d53b699fda98b6af9ff4eed30e693
Illia Mazuriak <illay1994@gmail.com>
2023-10-24 19:07:47 +0200

    Add new migration

--- lib/config/migrations/custom/fetch-release-notes-migration.ts
+++ lib/config/migrations/custom/fetch-release-notes-migration.ts
@@ -1,12 +1,21 @@
 import is from '@sindresorhus/is';
+import type { FetchChangeLogsOptions } from '../../types';
 import { AbstractMigration } from '../base/abstract-migration';
 
 export class FetchReleaseNotesMigration extends AbstractMigration {
+  override deprecated = true;
   override readonly propertyName = 'fetchReleaseNotes';
 
+  readonly newPropertyName = 'fetchChangeLogs';
+
   override run(value: unknown): void {
+    let newValue: FetchChangeLogsOptions | undefined;
     if (is.boolean(value)) {
-      this.rewrite(value ? 'pr' : 'off');
+      newValue = value ? 'pr' : 'off';
+    } else if (value === 'off' || value === 'pr' || value === 'branch') {
+      newValue = value;
     }
+
+    this.setSafely(this.newPropertyName, newValue);
   }
 }
