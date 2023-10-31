bcfe33f3d5cc710bd6e4adc30eede310916fefca
RahulGautamSingh <rahultesnik@gmail.com>
2023-10-31 09:17:21 +0545

    fix lint issues

--- lib/workers/repository/extract/extract-fingerprint-config.ts
+++ lib/workers/repository/extract/extract-fingerprint-config.ts
@@ -1,12 +1,6 @@
 import { getManagerConfig, mergeChildConfig } from '../../../config';
-import type {
-  RegexManagerTemplates,
-  RenovateConfig,
-} from '../../../config/types';
-import {
-  allManagersList,
-  getEnabledManagersList,
-} from '../../../modules/manager';
+import type { RenovateConfig } from '../../../config/types';
+import { getEnabledManagersList } from '../../../modules/manager';
 import { isCustomManager } from '../../../modules/manager/custom';
 import type { RegexManagerTemplates } from '../../../modules/manager/custom/regex/types';
 import { validMatchFields } from '../../../modules/manager/custom/regex/utils';
