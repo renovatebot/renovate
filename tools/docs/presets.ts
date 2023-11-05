b909414bd4469511d05d4e3465d55d37b4119d03
RahulGautamSingh <rahultesnik@gmail.com>
2023-11-05 09:00:44 +0545

    add edit-urls in meta of preset pages

--- tools/docs/presets.ts
+++ tools/docs/presets.ts
@@ -1,23 +1,54 @@
 import { groups as presetGroups } from '../../lib/config/presets/internal';
 import { logger } from '../../lib/logger';
+import { regEx } from '../../lib/util/regex';
 import { updateFile } from '../utils';
 
 function jsUcfirst(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+// matches preset names like: regexManagers and mergeConfidence
+const longPresetNameRegex = regEx(/(?<first>[a-z]+)(?<second>[A-Z][a-z]+)/);
+
+function getEditUrl(name: string): string {
+  const url =
+    'https://github.com/renovatebot/renovate/edit/main/lib/config/presets/internal/';
+
+  const groups = name.match(longPresetNameRegex);
+  if (!groups) {
+    return `${url}${name}.ts`;
+  }
+
+  return `${url}${groups[1]}-${groups[2].toLowerCase()}.ts`;
+  // switch (newName) {
+  //   case 'regexmanagers':
+  //     return `${url}regex-managers.ts`;
+  //   case 'mergeconfidence':
+  //     return `${url}merge-confidence.ts`;
+  //   case 'full config':
+  //     return `${url}config.ts`;
+  //   default:
+  //     return `${url}${newName}.ts`;
+  // }
+}
 
 /**
- * @param {string} name
+ * @param {string} presetTitle
  * @param {number} order
+ * @param {string} presetName
  */
-function generateFrontMatter(name: string, order: number): string {
+function generateFrontMatter(
+  presetTitle: string,
+  order: number,
+  presetName: string
+): string {
   return `---
 date: 2017-12-07
-title: ${name} Presets
+title: ${presetTitle} Presets
 categories:
     - config-presets
 type: Document
 order: ${order}
+edit_url: ${getEditUrl(presetName)}
 ---
 `;
 }
@@ -30,7 +61,7 @@ export async function generatePresets(dist: string): Promise<void> {
       .replace('Js', 'JS')
       .replace(/s$/, '')
       .replace(/^Config$/, 'Full Config');
-    const frontMatter = generateFrontMatter(formattedName, index);
+    const frontMatter = generateFrontMatter(formattedName, index, name);
     let content = `\n`;
     for (const [preset, value] of Object.entries(presetConfig)) {
       let header = `\n### ${name === 'default' ? '' : name}:${preset}`;
