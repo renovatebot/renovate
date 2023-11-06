d40e509ada8c295b5057d1a3cc3545830abb1b7a
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-11-06 04:08:56 -0300

    Update lib/util/toml.ts
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/util/toml.ts
+++ lib/util/toml.ts
@@ -1,7 +1,7 @@
 import { getStaticTOMLValue, parseTOML } from 'toml-eslint-parser';
 import type { JsonValue } from 'type-fest';
 
-export function parse(input: string): JsonValue {
+export function parse(input: string): unknown {
   const ast = parseTOML(input);
   return getStaticTOMLValue(ast) as JsonValue;
 }
