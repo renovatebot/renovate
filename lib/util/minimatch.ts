a8dac5362feb5754ef1747d49c6e4ab8afb377d7
RahulGautamSingh <rahultesnik@gmail.com>
2023-11-17 13:05:26 +0545

    add minimatchFilter fn

--- lib/util/minimatch.ts
+++ lib/util/minimatch.ts
@@ -22,3 +22,24 @@ export function minimatch(
   }
   return instance;
 }
+
+export function minimatchFilter(
+  pattern: string,
+  options?: MinimatchOptions,
+  useCache = true,
+): (fileName: string) => boolean {
+  const key = options ? `${pattern}:${JSON.stringify(options)}` : pattern;
+
+  if (useCache) {
+    const cachedResult = cache.get(key);
+    if (cachedResult) {
+      return (fileName) => cachedResult.match(fileName);
+    }
+  }
+
+  const instance = new Minimatch(pattern, options);
+  if (useCache) {
+    cache.set(key, instance);
+  }
+  return (fileName) => instance.match(fileName);
+}
