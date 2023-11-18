78a3429d8e1c1e78c025a680d520c4b561704d3c
frantic1048 <i@frantic1048.com>
2023-11-18 18:26:05 +0800

    style: format with prettier

--- lib/workers/repository/update/pr/changelog/gitlab/index.ts
+++ lib/workers/repository/update/pr/changelog/gitlab/index.ts
@@ -17,7 +17,7 @@ const http = new GitlabHttp(id);
 export async function getReleaseNotesMd(
   repository: string,
   apiBaseUrl: string,
-  sourceDirectory?: string
+  sourceDirectory?: string,
 ): Promise<ChangeLogFile | null> {
   logger.trace('gitlab.getReleaseNotesMd()');
   const urlEncodedRepo = encodeURIComponent(repository);
@@ -31,7 +31,7 @@ export async function getReleaseNotesMd(
       }`,
       {
         paginate: true,
-      }
+      },
     )
   ).body;
   const allFiles = tree.filter((f) => f.type === 'blob');
@@ -49,7 +49,7 @@ export async function getReleaseNotesMd(
   /* istanbul ignore if */
   if (files.length !== 0) {
     logger.debug(
-      `Multiple candidates for changelog file, using ${changelogFile}`
+      `Multiple candidates for changelog file, using ${changelogFile}`,
     );
   }
 
@@ -61,7 +61,7 @@ export async function getReleaseNotesMd(
 
 export async function getReleaseList(
   project: ChangeLogProject,
-  _release: ChangeLogRelease
+  _release: ChangeLogRelease,
 ): Promise<ChangeLogNotes[]> {
   logger.trace('gitlab.getReleaseNotesMd()');
   const apiBaseUrl = project.apiBaseUrl;
