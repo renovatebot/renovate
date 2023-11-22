c82b9afb364826f78b0817fc136295097e7815a4
Tom Vervoort <tv.vervoort@gmail.com>
2023-11-22 07:30:19 +0100

    refactor: strongly type commit SHAs (#25207)
    Co-authored-by: Rhys Arkins <rhys@arkins.net>

--- lib/modules/platform/github/index.ts
+++ lib/modules/platform/github/index.ts
@@ -828,7 +828,10 @@ export async function findPr({
 
 const REOPEN_THRESHOLD_MILLIS = 1000 * 60 * 60 * 24 * 7;
 
-async function ensureBranchSha(branchName: string, sha: string): Promise<void> {
+async function ensureBranchSha(
+  branchName: string,
+  sha: LongCommitSha,
+): Promise<void> {
   const repository = config.repository!;
   try {
     const commitUrl = `/repos/${repository}/git/commits/${sha}`;
@@ -1918,7 +1921,7 @@ async function pushFiles(
       `/repos/${config.repository}/git/commits`,
       { body: { message, tree: treeSha, parents: [parentCommitSha] } },
     );
-    const remoteCommitSha = commitRes.body.sha;
+    const remoteCommitSha = commitRes.body.sha as LongCommitSha;
     await ensureBranchSha(branchName, remoteCommitSha);
     return remoteCommitSha;
   } catch (err) {
