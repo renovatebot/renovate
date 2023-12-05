17a75ac3568c0e228d378fbd184fcae4c301d679
Patrick Hobusch <patrick@hobusch.net>
2023-12-05 17:19:36 +0800

    fix(helmfile): just a small test thing

--- lib/modules/manager/helmfile/extract.spec.ts
+++ lib/modules/manager/helmfile/extract.spec.ts
@@ -1,3 +1,4 @@
+import { codeBlock } from 'common-tags';
 import { Fixtures } from '../../../../test/fixtures';
 import { fs } from '../../../../test/util';
 import { GlobalConfig } from '../../../config/global';
@@ -15,9 +16,9 @@ describe('modules/manager/helmfile/extract', () => {
     });
 
     it('skip null YAML document', async () => {
-      const content = `
-      ~
-      `;
+      const content = codeBlock`
+        ~
+        `;
       const fileName = 'helmfile.yaml';
       const result = await extractPackageFile(content, fileName, {
         registryAliases: {
