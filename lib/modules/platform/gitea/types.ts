a2672ef7222d1006d888118e10d85927acf2f903
Michael Kriese <michael.kriese@visualon.de>
2023-12-05 12:27:30 +0100

    fix(gitea): check for disabled issues (#26134)

--- lib/modules/platform/gitea/types.ts
+++ lib/modules/platform/gitea/types.ts
@@ -64,6 +64,7 @@ export interface Repo {
   allow_squash_merge: boolean;
   archived: boolean;
   clone_url?: string;
+  has_issues: boolean;
   ssh_url?: string;
   default_branch: string;
   empty: boolean;
