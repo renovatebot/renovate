c443aec1cf0347befd3ebbdbf22ad314772e36de
Tobias Bieniek <tobias@bieniek.cloud>
2023-12-06 10:59:04 +0100

    fix(presets): update smithy-rs monorepo (#26152)

--- lib/config/presets/internal/monorepo.ts
+++ lib/config/presets/internal/monorepo.ts
@@ -51,6 +51,7 @@ const repoGroups = {
   'aws-sdk-js-v3': 'https://github.com/aws/aws-sdk-js-v3',
   'aws-sdk-net': 'https://github.com/aws/aws-sdk-net',
   'aws-sdk-rust': [
+    'https://github.com/smithy-lang/smithy-rs',
     'https://github.com/awslabs/smithy-rs',
     'https://github.com/awslabs/aws-sdk-rust',
   ],
