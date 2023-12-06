a5542b6204c5bdb7a2c8883591e9a8b0041b51b0
Michael Kriese <michael.kriese@visualon.de>
2023-12-06 10:37:36 +0100

    feat(datasource/jenkins-plugins): support constraints filtering

--- lib/modules/datasource/jenkins-plugins/types.ts
+++ lib/modules/datasource/jenkins-plugins/types.ts
@@ -7,6 +7,9 @@ export interface JenkinsPluginVersion {
   version: string;
   buildDate?: string;
   url?: string;
+  requiredCore?: string;
+
+  releaseTimestamp?: string;
 }
 
 export interface JenkinsPluginsInfoResponse {
