3fb9e5dfe20f296ce961f433f7990fa76a85530d
Koen van Zuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2024-01-08 17:32:39 +0100

    Apply suggestions from code review
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/modules/manager/types.ts
+++ lib/modules/manager/types.ts
@@ -235,7 +235,7 @@ export interface ManagerApi extends ModuleApi {
     content: string,
     currentValue: string,
     bumpVersion: ReleaseType,
-    packageFile?: string,
+    packageFile: string,
   ): Result<BumpPackageVersionResult>;
 
   detectGlobalConfig?(): Result<GlobalManagerConfig>;
