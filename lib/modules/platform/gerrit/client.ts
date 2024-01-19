renovatebot/renovate

commit faa1618f2d32bb6e0e1d450881754b83995d33f9

fix(gerrit): include prTitle in search for changes (findPr) (#26745)

lib/modules/platform/gerrit/client.ts
@@ -234,6 +234,9 @@ class GerritClient {
     if (searchConfig.label) {
       filters.push(`label:Code-Review=${searchConfig.label}`);
     }
+    if (searchConfig.prTitle) {
+      filters.push(`message:${encodeURIComponent(searchConfig.prTitle)}`);
+    }
     return filters;
   }
 }

lib/modules/platform/gerrit/client.spec.ts
@@ -111,6 +111,13 @@ describe('modules/platform/gerrit/client', () => {
           state: 'closed' as FindPRConfig['state'],
         },
       ],
+      [
+        'message:fix(deps): update dependency react-router-dom to v6.21.2',
+        {
+          branchName: 'dependency-xyz',
+          prTitle: 'fix(deps): update dependency react-router-dom to v6.21.2',
+        },
+      ],
     ])(
       'query contains %p',
       async (expectedQueryPart: string, config: GerritFindPRConfig) => {

commit d2a38e11ff5d1e3c7d788c0d9357e51c241a64e0

fix(gerrit): quote the prTitle in search for changes (findPr)
Fixes regression introduced in #26745

lib/modules/platform/gerrit/client.ts
@@ -235,7 +235,9 @@ class GerritClient {
       filters.push(`label:Code-Review=${searchConfig.label}`);
     }
     if (searchConfig.prTitle) {
-      filters.push(`message:${encodeURIComponent(searchConfig.prTitle)}`);
+      filters.push(
+        `message:${encodeURIComponent('"' + searchConfig.prTitle + '"')}`,
+      );
     }
     return filters;
   }

lib/modules/platform/gerrit/client.spec.ts
@@ -112,7 +112,7 @@ describe('modules/platform/gerrit/client', () => {
         },
       ],
       [
-        'message:fix(deps): update dependency react-router-dom to v6.21.2',
+        'message:"fix(deps): update dependency react-router-dom to v6.21.2"',
         {
           branchName: 'dependency-xyz',
           prTitle: 'fix(deps): update dependency react-router-dom to v6.21.2',