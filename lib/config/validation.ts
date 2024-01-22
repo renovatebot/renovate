92d95b9995a568266ab6bb3d223d09327b933f15
RahulGautamSingh <rahultesnik@gmail.com>
2024-01-22 07:39:59 +0545

    Update lib/config/validation.ts
    Co-authored-by: Rhys Arkins <rhys@arkins.net>

--- lib/config/validation.ts
+++ lib/config/validation.ts
@@ -230,7 +230,7 @@ export async function validateConfig(
         // TODO: types (#22198)
         const message = `${key} should only be configured within one of "${optionParents[
           key
-        ]?.join(',')}" objects. Was found in ${parentName}`;
+        ]?.join(' or ')}" objects. Was found in ${parentName}`;
         warnings.push({
           topic: `${parentPath ? `${parentPath}.` : ''}${key}`,
           message,
