09fc354e62a3096cb0566c551f1f9c3d73fed332
Lakshmipriya Sivakumar <lpriya.sivakumar@gmail.com>
2024-02-04 19:27:45 -0500

    Apply suggestions from code review
    Co-authored-by: Johannes Feichtner <343448+Churro@users.noreply.github.com>

--- lib/modules/manager/maven/extract.spec.ts
+++ lib/modules/manager/maven/extract.spec.ts
@@ -176,14 +176,14 @@ describe('modules/manager/maven/extract', () => {
     });
 
     it('extract registries from a settings file that uses updated schema', () => {
-      const settingsUpatedContent = codeBlock`
+      const settingsUpdatedContent = codeBlock`
         <settings xmlns="http://maven.apache.org/SETTINGS/1.2.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
           xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.2.0 http://maven.apache.org/xsd/settings-1.2.0.xsd">
           <mirrors>
                 <mirror>
                     <id>Test-Internal-repository</id>
                     <name>Proxy Repository Manager</name>
-                    <url>https:/proxy-repo.com/artifactory/apache-maven</url>
+                    <url>https://proxy-repo.com/artifactory/apache-maven</url>
                     <mirrorOf>central</mirrorOf>
                 </mirror>
             </mirrors>
@@ -191,9 +191,9 @@ describe('modules/manager/maven/extract', () => {
           <activeProfiles/>
         </settings>
         `;
-      const res = extractRegistries(settingsUpatedContent);
+      const res = extractRegistries(settingsUpdatedContent);
       expect(res).toStrictEqual([
-        'https:/proxy-repo.com/artifactory/apache-maven',
+        'https://proxy-repo.com/artifactory/apache-maven',
       ]);
     });
   });
