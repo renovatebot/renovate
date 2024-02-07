2c78a5046ed1b3dd6f98a7ad2ac1afe7c268f2fc
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-02-07 20:45:27 -0300

    Simplify implementation

--- lib/logger/remap.ts
+++ lib/logger/remap.ts
@@ -1,42 +1,17 @@
 import type { LogLevelString } from 'bunyan';
-import { re2 } from '../expose.cjs';
-import { minimatch } from '../util/minimatch';
+import {
+  StringMatchPredicate,
+  makeRegexOrMinimatchPredicate,
+} from '../util/string-match';
 import type { LogLevelRemap } from './types';
 
-let RegEx: RegExpConstructor;
-if (!process.env.RENOVATE_X_IGNORE_RE2) {
-  try {
-    const RE2 = re2();
-    // Test if native is working
-    new RE2('.*').exec('test');
-    RegEx = RE2;
-  } catch (err) {
-    RegEx = RegExp;
-  }
-}
-
-let matcherCache = new WeakMap<LogLevelRemap, (input: string) => boolean>();
+let matcherCache = new WeakMap<LogLevelRemap, StringMatchPredicate>();
 
 function match(remap: LogLevelRemap, input: string): boolean {
   const { matchMessage: pattern } = remap;
   let matchFn = matcherCache.get(remap);
   if (!matchFn) {
-    if (
-      pattern.length > 2 &&
-      pattern.startsWith('/') &&
-      pattern.endsWith('/')
-    ) {
-      try {
-        const regex = new RegEx(pattern.slice(1, -1));
-        matchFn = (input) => regex.test(input);
-      } catch (err) {
-        matchFn = () => false;
-      }
-    } else {
-      const mm = minimatch(pattern, { dot: true });
-      matchFn = (input) => mm.match(input);
-    }
-
+    matchFn = makeRegexOrMinimatchPredicate(pattern) ?? (() => false);
     matcherCache.set(remap, matchFn);
   }
 
