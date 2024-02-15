8695d2789a02e893d38d51c655aa32f7037d940f
Rhys Arkins <rhys@arkins.net>
2024-02-15 15:55:45 +0100

    extra test

--- lib/modules/versioning/semver-coerced/index.spec.ts
+++ lib/modules/versioning/semver-coerced/index.spec.ts
@@ -267,6 +267,14 @@ describe('modules/versioning/semver-coerced/index', () => {
           newVersion: 'v1.1.0',
         }),
       ).toBe('1.1.0');
+      expect(
+        semverCoerced.getNewValue({
+          currentValue: '1.0.0',
+          rangeStrategy: 'auto',
+          currentVersion: 'v1.0.0',
+          newVersion: '1.1.0',
+        }),
+      ).toBe('1.1.0');
     });
   });
 
