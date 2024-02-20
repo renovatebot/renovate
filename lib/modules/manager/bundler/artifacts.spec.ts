e0431ad4fc183d0c1f9711b50bb61b3ae900fc4e
Ben Langfeld <blangfeld@powerhrg.com>
2024-02-20 17:29:27 +0000

    feature(manager/bundler): Allow upgrading bundler itself

--- lib/modules/manager/bundler/artifacts.spec.ts
+++ lib/modules/manager/bundler/artifacts.spec.ts
@@ -967,6 +967,28 @@ describe('modules/manager/bundler/artifacts', () => {
         expect(res).toMatchObject([{ file: { path: 'Gemfile.lock' } }]);
         expect(execSnapshots).toMatchObject([{ cmd: 'bundler lock' }]);
       });
+
+      it('updates the Gemfile.lock when bundler upgraded', async () => {
+        fs.readLocalFile.mockResolvedValue('Current Gemfile.lock');
+        const execSnapshots = mockExecSequence([{ stdout: '', stderr: '' }]);
+        git.getRepoStatus.mockResolvedValueOnce(
+          partial<StatusResult>({
+            modified: ['Gemfile.lock'],
+          }),
+        );
+
+        const res = await updateArtifacts({
+          packageFileName: 'Gemfile',
+          updatedDeps: [{ depName: 'bundler', updateType: 'patch' }],
+          newPackageFileContent: '{}',
+          config,
+        });
+
+        expect(res).toMatchObject([{ file: { path: 'Gemfile.lock' } }]);
+        expect(execSnapshots).toMatchObject([
+          { cmd: 'bundler lock --update --bundler' },
+        ]);
+      });
     });
   });
 });
