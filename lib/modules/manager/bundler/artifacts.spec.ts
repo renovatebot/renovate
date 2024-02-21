052b389615166ad8ab618dbefd031013415ef3eb
Ben Langfeld <blangfeld@powerhrg.com>
2024-02-21 14:39:37 -0300

    Language tweaks

--- lib/modules/manager/bundler/artifacts.spec.ts
+++ lib/modules/manager/bundler/artifacts.spec.ts
@@ -947,7 +947,7 @@ describe('modules/manager/bundler/artifacts', () => {
         ]);
       });
 
-      it('updates the Gemfile.lock when ruby upgraded', async () => {
+      it('updates the Gemfile.lock when upgrading bundler', async () => {
         // See https://github.com/renovatebot/renovate/issues/15114
         fs.readLocalFile.mockResolvedValue('Current Gemfile.lock');
         const execSnapshots = mockExecSequence([{ stdout: '', stderr: '' }]);
@@ -968,7 +968,7 @@ describe('modules/manager/bundler/artifacts', () => {
         expect(execSnapshots).toMatchObject([{ cmd: 'bundler lock' }]);
       });
 
-      it('updates the Gemfile.lock when bundler upgraded', async () => {
+      it('updates the Gemfile.lock when upgrading bundler', async () => {
         fs.readLocalFile.mockResolvedValue('Current Gemfile.lock');
         const execSnapshots = mockExecSequence([{ stdout: '', stderr: '' }]);
         git.getRepoStatus.mockResolvedValueOnce(
