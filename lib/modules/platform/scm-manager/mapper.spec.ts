62ab7bcebd1be74bdc8ba964d8b7e9b63119ea08
zT-1337 <thomas.zerr@googlemail.com>
2024-02-23 14:13:48 +0100

    Update lib/modules/platform/scm-manager/mapper.spec.ts
    Co-authored-by: HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>

--- lib/modules/platform/scm-manager/mapper.spec.ts
+++ lib/modules/platform/scm-manager/mapper.spec.ts
@@ -2,7 +2,7 @@ import { mapPrFromScmToRenovate } from './mapper';
 import type { PullRequest as SCMPullRequest } from './types';
 
 describe('modules/platform/scm-manager/mapper', () => {
-  it('should correctly map the scm type of a PR to the Renovate PR type', () => {
+  it('should correctly map the scm-manager type of a PR to the Renovate PR type', () => {
     const scmPr: SCMPullRequest = {
       source: 'feat/new',
       target: 'develop',
