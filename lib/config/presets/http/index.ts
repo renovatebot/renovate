9e18c9b30c5df66c86edd735e4df5cc592b2ffd2
Aleksandr Mezin <aleksandr.mezin@webpros.com>
2024-02-23 02:58:05 +0200

    Add type

--- lib/config/presets/http/index.ts
+++ lib/config/presets/http/index.ts
@@ -1,6 +1,7 @@
 import { logger } from '../../../logger';
 import { ExternalHostError } from '../../../types/errors/external-host-error';
 import { Http } from '../../../util/http';
+import type { HttpResponse } from '../../../util/http/types';
 import { parseUrl } from '../../../util/url';
 import type { Preset, PresetConfig } from '../types';
 import { PRESET_DEP_NOT_FOUND, parsePreset } from '../util';
@@ -11,7 +12,7 @@ export async function getPreset({
   repo: url,
 }: PresetConfig): Promise<Preset | null | undefined> {
   const parsedUrl = parseUrl(url);
-  let response;
+  let response: HttpResponse;
 
   if (!parsedUrl) {
     logger.debug(`Preset URL ${url} is malformed`);
