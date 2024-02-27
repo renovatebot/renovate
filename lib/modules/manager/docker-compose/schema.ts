renovatebot/renovate

commit 14cba69c58bd35a0851524afea70e6a8cf757383

refactor(managers): yaml schema checks (#26811)

lib/modules/manager/docker-compose/schema.ts
@@ -1,0 +1,23 @@
+import { z } from 'zod';
+
+const DockerComposeService = z.object({
+  image: z.string().optional(),
+  build: z
+    .object({
+      context: z.string().optional(),
+      dockerfile: z.string().optional(),
+    })
+    .optional(),
+});
+
+const DockerComposeFileV1 = z.record(DockerComposeService);
+const DockerComposeFileModern = z.object({
+  // compose does not use this strictly, so we shouldn't be either
+  // https://docs.docker.com/compose/compose-file/04-version-and-name/#version-top-level-element
+  version: z.string().optional(),
+  services: z.record(DockerComposeService),
+});
+
+export const DockerComposeFile =
+  DockerComposeFileModern.or(DockerComposeFileV1);
+export type DockerComposeFile = z.infer<typeof DockerComposeFile>;

lib/modules/manager/argocd/schema.ts
@@ -1,0 +1,35 @@
+import { z } from 'zod';
+
+export const KubernetesResource = z.object({
+  apiVersion: z.string(),
+});
+
+export const ApplicationSource = z.object({
+  chart: z.string().optional(),
+  repoURL: z.string(),
+  targetRevision: z.string(),
+});
+export type ApplicationSource = z.infer<typeof ApplicationSource>;
+
+export const ApplicationSpec = z.object({
+  source: ApplicationSource.optional(),
+  sources: z.array(ApplicationSource).optional(),
+});
+export type ApplicationSpec = z.infer<typeof ApplicationSpec>;
+
+export const Application = KubernetesResource.extend({
+  kind: z.literal('Application'),
+  spec: ApplicationSpec,
+});
+
+export const ApplicationSet = KubernetesResource.extend({
+  kind: z.literal('ApplicationSet'),
+  spec: z.object({
+    template: z.object({
+      spec: ApplicationSpec,
+    }),
+  }),
+});
+
+export const ApplicationDefinition = Application.or(ApplicationSet);
+export type ApplicationDefinition = z.infer<typeof ApplicationDefinition>;

lib/modules/manager/fleet/schema.ts
@@ -1,0 +1,45 @@
+import { z } from 'zod';
+
+const FleetHelmBlock = z.object({
+  chart: z.string().optional(),
+  repo: z.string().optional(),
+  version: z.string().optional(),
+});
+export type FleetHelmBlock = z.infer<typeof FleetHelmBlock>;
+
+const FleetFileHelm = FleetHelmBlock.extend({
+  releaseName: z.string(),
+});
+
+/**
+  Represent a GitRepo Kubernetes manifest of Fleet.
+  @link https://fleet.rancher.io/gitrepo-add/#create-gitrepo-instance
+ */
+export const GitRepo = z.object({
+  metadata: z.object({
+    name: z.string(),
+  }),
+  kind: z.string(),
+  spec: z.object({
+    repo: z.string().optional(),
+    revision: z.string().optional(),
+  }),
+});
+export type GitRepo = z.infer<typeof GitRepo>;
+
+/**
+ Represent a Bundle configuration of Fleet, which is located in `fleet.yaml` files.
+ @link https://fleet.rancher.io/gitrepo-structure/#fleetyaml
+ */
+export const FleetFile = z.object({
+  helm: FleetFileHelm,
+  targetCustomizations: z
+    .array(
+      z.object({
+        name: z.string(),
+        helm: FleetHelmBlock.partial().optional(),
+      }),
+    )
+    .optional(),
+});
+export type FleetFile = z.infer<typeof FleetFile>;

lib/modules/manager/flux/schema.ts
@@ -1,0 +1,76 @@
+import { z } from 'zod';
+
+export const KubernetesResource = z.object({
+  apiVersion: z.string(),
+  kind: z.string(),
+  metadata: z.object({
+    name: z.string(),
+    // For Flux, the namespace property is optional, but matching HelmReleases to HelmRepositories would be
+    // much more difficult without it (we'd have to examine the parent Kustomizations to discover the value),
+    // so we require it for renovation.
+    namespace: z.string().optional(),
+  }),
+});
+
+export const HelmRelease = KubernetesResource.extend({
+  apiVersion: z.string().startsWith('helm.toolkit.fluxcd.io/'),
+  kind: z.literal('HelmRelease'),
+  spec: z.object({
+    chart: z.object({
+      spec: z.object({
+        chart: z.string(),
+        version: z.string().optional(),
+        sourceRef: z
+          .object({
+            kind: z.string().optional(),
+            name: z.string().optional(),
+            namespace: z.string().optional(),
+          })
+          .optional(),
+      }),
+    }),
+  }),
+});
+
+export const HelmRepository = KubernetesResource.extend({
+  apiVersion: z.string().startsWith('source.toolkit.fluxcd.io/'),
+  kind: z.literal('HelmRepository'),
+  spec: z.object({
+    url: z.string(),
+    type: z.enum(['oci', 'default']).optional(),
+  }),
+});
+export type HelmRepository = z.infer<typeof HelmRepository>;
+
+export const GitRepository = KubernetesResource.extend({
+  apiVersion: z.string().startsWith('source.toolkit.fluxcd.io/'),
+  kind: z.literal('GitRepository'),
+  spec: z.object({
+    url: z.string(),
+    ref: z
+      .object({
+        tag: z.string().optional(),
+        commit: z.string().optional(),
+      })
+      .optional(),
+  }),
+});
+
+export const OCIRepository = KubernetesResource.extend({
+  apiVersion: z.string().startsWith('source.toolkit.fluxcd.io/'),
+  kind: z.literal('OCIRepository'),
+  spec: z.object({
+    url: z.string(),
+    ref: z
+      .object({
+        tag: z.string().optional(),
+        digest: z.string().optional(),
+      })
+      .optional(),
+  }),
+});
+
+export const FluxResource = HelmRelease.or(HelmRepository)
+  .or(GitRepository)
+  .or(OCIRepository);
+export type FluxResource = z.infer<typeof FluxResource>;

lib/modules/manager/argocd/extract.ts
@@ -11,11 +11,11 @@ import type {
   PackageDependency,
   PackageFileContent,
 } from '../types';
-import type {
+import {
   ApplicationDefinition,
-  ApplicationSource,
-  ApplicationSpec,
-} from './types';
+  type ApplicationSource,
+  type ApplicationSpec,
+} from './schema';
 import { fileTestRegex } from './util';
 
 export function extractPackageFile(
@@ -33,27 +33,21 @@ export function extractPackageFile(
 
   let definitions: ApplicationDefinition[];
   try {
-    // TODO: use schema (#9610)
-    definitions = parseYaml(content);
+    definitions = parseYaml(content, null, {
+      customSchema: ApplicationDefinition,
+      failureBehaviour: 'filter',
+    });
   } catch (err) {
     logger.debug({ err, packageFile }, 'Failed to parse ArgoCD definition.');
     return null;
   }
 
-  const deps = definitions.filter(is.plainObject).flatMap(processAppSpec);
+  const deps = definitions.flatMap(processAppSpec);
 
   return deps.length ? { deps } : null;
 }
 
 function processSource(source: ApplicationSource): PackageDependency | null {
-  if (
-    !source ||
-    !is.nonEmptyString(source.repoURL) ||
-    !is.nonEmptyString(source.targetRevision)
-  ) {
-    return null;
-  }
-
   // a chart variable is defined this is helm declaration
   if (source.chart) {
     // assume OCI helm chart if repoURL doesn't contain explicit protocol
@@ -89,14 +83,10 @@ function processSource(source: ApplicationSource): PackageDependency | null {
 function processAppSpec(
   definition: ApplicationDefinition,
 ): PackageDependency[] {
-  const spec: ApplicationSpec | null | undefined =
+  const spec: ApplicationSpec =
     definition.kind === 'Application'
-      ? definition?.spec
-      : definition?.spec?.template?.spec;
-
-  if (is.nullOrUndefined(spec)) {
-    return [];
-  }
+      ? definition.spec
+      : definition.spec.template.spec;
 
   const deps: (PackageDependency | null)[] = [];
 

lib/modules/manager/argocd/types.ts
@@ -1,30 +1,0 @@
-export interface KubernetesResource {
-  apiVersion: string;
-}
-
-export interface ApplicationSource {
-  chart?: string;
-  repoURL: string;
-  targetRevision: string;
-}
-
-export interface ApplicationSpec {
-  source?: ApplicationSource;
-  sources?: ApplicationSource[];
-}
-
-export interface Application extends KubernetesResource {
-  kind: 'Application';
-  spec: ApplicationSpec;
-}
-
-export interface ApplicationSet extends KubernetesResource {
-  kind: 'ApplicationSet';
-  spec: {
-    template: {
-      spec: ApplicationSpec;
-    };
-  };
-}
-
-export type ApplicationDefinition = Application | ApplicationSet;

lib/modules/manager/crossplane/extract.ts
@@ -6,7 +6,7 @@ import type {
   PackageDependency,
   PackageFileContent,
 } from '../types';
-import { XPKGSchema } from './schema';
+import { type XPKG, XPKGSchema } from './schema';
 
 export function extractPackageFile(
   content: string,
@@ -19,9 +19,12 @@ export function extractPackageFile(
     return null;
   }
 
-  let list = [];
+  let list: XPKG[] = [];
   try {
-    list = parseYaml(content);
+    list = parseYaml(content, null, {
+      customSchema: XPKGSchema,
+      failureBehaviour: 'filter',
+    });
   } catch (err) {
     logger.debug(
       { err, packageFile },
@@ -31,16 +34,7 @@ export function extractPackageFile(
   }
 
   const deps: PackageDependency[] = [];
-  for (const item of list) {
-    const parsed = XPKGSchema.safeParse(item);
-    if (!parsed.success) {
-      logger.trace(
-        { item, errors: parsed.error },
-        'Invalid Crossplane package',
-      );
-      continue;
-    }
-    const xpkg = parsed.data;
+  for (const xpkg of list) {
     const dep = getDep(xpkg.spec.package, true, extractConfig?.registryAliases);
     dep.depType = xpkg.kind.toLowerCase();
     deps.push(dep);

lib/modules/manager/docker-compose/extract.ts
@@ -4,7 +4,7 @@ import { newlineRegex, regEx } from '../../../util/regex';
 import { parseSingleYaml } from '../../../util/yaml';
 import { getDep } from '../dockerfile/extract';
 import type { ExtractConfig, PackageFileContent } from '../types';
-import type { DockerComposeConfig } from './types';
+import { DockerComposeFile } from './schema';
 
 class LineMapper {
   private imageLines: { line: string; lineNumber: number; used: boolean }[];
@@ -34,24 +34,12 @@ export function extractPackageFile(
   extractConfig: ExtractConfig,
 ): PackageFileContent | null {
   logger.debug(`docker-compose.extractPackageFile(${packageFile})`);
-  let config: DockerComposeConfig;
+  let config: DockerComposeFile;
   try {
-    // TODO: use schema (#9610)
-    config = parseSingleYaml(content, { json: true });
-    if (!config) {
-      logger.debug(
-        { packageFile },
-        'Null config when parsing Docker Compose content',
-      );
-      return null;
-    }
-    if (typeof config !== 'object') {
-      logger.debug(
-        { packageFile, type: typeof config },
-        'Unexpected type for Docker Compose content',
-      );
-      return null;
-    }
+    config = parseSingleYaml(content, {
+      json: true,
+      customSchema: DockerComposeFile,
+    });
   } catch (err) {
     logger.debug(
       { err, packageFile },

commit e795122c0d6721db198ba50877cbbbd6b84d447a

fix(manager/docker-compose): Allow strings or objects for service build
Resolves issue introduced by https://github.com/renovatebot/renovate/pull/26811

See: https://docs.docker.com/compose/compose-file/compose-file-v2/#build
Signed-off-by: Kyle Harding <kyle@balena.io>

lib/modules/manager/docker-compose/__fixtures__/docker-compose.1.yml
@@ -81,3 +81,6 @@ debugapp:
   build:
     context: .
     dockerfile: Dockerfile.local
+
+build-context:
+    build: ./debugapp

lib/modules/manager/docker-compose/__fixtures__/docker-compose.3-no-version.yml
@@ -95,6 +95,9 @@ services:
       context: .
       dockerfile: Dockerfile.local
 
+  build-context:
+    build: ./debugapp
+
 networks:
   frontend:
   backend:

lib/modules/manager/docker-compose/__fixtures__/docker-compose.3.yml
@@ -96,6 +96,9 @@ services:
       context: .
       dockerfile: Dockerfile.local
 
+  build-context:
+    build: ./debugapp
+
 networks:
   frontend:
   backend:

lib/modules/manager/docker-compose/schema.ts
@@ -3,10 +3,13 @@ import { z } from 'zod';
 const DockerComposeService = z.object({
   image: z.string().optional(),
   build: z
-    .object({
-      context: z.string().optional(),
-      dockerfile: z.string().optional(),
-    })
+    .union([
+      z.string(),
+      z.object({
+        context: z.string().optional(),
+        dockerfile: z.string().optional(),
+      }),
+    ])
     .optional(),
 });
 