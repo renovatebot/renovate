54d2a4f2da21b88cd2348d93cd928e47d1ef94da
Norbert Szulc <norbert@icetek.io>
2024-02-27 23:52:37 +0100

    Formatting

--- lib/modules/datasource/python-version/schema.ts
+++ lib/modules/datasource/python-version/schema.ts
@@ -5,26 +5,19 @@ export const PythonRelease = z
   .object({
     /** e.g: "Python 3.9.0b1" */
     name: z.string(),
-
     /** e.g: "python-390b1" */
     slug: z.string(),
-
     /** Major version e.g: 3 */
     version: z.number(),
-    is_published: z.boolean(),
-
     /** is latest major version, true for Python 2.7.18 and latest Python 3 */
     is_latest: z.boolean(),
-
+    is_published: z.boolean(),
     release_date: z.string(),
     pre_release: z.boolean(),
     release_page: z.string().nullable(),
-
+    show_on_download_page: z.boolean(),
     /** Changelog e.g: "https://docs.python.org/…html#python-3-9-0-beta-1" */
     release_notes_url: z.string(),
-
-    show_on_download_page: z.boolean(),
-
     /** Download URL e.g: "https://www.python.org/api/v2/downloads/release/436/" */
     resource_uri: z.string(),
   })
