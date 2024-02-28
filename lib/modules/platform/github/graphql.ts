09e94e0f89b51092b81299397de4b54048f3d59c
Rhys Arkins <rhys@arkins.net>
2024-02-28 13:11:51 +0100

    Revert "feat(github): use REST for etag caching of issues (#26793)"
    This reverts commit 924b9dad68a48612e8a58025e9985ecd9189bd9d.

--- lib/modules/platform/github/graphql.ts
+++ lib/modules/platform/github/graphql.ts
@@ -93,6 +93,36 @@ query($owner: String!, $name: String!, $count: Int, $cursor: String) {
 }
 `;
 
+export const getIssuesQuery = `
+query(
+  $owner: String!,
+  $name: String!,
+  $user: String!,
+  $count: Int,
+  $cursor: String
+) {
+  repository(owner: $owner, name: $name) {
+    issues(
+      orderBy: { field: UPDATED_AT, direction: DESC },
+      filterBy: { createdBy: $user },
+      first: $count,
+      after: $cursor
+    ) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      nodes {
+        number
+        state
+        title
+        body
+      }
+    }
+  }
+}
+`;
+
 export const vulnerabilityAlertsQuery = (filterByState: boolean): string => `
 query($owner: String!, $name: String!) {
   repository(owner: $owner, name: $name) {
