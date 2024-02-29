4bc1a002bb238abe3dee64b56fd163ade8f53f0a
Norbert Szulc <norbert@icetek.io>
2024-02-29 12:14:30 +0100

    fix(workers/repository): defensive assignment in patchConfigForArtifactsUpdate

--- lib/workers/repository/update/branch/get-updated.ts
+++ lib/workers/repository/update/branch/get-updated.ts
@@ -379,10 +379,15 @@ function patchConfigForArtifactsUpdate(
     const packageFile = managerPackageFiles.find(
       (p) => p.packageFile === packageFileName,
     );
-    if (packageFile) {
-      updatedConfig.lockFiles ??= packageFile.lockFiles;
+    if (
+      packageFile &&
+      is.nonEmptyArray(updatedConfig.lockFiles) &&
+      is.nonEmptyArray(packageFile.lockFiles)
+    ) {
+      updatedConfig.lockFiles = packageFile.lockFiles;
     }
   }
+  console.log('patchConfigForArtifactsUpdate', updatedConfig);
   return updatedConfig;
 }
 
