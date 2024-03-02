ff3f1a651da00a6aafb1a7634d878bc615468c2d
Rhys Arkins <rhys@arkins.net>
2024-03-02 06:53:36 +0100

    coverage, docs

--- lib/modules/manager/pre-commit/index.ts
+++ lib/modules/manager/pre-commit/index.ts
@@ -12,7 +12,8 @@ export const defaultConfig = {
   enabled: false,
   fileMatch: ['(^|/)\\.pre-commit-config\\.ya?ml$'],
   prBodyNotes: process.env.RENOVATE_X_SUPPRESS_PRE_COMMIT_WARNING
-    ? []
+    ? /* istanbul ignore next */
+      []
     : [
         'Note: The `pre-commit` manager in Renovate is not supported by the `pre-commit` maintainers or community. Please do not report any problems there, instead [create a Discussion in the Renovate repository](https://github.com/renovatebot/renovate/discussions/new) if you have any questions.',
       ],
