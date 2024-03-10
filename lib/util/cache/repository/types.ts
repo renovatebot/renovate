48daacafdf8553c7d95172a8424786e66b5de550
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-03-10 10:08:51 -0300

    refactor: Remove dependency on Gitea and Bitbucket types in cache (#27821)

--- lib/util/cache/repository/types.ts
+++ lib/util/cache/repository/types.ts
@@ -4,8 +4,6 @@ import type {
   UpdateType,
 } from '../../../config/types';
 import type { PackageFile } from '../../../modules/manager/types';
-import type { BitbucketPrCacheData } from '../../../modules/platform/bitbucket/types';
-import type { GiteaPrCacheData } from '../../../modules/platform/gitea/types';
 import type { RepoInitConfig } from '../../../workers/repository/init/types';
 import type { PrBlockedBy } from '../../../workers/types';
 import type { HttpResponse } from '../../http/types';
@@ -142,7 +140,7 @@ export interface RepoCacheData {
   lastPlatformAutomergeFailure?: string;
   platform?: {
     gitea?: {
-      pullRequestsCache?: GiteaPrCacheData;
+      pullRequestsCache?: unknown;
     };
     github?: {
       /**
@@ -152,7 +150,7 @@ export interface RepoCacheData {
       graphqlPageCache?: unknown;
     };
     bitbucket?: {
-      pullRequestsCache?: BitbucketPrCacheData;
+      pullRequestsCache?: unknown;
     };
   };
   prComments?: Record<number, Record<string, string>>;
