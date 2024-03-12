7ed32f93ed09bdac2785b82228cbcb8db3bbee52
Rhys Arkins <rhys@arkins.net>
2024-03-12 20:50:30 +0100

    drop warn

--- lib/workers/repository/index.ts
+++ lib/workers/repository/index.ts
@@ -42,7 +42,6 @@ export async function renovateRepository(
   let config = GlobalConfig.set(
     applySecretsToConfig(repoConfig, undefined, false),
   );
-  logger.warn('GlobalConfig.set()');
   await removeDanglingContainers();
   setMeta({ repository: config.repository });
   logger.info({ renovateVersion: pkg.version }, 'Repository started');
