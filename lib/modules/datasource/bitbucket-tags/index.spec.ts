9ee78ca09713cc76bc120f440abec8fd19cefa2f
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-03-18 11:24:57 -0300

    Remove more

--- lib/modules/datasource/bitbucket-tags/index.spec.ts
+++ lib/modules/datasource/bitbucket-tags/index.spec.ts
@@ -92,12 +92,9 @@ describe('modules/datasource/bitbucket-tags/index', () => {
         .scope('https://api.bitbucket.org')
         .get('/2.0/repositories/some/dep2')
         .reply(200, {
-          owner: { username: 'some' },
           mainbranch: { name: 'master' },
-          has_issues: true,
           uuid: '123',
           full_name: 'some/repo',
-          is_private: false,
         });
       httpMock
         .scope('https://api.bitbucket.org')
