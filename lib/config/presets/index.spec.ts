ca82e7bc638f23b172812d8e0b03e16d8756cd3e
lstoeferle <48953604+lstoeferle@users.noreply.github.com>
2024-03-18 12:31:56 +0100

    test: remove obsolete snapshot

--- lib/config/presets/index.spec.ts
+++ lib/config/presets/index.spec.ts
@@ -327,7 +327,6 @@ describe('config/presets/index', () => {
 
       expect(res.labels).toEqual(['self-hosted with template resolved']);
       expect(local.getPreset.mock.calls).toHaveLength(1);
-      expect(res).toMatchSnapshot();
     });
 
     it('resolves self-hosted transitive presets without baseConfig', async () => {
