054b2415fe2fe9d9f737d32c78173790bc080e12
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-03-21 17:28:06 -0300

    feat: Make memory cache available before repo init

--- lib/workers/repository/init/cache.ts
+++ lib/workers/repository/init/cache.ts
@@ -1,13 +1,11 @@
 import fs from 'fs-extra';
 import * as npmApi from '../../../modules/datasource/npm';
-import * as memCache from '../../../util/cache/memory';
 import * as repositoryCache from '../../../util/cache/repository';
 import { initRepoCache } from '../../../util/cache/repository/init';
 import { privateCacheDir } from '../../../util/fs';
 import type { WorkerPlatformConfig } from './apis';
 
 export async function resetCaches(): Promise<void> {
-  memCache.reset();
   repositoryCache.resetCache();
   await fs.remove(privateCacheDir());
 }
@@ -15,7 +13,6 @@ export async function resetCaches(): Promise<void> {
 export async function initializeCaches(
   config: WorkerPlatformConfig,
 ): Promise<void> {
-  memCache.init();
   await initRepoCache(config);
   await fs.ensureDir(privateCacheDir());
   npmApi.setNpmrc();
