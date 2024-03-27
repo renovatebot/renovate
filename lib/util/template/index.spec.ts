95ef9a7a9a753c802c7e82aa6a5b4df7c6c7b0d0
Juan Carlos Blanco Delgado <juancarlosjr97@gmail.com>
2024-03-27 17:01:08 +0000

    test(templates): update to use labels example for includes handlebars helper
    #27818

--- lib/util/template/index.spec.ts
+++ lib/util/template/index.spec.ts
@@ -280,9 +280,9 @@ describe('util/template/index', () => {
   describe('includes', () => {
     it('includes is true', () => {
       const output = template.compile(
-        '{{#if (includes depTypes "dependencies")}}production{{else}}notProduction{{/if}}',
+        '{{#if (includes labels "dependencies")}}production{{else}}notProduction{{/if}}',
         {
-          depTypes: ['dependencies'],
+          labels: ['dependencies'],
         },
       );
 
@@ -291,9 +291,9 @@ describe('util/template/index', () => {
 
     it('includes is false', () => {
       const output = template.compile(
-        '{{#if (includes depTypes "dependencies")}}production{{else}}notProduction{{/if}}',
+        '{{#if (includes labels "dependencies")}}production{{else}}notProduction{{/if}}',
         {
-          depTypes: ['devDependencies'],
+          labels: ['devDependencies'],
         },
       );
 
@@ -302,9 +302,9 @@ describe('util/template/index', () => {
 
     it('includes with incorrect type first argument', () => {
       const output = template.compile(
-        '{{#if (includes depTypes "dependencies")}}production{{else}}notProduction{{/if}}',
+        '{{#if (includes labels "dependencies")}}production{{else}}notProduction{{/if}}',
         {
-          depTypes: 'devDependencies',
+          labels: 'devDependencies',
         },
       );
 
@@ -313,9 +313,9 @@ describe('util/template/index', () => {
 
     it('includes with incorrect type second argument', () => {
       const output = template.compile(
-        '{{#if (includes depTypes 555)}}production{{else}}notProduction{{/if}}',
+        '{{#if (includes labels 555)}}production{{else}}notProduction{{/if}}',
         {
-          depTypes: ['devDependencies'],
+          labels: ['devDependencies'],
         },
       );
 
