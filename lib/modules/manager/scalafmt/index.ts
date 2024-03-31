ab76c2814c597062ca7353d3cf5e419a23fc89ad
Gaël Jourdan-Weil <gjourdanweil@gmail.com>
2024-03-31 12:47:45 +0000

    pouet

--- lib/modules/manager/scalafmt/index.ts
+++ lib/modules/manager/scalafmt/index.ts
@@ -2,9 +2,7 @@ import type { Category } from '../../../constants';
 import { GithubReleasesDatasource } from '../../datasource/github-releases';
 import * as semverVersioning from '../../versioning/semver';
 
-export const supportedDatasources = [
-  GithubReleasesDatasource.id,
-];
+export const supportedDatasources = [GithubReleasesDatasource.id];
 
 export const defaultConfig = {
   fileMatch: ['\\.scalafmt.conf$'],
