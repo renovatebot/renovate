6fc9421baa52484d9c584bdbed229f92e325ce6d
Rob Jack Stewart <robjackstewart@gmail.com>
2024-03-31 15:01:34 +0000

    add support for dev container features

--- lib/modules/manager/dev-container/extract.ts
+++ lib/modules/manager/dev-container/extract.ts
@@ -8,25 +8,49 @@ export function extractPackageFile(
   packageFile: string,
   extractConfig: ExtractConfig,
 ): PackageFileContent | null {
-  let config: DevContainerFile;
+  let file: DevContainerFile;
   try {
-    config = JSON.parse(content) as DevContainerFile;
+    file = JSON.parse(content) as DevContainerFile;
   } catch (err) {
-    logger.debug({ packageFile, err }, `Failed to parse dev container file`);
+    logger.debug(
+      { packageFile, err },
+      `Failed to parse dev container JSON file`,
+    );
     return null;
   }
 
   try {
-    const image = config.image;
-    if (image === undefined || image === null) {
+    let images: string[] = [];
+    const image = getImage(file);
+
+    if (image !== undefined && image !== null) {
+      images.push(image);
+    } else {
       logger.debug(
-        `Dev container definition '${packageFile}' does not contain an 'image' property. Skipping.`,
+        `Could not determine image for dev container: ${packageFile}`,
+      );
+    }
+
+    const featureImages = getFeatureImages(file) ?? [];
+
+    if (featureImages.length > 0) {
+      images = images.concat(featureImages);
+    } else {
+      logger.debug(`Dev container JSON file has no features: ${packageFile}`);
+    }
+
+    if (images.length < 1) {
+      logger.debug(
+        `No images found in dev container JSON file: ${packageFile}`,
       );
       return null;
     }
 
-    const dep = getDep(image, true, extractConfig.registryAliases);
-    return { deps: [dep] };
+    const deps = images.map((image) =>
+      getDep(image, true, extractConfig.registryAliases),
+    );
+
+    return { deps };
   } catch (err) /* istanbul ignore next */ {
     logger.debug(
       { packageFile, err },
@@ -34,4 +58,24 @@ export function extractPackageFile(
     );
     return null;
   }
+}
+
+function getImage(file: DevContainerFile): string | null {
+  const image = file?.image;
+  if (image === undefined || image === null) {
+    return null;
+  }
+  return image;
+}
+
+function getFeatureImages(file: DevContainerFile): string[] {
+  const images: string[] = [];
+  const features = file?.features;
+
+  if (features !== undefined && features !== null) {
+    for (const feature in features) {
+      images.push(feature);
+    }
+  }
+  return images;
 }
--- lib/modules/manager/dev-container/schema.ts
+++ lib/modules/manager/dev-container/schema.ts
@@ -1,3 +1,4 @@
 export interface DevContainerFile {
   image?: string;
+  features: any;
 }
