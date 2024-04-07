15a8720dd3db6958cbc857e30fe4bff05f8d02d5
Rahul Gautam Singh <rahultesnik@gmail.com>
2024-04-07 16:57:04 +0545

    add tests

---
+++ lib/util/experimental-flags.spec.ts
@@ -0,0 +1,30 @@
+import { GlobalConfig } from '../config/global';
+import { experimentalFlagValue } from './experimental-flags';
+
+describe('util/experimental-flags', () => {
+  describe('experimentalFlagValue()', () => {
+    beforeEach(() => {
+      GlobalConfig.reset();
+    });
+
+    it('returns null if flag not found', () => {
+      GlobalConfig.set({
+        experimentalFlags: ['dockerMaxPages=19'],
+      });
+      expect(experimentalFlagValue('dockerHubTags')).toBeNull();
+    });
+
+    it('returns null if experimentalFlags is undefined', () => {
+      expect(experimentalFlagValue('dockerHubTags')).toBeNull();
+    });
+
+    it('returns value', () => {
+      GlobalConfig.set({
+        experimentalFlags: ['dockerHubTags', 'dockerMaxPages=19'],
+      });
+      expect(experimentalFlagValue('dockerHubTags')).toBe('dockerHubTags');
+      expect(experimentalFlagValue('dockerMaxPages')).toBe('19');
+      expect(experimentalFlagValue('dockerMaxPages')).toBe('19'); // coverage for stored values
+    });
+  });
+});
