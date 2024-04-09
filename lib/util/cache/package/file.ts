77a9568c64975566ec441296af91079eed6ac2e0
Michael Kriese <michael.kriese@visualon.de>
2024-04-09 11:49:45 +0200

    Update lib/util/cache/package/file.ts

--- lib/util/cache/package/file.ts
+++ lib/util/cache/package/file.ts
@@ -99,7 +99,7 @@ export async function cleanup(): Promise<void> {
         deletedCount += 1;
       }
     }
-    logger.debug("Verifying and cleaning cache.", { cacheFileName });
+    logger.debug({ cacheFileName }, "Verifying and cleaning cache.");
     await cacache.verify(cacheFileName);
     const durationMs = Math.round(Date.now() - startTime);
     logger.debug(
