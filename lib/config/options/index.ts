6e2c082c3ef68c4c2ed8d1513590e303d0f8140d
Tom Cammann <tom.cammann@forgerock.com>
2024-04-14 09:36:43 +0100

    fix(config)!: Use `depNameSanitized` in branch name for vulnerability alerts (#25166)
    Use sanitized depName in vulnerability/remediation branches instead of raw depName. This will result in some open remediation branches being autoclosed and replaced for ecosystems like go in particular which have special characters in depNames.
    
    BREAKING CHANGE: Branch names for remediation will be sanitized to exclude special characters, potentially resulting in some autoclosing/replacing of existing PRs.

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2065,7 +2065,7 @@ const options: RenovateOptions[] = [
       minimumReleaseAge: null,
       rangeStrategy: 'update-lockfile',
       commitMessageSuffix: '[SECURITY]',
-      branchTopic: `{{{datasource}}}-{{{depName}}}-vulnerability`,
+      branchTopic: `{{{datasource}}}-{{{depNameSanitized}}}-vulnerability`,
       prCreation: 'immediate',
     },
     mergeable: true,
