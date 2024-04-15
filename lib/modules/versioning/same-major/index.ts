8db1874d9eff870d0989c2514a890738372dd4bf
Rahul Gautam Singh <rahultesnik@gmail.com>
2024-04-15 19:34:07 +0545

    add urls

--- lib/modules/versioning/same-major/index.ts
+++ lib/modules/versioning/same-major/index.ts
@@ -5,6 +5,7 @@ import type { VersioningApi } from '../types';
 
 export const id = 'same-major';
 export const displayName = 'Same Major Versioning';
+export const urls = [];
 export const supportsRanges = true;
 
 /**
