68fd387309461b81b5b46da34ec7613918eb8d8e
RahulGautamSingh <rahultesnik@gmail.com>
2024-04-21 17:53:29 +0545

    feat(logging)!: initialize file logging via env (#28104)

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -526,6 +526,8 @@ const options: RenovateOptions[] = [
     stage: 'global',
     type: 'string',
     globalOnly: true,
+    deprecationMsg:
+      'Instead of configuring log file path in the file config. Use the `LOG_FILE` environment variable instead.',
   },
   {
     name: 'logFileLevel',
@@ -534,6 +536,8 @@ const options: RenovateOptions[] = [
     type: 'string',
     default: 'debug',
     globalOnly: true,
+    deprecationMsg:
+      'Instead of configuring log file level in the file config. Use the `LOG_FILE_LEVEL` environment variable instead.',
   },
   {
     name: 'logContext',
