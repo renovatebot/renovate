d6e1cc073d7ffe604fd00173df211c1a47ab7fa2
Michael Kriese <michael.kriese@visualon.de>
2024-04-25 08:59:09 +0200

    Update lib/modules/manager/pipenv/extract.ts

--- lib/modules/manager/pipenv/extract.ts
+++ lib/modules/manager/pipenv/extract.ts
@@ -114,7 +114,7 @@ function isPipRequirements(
   return (
     !is.array(section) &&
     is.object(section) &&
-    !Object.values(section).find((dep) => !is.object(dep) && !is.string(dep))
+    !Object.values(section).some((dep) => !is.object(dep) && !is.string(dep))
   );
 }
 
