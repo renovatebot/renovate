b5ed36e0439d78b607e098c8cc27081648b75046
Lennart Schmidt <lennart.schmidt@gmx.de>
2024-04-26 09:21:07 +0200

    fix: only log parsed config

--- lib/modules/manager/fvm/extract.ts
+++ lib/modules/manager/fvm/extract.ts
@@ -11,16 +11,16 @@ export function extractPackageFile(
   try {
     const config = FvmConfig.parse(JSON.parse(content));
     flutterVersion = config.flutter ?? config.flutterSdkVersion;
+
+    if (!flutterVersion) {
+      logger.debug(
+        { contents: config },
+        'FVM config does not have a flutter version specified',
+      );
+      return null;
+    }
   } catch (err) {
     logger.debug({ packageFile, err }, 'Invalid FVM config');
-    return null;
-  }
-
-  if (!flutterVersion) {
-    logger.debug(
-      { contents: content },
-      'FVM config does not have a flutter version specified',
-    );
     return null;
   }
 
