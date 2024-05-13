7d83eb7d277b2a668836e41f96a6fe23614def15
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-05-13 09:21:52 -0300

    Fix

--- lib/workers/repository/update/branch/index.ts
+++ lib/workers/repository/update/branch/index.ts
@@ -147,7 +147,9 @@ export async function processBranch(
   }
   const keepUpdatedLabel = config.keepUpdatedLabel;
   const artifactErrorTopic = emojify(':warning: Artifact update problem');
-  const artifactNoticeTopic = emojify(':information_source: Artifact update notice');
+  const artifactNoticeTopic = emojify(
+    ':information_source: Artifact update notice',
+  );
   try {
     // Check if branch already existed
     const existingPr =
