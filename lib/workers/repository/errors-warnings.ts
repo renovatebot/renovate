1b34166ae938e8102ab8480768866cd1e4cb47d1
philipabed <philip.abed@whitesourcesoftware.com>
2024-05-22 14:05:52 +0300

    rollback

--- lib/workers/repository/errors-warnings.ts
+++ lib/workers/repository/errors-warnings.ts
@@ -127,6 +127,7 @@ export function getPrWarnings(
   if (is.nonEmptyArray(warningLines)) {
     warningText += emojify(`\n---\n\n> :warning: **Warning**\n> \n`);
     warningText += warningLines.join('\n> \n');
+    warningText += '\n\n';
   }
   return warningText;
 }
