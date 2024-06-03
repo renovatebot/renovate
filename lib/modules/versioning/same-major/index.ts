05341de3a4289e22534a021b0f87c1c617a141c7
Rahul Gautam Singh <rahultesnik@gmail.com>
2024-06-03 19:22:56 +0545

    Revert "modify isGreaterThan"
    This reverts commit e2600ec573241d2228a1b96add33d0a96913caca.

--- lib/modules/versioning/same-major/index.ts
+++ lib/modules/versioning/same-major/index.ts
@@ -22,12 +22,6 @@ function massageVersion(input: string): string {
   return `>=${input} <${major + 1}`;
 }
 
-function isGreaterThan(version: string, other: string): boolean {
-  const major = semverCoerced.getMajor(other)!;
-
-  return semverCoerced.isGreaterThan(version, `${major + 1}.0.0`);
-}
-
 function matches(version: string, range: string): boolean {
   return semverCoerced.matches(version, massageVersion(range));
 }
@@ -56,6 +50,5 @@ export const api: VersioningApi = {
   getSatisfyingVersion,
   minSatisfyingVersion,
   isLessThanRange,
-  isGreaterThan,
 };
 export default api;
