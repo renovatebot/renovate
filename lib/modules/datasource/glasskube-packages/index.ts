1e256cc7e52183a7c417744b5f64ed9b09576dd2
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-06-07 14:13:56 +0200

    fix: add missing import

--- lib/modules/datasource/glasskube-packages/index.ts
+++ lib/modules/datasource/glasskube-packages/index.ts
@@ -1,4 +1,5 @@
 import { logger } from '../../../logger';
+import { cache } from '../../../util/cache/package/decorator';
 import { joinUrlParts } from '../../../util/url';
 import { Datasource } from '../datasource';
 import type { GetReleasesConfig, ReleaseResult } from '../types';
