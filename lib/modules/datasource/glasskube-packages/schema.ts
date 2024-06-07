3441e47871e89003d90a2a28c61e64da7f4a799b
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-06-07 14:18:33 +0200

    chore: remove unnecessary exports

--- lib/modules/datasource/glasskube-packages/schema.ts
+++ lib/modules/datasource/glasskube-packages/schema.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 import { Yaml } from '../../../util/schema-utils';
 
-export const GlasskubePackageVersions = z.object({
+const GlasskubePackageVersions = z.object({
   latestVersion: z.string(),
   versions: z.array(z.object({ version: z.string() })),
 });
 
-export const GlasskubePackageManifest = z.object({
+const GlasskubePackageManifest = z.object({
   references: z.optional(
     z.array(
       z.object({
