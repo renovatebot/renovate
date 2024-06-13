20461ccfb1f7d43ebed10a654017e75aca2ef83b
George Wilson <george.wilson@rea-group.com>
2024-06-13 15:29:09 +1000

    eslint

--- lib/modules/manager/sbt/util.spec.ts
+++ lib/modules/manager/sbt/util.spec.ts
@@ -15,34 +15,41 @@ describe('modules/manager/sbt/util', () => {
       ]);
     });
   });
+
   describe('normalizeScalaVersion()', () => {
     it('does not normalize prior to 2.10', () => {
       const version = '2.9.3';
-      expect(normalizeScalaVersion(version)).toEqual('2.9.3');
+      expect(normalizeScalaVersion(version)).toBe('2.9.3');
     });
+
     it('normalizes a Scala 2.10 version number', () => {
       const version = '2.10.7';
-      expect(normalizeScalaVersion(version)).toEqual('2.10');
+      expect(normalizeScalaVersion(version)).toBe('2.10');
     });
+
     it('normalizes a Scala 2.11 version number', () => {
       const version = '2.11.12';
-      expect(normalizeScalaVersion(version)).toEqual('2.11');
+      expect(normalizeScalaVersion(version)).toBe('2.11');
     });
+
     it('normalizes a Scala 2.12 version number', () => {
       const version = '2.12.19';
-      expect(normalizeScalaVersion(version)).toEqual('2.12');
+      expect(normalizeScalaVersion(version)).toBe('2.12');
     });
+
     it('normalizes a Scala 2.13 version number', () => {
       const version = '2.13.14';
-      expect(normalizeScalaVersion(version)).toEqual('2.13');
+      expect(normalizeScalaVersion(version)).toBe('2.13');
     });
+
     it('normalizes a Scala 3 LTS version number', () => {
       const version = '3.3.3';
-      expect(normalizeScalaVersion(version)).toEqual('3');
+      expect(normalizeScalaVersion(version)).toBe('3');
     });
+
     it('normalizes a Scala 3 current version number', () => {
       const version = '3.4.2';
-      expect(normalizeScalaVersion(version)).toEqual('3');
+      expect(normalizeScalaVersion(version)).toBe('3');
     });
   });
 });
