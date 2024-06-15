2b19582c91593616a46f881208f29bc38162570e
James Griffiths <james.griffiths.2222@gmail.com>
2024-06-15 17:56:56 +0100

    inline parent project example

--- lib/modules/manager/leiningen/extract.spec.ts
+++ lib/modules/manager/leiningen/extract.spec.ts
@@ -1,3 +1,4 @@
+import { codeBlock } from 'common-tags';
 import { Fixtures } from '../../../../test/fixtures';
 import { ClojureDatasource } from '../../datasource/clojure';
 import { extractFromVectors, extractVariables, trimAtKey } from './extract';
@@ -145,7 +146,21 @@ describe('modules/manager/leiningen/extract', () => {
       ],
     });
 
-    expect(extractPackageFile(Fixtures.get(`parent.clj`))).toMatchObject({
+    const parentProjectSrc = codeBlock`
+(defproject org.example/parent-project "1.0.0-SNAPSHOT"
+  :plugins [[lein-parent "0.3.9"]
+            [lein-project-version "0.1.0"]
+            [lein-shell "0.5.0"]]
+  :parent-project {:coords [my-org/my-parent "4.3.0"]
+                   :inherit [:profiles :managed-dependencies :local-repo]}
+  :profiles {:cljfmt {:plugins [[lein-cljfmt "0.9.2"]]}}
+  :dependencies [[org.clojure/core.async "1.6.681"]
+                 [org.clojure/core.match "1.1.0"]
+                 [org.clojure/data.csv "1.1.0"]
+                 [org.clojure/tools.cli "1.1.230"]
+                 [metosin/malli "0.15.0"]])`;
+
+    expect(extractPackageFile(parentProjectSrc)).toMatchObject({
       deps: [
         {
           depName: 'org.clojure:core.async',
