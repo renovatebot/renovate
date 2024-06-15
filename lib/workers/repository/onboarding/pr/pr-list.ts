2bd32277b1b9bb77f53bd2a8c989bc8b617339ec
Rhys Arkins <rhys@arkins.net>
2024-06-15 08:10:48 +0200

    fix(onboarding): remove superfluous <br />

--- lib/workers/repository/onboarding/pr/pr-list.ts
+++ lib/workers/repository/onboarding/pr/pr-list.ts
@@ -70,7 +70,7 @@ export function getPrList(
     prHourlyLimit < branches.length
   ) {
     prDesc += emojify(
-      `<br />\n\n:children_crossing: Branch creation will be limited to maximum ${prHourlyLimit} per hour, so it doesn't swamp any CI resources or overwhelm the project. See docs for \`prhourlylimit\` for details.\n\n`,
+      `\n\n:children_crossing: Branch creation will be limited to maximum ${prHourlyLimit} per hour, so it doesn't swamp any CI resources or overwhelm the project. See docs for \`prhourlylimit\` for details.\n\n`,
     );
   }
   return prDesc;
