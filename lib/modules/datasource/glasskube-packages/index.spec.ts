636ade386c729e230316fbf63c75a51d8984208f
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-06-17 13:51:14 +0200

    fix: add test for default registry url
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- lib/modules/datasource/glasskube-packages/index.spec.ts
+++ lib/modules/datasource/glasskube-packages/index.spec.ts
@@ -4,11 +4,19 @@ import * as httpMock from '../../../../test/http-mock';
 import { GlasskubePackagesDatasource } from '.';
 
 describe('modules/datasource/glasskube-packages/index', () => {
-  const registryUrl = 'https://packages.test.example/packages';
-  const versionsUrl = new URL(`${registryUrl}/cloudnative-pg/versions.yaml`);
+  const customRegistryUrl = 'https://packages.test.example/packages';
+  const customVersionsUrl = new URL(
+    `${customRegistryUrl}/cloudnative-pg/versions.yaml`,
+  );
+  const defaultVersionUrl = new URL(
+    `${GlasskubePackagesDatasource.defaultRegistryUrl}/cloudnative-pg/versions.yaml`,
+  );
   const versionsYaml = Fixtures.get('versions.yaml');
-  const packageManifestUrl = new URL(
-    `${registryUrl}/cloudnative-pg/v1.23.1+1/package.yaml`,
+  const customPackageManifestUrl = new URL(
+    `${customRegistryUrl}/cloudnative-pg/v1.23.1+1/package.yaml`,
+  );
+  const defaultPackageManifestUrl = new URL(
+    `${GlasskubePackagesDatasource.defaultRegistryUrl}/cloudnative-pg/v1.23.1+1/package.yaml`,
   );
   const packageManifestYaml = Fixtures.get('package.yaml');
   const packageManifestNoReferencesYaml = Fixtures.get(
@@ -17,118 +25,152 @@ describe('modules/datasource/glasskube-packages/index', () => {
 
   it('should handle error response on versions request', async () => {
     httpMock
-      .scope(versionsUrl.origin)
-      .get(versionsUrl.pathname)
+      .scope(customVersionsUrl.origin)
+      .get(customVersionsUrl.pathname)
       .reply(500, 'internal server error');
     await expect(
       getPkgReleases({
         datasource: GlasskubePackagesDatasource.id,
         packageName: 'cloudnative-pg',
-        registryUrls: [registryUrl],
+        registryUrls: [customRegistryUrl],
       }),
     ).rejects.toThrow();
   });
 
   it('should handle empty response on versions request', async () => {
-    httpMock.scope(versionsUrl.origin).get(versionsUrl.pathname).reply(200);
+    httpMock
+      .scope(customVersionsUrl.origin)
+      .get(customVersionsUrl.pathname)
+      .reply(200);
     const response = await getPkgReleases({
       datasource: GlasskubePackagesDatasource.id,
       packageName: 'cloudnative-pg',
-      registryUrls: [registryUrl],
+      registryUrls: [customRegistryUrl],
     });
     expect(response).toBeNull();
   });
 
   it('should handle error response on manifest request', async () => {
     httpMock
-      .scope(versionsUrl.origin)
-      .get(versionsUrl.pathname)
+      .scope(customVersionsUrl.origin)
+      .get(customVersionsUrl.pathname)
       .reply(200, versionsYaml);
     httpMock
-      .scope(packageManifestUrl.origin)
-      .get(packageManifestUrl.pathname)
+      .scope(customPackageManifestUrl.origin)
+      .get(customPackageManifestUrl.pathname)
       .reply(500, 'internal server error');
     await expect(
       getPkgReleases({
         datasource: GlasskubePackagesDatasource.id,
         packageName: 'cloudnative-pg',
-        registryUrls: [registryUrl],
+        registryUrls: [customRegistryUrl],
       }),
     ).rejects.toThrow();
   });
 
   it('should handle empty response on manifest request', async () => {
     httpMock
-      .scope(versionsUrl.origin)
-      .get(versionsUrl.pathname)
+      .scope(customVersionsUrl.origin)
+      .get(customVersionsUrl.pathname)
       .reply(200, versionsYaml);
     httpMock
-      .scope(packageManifestUrl.origin)
-      .get(packageManifestUrl.pathname)
+      .scope(customPackageManifestUrl.origin)
+      .get(customPackageManifestUrl.pathname)
       .reply(200);
     const response = await getPkgReleases({
       datasource: GlasskubePackagesDatasource.id,
       packageName: 'cloudnative-pg',
-      registryUrls: [registryUrl],
+      registryUrls: [customRegistryUrl],
     });
     expect(response).toBeNull();
   });
 
   it('should handle package manifest without references', async () => {
     httpMock
-      .scope(versionsUrl.origin)
-      .get(versionsUrl.pathname)
+      .scope(customVersionsUrl.origin)
+      .get(customVersionsUrl.pathname)
       .reply(200, versionsYaml);
     httpMock
-      .scope(packageManifestUrl.origin)
-      .get(packageManifestUrl.pathname)
+      .scope(customPackageManifestUrl.origin)
+      .get(customPackageManifestUrl.pathname)
       .reply(200, packageManifestNoReferencesYaml);
     const response = await getPkgReleases({
       datasource: GlasskubePackagesDatasource.id,
       packageName: 'cloudnative-pg',
-      registryUrls: [registryUrl],
+      registryUrls: [customRegistryUrl],
     });
     expect(response).toEqual({
-      registryUrl,
+      registryUrl: customRegistryUrl,
       releases: [
         {
           version: 'v1.22.0+1',
-          registryUrl,
+          registryUrl: customRegistryUrl,
         },
         {
           version: 'v1.23.1+1',
-          registryUrl,
+          registryUrl: customRegistryUrl,
         },
       ],
     });
   });
 
-  it('should handle package manifest with references', async () => {
+  it('should handle package manifest with references and default url', async () => {
     httpMock
-      .scope(versionsUrl.origin)
-      .get(versionsUrl.pathname)
+      .scope(defaultVersionUrl.origin)
+      .get(defaultVersionUrl.pathname)
       .reply(200, versionsYaml);
     httpMock
-      .scope(packageManifestUrl.origin)
-      .get(packageManifestUrl.pathname)
+      .scope(defaultPackageManifestUrl.origin)
+      .get(defaultPackageManifestUrl.pathname)
       .reply(200, packageManifestYaml);
     const response = await getPkgReleases({
       datasource: GlasskubePackagesDatasource.id,
       packageName: 'cloudnative-pg',
-      registryUrls: [registryUrl],
+      registryUrls: [],
     });
     expect(response).toEqual({
       sourceUrl: 'https://github.com/cloudnative-pg/cloudnative-pg',
       homepage: 'https://cloudnative-pg.io/',
-      registryUrl,
+      registryUrl: GlasskubePackagesDatasource.defaultRegistryUrl,
       releases: [
         {
           version: 'v1.22.0+1',
-          registryUrl,
+          registryUrl: GlasskubePackagesDatasource.defaultRegistryUrl,
         },
         {
           version: 'v1.23.1+1',
-          registryUrl,
+          registryUrl: GlasskubePackagesDatasource.defaultRegistryUrl,
+        },
+      ],
+    });
+  });
+
+  it('should handle package manifest with references and custom url', async () => {
+    httpMock
+      .scope(customVersionsUrl.origin)
+      .get(customVersionsUrl.pathname)
+      .reply(200, versionsYaml);
+    httpMock
+      .scope(customPackageManifestUrl.origin)
+      .get(customPackageManifestUrl.pathname)
+      .reply(200, packageManifestYaml);
+    const response = await getPkgReleases({
+      datasource: GlasskubePackagesDatasource.id,
+      packageName: 'cloudnative-pg',
+      registryUrls: [customRegistryUrl],
+    });
+    expect(response).toEqual({
+      sourceUrl: 'https://github.com/cloudnative-pg/cloudnative-pg',
+      homepage: 'https://cloudnative-pg.io/',
+      registryUrl: customRegistryUrl,
+      releases: [
+        {
+          version: 'v1.22.0+1',
+          registryUrl: customRegistryUrl,
+        },
+        {
+          version: 'v1.23.1+1',
+          registryUrl: customRegistryUrl,
         },
       ],
     });
