676d2f24d1addedb93fb9430ad21960495da8edc
james0209 <jbrookes@confluent.io>
2024-06-30 16:13:17 +0000

    fix: adjust fileMatch

--- lib/modules/manager/mise/index.ts
+++ lib/modules/manager/mise/index.ts
@@ -5,7 +5,7 @@ export { extractPackageFile } from './extract';
 export const displayName = 'mise';
 
 export const defaultConfig = {
-  fileMatch: ['(^|/).mise.toml$'],
+  fileMatch: ['(^|/)\\.mise\\.toml$'],
 };
 
 // Shares the same datasources as asdf as they both support the same plugins.
