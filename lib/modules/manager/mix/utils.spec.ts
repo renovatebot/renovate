6038fa7b2036e2c7517dbf8ddedfae72efb25781
Bryan Naegele <bryan@simplebet.io>
2024-07-08 19:18:53 -0600

    util tests are sync

--- lib/modules/manager/mix/utils.spec.ts
+++ lib/modules/manager/mix/utils.spec.ts
@@ -1,7 +1,7 @@
 import { resolveAlias } from './utils';
 
 describe('modules/manager/mix/utils', () => {
-  it('returns the alias if package is a repo', async () => {
+  it('returns the alias if package is a repo', () => {
     expect(
       resolveAlias('repo:oban', {
         oban: 'https://getoban.pro/repo',
@@ -9,7 +9,7 @@ describe('modules/manager/mix/utils', () => {
     ).toBe('https://getoban.pro/repo');
   });
 
-  it('returns the package if package is not a repo', async () => {
+  it('returns the package if package is not a repo', () => {
     expect(
       resolveAlias('oban', {
         oban: 'https://getoban.pro/repo',
@@ -17,7 +17,7 @@ describe('modules/manager/mix/utils', () => {
     ).toBe('oban');
   });
 
-  it('returns the null if package is a repo but not in aliases', async () => {
+  it('returns the null if package is a repo but not in aliases', () => {
     expect(resolveAlias('repo:oban', {})).toBeNull();
   });
 });
