198de5843ceae96bb8f3f096bb779a198e3ab985
RahulGautamSingh <rahultesnik@gmail.com>
2024-07-13 13:19:31 +0530

    fix(manager/dockerfile): remove bom marker (#30156)

--- lib/modules/manager/dockerfile/extract.ts
+++ lib/modules/manager/dockerfile/extract.ts
@@ -244,6 +244,7 @@ export function extractPackageFile(
   _packageFile: string,
   config: ExtractConfig,
 ): PackageFileContent | null {
+  const sanitizedContent = content.replace(regEx(/^\uFEFF/), ''); // remove bom marker
   const deps: PackageDependency[] = [];
   const stageNames: string[] = [];
   const args: Record<string, string> = {};
@@ -253,8 +254,8 @@ export function extractPackageFile(
   let lookForEscapeChar = true;
   let lookForSyntaxDirective = true;
 
-  const lineFeed = content.indexOf('\r\n') >= 0 ? '\r\n' : '\n';
-  const lines = content.split(newlineRegex);
+  const lineFeed = sanitizedContent.indexOf('\r\n') >= 0 ? '\r\n' : '\n';
+  const lines = sanitizedContent.split(newlineRegex);
   for (let lineNumber = 0; lineNumber < lines.length; ) {
     const lineNumberInstrStart = lineNumber;
     let instruction = lines[lineNumber];
