13d25f737a0f9e9c7c4e4f82609a247a8fe80271
Bryan Naegele <bryan@simplebet.io>
2024-07-18 11:49:58 -0600

    file rename

--- tools/generate-protos.ts
+++ tools/generate-protos.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { ERROR } from 'bunyan';
+import { getProblems, logger } from '../lib/logger';
 import { exec } from './utils/exec';
 
 process.on('unhandledRejection', (err) => {
@@ -8,17 +9,17 @@ process.on('unhandledRejection', (err) => {
   process.exit(-1);
 });
 
-function moveFiles(sourceDir, destDir) {
+function moveFiles(sourceDir: string, destDir: string): void {
   try {
     // Read all files in the source directory
-    const files = fs.readdirSync(sourceDir);
+    const files: string[] = fs.readdirSync(sourceDir);
 
     // find and move generated ts files
-    const tsFiles = files.filter((file) => file.endsWith('.ts'));
+    const tsFiles: string[] = files.filter((file) => file.endsWith('.ts'));
 
     for (const file of tsFiles) {
-      const sourcePath = `${sourceDir}/${file}`;
-      const destPath = `${destDir}/${file}`;
+      const sourcePath: string = `${sourceDir}/${file}`;
+      const destPath: string = `${destDir}/${file}`;
 
       fs.renameSync(sourcePath, destPath);
     }
@@ -32,10 +33,15 @@ function moveFiles(sourceDir, destDir) {
 void (async () => {
   try {
     // protobuf definitions
+    logger.info('Generating protobufs');
     await generateHexProtos();
   } catch (err) {
+    logger.error({ err }, 'Unexpected error');
   } finally {
-    process.exit(1);
+    const loggerErrors = getProblems().filter((p) => p.level >= ERROR);
+    if (loggerErrors.length) {
+      process.exit(1);
+    }
   }
 })();
 
@@ -50,18 +56,25 @@ function generateProto(protos_path, file) {
     ]);
 
     if (res.signal) {
+      logger.error(`Signal received: ${res.signal}`);
       reject('');
       process.exit(-1);
     } else if (res.status && res.status !== 0) {
+      logger.error(`Error occured:\n${res.stderr || res.stdout}`);
       reject('');
       process.exit(res.status);
     } else {
+      logger.debug(
+        `Hex protos generation succeeded:\n${res.stdout || res.stderr}`,
+      );
       return resolve('');
     }
   });
 }
 
 async function generateHexProtos() {
+  logger.info('Generating Hex protos ...');
+
   const protos_path = './lib/modules/datasource/hex/protos';
   await generateProto(protos_path, 'package.proto');
   await generateProto(protos_path, 'signed.proto');
