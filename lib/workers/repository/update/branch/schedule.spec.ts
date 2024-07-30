8504ea89a769548c4e552b15ce0b61824dca1359
Rhys Arkins <rhys@arkins.net>
2024-07-30 22:15:59 +0200

    prettier-fix

--- lib/workers/repository/update/branch/schedule.spec.ts
+++ lib/workers/repository/update/branch/schedule.spec.ts
@@ -410,18 +410,18 @@ describe('workers/repository/update/branch/schedule', () => {
   });
   describe('cronstrue', () => {
     it('should correctly convert "0 22 4 * *" to human-readable format', () => {
-      const result = cronstrue.toString("0 22 4 * *");
-      expect(result).toBe("At 10:00 PM, on day 4 of the month");
+      const result = cronstrue.toString('0 22 4 * *');
+      expect(result).toBe('At 10:00 PM, on day 4 of the month');
     });
 
     it('should correctly convert "*/2 * * * *" to human-readable format', () => {
-      const result = cronstrue.toString("*/2 * * * *");
-      expect(result).toBe("Every 2 minutes");
+      const result = cronstrue.toString('*/2 * * * *');
+      expect(result).toBe('Every 2 minutes');
     });
 
     it('should correctly convert "0 23 * * *" to human-readable format', () => {
-      const result = cronstrue.toString("0 23 * * *");
-      expect(result).toBe("At 11:00 PM");
+      const result = cronstrue.toString('0 23 * * *');
+      expect(result).toBe('At 11:00 PM');
     });
   });
 });
