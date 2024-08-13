98027ccab84c9270e0761289af10d79a72f7f776
Michael Kriese <michael.kriese@visualon.de>
2024-08-13 10:20:46 +0200

    docs: validate with mkdocs

---
+++ tools/validate-docs.ts
@@ -0,0 +1,28 @@
+import { logger } from '../lib/logger';
+import { generateDocs } from './docs';
+import { exec } from './utils/exec';
+
+process.on('unhandledRejection', (err) => {
+  // Will print "unhandledRejection err is not defined"
+  logger.error({ err }, 'unhandledRejection');
+  process.exit(-1);
+});
+
+void (async () => {
+  logger.info('validating docs');
+  logger.info('* generate docs');
+  await generateDocs('tools/mkdocs', false);
+  logger.info('* running mkdocs');
+  const res = exec('poetry', ['run', 'mkdocs', 'build'], {
+    cwd: 'tools/mkdocs',
+  });
+  if (res.signal) {
+    logger.error(`Signal received: ${res.signal}`);
+    process.exit(-1);
+  } else if (res.status && res.status !== 0) {
+    logger.error(`Error occured:\n${res.stderr || res.stdout}`);
+    process.exit(res.status);
+  } else {
+    logger.debug(`Build succeeded:\n${res.stdout || res.stderr}`);
+  }
+})();
