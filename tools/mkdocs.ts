d90de484b1d32511204273ea7fe475890091cee4
Michael Kriese <michael.kriese@visualon.de>
2024-08-14 12:42:23 +0200

    docs: validate with mkdocs (#30735)
    Co-authored-by: HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>

---
+++ tools/mkdocs.ts
@@ -0,0 +1,43 @@
+import { Command } from 'commander';
+import fs from 'fs-extra';
+import { logger } from '../lib/logger';
+import { generateDocs } from './docs';
+import { exec } from './utils/exec';
+
+process.on('unhandledRejection', (err) => {
+  // Will print "unhandledRejection err is not defined"
+  logger.error({ err }, 'unhandledRejection');
+  process.exit(-1);
+});
+
+const program = new Command('pnpm build:mkdocs')
+  .description('Build mkdocs')
+  .option('--build <boolean>', 'build docs from source', (s) =>
+    s ? s !== 'false' : undefined,
+  );
+
+void (async () => {
+  await program.parseAsync();
+  const opts = program.opts();
+  logger.info('validating docs');
+  if (opts.build) {
+    logger.info('* generate docs');
+    await generateDocs('tools/mkdocs', false);
+  } else {
+    logger.info('* using prebuild docs from build step');
+    await fs.copy('tmp/docs', 'tools/mkdocs/docs');
+  }
+  logger.info('* running mkdocs build');
+  const res = exec('pdm', ['run', 'mkdocs', 'build'], {
+    cwd: 'tools/mkdocs',
+  });
+  if (res.signal) {
+    logger.error(`Signal received: ${res.signal}`);
+    process.exit(-1);
+  } else if (res.status && res.status !== 0) {
+    logger.error(`Error occured:\n${res.stderr || res.stdout}`);
+    process.exit(res.status);
+  } else {
+    logger.debug(`Build completed:\n${res.stdout || res.stderr}`);
+  }
+})();
