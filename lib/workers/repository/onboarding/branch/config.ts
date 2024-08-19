70360fd0d530fbf65257749905f0118e444202b5
LukasAuerbeck <17929465+LukasAuerbeck@users.noreply.github.com>
2024-08-19 12:35:20 +0200

    fix: fixed quote escapes
    Signed-off-by: LukasAuerbeck <17929465+LukasAuerbeck@users.noreply.github.com>

--- lib/workers/repository/onboarding/branch/config.ts
+++ lib/workers/repository/onboarding/branch/config.ts
@@ -16,9 +16,10 @@ async function getOnboardingConfig(
 
   const hasPredefinedPreset = onboardingConfig?.extends !== undefined;
   if (hasPredefinedPreset) {
-    logger.debug('Predefined onboardingConfig.extends found, so default org presets won't be used');
-  }
-  if (!hasPredefinedPreset) {
+    logger.debug(
+      "Predefined onboardingConfig.extends found, so default org presets won't be used",
+    );
+  } else {
     // TODO #22198 fix types
     const foundPreset = await searchDefaultOnboardingPreset(config.repository!);
 
