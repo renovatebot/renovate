bf2f3c3ca63db520e64bdce57f6cae7129857008
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-08-20 06:53:38 -0300

    fix: Revert "fix(github): Sanitize mentions in commit bodies (#30842)" (#30910)

--- lib/modules/platform/github/scm.ts
+++ lib/modules/platform/github/scm.ts
@@ -1,12 +1,7 @@
-import is from '@sindresorhus/is';
 import * as git from '../../../util/git';
 import type { CommitFilesConfig, LongCommitSha } from '../../../util/git/types';
 import { DefaultGitScm } from '../default-scm';
 import { commitFiles, isGHApp } from './';
-
-export function sanitizeMentions(input: string): string {
-  return input.replaceAll('@', '@\u{8203}');
-}
 
 export class GithubScm extends DefaultGitScm {
   override commitAndPush(
@@ -16,11 +11,6 @@ export class GithubScm extends DefaultGitScm {
     if (platformCommit === 'auto' && isGHApp()) {
       platformCommit = 'enabled';
     }
-
-    const sanitizedMessage = is.array(commitConfig.message)
-      ? commitConfig.message.map(sanitizeMentions)
-      : sanitizeMentions(commitConfig.message);
-    commitConfig.message = sanitizedMessage;
 
     return platformCommit === 'enabled'
       ? commitFiles(commitConfig)
