718c9ffda6de47e8a73a4853a877c923ecc6221e
Matthias Kay <matthias.kay@hlag.com>
2024-08-22 11:01:14 +0200

    fix api.ts

--- lib/modules/datasource/api.ts
+++ lib/modules/datasource/api.ts
@@ -1,7 +1,7 @@
 import { ArtifactoryDatasource } from './artifactory';
 import { AwsLambdaLayerDataSource } from './aws-lambda-layer';
-import { AwsMachineImageDataSource } from './aws-machine-image';
-import { AwsRdsDataSource } from './aws-rds';
+import { AwsMachineImageDatasource } from './aws-machine-image';
+import { AwsRdsDatasource } from './aws-rds';
 import { AzureBicepResourceDatasource } from './azure-bicep-resource';
 import { AzurePipelinesTasksDatasource } from './azure-pipelines-tasks';
 import { BazelDatasource } from './bazel';
@@ -70,8 +70,8 @@ export default api;
 
 api.set(ArtifactoryDatasource.id, new ArtifactoryDatasource());
 api.set(AwsLambdaLayerDataSource.id, new AwsLambdaLayerDataSource());
-api.set(AwsMachineImageDataSource.id, new AwsMachineImageDataSource());
-api.set(AwsRdsDataSource.id, new AwsRdsDataSource());
+api.set(AwsMachineImageDatasource.id, new AwsMachineImageDatasource());
+api.set(AwsRdsDatasource.id, new AwsRdsDatasource());
 api.set(AzureBicepResourceDatasource.id, new AzureBicepResourceDatasource());
 api.set(AzurePipelinesTasksDatasource.id, new AzurePipelinesTasksDatasource());
 api.set(BazelDatasource.id, new BazelDatasource());
