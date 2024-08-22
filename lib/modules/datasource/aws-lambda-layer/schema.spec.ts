f62203a83d8ba6a276b1407dc715ac428cee96c0
Matthias Kay <matthias.kay@hlag.com>
2024-08-22 10:06:17 +0200

    inline fixtures

--- lib/modules/datasource/aws-lambda-layer/schema.spec.ts
+++ lib/modules/datasource/aws-lambda-layer/schema.spec.ts
@@ -1,10 +1,13 @@
-import { Fixtures } from '../../../../test/fixtures';
 import { AwsLambdaLayerFilterMetadata } from './schema';
 
 describe('modules/datasource/aws-lambda-layer/schema', () => {
   describe('AwsLambdaLayerFilterMetadata', () => {
     it('parses metadata', () => {
-      const metadataJson = Fixtures.get('layer-filter-valid.json');
+      const metadataJson = '{\n' +
+        '  "runtime": "nodejs14.x",\n' +
+        '  "architecture": "x86_64",\n' +
+        '  "arn": "arn:aws:lambda:us-east-1:123456789012:layer:my-layer"\n' +
+        '}';
       const metadata = AwsLambdaLayerFilterMetadata.parse(
         JSON.parse(metadataJson),
       );
@@ -15,31 +18,38 @@ describe('modules/datasource/aws-lambda-layer/schema', () => {
       );
       expect(metadata.runtime).toBe('nodejs14.x');
     });
-  });
 
-  it('has optional runtime attribute', () => {
-    const metadataJson = Fixtures.get('layer-filter-no-runtime.json');
-    const metadata = AwsLambdaLayerFilterMetadata.parse(
-      JSON.parse(metadataJson),
-    );
+    it('has optional runtime attribute', () => {
+      const metadataJson = '{\n' +
+        '  "architecture": "x86_64",\n' +
+        '  "arn": "arn:aws:lambda:us-east-1:123456789012:layer:my-layer"\n' +
+        '}';
 
-    expect(metadata.architecture).toBe('x86_64');
-    expect(metadata.arn).toBe(
-      'arn:aws:lambda:us-east-1:123456789012:layer:my-layer',
-    );
-    expect(metadata.runtime).toBeUndefined();
-  });
+      const metadata = AwsLambdaLayerFilterMetadata.parse(
+        JSON.parse(metadataJson),
+      );
 
-  it('has optional architecture attribute', () => {
-    const metadataJson = Fixtures.get('layer-filter-no-architecture.json');
-    const metadata = AwsLambdaLayerFilterMetadata.parse(
-      JSON.parse(metadataJson),
-    );
+      expect(metadata.architecture).toBe('x86_64');
+      expect(metadata.arn).toBe(
+        'arn:aws:lambda:us-east-1:123456789012:layer:my-layer',
+      );
+      expect(metadata.runtime).toBeUndefined();
+    });
 
-    expect(metadata.architecture).toBeUndefined();
-    expect(metadata.arn).toBe(
-      'arn:aws:lambda:us-east-1:123456789012:layer:my-layer',
-    );
-    expect(metadata.runtime).toBe('nodejs14.x');
+    it('has optional architecture attribute', () => {
+      const metadataJson = '{\n' +
+        '  "runtime": "nodejs14.x",\n' +
+        '  "arn": "arn:aws:lambda:us-east-1:123456789012:layer:my-layer"\n' +
+        '}';
+      const metadata = AwsLambdaLayerFilterMetadata.parse(
+        JSON.parse(metadataJson),
+      );
+
+      expect(metadata.architecture).toBeUndefined();
+      expect(metadata.arn).toBe(
+        'arn:aws:lambda:us-east-1:123456789012:layer:my-layer',
+      );
+      expect(metadata.runtime).toBe('nodejs14.x');
+    });
   });
 });
