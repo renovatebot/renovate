1106ac7cfa592a918d4b485fc839e1301e9215b0
Jason Sipula <SnakeDoc@users.noreply.github.com>
2024-08-25 02:41:08 -0700

    Update lib/modules/manager/gleam/extract.ts
    Co-authored-by: Rhys Arkins <rhys@arkins.net>

--- lib/modules/manager/gleam/extract.ts
+++ lib/modules/manager/gleam/extract.ts
@@ -66,7 +66,7 @@ export async function extractPackageFile(
   try {
     gleamToml = GleamToml.parse(content);
   } catch (err) {
-    logger.debug({ err }, 'Error parsing package file content');
+    logger.debug({ err }, 'Error parsing Gleam package file content');
     return null;
   }
 
