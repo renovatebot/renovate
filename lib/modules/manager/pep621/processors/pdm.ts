9558706cfaab0deb80a8f46d5ab581ac97ba3e28
secustor <sebastian@poxhofer.at>
2024-09-03 19:25:33 +0200

    Revert "fix(managers/pep621): use :all group"
    This reverts commit 9dbbe1030d5b7d700b36f3a95e9b351ecea093f2.

--- lib/modules/manager/pep621/processors/pdm.ts
+++ lib/modules/manager/pep621/processors/pdm.ts
@@ -169,18 +169,10 @@ function generateCMDs(updatedDeps: Upgrade[]): string[] {
   const packagesByCMD: Record<string, string[]> = {};
   for (const dep of updatedDeps) {
     switch (dep.depType) {
-      case depTypes.optionalDependencies: {
-        addPackageToCMDRecord(
-          packagesByCMD,
-          `${pdmUpdateCMD} -G ":all"`,
-          dep.packageName!,
-        );
-        break;
-      }
       case depTypes.pdmDevDependencies: {
         addPackageToCMDRecord(
           packagesByCMD,
-          `${pdmUpdateCMD} -d -G ":all"`,
+          `${pdmUpdateCMD} -d`,
           dep.packageName!,
         );
         break;
