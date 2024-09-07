718f187f2f3791a6ebc6b6c9d5ff8bef32651488
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-09-07 10:17:40 -0300

    Fix s3 tests

--- lib/modules/datasource/maven/s3.spec.ts
+++ lib/modules/datasource/maven/s3.spec.ts
@@ -51,34 +51,7 @@ describe('modules/datasource/maven/s3', () => {
           Bucket: 'repobucket',
           Key: 'org/example/package/maven-metadata.xml',
         })
-        .resolvesOnce({ Body: meta as never })
-        .on(HeadObjectCommand, {
-          Bucket: 'repobucket',
-          Key: 'org/example/package/0.0.1/package-0.0.1.pom',
-        })
-        .resolvesOnce({ DeleteMarker: true })
-        .on(HeadObjectCommand, {
-          Bucket: 'repobucket',
-          Key: 'org/example/package/1.0.0/package-1.0.0.pom',
-        })
-        .rejectsOnce('NoSuchKey')
-        .on(HeadObjectCommand, {
-          Bucket: 'repobucket',
-          Key: 'org/example/package/1.0.1/package-1.0.1.pom',
-        })
-        .rejectsOnce('Unknown')
-        .on(HeadObjectCommand, {
-          Bucket: 'repobucket',
-          Key: 'org/example/package/1.0.2/package-1.0.2.pom',
-        })
-        .resolvesOnce({})
-        .on(HeadObjectCommand, {
-          Bucket: 'repobucket',
-          Key: 'org/example/package/1.0.3/package-1.0.3.pom',
-        })
-        .resolvesOnce({
-          LastModified: DateTime.fromISO(`2020-01-01T00:00:00.000Z`).toJSDate(),
-        });
+        .resolvesOnce({ Body: meta as never });
 
       const res = await get('org.example:package', baseUrlS3);
 
@@ -88,8 +61,11 @@ describe('modules/datasource/maven/s3', () => {
         name: 'package',
         registryUrl: 's3://repobucket',
         releases: [
+          { version: '0.0.1' },
+          { version: '1.0.0' },
+          { version: '1.0.1' },
           { version: '1.0.2' },
-          { version: '1.0.3', releaseTimestamp: '2020-01-01T00:00:00.000Z' },
+          { version: '1.0.3' },
         ],
         isPrivate: true,
       });
