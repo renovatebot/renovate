698af36c88e3821c9c5cdf3f265d05c380e562c7
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-09-29 19:33:52 -0300

    test: Improve errors for missing/unused HTTP mocks

--- test/http-mock.ts
+++ test/http-mock.ts
@@ -1,5 +1,7 @@
 import type { Url } from 'node:url';
 import { afterAll, afterEach, beforeAll } from '@jest/globals';
+import { codeBlock } from 'common-tags';
+import fs from 'fs-extra';
 // eslint-disable-next-line no-restricted-imports
 import nock from 'nock';
 import { makeGraphqlSnapshot } from './graphql-snapshot';
@@ -7,18 +9,24 @@ import { makeGraphqlSnapshot } from './graphql-snapshot';
 // eslint-disable-next-line no-restricted-imports
 export type { Scope, ReplyHeaders, Body } from 'nock';
 
-interface RequestLogItem {
+interface RequestLog {
   headers: Record<string, string>;
   method: string;
   url: string;
+  status: number;
   body?: any;
   graphql?: any;
 }
 
+interface MissingRequestLog {
+  method: string;
+  url: string;
+}
+
 type BasePath = string | RegExp | Url;
 
-let requestLog: RequestLogItem[] = [];
-let missingLog: string[] = [];
+let requestsDone: RequestLog[] = [];
+let requestsMissing: MissingRequestLog[] = [];
 
 type TestRequest = {
   method: string;
@@ -27,9 +35,9 @@ type TestRequest = {
 
 function onMissing(req: TestRequest, opts?: TestRequest): void {
   if (opts) {
-    missingLog.push(`  ${opts.method} ${opts.href}`);
+    requestsMissing.push({ method: opts.method, url: opts.href });
   } else {
-    missingLog.push(`  ${req.method} ${req.href}`);
+    requestsMissing.push({ method: req.method, url: req.href });
   }
 }
 
@@ -37,31 +45,52 @@ export function allUsed(): boolean {
   return nock.isDone();
 }
 
+function getPending(): string[] {
+  return nock.pendingMocks().map((req) => `- ${req.replace(':443/', '/')}`);
+}
+
 /**
  *  Clear nock state. Will be called in `afterEach`
- *  @argument throwOnPending Use `false` to simply clear mocks.
+ *
+ *  @argument check Use `false` to clear mocks without checking for the missing/unused ones.
+ *                  Disabling such checks is discouraged.
  */
-export function clear(throwOnPending = true): void {
+export function clear(check = true): void {
   const isDone = nock.isDone();
-  const pending = nock.pendingMocks();
+  const pending = getPending();
+
   nock.abortPendingRequests();
   nock.cleanAll();
-  const missing = missingLog;
-  requestLog = [];
-  missingLog = [];
-  if (missing.length && throwOnPending) {
-    throw new Error(`Missing mocks!\n * ${missing.join('\n * ')}`);
+
+  const done = requestsDone;
+  requestsDone = [];
+
+  const missing = requestsMissing;
+  requestsMissing = [];
+
+  if (!check) {
+    return;
   }
-  if (!isDone && throwOnPending) {
-    throw new Error(`Pending mocks!\n * ${pending.join('\n * ')}`);
+
+  if (missing.length) {
+    const err = new Error(missingHttpMockMessage(done, missing));
+    massageHttpMockStacktrace(err);
+    throw err;
+  }
+
+  if (!isDone) {
+    const err = new Error(unusedHttpMockMessage(done, pending));
+    massageHttpMockStacktrace(err);
+    throw err;
   }
 }
 
 export function scope(basePath: BasePath, options?: nock.Options): nock.Scope {
-  return nock(basePath, options).on('request', (req) => {
+  return nock(basePath, options).on('replied', (req) => {
     const { headers, method } = req;
     const url = req.options?.href;
-    const result: RequestLogItem = { headers, method, url };
+    const status = req.response?.statusCode;
+    const result: RequestLog = { headers, method, url, status };
     const requestBody = req.requestBodyBuffers?.[0]?.toString();
 
     if (requestBody && headers['content-type'] === 'application/json') {
@@ -77,30 +106,141 @@ export function scope(basePath: BasePath, options?: nock.Options): nock.Scope {
         result.body = requestBody;
       }
     }
-    requestLog.push(result);
+    requestsDone.push(result);
   });
 }
 
-export function getTrace(): RequestLogItem[] /* istanbul ignore next */ {
-  const errorLines: string[] = [];
-  if (missingLog.length) {
-    errorLines.push('Missing mocks:');
-    errorLines.push(...missingLog);
+export function getTrace(): RequestLog[] /* istanbul ignore next */ {
+  return requestsDone;
+}
+
+function massageHttpMockStacktrace(err: Error): void {
+  if (!err.stack) {
+    return;
   }
-  if (!nock.isDone()) {
-    errorLines.push('Unused mocks:');
-    errorLines.push(...nock.pendingMocks().map((x) => `  ${x}`));
+
+  const state = expect.getState();
+  if (!state.currentTestName || !state.testPath) {
+    return;
   }
-  if (errorLines.length) {
-    throw new Error(
-      [
-        'Completed requests:',
-        ...requestLog.map(({ method, url }) => `  ${method} ${url}`),
-        ...errorLines,
-      ].join('\n'),
-    );
+
+  const content = fs.readFileSync(state.testPath, { encoding: 'utf8' });
+
+  let testName = state.currentTestName.replace(/^[^\s]*\s/, '');
+  let idx = content.indexOf(testName);
+  while (testName.length) {
+    if (idx !== -1) {
+      break;
+    }
+
+    testName = testName.replace(/^[^\s]*\s/, '');
+    idx = content.indexOf(testName);
   }
-  return requestLog;
+
+  if (idx === -1) {
+    return;
+  }
+
+  const lines = content.slice(0, idx).split('\n');
+  const lineNum = lines.length;
+  const linePos = lines[lines.length - 1].length + 1;
+
+  const stackLine = `    at test(${state.testPath}:${lineNum}:${linePos})`;
+  err.stack = err.stack.replace(/\+\+\+.*$/s, stackLine);
+}
+
+function missingHttpMockMessage(
+  done: RequestLog[],
+  missing: MissingRequestLog[],
+): string {
+  const blocks: string[] = [];
+
+  const title = codeBlock`
+    *** Missing HTTP mocks ***
+  `;
+
+  const explanation = codeBlock`
+    ---
+
+    Renovate testing strategy requires that every HTTP request
+    has a corresponding mock.
+
+    This error occurs when some of the request aren't mocked.
+
+    Let's suppose your code performs two HTTP calls:
+
+      GET   https://example.com/foo/bar/fail     404  <without body>
+      POST  https://example.com/foo/bar/success  200  { "ok": true }
+
+    The unit test should have this mock:
+
+      httpMock.scope('https://example.com/foo/bar')
+        .get('/fail')
+        .reply(404)
+        .post('/success')
+        .reply(200, { ok: true });
+
+    Note: \`httpMock.scope(...)\` is the Renovate-specific construct.
+          The scope object itself is provided by the \`nock\` library.
+
+    Details: https://github.com/nock/nock?tab=readme-ov-file#usage
+
+    +++
+  `;
+
+  blocks.push(title);
+
+  blocks.push(codeBlock`
+    ${missing.map(({ method, url }) => `- ${method} ${url}`).join('\n')}
+  `);
+
+  if (done.length) {
+    blocks.push(codeBlock`
+      Requests done:
+
+      ${done.map(({ method, url, status }) => `- ${method} ${url} [${status}]`).join('\n')}
+    `);
+  }
+
+  blocks.push(explanation);
+
+  return blocks.join('\n\n');
+}
+
+function unusedHttpMockMessage(done: RequestLog[], pending: string[]): string {
+  const blocks: string[] = [];
+
+  const title = codeBlock`
+    *** Unused HTTP mocks ***
+  `;
+
+  const explanation = codeBlock`
+    ---
+
+    Renovate testing strategy requires that every HTTP request
+    has a corresponding mock.
+
+    This error occurs because some of the created mocks are unused.
+
+    In most cases, you simply need to remove these.
+
+    +++
+  `;
+
+  blocks.push(title);
+  blocks.push(pending.join('\n'));
+
+  if (done.length) {
+    blocks.push(codeBlock`
+      Requests done:
+
+      ${done.map(({ method, url, status }) => `- ${method} ${url} [${status}]`).join('\n')}
+    `);
+  }
+
+  blocks.push(explanation);
+
+  return blocks.join('\n\n');
 }
 
 // init nock
