89a2493bed01439301469f5306ce4aa3c68f0fad
Rahul Gautam Singh <rahultesnik@gmail.com>
2024-10-01 04:01:12 +0530

    test: add ReplacementsSchema

--- tools/schemas/schema.ts
+++ tools/schemas/schema.ts
@@ -11,4 +11,46 @@ const MonorepoSchema = z.object({
   patternGroups: UrlSchema,
 });
 
-export { MonorepoSchema };
+const PackageRuleSchema = z.object({
+  matchCurrentVersion: z.string().optional(),
+  matchDatasources: z.array(z.string()),
+  matchPackageNames: z.array(z.string()),
+  replacementName: z.string().optional(),
+  replacementVersion: z.string().optional(),
+  description: z.string().optional(),
+  replacementNameTemplate: z.string().optional(),
+});
+
+const RuleSetSchema = z.object({
+  description: z.string(),
+  packageRules: z
+    .array(PackageRuleSchema)
+    .min(1)
+    .refine(
+      (rules) =>
+        rules.some(
+          (rule) =>
+            rule.replacementName !== undefined ||
+            rule.replacementNameTemplate !== undefined,
+        ),
+      {
+        message:
+          'At least one package rule must have either replacementName or replacementNameTemplate',
+      },
+    ),
+});
+
+const AllSchema = z.object({
+  description: z.string(),
+  extends: z.array(z.string()),
+  ignoreDeps: z.array(z.string()).optional(),
+});
+
+const ReplacementsSchema = z
+  .object({
+    $schema: z.string(),
+    all: AllSchema,
+  })
+  .catchall(RuleSetSchema);
+
+export { MonorepoSchema, ReplacementsSchema };
