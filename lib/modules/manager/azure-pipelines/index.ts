4fc0d85225a54013542119f1fe801c64326b72d1
Markus Schulte <mail@schulte-development.de>
2024-10-01 18:44:59 +0200

    feat(manager/azure-pipelines): Include `.azuredevops` folder paths
    Revised the `fileMatch` patterns to include `.azuredevops` folder paths (#31730).

--- lib/modules/manager/azure-pipelines/index.ts
+++ lib/modules/manager/azure-pipelines/index.ts
@@ -4,7 +4,10 @@ import { GitTagsDatasource } from '../../datasource/git-tags';
 export { extractPackageFile } from './extract';
 
 export const defaultConfig = {
-  fileMatch: ['azure.*pipelines?.*\\.ya?ml$'],
+  fileMatch: [
+    '(^|/).azuredevops/.+\\.ya?ml$',
+    'azure.*pipelines?.+\\.ya?ml$',
+  ],
   enabled: false,
 };
 
