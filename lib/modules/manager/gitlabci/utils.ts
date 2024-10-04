354e6f866c1e25bece64068ad54ee2fa0ccf2d90
Timo Webler <timo.webler@dkd.de>
2024-10-04 15:50:05 +0200

    refactor(manager/gitlabci): remove method "replaceReferenceTags" (#31787)

--- lib/modules/manager/gitlabci/utils.ts
+++ lib/modules/manager/gitlabci/utils.ts
@@ -2,19 +2,6 @@ import { regEx } from '../../../util/regex';
 import { getDep } from '../dockerfile/extract';
 import type { PackageDependency } from '../types';
 
-const re = /!reference \[[^\]]+\]/g;
-
-/**
- * Replaces GitLab reference tags before parsing, because our yaml parser cannot process them anyway.
- * @param content pipeline yaml
- * @returns replaced pipeline content
- * https://docs.gitlab.com/ee/ci/yaml/#reference-tags
- */
-export function replaceReferenceTags(content: string): string {
-  const res = content.replace(re, '');
-  return res;
-}
-
 const depProxyRe = regEx(
   `(?<prefix>\\$\\{?CI_DEPENDENCY_PROXY_(?:DIRECT_)?GROUP_IMAGE_PREFIX\\}?/)(?<depName>.+)`,
 );
