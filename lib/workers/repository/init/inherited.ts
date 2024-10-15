8786fba27314f480d3ad8aff41cc2038478e14df
Rahul Gautam Singh <rahultesnik@gmail.com>
2024-10-15 22:25:56 +0530

    skip npm preset resolution

--- lib/workers/repository/init/inherited.ts
+++ lib/workers/repository/init/inherited.ts
@@ -108,13 +108,10 @@ export async function mergeInheritedConfig(
 
   let returnConfig = filteredConfig;
 
-  // Decrypt before resolving, in case it contains npm authentication for any preset
-  const decryptedConfig = await decryptConfig(returnConfig, config.repository);
-
-  // Decrypt after resolving, in case the preset contains npm authentication instead
+  // Decrypt after resolving, in case the preset contains npm authentication
   logger.debug('Resolving presets found in inherited config');
   const resolvedConfig = await decryptConfig(
-    await resolveConfigPresets(decryptedConfig, config, config.ignorePresets),
+    await resolveConfigPresets(returnConfig, config, config.ignorePresets),
     config.repository,
   );
   logger.trace({ config: resolvedConfig }, 'Resolved inherited config');
