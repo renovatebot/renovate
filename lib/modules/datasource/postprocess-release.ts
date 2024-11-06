cc5d7b804cfa0c368047946b8452b56b3f4eb368
Sergei Zharinov <zharinov@users.noreply.github.com>
2024-11-06 17:09:00 -0300

    fix: Avoid rejecting releases when `extractVersion` is used

--- lib/modules/datasource/postprocess-release.ts
+++ lib/modules/datasource/postprocess-release.ts
@@ -49,6 +49,20 @@ export async function postprocessRelease(
     );
 
     if (result === 'reject') {
+      if (config.extractVersion) {
+        logger.debug(
+          {
+            datasource,
+            packageName,
+            registryUrl,
+            version: release.version,
+            extractVersion: config.extractVersion,
+          },
+          'Rejected release combined with `extractVersion`: preserving the release',
+        );
+        return release;
+      }
+
       logger.debug(
         { datasource, packageName, registryUrl, version: release.version },
         'Rejected release',
