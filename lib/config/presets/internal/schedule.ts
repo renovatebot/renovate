3bed36c99989cd9c680f58a702de717aca2b610f
Rahul Gautam Singh <rahultesnik@gmail.com>
2024-11-27 02:44:58 +0530

    presets: improve description

--- lib/config/presets/internal/schedule.ts
+++ lib/config/presets/internal/schedule.ts
@@ -6,7 +6,7 @@ const daily = ['* 0-3 * * *'];
 const earlyMondays = ['* 0-3 * * 1'];
 const monthly = ['* 0-3 1 * *'];
 const nonOfficeHours = ['* 0-4,22-23 * * 1-5', '* * * * 6,7'];
-const quarterly = ['* * * */3 *'];
+const quarterly = ['* * 1 */3 *'];
 const weekdays = ['* * * * 1-5'];
 const weekends = ['* * * * 6,7'];
 const yearly = ['* * 1 */12 *'];
@@ -14,32 +14,34 @@ const yearly = ['* * 1 */12 *'];
 export const presets: Record<string, Preset> = {
   automergeDaily: {
     automergeSchedule: daily,
-    description: 'Schedule automerge daily.',
+    description: 'Schedule automerge daily before 4 AM.',
   },
   automergeEarlyMondays: {
     automergeSchedule: earlyMondays,
-    description: 'Weekly automerge schedule on early Monday mornings.',
+    description: 'Schedule automerge on Monday mornings (before 4 AM).',
   },
   automergeMonthly: {
     automergeSchedule: monthly,
-    description: 'Schedule automerge monthly.',
+    description:
+      'Schedule automerge for the first day of each month, before 4 AM.',
   },
   automergeNonOfficeHours: {
     automergeSchedule: nonOfficeHours,
     description:
-      'Schedule automerge for typical non-office hours (night time and weekends).',
+      'Schedule automerge during typical non-office hours on weekdays (i.e., 10 PM - 5 AM) and anytime on weekends.',
   },
   automergeQuarterly: {
     automergeSchedule: quarterly,
-    description: 'Schedule automerge quarterly.',
+    description:
+      'Schedule automerge on the first day of each quarter (i.e., January, April, July, October).',
   },
   automergeWeekdays: {
     automergeSchedule: weekdays,
-    description: 'Schedule automerge for weekdays.',
+    description: 'Schedule automerge anytime on weekdays.',
   },
   automergeWeekends: {
     automergeSchedule: weekends,
-    description: 'Schedule automerge for weekends.',
+    description: 'Schedule automerge anytime on weekends.',
   },
   automergeWeekly: {
     description: 'Schedule automerge weekly.',
@@ -47,27 +49,30 @@ export const presets: Record<string, Preset> = {
   },
   automergeYearly: {
     automergeSchedule: yearly,
-    description: 'Schedule automerge once a year (not recommended).',
+    description:
+      'Schedule automerge once a year on the first day of January (not recommended).',
   },
   daily: {
-    description: 'Schedule daily.',
+    description: 'Schedule daily before 4 AM.',
     schedule: daily,
   },
   earlyMondays: {
-    description: 'Weekly schedule on early Monday mornings.',
+    description: 'Weekly schedule on early Monday mornings (before 4 AM).',
     schedule: earlyMondays,
   },
   monthly: {
-    description: 'Schedule monthly.',
+    description:
+      'Schedule once a month on the first day of the month before 4 AM.',
     schedule: monthly,
   },
   nonOfficeHours: {
     description:
-      'Schedule for typical non-office hours (night time and weekends).',
+      'Schedule during typical non-office hours on weekdays (i.e., 10 PM - 5 AM) and anytime on weekends.',
     schedule: nonOfficeHours,
   },
   quarterly: {
-    description: 'Schedule quarterly.',
+    description:
+      'Schedule on the first day of each quarter (i.e., January, April, July, October).',
     schedule: quarterly,
   },
   weekdays: {
@@ -83,7 +88,8 @@ export const presets: Record<string, Preset> = {
     extends: ['schedule:earlyMondays'],
   },
   yearly: {
-    description: 'Schedule once a year (not recommended).',
+    description:
+      'Schedule once a year on the first day of January (not recommended).',
     schedule: yearly,
   },
 };
