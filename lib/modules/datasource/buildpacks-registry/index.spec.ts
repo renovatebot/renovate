9f72055f51f19c5cbf9ff9619841e88950a81b77
Ralf Pannemans <ralf.pannemans@sap.com>
2024-11-29 14:33:41 +0100

    Style changes

--- lib/modules/datasource/buildpacks-registry/index.spec.ts
+++ lib/modules/datasource/buildpacks-registry/index.spec.ts
@@ -3,27 +3,50 @@ import * as httpMock from '../../../../test/http-mock';
 import { BuildpacksRegistryDatasource } from '.';
 
 const baseUrl = 'https://registry.buildpacks.io/api/v1/buildpacks/';
-const herokuResult = {"latest":{"version":"0.17.1","namespace":"heroku","name":"python","description":"Heroku's buildpack for Python applications.","homepage":"https://github.com/heroku/buildpacks-python","licenses":["BSD-3-Clause"],"stacks":["*"],"id":"75946bf8-3f6a-4af0-a757-614bebfdfcd6"},"versions":[{"version":"0.17.1","_link":"https://registry.buildpacks.io//api/v1/buildpacks/heroku/python/0.17.1"},{"version":"0.17.0","_link":"https://registry.buildpacks.io//api/v1/buildpacks/heroku/python/0.17.0"}]};
-const emptResult = {}
 
 describe('modules/datasource/buildpacks-registry/index', () => {
   describe('getReleases', () => {
     it('processes real data', async () => {
-      httpMock.scope(baseUrl).get('/heroku/python').reply(200, herokuResult);
+      httpMock
+        .scope(baseUrl)
+        .get('/heroku/python')
+        .reply(200, {
+          latest: {
+            version: '0.17.1',
+            namespace: 'heroku',
+            name: 'python',
+            description: "Heroku's buildpack for Python applications.",
+            homepage: 'https://github.com/heroku/buildpacks-python',
+            licenses: ['BSD-3-Clause'],
+            stacks: ['*'],
+            id: '75946bf8-3f6a-4af0-a757-614bebfdfcd6',
+          },
+          versions: [
+            {
+              version: '0.17.1',
+              _link:
+                'https://registry.buildpacks.io//api/v1/buildpacks/heroku/python/0.17.1',
+            },
+            {
+              version: '0.17.0',
+              _link:
+                'https://registry.buildpacks.io//api/v1/buildpacks/heroku/python/0.17.0',
+            },
+          ],
+        });
       const res = await getPkgReleases({
         datasource: BuildpacksRegistryDatasource.id,
         packageName: 'heroku/python',
       });
-      const expectedResponse = {
+      expect(res).toEqual({
         registryUrl: 'https://registry.buildpacks.io/',
         releases: [{ version: '0.17.0' }, { version: '0.17.1' }],
         sourceUrl: 'https://github.com/heroku/buildpacks-python',
-      };
-      expect(res).toEqual(expectedResponse);
+      });
     });
 
     it('returns null on empty result', async () => {
-      httpMock.scope(baseUrl).get('/heroku/empty').reply(200, emptResult);
+      httpMock.scope(baseUrl).get('/heroku/empty').reply(200, {});
       const res = await getPkgReleases({
         datasource: BuildpacksRegistryDatasource.id,
         packageName: 'heroku/empty',
