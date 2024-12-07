d2c3b5edf4772c883cb832f523cacf0483db0a6c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-12-07 01:03:27 +0100

    Fix prettier

--- lib/modules/manager/nix/extract.ts
+++ lib/modules/manager/nix/extract.ts
@@ -117,9 +117,7 @@ export async function extractPackageFile(
           replaceString: flakeLocked.rev,
           datasource: GitRefsDatasource.id,
           // type tarball always contains this link
-          packageName: (
-            flakeOriginal.url!
-          ).replace(
+          packageName: (flakeOriginal.url!).replace(
             lockableHTTPTarballProtocol,
             'https://$<domain>/$<owner>/$<repo>',
           ),
