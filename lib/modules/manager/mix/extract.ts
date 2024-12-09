e491ff46d226dacb2515394d2992b4cdd7657434
Rhys Arkins <rhys@arkins.net>
2024-12-09 20:17:55 +0100

    Update extract.ts
    Co-authored-by: Michael Kriese <michael.kriese@gmx.de>

--- lib/modules/manager/mix/extract.ts
+++ lib/modules/manager/mix/extract.ts
@@ -45,9 +45,10 @@ export async function extractPackageFile(
         const ref = refRegexp.exec(opts)?.groups?.value;
         const branchOrTag = branchOrTagRegexp.exec(opts)?.groups?.value;
         const organization = organizationRegexp.exec(opts)?.groups?.value;
+        const hexGroups = hexRegexp.exec(opts)?.groups;
         const hex =
-          hexRegexp.exec(opts)?.groups?.strValue ??
-          hexRegexp.exec(opts)?.groups?.atomValue;
+          hexGroups?.strValue ??
+          hexGroups?.atomValue;
 
         let dep: PackageDependency;
 
