a6db5677801884c3c7c9d360dd23db7bb32fc8b6
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2024-12-11 13:20:20 +0200

    create logger class

--- lib/logger/index.ts
+++ lib/logger/index.ts
@@ -17,9 +17,7 @@ import {
   withSanitizer,
 } from './utils';
 
-let logContext: string = getEnv('LOG_CONTEXT') ?? nanoid();
-let curMeta: Record<string, unknown> = {};
-
+const logContext: string = getEnv('LOG_CONTEXT') ?? nanoid();
 const problems = new ProblemStream();
 
 let stdoutLevel = validateLogLevel(getEnv('LOG_LEVEL'), 'info');
@@ -67,7 +65,10 @@ const bunyanLogger = bunyan.createLogger({
 });
 
 const logFactory = (
+  bunyanLogger: bunyan,
   _level: bunyan.LogLevelString,
+  curMeta: Record<string, unknown>,
+  logContext: string,
 ): ((p1: unknown, p2: unknown) => void) => {
   return (p1: any, p2: any): void => {
     let level = _level;
@@ -109,23 +110,94 @@ const loggerLevels: bunyan.LogLevelString[] = [
   'fatal',
 ];
 
-export const logger: Logger = { once: { reset: onceReset } } as any;
+export class RenovateLogger implements Logger {
+  logger: Logger = { once: { reset: onceReset } } as any;
 
-loggerLevels.forEach((loggerLevel) => {
-  logger[loggerLevel] = logFactory(loggerLevel) as never;
+  constructor(
+    private readonly bunyanLogger: bunyan,
+    private context: string,
+    private meta: Record<string, unknown>,
+  ) {
+    for (const level of loggerLevels) {
+      this.setupLoggerLevel(level);
+    }
+  }
 
-  const logOnceFn = (p1: any, p2: any): void => {
-    once(() => {
-      const logFn = logger[loggerLevel];
-      if (is.undefined(p2)) {
-        logFn(p1);
-      } else {
-        logFn(p1, p2);
+  addStream(stream: bunyan.Stream): void {
+    this.bunyanLogger.addStream(withSanitizer(stream));
+  }
+
+  once = this.logger.once;
+
+  trace = this.log.bind(this, 'trace');
+  debug = this.log.bind(this, 'debug');
+  info = this.log.bind(this, 'info');
+  warn = this.log.bind(this, 'warn');
+  error = this.log.bind(this, 'error');
+  fatal = this.log.bind(this, 'fatal');
+
+  get logContext(): string {
+    return this.context;
+  }
+
+  set logContext(context: string) {
+    this.context = context;
+  }
+
+  setMeta(obj: Record<string, unknown>): void {
+    this.meta = { ...obj };
+  }
+
+  addMeta(obj: Record<string, unknown>): void {
+    this.meta = { ...this.meta, ...obj };
+  }
+
+  removeMeta(fields: string[]): void {
+    for (const key of Object.keys(this.meta)) {
+      if (fields.includes(key)) {
+        delete this.meta[key];
       }
-    }, logOnceFn);
-  };
-  logger.once[loggerLevel] = logOnceFn as never;
-});
+    }
+  }
+
+  private setupLoggerLevel(level: bunyan.LogLevelString): void {
+    this.logger[level] = this.createLogFactory(level) as never;
+    this.logger.once[level] = this.createLogOnceFunction(level);
+  }
+
+  private createLogFactory(
+    level: bunyan.LogLevelString,
+  ): (p1: unknown, p2: unknown) => void {
+    return logFactory(this.bunyanLogger, level, this.meta, this.context);
+  }
+
+  private createLogOnceFunction(
+    level: bunyan.LogLevelString,
+  ): (p1: string | Record<string, any>, p2?: string) => void {
+    const logOnceFn = (p1: string | Record<string, any>, p2?: string): void => {
+      once(() => {
+        this.log(level, p1, p2);
+      }, logOnceFn);
+    };
+    return logOnceFn;
+  }
+
+  private log(
+    level: bunyan.LogLevelString,
+    p1: string | Record<string, any>,
+    p2?: string,
+  ): void {
+    const logFn = this.logger[level];
+    if (is.string(p1)) {
+      logFn(p1);
+    } else {
+      logFn(p1, p2);
+    }
+  }
+}
+
+const loggerInternal = new RenovateLogger(bunyanLogger, logContext, {});
+export const logger: Logger = loggerInternal;
 
 const logFile = getEnv('LOG_FILE');
 // istanbul ignore if: not easily testable
@@ -142,36 +214,32 @@ if (is.string(logFile)) {
 }
 
 export function setContext(value: string): void {
-  logContext = value;
+  loggerInternal.logContext = value;
 }
 
 export function getContext(): any {
-  return logContext;
+  return loggerInternal.logContext;
 }
 
 // setMeta overrides existing meta, may remove fields if no longer existing
 export function setMeta(obj: Record<string, unknown>): void {
-  curMeta = { ...obj };
+  loggerInternal.setMeta(obj);
 }
 
 // addMeta overrides or adds fields but does not remove any
 export function addMeta(obj: Record<string, unknown>): void {
-  curMeta = { ...curMeta, ...obj };
+  loggerInternal.addMeta(obj);
 }
 
 // removeMeta removes the provided fields from meta
 export function removeMeta(fields: string[]): void {
-  Object.keys(curMeta).forEach((key) => {
-    if (fields.includes(key)) {
-      delete curMeta[key];
-    }
-  });
+  loggerInternal.removeMeta(fields);
 }
 
 export /* istanbul ignore next */ function addStream(
   stream: bunyan.Stream,
 ): void {
-  bunyanLogger.addStream(withSanitizer(stream));
+  loggerInternal.addStream(stream);
 }
 
 /**
