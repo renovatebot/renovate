fa77b21070bcd68f6e372cbce89935050dd7c291
Rhys Arkins <rhys@arkins.net>
2024-12-14 09:39:27 +0100

    lint-fix

--- lib/config/presets/internal/group.spec.ts
+++ lib/config/presets/internal/group.spec.ts
@@ -6,6 +6,7 @@ describe('config/presets/internal/group', () => {
   const presetNames = Object.keys(presets).filter(
     (name) => !exceptions.has(name),
   );
+
   it.each(presetNames)('group:%s contains packageRules', (name: string) => {
     expect(presets[name]).toHaveProperty('packageRules');
   });
