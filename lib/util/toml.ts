d9123965732b2aad25291a967c320a0b55541411
Jia Wei Kho <jiaweikho@gmail.com>
2024-12-15 01:57:28 +0000

    Remove jinja delimiters
    Following the specification in https://jinja.palletsprojects.com/en/stable/templates/#synopsis,
    there are three types of delimiters:
    
    1. Statements
    2. Expressions
    3. Comments

--- lib/util/toml.ts
+++ lib/util/toml.ts
@@ -1,6 +1,15 @@
 import { getStaticTOMLValue, parseTOML } from 'toml-eslint-parser';
+import { regEx } from './regex';
 
 export function parse(input: string): unknown {
-  const ast = parseTOML(input);
+  const massagedContent = massageContent(input);
+  const ast = parseTOML(massagedContent);
   return getStaticTOMLValue(ast);
+}
+
+function massageContent(content: string): string {
+  return content
+    .replace(regEx(/{%.+?%}/gs), '')
+    .replace(regEx(/{{.+?}}/gs), '')
+    .replace(regEx(/{#.+?#}/gs), '');
 }
