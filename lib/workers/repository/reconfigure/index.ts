d79aa81503461e7766c1ec44da2710ff45a96009
Rahul Gautam Singh <rahultesnik@gmail.com>
2024-12-30 21:22:56 +0530

    refactor: remove comment

--- lib/workers/repository/reconfigure/index.ts
+++ lib/workers/repository/reconfigure/index.ts
@@ -6,18 +6,6 @@ import { deleteReconfigureBranchCache } from './reconfigure-cache';
 import { getReconfigureBranchName } from './utils';
 import { validateReconfigureBranch } from './validate';
 
-/**
- * In a reconfigure branch:
- * first check if such a branch exists (no->return)
- * yes:
- * check sha and only continue if sha doesn't match cached sha
- * check if it has a valid config file (no failing check? and return)
- * get the content, validate and give passing check as per it
- *
- * extractDeps
- * ensure pr
- */
-
 export async function checkReconfigureBranch(
   config: RenovateConfig,
 ): Promise<void> {
