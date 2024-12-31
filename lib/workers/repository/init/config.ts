bce95be90346dc78c59fa16a0574d4ddb38f068c
Gabriel Ladzaretti <gabriel.ladzaretti@gmail.com>
2024-12-31 17:39:02 +0200

    parse repo env config

--- lib/workers/repository/init/config.ts
+++ lib/workers/repository/init/config.ts
@@ -1,4 +1,8 @@
-import type { RenovateConfig } from '../../../config/types';
+import is from '@sindresorhus/is';
+import { mergeChildConfig } from '../../../config';
+import type { AllConfig, RenovateConfig } from '../../../config/types';
+import { logger } from '../../../logger';
+import { parseAndValidateOrExit } from '../../global/config/parse/env';
 import { checkOnboardingBranch } from '../onboarding/branch';
 import { mergeInheritedConfig } from './inherited';
 import { mergeRenovateConfig } from './merge';
@@ -10,7 +14,25 @@ export async function getRepoConfig(
   let config = { ...config_ };
   config.baseBranch = config.defaultBranch;
   config = await mergeInheritedConfig(config);
+  config = await mergeRepoEnvConfig(config, process.env);
   config = await checkOnboardingBranch(config);
   config = await mergeRenovateConfig(config);
   return config;
+}
+
+export async function mergeRepoEnvConfig(
+  config: AllConfig,
+  env: NodeJS.ProcessEnv,
+): Promise<AllConfig> {
+  const repoEnvConfig = await parseAndValidateOrExit(
+    env,
+    'RENOVATE_REPO_CONFIG',
+  );
+
+  if (!is.nonEmptyObject(repoEnvConfig)) {
+    return config;
+  }
+
+  logger.debug({ repoEnvConfig }, 'detected repo env config');
+  return mergeChildConfig(config, repoEnvConfig);
 }
