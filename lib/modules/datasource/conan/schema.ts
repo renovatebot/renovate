ab3f71ae63aaa29feb903a4ebd2add3cdca8fc35
Sergei Zharinov <zharinov@users.noreply.github.com>
2025-01-10 20:05:42 -0300

    refactor(conan): Use schema for datasource

---
+++ lib/modules/datasource/conan/schema.ts
@@ -0,0 +1,63 @@
+import { z } from 'zod';
+import { LooseArray, Yaml } from '../../../util/schema-utils';
+import type { ReleaseResult } from '../types';
+import { conanDatasourceRegex } from './common';
+
+export const ConanCenterReleases = Yaml.pipe(
+  z.object({
+    versions: z.record(z.string(), z.unknown()),
+  }),
+)
+  .transform(
+    ({ versions }): ReleaseResult => ({
+      releases: Object.keys(versions).map((version) => ({ version })),
+    }),
+  )
+  .nullable()
+  .catch(null);
+
+export const ConanJSON = z
+  .object({
+    results: z
+      .string()
+      .array()
+      .transform((array) =>
+        array.map((val) => val.match(conanDatasourceRegex)?.groups),
+      )
+      .pipe(
+        LooseArray(
+          z.object({
+            name: z.string(),
+            version: z.string(),
+            userChannel: z.string(),
+          }),
+        ),
+      ),
+  })
+  .transform(({ results }) => results)
+  .nullable()
+  .catch(null);
+
+export const ConanRevisionJSON = z.object({
+  revision: z.string(),
+  time: z.string(),
+});
+
+export const ConanLatestRevision = z
+  .object({ revisions: z.unknown().array() })
+  .transform(({ revisions }) => revisions[0])
+  .pipe(ConanRevisionJSON)
+  .transform(({ revision }) => revision)
+  .nullable()
+  .catch(null);
+
+export const ConanProperties = z
+  .object({
+    properties: z.object({
+      'conan.package.url': z.string().array(),
+    }),
+  })
+  .transform(({ properties }) => {
+    const sourceUrl = properties['conan.package.url'][0];
+    return { sourceUrl };
+  });
