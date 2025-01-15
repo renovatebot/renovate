cbf1d84e6d7b0f4627a65cca988241957004a228
Rahul Gautam Singh <rahultesnik@gmail.com>
2025-01-16 02:53:31 +0530

    feat(config/migration): migrate package.json config

--- lib/workers/repository/config-migration/branch/create.ts
+++ lib/workers/repository/config-migration/branch/create.ts
@@ -1,18 +1,24 @@
+import upath from 'upath';
 import { GlobalConfig } from '../../../../config/global';
 import type { RenovateConfig } from '../../../../config/types';
 import { logger } from '../../../../logger';
 import { scm } from '../../../../modules/platform/scm';
+import { readLocalFile } from '../../../../util/fs';
+import type { FileChange } from '../../../../util/git/types';
 import { getMigrationBranchName } from '../common';
 import { ConfigMigrationCommitMessageFactory } from './commit-message';
-import { MigratedDataFactory } from './migrated-data';
-import type { MigratedData } from './migrated-data';
+import { MigratedDataFactory, applyPrettierFormatting } from './migrated-data';
+import type { MigratedData, PrettierParser } from './migrated-data';
 
 export async function createConfigMigrationBranch(
   config: Partial<RenovateConfig>,
   migratedConfigData: MigratedData,
 ): Promise<string | null> {
   logger.debug('createConfigMigrationBranch()');
-  const configFileName = migratedConfigData.filename;
+  const pJsonMigration = migratedConfigData.filename === 'package.json';
+  const configFileName = pJsonMigration
+    ? 'renovate.json'
+    : migratedConfigData.filename;
   logger.debug('Creating config migration branch');
 
   const commitMessageFactory = new ConfigMigrationCommitMessageFactory(
@@ -31,16 +37,37 @@ export async function createConfigMigrationBranch(
   await scm.checkoutBranch(config.defaultBranch!);
   const contents =
     await MigratedDataFactory.applyPrettierFormatting(migratedConfigData);
+
+  const files = [
+    {
+      type: 'addition',
+      path: configFileName,
+      contents,
+    } satisfies FileChange,
+  ];
+
+  if (pJsonMigration) {
+    const pJson = JSON.parse((await readLocalFile('package.json', 'utf8'))!);
+    if (pJson.renovate) {
+      delete pJson.renovate;
+    }
+    const pJsonContent = await applyPrettierFormatting(
+      'package.json',
+      JSON.stringify(pJson, undefined, migratedConfigData.indent.indent),
+      upath.extname('package.json').replace('.', '') as PrettierParser,
+      migratedConfigData.indent,
+    );
+    files.push({
+      type: 'addition',
+      path: 'package.json',
+      contents: pJsonContent,
+    });
+  }
+
   return scm.commitAndPush({
     baseBranch: config.baseBranch,
     branchName: getMigrationBranchName(config),
-    files: [
-      {
-        type: 'addition',
-        path: configFileName,
-        contents,
-      },
-    ],
+    files,
     message: commitMessage.toString(),
     platformCommit: config.platformCommit,
     force: true,
