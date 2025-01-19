524ec9a02386865c9d4737b610d1e8e71fb57919
Sergei Zharinov <zharinov@users.noreply.github.com>
2025-01-19 19:38:41 -0300

    Fix

--- lib/util/github/graphql/query-adapters/tags-query-adapter.ts
+++ lib/util/github/graphql/query-adapters/tags-query-adapter.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { MaybeTimestamp } from '../../../timestamp';
+import { Timestamp } from '../../../timestamp';
 import type { GithubGraphqlDatasourceAdapter, GithubTagItem } from '../types';
 import { prepareQuery } from '../util';
 
@@ -11,7 +11,7 @@ const GithubGraphqlTag = z.object({
     z.object({
       type: z.literal('Commit'),
       oid: z.string(),
-      releaseTimestamp: MaybeTimestamp,
+      releaseTimestamp: Timestamp,
     }),
     z.object({
       type: z.literal('Tag'),
@@ -19,7 +19,7 @@ const GithubGraphqlTag = z.object({
         oid: z.string(),
       }),
       tagger: z.object({
-        releaseTimestamp: MaybeTimestamp,
+        releaseTimestamp: Timestamp,
       }),
     }),
   ]),
