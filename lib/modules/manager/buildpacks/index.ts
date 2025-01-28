d581af59bc82ec57a0bcd36ce0179e4e3ead671c
Ralf Pannemans <ralf.pannemans@googlemail.com>
2025-01-28 17:31:07 +0100

    feat(datasource): Add datasource for buildpack registry (#32721)
    Co-authored-by: Nicolas Bender <nicolas.bender@sap.com>
    Co-authored-by: HonkingGoose <34918129+HonkingGoose@users.noreply.github.com>
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>
    Co-authored-by: Pavel Busko <pavel.busko@sap.com>
    Co-authored-by: Johannes Dillmann <j.dillmann@sap.com>
    Co-authored-by: Michael Kriese <michael.kriese@gmx.de>

--- lib/modules/manager/buildpacks/index.ts
+++ lib/modules/manager/buildpacks/index.ts
@@ -1,4 +1,5 @@
 import type { Category } from '../../../constants';
+import { BuildpacksRegistryDatasource } from '../../datasource/buildpacks-registry';
 import { DockerDatasource } from '../../datasource/docker';
 export { extractPackageFile } from './extract';
 
@@ -8,5 +9,8 @@ export const defaultConfig = {
   pinDigests: false,
 };
 
-export const categories: Category[] = ['docker'];
-export const supportedDatasources = [DockerDatasource.id];
+export const categories: Category[] = ['docker', 'ci', 'cd'];
+export const supportedDatasources = [
+  DockerDatasource.id,
+  BuildpacksRegistryDatasource.id,
+];
