cddf8592ef562fcbec493149ccc29bc6108689e5
Rhys Arkins <rhys@arkins.net>
2025-02-13 09:04:05 +0100

    feat(bun): workspaces support

--- lib/modules/manager/bun/extract.ts
+++ lib/modules/manager/bun/extract.ts
@@ -1,10 +1,16 @@
+import is from '@sindresorhus/is';
 import { logger } from '../../../logger';
-import { getSiblingFileName, readLocalFile } from '../../../util/fs';
+import {
+  getParentDir,
+  getSiblingFileName,
+  readLocalFile,
+} from '../../../util/fs';
 
 import { extractPackageJson } from '../npm/extract/common/package-file';
 import type { NpmPackage } from '../npm/extract/types';
 import type { NpmManagerData } from '../npm/types';
 import type { ExtractConfig, PackageFile } from '../types';
+import { filesMatchingWorkspaces } from '../npm/extract/utils';
 
 function matchesFileName(fileNameWithPath: string, fileName: string): boolean {
   return (
@@ -50,11 +56,36 @@ export async function extractAllPackageFiles(
     logger.debug('No bun lockfiles found');
     return packageFiles;
   }
+  const allPackageJson = matchedFiles.filter((file) =>
+    matchesFileName(file, 'package.json'),
+  );
   for (const lockFile of allLockFiles) {
     const packageFile = getSiblingFileName(lockFile, 'package.json');
     const res = await processPackageFile(packageFile);
     if (res) {
       packageFiles.push({ ...res, lockFiles: [lockFile] });
+    }
+    // Check if package.json contains workspaces
+    const workspaces = res?.managerData?.workspaces;
+
+    if (!(is.array(workspaces), is.string)) {
+      continue;
+    }
+    logger.debug(`Found bun workspaces in ${packageFile}`);
+    const pwd = getParentDir(packageFile);
+    const workspacePackageFiles = filesMatchingWorkspaces(
+      pwd,
+      allPackageJson,
+      workspaces,
+    );
+    if (workspacePackageFiles.length) {
+      logger.debug({ workspacePackageFiles }, 'Found bun workspace files');
+      for (const workspaceFile of workspacePackageFiles) {
+        const res = await processPackageFile(workspaceFile);
+        if (res) {
+          packageFiles.push({ ...res, lockFiles: [lockFile] });
+        }
+      }
     }
   }
 
