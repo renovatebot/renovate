e15a23c36d6e58f081625302f00c807aeb8df7e9
Trim21 <trim21.me@gmail.com>
2025-02-22 03:36:04 +0800

    try to fix fs mock for wasm

--- __mocks__/fs.ts
+++ __mocks__/fs.ts
@@ -1,15 +1,3 @@
-import { readFileSync } from 'fs';
 import { fs } from 'memfs';
 
-module.exports = {
-  ...fs,
-  readFileSync(path, options) {
-    if (typeof path === 'string') {
-      if (path.endsWith('.wasm')) {
-        return readFileSync(path, options);
-      }
-    }
-
-    return fs.readFileSync(path, options);
-  },
-};
+module.exports = fs;
