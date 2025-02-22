0e19c8c93b5e53d999d7c37fad6ef9d466c6071f
Michael Kriese <michael.kriese@visualon.de>
2025-02-23 00:10:49 +0100

    build: don't push short tags for prereleases

--- tools/utils/docker.ts
+++ tools/utils/docker.ts
@@ -32,8 +32,10 @@ export async function bake(
   if (opts.version) {
     console.log(`Using version: ${opts.version.version}`);
     process.env.RENOVATE_VERSION = opts.version.version;
-    process.env.RENOVATE_MAJOR_VERSION = `${opts.version.major}`;
-    process.env.RENOVATE_MAJOR_MINOR_VERSION = `${opts.version.major}.${opts.version.minor}`;
+    if (!opts.version.prerelease) {
+      process.env.RENOVATE_MAJOR_VERSION = `${opts.version.major}`;
+      process.env.RENOVATE_MAJOR_MINOR_VERSION = `${opts.version.major}.${opts.version.minor}`;
+    }
   }
 
   const metadataFile = path.join(await tmp, 'metadata.json');
