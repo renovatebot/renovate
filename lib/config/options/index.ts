1cb61dc44cc28ea8cdc4296176747d4f5f87d512
RahulGautamSingh <rahultesnik@gmail.com>
2025-02-24 12:21:36 +0530

    feat(manager/jsonata): add toml support (#34380)
    Co-authored-by: Rhys Arkins <rhys@arkins.net>
    Co-authored-by: Michael Kriese <michael.kriese@visualon.de>

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2766,7 +2766,7 @@ const options: RenovateOptions[] = [
     description:
       'It specifies the syntax of the package file being managed by the custom JSONata manager.',
     type: 'string',
-    allowedValues: ['json', 'yaml'],
+    allowedValues: ['json', 'toml', 'yaml'],
     parents: ['customManagers'],
     cli: false,
     env: false,
