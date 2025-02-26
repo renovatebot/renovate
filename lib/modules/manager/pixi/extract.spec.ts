e1594bae7fb039e7b6d321c4ae03e0c4453b34c6
Trim21 <trim21.me@gmail.com>
2025-02-26 20:04:29 +0800

    fix

--- lib/modules/manager/pixi/extract.spec.ts
+++ lib/modules/manager/pixi/extract.spec.ts
@@ -1,4 +1,4 @@
-import { vi, describe, it, expect } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 
 import { fs } from '../../../../test/util';
 import { extractPackageFile } from '.';
