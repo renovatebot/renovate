b474f3e1523e01a5b269dc7a7e5b4553fe3be925
Micke Lisinge <hi@micke.me>
2025-03-05 17:54:53 +0100

    Support ranges of helm version constraints

--- lib/modules/manager/kustomize/common.spec.ts
+++ lib/modules/manager/kustomize/common.spec.ts
@@ -16,7 +16,7 @@ describe('modules/manager/kustomize/common', () => {
   });
 
   describe('generateHelmEnvs', () => {
-    it('generates envs', () => {
+    it('generates envs for specific helm version not requiring HELM_EXPERIMENTAL_OCI', () => {
       const config = {
         constraints: { helm: '3.8.0' },
       };
@@ -31,9 +31,41 @@ describe('modules/manager/kustomize/common', () => {
       });
     });
 
-    it('generates envs with experimental OCI', () => {
+    it('generates envs for helm version range not requiring HELM_EXPERIMENTAL_OCI', () => {
+      const config = {
+        constraints: { helm: '>= 3.8.0' },
+      };
+      const envs = generateHelmEnvs(config);
+      expect(envs).toEqual({
+        HELM_REGISTRY_CONFIG:
+          '/tmp/cache/__renovate-private-cache/registry.json',
+        HELM_REPOSITORY_CONFIG:
+          '/tmp/cache/__renovate-private-cache/repositories.yaml',
+        HELM_REPOSITORY_CACHE:
+          '/tmp/cache/__renovate-private-cache/repositories',
+      });
+    });
+
+    it('generates envs for specific helm version requiring HELM_EXPERIMENTAL_OCI', () => {
       const config = {
         constraints: { helm: '3.7.0' },
+        postUpdateOptions: ['kustomizeInflateHelmCharts'],
+      };
+      const envs = generateHelmEnvs(config);
+      expect(envs).toEqual({
+        HELM_REGISTRY_CONFIG:
+          '/tmp/cache/__renovate-private-cache/registry.json',
+        HELM_REPOSITORY_CONFIG:
+          '/tmp/cache/__renovate-private-cache/repositories.yaml',
+        HELM_REPOSITORY_CACHE:
+          '/tmp/cache/__renovate-private-cache/repositories',
+        HELM_EXPERIMENTAL_OCI: '1',
+      });
+    });
+
+    it('generates envs for helm range version requiring HELM_EXPERIMENTAL_OCI', () => {
+      const config = {
+        constraints: { helm: '>=3.7.0' },
         postUpdateOptions: ['kustomizeInflateHelmCharts'],
       };
       const envs = generateHelmEnvs(config);
