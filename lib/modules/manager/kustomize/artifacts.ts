46b260eb33b219d8a20adb63c395aecd6a2ffedd
Michael Kriese <michael.kriese@gmx.de>
2025-03-06 14:48:26 +0100

    Update lib/modules/manager/kustomize/artifacts.ts

--- lib/modules/manager/kustomize/artifacts.ts
+++ lib/modules/manager/kustomize/artifacts.ts
@@ -126,7 +126,7 @@ export async function updateArtifacts({
 
   const chartHome = getSiblingFileName(
     packageFileName,
-    project.helmGlobals!.chartHome!
+    project.helmGlobals!.chartHome!,
   );
 
   try {
