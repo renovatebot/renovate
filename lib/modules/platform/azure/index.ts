2950d61c34002904907bda0fcd692e35bf66c095
Sergei Zharinov <zharinov@users.noreply.github.com>
2025-03-09 20:06:49 -0300

    Change all platform code

--- lib/modules/platform/azure/index.ts
+++ lib/modules/platform/azure/index.ts
@@ -162,7 +162,7 @@ export async function getRawFile(
     );
 
     return item?.content ?? null;
-  } catch (err) /* istanbul ignore next */ {
+  } catch (err) /* v8 ignore start */ {
     if (
       err.message?.includes('<title>Azure DevOps Services Unavailable</title>')
     ) {
@@ -178,7 +178,7 @@ export async function getRawFile(
       throw new ExternalHostError(err, id);
     }
     throw err;
-  }
+  } /* v8 ignore stop */
 }
 
 export async function getJsonFile(
@@ -209,11 +209,11 @@ export async function initRepo({
     logger.debug('Repository is disabled- throwing error to abort renovation');
     throw new Error(REPOSITORY_ARCHIVED);
   }
-  // istanbul ignore if
+  /* v8 ignore start */
   if (!repo.defaultBranch) {
     logger.debug('Repo is empty');
     throw new Error(REPOSITORY_EMPTY);
-  }
+  } /* v8 ignore stop */
   // TODO #22198
   config.repoId = repo.id!;
 
@@ -613,12 +613,12 @@ export async function updatePr({
     const existingIndex = config.prList.findIndex(
       (item) => item.number === prNo,
     );
-    // istanbul ignore if: should not happen
+    /* v8 ignore start: should not happen */
     if (existingIndex === -1) {
       logger.warn({ prNo }, 'PR not found in cache');
       // Add to cache
       config.prList.push(prToCache);
-    } else {
+    } /* v8 ignore stop */ else {
       // overwrite existing PR in cache
       config.prList[existingIndex] = prToCache;
     }
@@ -861,31 +861,31 @@ export function maxBodyLength(): number {
   return 4000;
 }
 
-/* istanbul ignore next */
+/* v8 ignore start */
 export function findIssue(): Promise<Issue | null> {
   // TODO: Needs implementation (#9592)
   logger.debug(`findIssue() is not implemented`);
   return Promise.resolve(null);
-}
+} /* v8 ignore stop */
 
-/* istanbul ignore next */
+/* v8 ignore start */
 export function ensureIssue(): Promise<EnsureIssueResult | null> {
   // TODO: Needs implementation (#9592)
   logger.debug(`ensureIssue() is not implemented`);
   return Promise.resolve(null);
-}
+} /* v8 ignore stop */
 
-/* istanbul ignore next */
+/* v8 ignore start */
 export function ensureIssueClosing(): Promise<void> {
   return Promise.resolve();
-}
+} /* v8 ignore stop */
 
-/* istanbul ignore next */
+/* v8 ignore start */
 export function getIssueList(): Promise<Issue[]> {
   logger.debug(`getIssueList()`);
   // TODO: Needs implementation (#9592)
   return Promise.resolve([]);
-}
+} /* v8 ignore stop */
 
 async function getUserIds(users: string[]): Promise<User[]> {
   const azureApiGit = await azureApi.gitApi();
