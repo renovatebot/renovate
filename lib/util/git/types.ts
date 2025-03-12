a0e675e215d8c1fcae02a79bcb649cfbf9731fca
Jan Høydahl <jh@cominvent.com>
2025-03-12 02:01:54 +0100

    Test change of type for upstreamUrl in StorageConfig

--- lib/util/git/types.ts
+++ lib/util/git/types.ts
@@ -19,7 +19,7 @@ export interface StorageConfig {
   currentBranch?: string;
   defaultBranch?: string;
   url: string;
-  upstreamUrl?: string;
+  upstreamUrl?: string | undefined;
   extraCloneOpts?: GitOptions;
   cloneSubmodules?: boolean;
   cloneSubmodulesFilter?: string[];
