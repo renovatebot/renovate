edb275c697716059700d72171e909b2be618241d
Trim21 <trim21.me@gmail.com>
2025-03-19 11:39:57 +0800

    fix lint

--- lib/modules/manager/pixi/extract.ts
+++ lib/modules/manager/pixi/extract.ts
@@ -9,7 +9,11 @@ import { ensureTrailingSlash } from '../../../util/url';
 import { defaultRegistryUrl as defaultCondaRegistryAPi } from '../../datasource/conda/common';
 import { PyProjectSchema } from '../pep621/schema';
 import type { ExtractConfig, PackageFileContent } from '../types';
-import { type PixiConfig, PixiPackageDependency, PixiToml } from './schema';
+import {
+  type PixiConfig,
+  type PixiPackageDependency,
+  PixiToml,
+} from './schema';
 
 const PyProjectToml = Toml.pipe(PyProjectSchema);
 
