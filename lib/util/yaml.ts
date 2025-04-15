8bb9a00a929ff7879c4bfdc6c89b097a526b7ecd
MGSousa <31368750+MGSousa@users.noreply.github.com>
2025-04-15 16:23:10 +0100

    feat(yaml): Improve YAML parser on Jinja2 templates (#35374)

--- lib/util/yaml.ts
+++ lib/util/yaml.ts
@@ -9,7 +9,6 @@ import type {
 import { parseAllDocuments, parseDocument, stringify } from 'yaml';
 import type { ZodType } from 'zod';
 import { logger } from '../logger';
-import { regEx } from './regex';
 import { stripTemplates } from './string';
 
 export interface YamlOptions<
@@ -168,7 +167,7 @@ export function dump(obj: any, opts?: DumpOptions): string {
 
 function massageContent(content: string, options?: YamlOptions): string {
   if (options?.removeTemplates) {
-    return stripTemplates(content.replace(regEx(/\s+{{.+?}}:.+/gs), ''));
+    return stripTemplates(content);
   }
 
   return content;
