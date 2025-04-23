b8850bd084f9ba3ce30d2adffa24efb1d56e8c56
Tobias Gabriel <tobias.gabriel@sap.com>
2025-04-23 19:24:14 +0200

    add test for package with multiple components

--- lib/modules/manager/gomod/update.spec.ts
+++ lib/modules/manager/gomod/update.spec.ts
@@ -112,7 +112,7 @@ describe('modules/manager/gomod/update', () => {
       expect(res).toContain('github.com/pkg/errors/v2 v2.0.0');
     });
 
-    it('bumps major with short name', () => {
+    it('bumps major with single package name component', () => {
       const upgrade = {
         depName: 'sigs.k8s.io/structured-merge-diff/v4',
         managerData: { lineNumber: 15 },
@@ -125,6 +125,23 @@ describe('modules/manager/gomod/update', () => {
       const res = updateDependency({ fileContent: gomod1, upgrade });
       expect(res).not.toEqual(gomod1);
       expect(res).toContain('sigs.k8s.io/structured-merge-diff/v6 v6.0.0');
+    });
+
+    it('bumps major with multiple package name components', () => {
+      const upgrade = {
+        depName: 'github.com/cucumber/common/messages/go/v18',
+        managerData: { lineNumber: 16 },
+        currentValue: 'v18.0.0',
+        newValue: 'v19.0.0',
+        newMajor: 19,
+        updateType: 'major' as UpdateType,
+        depType: 'require',
+      };
+      const res = updateDependency({ fileContent: gomod1, upgrade });
+      expect(res).not.toEqual(gomod1);
+      expect(res).toContain(
+        'github.com/cucumber/common/messages/go/v19 v19.0.0',
+      );
     });
 
     it('replaces major gopkg.in updates', () => {
