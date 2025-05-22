660c1c33d249210e31e6c87ef7ecfb35e09e56c2
Weihang Lo <me@weihanglo.tw>
2025-05-22 14:29:27 -0400

    fix(cargo): set versionOrig if contains build metadata

--- lib/modules/datasource/crate/index.ts
+++ lib/modules/datasource/crate/index.ts
@@ -110,8 +110,11 @@ export class CrateDatasource extends Datasource {
     result.releases = lines
       .map((version) => {
         const release: Release = {
-          version: version.vers,
+          version: version.vers.replace(/\+.*$/, ''),
         };
+        if release.version != version.vers {
+          release.versionOrig = version.vers;
+        }
         if (version.yanked) {
           release.isDeprecated = true;
         }
