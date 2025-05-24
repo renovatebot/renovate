d85f2d4922dc5fbcda01a6b0a9b64b5975eb6885
Felipe Santos <felipecassiors@gmail.com>
2025-05-24 12:37:15 -0300

    Add jsdoc for rebaseLabel in massageMarkdown type

--- lib/modules/platform/types.ts
+++ lib/modules/platform/types.ts
@@ -242,7 +242,14 @@ export interface Platform {
   ensureIssue(
     issueConfig: EnsureIssueConfig,
   ): Promise<EnsureIssueResult | null>;
-  massageMarkdown(prBody: string, rebaseLabel?: string): string;
+  massageMarkdown(
+    prBody: string,
+    /**
+     * Useful for suggesting the use of rebase label when there is no better
+     * way, e.g. for Gerrit.
+     */
+    rebaseLabel?: string,
+  ): string;
   updatePr(prConfig: UpdatePrConfig): Promise<void>;
   mergePr(config: MergePRConfig): Promise<boolean>;
   addReviewers(number: number, reviewers: string[]): Promise<void>;
