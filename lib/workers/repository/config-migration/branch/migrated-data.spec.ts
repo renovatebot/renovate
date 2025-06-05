5175e117230863479052b2729f78e79bf8377b8d
Tobias Gabriel <tobias.gabriel@sap.com>
2025-06-05 18:02:05 +0200

    add unit test

--- lib/workers/repository/config-migration/branch/migrated-data.spec.ts
+++ lib/workers/repository/config-migration/branch/migrated-data.spec.ts
@@ -174,6 +174,19 @@ describe('workers/repository/config-migration/branch/migrated-data', () => {
       ).resolves.toEqual(formatted);
     });
 
+    it('formats without prettier if in .renovaterc', async () => {
+      const migratedDataRenovaterc = vi
+        .mocked(scm.getFileList)
+        .mockResolvedValue(['.prettierrc']);
+      await MigratedDataFactory.getAsync();
+      await expect(
+        MigratedDataFactory.applyPrettierFormatting({
+          ...migratedData,
+          filename: '.renovaterc',
+        }),
+      ).resolves.toEqual(migratedData.content);
+    });
+
     it('formats when finds prettier config inside the package.json file', async () => {
       const formatted = formattedMigratedData.content;
       vi.mocked(detectRepoFileConfig).mockResolvedValueOnce({
