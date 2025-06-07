9f7adb2df10ceb01aec343883baf95bf0d4c6374
Felipe Santos <felipecassiors@gmail.com>
2025-06-07 18:08:56 -0300

    Allow to use the rebase label in PR body, issues, or comments

--- lib/workers/repository/dependency-dashboard.ts
+++ lib/workers/repository/dependency-dashboard.ts
@@ -556,7 +556,7 @@ export async function ensureDependencyDashboard(
     await platform.ensureIssue({
       title: config.dependencyDashboardTitle!,
       reuseTitle,
-      body: platform.massageMarkdown(issueBody),
+      body: platform.massageMarkdown(issueBody, config.rebaseLabel),
       labels: config.dependencyDashboardLabels,
       confidential: config.confidential,
     });
