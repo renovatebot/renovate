c1c615a40f1a75756f2af35d5c6662cb7c726af1
Sergei Zharinov <zharinov@users.noreply.github.com>
2025-06-08 19:33:16 -0300

    Fix

--- lib/util/schema-utils/v4.ts
+++ lib/util/schema-utils/v4.ts
@@ -1,13 +1,10 @@
 import { z } from 'zod/v4';
 
-export const Json = z
-  .string()
-  .transform((str, ctx): unknown => {
-    try {
-      return JSON.parse(str);
-    } catch {
-      ctx.addIssue({ code: 'custom', message: 'Invalid JSON' });
-      return z.NEVER;
-    }
-  })
-  .pipe(z.json());
+export const Json = z.string().transform((str, ctx): unknown => {
+  try {
+    return JSON.parse(str);
+  } catch {
+    ctx.addIssue({ code: 'custom', message: 'Invalid JSON' });
+    return z.NEVER;
+  }
+});
