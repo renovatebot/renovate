d0d2b1c23d01cc83ed10d3198dbaabdd2ae745a1
Michael Kriese <michael.kriese@visualon.de>
2025-06-10 14:53:27 +0200

    fix: handle modern aggregate error
    - https://github.com/renovatebot/renovate/issues/36429

--- lib/util/github/graphql/datasource-fetcher.ts
+++ lib/util/github/graphql/datasource-fetcher.ts
@@ -1,4 +1,4 @@
-import AggregateError from 'aggregate-error';
+import AggregateErrorLegacy from 'aggregate-error';
 import { GlobalConfig } from '../../../config/global';
 import { logger } from '../../../logger';
 import { ExternalHostError } from '../../../types/errors/external-host-error';
@@ -37,7 +37,14 @@ function isUnknownGraphqlError(err: Error): boolean {
 }
 
 function canBeSolvedByShrinking(err: Error): boolean {
-  const errors: Error[] = err instanceof AggregateError ? [...err] : [err];
+  // AggregateErrorLegacy can be dropped after `p_all` was upgraded too
+  // https://github.com/renovatebot/renovate/issues/36429
+  const errors: Error[] =
+    err instanceof AggregateError
+      ? err.errors
+      : err instanceof AggregateErrorLegacy
+        ? [...err]
+        : [err];
   return errors.some(
     (e) => err instanceof ExternalHostError || isUnknownGraphqlError(e),
   );
