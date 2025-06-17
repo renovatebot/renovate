93fdecf22d8e8eb49a40f4de2e9975c0a1e9d9fc
Borja Domínguez <iam.n3uro@gmail.com>
2025-06-17 12:00:07 +0200

    Update lib/modules/manager/unity3d/index.ts
    Co-authored-by: Rhys Arkins <rhys@arkins.net>

--- lib/modules/manager/unity3d/index.ts
+++ lib/modules/manager/unity3d/index.ts
@@ -3,7 +3,7 @@ import { Unity3dDatasource } from '../../datasource/unity3d';
 export { extractPackageFile } from './extract';
 
 export const defaultConfig = {
-  managerFilePatterns: ['/(^|/)ProjectSettings/ProjectVersion\\.txt$/'],
+  managerFilePatterns: ['**/ProjectSettings/ProjectVersion.txt'],
 };
 
 export const categories: Category[] = ['unity3d'];
