8e8487715bfa5382f27b716ef104b09a23c400ff
Adam Setch <adam.setch@outlook.com>
2025-07-09 10:03:11 -0400

    refactor(dashboard): update issue body language for consistency
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- lib/modules/platform/utils/read-only-issue-body.ts
+++ lib/modules/platform/utils/read-only-issue-body.ts
@@ -1,11 +1,8 @@
 import { regEx } from '../../../util/regex';
 
 export function readOnlyIssueBody(body: string): string {
-  // TODO ADAM update this
   return body
-    .replace(' only once you click their checkbox below', '')
-    .replace(' unless you click a checkbox below', '')
-    .replace(' To discard all commits and start over, click on a checkbox.', '')
+    .replace(regEx(/ To.*?, click on a checkbox below\./g), '')
     .replace(regEx(/ Click (?:on |)a checkbox.*\./g), '')
     .replace(regEx(/\[ ] <!-- \w*-branch.*-->/g), '')
     .replace(regEx(/- \[ ] <!-- rebase-all-open-prs -->.*/g), '')
