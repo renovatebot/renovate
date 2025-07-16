218ef3a9197d04432f1a90d1ac0aece113fe967f
Rahul Gautam Singh <rahultesnik@gmail.com>
2025-07-16 22:24:12 +0530

    add deleteAdditionalConfigFile option

--- lib/config/types.ts
+++ lib/config/types.ts
@@ -131,6 +131,7 @@ export interface GlobalOnlyConfig {
   repositories?: RenovateRepository[];
   useCloudMetadataServices?: boolean;
   deleteConfigFile?: boolean;
+  deleteAdditionalConfigFile?: boolean;
 }
 
 // Config options used within the repository worker, but not user configurable
