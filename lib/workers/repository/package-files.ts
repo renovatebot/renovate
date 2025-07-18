6be29915af2e3c78e1a6367eb2a01edf8774343d
Trim21 <trim21.me@gmail.com>
2025-07-19 03:39:55 +0800

    Apply suggestions from code review
    Co-authored-by: Michael Kriese <michael.kriese@gmx.de>

--- lib/workers/repository/package-files.ts
+++ lib/workers/repository/package-files.ts
@@ -85,7 +85,7 @@ export class PackageFiles {
     const pad = data.size > 1; // padding condition for a multi base branch repo
     let deps = '';
 
-    for (const [branch, packageFiles] of Array.from(data).toSorted(([a], [b]) =>
+    for (const [branch, packageFiles] of Array.from(data).sort(([a], [b]) =>
       a.localeCompare(b),
     )) {
       deps += pad
@@ -97,18 +97,16 @@ export class PackageFiles {
         continue;
       }
 
-      const managers = Array.from(Object.keys(packageFiles));
+      const managers = Object.keys(packageFiles).sort();
       if (managers.length === 0) {
         deps += none;
         deps += pad ? '</blockquote>\n</details>\n\n' : '';
         continue;
       }
 
-      managers.sort();
-
       for (const manager of managers) {
         deps += `<details><summary>${manager}</summary>\n<blockquote>\n\n`;
-        for (const packageFile of Array.from(packageFiles[manager]).toSorted(
+        for (const packageFile of Array.from(packageFiles[manager]).sort(
           (a, b) => a.packageFile.localeCompare(b.packageFile),
         )) {
           deps += `<details><summary>${packageFile.packageFile}</summary>\n\n`;
