bffdadc1686a07c33c53dc0c77192f57b1b9a68a
trim21 <trim21.me@gmail.com>
2025-07-18 22:56:47 +0800

    feat: make dashboard branch/manager output in stable order

--- lib/workers/repository/package-files.ts
+++ lib/workers/repository/package-files.ts
@@ -85,7 +85,9 @@ export class PackageFiles {
     const pad = data.size > 1; // padding condition for a multi base branch repo
     let deps = '';
 
-    for (const [branch, packageFiles] of data) {
+    for (const [branch, packageFiles] of Array.from(data).toSorted(([a], [b]) =>
+      a.localeCompare(b),
+    )) {
       deps += pad
         ? `<details><summary>Branch ${branch}</summary>\n<blockquote>\n\n`
         : '';
@@ -95,12 +97,14 @@ export class PackageFiles {
         continue;
       }
 
-      const managers = Object.keys(packageFiles);
+      const managers = Array.from(Object.keys(packageFiles));
       if (managers.length === 0) {
         deps += none;
         deps += pad ? '</blockquote>\n</details>\n\n' : '';
         continue;
       }
+
+      managers.sort();
 
       for (const manager of managers) {
         deps += `<details><summary>${manager}</summary>\n<blockquote>\n\n`;
