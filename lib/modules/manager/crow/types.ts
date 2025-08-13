3d0c21a37395fcc00b749a7fcc48c66fffae69f1
pat-s <patrick.schratz@gmail.com>
2025-08-13 23:14:42 +0200

    rm lib/modules/manager/crow/types.ts

--- lib/modules/manager/crow/types.ts
+++
@@ -1,9 +0,0 @@
-export interface CrowConfig {
-  pipeline?: Record<string, CrowStep>;
-  steps?: Record<string, CrowStep>;
-  clone?: Record<string, CrowStep>;
-}
-
-export interface CrowStep {
-  image?: string;
-}
