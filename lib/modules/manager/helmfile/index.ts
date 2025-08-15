17975cbd9c682b726d8c518c3b01414102ad26ab
Bernd Schorgers <me@bjw-s.dev>
2025-08-15 19:53:07 +0200

    fix linting issue

--- lib/modules/manager/helmfile/index.ts
+++ lib/modules/manager/helmfile/index.ts
@@ -1,20 +1,23 @@
-import type { Category } from '../../../constants';
-import { DockerDatasource } from '../../datasource/docker';
-import { HelmDatasource } from '../../datasource/helm';
-export { extractPackageFile } from './extract';
-export { updateArtifacts } from './artifacts';
+import type { Category } from "../../../constants";
+import { DockerDatasource } from "../../datasource/docker";
+import { HelmDatasource } from "../../datasource/helm";
+export { extractPackageFile } from "./extract";
+export { updateArtifacts } from "./artifacts";
 
 export const supportsLockFileMaintenance = true;
 
-export const url = 'https://helmfile.readthedocs.io';
-export const categories: Category[] = ['cd', 'helm', 'kubernetes'];
+export const url = "https://helmfile.readthedocs.io";
+export const categories: Category[] = ["cd", "helm", "kubernetes"];
 
 export const defaultConfig = {
   registryAliases: {
-    stable: 'https://charts.helm.sh/stable',
+    stable: "https://charts.helm.sh/stable",
   },
-  commitMessageTopic: 'helm chart {{depName}}',
-  managerFilePatterns: ['/(^|/)helmfile\\.ya?ml(?:\\.gotmpl)?$/','/(^|/)helmfile\\.d/.+\\.ya?ml$/'],
+  commitMessageTopic: "helm chart {{depName}}",
+  managerFilePatterns: [
+    "/(^|/)helmfile\\.ya?ml(?:\\.gotmpl)?$/",
+    "/(^|/)helmfile\\.d/.+\\.ya?ml$/",
+  ],
 };
 
 export const supportedDatasources = [HelmDatasource.id, DockerDatasource.id];
