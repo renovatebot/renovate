1184814263016915c6e2fd1be59603546cf73fd9
Bernd Schorgers <me@bjw-s.dev>
2025-08-15 20:01:08 +0200

    Prettier inside web IDE was not my friend

--- lib/modules/manager/helmfile/index.ts
+++ lib/modules/manager/helmfile/index.ts
@@ -1,22 +1,22 @@
-import type { Category } from "../../../constants";
-import { DockerDatasource } from "../../datasource/docker";
-import { HelmDatasource } from "../../datasource/helm";
-export { extractPackageFile } from "./extract";
-export { updateArtifacts } from "./artifacts";
+import type { Category } from '../../../constants';
+import { DockerDatasource } from '../../datasource/docker';
+import { HelmDatasource } from '../../datasource/helm';
+export { extractPackageFile } from './extract';
+export { updateArtifacts } from './artifacts';
 
 export const supportsLockFileMaintenance = true;
 
-export const url = "https://helmfile.readthedocs.io";
-export const categories: Category[] = ["cd", "helm", "kubernetes"];
+export const url = 'https://helmfile.readthedocs.io';
+export const categories: Category[] = ['cd', 'helm', 'kubernetes'];
 
 export const defaultConfig = {
   registryAliases: {
-    stable: "https://charts.helm.sh/stable",
+    stable: 'https://charts.helm.sh/stable',
   },
-  commitMessageTopic: "helm chart {{depName}}",
+  commitMessageTopic: 'helm chart {{depName}}',
   managerFilePatterns: [
-    "/(^|/)helmfile\\.ya?ml(?:\\.gotmpl)?$/",
-    "/(^|/)helmfile\\.d/.+\\.ya?ml$/",
+    '/(^|/)helmfile\\.ya?ml(?:\\.gotmpl)?$/',
+    '/(^|/)helmfile\\.d/.+\\.ya?ml$/',
   ],
 };
 
