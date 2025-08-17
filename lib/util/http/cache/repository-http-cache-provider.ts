e183b9329d5710399e855d451246c1ebca570d5c
Sergei Zharinov <zharinov@users.noreply.github.com>
2025-08-17 17:01:45 -0300

    feat(cache): Reuse repo cache during run

--- lib/util/http/cache/repository-http-cache-provider.ts
+++ lib/util/http/cache/repository-http-cache-provider.ts
@@ -1,6 +1,9 @@
+import { get as memGet, set as memSet } from '../../cache/memory';
 import { getCache } from '../../cache/repository';
+import type { HttpResponse } from '../types';
+import { copyResponse } from '../util';
 import { AbstractHttpCacheProvider } from './abstract-http-cache-provider';
-import type { HttpCache } from './schema';
+import { HttpCache } from './schema';
 
 export class RepositoryHttpCacheProvider extends AbstractHttpCacheProvider {
   override load(url: string): Promise<unknown> {
@@ -14,6 +17,60 @@ export class RepositoryHttpCacheProvider extends AbstractHttpCacheProvider {
     cache.httpCache ??= {};
     cache.httpCache[url] = data;
     return Promise.resolve();
+  }
+
+  private getSyncFlags(): Record<string, boolean> {
+    let flags = memGet<Record<string, boolean>>('repo-cache-flags');
+    if (!flags) {
+      flags = {};
+      memSet('repo-cache-flags', flags);
+    }
+    return flags;
+  }
+
+  private isSynced(url: string): boolean {
+    const flags = this.getSyncFlags();
+    return !!flags[url];
+  }
+
+  private flagSynced(url: string): void {
+    const flags = this.getSyncFlags();
+    flags[url] = true;
+  }
+
+  unflagSynced(url: string): void {
+    const flags = this.getSyncFlags();
+    for (const fullUrl of Object.keys(flags)) {
+      if (fullUrl.endsWith(url)) {
+        delete flags[fullUrl];
+      }
+    }
+  }
+
+  override wrapServerResponse<T>(
+    url: string,
+    resp: HttpResponse<T>,
+  ): Promise<HttpResponse<T>> {
+    const res = super.wrapServerResponse(url, resp);
+    this.flagSynced(url);
+    return res;
+  }
+
+  override async bypassServer<T>(
+    url: string,
+    _ignoreSoftTtl: boolean,
+  ): Promise<HttpResponse<T> | null> {
+    if (!this.isSynced(url)) {
+      return null;
+    }
+
+    const cache = await this.load(url);
+    const httpCache = HttpCache.parse(cache);
+    if (!httpCache) {
+      return null;
+    }
+
+    return copyResponse(httpCache.httpResponse as HttpResponse<T>, true);
   }
 }
 
