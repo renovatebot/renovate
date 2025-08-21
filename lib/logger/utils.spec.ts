917fe35f28e8ad71c60f1f6d9a1521719e920bf9
IceHamster <marius.mo@zaptec.com>
2025-08-21 15:36:28 +0200

    chore: Mask username when an JWT token

--- lib/logger/utils.spec.ts
+++ lib/logger/utils.spec.ts
@@ -84,6 +84,19 @@ describe('logger/utils', () => {
     expect(sanitizeValue(input)).toEqual(expected);
   });
 
+  it('redacts username field if it contains a JWT', () => {
+    const input = {
+      password: 'none',
+      username:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+    };
+    const expected = {
+      password: '***********',
+      username: '***********',
+    };
+    expect(sanitizeValue(input)).toEqual(expected);
+  });
+
   describe('prepareError', () => {
     function getError<T extends z.ZodType>(
       schema: T,
