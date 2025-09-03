3e57dfbccb9d959a00c009f85d8ffa72f3302bf3
trev <me@trev.xyz>
2025-09-03 09:48:31 -0400

    fix: only set lockedVersion for lockable tarballs, cleanup

--- lib/modules/manager/nix/extract.spec.ts
+++ lib/modules/manager/nix/extract.spec.ts
@@ -833,7 +833,6 @@ describe('modules/manager/nix/extract', () => {
     expect(await extractPackageFile('', 'flake.nix')).toMatchObject({
       deps: [
         {
-          currentValue: 'nixpkgs-unstable',
           datasource: 'git-refs',
           depName: 'nixpkgs',
           packageName: 'https://github.com/NixOS/nixpkgs',
@@ -921,12 +920,10 @@ describe('modules/manager/nix/extract', () => {
     expect(await extractPackageFile(flakeNix, 'flake.nix')).toEqual({
       deps: [
         {
-          currentValue: 'nixpkgs-unstable',
           datasource: 'git-refs',
           depName: 'nixpkgs',
           packageName: 'https://github.com/NixOS/nixpkgs',
-          lockedVersion:
-            'https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz',
+          lockedVersion: 'b62d2a95c72f',
         },
       ],
     });
