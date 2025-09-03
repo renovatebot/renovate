c0e287ee1d58cb5822cecb3784caf506a2e97ed5
Johannes Schillinger <johannes.schillinger@dm.de>
2025-09-03 13:05:52 +0200

    feat(copier): make linter happy

--- lib/modules/manager/copier/schema.ts
+++ lib/modules/manager/copier/schema.ts
@@ -2,11 +2,9 @@ import { z } from 'zod';
 import { regEx } from '../../../util/regex';
 import { Yaml } from '../../../util/schema-utils';
 
-const GitSshUrl = z
-  .string()
-  .regex(regEx(/^[^@]+@[^:]*:.+$/), {
-    message: 'Invalid Git SSH URL format',
-  });
+const GitSshUrl = z.string().regex(regEx(/^[^@]+@[^:]*:.+$/), {
+  message: 'Invalid Git SSH URL format',
+});
 export const CopierAnswersFile = Yaml.pipe(
   z.object({
     _commit: z.string(),
