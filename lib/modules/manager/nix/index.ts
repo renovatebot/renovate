7b6b5d6a5ac69143671c30570f9fa5eaa6318e89
Josh Spence <josh@spence.com.au>
2025-09-09 09:57:07 +1000

    Minor tidying

--- lib/modules/manager/nix/index.ts
+++ lib/modules/manager/nix/index.ts
@@ -1,17 +1,15 @@
 import { GitRefsDatasource } from '../../datasource/git-refs';
 
-export { extractPackageFile } from './extract';
 export { updateArtifacts } from './artifacts';
+export { extractPackageFile } from './extract';
 export { getRangeStrategy } from './range';
 export { updateDependency } from './update';
 
+export const supportedDatasources = [GitRefsDatasource.id];
 export const supportsLockFileMaintenance = true;
-
 export const url = 'https://nix.dev';
 
 export const defaultConfig = {
   managerFilePatterns: ['/(^|/)flake\\.nix$/'],
   enabled: false,
 };
-
-export const supportedDatasources = [GitRefsDatasource.id];
