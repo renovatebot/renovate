200c1328cca43b845937e68091e0c797d360d6f0
Gregoire PARIS <gregoire.paris@manomano.com>
2025-09-23 16:12:40 +0200

    feat: print reconfigureBranch
    I have created a reconfigure branch, and Renovate keeps printing that it
    cannot find it. I think adding the name of the branch it is looking for
    would help, in case I am confused about what branchPrefix is exactly.

--- lib/workers/repository/reconfigure/index.ts
+++ lib/workers/repository/reconfigure/index.ts
@@ -40,7 +40,10 @@ export async function checkReconfigureBranch(
 
   // this is something the user initiates, skip if no branch exists
   if (!branchExists) {
-    logger.debug('No reconfigure branch found');
+    logger.debug(
+      { reconfigureBranch: reconfigureBranch },
+      'No reconfigure branch found'
+    );
     deleteReconfigureBranchCache(); // in order to remove cache when the branch has been deleted
     return;
   }
