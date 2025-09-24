b3d39986acede2303f2f90bd918a73625a6f2a68
Mirko Kruschke <mirko.kruschke@gmail.com>
2025-09-24 12:56:12 +0000

    fix: improve schema

--- lib/modules/manager/npm/schema.ts
+++ lib/modules/manager/npm/schema.ts
@@ -6,11 +6,11 @@ export const PnpmCatalogs = z.object({
   catalogs: z.optional(z.record(z.record(z.string()))),
 });
 
-export const YarnCatalog = z.optional(z.record(z.string()));
+export const YarnCatalog = z.record(z.string());
 export type YarnCatalog = z.infer<typeof YarnCatalog>;
 
 export const YarnCatalogs = z.union([
-  z.optional(z.record(z.record(z.string()))), // yarn built-in implementation
+  z.optional(z.record(YarnCatalog)), // yarn built-in implementation
   z.object({
     options: z.optional(z.union([z.string(), z.array(z.string())])),
     list: z.record(z.union([z.string(), z.record(z.string())])),
