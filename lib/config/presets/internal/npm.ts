c4444e86b2dbb1a6eab129aeaa24552a03587634
Jamie Tanna <jamie.tanna@mend.io>
2025-10-01 11:04:42 +0100

    fix(presets): correctly create new `security:minimumReleaseAgeNpm` preset (#38310)
    In abb65969d8e2c85215d5236e0708ac470cec0a07 we introduced a migration
    for `npm:unpublishSafe` to `security:minimumReleaseAgeNpm`.
    
    However, we forgot to create the new `security:minimumReleaseAgeNpm`
    preset, which means we'll be migrating folks to a non-existent preset,
    breaking builds.

--- lib/config/presets/internal/npm.ts
+++ lib/config/presets/internal/npm.ts
@@ -2,12 +2,4 @@ import type { Preset } from '../types';
 
 /* eslint sort-keys: ["error", "asc", {caseSensitive: false, natural: true}] */
 
-export const presets: Record<string, Preset> = {
-  unpublishSafe: {
-    description:
-      'Wait until the npm package is three days old before raising the update, this prevents npm unpublishing a package you already upgraded to.',
-    npm: {
-      minimumReleaseAge: '3 days',
-    },
-  },
-};
+export const presets: Record<string, Preset> = {};
