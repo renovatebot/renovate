78998edbaafea2fccfefb8caada3bf97e66c345e
Tobias Bieniek <tobias@bieniek.cloud>
2025-10-12 16:26:35 +0200

    feat(rust-nightly): Add `rust-nightly-version` datasource

---
+++ lib/modules/datasource/rust-nightly-version/index.spec.ts
@@ -0,0 +1,111 @@
+import type { GetPkgReleasesConfig, GetReleasesConfig } from '..';
+import { getPkgReleases } from '..';
+import { ExternalHostError } from '../../../types/errors/external-host-error';
+import { RustNightlyVersionDatasource } from '.';
+import * as httpMock from '~test/http-mock';
+import { partial } from '~test/util';
+
+const registryUrl = 'https://rust-lang.github.io/rustup-components-history/';
+const datasource = RustNightlyVersionDatasource.id;
+
+let config: GetPkgReleasesConfig;
+
+describe('modules/datasource/rust-nightly-version/index', () => {
+  describe('getReleases', () => {
+    beforeEach(() => {
+      config = {
+        datasource,
+        packageName: 'rust',
+      };
+    });
+
+    it('returns releases for successful API call', async () => {
+      httpMock
+        .scope(registryUrl)
+        .get('/x86_64-unknown-linux-gnu/rust.json')
+        .reply(200, {
+          '2025-10-06': true,
+          '2025-10-07': true,
+          '2025-10-08': false,
+          '2025-10-09': true,
+          '2025-10-10': true,
+          '2025-10-11': true,
+          '2025-10-12': true,
+          last_available: '2025-10-12',
+        });
+
+      const res = await getPkgReleases(config);
+
+      expect(res).not.toBeNull();
+      expect(res?.releases).toHaveLength(6);
+      expect(res?.releases).toEqual([
+        {
+          version: 'nightly-2025-10-06',
+          releaseTimestamp: '2025-10-06T00:00:00.000Z',
+        },
+        {
+          version: 'nightly-2025-10-07',
+          releaseTimestamp: '2025-10-07T00:00:00.000Z',
+        },
+        {
+          version: 'nightly-2025-10-09',
+          releaseTimestamp: '2025-10-09T00:00:00.000Z',
+        },
+        {
+          version: 'nightly-2025-10-10',
+          releaseTimestamp: '2025-10-10T00:00:00.000Z',
+        },
+        {
+          version: 'nightly-2025-10-11',
+          releaseTimestamp: '2025-10-11T00:00:00.000Z',
+        },
+        {
+          version: 'nightly-2025-10-12',
+          releaseTimestamp: '2025-10-12T00:00:00.000Z',
+        },
+      ]);
+      expect(res?.sourceUrl).toBe('https://github.com/rust-lang/rust');
+    });
+
+    it('returns null on 404', async () => {
+      httpMock
+        .scope(registryUrl)
+        .get('/x86_64-unknown-linux-gnu/rust.json')
+        .reply(404);
+
+      const res = await getPkgReleases(config);
+
+      expect(res).toBeNull();
+    });
+
+    it('handles empty releases', async () => {
+      httpMock
+        .scope(registryUrl)
+        .get('/x86_64-unknown-linux-gnu/rust.json')
+        .reply(200, {
+          last_available: '2025-10-12',
+        });
+
+      const res = await getPkgReleases(config);
+
+      expect(res).toBeNull();
+    });
+
+    it('throws on HTTP errors', async () => {
+      httpMock
+        .scope(registryUrl)
+        .get('/x86_64-unknown-linux-gnu/rust.json')
+        .reply(500);
+
+      const rustNightlyVersionDatasource = new RustNightlyVersionDatasource();
+
+      await expect(
+        rustNightlyVersionDatasource.getReleases(
+          partial<GetReleasesConfig>({
+            registryUrl,
+          }),
+        ),
+      ).rejects.toThrow(ExternalHostError);
+    });
+  });
+});
---
+++ lib/modules/datasource/rust-nightly-version/index.ts
@@ -0,0 +1,68 @@
+import { cache } from '../../../util/cache/package/decorator';
+import { asTimestamp } from '../../../util/timestamp';
+import * as rustToolchainNightly from '../../versioning/rust-toolchain-nightly';
+import { Datasource } from '../datasource';
+import type { GetReleasesConfig, Release, ReleaseResult } from '../types';
+
+export class RustNightlyVersionDatasource extends Datasource {
+  static readonly id = 'rust-nightly-version';
+
+  constructor() {
+    super(RustNightlyVersionDatasource.id);
+  }
+
+  override readonly defaultRegistryUrls = [
+    'https://rust-lang.github.io/rustup-components-history/',
+  ];
+
+  override readonly defaultVersioning = rustToolchainNightly.id;
+
+  override readonly customRegistrySupport = false;
+
+  override readonly releaseTimestampSupport = true;
+  override readonly releaseTimestampNote =
+    'The release timestamp is determined from the date in the version.';
+  override readonly sourceUrlSupport = 'package';
+
+  @cache({
+    namespace: `datasource-${RustNightlyVersionDatasource.id}`,
+    key: ({ registryUrl }: GetReleasesConfig) => `${registryUrl}`,
+    ttlMinutes: 60,
+  })
+  async getReleases({
+    registryUrl,
+  }: GetReleasesConfig): Promise<ReleaseResult | null> {
+    /* v8 ignore next 3 -- should never happen */
+    if (!registryUrl) {
+      return null;
+    }
+
+    const url = `${registryUrl}x86_64-unknown-linux-gnu/rust.json`;
+
+    let releases: Release[];
+    try {
+      const response =
+        await this.http.getJsonUnchecked<Record<string, boolean | string>>(url);
+      const data = response.body;
+
+      releases = Object.keys(data)
+        .filter((key) => key !== 'last_available' && data[key] === true)
+        .map((date) => {
+          const version = `nightly-${date}`;
+          const releaseTimestamp = asTimestamp(date);
+          return { version, releaseTimestamp };
+        });
+    } catch (err) {
+      this.handleGenericErrors(err);
+    }
+
+    const res: ReleaseResult = {
+      releases,
+      sourceUrl: 'https://github.com/rust-lang/rust',
+    };
+    if (res.releases.length) {
+      return res;
+    }
+    return null;
+  }
+}
