a1c4d4199735df235eed4a3470fc0e6e6ed3685e
RahulGautamSingh <rahultesnik@gmail.com>
2025-10-18 21:16:16 +0530

    make head_pipleline nullable

--- lib/modules/platform/gitlab/schema.ts
+++ lib/modules/platform/gitlab/schema.ts
@@ -9,10 +9,12 @@ export const LastPipelineId = z
   })
   .transform(({ last_pipeline }) => last_pipeline.id);
 
-const GitlabUserSchema = z.object({
-  id: z.number(),
-  username: z.string(),
-});
+const GitlabUserSchema = z
+  .object({
+    id: z.number(),
+    username: z.string(),
+  })
+  .nullable();
 
 const LongCommitShaSchema = z.string().transform((val) => val as LongCommitSha);
 
@@ -27,7 +29,7 @@ export const GitLabMergeRequestSchema = z.object({
   updated_at: z.string(),
   diverged_commits_count: z.number().optional(),
   merge_status: z.string().optional(),
-  assignee: GitlabUserSchema.optional().nullable(),
+  assignee: GitlabUserSchema.optional(),
   assignees: z.array(GitlabUserSchema).optional(),
   reviewers: z.array(GitlabUserSchema).optional(),
   labels: z.array(z.string()).optional(),
@@ -37,7 +39,8 @@ export const GitLabMergeRequestSchema = z.object({
       status: z.string(),
       sha: LongCommitShaSchema,
     })
-    .optional(),
+    .optional()
+    .nullable(),
 });
 
 export const GitLabMergeRequestsSchema = z.array(GitLabMergeRequestSchema);
