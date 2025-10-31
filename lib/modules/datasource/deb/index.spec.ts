2c5d6a4f432b738d9d6bd307d072e79c71247f13
Sergei Zharinov <zharinov@users.noreply.github.com>
2025-10-31 11:38:11 -0300

    Unmock mutex in deb datasource spec to prevent race condition

--- lib/modules/datasource/deb/index.spec.ts
+++ lib/modules/datasource/deb/index.spec.ts
@@ -274,6 +274,7 @@ describe('modules/datasource/deb/index', () => {
     });
 
     it('should not lead to a race condition on parallel lookups', async () => {
+      vi.unmock('../../../util/mutex');
       const packages = [
         'album',
         'album-data',
