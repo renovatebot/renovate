3826a81387076b7258483bdce1a3cecd8fc54dfb
Jamie Tanna <jamie.tanna@mend.io>
2025-11-11 15:29:17 +0000

    feat(presets): add global preset to allow unsafe actions
    As a means to more easily support self-hosted administrators who wish
    to allowlist certain configuration, we can introduce a new preset,
    `global:allowAllUnsafe`, to turn the configuration all on.
    
    We can add a warning - both in the description and the name - to hint to
    administrators that they should be /very/ careful when enabling this due
    to the additional remote code execution this allows.

--- lib/config/presets/internal/global.ts
+++ lib/config/presets/internal/global.ts
@@ -3,6 +3,16 @@ import type { GlobalPreset } from '../types';
 /* eslint sort-keys: ["error", "asc", {caseSensitive: false, natural: true}] */
 
 export const presets: Record<string, GlobalPreset> = {
+  allowAllUnsafe: {
+    allowCustomCrateRegistries: true,
+    allowedCommands: ['/.*/'],
+    allowedEnv: ['/.*/'],
+    allowedHeaders: ['/.*/'],
+    allowPlugins: true,
+    allowScripts: true,
+    description:
+      'Set all `allow*` and `allowed*` settings to true. Administrators should think carefully before enabling this for their self-hosted usages, as it will introduce all possible avenues for remote code execution from packages and repo owners alike.',
+  },
   safeEnv: {
     allowedEnv: ['GO*', 'GRADLE_OPTS', 'RUSTC_BOOTSTRAP'],
     description:
