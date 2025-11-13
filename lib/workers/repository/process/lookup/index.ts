051de37956b9a3d56319784a2625cb1b41fb0a3b
Josh Spence <josh@spence.com.au>
2025-11-13 22:42:21 +1100

    Revert "Simpler solution"
    This reverts commit 1eb0011bc29c8a3f025a636a69bd26530bdf5b8a.

--- lib/workers/repository/process/lookup/index.ts
+++ lib/workers/repository/process/lookup/index.ts
@@ -778,7 +778,7 @@ export async function lookupUpdates(
       return Result.err(err);
     }
 
-    logger.warn(
+    logger.error(
       {
         currentDigest: config.currentDigest,
         currentValue: config.currentValue,
