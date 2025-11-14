8f6f4218805a0fa3f49fba60e3ad6b4945490e20
Jamie Tanna <jamie.tanna@mend.io>
2025-11-14 16:27:25 +0000

    feat(presets): log the resolved configuration (without internal presets)
    As noted in #15827, it would be useful to log the "shallow"
    configuration for a given repository - that is, the resolved
    configuration, but without any Renovate internal presets being included.
    
    This will be very useful for debugging, as it shows the final resulting
    configuration, but without the - often very - verbose internal presets.
    
    In the case that only this log line is shared, we can make sure to
    reference the version of the Renovate CLI in the log line.
    
    This requires building on top of changes in
    6a4fa3f245dd8976fc8477c4d72f7aae32adab34 to be able to return a `merged`
    and `unmerged` list of presets, and uses helpers from
    c8f42e86b4f182a6c4c530250b2b1d2a0fb20e22, and
    66b84dd12bd9a638b914883fa2781e2def601e9a to process the internal presets
    accordingly.
    
    Since we first worked on this, we've worked on improvements to caching
    which should provide a require no additional performance overhead
    performing this resolution twice.
    
    This can also lead to being able to understand more clearly how Renovate
    configuration is being used[0], as we have the final resolved configuration.
    
    We want to make sure we don't try and merge or visit any internal
    presets if we're not going to merge them, so we can avoid verbose
    `config` or `visitedPresets` results.
    
    [0]: https://www.jvt.me/posts/2024/04/14/renovate-config-sql/

--- lib/workers/repository/init/merge.ts
+++ lib/workers/repository/init/merge.ts
@@ -43,6 +43,7 @@ import {
   getDefaultConfigFileName,
 } from '../onboarding/common.ts';
 import type { RepoFileConfig } from './types.ts';
+import { pkg } from '../../../expose.ts';
 
 export async function detectConfigFile(): Promise<string | null> {
   const fileList = await scm.getFileList();
@@ -245,6 +246,21 @@ export async function mergeRenovateConfig(
     logger.debug('Found npmrc in decrypted config - setting');
     npmApi.setNpmrc(decryptedConfig.npmrc);
   }
+
+  // Provide insight into the repository config, resolving everything but internal Renovate presets
+  // This allows users to understand the fully resolved configuration, including any `github>`, `local>`, etc presets, but excluding anything that's internal to Renovate (which can be verbose and/or less relevant)
+  // This is also known as the "shallow" config
+  const { config: resolvedConfigWithoutInternalPresets, visitedPresets } =
+    await presets.resolveConfigPresets(decryptedConfig, config, [], [], false);
+  logger.debug(
+    {
+      renovateVersion: pkg.version,
+      config: resolvedConfigWithoutInternalPresets,
+      visitedPresets,
+    },
+    'Resolved shallow config, without merging internal presets',
+  );
+
   // Decrypt after resolving in case the preset contains npm authentication instead
   const { config: configToDecrypt } = await presets.resolveConfigPresets(
     decryptedConfig,
