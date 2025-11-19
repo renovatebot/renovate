8d96b445c562b10c024092c9baac191c720216e2
Jamie Tanna <jamie.tanna@mend.io>
2025-11-19 13:07:22 +0000

    fix(security:minimumReleaseAgeNpm): correctly opt-out `lockFileMaintenance` and `replacement`
    Otherwise this will apply to all usages of `minimumReleaseAge` across
    all datasources.
    
    This also wasn't actually opting out of the `minimumReleaseAge`, too.

--- lib/config/presets/internal/security.ts
+++ lib/config/presets/internal/security.ts
@@ -48,18 +48,22 @@ export const presets: Record<string, Preset> = {
       {
         description:
           'Do not require Minimum Release Age for update types that are controlled by the package manager',
+        matchDatasources: ['npm'],
         matchUpdateTypes: ['lockFileMaintenance'],
         prBodyNotes: [
           "⚠️ Renovate's lock file maintenance functionality does not support validating Minimum Release Age, as the package manager performs the required changes to update package(s). Confirm whether your package manager perform its own validation for the Minimum Release Age of packages.",
         ],
+        minimumReleaseAge: null,
       },
       {
         description:
           'Do not require Minimum Release Age for package replacements',
+        matchDatasources: ['npm'],
         matchUpdateTypes: ['replacement'],
         prBodyNotes: [
           "⚠️ Renovate's replacement functionality [does not currently](https://github.com/renovatebot/renovate/issues/39400) wire in the release age for a package, so the Minimum Release Age checks can apply. You will need to manually validate the Minimum Release Age for these package(s).",
         ],
+        minimumReleaseAge: null,
       },
     ],
   },
