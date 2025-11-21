4145c7c9c5486e5ddafd48b5f2f851b7cf3538a7
Jamie Tanna <jamie.tanna@mend.io>
2025-11-21 09:40:22 +0000

    chore(onboarding): add link to `prHourlyLimit` docs
    To make this more actionable.

--- lib/workers/repository/onboarding/pr/pr-list.spec.ts
+++ lib/workers/repository/onboarding/pr/pr-list.spec.ts
@@ -136,7 +136,7 @@ describe('workers/repository/onboarding/pr/pr-list', () => {
 
 
 
-        🚸 Branch creation will be limited to maximum 1 per hour, so it doesn't swamp any CI resources or overwhelm the project. See docs for \`prHourlyLimit\` for details.
+        🚸 Branch creation will be limited to maximum 1 per hour, so it doesn't swamp any CI resources or overwhelm the project. See [docs for \`prHourlyLimit\`](https://docs.renovatebot.com/configuration-options/#prhourlylimit) for details.
 
         "
       `);
