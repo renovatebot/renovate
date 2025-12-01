fe8fe42471c1227b8cdaee0b71d23b2f90be6580
Jamie Tanna <jamie.tanna@mend.io>
2025-12-01 09:51:59 +0000

    chore(instrumentation): add helpers for Git instrumentation (#38582)
    As part of being able to fully instrument our Git operations - using
    SimpleGit - we can introduce some Git-specific instrumentation helpers.
    
    These helpers will be used as part of a future change, where we will
    instrument methods of SimpleGit that we're using.
    
    This provides a set of types which will be used as OpenTelemetry
    Attributes to categorise command types.
    
    A number of subcommands that aren't currently deemed to be too important
    are bucketed into an `other` category.

--- lib/instrumentation/types.ts
+++ lib/instrumentation/types.ts
@@ -3,6 +3,7 @@ import type { RenovateSplit } from '../config/types';
 import type { BunyanRecord } from '../logger/types';
 import type { PackageFile } from '../modules/manager/types';
 import type { BranchCache } from '../util/cache/repository/types';
+import type { GitOperationType } from '../util/git/types';
 
 export type RenovateSpanOptions = {
   attributes?: RenovateSpanAttributes;
@@ -10,6 +11,7 @@ export type RenovateSpanOptions = {
 
 export type RenovateSpanAttributes = {
   [ATTR_RENOVATE_SPLIT]?: RenovateSplit;
+  [ATTR_VCS_GIT_OPERATION_TYPE]?: GitOperationType;
 } & Attributes;
 
 /**
@@ -65,3 +67,14 @@ export interface DependencyStatus {
 }
 
 export const ATTR_RENOVATE_SPLIT = 'renovate.split';
+
+/**
+ * the Git Version Control System (VCS)'s Operation Type
+ *
+ * @see GitOperationType
+ *
+ */
+export const ATTR_VCS_GIT_OPERATION_TYPE = 'vcs.git.operation.type';
+
+/** the Git Version Control System (VCS)'s subcommand */
+export const ATTR_VCS_GIT_SUBCOMMAND = 'vcs.git.subcommand';
