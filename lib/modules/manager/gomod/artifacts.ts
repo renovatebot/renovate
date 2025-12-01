8a8094084ed41a533964e4dd518f7ac450430ee1
Mike Glazer <mike.glazer@gmail.com>
2025-12-01 14:03:05 -0800

    Update fixes

--- lib/modules/manager/gomod/artifacts.ts
+++ lib/modules/manager/gomod/artifacts.ts
@@ -133,8 +133,11 @@ export async function updateArtifacts({
       (await readLocalFile(vendorModulesFileName)) !== null);
   let massagedGoMod = newGoModContent;
   const useGoGenerate = !!config.postUpdateOptions?.includes('goGenerate');
-  const globalConfig = GlobalConfig.get('allowedUnsafeExecutions', []);
-  const goGenerateAllowed = globalConfig.includes('goGenerate');
+  const allowedUnsafeExecutions = GlobalConfig.get(
+    'allowedUnsafeExecutions',
+    [],
+  );
+  const goGenerateAllowed = allowedUnsafeExecutions.includes('goGenerate');
 
   if (config.postUpdateOptions?.includes('gomodMassage')) {
     // Regex match inline replace directive, example:
