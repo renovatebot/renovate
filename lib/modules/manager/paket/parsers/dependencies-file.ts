794a2e7f3600049e67648abc48a096378759d3f1
fpellet <git@florentpellet.com>
2025-12-01 15:18:48 +0100

    reduce => loop

--- lib/modules/manager/paket/parsers/dependencies-file.ts
+++ lib/modules/manager/paket/parsers/dependencies-file.ts
@@ -16,33 +16,39 @@ interface ReduceState {
   currentGroup: DependenciesGroup;
 }
 
+function analyzeLine(state: ReduceState, line: string): ReduceState {
+  const lineParts = line.split(/\s+/).filter(isNonEmptyString);
+  if (lineParts.length < 2) {
+    return state;
+  }
+
+  switch (lineParts[0]) {
+    case 'nuget':
+      state.currentGroup.nugetPackages.push({
+        name: lineParts[1],
+        options: lineParts.slice(2),
+      });
+      break;
+    case 'group':
+      state.groups.push(state.currentGroup);
+      state.currentGroup = {
+        groupName: lineParts[1],
+        nugetPackages: [],
+      };
+  }
+
+  return state;
+}
+
 export function parse(content: string): DependenciesFile {
-  const result: ReduceState = content.split('\n').reduce(
-    (state: ReduceState, line: string) => {
-      const lineParts = line.split(/\s+/).filter(isNonEmptyString);
-      if (lineParts.length < 2) {
-        return state;
-      }
+  let result: ReduceState = {
+    groups: [],
+    currentGroup: { groupName: 'Main', nugetPackages: [] },
+  };
 
-      switch (lineParts[0]) {
-        case 'nuget':
-          state.currentGroup.nugetPackages.push({
-            name: lineParts[1],
-            options: lineParts.slice(2),
-          });
-          break;
-        case 'group':
-          state.groups.push(state.currentGroup);
-          state.currentGroup = {
-            groupName: lineParts[1],
-            nugetPackages: [],
-          };
-      }
-
-      return state;
-    },
-    { groups: [], currentGroup: { groupName: 'Main', nugetPackages: [] } },
-  );
+  for (const item of content.split('\n')) {
+    result = analyzeLine(result, item);
+  }
 
   if (result.currentGroup) {
     result.groups.push(result.currentGroup);
