a3a8b2748cf46c030b957a1c610c26f7b1023f11
Jamie Tanna <jamie.tanna@mend.io>
2025-12-01 17:28:11 +0000

    fix(global): ensure that all `globalOnly` options can be set with Global Config
    As part of #9603, we intended to come back and make sure that all of our
    `globalOnly` configuration fields are set here, so they can be correctly
    updated by configuration (in various forms).
    
    As some configuration may not work if we do not set this, we should make
    sure that this is applied, as well as ensuring that there is a test to
    catch this requirement.

--- lib/config/global.spec.ts
+++ lib/config/global.spec.ts
@@ -1,4 +1,5 @@
 import { GlobalConfig } from './global';
+import { getOptions } from './options';
 
 describe('config/global', () => {
   it('all values in OPTIONS are sorted', () => {
@@ -9,5 +10,18 @@ describe('config/global', () => {
     expect(defined, 'OPTIONS should be sorted alphabetically').toStrictEqual(
       sorted,
     );
+  });
+
+  it('all globalOnly configuration options should be defined in OPTIONS', () => {
+    const globalOnlyOptions = getOptions()
+      .filter((o) => o.globalOnly)
+      .map((o) => o.name);
+
+    const defined = GlobalConfig.OPTIONS;
+
+    expect(
+      globalOnlyOptions,
+      'All globalOnly options should be defined in OPTIONS',
+    ).toEqual(defined);
   });
 });
