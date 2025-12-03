48b736d1ab5ac362061f7034f39d74245a9583bc
Erik Müller <9016208+Seros@users.noreply.github.com>
2025-12-03 12:02:19 +0100

    Add sourceUrl for group:api-platform
    As api-platform is handling its packages a similar way like Symfony but only post the changelog in the main repo, this should be used as the source for the packages and changelog

--- lib/config/presets/internal/group.ts
+++ lib/config/presets/internal/group.ts
@@ -71,6 +71,7 @@ const staticGroups = {
           '!api-platform/postman-collection-generator',
           '!api-platform/schema-generator',
         ],
+        "sourceUrl": "https://github.com/api-platform/core",
       },
     ],
   },
