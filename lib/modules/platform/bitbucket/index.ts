22ee5bb85d3fe6dde4e65b1f813c615ecea4d525
Adam Setch <adam.setch@outlook.com>
2025-12-05 11:12:37 -0800

    fix: more targeted movement of note
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- lib/modules/platform/bitbucket/index.ts
+++ lib/modules/platform/bitbucket/index.ts
@@ -615,10 +615,11 @@ export function massageMarkdown(input: string): string {
       'renaming the PR to start with "rebase!"',
     )
     .replace(
-      regEx(/<summary>View abandoned dependencies(.*)<\/summary>/),
-      '## Abandoned dependencies $1',
+      regEx(
+        /(> ℹ \*\*Note\*\*[\s\S]*?unmaintained:\n)[\s\S]*?(View abandoned dependencies(.*)<\/summary>)/m,
+      ),
+      '## Abandoned dependencies$3\n$1',
     )
-    .replace(regEx(/(>[\s\S]+?)(## Abandoned dependencies.*)/), '$2\n$1')
     .replace(regEx(/<\/?summary>/g), '**')
     .replace(regEx(/<\/?(details|blockquote)>/g), '')
     .replace(regEx(`\n---\n\n.*?<!-- rebase-check -->.*?\n`), '')
