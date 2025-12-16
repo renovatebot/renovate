c9f9fdb93da937aa80a269c67d53a30eee5df526
Johannes Feichtner <johannes@web-wack.at>
2025-12-16 19:42:51 +0100

    fix(workers): re-attempt platform automerge only in case of new commits

--- lib/workers/repository/update/branch/index.ts
+++ lib/workers/repository/update/branch/index.ts
@@ -711,6 +711,7 @@ export async function processBranch(
     if (branchPr) {
       const platformPrOptions = getPlatformPrOptions(config);
       if (
+        commitSha &&
         platformPrOptions.usePlatformAutomerge &&
         platform.reattemptPlatformAutomerge
       ) {
