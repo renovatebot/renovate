e462ac2f9127e31703e50e18533252b99d4732ea
RahulGautamSingh <rahultesnik@gmail.com>
2025-12-18 16:43:14 +0530

    filter files for other platforms

--- lib/config/app-strings.spec.ts
+++ lib/config/app-strings.spec.ts
@@ -12,4 +12,10 @@ describe('config/app-strings', () => {
     expect(filenames.includes('abc')).toBeTrue();
     expect(filenames.includes('def')).toBeTrue();
   });
+
+  it('filters based on platform', () => {
+    const filenames = getConfigFileNames('gitea');
+    expect(filenames.includes('.github/renovate.json')).toBeFalse();
+    expect(filenames.includes('.gitea/renovate.json')).toBeTrue();
+  });
 });
