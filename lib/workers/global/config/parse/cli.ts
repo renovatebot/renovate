69551ced45045e751ebe31e1afd469da72a9e598
Jamie Tanna <jamie.tanna@mend.io>
2025-12-24 13:42:50 +0000

    rew! chore: show default values in the `--help` output

--- lib/workers/global/config/parse/cli.ts
+++ lib/workers/global/config/parse/cli.ts
@@ -58,6 +58,7 @@ export function getConfig(input: string[]): AllConfig {
         optionString,
         option.description,
         coersions[option.type],
+        option.default,
       );
     }
   });
