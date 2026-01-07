be38498726cdeab5b2450d41a5a81223fc66451b
Sharon Yogev <sharon@glow.io>
2026-01-07 11:21:05 +0200

    Use brace expansions in the patterns

--- lib/modules/manager/mise/index.ts
+++ lib/modules/manager/mise/index.ts
@@ -21,23 +21,12 @@ export const url = 'https://mise.jdx.dev';
 
 export const defaultConfig = {
   managerFilePatterns: [
-    '**/mise.toml',
-    '**/mise.*.toml',
-    '**/.mise.toml',
-    '**/.mise.*.toml',
-    '**/mise/config.toml',
-    '**/mise/config.*.toml',
-    '**/.mise/config.toml',
-    '**/.mise/config.*.toml',
-    '**/.config/mise.toml',
-    '**/.config/mise.*.toml',
-    '**/.config/mise/config.toml',
-    '**/.config/mise/config.*.toml',
-    '**/.config/mise/mise.toml',
-    '**/.config/mise/mise.*.toml',
+    '**/{,.}mise{,.*}.toml',
+    '**/{,.}mise/config{,.*}.toml',
+    '**/.config/mise{,,*}.toml',
+    '**/.config/mise/{mise,config}{,.*}.toml',
     '**/.config/mise/conf.d/*.toml',
-    '**/.rtx.toml',
-    '**/.rtx.*.toml',
+    '**/.rtx{,.*}.toml',
   ],
 };
 
