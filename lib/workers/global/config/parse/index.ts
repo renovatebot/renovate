0859950540313d5648e5e548f23a8f77feea9171
Jamie Tanna <jamie.tanna@mend.io>
2026-01-14 14:04:45 +0000

    refactor(presets): return merged presets from `resolveConfigPresets`
    As part of future changes, we want to log the shallow configuration (the
    full resolved configuration, but without any internal Renovate
    configuration presets), where we'll want to note which presets have not
    been merged in.
    
    Before that, we can start with an initial refactor to indicate which
    have been merged, and return them.

--- lib/workers/global/config/parse/index.ts
+++ lib/workers/global/config/parse/index.ts
@@ -28,7 +28,7 @@ export async function resolveGlobalExtends(
   try {
     // Make a "fake" config to pass to resolveConfigPresets and resolve globalPresets
     const config = { extends: globalExtends, ignorePresets };
-    const resolvedConfig = await resolveConfigPresets(config);
+    const { config: resolvedConfig } = await resolveConfigPresets(config);
     return resolvedConfig;
   } catch (err) {
     logger.error({ err }, 'Error resolving config preset');
