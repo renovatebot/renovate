c412a8ea93a3b1485b2eb1b263a9f637c9a4997d
Jamie Tanna <jamie.tanna@mend.io>
2026-01-26 11:59:07 +0000

    fix(self-hosted)!: don't allow any unsafe commands by default
    As a safety measure for self-hosted administrators, we can make sure
    that we set `allowedUnsafeExecutions` to not allow any commands by
    default.
    
    This closes off a significant risk of executing Gradle Wrapper
    commands, which use the repository's copy of the wrapper, and any
    Gradle buildscript, when executing, which can lead to significant remote
    code execution, even before you take into account malicious dependencies.
    
    As this is now off-by-default, we can enable it by default in our tests
    to make sure that there are minimal changes required.

--- lib/modules/manager/gradle/artifacts.ts
+++ lib/modules/manager/gradle/artifacts.ts
@@ -29,9 +29,7 @@ import {
 import { isGradleBuildFile } from './utils.ts';
 
 export function isGradleExecutionAllowed(command: string): boolean {
-  const allowlist = GlobalConfig.get('allowedUnsafeExecutions', [
-    'gradleWrapper',
-  ]);
+  const allowlist = GlobalConfig.get('allowedUnsafeExecutions', []);
 
   if (!allowlist.includes('gradleWrapper')) {
     logger.once.warn(
