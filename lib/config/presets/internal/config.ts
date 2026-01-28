28dccbaf9f4f3aecd562ba09dcfe9470a0c0e6d5
Adam Setch <adam.setch@outlook.com>
2026-01-28 02:58:27 -0500

    feat(presets)!: add `maintainLockFilesWeekly` to `best-practices` preset (#40735)
    As a commonly asked about feature, and a "rough edge" at times, 
    enable this by default, as it is a best practice we recommend.
    
    We use `:maintainLockFilesWeekly` instead of manually adding it,
    so it's easier to use with `ignorePresets`.
    
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- lib/config/presets/internal/config.ts
+++ lib/config/presets/internal/config.ts
@@ -14,6 +14,7 @@ export const presets: Record<string, Preset> = {
       ':pinDevDependencies',
       'abandonments:recommended',
       'security:minimumReleaseAgeNpm',
+      ':maintainLockFilesWeekly',
     ],
   },
   'js-app': {
