09eacc5b04ec1c3f2fb69c1f4a5443ce5fc80a9d
Clément Guillot <guillot.clement@pm.me>
2026-02-05 09:34:26 +0100

    feat(bazel-module): use `crate` versioning when building package dependencies

--- lib/modules/manager/bazel-module/parser/crate.ts
+++ lib/modules/manager/bazel-module/parser/crate.ts
@@ -48,7 +48,7 @@ export const RuleToCratePackageDep = ExtensionTagFragment.extend({
 
     const dep: PackageDependency<CargoManagerData> = {
       datasource: CrateDatasource.id,
-      versioning: 'semver',
+      versioning: CrateDatasource.id,
       depName: packageName.value,
       currentValue,
       depType: 'crate_spec',
