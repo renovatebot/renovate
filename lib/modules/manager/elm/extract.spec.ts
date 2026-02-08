575b53a9532db04fc8ece1570d1428bd19381d13
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2026-02-08 10:59:43 +0100

    refactor(elm): inline test fixtures

--- lib/modules/manager/elm/extract.spec.ts
+++ lib/modules/manager/elm/extract.spec.ts
@@ -1,10 +1,71 @@
 import { codeBlock } from 'common-tags';
 import { extractPackageFile } from './index.ts';
-import { Fixtures } from '~test/fixtures.ts';
 
-const applicationJson = Fixtures.get('application.json');
-const packageJson = Fixtures.get('package.json');
-const minimalJson = Fixtures.get('minimal.json');
+const applicationJson = codeBlock`
+  {
+      "type": "application",
+      "source-directories": [
+          "src"
+      ],
+      "elm-version": "0.19.1",
+      "dependencies": {
+          "direct": {
+              "NoRedInk/elm-json-decode-pipeline": "1.0.0",
+              "elm/browser": "1.0.0",
+              "elm/core": "1.0.0",
+              "elm/html": "1.0.0",
+              "elm/http": "1.0.0",
+              "elm/json": "1.0.0",
+              "elm/time": "1.0.0",
+              "elm/url": "1.0.0"
+          },
+          "indirect": {
+              "elm/parser": "1.0.0",
+              "elm/virtual-dom": "1.0.0"
+          }
+      },
+      "test-dependencies": {
+          "direct": {
+              "elm-explorations/test": "1.0.0"
+          },
+          "indirect": {
+              "elm/random": "1.0.0"
+          }
+      }
+  }
+`;
+
+const packageJson = codeBlock`
+  {
+      "type": "package",
+      "name": "elm/core",
+      "summary": "Elm's standard library",
+      "license": "BSD-3-Clause",
+      "version": "1.0.5",
+      "exposed-modules": {
+          "Primitives": [
+              "Basics",
+              "String",
+              "Char"
+          ]
+      },
+      "elm-version": "0.19.0 <= v < 0.20.0",
+      "dependencies": {
+          "elm/json": "1.0.0 <= v < 2.0.0"
+      },
+      "test-dependencies": {
+          "elm-explorations/test": "1.0.0 <= v < 2.0.0"
+      }
+  }
+`;
+
+const minimalJson = codeBlock`
+  {
+      "type": "application",
+      "elm-version": "0.19.1",
+      "source-directories": ["src"]
+  }
+`;
 
 describe('modules/manager/elm/extract', () => {
   describe('extractPackageFile', () => {
