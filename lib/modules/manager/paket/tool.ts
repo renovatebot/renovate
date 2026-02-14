c3254cba540b1c52dbd8417f9300fea76b842aeb
fpellet <git@florentpellet.com>
2026-02-14 12:21:58 +0100

    Add doc

--- lib/modules/manager/paket/tool.ts
+++ lib/modules/manager/paket/tool.ts
@@ -3,6 +3,12 @@ import { exec } from '../../../util/exec/index.ts';
 import type { ExecOptions } from '../../../util/exec/types.ts';
 import type { UpdatePackage } from './types.ts';
 
+/**
+ * Call the paket cli to update dependencies by refreshing the lock file.
+ * https://fsprojects.github.io/Paket/paket-update.html
+ *
+ * @param command - parameters of paket cli
+ */
 export async function updatePackage(command: UpdatePackage): Promise<void> {
   const execOptions: ExecOptions = {
     cwdFile: command.filePath,
@@ -20,6 +26,13 @@ export async function updatePackage(command: UpdatePackage): Promise<void> {
   );
 }
 
+/**
+ * Call the paket cli to update dependencies by refreshing the lock file.
+ * https://fsprojects.github.io/Paket/paket-update.html
+ *
+ * @param filePath - The path to Paket dependencies file. Allows you to know the directory where to execute the paket command.
+ * @param group - The dependency group name. If not specified, then it updates all groups.
+ */
 export async function updateAllPackages(
   filePath: string,
   group?: string,
