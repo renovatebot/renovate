c7970cce0e78079f7f29f0bc4df2fc4f600942a6
swiftty <62803132+swiftty@users.noreply.github.com>
2026-02-26 03:24:55 +0900

    test: add test cases

--- lib/modules/manager/swift/index.spec.ts
+++ lib/modules/manager/swift/index.spec.ts
@@ -106,47 +106,75 @@ describe('modules/manager/swift/index', () => {
       ).toBeNull();
     });
 
-    it('parses package descriptions', () => {
+    it.each`
+      extraArgs
+      ${''}
+      ${', traits: []'}
+      ${', traits: [.defaults]'}
+      ${', traits: [\n.trait(name: "CUSTOM_TRAIT"),\n.trait(name: "ANOTHER_TRAIT")\n]'}
+    `('parses package descriptions', ({ extraArgs }) => {
       expect(
         extractPackageFile(
-          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",from:"1.2.3")]`,
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",from:"1.2.3"${extraArgs})]`,
         ),
       ).toMatchObject({ deps: [{ currentValue: 'from:"1.2.3"' }] });
       expect(
         extractPackageFile(
-          `dependencies:[.package(url:"https://github.com/vapor/vapor.git","1.2.3"...)]`,
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git","1.2.3"...${extraArgs})]`,
         ),
       ).toMatchObject({ deps: [{ currentValue: '"1.2.3"...' }] });
       expect(
         extractPackageFile(
-          `dependencies:[.package(url:"https://github.com/vapor/vapor.git","1.2.3"..."1.2.4")]`,
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git","1.2.3"..."1.2.4"${extraArgs})]`,
         ),
       ).toMatchObject({ deps: [{ currentValue: '"1.2.3"..."1.2.4"' }] });
       expect(
         extractPackageFile(
-          `dependencies:[.package(url:"https://github.com/vapor/vapor.git","1.2.3"..<"1.2.4")]`,
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git","1.2.3"..<"1.2.4"${extraArgs})]`,
         ),
       ).toMatchObject({ deps: [{ currentValue: '"1.2.3"..<"1.2.4"' }] });
       expect(
         extractPackageFile(
-          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",..."1.2.3")]`,
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",..."1.2.3"${extraArgs})]`,
         ),
       ).toMatchObject({ deps: [{ currentValue: '..."1.2.3"' }] });
       expect(
         extractPackageFile(
-          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",..<"1.2.3")]`,
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",..<"1.2.3"${extraArgs})]`,
         ),
       ).toMatchObject({ deps: [{ currentValue: '..<"1.2.3"' }] });
       expect(
         extractPackageFile(
-          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",.exact("1.2.3"))]`,
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",.exact("1.2.3")${extraArgs})]`,
         ),
       ).toMatchObject({ deps: [{ currentValue: '1.2.3' }] });
       expect(
         extractPackageFile(
-          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",exact:"1.2.3")]`,
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",exact:"1.2.3"${extraArgs})]`,
         ),
       ).toMatchObject({ deps: [{ currentValue: '1.2.3' }] });
+      expect(
+        extractPackageFile(
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",.branch("main")${extraArgs})]`,
+        ),
+      ).toMatchObject({ deps: [{ currentValue: '"main"' }] });
+      expect(
+        extractPackageFile(
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",branch:"develop"${extraArgs})]`,
+        ),
+      ).toMatchObject({ deps: [{ currentValue: '"develop"' }] });
+      expect(
+        extractPackageFile(
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",.revision("c9fec45276bda81f30970316de8de246398ee1c1")${extraArgs})]`,
+        ),
+      ).toMatchObject({
+        deps: [{ currentValue: '"c9fec45276bda81f30970316de8de246398ee1c1"' }],
+      });
+      expect(
+        extractPackageFile(
+          `dependencies:[.package(url:"https://github.com/vapor/vapor.git",revision:"abc123"${extraArgs})]`,
+        ),
+      ).toMatchObject({ deps: [{ currentValue: '"abc123"' }] });
     });
 
     it('parses multiple packages', () => {
