ca1c14db85ba8746d41eb94e5b2b29d3b7548848
Felipe Santos <felipe.santos@ericsson.com>
2026-02-26 12:33:21 -0300

    refactor: extract `applyHostRules` and `applyNpmrc` to functions

--- lib/workers/repository/init/merge.ts
+++ lib/workers/repository/init/merge.ts
@@ -239,12 +239,8 @@ export async function mergeRenovateConfig(
   const repository = config.repository!;
   // Decrypt before resolving in case we need npm authentication for any presets
   const decryptedConfig = await decryptConfig(migratedConfig, repository);
-  setNpmTokenInNpmrc(decryptedConfig);
-  // istanbul ignore if
-  if (isString(decryptedConfig.npmrc)) {
-    logger.debug('Found npmrc in decrypted config - setting');
-    npmApi.setNpmrc(decryptedConfig.npmrc);
-  }
+  logger.debug('Processing npmrc in decrypted config');
+  applyNpmrc(decryptedConfig);
   // Decrypt after resolving in case the preset contains npm authentication instead
   const { config: configToDecrypt } = await presets.resolveConfigPresets(
     decryptedConfig,
@@ -259,14 +255,8 @@ export async function mergeRenovateConfig(
     logger.trace({ config: resolvedConfig }, 'resolved config after migrating');
     resolvedConfig = migrationResult.migratedConfig;
   }
-  setNpmTokenInNpmrc(resolvedConfig);
-  // istanbul ignore if
-  if (isString(resolvedConfig.npmrc)) {
-    logger.debug(
-      'Ignoring any .npmrc files in repository due to configured npmrc',
-    );
-    npmApi.setNpmrc(resolvedConfig.npmrc);
-  }
+  logger.debug('Processing npmrc in resolved config');
+  applyNpmrc(resolvedConfig);
   resolvedConfig = applySecretsAndVariablesToConfig({
     config: resolvedConfig,
     secrets: mergeChildConfig(
@@ -279,24 +269,7 @@ export async function mergeRenovateConfig(
     ),
   });
 
-  // istanbul ignore if
-  if (resolvedConfig.hostRules) {
-    logger.debug('Setting hostRules from config');
-    for (const rule of resolvedConfig.hostRules) {
-      try {
-        hostRules.add(rule);
-      } catch (err) {
-        logger.warn(
-          { err, config: rule },
-          'Error setting hostRule from config',
-        );
-      }
-    }
-    // host rules can change concurrency
-    queue.clear();
-    throttle.clear();
-    delete resolvedConfig.hostRules;
-  }
+  applyHostRules(resolvedConfig);
   returnConfig = mergeChildConfig(returnConfig, resolvedConfig);
   ({ config: returnConfig } = await presets.resolveConfigPresets(
     returnConfig,
@@ -315,6 +288,34 @@ export async function mergeRenovateConfig(
   delete returnConfig.env;
 
   return returnConfig;
+}
+
+export function applyNpmrc(config: RenovateConfig): void {
+  setNpmTokenInNpmrc(config);
+  if (!isString(config.npmrc)) {
+    return;
+  }
+  logger.debug('Setting npmrc from config');
+  npmApi.setNpmrc(config.npmrc);
+}
+
+export function applyHostRules(config: RenovateConfig): void {
+  if (!config.hostRules) {
+    return;
+  }
+
+  logger.debug('Setting hostRules from config');
+  for (const rule of config.hostRules) {
+    try {
+      hostRules.add(rule);
+    } catch (err) {
+      logger.warn({ err, config: rule }, 'Error setting hostRule from config');
+    }
+  }
+  // host rules can change concurrency
+  queue.clear();
+  throttle.clear();
+  delete config.hostRules;
 }
 
 /** needed when using portal secrets for npmToken */
