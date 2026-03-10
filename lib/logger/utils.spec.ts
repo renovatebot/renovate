e438887dfb20ffe82fb24b8df5d546c0290d9f8d
Michael Kriese <michael.kriese@visualon.de>
2026-03-10 17:57:29 +0100

    build: lazy load and initialize logger (#41425)
    * build: lazy load initialization modules
    
    * build: lazy load and initialize logger
    
    * chore: lint fix
    
    * Update lib/constants/error-messages.ts
    
    Co-authored-by: Jamie Tanna <jamie.tanna@mend.io>
    
    * Apply suggestion
    
    Co-authored-by: Jamie Tanna <jamie.tanna@mend.io>
    
    ---------
    
    Co-authored-by: Jamie Tanna <jamie.tanna@mend.io>

--- lib/logger/utils.spec.ts
+++ lib/logger/utils.spec.ts
@@ -1,44 +1,11 @@
 import { TimeoutError } from 'got';
 import { z } from 'zod/v3';
 import { ExecError } from '../util/exec/exec-error.ts';
-import prepareError, {
-  prepareZodIssues,
-  sanitizeValue,
-  validateLogLevel,
-} from './utils.ts';
+import prepareError, { prepareZodIssues, sanitizeValue } from './utils.ts';
 
 describe('logger/utils', () => {
   afterEach(() => {
     vi.restoreAllMocks();
-  });
-
-  it('checks for valid log levels', () => {
-    expect(validateLogLevel(undefined, 'info')).toBe('info');
-    expect(validateLogLevel('warn', 'info')).toBe('warn');
-    expect(validateLogLevel('debug', 'info')).toBe('debug');
-    expect(validateLogLevel('trace', 'info')).toBe('trace');
-    expect(validateLogLevel('info', 'info')).toBe('info');
-    expect(validateLogLevel('error', 'info')).toBe('error');
-    expect(validateLogLevel('fatal', 'info')).toBe('fatal');
-  });
-
-  it.each`
-    input
-    ${'warning'}
-    ${'100'}
-    ${''}
-    ${' '}
-  `('checks for invalid log level: $input', (input) => {
-    // Mock when the function exits
-    const mockExit = vi.spyOn(process, 'exit');
-    mockExit.mockImplementationOnce((number) => {
-      // TODO: types (#22198)
-      throw new Error(`process.exit: ${number}`);
-    });
-    expect(() => {
-      validateLogLevel(input, 'info');
-    }).toThrow();
-    expect(mockExit).toHaveBeenCalledExactlyOnceWith(1);
   });
 
   it.each`
