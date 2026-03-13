52194a90acd671a131a8b0508020b38a15f48018
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2026-03-12 22:31:26 -0400

    fix(config): remove dead rpmMetadataSource validation

--- lib/config/validation.ts
+++ lib/config/validation.ts
@@ -930,14 +930,6 @@ async function validateGlobalConfig(
             topic: 'Configuration Error',
             message: `Invalid value \`${val}\` for \`${currentPath}\`. The allowed values are ${['default', 'ssh', 'endpoint'].join(', ')}.`,
           });
-        } else if (
-          key === 'rpmMetadataSource' &&
-          !['auto', 'primary', 'primary_db'].includes(val)
-        ) {
-          warnings.push({
-            topic: 'Configuration Error',
-            message: `Invalid value \`${val}\` for \`${currentPath}\`. The allowed values are ${['auto', 'primary', 'primary_db'].join(', ')}.`,
-          });
         }
 
         if (
