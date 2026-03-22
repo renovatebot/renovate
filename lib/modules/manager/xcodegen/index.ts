4f91999309f2f63a533bab63fe8a4a9a03f7f264
Stefan Ceriu <stefan.ceriu@gmail.com>
2026-03-22 12:40:44 +0200

    fix(xcodegen): use semver instead of swift versioning

--- lib/modules/manager/xcodegen/index.ts
+++ lib/modules/manager/xcodegen/index.ts
@@ -2,7 +2,6 @@ import type { Category } from '../../../constants/index.ts';
 import { GitTagsDatasource } from '../../datasource/git-tags/index.ts';
 import { GithubTagsDatasource } from '../../datasource/github-tags/index.ts';
 import { GitlabTagsDatasource } from '../../datasource/gitlab-tags/index.ts';
-import * as swiftVersioning from '../../versioning/swift/index.ts';
 
 export { extractPackageFile } from './extract.ts';
 
@@ -12,7 +11,6 @@ export const categories: Category[] = ['swift'];
 
 export const defaultConfig = {
   managerFilePatterns: ['/(^|/)project\\.yml$/'],
-  versioning: swiftVersioning.id,
   pinDigests: false,
 };
 
