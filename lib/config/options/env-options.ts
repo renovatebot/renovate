58ad66f806623ed9902fa1a2653eec43d45dc21d
Jamie Tanna <jamie.tanna@mend.io>
2026-03-27 14:26:38 +0000

    sq

--- lib/config/options/env-options.ts
+++ lib/config/options/env-options.ts
@@ -11,17 +11,21 @@ export interface EnvOptionInfo {
 export type EnvOptionsMap = Record<string, EnvOptionInfo>;
 
 export function getEnvOptionsMap(): EnvOptionsMap {
-  const map: EnvOptionsMap = {};
+  const entries: [string, EnvOptionInfo][] = [];
   for (const option of getOptions()) {
     const envName = getEnvName(option);
     if (envName) {
-      map[envName] = {
-        configName: option.name,
-        globalOnly: option.globalOnly ?? false,
-        inheritConfigSupport: option.inheritConfigSupport ?? false,
-        type: option.type,
-      };
+      entries.push([
+        envName,
+        {
+          configName: option.name,
+          globalOnly: option.globalOnly ?? false,
+          inheritConfigSupport: option.inheritConfigSupport ?? false,
+          type: option.type,
+        },
+      ]);
     }
   }
-  return map;
+  entries.sort(([a], [b]) => a.localeCompare(b));
+  return Object.fromEntries(entries);
 }
