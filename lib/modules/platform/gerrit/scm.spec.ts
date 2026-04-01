6595300f36a520b9cf437c36fa4f868aba9c7ecc
Felipe Santos <felipe.santos@ericsson.com>
2026-04-01 20:07:16 +0200

    Fix test case name

--- lib/modules/platform/gerrit/scm.spec.ts
+++ lib/modules/platform/gerrit/scm.spec.ts
@@ -479,7 +479,7 @@ describe('modules/platform/gerrit/scm', () => {
       expect(git.pushCommit).not.toHaveBeenCalled();
     });
 
-    it('commitAndPush() - existing change without new changes', async () => {
+    it('commitAndPush() - existing change keeps original target branch', async () => {
       const existingChange = partial<GerritChange>({
         change_id: 'Ifcd936eef0ced620040a07a337c586d0a882725b',
         branch: 'main',
