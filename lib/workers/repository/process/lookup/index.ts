e8d13714aa06792f9f360ec79c773bf4a95c6f11
Michael Kriese <michael.kriese@visualon.de>
2026-04-02 13:58:01 +0200

    fix(replacements): don't use current releases for new digest when new name differs (#42333)
    * fix(replacements): don't use current releases for new digest when new name differs
    
    * chore: fix types

--- lib/workers/repository/process/lookup/index.ts
+++ lib/workers/repository/process/lookup/index.ts
@@ -93,6 +93,7 @@ export async function lookupUpdates(
     );
     if (config.currentValue && !isString(config.currentValue)) {
       // If currentValue is not a string, then it's invalid
+      // v8 ignore else -- TODO: add test #40625
       if (config.currentValue) {
         logger.debug(
           `Invalid currentValue for ${config.packageName}: ${JSON.stringify(config.currentValue)} (${typeof config.currentValue})`,
@@ -326,6 +327,7 @@ export async function lookupUpdates(
         )!;
 
       if (!currentVersion) {
+        // v8 ignore else -- TODO: add test #40625
         if (!config.lockedVersion) {
           logger.debug(
             `No currentVersion or lockedVersion found for ${config.packageName}`,
@@ -470,6 +472,7 @@ export async function lookupUpdates(
           release.version,
           versioningApi,
         );
+        // v8 ignore else -- TODO: add test #40625
         if (isString(bucket)) {
           if (buckets[bucket]) {
             buckets[bucket].push(release);
@@ -616,6 +619,7 @@ export async function lookupUpdates(
     ) {
       for (const update of res.updates) {
         logger.debug({ update });
+        // v8 ignore else -- TODO: add test #40625
         if (isString(config.currentValue) && isString(update.newValue)) {
           update.newValue = config.currentValue.replace(
             compareValue,
@@ -637,6 +641,7 @@ export async function lookupUpdates(
         }
       } else if (config.pinDigests) {
         // Create a pin only if one doesn't already exists
+        // v8 ignore else -- TODO: add test #40625
         if (!res.updates.some((update) => update.updateType === 'pin')) {
           // pin digest
           res.updates.push({
@@ -649,7 +654,7 @@ export async function lookupUpdates(
       if (versioningApi.valueToVersion) {
         // TODO #22198
         res.currentVersion = versioningApi.valueToVersion(res.currentVersion!);
-        for (const update of res.updates || /* istanbul ignore next*/ []) {
+        for (const update of res.updates) {
           // TODO #22198
           update.newVersion = versioningApi.valueToVersion(update.newVersion!);
         }
@@ -682,11 +687,22 @@ export async function lookupUpdates(
             getDigestConfig.replacementName = update.newName;
           }
 
-          // TODO #22198
-          update.newDigest ??=
-            dependency?.releases.find((r) => r.version === update.newValue)
-              ?.newDigest ??
-            (await getDigest(getDigestConfig, update.newValue))!;
+          // Don't use current releases if replacement changes name, otherwise we use the wrong new digest.
+          // This happens on datasources which return the digest in release info like `github-tags`.
+          // We can still use it when only version is changing.
+          if (
+            update.updateType !== 'replacement' ||
+            update.newName === config.packageName
+          ) {
+            update.newDigest ??= dependency?.releases.find(
+              (r) => r.version === update.newValue,
+            )?.newDigest;
+          }
+
+          update.newDigest ??= await getDigest(
+            getDigestConfig,
+            update.newValue,
+          );
 
           // If the digest could not be determined, report this as otherwise the
           // update will be omitted later on without notice.
@@ -754,8 +770,7 @@ export async function lookupUpdates(
     if (config.rollbackPrs && config.followTag) {
       res.updates = res.updates.filter(
         (update) =>
-          res.updates.length === 1 ||
-          /* istanbul ignore next */ update.updateType !== 'rollback',
+          update.updateType !== 'rollback' || res.updates.length === 1,
       );
     }
 
