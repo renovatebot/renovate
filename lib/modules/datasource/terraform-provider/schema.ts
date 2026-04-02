377f5434a2b5a9fb47c27f37e08cac8b57032e68
Arnaud Dezandee <arnaud.dezandee@gmail.com>
2026-04-02 15:43:49 +0200

    feat(terraform-provider): add zod schemas for v2 API response

---
+++ lib/modules/datasource/terraform-provider/schema.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod/v3';
+
+const TerraformProviderV2Attributes = z.object({
+  source: z.string().optional(),
+});
+
+const TerraformProviderVersionV2 = z.object({
+  type: z.literal('provider-versions'),
+  attributes: z.object({
+    version: z.string(),
+    'published-at': z.string().optional(),
+  }),
+});
+
+export const TerraformProviderV2Response = z.object({
+  data: z.object({
+    attributes: TerraformProviderV2Attributes,
+  }),
+  included: TerraformProviderVersionV2.array().optional(),
+});
+export type TerraformProviderV2Response = z.infer<
+  typeof TerraformProviderV2Response
+>;
