afdbf0ed053e93513de2d5a89980aa4b50c05398
Mike Ryan <justfalter@users.noreply.github.com>
2026-04-07 17:36:12 -0500

    Refactor comments for clarity in types.ts

--- lib/workers/repository/changelog/types.ts
+++ lib/workers/repository/changelog/types.ts
@@ -1,8 +1,8 @@
 import type { FetchChangeLogsOptions } from '../../../config/types.ts';
 import type { BranchUpgradeConfig } from '../../types.ts';
 
-// Extend is ensures that U is a subset of T, helping us be sure that we get a typescript error should
-// FetchChangeLogOptions someday remove 'branch' or 'pr' from the union.
+// The Extend utility type just ensures that U is a subset of T, making sure that we get a typescript error should
+// 'branch' or 'pr' ever be removed from the FetchChangeLogOptions union.
 type Extends<T, U extends T> = U;
 export type SupportedChangelogStages = Extends<
   FetchChangeLogsOptions,
