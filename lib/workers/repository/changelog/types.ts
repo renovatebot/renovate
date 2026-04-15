cc8532265051b8bddf77b3f63720f6dbd320bd9a
Mike Ryan <falter@gmail.com>
2026-04-15 15:17:14 -0500

    relocate Extends utility type

--- lib/workers/repository/changelog/types.ts
+++ lib/workers/repository/changelog/types.ts
@@ -1,9 +1,7 @@
 import type { FetchChangeLogsOptions } from '../../../config/types.ts';
+import type { Extends } from '../../../types/index.ts';
 import type { BranchUpgradeConfig } from '../../types.ts';
 
-// The Extend utility type just ensures that U is a subset of T, making sure that we get a typescript error should
-// 'branch' or 'pr' ever be removed from the FetchChangeLogOptions union.
-type Extends<T, U extends T> = U;
 export type SupportedChangelogStages = Extends<
   FetchChangeLogsOptions,
   'branch' | 'pr'
