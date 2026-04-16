081f39b46c101f2134d298b6a9210e6687783027
Felipe Santos <felipe.santos@ericsson.com>
2026-04-15 21:36:52 -0300

    Revert "Simplify Gerrit commitAndPush by leveraging git.commitFiles"
    This reverts commit d71edcc27942327b23003eacdf807c6033592797.

--- lib/util/git/index.ts
+++ lib/util/git/index.ts
@@ -1429,9 +1429,7 @@ export async function commitFiles(
     if (commitResult) {
       const pushResult = await pushCommit({
         sourceRef: commitConfig.branchName,
-        targetRef: commitConfig.targetRef,
         files: commitConfig.files,
-        pushOptions: commitConfig.pushOptions,
       });
       // v8 ignore else -- TODO: add test #40625
       if (pushResult) {
