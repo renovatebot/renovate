608966f5f0dfadd027ae9f32e5be4d41e7731c78
Jamie Tanna <jamie.tanna@mend.io>
2026-04-27 10:00:39 +0100

    fixup! fix(datasource): handle both config and release constraints as a range

--- lib/modules/datasource/index.spec.ts
+++ lib/modules/datasource/index.spec.ts
@@ -939,7 +939,11 @@ describe('modules/datasource/index', () => {
             constraintsFiltering: 'strict',
           });
           expect(res).toMatchObject({
-            releases: [{ version: '0.0.3' }, { version: '0.0.4' }],
+            releases: [
+              { version: '0.0.2' },
+              { version: '0.0.3' },
+              { version: '0.0.4' },
+            ],
           });
         });
       });
