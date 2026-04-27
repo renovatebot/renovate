09b676c3b17dc1f6e33cc82c4888ebc8480f6ddf
Jamie Tanna <jamie.tanna@mend.io>
2026-04-27 15:16:23 +0100

    refactor: use `execa`

--- tools/static-data/generate-mise-registry.ts
+++ tools/static-data/generate-mise-registry.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import { execa } from 'execa';
 import { z } from 'zod/v4';
 import { MiseRegistryJson } from '../../lib/modules/manager/mise/schema.ts';
 import { updateJsonFile } from './utils.mjs';
@@ -9,11 +9,15 @@ const MiseRegistry = z.array(
 );
 
 // use the JSON output, as the default output includes a banner
-const versionOutput = execSync('mise version --json', { encoding: 'utf8' });
-const version = MiseVersion.parse(JSON.parse(versionOutput));
+const versionOutput = await execa('mise', ['version', '--json'], {
+  encoding: 'utf8',
+});
+const version = MiseVersion.parse(JSON.parse(versionOutput.stdout));
 console.log(`Generating mise registry using mise version ${version.version}`);
-const output = execSync('mise registry --json', { encoding: 'utf8' });
-const tools = MiseRegistry.parse(JSON.parse(output));
+const output = await execa('mise', ['registry', '--json'], {
+  encoding: 'utf8',
+});
+const tools = MiseRegistry.parse(JSON.parse(output.stdout));
 
 const registry = MiseRegistryJson.parse(
   Object.fromEntries(
