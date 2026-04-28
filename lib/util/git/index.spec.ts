29fad4383c5a1e785fd533f3b9433e5c5efad76d
Felipe Santos <felipe.santos@ericsson.com>
2026-04-28 02:28:54 +0200

    Fix test

--- lib/util/git/index.spec.ts
+++ lib/util/git/index.spec.ts
@@ -453,9 +453,7 @@ describe('util/git/index', { timeout: 10000 }, () => {
       const defaultDate = await git.getBranchUpdateDate(defaultBranch);
       expect(date!.toISO()).toBe(defaultDate!.toISO());
       expect(date).toBeInstanceOf(DateTime);
-      expect(
-        updateDateCache.setCachedUpdateDateResult,
-      ).toHaveBeenCalledExactlyOnceWith(
+      expect(updateDateCache.setCachedUpdateDateResult).toHaveBeenCalledWith(
         'renovate/equal_branch',
         expect.any(DateTime),
       );
