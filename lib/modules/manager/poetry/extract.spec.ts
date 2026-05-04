17a484aa21d7b939ab8ad84b735d296cb079fa99
Jonas Kuske <mail@jonaskuske.com>
2026-05-04 09:16:20 +0200

    fix(poetry): perform deepmerge to fully enrich projectDep with info from poetryDep (#43064)
    * fix(poetry): perform deepmerge to fully enrich projectDep with info from poetryDep
    
    previously, just additional `managerData` from the poetryDep was merged into the projectDep specification
    
    * test(poetry): verify 'invalid-dependency-specification' if constraints specified twice

--- lib/modules/manager/poetry/extract.spec.ts
+++ lib/modules/manager/poetry/extract.spec.ts
@@ -580,6 +580,14 @@ describe('modules/manager/poetry/extract', () => {
       expect(res).toEqual({
         deps: [
           {
+            currentValue: '==2.0.0',
+            currentVersion: '2.0.0',
+            datasource: 'pypi',
+            depName: 'poetry-core',
+            depType: 'build-system.requires',
+            packageName: 'poetry-core',
+          },
+          {
             currentValue: '==4.11.2',
             currentVersion: '4.11.2',
             datasource: 'pypi',
@@ -597,14 +605,6 @@ describe('modules/manager/poetry/extract', () => {
               depGroup: 'decouple',
             },
             packageName: 'python-decouple',
-          },
-          {
-            currentValue: '==2.0.0',
-            currentVersion: '2.0.0',
-            datasource: 'pypi',
-            depName: 'poetry-core',
-            depType: 'build-system.requires',
-            packageName: 'poetry-core',
           },
         ],
         extractedConstraints: {},
@@ -663,13 +663,14 @@ describe('modules/manager/poetry/extract', () => {
   it('enriches pep621/pep735 dependencies with poetry managerData', async () => {
     const content = codeBlock`
         [project]
-        dependencies = ["click"]
+        dependencies = ["click", "pytest-cov==5.0.0"]
         [project.optional-dependencies]
         other = ["zoom==1.0.0"]
         [dependency-groups]
         typing = ["mypy==1.13.0", "types-requests"]
         [tool.poetry.dependencies]
-        click = { source = "artifactory" }
+        click = { version="==1.0.0", source = "artifactory" }
+        pytest-cov = { version = "==5.0.0" }
         zoom = { source = "artifactory" }
         [tool.poetry.group.typing.dependencies]
         types-requests = { source = "artifactory" }
@@ -682,18 +683,19 @@ describe('modules/manager/poetry/extract', () => {
         packageName: 'click',
         datasource: 'pypi',
         depType: 'project.dependencies',
-        skipReason: 'unspecified-version',
+        currentValue: '==1.0.0',
         depName: 'click',
-        managerData: { sourceName: 'artifactory' },
+        managerData: { nestedVersion: true, sourceName: 'artifactory' },
       },
       {
-        packageName: 'zoom',
+        currentValue: '==5.0.0',
+        currentVersion: '5.0.0',
         datasource: 'pypi',
-        depType: 'project.optional-dependencies',
-        currentValue: '==1.0.0',
-        currentVersion: '1.0.0',
-        depName: 'zoom',
-        managerData: { depGroup: 'other', sourceName: 'artifactory' },
+        depName: 'pytest-cov',
+        depType: 'project.dependencies',
+        managerData: {},
+        packageName: 'pytest-cov',
+        skipReason: 'invalid-dependency-specification',
       },
       {
         packageName: 'mypy',
@@ -711,6 +713,15 @@ describe('modules/manager/poetry/extract', () => {
         skipReason: 'unspecified-version',
         depName: 'types-requests',
         managerData: { depGroup: 'typing', sourceName: 'artifactory' },
+      },
+      {
+        packageName: 'zoom',
+        datasource: 'pypi',
+        depType: 'project.optional-dependencies',
+        currentValue: '==1.0.0',
+        currentVersion: '1.0.0',
+        depName: 'zoom',
+        managerData: { depGroup: 'other', sourceName: 'artifactory' },
       },
     ]);
   });
