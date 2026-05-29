3bc7447f6090366ff9276f11f160ae1027149518
Felipe Santos <felipe.santos@ericsson.com>
2026-05-29 15:23:53 +0200

    fix(npm): skip update when replacement dependency already exists

--- lib/modules/manager/npm/update/dependency/index.ts
+++ lib/modules/manager/npm/update/dependency/index.ts
@@ -117,38 +117,6 @@ function replaceAsString(
   throw new Error('Could not replace dependency');
 }
 
-function removeAsString(
-  parsedContents: NpmPackage,
-  fileContent: string,
-  depType: NpmDepType,
-  depName: string,
-  currentValue: string,
-): string {
-  delete parsedContents[depType]![depName];
-
-  const escapedDepName = escapeRegExp(depName);
-  const escapedValue = escapeRegExp(currentValue);
-  const keyValue = `"${escapedDepName}"\\s*:\\s*"${escapedValue}"`;
-  const regex = regEx(`(?:,\\s*${keyValue}|${keyValue}\\s*,)`);
-
-  // Skip ahead to depType section
-  const searchIndex = fileContent.indexOf(`"${depType}"`) + depType.length;
-  logger.trace(`Starting search at index ${searchIndex}`);
-
-  // Only search within the content starting from the depType section
-  const contentBeforeDepType = fileContent.slice(0, searchIndex);
-  const contentFromDepType = fileContent.slice(searchIndex);
-  const newContentFromDepType = contentFromDepType.replace(regex, '');
-  const newContent = contentBeforeDepType + newContentFromDepType;
-
-  // v8 ignore else -- TODO: add test #40625
-  if (dequal(parsedContents, JSON.parse(newContent))) {
-    return newContent;
-  } else {
-    throw new Error('Could not remove dependency');
-  }
-}
-
 export function updateDependency({
   fileContent,
   packageFile: packageFileName,
@@ -239,13 +207,10 @@ export function updateDependency({
         const newNameExists =
           parsedContents[depType as NpmDepType]?.[upgrade.newName];
         if (newNameExists) {
-          newFileContent = removeAsString(
-            parsedContents,
-            newFileContent,
-            depType as NpmDepType,
-            depName,
-            newValue!,
+          logger.debug(
+            `Replacement ${upgrade.newName} already exists, skipping update of ${depName}`,
           );
+          return null;
         } else {
           newFileContent = replaceAsString(
             parsedContents,
