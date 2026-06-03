e18e50dd1aea20ea4544461a11804a4104a571e8
Sharpened Blade <sharpenedblade@proton.me>
2026-06-03 14:36:29 -0700

    Update lib/config/options/index.ts
    Co-authored-by: RahulGautamSingh <rahultesnik@gmail.com>

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -1362,8 +1362,8 @@ const options: Readonly<RenovateOptions>[] = [
       'maven',
       'terraform',
       'vendir',
-      'custom.regex',
-      'custom.jsonata',
+      'regex',
+      'jsonata',
     ],
   },
   {
