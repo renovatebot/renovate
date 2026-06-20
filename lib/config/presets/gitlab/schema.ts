42bedb9a15633d8d2d3969e048bf8c0b25b26a88
secustor <sebastian@poxhofer.at>
2026-06-20 20:50:33 +0200

    remove unnecessary additional schema

--- lib/config/presets/gitlab/schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod/v4';
-
-export const GitlabProject = z.object({
-  default_branch: z.string().optional().nullable(),
-});
-export type GitlabProject = z.infer<typeof GitlabProject>;
