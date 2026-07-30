7c1f746539405aad4a76107f1e2ad7f2c607dd67
Adam Setch <adam.setch@outlook.com>
2026-07-30 11:32:33 -0400

    fix test
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- lib/util/markdown.spec.ts
+++ lib/util/markdown.spec.ts
@@ -92,7 +92,9 @@ describe('util/markdown', () => {
   describe('.sanitizeMarkdown', () => {
     it('should not add zero-width space to @ inside triple-backtick code blocks', () => {
       const input = codeBlock`
+        \`\`\`
         import foo from '@atlaskit/tokens';
+        \`\`\`
       `;
 
       expect(sanitizeMarkdown(input)).toEqual(input);
