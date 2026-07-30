5f4a8d57aaccca27ad66c1ec588723a611d89d7d
Adam Setch <adam.setch@outlook.com>
2026-07-30 09:12:15 -0400

    use codeblock
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- lib/util/markdown.spec.ts
+++ lib/util/markdown.spec.ts
@@ -91,7 +91,10 @@ describe('util/markdown', () => {
 
   describe('.sanitizeMarkdown', () => {
     it('should not add zero-width space to @ inside triple-backtick code blocks', () => {
-      const input = "```\nimport foo from '@atlaskit/tokens';\n```";
+      const input = codeBlock`
+        import foo from '@atlaskit/tokens';
+      `;
+
       expect(sanitizeMarkdown(input)).toEqual(input);
     });
 
