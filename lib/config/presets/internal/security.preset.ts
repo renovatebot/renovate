f4fce1da80944dcf48de1bc14c9f64ffda95da3a
Tobias Bieniek <tobias@bieniek.cloud>
2026-07-31 15:15:47 +0200

    Update lib/config/presets/internal/security.preset.ts
    Co-authored-by: Jamie Tanna <github@jamietanna.co.uk>

--- lib/config/presets/internal/security.preset.ts
+++ lib/config/presets/internal/security.preset.ts
@@ -25,7 +25,7 @@ export const presets: Record<string, Preset> = {
   },
   minimumReleaseAgeCrate: {
     description:
-      "Wait until the crates.io crate version is three days old before raising the update. This a) introduces a short delay to allow for malware researchers and scanners to (possibly) detect any malicious behaviour in crates, and b) reduces the risk of upgrading to a newly published crate that its owner deletes during crates.io's initial 72-hour deletion window.",
+      "Wait until the Crate version is three days old before raising the update. This a) introduces a short delay to allow for malware researchers and scanners to (possibly) detect any malicious behaviour in crates, and b) reduces the risk of upgrading to a newly published crate that its owner deletes during crates.io's initial 72-hour deletion window.",
     packageRules: [
       {
         internalChecksFilter: 'strict',
