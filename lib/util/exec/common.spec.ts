bb1cae6bcc8fd03f8378e6864267eac7e20f8f5a
Dharma Teja Thoutam <dharma.teja.thoutam@ericsson.com>
2026-08-28 15:08:29 +0200

    fix: add type annotation for catch mock parameter

--- lib/util/exec/common.spec.ts
+++ lib/util/exec/common.spec.ts
@@ -155,7 +155,7 @@ function getSpawnStub(args: StubArgs): any {
     unref,
     kill,
     pid,
-    catch: (fn) => fn(new Error('mock')),
+    catch: (fn: (err: Error) => void) => fn(new Error('mock')),
   };
 }
 
