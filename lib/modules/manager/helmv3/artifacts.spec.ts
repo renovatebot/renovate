707a285f20efb307098e462f36d5a6ff2807548e
Iridias <iridias@tiphares.de>
2026-09-02 15:05:40 +0200

    add replacement for the former helmv3 test 'prevents injections'

--- lib/modules/manager/helmv3/artifacts.spec.ts
+++ lib/modules/manager/helmv3/artifacts.spec.ts
@@ -1175,4 +1175,40 @@ describe('modules/manager/helmv3/artifacts', () => {
       },
     ]);
   });
+
+  it('prevents injections when no credentials are configured for ECR auth', async () => {
+    const username = 'user';
+    const password = 'pass>word';
+    mockEcrAuthResolve({
+      authorizationData: [
+        { authorizationToken: toBase64(`${username}:${password}`) },
+      ],
+    });
+
+    hostRules.add({
+      hostType: 'docker',
+      matchHost: '123456789.dkr.ecr.us-east-1.amazonaws.com',
+    });
+    fs.getSiblingFileName.mockReturnValueOnce('Chart.lock');
+    fs.readLocalFile.mockResolvedValueOnce(ociLockFile1ECR);
+    fs.privateCacheDir.mockReturnValue(
+      '/tmp/renovate/cache/__renovate-private-cache',
+    );
+    fs.getParentDir.mockReturnValue('');
+    const execSnapshots = mockExecAll();
+    await helmv3.updateArtifacts({
+      packageFileName: 'Chart.yaml',
+      updatedDeps: [{}],
+      newPackageFileContent: `dependencies: { repository: oci://123456789.dkr.ecr.us-east-1.amazonaws.com/bitnami || date }`,
+      config: { ...config },
+    });
+    expect(execSnapshots).toMatchObject([
+      {
+        cmd: `helm registry login --username ${username} --password '${password}' '123456789.dkr.ecr.us-east-1.amazonaws.com/bitnami || date'`,
+      },
+      {
+        cmd: "helm dependency update ''",
+      },
+    ]);
+  });
 });
