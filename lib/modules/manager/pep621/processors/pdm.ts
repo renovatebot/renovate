ae546a7fc3eb1f484a2c9a53846636bdd12bd396
astein-mam-dev <astein@1und1.de>
2026-09-09 18:17:47 +0200

    feat(manager/pep621): add PDM lock update strategy

--- lib/modules/manager/pep621/processors/pdm.ts
+++ lib/modules/manager/pep621/processors/pdm.ts
@@ -23,7 +23,8 @@ import type { Pep621ManagerData } from '../types.ts';
 import { depTypes } from '../utils.ts';
 import { BasePyProjectProcessor } from './abstract.ts';
 
-const pdmUpdateCMD = 'pdm update --no-sync --update-eager';
+const pdmUpdateEagerCMD = 'pdm update --no-sync --update-eager';
+const pdmUpdateAllCMD = 'pdm update --no-sync --update-all';
 const gitExec = withGitEnvironment(['pep621']);
 
 export class PdmProcessor extends BasePyProjectProcessor {
@@ -118,7 +119,11 @@ export class PdmProcessor extends BasePyProjectProcessor {
       // else only update specific packages
       const cmds: string[] = [];
       if (isLockFileMaintenance) {
-        cmds.push(pdmUpdateCMD);
+        cmds.push(
+          config.pdmUpdateStrategy === 'all'
+            ? pdmUpdateAllCMD
+            : pdmUpdateEagerCMD,
+        );
       } else {
         cmds.push(...generateCMDs(updatedDeps));
       }
@@ -173,7 +178,7 @@ function generateCMDs(updatedDeps: Upgrade<Pep621ManagerData>[]): string[] {
         }
         addPackageToCMDRecord(
           packagesByCMD,
-          `${pdmUpdateCMD} -G ${quote(dep.managerData.depGroup)}`,
+          `${pdmUpdateEagerCMD} -G ${quote(dep.managerData.depGroup)}`,
           dep.packageName!,
         );
         break;
@@ -189,7 +194,7 @@ function generateCMDs(updatedDeps: Upgrade<Pep621ManagerData>[]): string[] {
         }
         addPackageToCMDRecord(
           packagesByCMD,
-          `${pdmUpdateCMD} -dG ${quote(dep.managerData.depGroup)}`,
+          `${pdmUpdateEagerCMD} -dG ${quote(dep.managerData.depGroup)}`,
           dep.packageName!,
         );
         break;
@@ -199,7 +204,11 @@ function generateCMDs(updatedDeps: Upgrade<Pep621ManagerData>[]): string[] {
         // Reference: https://github.com/pdm-project/pdm/discussions/2869
         break;
       default: {
-        addPackageToCMDRecord(packagesByCMD, pdmUpdateCMD, dep.packageName!);
+        addPackageToCMDRecord(
+          packagesByCMD,
+          pdmUpdateEagerCMD,
+          dep.packageName!,
+        );
       }
     }
   }
