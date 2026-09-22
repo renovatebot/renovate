b7fd8cb553fd6cd1c44b3f047c2d9a1fca89d84b
Aleksandr Mezin <mezin.alexander@gmail.com>
2026-09-22 09:51:56 +0300

    Improve util tests

--- lib/util/compress.spec.ts
+++ lib/util/compress.spec.ts
@@ -34,44 +34,60 @@ describe('util/compress', () => {
     expect(decompressed).toBe(input);
   });
 
-  it.each(['', 'foo', 'foob', 'foobar'])(
-    'autodetects and passes through non-compressed stream with data %j',
-    async (input) => {
-      const noncompressedStream = Readable.from(Buffer.from(input), {
-        objectMode: false,
-      });
-      const decompressStream =
-        await createDecompressStream(noncompressedStream);
-      expect(decompressStream).toBe(noncompressedStream);
-      const decompressed = await streamToString(decompressStream);
-      expect(decompressed).toBe(input);
-    },
-  );
+  describe('createDecompressStream', () => {
+    it.each(['', 'foo', 'foob', 'foobar'])(
+      'autodetects and passes through non-compressed stream with data %j',
+      async (input) => {
+        const noncompressedStream = Readable.from(Buffer.from(input), {
+          objectMode: false,
+        });
+        const decompressStream =
+          await createDecompressStream(noncompressedStream);
+        expect(decompressStream).toBe(noncompressedStream);
+        const decompressed = await streamToString(decompressStream);
+        expect(decompressed).toBe(input);
+      },
+    );
 
-  it.each([
-    { name: 'gzip', compress: gzip },
-    { name: 'zstdCompress', compress: zstdCompress },
-  ])(
-    'autodetects and decompresses stream of data compressed with $name',
-    async ({ compress }) => {
-      const input = 'foobarbarfoofoobarfoobarfoobarfoobarfoobar!';
-      const compressedStream = Readable.from(await compress(input), {
-        objectMode: false,
-      });
-      const decompressed = await streamToString(
-        await createDecompressStream(compressedStream),
-      );
-      expect(decompressed).toBe(input);
-    },
-  );
+    it.each([
+      { name: 'gzip', compress: gzip },
+      { name: 'zstdCompress', compress: zstdCompress },
+    ])(
+      'autodetects and decompresses stream of data compressed with $name',
+      async ({ compress }) => {
+        const input = 'foobarbarfoofoobarfoobarfoobarfoobarfoobar!';
+        const compressedStream = Readable.from(await compress(input), {
+          objectMode: false,
+        });
+        const decompressed = await streamToString(
+          await createDecompressStream(compressedStream),
+        );
+        expect(decompressed).toBe(input);
+      },
+    );
 
-  it('rejects if input stream fails before any data', async () => {
-    const error = new Error('stream error');
-    const inputStream = new PassThrough();
+    it('rejects if input stream fails before any data', async () => {
+      const error = new Error('stream error');
+      const inputStream = new PassThrough({ objectMode: false });
 
-    const result = createDecompressStream(inputStream);
-    inputStream.destroy(error);
+      const result = createDecompressStream(inputStream);
+      inputStream.destroy(error);
 
-    await expect(result).rejects.toThrow(error);
+      await expect(result).rejects.toThrow(error);
+    });
+
+    it('rejects if input stream fails after the magic number', async () => {
+      const error = new Error('stream error');
+      const inputStream = new PassThrough({ objectMode: false });
+
+      inputStream.write(Uint8Array.from([0x1f, 0x8b, 0xab, 0xbc, 0xcd]));
+      const decompressor = await createDecompressStream(inputStream);
+
+      expect(decompressor).toBeInstanceOf(zlib.Gunzip);
+
+      inputStream.destroy(error);
+
+      await expect(decompressor.toArray()).rejects.toThrow(error);
+    });
   });
 });
