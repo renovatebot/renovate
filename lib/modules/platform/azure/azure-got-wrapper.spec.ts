4f9e99e647b47e735857bfc25f36c0152dc2227a
Alex Smedin <alex.smedin@outlook.com>
2026-09-23 13:13:44 +0200

    Add tests for additional endpoint formats

--- lib/modules/platform/azure/azure-got-wrapper.spec.ts
+++ lib/modules/platform/azure/azure-got-wrapper.spec.ts
@@ -165,6 +165,69 @@ describe('modules/platform/azure/azure-got-wrapper', () => {
         },
       });
     });
+
+    it('should normalize simple TFS collection endpoint', () => {
+      hostRules.add({
+        hostType: 'azure',
+        token: '123test',
+        matchHost: 'https://devops.example.com/tfs/MyCollection',
+      });
+      azure.setEndpoint('https://devops.example.com/tfs/MyCollection');
+
+      const res = azure.azureObj();
+
+      delete res.rest.client.userAgent;
+      delete res.vsoClient.restClient.client.userAgent;
+
+      expect(res).toMatchObject({
+        serverUrl: 'https://devops.example.com/tfs',
+        authHandler: {
+          token: '123test',
+        },
+      });
+    });
+
+    it('should not normalize cloud endpoint with single path segment', () => {
+      hostRules.add({
+        hostType: 'azure',
+        token: '123test',
+        matchHost: 'https://dev.azure.com/organization',
+      });
+      azure.setEndpoint('https://dev.azure.com/organization');
+
+      const res = azure.azureObj();
+
+      delete res.rest.client.userAgent;
+      delete res.vsoClient.restClient.client.userAgent;
+
+      expect(res).toMatchObject({
+        serverUrl: 'https://dev.azure.com/organization',
+        authHandler: {
+          token: '123test',
+        },
+      });
+    });
+
+    it('should return original endpoint for invalid URL', () => {
+      hostRules.add({
+        hostType: 'azure',
+        token: '123test',
+        matchHost: 'not-a-valid-url',
+      });
+      azure.setEndpoint('not-a-valid-url');
+
+      const res = azure.azureObj();
+
+      delete res.rest.client.userAgent;
+      delete res.vsoClient.restClient.client.userAgent;
+
+      expect(res).toMatchObject({
+        serverUrl: 'not-a-valid-url',
+        authHandler: {
+          token: '123test',
+        },
+      });
+    });
   });
 
   describe('isHosted', () => {
